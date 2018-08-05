18b880dfbf34c5e83bea5d557b12d912512dadb6
Raine Revere <raineorshine@gmail.com>
2018-08-04 19:15:16 -0600

    Slugify and uuid ids.

--- opml-to-data.js
+++ opml-to-data.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const xml2json = require('xml2json')
+const slugify = require('@sindresorhus/slugify')
+const uuid = require('uuid/v4')
 
 const dataRaw = fs.readFileSync('./data.opml', 'utf-8')
 const data = xml2json.toJson(dataRaw, { object: true }).opml.body
@@ -7,21 +9,23 @@ const data = xml2json.toJson(dataRaw, { object: true }).opml.body
 const isLeaf = item => !Array.isArray(item.outline)
 
 let i = 0
-const max = 1000
+const max = 300
+const slugLength = 16
+const uuidLength = 8
 
 const dataToObject = (o, start={}, lvl=0) => {
 
   if (i++ > max) return {}
 
   return o.outline.reduce((accum, item) => {
+    const id = slugify(item.text).slice(0, slugLength) + '-' + uuid().slice(0, uuidLength)
     return Object.assign({},
       // accumulated keys
       accum,
       // current
-      { [item.text]: accum[item.text] ? { /* TODO */ } : {
-        id: item.text,
+      { [id]: accum[id] || {
+        id,
         value: item.text,
-        parents: [],
         children: !isLeaf(item) ? item.outline.map(child => child.text) : []
       } },
       // children
