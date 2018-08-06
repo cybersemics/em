bf5a9fdca22b3d7db8314aa9bda6b5ae38f80b62
Raine Revere <raineorshine@gmail.com>
2018-08-05 19:38:11 -0600

    Ontological linking with merged parents.

--- opml-to-data.js
+++ opml-to-data.js
@@ -1,29 +1,41 @@
 const fs = require('fs')
 const xml2json = require('xml2json')
 const slugify = require('@sindresorhus/slugify')
-const uuid = require('uuid/v4')
 const md5 = require('md5')
 
 /**************************************************************
  * Constants
  **************************************************************/
 
-const maxRecursion = 3000
+const maxRecursion = 300
 const slugLength = 16
 const hashLength = 8
-const dataFile = './data.opml'
+const dataFile = process.argv[2] || './data.opml'
 const SEP = '|SEPARATOR_TOKEN|'
 
 /**************************************************************
  * Helpers
  **************************************************************/
 
-// create an id unique to the item and its ancestors
-const idify = (item, ancestors) => slugify(item.text).slice(0, slugLength) + '-' + md5(item.text + SEP + ancestors.join(SEP)).slice(0, hashLength)
-// const idify = item => slugify(item.text).slice(0, slugLength) + '-' + uuid().slice(0, uuidLength)
+/*
+// create an id unique to the item's value and its ancestors
+const idAncestry = (item, ancestors) => slugify(item.text).slice(0, slugLength) + '-' + md5(item.text + SEP + ancestors.join(SEP)).slice(0, hashLength)
+*/
+
+// create an id unique to the item's value
+const idValue = item => slugify(item.text).slice(0, slugLength) + '-' + md5(slugify(item.text)).slice(0, hashLength)
+
 // item.outline may be an array or an object if the item has a single child so it must be normalied to an array
 const children = item => item.outline ? [].concat(item.outline) : []
+
 const isLeaf = item => children(item).length === 0
+
+const unique = list => {
+  const o = {}
+  const out = []
+  list.forEach(item => o[item] = true)
+  return Object.keys(o)
+}
 
 let i = 0
 const dataToObject = (startItem, initial={}, ancestors=[], lvl=0) => {
@@ -31,7 +43,7 @@ const dataToObject = (startItem, initial={}, ancestors=[], lvl=0) => {
   if (i++ > maxRecursion) return {}
 
   return children(startItem).reduce((accum, item) => {
-    const id = idify(item, ancestors)
+    const id = idValue(item, ancestors)
     return Object.assign({},
       // accumulated keys
       accum,
@@ -39,18 +51,22 @@ const dataToObject = (startItem, initial={}, ancestors=[], lvl=0) => {
       { [id]: {
         id,
         value: item.text,
-        // no duplicate ids
-        // children: !isLeaf(item) ? children(item).map(idify) : []
+        /*
+        // unique by ancestors
+        children: children(item).map(child => idAncestry(child, ancestors)))
+        */
+        parents: ancestors.length > 0
+          ? unique((accum[id] ? accum[id].parents : []).concat(ancestors[ancestors.length-1]))
+          : [],
         // merge children from categories with same name
-        children: children(item).map(item => idify(item, ancestors.concat(id)))
-          // // non-leaf
-          // ? accum[id] && accum[id].children.length > 0
-          //   // category already exists
-          //   ? accum[id].children.concat(children(item).map(child => '!!! ' + ancestors.join('/') + ' ' + idify(child, ancestors)))
-          //   // new category
-          //   : children(item).map(item => idify(item, ancestors))
-          // // leaf
-          // : []
+        children: !isLeaf(item) ?
+          // if category already exists...
+          accum[id] && accum[id].children.length > 0 ?
+            // merge children
+            accum[id].children.concat(children(item).map(child => '!!! ' + ancestors.join('/') + ' ' + idValue(child)))
+            // else create new category
+            : children(item).map(idValue)
+          : []
       } },
       // children
       !isLeaf(item) ? dataToObject(item, accum, ancestors.concat(id), lvl + 1) : {}
@@ -70,7 +86,7 @@ const objWithRoot = Object.assign({}, obj, {
   root: {
     id: 'root',
     value: '',
-    children: data.outline.map(item => idify(item, []))
+    children: data.outline.map(item => idValue(item, []))
   }
 })
 
