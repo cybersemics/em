e94b6df91be9ab2b8b203a5c446d9c38e851c955
Raine Revere <raineorshine@gmail.com>
2018-08-08 08:09:06 -0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -81,64 +81,26 @@ const AppComponent = connect(state => ({ focus: state.focus }))(({ focus, dispat
 
 const Context = connect()(({ items, depth=0, label, derived, dispatch }) => {
 
-  // missing (e.g. due to rendering only a subset of items)
-  // if (!(items[items.length - 1] in data)) {
-  //   return <div className={'item missing container-depth' + depth}>{items}</div>
-  // }
-
   const children = getChildren(items)
 
   // derived children are items that the current context (items) is a non-leaf memberOf
   // only generate derived children at top level of view
-  // let x = 0
-  // console.log('items', items)
   const derivedChildren = depth === 0 ? uniqueSet(Array.prototype.concat.apply([], Object.keys(data).map(key =>
-    data[key].memberOf.filter(parent => {
-      // if (x < 20 && superSet(parent, items) &&
-      //   !deepEqual(parent, items)
-      //   && parent[parent.length - 1] === items[items.length - 1]
-      // ) {
-      //   console.log('!', key, parent)
-      //   x++
-      // }
-      return superSet(parent, items) &&
-        !deepEqual(parent, items)
-        && parent[parent.length - 1] === items[items.length - 1]
-    })
+    data[key].memberOf.filter(parent =>
+      superSet(parent, items) &&
+      !deepEqual(parent, items) &&
+      parent[parent.length - 1] === items[items.length - 1]
+    )
   ))) : []
 
-  // console.log('derivedChildren', derivedChildren)
-
-  // group derived items
-  // const groups = derivedChildren.reduce((accum, key) => {
-  //   return data[key].memberOf.reduce((accum, items) => {
-  //     return Object.assign({}, accum, {
-  //       [items.join(SEP)]: items
-  //     })
-  //   }, accum)
-  // }, {})
-
-  // XOR with getChildren
-  // const groupDerivedChildren = Object.values(groups).filter(groupItems =>
-  //   !children.some(childValue => deepEqual(groupItems, items.concat(childValue)))
-  // )
-
-  // if (depth === 0) {
-  //   console.log(groups)
-  // }
-
   const otherContexts = data[items[items.length - (derived ? 2 : 1)]].memberOf
-  // TODO: filter out contexts where this item is a leaf
-  // .filter(parent => getChildren(parent).length > 0)
-  // console.log(items, derived)
-
-  const root = items[0] === 'root'
+  const isRoot = items[0] === 'root'
   const isLeaf = children.length === 0 && derivedChildren.length === 0
 
   return <div className={'item-container container-depth' + depth + (isLeaf ? ' leaf' : '')}>
 
     { // item
-    !root ? <div className={'item depth' + depth}>
+    !isRoot ? <div className={'item depth' + depth}>
 
       <div>
 
@@ -161,12 +123,12 @@ const Context = connect()(({ items, depth=0, label, derived, dispatch }) => {
 
     { // direct children
     depth < (derived ? 2 : 1) ? <div className='direct'>
-      {children.map((childValue, i) => <Context key={i} items={(root ? [] : items).concat(childValue)} depth={depth + (root ? 0 : 1)}/>)}
+      {children.map((childValue, i) => <Context key={i} items={(isRoot ? [] : items).concat(childValue)} depth={depth + (isRoot ? 0 : 1)}/>)}
     </div> : null}
 
     { // derived children
     depth < 1 && children.length === 0 ? <div className='derived'>
-      {derivedChildren.map((items, i) => <Context key={i} items={items} label={items.filter(item => item !== items[items.length - 1]).join(' + ')} depth={depth + (root ? 0 : 1)} derived={true} />)}
+      {derivedChildren.map((items, i) => <Context key={i} items={items} label={items.filter(item => item !== items[items.length - 1]).join(' + ')} depth={depth + (isRoot ? 0 : 1)} derived={true} />)}
     </div> : null
     }
 
