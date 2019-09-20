ad8008c90869c7ec1ee9721bd448cdf159c02d23
Raine Revere <raineorshine@gmail.com>
2019-09-20 10:23:48 -0600

    Add refresh link in footer for debugging purposes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,7 +25,9 @@ export const Footer = connect(({ status, settings, user }) => ({ status, setting
     cursorBack()
   }}>
     <li>
-      <a tabIndex='-1'/* TODO: Add setting to enable tabIndex for accessibility */ className='settings-dark' onClick={() => dispatch({ type: 'settings', key: 'dark', value: !settings.dark })}>Dark Mode</a>
+      <a tabIndex='-1'/* TODO: Add setting to enable tabIndex for accessibility */ onClick={() => dispatch({ type: 'settings', key: 'dark', value: !settings.dark })}>Dark Mode</a>
+      <span> | </span>
+      <a tabIndex='-1' onClick={() => window.location.reload()}>Refresh</a>
       <span> | </span>
       <a tabIndex='-1' href='https://forms.gle/ooLVTDNCSwmtdvfA8' target='_blank' rel='noopener noreferrer'>Feedback <img src={`https://img.icons8.com/small/16/${settings.dark ? '87ceeb' : '1b6f9a'}/open-in-popup.png`} alt='' style={{ verticalAlign: 'middle' }}/></a>
       <span> | </span>
@@ -35,8 +37,8 @@ export const Footer = connect(({ status, settings, user }) => ({ status, setting
       }}>{isMobile ? 'Gestures' : 'Shortcuts'}</a>
       {window.firebase ? <span>
         <span> | </span>
-        {status === 'offline' || status === 'disconnected' || status === 'connected' ? <a tabIndex='-1' className='settings-logout' onClick={login}>Log In</a>
-        : <a tabIndex='-1' className='settings-logout' onClick={logout}>Log Out</a>
+        {status === 'offline' || status === 'disconnected' || status === 'connected' ? <a tabIndex='-1' onClick={login}>Log In</a>
+        : <a tabIndex='-1' onClick={logout}>Log Out</a>
         }
       </span> : null}
     </li><br/>
