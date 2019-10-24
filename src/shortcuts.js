08cdda4457cafe9890f34e319b0629bf8779dddc
Tara Hackley <tarascript1@gmail.com>
2019-10-24 14:43:32 -0600

    Add keyboard shortcut to open shortcut popup. Fixes #101.

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -513,6 +513,17 @@ export const globalShortcuts = perma(() => [
     description: 'Navigate to Home.',
     keyboard: { key: 'h', shift: true, meta: true },
     exec: home
+  },
+
+  {
+    id: 'openShortcutPopup',
+    name: 'Open Shortcut Popup',
+    description: `Open the help screen which contains the tutorials and a list of all ${ isMobile ? 'gestures' : 'keyboard shortcuts'}.`,
+    keyboard: { key: '/', meta: true},
+    exec: e => {
+      window.scrollTo(0, 0)
+      store.dispatch({ type: 'showHelper', id: 'shortcuts' })
+    }
   }
 ]
 
