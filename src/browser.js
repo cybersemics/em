5fa807445b58e905a5926497beffa5f56798c340
Paul <41552663+molecula451@users.noreply.github.com>
2019-11-26 04:30:14 -0400

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -5,6 +5,7 @@ import * as uuid from 'uuid/v4'
 export const isMobile = /Mobile/.test(navigator.userAgent)
 export const isMac = navigator.platform === 'MacIntel'
 export const isAndroid = navigator.platform === 'Android'
+export const isWindows = navigator.platform === 'Windows'
 
 // Use clientId to ignore value events from firebase originating from this client in this session
 // This approach has a possible race condition though. See https://stackoverflow.com/questions/32087031/how-to-prevent-value-event-on-the-client-that-issued-set#comment100885493_32107959
