b52a071e39aaf9dd0f738cd52f4f4932cde5969f
Raine Revere <raine@cybersemics.org>
2019-11-26 15:41:10 -0700

    existingItemDelete: Fix descendant contextChildren updates. #182.

--- src/reducers/existingItemDelete.js
+++ src/reducers/existingItemDelete.js
@@ -22,7 +22,7 @@ export const existingItemDelete = (state, { itemsRanked, rank, showContexts }) =
   const value = signifier(items)
   const item = getThought(value, state.data)
   const context = rootedIntersections(items)
-  const newData = Object.assign({}, state.data)
+  const newData = { ...state.data }
 
   // the old item less the context
   const newOldItem = item.memberOf && item.memberOf.length > 1
@@ -41,22 +41,10 @@ export const existingItemDelete = (state, { itemsRanked, rank, showContexts }) =
   const itemChildren = (state.contextChildren[contextEncoded] || [])
     .filter(child => !equalItemRanked(child, { key: value, rank }))
 
-  // if removing an item from a context via the context view and the context has no more members or contexts, delete the context
-  // const isItemOldOrphan = () => !item.memberOf || item.memberOf.length < 2
-  // const isItemOldChildless = () => getChildrenWithRank([value], newData).length < 2
-  // let emptyContextDelete = {}
-  // if(showContexts && getChildrenWithRank(intersections(items), newData).length === 0) {
-    // const emptyContextValue = signifier(intersections(items))
-    // delete newData[hashThought(emptyContextValue)]
-    // localStorage.removeItem('data-' + emptyContextValue)
-    // emptyContextDelete = {
-    //   [emptyContextValue]: null
-    // }
-  // }
-
   // generates a firebase update object that can be used to delete/update all descendants and delete/update contextChildren
-  const recursiveDeletes = itemsRanked => {
+  const recursiveDeletes = (itemsRanked, accumRecursive = {}) => {
     return getChildrenWithRank(itemsRanked, newData, state.contextChildren).reduce((accum, child) => {
+      const hashedKey = hashThought(child.key)
       const childItem = getThought(child.key, newData)
       const childNew = childItem && childItem.memberOf && childItem.memberOf.length > 1
         // update child with deleted context removed
@@ -66,63 +54,79 @@ export const existingItemDelete = (state, { itemsRanked, rank, showContexts }) =
 
       // update local data so that we do not have to wait for firebase
       if (childNew) {
-        newData[hashThought(child.key)] = childNew
+        newData[hashedKey] = childNew
       }
       else {
-        delete newData[hashThought(child.key)] // eslint-disable-line fp/no-delete
+        delete newData[hashedKey] // eslint-disable-line fp/no-delete
       }
 
-      return Object.assign({}, accum,
-        // direct child
-        {
-          [hashThought(child.key)]: {
-            key: child.key,
-            data: childNew,
-            context: unrank(itemsRanked)
-          }
+      const contextEncoded = encodeItems(unrank(itemsRanked))
+
+      const dataMerged = {
+        ...accumRecursive.data,
+        ...accum.data,
+        [hashedKey]: childNew
+      }
+
+      const contextChildrenMerged = {
+        ...accumRecursive.contextChildren,
+        ...accum.contextChildren,
+        [contextEncoded]: null
+      }
+
+      // RECURSION
+      const recursiveResults = recursiveDeletes(itemsRanked.concat(child), {
+        data: dataMerged,
+        contextChildren: contextChildrenMerged
+      })
+
+      return {
+        data: {
+          ...dataMerged,
+          ...recursiveResults.data
         },
-        recursiveDeletes(itemsRanked.concat(child)) // RECURSIVE
-      )
-    }, {})
+        contextChildren: {
+          ...contextChildrenMerged,
+          ...recursiveResults.contextChildren
+        }
+      }
+    }, {
+      data: {},
+      contextChildren: {}
+    })
   }
 
   // do not delete descendants when the thought has a duplicate sibling
-  const duplicateSiblings = itemChildren.filter(child => hashThought(child.key) === hashThought(value))
-  const descendantUpdatesResult = duplicateSiblings.length === 0
+  const hasDuplicateSiblings = itemChildren.some(child => hashThought(child.key) === hashThought(value))
+  const descendantUpdatesResult = !hasDuplicateSiblings
     ? recursiveDeletes(itemsRanked)
-    : {}
-  const descendantUpdates = Object.keys(descendantUpdatesResult).reduce((accum, hashedKey) =>
-    Object.assign({}, accum, {
-      [hashedKey]: descendantUpdatesResult[hashedKey].data
-    })
-  , {})
-
-  const contextChildrenDescendantUpdates = Object.keys(descendantUpdatesResult).reduce((accum, hashedKey) => {
-    const encodedContextRecursive = encodeItems(descendantUpdatesResult[hashedKey].context)
-    return Object.assign({}, accum, {
-      [encodedContextRecursive]: (accum[encodedContextRecursive] || state.contextChildren[encodedContextRecursive] || [])
-        .filter(child => child.key !== descendantUpdatesResult[hashedKey].key),
-    })
-  }, {})
+    : {
+      data: {},
+      contextChildren: {}
+    }
 
   const dataUpdates = {
     [hashThought(value)]: newOldItem,
-    ...descendantUpdates,
+    ...descendantUpdatesResult.data,
     // emptyContextDelete
   }
 
   const contextChildrenUpdates = {
+    // current thought
     [contextEncoded]: itemChildren.length > 0 ? itemChildren : null,
-    ...contextChildrenDescendantUpdates
+    // descendants
+    ...descendantUpdatesResult.contextChildren
   }
   const newContextChildren = Object.assign({}, state.contextChildren, contextChildrenUpdates)
 
-  // null values not needed in state
+  // null values must be manually deleted in state
+  // current thought
   if (!itemChildren || itemChildren.length === 0) {
     delete newContextChildren[contextEncoded] // eslint-disable-line fp/no-delete
   }
-  Object.keys(contextChildrenDescendantUpdates).forEach(contextEncoded => {
-    const itemChildren = contextChildrenDescendantUpdates[contextEncoded]
+  // descendants
+  Object.keys(descendantUpdatesResult.contextChildren).forEach(contextEncoded => {
+    const itemChildren = descendantUpdatesResult.contextChildren[contextEncoded]
     if (!itemChildren || itemChildren.length === 0) {
       delete newContextChildren[contextEncoded] // eslint-disable-line fp/no-delete
     }
@@ -134,7 +138,7 @@ export const existingItemDelete = (state, { itemsRanked, rank, showContexts }) =
   })
 
   return {
-    data: Object.assign({}, newData),
+    data: newData,
     dataNonce: state.dataNonce + 1,
     contextChildren: newContextChildren
   }
