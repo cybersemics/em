1f3f8e58f9dcd7415f3488ef60dea6947ebf43a3
Paul <41552663+molecula451@users.noreply.github.com>
2019-11-30 11:21:45 -0400

    Update AppComponent.js

--- src/components/AppComponent.js
+++ src/components/AppComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as classNames from 'classnames'
-import { isMobile } from '../browser.js'
+import { isMobile, isAndroid } from '../browser.js'
 import { store } from '../store.js'
 import globals from '../globals.js'
 import { handleGesture } from '../shortcuts.js'
