4e45eca939886529a9380d911e7560ad3213b223
Raine Revere <raine@cybersemics.org>
2019-12-09 14:45:11 -0700

    External Link: Change icon, fix light mode, clean up positioning (#71).

--- src/components/ThoughtAnnotation.js
+++ src/components/ThoughtAnnotation.js
@@ -19,7 +19,7 @@ import {
 } from '../util.js'
 
 /** A non-interactive annotation overlay that contains intrathought links (superscripts and underlining). */
-export const ThoughtAnnotation = connect(({ cursor, cursorBeforeEdit, focusOffset }, props) => {
+export const ThoughtAnnotation = connect(({ cursor, cursorBeforeEdit, focusOffset, settings: { dark } = {} }, props) => {
 
   // reerender annotation in realtime when thought is edited
   const itemsResolved = props.contextChain && props.contextChain.length > 0
@@ -31,11 +31,12 @@ export const ThoughtAnnotation = connect(({ cursor, cursorBeforeEdit, focusOffse
     : props.itemsRanked
 
   return {
+    dark,
     itemsRanked: itemsRankedLive,
     isEditing,
     focusOffset
   }
-})(({ itemsRanked, showContexts, showContextBreadcrumbs, contextChain, homeContext, isEditing, focusOffset, minContexts = 2, isLinkParent, childLink }) => {
+})(({ dark, itemsRanked, showContexts, showContextBreadcrumbs, contextChain, homeContext, isEditing, focusOffset, minContexts = 2, isLinkParent, childLink }) => {
 
   // disable intrathought linking until add, edit, delete, and expansion can be implemented
   // get all subthoughts and the subthought under the selection
@@ -71,8 +72,20 @@ export const ThoughtAnnotation = connect(({ cursor, cursorBeforeEdit, focusOffse
           {isLinkParent
             // eslint-disable-next-line no-undef
             ? <a href={childLink} rel="noopener noreferrer" target="_blank">
-                <svg style={{ marginLeft: '2px', cursor: 'pointer', pointerEvents: 'all' }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-                  <path fill="#ffffff" d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                  width="18" height="18"
+                  viewBox="0 0 30 30"
+                  style={{
+                    marginLeft: '2px',
+                    marginTop: '-4px',
+                    padding: '3px 3px 3px 1px',
+                    cursor: 'pointer',
+                    pointerEvents: 'all'
+                  }}
+                ><path
+                  fill='#838283'
+                  d="M 25.980469 2.9902344 A 1.0001 1.0001 0 0 0 25.869141 3 L 20 3 A 1.0001 1.0001 0 1 0 20 5 L 23.585938 5 L 13.292969 15.292969 A 1.0001 1.0001 0 1 0 14.707031 16.707031 L 25 6.4140625 L 25 10 A 1.0001 1.0001 0 1 0 27 10 L 27 4.1269531 A 1.0001 1.0001 0 0 0 25.980469 2.9902344 z M 6 7 C 4.9069372 7 4 7.9069372 4 9 L 4 24 C 4 25.093063 4.9069372 26 6 26 L 21 26 C 22.093063 26 23 25.093063 23 24 L 23 14 L 23 11.421875 L 21 13.421875 L 21 16 L 21 24 L 6 24 L 6 9 L 14 9 L 16 9 L 16.578125 9 L 18.578125 7 L 16 7 L 14 7 L 6 7 z">
+                  </path>
                 </svg>
               </a>
             : null
