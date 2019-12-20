04b2684ef4a984c2013631b68fefbc1701d5dfbd
Edson Ayllon <eayllon1@gmail.com>
2019-12-20 16:47:43 -0500

    lint

--- src/util/ellipsize.js
+++ src/util/ellipsize.js
@@ -1,5 +1,3 @@
 import { THOUGHT_ELLIPSIZED_CHARS } from '../constants.js'
 
-export const ellipsize = s => s.length > THOUGHT_ELLIPSIZED_CHARS
-  ? s.slice(0, THOUGHT_ELLIPSIZED_CHARS) + '...'
-  : s
+export const ellipsize = s => s.length > THOUGHT_ELLIPSIZED_CHARS ? s.slice(0, THOUGHT_ELLIPSIZED_CHARS) + '...' : s
