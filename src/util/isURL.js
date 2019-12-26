bf06dcb2790ba3af6642321758fc01bdf2673216
Raine Revere <raine@cybersemics.org>
2019-12-26 07:56:43 -0700

    Fix isURL to not match "i.e.". Fixes #236.

--- src/util/isURL.js
+++ src/util/isURL.js
@@ -1,3 +1,3 @@
 // checks if string contains URL
 // eslint-disable-next-line no-useless-escape
-export const isURL = s => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/i.test(s)
+export const isURL = s => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w-]+)+[\w\-_~:/?#[\]\@!\$&'\(\)\*\+,;=]+$/i.test(s)
