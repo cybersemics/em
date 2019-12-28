e6020c801fbd79cf15e16e8abd36012c71a1c099
Muganwa <stevenmuganwa@gmail.com>
2019-12-28 19:58:02 +0300

    refactor svgs from single file to individual icon

--- src/shortcuts/subcategorizeAll.js
+++ src/shortcuts/subcategorizeAll.js
@@ -15,7 +15,11 @@ import {
   splitChain,
 } from '../util.js'
 
-import { subcategorizeAllSVG } from '../svgs'
+const subcategorizeAllSVG = <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 19.481 19.481" enableBackground="new 0 0 19.481 19.481">
+    <g>
+        <path d="m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z" fill="white"/>
+    </g>
+</svg>
 
 export default {
   id: 'subcategorizeAll',
