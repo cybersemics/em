3e12dbe7138c67b6a78dd074c4906ec3c4899694
Muganwa <stevenmuganwa@gmail.com>
2020-01-05 13:26:45 +0300

    change to window wide event listeners & id 2 svg

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,14 +2,54 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { shortcutById } from '../shortcuts'
 import { overlayReveal, overlayHide, overlayUpdate } from '../action-creators/toolbar'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+const isTouchEnabled = () => {
+    return ('ontouchstart' in window) ||
+           (navigator.maxTouchPoints > 0) ||
+           (navigator.msMaxTouchPoints > 0)
+}
 
 (() => {
     window.addEventListener('mouseup', () => {
         clearHoldTimer()
     })
-    window.addEventListener('mousedown', e => {
-        console.log(e.target.id)
+    window.addEventListener('mouseover', e => {
+        const id = e.target.id
+        if (shortcutIds.includes(id)) {
+            updateOverlayInfo(id)
+        }
     })
+    window.addEventListener('mousedown', e => {
+        const id = e.target.id
+        if (shortcutIds.includes(id)) {
+            clickOrHoldAction(id)
+        }
+    })
+    window.addEventListener('click', e => {
+        const id = e.target.id
+        if (shortcutIds.includes(id)) {
+            const { exec } = shortcutById(id)
+            exec(id)
+        }
+    })
+    if (isTouchEnabled()) {
+        window.addEventListener('touchstart', e => {
+            const id = e.target.id
+            if (shortcutIds.includes(id)) {
+                clickOrHoldAction(id)
+            }
+        })
+        window.addEventListener('touchmove', e => {
+            const id = e.target.id
+            if (shortcutIds.includes(id)) {
+                updateOverlayInfo(id)
+            }
+        })
+        window.addEventListener('touchend', () => {
+            clearHoldTimer()
+        })
+    }
 })()
 
 const shortcutIds = [
@@ -21,33 +61,32 @@ const shortcutIds = [
   'search',
 ]
 
-const isTouchEnabled = () => {
-    return ('ontouchstart' in window) ||
-           (navigator.maxTouchPoints > 0) ||
-           (navigator.msMaxTouchPoints > 0)
-}
-
 const ToolbarMessageOverlay = ({ classname = 'toolbar-overlay', name, description }) => {
-    return <div className={classname}>
-            <div className={'overlay-name'}>{ name }</div>
-            <div className={'overlay-body'}>{ description }</div>
-        </div>
+    return <TransitionGroup>
+            <CSSTransition key={0} timeout={200} classNames='fade'>
+                <div className={classname}>
+                    <div className={'overlay-name'}>{ name }</div>
+                    <div className={'overlay-body'}>{ description }</div>
+                </div>
+            </CSSTransition>
+        </TransitionGroup>
 }
 
 var holdTimer
 var currentId
 
-const updateOverlayInfo = (id, name, description) => {
+const updateOverlayInfo = id => {
     // console.log('updating')
     currentId = id
+    const { name, description } = shortcutById(id)
     overlayUpdate({ id, name, description })
 }
 
-const clickOrHoldAction = (id, name, description) => {
+const clickOrHoldAction = (id) => {
     currentId = id
     // on chrome setTimeout doesn't seem to work on the first click, clearing it before hand fixes the problem
     clearTimeout(holdTimer)
-
+    const { name, description } = shortcutById(id)
     holdTimer = setTimeout(() => {
         overlayReveal({ id, name, description })
     }, 500)
@@ -63,33 +102,13 @@ export const Toolbar = connect(({ toolbarOverlay, settings: { dark } }) => ({ da
         <div className={'toolbar-container'}>
             <div className="toolbar">
                 { shortcutIds.map(id => {
-                    const { name, svg: Icon, description, exec } = shortcutById(id)
-                    if (isTouchEnabled()) {
-                        return <div
-                                key={name}
-                                id={id}
-                                className="toolbar-icon"
-                                onTouchStart={() => clickOrHoldAction(id, name, description)}
-                                onTouchEnd={clearHoldTimer}
-                                onTouchMove={clearHoldTimer}
-                                onMouseDown={() => clickOrHoldAction(id, name, description)}
-                                onClick={() => exec(id)}
-                            >
-                                <Icon fill={dark ? 'white' : 'black'} />
-                            </div>
-                    }
-                    else {
-                        return <div
+                    const { name, svg: Icon } = shortcutById(id)
+                    return <div
                             key={name}
-                            id={id}
                             className="toolbar-icon"
-                            onMouseDown={() => clickOrHoldAction(id, name, description)}
-                            onMouseOver={() => updateOverlayInfo(id, name, description)}
-                            onClick={() => exec(id)}
                         >
-                            <Icon fill={dark ? 'white' : 'black'} />
+                            <Icon id={id} fill={dark ? 'white' : 'black'} />
                         </div>
-                    }
                 })}
             </div>
             { !isTouchEnabled() && showOverlay && shortcutId === currentId ?
