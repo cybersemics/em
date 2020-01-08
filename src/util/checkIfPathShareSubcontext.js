b154ed66d16e5fbe4c62d6d2f18e274659f0d648
Bijay Shrestha <sbijay777@gmail.com>
2020-01-08 20:08:15 +0545

    resolved recent issues

--- src/util/checkIfPathShareSubcontext.js
+++ src/util/checkIfPathShareSubcontext.js
@@ -1,20 +1,20 @@
 import { equalThoughtRanked } from '../util'
 
 export const checkIfPathShareSubcontext = (rankedThoughtsA, rankedThoughtsB) => {
-  /* this method returns -1 if there is no common majority subcontext else returns the index up to which two rankedThought share common majority subcontext */
+  // this method returns -1 if there is no common majority subcontext else returns the index up to which two rankedThought share common majority subcontext
   const longPath = rankedThoughtsA.length > rankedThoughtsB.length ? rankedThoughtsA : rankedThoughtsB
   const shortPath = rankedThoughtsB.length < rankedThoughtsA.length ? rankedThoughtsB : rankedThoughtsA
   const middle = Math.round(longPath.length / 2)
 
-  /* checking if common majoity subcontext is possible to avoid iteration */
+  // checking if common majoity subcontext is possible to avoid iteration
   if (shortPath.length < middle) return -1
 
   const firstDiff = shortPath.findIndex((_, i) => !equalThoughtRanked(shortPath[i], longPath[i]))
 
-  /* if firstDiff is -1 it means shortPath is a subset of longPath so index must be shortPath.length - 1 */
+  // if firstDiff is -1 it means shortPath is a subset of longPath so index must be shortPath.length - 1
 
   const index = (firstDiff === -1 ? shortPath.length : firstDiff) - 1
 
-  /* checking if there are any common thoughts and also if the number of common thoughts are majority */
-  return (index > -1 && (index + 1) >= middle) ? index : -1
+  // checking if there are any common thoughts and also if the number of common thoughts are majority
+  return (index + 1) >= middle ? index : -1
 }
