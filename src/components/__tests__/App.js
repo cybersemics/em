2dded3cc180da39ae899ed5c6322fa1d4219313b
Goran Mekić <meka@tilda.center>
2020-01-09 15:16:51 +0100

    Setup/teardown of tests

--- src/components/__tests__/App.js
+++ src/components/__tests__/App.js
@@ -3,8 +3,18 @@ import { mount } from 'enzyme'
 import { act } from 'react-dom/test-utils'
 import { App } from '../App'
 
-it('App', async () => {
+// eslint-disable-next-line no-undef
+beforeAll(() => {
   document.getSelection = () => ({ type: 'None' })
+})
+
+// eslint-disable-next-line no-undef
+afterAll(() => {
+  // eslint-disable-next-line fp/no-delete
+  delete document.getSelection
+})
+
+it('App', async () => {
   let wrapper // eslint-disable-line fp/no-let,no-unused-vars
   await act(async () => {
     wrapper = await mount(
