d185c083e6ea68c0eba2b29bee732b1fc9ce2f9e
Bijay Shrestha <sbijay777@gmail.com>
2020-01-17 22:16:22 +0545

    close sidedrawer on escape , drawer light theme applied, all visual surface of ha,burger menu made clickable

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -9,18 +9,21 @@ const HamburgerMenu = () => {
   const dispatch = useDispatch()
 
   return (
-    <div className='hamburger-menu' style={{ padding: isMobile ? '5%' : '2% 3%', width: '20px' }}>
+    <div
+      className='hamburger-menu'
+      style={{ padding: isMobile ? '5%' : '2% 3%' }}
+      onClick={() => {
+        dispatch({ type: 'toggleSidebar' })
+      }}
+    >
       <span style={{ cursor: 'pointer' }}>
         <ReactHamburger
           isOpen={showSidebar}
-          menuClicked={() => {
-            dispatch({ type: 'toggleSidebar' })
-          }}
           width={20}
           height={15}
           strokeWidth={1.5}
           rotate={0}
-          color='white'
+          color=' ' // passing empty string to avoid ReactHamburger to pass deault styles to the menu UI (for applying theme)
           borderRadius={0}
           animationDuration={0.8}
         />
