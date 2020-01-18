ee73b90a8e86bcd0540c2540cc0337642e242db4
Shahroz Ansari <shahroz.ansari@enukesoftware.com>
2020-01-19 03:34:15 +0530

    Fixed two bugs
    Fixed 'Expand thoughts that end with : in root'
    Fixed 'do not collapse other visible thoughts that end with :'
    Passing context chain to expandThought for root thoughts left, needs debugging.

--- src/util/expandThoughts.js
+++ src/util/expandThoughts.js
@@ -1,7 +1,7 @@
 import { contextChainToPath } from './contextChainToPath.js'
 import { getThoughts } from './getThoughts.js'
 import { hashContext } from './hashContext.js'
-import { THOUGHT_ENDS_WITH } from '../constants'
+import { THOUGHT_ENDS_WITH, RANKED_ROOT } from '../constants'
 
 /** Returns an expansion map marking all thoughts that should be expanded
   * @example {
@@ -10,8 +10,7 @@ import { THOUGHT_ENDS_WITH } from '../constants'
     A__SEP__A2: true
   }
 */
-export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = {}, contextChain = [], depth = 0) => {
-
+export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = {}, contextChain = [], depth = 0, checkDefaultExpand = true) => {
   // arbitrarily limit depth to prevent infinite context view expansion (i.e. cycles)
   if (!path || path.length === 0 || depth > 5) return {}
 
@@ -21,7 +20,20 @@ export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = 
 
   const children = getThoughts(thoughtsRanked, thoughtIndex, contextIndex)
 
-  // expand only child
+  let defaultExpand = {} // eslint-disable-line fp/no-let
+  if (checkDefaultExpand) {
+    /**
+     * TODO::
+     * Currently passing contest chain blank
+     * Needs to debug how context chain is working and what needs to be passed
+     */
+    const thoughtsExpandedAtColon = getThoughts(RANKED_ROOT, thoughtIndex, contextIndex)
+      .filter(child => child.value[child.value.length - 1] === THOUGHT_ENDS_WITH)
+      .map(child => expandThoughts([child], thoughtIndex, contextIndex, contextViews, [], 0, false))
+
+    defaultExpand = Object.assign({}, ...thoughtsExpandedAtColon)
+  }
+
   return (children.length === 1 ? children : children.filter(child => child.value[child.value.length - 1] === THOUGHT_ENDS_WITH)).reduce(
     (accum, child) => {
       const newContextChain = contextChain.map(thoughts => thoughts.concat())
@@ -32,12 +44,13 @@ export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = 
       return Object.assign({}, accum,
         // RECURSIVE
         // passing contextChain here creates an infinite loop
-        expandThoughts(path.concat(child), thoughtIndex, contextIndex, contextViews, newContextChain, ++depth)
+        expandThoughts(path.concat(child), thoughtIndex, contextIndex, contextViews, newContextChain, ++depth, false)
       )
     },
     // expand current thought
     {
-      [hashContext(path)]: true
+      [hashContext(path)]: true,
+      ...defaultExpand
     }
   )
 }
