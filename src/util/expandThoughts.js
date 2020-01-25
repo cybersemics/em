604f94a7ecb555d743f7209cb638f02bc38b9148
Raine Revere <raine@cybersemics.org>
2020-01-25 06:33:08 -0700

    Expand context correctly. Disable splitChain on initial expandThoughts.

--- src/util/expandThoughts.js
+++ src/util/expandThoughts.js
@@ -1,7 +1,13 @@
 import { contextChainToPath } from './contextChainToPath.js'
 import { getThoughts } from './getThoughts.js'
 import { hashContext } from './hashContext.js'
-import { THOUGHT_ENDS_WITH, RANKED_ROOT } from '../constants'
+import { contextOf } from './contextOf.js'
+
+import {
+  MAX_EXPAND_DEPTH,
+  RANKED_ROOT,
+  EXPAND_THOUGHT_CHAR,
+} from '../constants'
 
 /** Returns an expansion map marking all thoughts that should be expanded
   * @example {
@@ -10,10 +16,10 @@ import { THOUGHT_ENDS_WITH, RANKED_ROOT } from '../constants'
     A__SEP__A2: true
   }
 */
-export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = {}, contextChain = [], depth = 0) => {
+export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = {}, contextChain = [], { depth = 0 } = {}) => {
 
   // arbitrarily limit depth to prevent infinite context view expansion (i.e. cycles)
-  if (depth > 5) return {}
+  if (depth > MAX_EXPAND_DEPTH) return {}
 
   const thoughtsRanked = !path || path.length === 0 ? RANKED_ROOT
     : contextChain.length > 0 ? contextChainToPath(contextChain)
@@ -21,9 +27,8 @@ export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = 
 
   const children = getThoughts(thoughtsRanked, thoughtIndex, contextIndex)
 
-  return (children.length === 1
-    ? children
-    : children.filter(child => child.value[child.value.length - 1] === THOUGHT_ENDS_WITH)
+  return (children.length === 1 ? children
+    : children.filter(child => child.value[child.value.length - 1] === EXPAND_THOUGHT_CHAR)
   ).reduce(
       (accum, child) => {
         const newContextChain = contextChain.map(thoughts => thoughts.concat())
@@ -34,12 +39,18 @@ export const expandThoughts = (path, thoughtIndex, contextIndex, contextViews = 
         return Object.assign({}, accum,
           // RECURSIVE
           // passing contextChain here creates an infinite loop
-          expandThoughts((path || []).concat(child), thoughtIndex, contextIndex, contextViews, newContextChain, ++depth, false)
+          expandThoughts((path || []).concat(child), thoughtIndex, contextIndex, contextViews, newContextChain, { depth: depth + 1 })
         )
       },
-      // expand current thought
       {
-        [hashContext(path || [])]: true
+        // expand current thought
+        [hashContext(path || [])]: true,
+
+        // expand context
+        // this allows uncles of the cursor that end in ":" to be expanded
+        ...(path && path.length >= 1 && depth === 0
+          ? expandThoughts(contextOf(path), thoughtIndex, contextIndex, contextViews, contextChain, { depth: depth + 1 })
+          : {})
       }
     )
 }
