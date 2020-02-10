30333013167a60b00465a515d688c77a50cdf52f
sparklingW <bitbetmy@gmail.com>
2020-02-10 11:08:13 +0200

    updated functionality of the strip fuction

--- src/util/strip.js
+++ src/util/strip.js
@@ -1,5 +1,17 @@
 /** Strip HTML tags, convert nbsp to normal spaces, and trim. */
-export const strip = html => html
-  .replace(/<(?:.|\n)*?>/gm, '')
-  .replace(/&nbsp;/gm, ' ')
-  .trim()
+// export const strip = html => html
+//   .replace(/<(?:.|\n)*?>/gm, '')
+//   .replace(/&nbsp;/gm, ' ')
+//   .trim()
+
+export const strip = (html, stripFormatting = true) => {
+  if (stripFormatting) {
+    return html
+    .replace(/<(?:.|\n)*?>/gm, '')
+    .replace(/&nbsp;/gm, ' ')
+    .trim()
+  }
+  else {
+    return html
+  }
+}
