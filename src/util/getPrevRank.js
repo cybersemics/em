ba89ec02d224012fad74f44d906b8717d95b5f5d
Raine Revere <raine@cybersemics.org>
2020-02-11 08:41:27 -0700

    getPrevRank: rename thoughtsRanked to context.

--- src/util/getPrevRank.js
+++ src/util/getPrevRank.js
@@ -2,8 +2,8 @@ import { getThoughtsRanked } from './getThoughtsRanked.js'
 
 /** Gets a rank that comes before all thoughts in a context. */
 // TODO: Take context not path
-export const getPrevRank = (thoughtsRanked, thoughtIndex, contextIndex) => {
-  const children = getThoughtsRanked(thoughtsRanked, thoughtIndex, contextIndex)
+export const getPrevRank = (context, thoughtIndex, contextIndex) => {
+  const children = getThoughtsRanked(context, thoughtIndex, contextIndex)
   return children.length > 0
     ? children[0].rank - 1
     : 0
