8a3005acbc5658b9b59d2bc3fdd83810c21e60ae
sparklingW <bitbetmy@gmail.com>
2020-02-13 11:38:52 +0200

    rewrite strip function

--- src/util/strip.js
+++ src/util/strip.js
@@ -1,15 +1,14 @@
 /** Strip HTML tags, convert nbsp to normal spaces, and trim. */
 export const strip = (html, stripFormatting = true) => {
-  if (stripFormatting) {
-    return html
-    .replace(/<(?:.|\n)*?>/gm, '')
-    .replace(/&nbsp;/gm, ' ')
-    .trim()
-  }
-  else {
-    return html
+  return stripFormatting
+    ?
+      html
+        .replace(/<(?:.|\n)*?>/gm, '')
+        .replace(/&nbsp;/gm, ' ')
+        .trim()
+    :
+      html
       .replace(/<(?:.|\n)*?([^bui])>/gm, '')
       .replace(/&nbsp;/gm, ' ')
       .trim()
-  }
 }
