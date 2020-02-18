8591bfeabf2b9a58f18122c43b121c58e6f59de3
SamvelARM <samvel.antanyan@gmail.com>
2020-02-19 03:06:13 +0400

    issue263: updated the isURL regexp to better handle good and bad urls.

---
+++ src/util/__tests__/isURL.test.js
@@ -0,0 +1,81 @@
+import { isURL } from '../isURL'
+
+const goodUrlList = [
+    'http://nonssl.com',
+    'https://ssl.com',
+    'localhost:3000/',
+    'localhost:3000',
+    'nohttp.com',
+    'withslash.com/',
+    'sub.domain.com',
+    'withpath.com/a/b',
+    'http://foo.com/blah_blah',
+    'http://foo.com/blah_blah/',
+    'http://foo.com/blah_blah_(wikipedia)',
+    'http://foo.com/blah_blah_(wikipedia)_(again)',
+    'http://www.example.com/wpstyle/?p=364',
+    'https://www.example.com/foo/?bar=baz&inga=42&quux',
+    'http://userid:password@example.com:8080',
+    'http://userid:password@example.com:8080/',
+    'http://userid@example.com',
+    'http://userid@example.com/',
+    'http://userid@example.com:8080',
+    'http://userid@example.com:8080/',
+    'http://userid:password@example.com',
+    'http://userid:password@example.com/',
+    'http://foo.com/blah_(wikipedia)#cite-1',
+    'http://foo.com/blah_(wikipedia)_blah#cite-1',
+    'http://foo.com/(something)?after=parens',
+    'http://code.google.com/events/#&product=browser',
+    'http://a.b-c.de',
+]
+
+const badUrlList = [
+    'e.g.',
+    '3.5/5',
+    'http://',
+    'http://.',
+    'http://..',
+    'http://../',
+    'http://?',
+    'http://??',
+    'http://??/',
+    'http://#',
+    'http://##',
+    'http://##/',
+    'http://foo.bar?q=Spaces should be encoded',
+    '//',
+    '//a',
+    '///a',
+    '///',
+    'http:///a',
+    'rdar://1234',
+    'h://test',
+    'http:// shouldfail.com',
+    ':// should fail',
+    'http://foo.bar/foo(bar)baz quux',
+    'ftps://foo.bar/',
+    'http://-error-.invalid/',
+    'http://-a.b.co',
+    'http://a.b-.co',
+    'http://3628126748',
+    'http://.www.foo.bar/',
+    'http://www.foo.bar./',
+    'http://.www.foo.bar./'
+]
+
+goodUrlList.forEach(url => {
+    describe(`isURL for ${url}`, () => {
+        it('should pass', () => {
+            expect(isURL(url)).toEqual(true)
+        })
+    })
+})
+
+badUrlList.forEach(url => {
+    describe(`isURL for ${url}`, () => {
+        it('should fail', () => {
+            expect(isURL(url)).toEqual(false)
+        })
+    })
+})
--- src/util/isURL.js
+++ src/util/isURL.js
@@ -1,3 +1,3 @@
 // checks if string contains URL
 // eslint-disable-next-line no-useless-escape
-export const isURL = s => /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w-]+)+[\w\-_~:/?#[\]\@!\$&'\(\)\*\+,;=]+$/i.test(s)
+export const isURL = s => /^(?:http(s)?:\/\/)?(www\.)?[a-zA-Z@:%_\+~#=]+[-\w@:%_\+~#=.]*[\w@:%_\+~#=]+[.:][\w()]{1,6}([\w-()@:%_\+~#?&//=]*)$/i.test(s)
