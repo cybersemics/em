485fdca057b9c25097e40aacdaea4207d93a8077
Anmol Arora <anmolaroraxz@gmail.com>
2020-02-24 22:13:48 +0530

    renames action-creator & reducer | moves side-effect to action-creator

--- src/action-creators/updateSplitPosition.js
+++ src/action-creators/updateSplitPosition.js
@@ -1,4 +1,6 @@
 export const updateSplitPosition = splitPosition => dispatch => {
+    localStorage.setItem('splitPosition', parseInt(splitPosition, 10))
+
     dispatch({
         type: 'updateSplitPosition',
         value: splitPosition
--- src/reducers/splitView.js
+++
@@ -1,9 +0,0 @@
-
-export const updateSplitPosition = (state, { value }) => {
-  localStorage.setItem('splitPosition', parseInt(value, 10))
-  return { ...state, splitPosition: value }
-}
-
-export const toggleSplitView = (state, { value }) => {
-  return { showSplitView: value == null ? !state.showSplitView : value }
-}
