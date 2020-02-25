da6d5c967165b2c1ed7d7296f425b5407c191ed6
Esen <s7b5en@gmail.com>
2020-02-25 21:14:45 +0600

    removed getSelection anchornode check

--- src/util/saveCurrentCursorOffset.js
+++ src/util/saveCurrentCursorOffset.js
@@ -3,10 +3,6 @@ import debounce from 'lodash.debounce';
 export default function saveCurrentCursorOffset() {
 	debounce(() => {
 		const offset = window.getSelection().focusOffset;
-		const isInitialSelectionComingFromReload = offset.anchorNode === null;
-
-		if (!isInitialSelectionComingFromReload) {
-			localStorage.setItem('currentCursorPosition', offset);
-		}
-	}, 100)();
+		localStorage.setItem('currentCursorPosition', offset);
+	}, 1)();
 }
