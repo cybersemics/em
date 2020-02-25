6017e43c1e4d2b13d02754262c7de1023a935818
Esen <s7b5en@gmail.com>
2020-02-26 01:06:18 +0600

    fix for incorrect use of debounce

--- src/util/saveCurrentCursorOffset.js
+++ src/util/saveCurrentCursorOffset.js
@@ -1,8 +1,10 @@
 import debounce from 'lodash.debounce'
 
+const saveCurrentOffsetToLocalStorage = debounce(() => {
+  const offset = window.getSelection().focusOffset
+  localStorage.setItem('currentCursorPosition', offset)
+}, 10)
+
 export default function saveCurrentCursorOffset() {
-  debounce(() => {
-    const offset = window.getSelection().focusOffset
-    localStorage.setItem('currentCursorPosition', offset)
-  }, 10)()
+  saveCurrentOffsetToLocalStorage()
 }
