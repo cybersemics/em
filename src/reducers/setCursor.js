13b0a1ad3bc893302d6b41e2419b909dc75d374a
Esen <s7b5en@gmail.com>
2020-02-25 20:27:26 +0600

    basic restoration for cursor on reload

--- src/reducers/setCursor.js
+++ src/reducers/setCursor.js
@@ -1,134 +1,138 @@
-import * as localForage from 'localforage'
+import * as localForage from 'localforage';
+import debounce from 'lodash.debounce';
 
 // constants
 import {
-  TUTORIAL_CONTEXT,
-  TUTORIAL_STEP_AUTOEXPAND,
-  TUTORIAL_STEP_AUTOEXPAND_EXPAND,
-  TUTORIAL2_STEP_CONTEXT_VIEW_SELECT,
-} from '../constants.js'
+	TUTORIAL_CONTEXT,
+	TUTORIAL_STEP_AUTOEXPAND,
+	TUTORIAL_STEP_AUTOEXPAND_EXPAND,
+	TUTORIAL2_STEP_CONTEXT_VIEW_SELECT
+} from '../constants.js';
 
 // util
 import {
-  chain,
-  dataIntegrityCheck,
-  equalPath,
-  expandThoughts,
-  getSetting,
-  hashContext,
-  hashContextUrl,
-  headValue,
-  lastThoughtsFromContextChain,
-  pathToContext,
-  updateUrlHistory,
-} from '../util.js'
+	chain,
+	dataIntegrityCheck,
+	equalPath,
+	expandThoughts,
+	getSetting,
+	hashContext,
+	hashContextUrl,
+	headValue,
+	lastThoughtsFromContextChain,
+	pathToContext,
+	updateUrlHistory
+} from '../util.js';
 
 // reducers
-import settings from './settings.js'
+import settings from './settings.js';
 
 // SIDE EFFECTS: updateUrlHistory, localStorage
 // set both cursorBeforeEdit (the transcendental head) and cursor (the live value during editing)
 // the other contexts superscript uses cursor when it is available
-export default (state, { thoughtsRanked, contextChain = [], cursorHistoryClear, cursorHistoryPop, replaceContextViews, editing }) => {
+export default (
+	state,
+	{ thoughtsRanked, contextChain = [], cursorHistoryClear, cursorHistoryPop, replaceContextViews, editing }
+) => {
+	const thoughtsResolved =
+		contextChain.length > 0 ? chain(contextChain, thoughtsRanked, state.thoughtIndex) : thoughtsRanked;
 
-  const thoughtsResolved = contextChain.length > 0
-    ? chain(contextChain, thoughtsRanked, state.thoughtIndex)
-    : thoughtsRanked
+	// sync replaceContextViews with state.contextViews
+	// ignore thoughts that are not in the path of replaceContextViews
+	// shallow copy
+	const newContextViews = replaceContextViews ? Object.assign({}, state.contextViews) : state.contextViews;
 
-  // sync replaceContextViews with state.contextViews
-  // ignore thoughts that are not in the path of replaceContextViews
-  // shallow copy
-  const newContextViews = replaceContextViews
-    ? Object.assign({}, state.contextViews)
-    : state.contextViews
+	if (replaceContextViews) {
+		// add
+		Object.keys(replaceContextViews).forEach((encoded) => {
+			newContextViews[encoded] = true;
+		});
 
-  if (replaceContextViews) {
+		// remove
+		Object.keys(state.contextViews).forEach((encoded) => {
+			if (!(encoded in replaceContextViews)) {
+				delete newContextViews[encoded]; // eslint-disable-line fp/no-delete
+			}
+		});
+	}
 
-    // add
-    Object.keys(replaceContextViews).forEach(encoded => {
-      newContextViews[encoded] = true
-    })
+	setTimeout(() => {
+		updateUrlHistory(thoughtsResolved, { contextViews: newContextViews });
 
-    // remove
-    Object.keys(state.contextViews).forEach(encoded => {
-      if (!(encoded in replaceContextViews)) {
-        delete newContextViews[encoded] // eslint-disable-line fp/no-delete
-      }
-    })
-  }
+		// persist the cursor so it can be restored after em is closed and reopened on the home page (see initialState)
+		if (thoughtsResolved) {
+			// persist the cursor to ensure the location does not change through refreshes in standalone PWA mode
+			localForage
+				.setItem('cursor', hashContextUrl(pathToContext(thoughtsResolved), { contextViews: newContextViews }))
+				.catch((err) => {
+					throw new Error(err);
+				});
+		} else {
+			localForage.removeItem('cursor').catch((err) => {
+				throw new Error(err);
+			});
+		}
+	});
 
-  setTimeout(() => {
+	const expanded = expandThoughts(
+		thoughtsResolved || [],
+		state.thoughtIndex,
+		state.contextIndex,
+		newContextViews,
+		contextChain.length > 0
+			? contextChain.concat([ thoughtsResolved.slice(lastThoughtsFromContextChain(contextChain, state).length) ])
+			: []
+	);
 
-    updateUrlHistory(thoughtsResolved, { contextViews: newContextViews })
+	const tutorialChoice = +getSetting('Tutorial Choice', state)[0] || 0;
+	const tutorialStep = +getSetting('Tutorial Step', state)[0] || 1;
+	const tutorialNext =
+		(tutorialStep === TUTORIAL_STEP_AUTOEXPAND &&
+			thoughtsResolved &&
+			thoughtsResolved.length === 1 &&
+			Object.keys(expanded).length === 1 &&
+			!state.contextIndex[hashContext(thoughtsResolved)]) ||
+		(tutorialStep === TUTORIAL_STEP_AUTOEXPAND_EXPAND && Object.keys(expanded).length > 1) ||
+		(tutorialStep === TUTORIAL2_STEP_CONTEXT_VIEW_SELECT &&
+			thoughtsResolved &&
+			thoughtsResolved.length >= 1 &&
+			headValue(thoughtsResolved).toLowerCase().replace(/"/g, '') ===
+				TUTORIAL_CONTEXT[tutorialChoice].toLowerCase());
 
-    // persist the cursor so it can be restored after em is closed and reopened on the home page (see initialState)
-    if (thoughtsResolved) {
-      // persist the cursor to ensure the location does not change through refreshes in standalone PWA mode
-      localForage.setItem('cursor', hashContextUrl(pathToContext(thoughtsResolved), { contextViews: newContextViews }))
-        .catch(err => {
-          throw new Error(err)
-        })
-    }
-    else {
-      localForage.removeItem('cursor').catch(err => {
-        throw new Error(err)
-      })
-    }
-  })
+	setTimeout(() => dataIntegrityCheck(thoughtsResolved), 100);
 
-  const expanded = expandThoughts(
-    thoughtsResolved || [],
-    state.thoughtIndex,
-    state.contextIndex,
-    newContextViews,
-    contextChain.length > 0
-      ? contextChain.concat([thoughtsResolved.slice(lastThoughtsFromContextChain(contextChain, state).length)])
-      : []
-  )
+	debounce(() => {
+		const offset = window.getSelection().focusOffset;
+		console.log('setting it to', offset);
+		localStorage.setItem('currentCursorPosition', offset);
+	}, 100)();
 
-  const tutorialChoice = +getSetting('Tutorial Choice', state)[0] || 0
-  const tutorialStep = +getSetting('Tutorial Step', state)[0] || 1
-  const tutorialNext = (
-    tutorialStep === TUTORIAL_STEP_AUTOEXPAND &&
-      thoughtsResolved &&
-      thoughtsResolved.length === 1 &&
-      Object.keys(expanded).length === 1 &&
-      !state.contextIndex[hashContext(thoughtsResolved)]
-  ) ||
-    (tutorialStep === TUTORIAL_STEP_AUTOEXPAND_EXPAND &&
-      Object.keys(expanded).length > 1) ||
-    (tutorialStep === TUTORIAL2_STEP_CONTEXT_VIEW_SELECT &&
-      thoughtsResolved &&
-      thoughtsResolved.length >= 1 &&
-      headValue(thoughtsResolved).toLowerCase().replace(/"/g, '') === TUTORIAL_CONTEXT[tutorialChoice].toLowerCase()
-    )
-
-  setTimeout(() => dataIntegrityCheck(thoughtsResolved), 100)
-
-  // only change editing status and expanded but do not move the cursor if cursor has not changed
-  return equalPath(thoughtsResolved, state.cursor) && state.contextViews === newContextViews
-    ? {
-      editing: editing != null ? editing : state.editing,
-      expanded,
-    }
-    : {
-    // dataNonce must be bumped so that <Subthoughts> are re-rendered
-    // otherwise the cursor gets lost when changing focus from an edited thought
-      expanded,
-      dataNonce: state.dataNonce + 1,
-      cursor: thoughtsResolved,
-      cursorBeforeEdit: thoughtsResolved,
-      codeView: false,
-      cursorHistory: cursorHistoryClear ? [] :
-        cursorHistoryPop ? state.cursorHistory.slice(0, state.cursorHistory.length - 1)
-          : state.cursorHistory,
-      contextViews: newContextViews,
-      editing: editing != null ? editing : state.editing,
-      ...(tutorialNext
-        ? settings(state, {
-          key: 'Tutorial Step',
-          value: tutorialStep + 1
-        })
-        : null)
-    }
-}
+	// only change editing status and expanded but do not move the cursor if cursor has not changed
+	return equalPath(thoughtsResolved, state.cursor) && state.contextViews === newContextViews
+		? {
+				editing: editing != null ? editing : state.editing,
+				expanded
+			}
+		: {
+				// dataNonce must be bumped so that <Subthoughts> are re-rendered
+				// otherwise the cursor gets lost when changing focus from an edited thought
+				expanded,
+				dataNonce: state.dataNonce + 1,
+				cursor: thoughtsResolved,
+				cursorBeforeEdit: thoughtsResolved,
+				codeView: false,
+				cursorHistory: cursorHistoryClear
+					? []
+					: cursorHistoryPop
+						? state.cursorHistory.slice(0, state.cursorHistory.length - 1)
+						: state.cursorHistory,
+				contextViews: newContextViews,
+				editing: editing != null ? editing : state.editing,
+				...(tutorialNext
+					? settings(state, {
+							key: 'Tutorial Step',
+							value: tutorialStep + 1
+						})
+					: null)
+			};
+};
