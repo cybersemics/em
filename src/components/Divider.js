f3c65bd24f885d89ff2be0c5dfba1c585f53b6e5
Maru-cat <belolutskaya@gmail.com>
2020-03-05 18:25:16 +0200

    - get max width of list elements that are nearby for divider, add 30px and set this width to divider

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -6,10 +6,34 @@ import {
   headRank
 } from '../util.js'
 
-export const Divider = ({ thoughtsRanked }) => <div className='divider-container'>
-  <div className={classNames({
-    divider: true,
-    // requires editable-hash className to be selected by the cursor navigation via editableNode
-    ['editable-' + hashContext(thoughtsRanked, headRank(thoughtsRanked))]: true,
-  })} />
-</div>
+export const Divider = ({ thoughtsRanked }) => {
+
+  const dividerSetWidth = React.createRef();
+
+  // get max width of nearby for divider list child elements, add 30 px and set this width for divider
+  const setStyle = () => {
+    if(dividerSetWidth.current) {
+      const parentUl = dividerSetWidth.current.closest("ul");
+      const children = parentUl.childNodes;
+      let maxWidth = 0;
+      children.forEach((child) => {
+        if(!child.classList.contains('child-divider')) {
+          const subs = child.getElementsByClassName('subthought');
+          if(subs.length && subs[0].offsetWidth > maxWidth) maxWidth = subs[0].offsetWidth;
+        }
+      });
+      maxWidth += 30;
+      dividerSetWidth.current.style.width = `${maxWidth}px`
+    }
+  }
+
+  setTimeout(() => {setStyle()}, 300)
+
+  return (<div ref={dividerSetWidth} style={{width: '100px', transition: 'width 800ms'}} className='divider-container'>
+    <div className={classNames({
+      divider: true,
+      // requires editable-hash className to be selected by the cursor navigation via editableNode
+      ['editable-' + hashContext(thoughtsRanked, headRank(thoughtsRanked))]: true,
+    })} />
+  </div>)
+}
