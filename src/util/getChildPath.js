a7338c777834d0f9033fad6bed1cf96dad9ee9b9
SamvelARM <samvel.antanyan@gmail.com>
2020-03-13 16:08:29 +0400

    issue262: made changed by reviewers request.

--- src/util/getChildPath.js
+++ src/util/getChildPath.js
@@ -5,6 +5,9 @@ import { headValue } from './headValue'
 import { rankThoughtsFirstMatch } from './rankThoughtsFirstMatch'
 import { unroot } from './unroot'
 
+/** Because the current thought only needs to hash match another thought
+    we need to use the exact value of the child from the other context
+    child.context SHOULD always be defined when showContexts is true . **/
 export const getChildPath = (child, thoughtsRanked, showContexts) => {
   const value = showContexts ? head(child.context) : child.value
 
