3571549764e3264cc41ad30af42711dc350c507c
SamvelARM <samvel.antanyan@gmail.com>
2020-03-13 15:46:13 +0400

    issue262: made changed by reviewers request.

--- src/action-creators/cursorDown.js
+++ src/action-creators/cursorDown.js
@@ -8,15 +8,15 @@ import {
   isDivider,
   restoreSelection,
   selectNextEditable,
+  getThoughtsRanked,
 } from '../util.js'
-import { getDescendants } from '../util/getDescendants'
 
 export const cursorDown = ({ target }) => dispatch => {
   const { cursor } = store.getState()
   if (cursor) {
     // select next editable
     const nextThought = getThoughtAfter(cursor)
-    const descendants = getDescendants(cursor)
+    const descendants = getThoughtsRanked(cursor)
     if (nextThought) {
       const nextThoughtsRanked = descendants && descendants.length > 0 ? contextOf(cursor).concat(cursor).concat(descendants[0]) : contextOf(cursor).concat(nextThought)
       if (isDivider(headValue(cursor))) {
