31313f87bb9bbe3dc5e39688f173c5ab77a9275c
Raine Revere <raine@cybersemics.org>
2020-03-14 13:36:00 -0600

    Allow thoughts to be moved across context boundary.

--- src/action-creators/moveThoughtUp.js
+++ src/action-creators/moveThoughtUp.js
@@ -4,11 +4,13 @@ import { error } from './error.js'
 // util
 import {
   contextOf,
-  headRank,
-  headValue,
   ellipsize,
+  getNextRank,
   getRankBefore,
   getSortPreference,
+  getThoughtBefore,
+  headRank,
+  headValue,
   meta,
   pathToContext,
   prevSibling,
@@ -19,57 +21,65 @@ import {
 export const moveThoughtUp = () => dispatch => {
   const { cursor } = store.getState()
 
-  if (cursor) {
+  if (!cursor) return
 
-    const context = contextOf(cursor)
-    const value = headValue(cursor)
-    const rank = headRank(cursor)
+  const context = contextOf(cursor)
+  const value = headValue(cursor)
+  const rank = headRank(cursor)
 
-    const prevThought = prevSibling(value, rootedContextOf(cursor), rank)
-    if (prevThought) {
+  const prevThought = prevSibling(value, rootedContextOf(cursor), rank)
 
-      // metaprogramming functions that prevent moving
-      const thoughtMeta = meta(pathToContext(cursor))
-      const contextMeta = meta(pathToContext(contextOf(cursor)))
-      const sortPreference = getSortPreference(contextMeta)
+  // if the cursor is the first thought in the second column of a table, move the thought up to the end of its prev uncle
+  const prevUncleThought = context.length > 0 && getThoughtBefore(context)
+  const prevContext = prevUncleThought && contextOf(context).concat(prevUncleThought)
 
-      if (sortPreference === 'Alphabetical') {
-        error(`Cannot move subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" while sort is enabled.`)
-        return
-      }
-      else if (thoughtMeta.readonly) {
-        error(`"${ellipsize(headValue(cursor))}" is read-only and cannot be moved.`)
-        return
-      }
-      else if (thoughtMeta.immovable) {
-        error(`"${ellipsize(headValue(cursor))}" is immovable.`)
-        return
-      }
-      else if (contextMeta.readonly && contextMeta.readonly.Subthoughts) {
-        error(`Subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" are read-only and cannot be moved.`)
-        return
-      }
-      else if (contextMeta.immovable && contextMeta.immovable.Subthoughts) {
-        error(`Subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" are immovable.`)
-        return
-      }
+  if (!prevThought && !prevContext) return
 
-      // store selection offset before existingThoughtMove is dispatched
-      const offset = window.getSelection().focusOffset
+  // metaprogramming functions that prevent moving
+  const thoughtMeta = meta(pathToContext(cursor))
+  const contextMeta = meta(pathToContext(contextOf(cursor)))
+  const sortPreference = getSortPreference(contextMeta)
 
-      const rankNew = getRankBefore(context.concat(prevThought))
-      const newPath = context.concat({
-        value,
-        rank: rankNew
-      })
-
-      dispatch({
-        type: 'existingThoughtMove',
-        oldPath: cursor,
-        newPath
-      })
-
-      restoreSelection(newPath, { offset })
-    }
+  if (sortPreference === 'Alphabetical') {
+    error(`Cannot move subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" while sort is enabled.`)
+    return
   }
+  else if (thoughtMeta.readonly) {
+    error(`"${ellipsize(headValue(cursor))}" is read-only and cannot be moved.`)
+    return
+  }
+  else if (thoughtMeta.immovable) {
+    error(`"${ellipsize(headValue(cursor))}" is immovable.`)
+    return
+  }
+  else if (contextMeta.readonly && contextMeta.readonly.Subthoughts) {
+    error(`Subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" are read-only and cannot be moved.`)
+    return
+  }
+  else if (contextMeta.immovable && contextMeta.immovable.Subthoughts) {
+    error(`Subthoughts of "${ellipsize(headValue(contextOf(cursor)))}" are immovable.`)
+    return
+  }
+
+  // store selection offset before existingThoughtMove is dispatched
+  const offset = window.getSelection().focusOffset
+
+  const rankNew = prevThought
+    // previous thought
+    ? getRankBefore(context.concat(prevThought))
+    // first thought in table column 2
+    : getNextRank(prevContext)
+
+  const newPath = (prevThought ? context : prevContext).concat({
+    value,
+    rank: rankNew
+  })
+
+  dispatch({
+    type: 'existingThoughtMove',
+    oldPath: cursor,
+    newPath
+  })
+
+  restoreSelection(newPath, { offset })
 }
