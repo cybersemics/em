5ad34fbb2bd2e36c6c8870ba6a87bb85f319f32a
Raine Revere <raine@cybersemics.org>
2020-03-18 15:38:57 -0600

    removeDuplicatedContext: Use predefined compareByRank.

--- src/util/removeDuplicatedContext.js
+++ src/util/removeDuplicatedContext.js
@@ -1,12 +1,14 @@
-import { equalArrays } from './equalArrays.js'
-import { notNull } from './notNull.js'
-import { makeCompareByProp } from './makeCompareByProp.js'
-import { sort } from './sort.js'
+import {
+  compareByRank,
+  equalArrays,
+  notNull,
+  sort,
+} from '../util.js'
 
 /** Returns a new thought remove duplicated given context. */
 export const removeDuplicatedContext = (thought, context) => {
   if (typeof thought === 'string') throw new Error('removeDuplicatedContext expects an [object] thought, not a [string] value.')
-  const topRankContext = sort((thought.contexts || []), makeCompareByProp('rank'))
+  const topRankContext = sort((thought.contexts || []), compareByRank)
     .find(parent => equalArrays(parent.context, context))
   return Object.assign({}, thought, notNull({
     contexts: (thought.contexts || [])
