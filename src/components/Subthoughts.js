2f1e808ba2ff068bdbef6eb4713b2e7f76b2adf3
Raine Revere <raine@cybersemics.org>
2020-03-21 18:25:13 -0600

    Do not paginate root.

--- src/components/Subthoughts.js
+++ src/components/Subthoughts.js
@@ -59,7 +59,7 @@ const toggleContextViewShortcut = shortcutById('toggleContextView')
 assert(subthoughtShortcut)
 assert(toggleContextViewShortcut)
 
-const PAGINATION_SIZE = 25
+const PAGINATION_SIZE = 5
 
 /*
   @param focus  Needed for Editable to determine where to restore the selection after delete
@@ -262,7 +262,9 @@ export const Subthoughts = connect(({ cursorBeforeEdit, cursor, contextViews, th
           (cursor && equalThoughtRanked(cursor[thoughtsRanked.length], child))
       })
 
-      const proposedPageSize = PAGINATION_SIZE * page
+      const proposedPageSize = isRoot(thoughtsRanked)
+        ? Infinity
+        : PAGINATION_SIZE * page
       if (editIndex > proposedPageSize - 1) {
         setPage(page + 1)
         return null
