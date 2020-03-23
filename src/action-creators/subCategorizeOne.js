7bfb19d6bc6da48d206ae7cc841960fddb1c3321
Raine Revere <raine@cybersemics.org>
2020-03-22 18:47:06 -0600

    subCategorizeOne: Move thought without RENDER_DELAY.

--- src/action-creators/subCategorizeOne.js
+++ src/action-creators/subCategorizeOne.js
@@ -4,11 +4,6 @@ import { store } from '../store.js'
 import { newThought } from './newThought'
 import { error } from './error.js'
 
-// constants
-import {
-  RENDER_DELAY,
-} from '../constants.js'
-
 // util
 import {
   contextOf,
@@ -42,11 +37,16 @@ export const subCategorizeOne = () => dispatch => {
   }
 
   const { rank } = dispatch(newThought({ insertBefore: true }))
+
+  // not sure why both the setTimeout and thunk are needed
+  // without them, the thought does not get moved correctly though
   setTimeout(() => {
-    dispatch({
-      type: 'existingThoughtMove',
-      oldPath: cursor,
-      newPath: contextOf(cursor).concat({ value: '', rank }, head(cursor))
+    dispatch(dispatch => {
+      dispatch({
+        type: 'existingThoughtMove',
+        oldPath: cursor,
+        newPath: contextOf(cursor).concat({ value: '', rank }, head(cursor))
+      })
     })
-  }, RENDER_DELAY)
+  })
 }
