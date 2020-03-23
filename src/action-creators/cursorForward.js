9dcd4729b8f6f7b260ba2ec79544a316e2df5fb6
Raine Revere <raine@cybersemics.org>
2020-03-22 18:18:48 -0600

    cursorForward: Remove restoreSelection.

--- src/action-creators/cursorForward.js
+++ src/action-creators/cursorForward.js
@@ -1,20 +1,15 @@
-import { isMobile } from '../browser.js'
 import { store } from '../store.js'
 
 // util
 import { getThoughtsRanked } from '../util/getThoughtsRanked.js'
-import { restoreSelection } from '../util/restoreSelection.js'
 
 export const cursorForward = () => dispatch => {
   const state = store.getState()
+
   // pop from cursor history
   if (state.cursorHistory.length > 0) {
     const cursorNew = state.cursorHistory[state.cursorHistory.length - 1]
     dispatch({ type: 'setCursor', thoughtsRanked: cursorNew, cursorHistoryPop: true })
-
-    if (state.cursor && (!isMobile || state.editing)) {
-      restoreSelection(cursorNew, { offset: 0 })
-    }
   }
   // otherwise move cursor to first child
   else {
@@ -23,9 +18,6 @@ export const cursorForward = () => dispatch => {
     if (firstSubthought) {
       const cursorNew = cursorOld.concat(firstSubthought)
       dispatch({ type: 'setCursor', thoughtsRanked: cursorNew })
-      if (!isMobile || state.editing) {
-        restoreSelection(cursorNew, { offset: 0 })
-      }
     }
   }
 }
