3659b33e47e744d141a0804691ec845d451aaf75
Brian Ogutu <codingbrian58@gmail.com>
2020-03-27 00:38:53 +0300

    Fix encode character url

--- src/util/hashContextUrl.js
+++ src/util/hashContextUrl.js
@@ -1,10 +1,17 @@
 import { isRoot } from './isRoot.js'
-import { isContextViewActive } from './isContextViewActive.js'
+// import { isContextViewActive } from './isContextViewActive.js'
 
 /** Encodes thoughts array into a URL. */
-export const hashContextUrl = (thoughts, { contextViews } = {}) =>
-  '/' + (!thoughts || isRoot(thoughts)
+export const hashContextUrl = (thoughts, { contextViews } = {}) => {
+  return '/' + (!thoughts || isRoot(thoughts)
     ? ''
     : thoughts.map((thought, i) =>
-      window.encodeURIComponent(thought) + (isContextViewActive(thoughts.slice(0, i + 1), { state: { contextViews } }) ? '~' : '')
+      window.encodeURIComponent(
+        thought)
+      //   +
+      // (isContextViewActive(
+      //   thoughts.slice(0, i + 1),
+      //   { state: { contextViews } }
+      // ) ? '~' : '')
     ).join('/'))
+}
