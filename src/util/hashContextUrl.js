7d194f3f0585920fbd466385f35564afa354a93a
Brian Ogutu <codingbrian58@gmail.com>
2020-03-27 10:59:21 +0300

    Update return to use arrow functions

--- src/util/hashContextUrl.js
+++ src/util/hashContextUrl.js
@@ -2,16 +2,14 @@ import { isRoot } from './isRoot.js'
 // import { isContextViewActive } from './isContextViewActive.js'
 
 /** Encodes thoughts array into a URL. */
-export const hashContextUrl = (thoughts, { contextViews } = {}) => {
-  return '/' + (!thoughts || isRoot(thoughts)
-    ? ''
-    : thoughts.map((thought, i) =>
-      window.encodeURIComponent(
-        thought)
-      //   +
-      // (isContextViewActive(
-      //   thoughts.slice(0, i + 1),
-      //   { state: { contextViews } }
-      // ) ? '~' : '')
-    ).join('/'))
-}
+export const hashContextUrl = (thoughts, { contextViews } = {}) => '/' + (!thoughts || isRoot(thoughts)
+  ? ''
+  : thoughts.map((thought, i) =>
+    window.encodeURIComponent(
+      thought)
+    //   +
+    // (isContextViewActive(
+    //   thoughts.slice(0, i + 1),
+    //   { state: { contextViews } }
+    // ) ? '~' : '')
+  ).join('/'))
