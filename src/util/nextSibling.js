58e8afa4ca4886a8a4fbc49c0041fccb541945c5
Raine Revere <raine@cybersemics.org>
2020-03-27 17:14:49 -0600

    nextSibling: Do not select hidden thoughts.

--- src/util/nextSibling.js
+++ src/util/nextSibling.js
@@ -1,3 +1,5 @@
+import { store } from '../store.js'
+
 // util
 import {
   equalThoughtRanked,
@@ -5,15 +7,19 @@ import {
   getThoughtsRanked,
   getThoughtsSorted,
   getSortPreference,
+  isFunction,
   meta,
   pathToContext,
 } from '../util.js'
 
 /** Gets thoughts's next sibling with its rank. */
 export const nextSibling = (value, context, rank) => {
+  const { showHiddenThoughts } = store.getState()
   const sortPreference = getSortPreference(meta(pathToContext(context)))
   const siblings = sortPreference === 'Alphabetical' ? getThoughtsSorted(context) : getThoughtsRanked(context)
-  const i = siblings.findIndex(child => sortPreference === 'Alphabetical' ? equalThoughtSorted(child, { value }) :
+  const notHidden = child => !isFunction(child.value) && !meta(context.concat(child.value)).hidden
+  const siblingsFiltered = showHiddenThoughts ? siblings : siblings.filter(notHidden)
+  const i = siblingsFiltered.findIndex(child => sortPreference === 'Alphabetical' ? equalThoughtSorted(child, { value }) :
     equalThoughtRanked(child, { value, rank }))
-  return siblings[i + 1]
+  return siblingsFiltered[i + 1]
 }
