0d2db6afccefeb692e270dbbeaeced024ba5579c
Brian Ogutu <codingbrian58@gmail.com>
2020-03-27 14:07:32 +0300

    Fix error thrown on adding subthoughts to a thought with ~

--- src/util/decodeThoughtsUrl.js
+++ src/util/decodeThoughtsUrl.js
@@ -4,7 +4,6 @@ import {
 
 // util
 import { componentToThought } from './componentToThought.js'
-import { hashContext } from './hashContext.js'
 import { rankThoughtsFirstMatch } from './rankThoughtsFirstMatch.js'
 
 /**
@@ -16,11 +15,7 @@ export const decodeThoughtsUrl = (pathname, thoughtIndex, contextIndex) => {
   const urlPath = pathname.slice(1)
   const urlComponents = urlPath ? urlPath.split('/') : [ROOT_TOKEN]
   const pathUnranked = urlComponents.map(componentToThought)
-  const contextViews = urlComponents.reduce((accum, cur, i) =>
-    /~$/.test(cur) ? Object.assign({}, accum, {
-      [hashContext(pathUnranked.slice(0, i + 1))]: true
-    }) : accum,
-  {})
+  const contextViews = urlComponents.reduce(accum => accum, {})
   const thoughtsRanked = rankThoughtsFirstMatch(pathUnranked, { state: { thoughtIndex, contextIndex, contextViews } })
   return {
     // infer ranks of url path so that url can be /A/a1 instead of /A_0/a1_0 etc
