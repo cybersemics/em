c08419f56e8a60327e075c3f24bae0e6ad46e39f
Raine Revere <raine@cybersemics.org>
2020-03-27 11:24:01 -0600

    getSetting: Fix state argument.

--- src/util/getSetting.js
+++ src/util/getSetting.js
@@ -10,6 +10,6 @@ import {
 } from '../util.js'
 
 /** Returns subthoughts of /em/Settings/...context, not including meta subthoughts */
-export const getSetting = (context, thoughtIndex = store.getState().thoughtIndex, contextIndex = store.getState().contextIndex, depth = 0) =>
-  (getThoughtsRanked([EM_TOKEN, 'Settings'].concat(context), thoughtIndex, contextIndex)
+export const getSetting = (context, state = store.getState(), depth = 0) =>
+  (getThoughtsRanked([EM_TOKEN, 'Settings'].concat(context), state.thoughtIndex, state.contextIndex)
     .find(child => !isFunction(child.value)) || {}).value
