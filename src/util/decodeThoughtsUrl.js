7a2bbbb6dc6af343701afe0ae4f69577d68df4f7
Brian Ogutu <codingbrian58@gmail.com>
2020-03-27 22:09:22 +0300

    Fix contex view breakage

--- src/util/decodeThoughtsUrl.js
+++ src/util/decodeThoughtsUrl.js
@@ -3,7 +3,8 @@ import {
 } from '../constants.js'
 
 // util
-import { componentToThought } from './componentToThought.js'
+// import { componentToThought } from './componentToThought.js'
+import { hashContext } from './hashContext.js'
 import { rankThoughtsFirstMatch } from './rankThoughtsFirstMatch.js'
 
 /**
@@ -14,8 +15,12 @@ import { rankThoughtsFirstMatch } from './rankThoughtsFirstMatch.js'
 export const decodeThoughtsUrl = (pathname, thoughtIndex, contextIndex) => {
   const urlPath = pathname.slice(1)
   const urlComponents = urlPath ? urlPath.split('/') : [ROOT_TOKEN]
-  const pathUnranked = urlComponents.map(componentToThought)
-  const contextViews = urlComponents.reduce(accum => accum, {})
+  const pathUnranked = urlComponents.map(component => window.decodeURIComponent((window.decodeURIComponent(component.replace(/~$/, '')))))
+  const contextViews = urlComponents.reduce((accum, cur, i) =>
+    /~$/.test(cur) ? Object.assign({}, accum, {
+      [hashContext(pathUnranked.slice(0, i + 1))]: true
+    }) : accum,
+  {})
   const thoughtsRanked = rankThoughtsFirstMatch(pathUnranked, { state: { thoughtIndex, contextIndex, contextViews } })
   return {
     // infer ranks of url path so that url can be /A/a1 instead of /A_0/a1_0 etc
--- src/util/hashContextUrl.js
+++ src/util/hashContextUrl.js
@@ -1,5 +1,10 @@
 import { isRoot } from './isRoot.js'
+import { isContextViewActive } from './isContextViewActive.js'
 
 /** Encodes thoughts array into a URL. */
-export const hashContextUrl = thoughts => '/' + (!thoughts || isRoot(thoughts)
-  ? '' : thoughts.map(thought => window.encodeURIComponent(thought)).join('/'))
+export const hashContextUrl = (thoughts, { contextViews } = {}) =>
+  '/' + (!thoughts || isRoot(thoughts)
+    ? ''
+    : thoughts.map((thought, i) =>
+      window.encodeURIComponent(thought).replace(/~/g, '%257e') + (isContextViewActive(thoughts.slice(0, i + 1), { state: { contextViews } }) ? '~' : '')
+    ).join('/'))
