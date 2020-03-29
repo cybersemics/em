9034ae0be6249165f96a7c1a881d73c4b2106ec0
Raine Revere <raine@cybersemics.org>
2020-03-28 19:17:22 -0600

    Add command Bump Thought Down. Closes #460.

---
+++ src/shortcuts/bumpThoughtDown.js
@@ -0,0 +1,67 @@
+import { store } from '../store.js'
+
+// util
+import {
+  contextOf,
+  getPrevRank,
+  getThoughts,
+  headRank,
+  pathToContext,
+  rootedContextOf,
+  unroot,
+} from '../util.js'
+
+import { subCategorizeOne } from '../action-creators/subCategorizeOne'
+
+export default {
+  id: 'bumpThought',
+  name: 'Bump Thought Down',
+  description: 'Bump the current thought down to its children and replace with empty text.',
+  gesture: 'rld',
+  exec: () => {
+    const { cursor } = store.getState()
+    const editable = document.querySelector('.editing .editable')
+
+    // presumably if one of these is true then both are
+    if (cursor && editable) {
+      const value = editable.innerHTML
+      const rank = headRank(cursor)
+      const subthoughts = getThoughts(cursor)
+
+      if (subthoughts.length > 0) {
+        // TODO: Resolve thoughtsRanked to make it work within the context view
+        // Cannot do this without the contextChain
+        // Need to store the full thoughtsRanked of each cursor segment in the cursor
+        const context = pathToContext(cursor)
+        const rankNew = getPrevRank(cursor)
+
+        store.dispatch({
+          type: 'existingThoughtChange',
+          oldValue: value,
+          newValue: '',
+          context: rootedContextOf(context),
+          rankInContext: rankNew,
+          thoughtsRanked: cursor
+        })
+
+        store.dispatch({
+          type: 'newThoughtSubmit',
+          context: unroot(contextOf(context).concat('')),
+          rank: rankNew,
+          value,
+        })
+
+        store.dispatch({
+          type: 'setCursor',
+          thoughtsRanked: unroot(contextOf(cursor).concat({
+            value: '',
+            rank
+          })),
+        })
+      }
+      else {
+        store.dispatch(subCategorizeOne())
+      }
+    }
+  }
+}
