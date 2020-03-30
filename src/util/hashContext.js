d1503e0a71ec49408f4d96abc898ae548afa3099
Raine Revere <raine@cybersemics.org>
2020-03-29 18:32:27 -0600

    hashContext: Remove memoize

--- src/util/hashContext.js
+++ src/util/hashContext.js
@@ -1,15 +1,15 @@
 import * as murmurHash3 from 'murmurhash3js'
 import globals from '../globals.js'
 import { ID } from '../constants.js'
-import _ from 'lodash'
 
 // util
 import { escapeSelector } from './escapeSelector.js'
 import { pathToContext } from './pathToContext.js'
 
+const SEPARATOR_TOKEN = '__SEP__'
+
 /** Encode the thoughts (and optionally rank) as a string. */
-export const hashContext = _.memoize((thoughts, rank) => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
+export const hashContext = (thoughts, rank) => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
   .map(thought => thought ? escapeSelector(thought) : '')
   .join('__SEP__')
-  + (rank != null ? '__SEP__' + rank : ''))
-)
+  + (rank != null ? SEPARATOR_TOKEN + rank : ''))
