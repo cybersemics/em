52524886654c5a6fd14950f467533de850eae103
A Q Khan <aqk.neo@gmail.com>
2020-04-01 23:20:27 +0500

    Disallowed merge thoughts (#472)
    * Disallowed merge thoughts
    * Allow delete if cursor has no children
    * deleteEmptyThought: Consolidate logic in canExecute.
    
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- src/shortcuts/deleteEmptyThought.js
+++ src/shortcuts/deleteEmptyThought.js
@@ -8,6 +8,7 @@ import { deleteEmptyThought } from '../action-creators/deleteEmptyThought'
 import {
   contextOf,
   getThoughtsRanked,
+  getThoughtBefore,
   headValue,
   isContextViewActive,
   isDivider,
@@ -18,13 +19,20 @@ import {
 const canExecute = () => {
   const { cursor, contextViews } = store.getState()
   const offset = window.getSelection().focusOffset
+
   if (cursor) {
     const showContexts = isContextViewActive(contextOf(cursor), { state: store.getState() })
     const contextChain = splitChain(cursor, contextViews)
     const thoughtsRanked = lastThoughtsFromContextChain(contextChain)
-    const children = getThoughtsRanked(thoughtsRanked)
+    const hasChildren = getThoughtsRanked(thoughtsRanked).length > 0
+    const prevThought = getThoughtBefore(cursor)
+    const isAtStart = offset === 0 && !showContexts
+    const hasChildrenAndPrevDivider = prevThought && isDivider(prevThought.value) && hasChildren
 
-    return ((headValue(cursor) === '' && children.length === 0) || isDivider(headValue(cursor))) || (offset === 0 && !showContexts)
+    // delete if the current thought is a divider
+    // delete if the browser selection as at the start of the thought (either deleting or merging if it has children)
+    // do not merge if previous thought is a divider
+    return isDivider(headValue(cursor)) || (isAtStart && !hasChildrenAndPrevDivider)
   }
 }
 
