271f894ac8300f3777a29899819fe9422bf36862
Raine Revere <raine@cybersemics.org>
2020-04-03 14:55:58 -0600

    Convert canShowModal to selector.

--- src/selectors/canShowModal.ts
+++ src/selectors/canShowModal.ts
@@ -1,10 +1,11 @@
 import { store } from '../store'
 
-// util
-// declare using traditional function syntax so it is hoisted
-export const canShowModal = (id, state = store ? store.getState() : null) => {
+const canShowModal = (state: any, id: number) => {
+  state = state || (typeof store !== 'undefined' ? store.getState() : null)
   return state &&
     (!state.showModal || state.showModal === id) &&
     !state.modals[id].complete &&
     state.modals[id].hideuntil < Date.now()
 }
+
+export default canShowModal
