6fcd10861b6749e8061dc99a3d9dd8531572bfb7
Raine Revere <raine@cybersemics.org>
2020-04-04 11:19:38 -0600

    Fix state passed to getNextRank.

--- src/action-creators/deleteEmptyThought.js
+++ src/action-creators/deleteEmptyThought.js
@@ -60,7 +60,7 @@ export const deleteEmptyThought = () => (dispatch, getState) => {
           thoughtsRanked: contextOf(thoughtsRanked).concat(prev)
         })
 
-        const nextRank = getNextRank(thoughtsRankedPrevNew)
+        const nextRank = getNextRank(thoughtsRankedPrevNew, state.thoughtIndex, state.contextIndex)
 
         // merge children into merged thought
         children.forEach((child, i) => {
