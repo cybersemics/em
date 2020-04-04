8b70747ea7cfba96d5586a67d66e30ce482644ee
Raine Revere <raine@cybersemics.org>
2020-04-04 17:14:37 -0600

    Remove .js from toggleAttribute imports.

--- src/shortcuts/proseView.js
+++ src/shortcuts/proseView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { store } from '../store'
-import toggleAttribute from '../action-creators/toggleAttribute.js'
+import toggleAttribute from '../action-creators/toggleAttribute'
 
 // util
 import {
