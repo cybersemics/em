44587855e72c684b864c6215d5d57333984efdac
Raine Revere <raine@cybersemics.org>
2020-04-04 17:25:36 -0600

    Remove .js from stripPunctuation imports.

--- src/util/getNgrams.js
+++ src/util/getNgrams.js
@@ -3,7 +3,7 @@ import { store } from '../store'
 // util
 import { flatten } from './flatten'
 import { getContexts } from './getContexts'
-import { stripPunctuation } from './stripPunctuation.js'
+import { stripPunctuation } from './stripPunctuation'
 
 /** Returns an array of { text, numContexts, charIndex } objects consisting of the largest contiguous linked or unlinked ngrams of the given text.
  * @param text Thought text.
