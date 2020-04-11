2d736416a11a503fcdb9fa8744f5d9fad17af187
umair-khanzada <umairkz52@gmail.com>
2020-04-11 13:40:21 +0500

    converted getRankBefore util function to selector.

--- src/action-creators/newThought.js
+++ src/action-creators/newThought.js
@@ -22,7 +22,6 @@ import {
 // util
 import {
   contextOf,
-  getRankBefore,
   getSetting,
   headValue,
   isContextViewActive,
@@ -33,7 +32,7 @@ import {
 } from '../util'
 
 // selectors
-import { getNextRank, getPrevRank, getRankAfter } from '../selectors'
+import { getNextRank, getPrevRank, getRankAfter, getRankBefore } from '../selectors'
 
 /** Adds a new thought to the cursor.
  * @param offset The focusOffset of the selection in the new thought. Defaults to end.
