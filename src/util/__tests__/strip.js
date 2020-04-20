5d328ea0b7c464119d615acdafec5986f2d2acc4
Raine Revere <raine@cybersemics.org>
2020-04-20 14:43:23 -0600

    Unit Tests: Use toBe instead of toEqual where possible.

--- src/util/__tests__/strip.js
+++ src/util/__tests__/strip.js
@@ -3,48 +3,48 @@ import {
 } from '../../util'
 
 it('self-closing tags', () => {
-  expect(strip('a<br/>')).toEqual('a')
+  expect(strip('a<br/>')).toBe('a')
 })
 
 it('self-closing tags with variable inner whitespace', () => {
-  expect(strip('a<br   />')).toEqual('a')
+  expect(strip('a<br   />')).toBe('a')
 })
 
 it('inline', () => {
-  expect(strip('<span>b</span>five')).toEqual('bfive')
+  expect(strip('<span>b</span>five')).toBe('bfive')
 })
 
 it('tag with attribute', () => {
-  expect(strip('<span style="color: blue">b</span>five')).toEqual('bfive')
+  expect(strip('<span style="color: blue">b</span>five')).toBe('bfive')
 })
 
 it('newlines', () => {
   expect(strip(`<l
-  >b</li>five`)).toEqual('bfive')
+  >b</li>five`)).toBe('bfive')
 })
 
 it('preserve bold', () => {
-  expect(strip('Hello <b>world</b>', { preserveFormatting: true })).toEqual('Hello <b>world</b>')
+  expect(strip('Hello <b>world</b>', { preserveFormatting: true })).toBe('Hello <b>world</b>')
 })
 
 it('preserve italic', () => {
-  expect(strip('Hello <i>sun</i>', { preserveFormatting: true })).toEqual('Hello <i>sun</i>')
+  expect(strip('Hello <i>sun</i>', { preserveFormatting: true })).toBe('Hello <i>sun</i>')
 })
 
 it('preserve underline', () => {
-  expect(strip('Hello <u>sky</u>', { preserveFormatting: true })).toEqual('Hello <u>sky</u>')
+  expect(strip('Hello <u>sky</u>', { preserveFormatting: true })).toBe('Hello <u>sky</u>')
 })
 
 it('preserve bold with value-less attribute', () => {
-  expect(strip('Hello <b m>red</b> world!', { preserveFormatting: true })).toEqual('Hello <b>red</b> world!')
+  expect(strip('Hello <b m>red</b> world!', { preserveFormatting: true })).toBe('Hello <b>red</b> world!')
 })
 
 it('preserve bold with attribute', () => {
-  expect(strip('Hello <b style="color: red">red</b> world!', { preserveFormatting: true })).toEqual('Hello <b>red</b> world!')
+  expect(strip('Hello <b style="color: red">red</b> world!', { preserveFormatting: true })).toBe('Hello <b>red</b> world!')
 })
 
 // This fails here, but succeeds on regex101.com (???)
 it.skip('preserve bold with newlines in attribute', () => {
   expect(strip(`Hello <b
-  style="color: red">red</b> world!`, { preserveFormatting: true })).toEqual('Hello <b>red</b> world!')
+  style="color: red">red</b> world!`, { preserveFormatting: true })).toBe('Hello <b>red</b> world!')
 })
--- src/util/__tests__/subsetThoughts.js
+++ src/util/__tests__/subsetThoughts.js
@@ -3,27 +3,27 @@ import {
 } from '../../util'
 
 it('equal returns true', () => {
-  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'a', rank: 0 }])).toEqual(true)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'a', rank: 0 }])).toBe(true)
 })
 
 it('subset returns true', () => {
-  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }], [{ value: 'a', rank: 0 }])).toEqual(true)
-  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }, { value: 'c', rank: 0 }], [{ value: 'b', rank: 0 }, { value: 'c', rank: 0 }])).toEqual(true)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }], [{ value: 'a', rank: 0 }])).toBe(true)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }, { value: 'c', rank: 0 }], [{ value: 'b', rank: 0 }, { value: 'c', rank: 0 }])).toBe(true)
 })
 
 it('value mismatch returns false', () => {
-  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'b', rank: 0 }])).toEqual(false)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'b', rank: 0 }])).toBe(false)
 })
 
 it('rank mismatch returns false', () => {
-  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'a', rank: 1 }])).toEqual(false)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }], [{ value: 'a', rank: 1 }])).toBe(false)
 })
 
 it('non-contiguous subset returns false', () => {
-  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }, { value: 'c', rank: 0 }, { value: 'd', rank: 0 }], [{ value: 'b', rank: 0 }, { value: 'd', rank: 0 }])).toEqual(false)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }, { value: 'c', rank: 0 }, { value: 'd', rank: 0 }], [{ value: 'b', rank: 0 }, { value: 'd', rank: 0 }])).toBe(false)
 })
 
 it('empty list returns false', () => {
-  expect(subsetThoughts([], [])).toEqual(false)
-  expect(subsetThoughts([{ value: 'a', rank: 0 }], [])).toEqual(false)
+  expect(subsetThoughts([], [])).toBe(false)
+  expect(subsetThoughts([{ value: 'a', rank: 0 }], [])).toBe(false)
 })
