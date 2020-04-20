b2753b735ab0cb78429b674882a250d797039ab1
Raine Revere <raine@cybersemics.org>
2020-04-20 14:30:01 -0600

    Move strip unit tests to jest.

--- src/util/strip.js
+++ src/util/strip.js
@@ -14,21 +14,3 @@ export const strip = (html, { preserveFormatting, preventTrim } = {}) => {
     ? replacedHtml
     : replacedHtml.trim()
 }
-
-// assert.strictEqual(strip('a<br/>', { preserveFormatting: true }), 'a')
-// assert.strictEqual(strip('a<br   />', { preserveFormatting: true }), 'a')
-// assert.strictEqual(strip('<span>b</span>five', { preserveFormatting: true }), 'bfive')
-// assert.strictEqual(strip('<span style="color: blue">b</span>five', { preserveFormatting: true }), 'bfive')
-// assert.strictEqual(strip('<li>b</li>five', { preserveFormatting: true }), 'bfive')
-// assert.strictEqual(strip(`<l
-//   >b</li>five`, { preserveFormatting: true }), 'bfive')
-// assert.strictEqual(strip('Hello <bike>red</bike> world!', { preserveFormatting: true }), 'Hello red world!')
-// assert.strictEqual(strip('Hello <b>world</b>', { preserveFormatting: true }), 'Hello <b>world</b>')
-// assert.strictEqual(strip('Hello <i>sun</i>', { preserveFormatting: true }), 'Hello <i>sun</i>')
-// assert.strictEqual(strip('Hello <u>sky</u>', { preserveFormatting: true }), 'Hello <u>sky</u>')
-// assert.strictEqual(strip('Hello <b m>red</b> world!', { preserveFormatting: true }), 'Hello <b>red</b> world!')
-// assert.strictEqual(strip('Hello <b style="color: red">red</b> world!', { preserveFormatting: true }), 'Hello <b>red</b> world!')
-
-// This fails here, but succeeds on regex101.com (???)
-// assert.strictEqual(strip(`Hello <b
-//   style="color: red">red</b> world!`, { preserveFormatting: true }), 'Hello <b>red</b> world!')
