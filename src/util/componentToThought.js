f06660f8780a307ac87e5b4d030f3160989cfd11
Raine Revere <raine@cybersemics.org>
2020-04-24 16:28:01 -0600

    componentToThought: Guard against % in thought.

--- src/util/componentToThought.js
+++ src/util/componentToThought.js
@@ -1,3 +1,3 @@
 /** Convert a single url component to an thought */
 // user entered "~" characters are double encoded
-export const componentToThought = component => window.decodeURIComponent(window.decodeURIComponent(component.replace(/~$/, '')))
+export const componentToThought = component => window.decodeURIComponent(window.decodeURIComponent(component.replace(/~|%25$/, '')))
