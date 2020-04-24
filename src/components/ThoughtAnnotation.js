fe9febc5279befe59826e3d6406ddbdf0f0c1dba
Raine Revere <raine@cybersemics.org>
2020-04-24 12:30:28 -0600

    UrlIconLink: Refactor and do not render on root thoughts in publish mode.

--- src/components/ThoughtAnnotation.js
+++ src/components/ThoughtAnnotation.js
@@ -14,6 +14,7 @@ import {
   headValue,
   meta,
   pathToContext,
+  publishMode,
   unroot,
 } from '../util'
 
@@ -24,6 +25,7 @@ import theme from '../selectors/theme'
 import HomeLink from './HomeLink'
 import StaticSuperscript from './StaticSuperscript'
 import ContextBreadcrumbs from './ContextBreadcrumbs'
+import UrlIcon from './icons/UrlIcon'
 
 const mapStateToProps = (state, props) => {
 
@@ -67,6 +69,25 @@ const ThoughtAnnotation = ({ dark, thoughtsRanked, showContexts, showContextBrea
   // const subthoughtUnderSelection = perma(() => findSubthoughtByIndex(subthoughts, focusOffset))
   const thoughtMeta = meta(pathToContext(thoughtsRanked))
 
+  const addMissingProtocol = url => (
+    !url.startsWith('http:') &&
+    !url.startsWith('https:') &&
+    !url.startsWith('localhost:')
+      ? 'https://'
+      : ''
+  ) + url
+
+  const UrlIconLink = () => <a href={addMissingProtocol(url)} rel="noopener noreferrer" target='_blank' className='external-link' onClick={e => {
+    if (url.startsWith(window.location.origin)) {
+      const { thoughtsRanked, contextViews } = decodeThoughtsUrl(url.slice(window.location.origin.length))
+      dispatch({ type: 'setCursor', thoughtsRanked, replaceContextViews: contextViews })
+      e.preventDefault()
+    }
+  }}
+  >
+    <UrlIcon />
+  </a>
+
   return <div className='thought-annotation' style={homeContext ? { height: '1em', marginLeft: 8 } : null}>
 
     {showContextBreadcrumbs ? <ContextBreadcrumbs thoughtsRanked={contextOf(contextOf(thoughtsRanked))} showContexts={showContexts} /> : null}
@@ -91,34 +112,8 @@ const ThoughtAnnotation = ({ dark, thoughtsRanked, showContexts, showContextBrea
                 : ellipsizeUrl(subthought.text)
             }
             </span>
-            {url
-            // eslint-disable-next-line no-undef
-              ? <a href={(!url.startsWith('http:') && !url.startsWith('https:') && !url.startsWith('localhost:') ? 'https://' : '') + url} rel="noopener noreferrer" target='_blank' className='external-link' onClick={e => {
-                if (url.startsWith(window.location.origin)) {
-                  const { thoughtsRanked, contextViews } = decodeThoughtsUrl(url.slice(window.location.origin.length))
-                  dispatch({ type: 'setCursor', thoughtsRanked, replaceContextViews: contextViews })
-                  e.preventDefault()
-                }
-              }}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                  width="18" height="18"
-                  viewBox="0 0 30 30"
-                  style={{
-                    marginLeft: '2px',
-                    marginTop: '-4px',
-                    padding: '3px 3px 3px 1px',
-                    cursor: 'pointer',
-                    pointerEvents: 'all'
-                  }}
-                ><path
-                    fill='#838283'
-                    d="M 25.980469 2.9902344 A 1.0001 1.0001 0 0 0 25.869141 3 L 20 3 A 1.0001 1.0001 0 1 0 20 5 L 23.585938 5 L 13.292969 15.292969 A 1.0001 1.0001 0 1 0 14.707031 16.707031 L 25 6.4140625 L 25 10 A 1.0001 1.0001 0 1 0 27 10 L 27 4.1269531 A 1.0001 1.0001 0 0 0 25.980469 2.9902344 z M 6 7 C 4.9069372 7 4 7.9069372 4 9 L 4 24 C 4 25.093063 4.9069372 26 6 26 L 21 26 C 22.093063 26 23 25.093063 23 24 L 23 14 L 23 11.421875 L 21 13.421875 L 21 16 L 21 24 L 6 24 L 6 9 L 14 9 L 16 9 L 16.578125 9 L 18.578125 7 L 16 7 L 14 7 L 6 7 z">
-                  </path>
-                </svg>
-              </a>
-              : null
-            }
+            { // do not render url icon on root thoughts in publish mode
+              url && !(publishMode() && thoughtsRanked.length === 1) && <UrlIconLink />}
             { // with the default minContexts of 2, do not count the whole thought
             // with real time context update we increase context length by 1
               minContexts === 0 || numContexts > (subthought.text === value ? 1 : 0)
