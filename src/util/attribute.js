04eb63fba603803f071d52350937da26a4548a9d
Raine Revere <raine@cybersemics.org>
2020-04-29 12:54:02 -0600

    attribute: Return visible first child.
    Needed to add =src/=preload.

--- src/util/attribute.js
+++ src/util/attribute.js
@@ -2,19 +2,24 @@ import { store } from '../store'
 
 import {
   getThoughts,
+  isFunction,
+  meta,
   pathToContext,
 } from '../util'
 
 /** Returns the value of an attribute of the given context. O(children). Use attributeEquals for O(1) attribute equality check. */
 export const attribute = (pathOrContext, attributeName, { state = store.getState() } = {}) => {
 
-  const children = getThoughts(pathToContext(pathOrContext).concat(attributeName), state.thoughtIndex, state.contextIndex)
+  const context = pathToContext(pathOrContext)
+  const children = getThoughts([...context, attributeName], state.thoughtIndex, state.contextIndex)
 
-  // lazy evaluation
-  const hasAttribute = () => pathToContext(getThoughts(pathToContext(pathOrContext), state.thoughtIndex, state.contextIndex)).includes(attributeName)
+  const notHidden = child => !isFunction(child.value) && !meta([...context, child.value]).hidden
+  const firstVisibleChild = children.find(notHidden)
+
+  const hasAttribute = () => pathToContext(getThoughts(context, state.thoughtIndex, state.contextIndex)).includes(attributeName)
 
   // differentiate between no attribute (return undefined) and an attribute with no children (return null)
-  return children.length > 0 ? children[0].value
+  return firstVisibleChild ? firstVisibleChild.value
     : hasAttribute() ? null
     : undefined
 }
