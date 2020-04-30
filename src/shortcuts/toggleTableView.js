74645b47a93f91e85d3ddf11d203deffa6bb5a3a
Raine Revere <raine@cybersemics.org>
2020-04-30 14:19:53 -0600

    Vertically align table toolbar icon.

--- src/shortcuts/toggleTableView.js
+++ src/shortcuts/toggleTableView.js
@@ -7,7 +7,7 @@ import {
   pathToContext,
 } from '../util'
 
-const Icon = ({ size = 20, style }) => <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width={size} height={size} style={{ ...style, transform: 'scale(0.9)' }} viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
+const Icon = ({ size = 20, style }) => <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width={size} height={size} style={style} viewBox="-2 -2 28 28">
   <g>
     <path d="m21.5 24h-19c-1.378 0-2.5-1.121-2.5-2.5v-19c0-1.379 1.122-2.5 2.5-2.5h19c1.378 0 2.5 1.121 2.5 2.5v19c0 1.379-1.122 2.5-2.5 2.5zm-19-23c-.827 0-1.5.673-1.5 1.5v19c0 .827.673 1.5 1.5 1.5h19c.827 0 1.5-.673 1.5-1.5v-19c0-.827-.673-1.5-1.5-1.5z"/>
     <path d="m23.5 12.5h-23c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h23c.276 0 .5.224.5.5s-.224.5-.5.5z"/>
