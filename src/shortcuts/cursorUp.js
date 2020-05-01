94750b8bd4dcb3a715247a7a67da93fb55cf8fcd
Raine Revere <raine@cybersemics.org>
2020-04-30 20:17:43 -0600

    Remove autoProse. Fixes #437.

--- src/shortcuts/cursorUp.js
+++ src/shortcuts/cursorUp.js
@@ -6,7 +6,6 @@ import { cursorUp } from '../action-creators/cursorUp'
 
 // util
 import {
-  autoProse,
   contextOf,
   pathToContext,
 } from '../util'
@@ -36,7 +35,7 @@ export default {
       const isProseView = attributeEquals(state, pathToContext(contextRanked), '=view', 'Prose')
 
       // default browser behavior in prose mode
-      if ((isProseView || autoProse(contextRanked)) && window.getSelection().focusOffset > 0) return false
+      if (isProseView && window.getSelection().focusOffset > 0) return false
     }
     return true
   },
