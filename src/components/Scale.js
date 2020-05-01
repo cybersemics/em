9485b2ef8126041ca9846262c1784e9952c67b35
Raine Revere <raine@cybersemics.org>
2020-04-30 19:34:56 -0600

    Scale: Temporarily disable scale in publish mode until #536 is fixed.

--- src/components/Scale.js
+++ src/components/Scale.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
+import { publishMode } from '../util'
+
 /** A container that scales its children by the given amount.
   @ param amount    A percentage from 0.0 to 1.0
 */
 const Scale = ({ amount, children }) =>
+  // temporarily disable scale in publish mode until #536 is fixed
   <div style={{
-    transform: `scale(${amount})`,
+    transform: `scale(${!publishMode() ? amount : 1})`,
     transformOrigin: '0 0',
-    width: `${100 * (1 / amount)}%`
+    width: `${100 * (1 / (!publishMode() ? amount : 1))}%`
   }}>{children}</div>
 
 export default Scale
