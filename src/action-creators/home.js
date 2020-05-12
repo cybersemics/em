6ca9b2dc20d9e19af498a49d6f07363aba73af3a
Raine Revere <raine@cybersemics.org>
2020-05-12 15:16:30 -0600

    Fix home link. Closes #648.

--- src/action-creators/home.js
+++ src/action-creators/home.js
@@ -1,9 +1,20 @@
-import { store } from '../store'
-
 // util
+import {
+  restoreCursorBeforeSearch,
+} from '../util'
+
 /* Navigates home and resets the scroll position */
-export default () => {
-  store.dispatch({ type: 'setCursor', thoughtsRanked: null, cursorHistoryClear: true })
-  window.scrollTo(0, 0)
-  document.getSelection().removeAllRanges()
+export default () => (dispatch, getState) => {
+
+  const { search } = getState()
+
+  if (search != null) {
+    dispatch({ type: 'search', value: null })
+    restoreCursorBeforeSearch()
+  }
+  else {
+    dispatch({ type: 'setCursor', thoughtsRanked: null, cursorHistoryClear: true })
+    window.scrollTo(0, 0)
+    document.getSelection().removeAllRanges()
+  }
 }
