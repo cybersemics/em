bd2e3b29740d175098c2326e251fd3d622ce9c46
aqkhan <aqk.neo@gmail.com>
2020-05-12 19:46:27 +0500

    Deleted empty archive on undo

--- src/action-creators/undoArchive.js
+++ src/action-creators/undoArchive.js
@@ -1,11 +1,24 @@
 import { store } from '../store'
 
+// utils
+import {
+  contextOf,
+  getThoughtsRanked,
+  head,
+  pathToContext,
+  rankThoughtsFirstMatch,
+  splice,
+} from '../util'
+
 // action-creators
 import alert from '../action-creators/alert'
 
 export const undoArchive = ({ originalPath, currPath, offset }) => dispatch => {
 
   const state = store.getState()
+
+  const archiveContext = contextOf(pathToContext(currPath))
+  const contextOfArchive = splice(archiveContext, archiveContext.length - 1, 1)
 
   // set the cursor to the original path before restoring the thought
   dispatch({
@@ -21,6 +34,14 @@ export const undoArchive = ({ originalPath, currPath, offset }) => dispatch => {
     newPath: originalPath,
     offset
   })
+
+  if (getThoughtsRanked(archiveContext).length === 0) {
+    dispatch({
+      type: 'existingThoughtDelete',
+      context: contextOfArchive,
+      thoughtRanked: head(rankThoughtsFirstMatch(archiveContext))
+    })
+  }
 
   // Hide the "Undo" alert
   alert(null)
