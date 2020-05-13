cea0649592350e86c44aa2ea3efa28a2e2e2b91a
aqkhan <aqk.neo@gmail.com>
2020-05-14 03:13:35 +0500

    Performance improvements - removed store and unnecessary use of getThouhgtsRanked

--- src/action-creators/undoArchive.js
+++ src/action-creators/undoArchive.js
@@ -1,25 +1,22 @@
-import { store } from '../store'
-
 // utils
 import {
-  contextOf,
   pathToContext,
+  rootedContextOf,
 } from '../util'
 
 // selectors
 import {
+  getThoughts,
   getThoughtsRanked,
 } from '../selectors'
 
 // action-creators
 import alert from '../action-creators/alert'
 
-export const undoArchive = ({ originalPath, currPath, offset }) => dispatch => {
+export const undoArchive = ({ originalPath, currPath, offset }) => (dispatch, getState) => {
 
-  const { getState } = store
-
-  const context = contextOf(pathToContext(currPath))
-  const archiveContext = contextOf(pathToContext(originalPath))
+  const context = rootedContextOf(pathToContext(currPath))
+  const archiveContext = rootedContextOf(pathToContext(originalPath))
 
   // set the cursor to the original path before restoring the thought
   dispatch({
@@ -37,7 +34,7 @@ export const undoArchive = ({ originalPath, currPath, offset }) => dispatch => {
   })
 
   // Check if =archive is empty
-  if (getThoughtsRanked(getState(), context).length === 0) {
+  if (getThoughts(getState(), context).length === 0) {
     dispatch({
       type: 'existingThoughtDelete',
       context: archiveContext,
