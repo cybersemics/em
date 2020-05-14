d7fffd0d531469e94a5e80ce4a8f9e3e4f60b913
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-14 10:21:54 +0200

    converts compareThought to ts | updates utilTypes to be more generic

--- src/util/makeCompareByProp.ts
+++ src/util/makeCompareByProp.ts
@@ -12,7 +12,7 @@ export const isSmaller = (a, b, key) => lower(a[key]) < lower(b[key])
  * Creates a function that takes two values and compares the given key.
  * Does case insensitive comparison with strings.
  */
-export const makeCompareByProp = key => (a, b) =>
+export const makeCompareByProp = (key:any):ComparatorFunction<Object> => (a: Object, b: Object) =>
   isGreater(a, b, key) ? 1
   : isSmaller(a, b, key) ? -1
   : 0
