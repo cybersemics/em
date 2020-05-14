03b033ffd17d913c71b22c1534f18172c336923a
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-14 06:26:09 +0200

    converts addContext to ts

--- src/util/addContext.ts
+++ src/util/addContext.ts
@@ -2,9 +2,10 @@
 import { equalArrays } from './equalArrays'
 import { notNull } from './notNull'
 import { timestamp } from './timestamp'
+import { Child, Context } from '../types'
 
 /** Returns a new thought plus the given context. Does not add duplicates. */
-export const addContext = (thought, context, rank) => {
+export const addContext = (thought: Child, context: Context, rank: number): Child => {
   if (typeof thought === 'string') throw new Error('removeContext expects an [object] thought, not a [string] value.')
   return Object.assign({}, thought, notNull({
     contexts: (thought.contexts || [])
