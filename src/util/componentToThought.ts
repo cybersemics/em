ccd73d9391003ebd0a5cd2774febec5fbb146ec2
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-15 11:19:01 +0200

    converts componentToThought to ts

--- src/util/componentToThought.ts
+++ src/util/componentToThought.ts
@@ -1,6 +1,4 @@
-//@ts-nocheck
-
 /** Convert a single url component to an thought.
  * User entered "~" characters are double encoded.
  */
-export const componentToThought = component => window.decodeURIComponent(window.decodeURIComponent(component.replace(/~|%25$/, '')))
+export const componentToThought = (component: string): string => window.decodeURIComponent(window.decodeURIComponent(component.replace(/~|%25$/, '')))
