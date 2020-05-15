cybersemics/em

commit 20bb267beb3af4dacf463f8d96c9f2470f8f4332

Disallow 'New Subthoughts' from parent with =unextendable (#655)
disallow new subthoughts from parent with =unextendable

src/action-creators/newThought.js
@@ -1,5 +1,6 @@
 import globals from '../globals'
 import { tutorialNext } from '../action-creators/tutorial'
+import error from '../action-creators/error'
 
 // constants
 import {
@@ -22,6 +23,7 @@ import {
 // util
 import {
   contextOf,
+  ellipsize,
   headValue,
   pathToContext,
   unroot,
@@ -36,6 +38,7 @@ import {
   getSetting,
   isContextViewActive,
   lastThoughtsFromContextChain,
+  meta,
   splitChain,
 } from '../selectors'
 
@@ -62,6 +65,17 @@ export default ({ at, insertNewSubthought, insertBefore, value = '', offset, pre
         tutorialStep === TUTORIAL_STEP_FIRSTTHOUGHT_ENTER))
 
   const path = at || state.cursor || RANKED_ROOT
+
+  // prevent adding Subthought to readonly or unextendable Thought
+  const sourceContext = insertNewSubthought ? state.cursor : contextOf(state.cursor)
+  if (meta(state, pathToContext(sourceContext)).readonly) {
+    dispatch(error(`"${ellipsize(headValue(sourceContext))}" is read-only. No subthoughts may be added.`))
+    return
+  }
+  else if (meta(state, pathToContext(sourceContext)).unextendable) {
+    dispatch(error(`"${ellipsize(headValue(sourceContext))}" is unextendable. No subthoughts may be added.`))
+    return
+  }
 
   const contextChain = splitChain(state, path)
   const showContexts = isContextViewActive(state, path)

src/shortcuts/newThought.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { isMobile } from '../browser'
 import { store } from '../store'
-import error from '../action-creators/error'
 
 // action-creators
 import newThoughtAtCursor from '../action-creators/newThoughtAtCursor'
@@ -15,14 +14,12 @@ import {
 // util
 import {
   contextOf,
-  ellipsize,
   headValue,
   isDocumentEditable,
-  pathToContext,
 } from '../util'
 
 // selectors
-import { getSetting, isContextViewActive, meta } from '../selectors'
+import { getSetting, isContextViewActive } from '../selectors'
 
 const Icon = ({ fill = 'black', size = 20, style }) => <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={fill} style={style} viewBox="0 0 19.481 19.481" enableBackground="new 0 0 19.481 19.481">
   <g>
@@ -39,18 +36,6 @@ const exec = (e, { type }) => {
 
   // cancel if tutorial has just started
   if (tutorial && tutorialStep === TUTORIAL_STEP_START) return
-
-  // cancel if parent is readonly
-  if (cursor) {
-    if (meta(state, pathToContext(contextOf(cursor))).readonly) {
-      store.dispatch(error(`"${ellipsize(headValue(contextOf(cursor)))}" is read-only. No subthoughts may be added.`))
-      return
-    }
-    else if (meta(state, pathToContext(contextOf(cursor))).unextendable) {
-      store.dispatch(error(`"${ellipsize(headValue(contextOf(cursor)))}" is unextendable. No subthoughts may be added.`))
-      return
-    }
-  }
 
   const offset = window.getSelection().focusOffset
 

src/action-creators/archiveThought.js
@@ -117,13 +117,13 @@ export default () => (dispatch, getState) => {
     if (!contextMeta.archive) {
       dispatch(newThought({ at: context, insertNewSubthought: true, insertBefore: true, value: '=archive', preventSetCursor: true }))
     }
-    alert((
+    alert(
       <div>Deleted "{ellipsize(headValue(path))}."&nbsp;
         <a onClick={() => {
           dispatch(undoArchive({ originalPath: path, currPath: pathToArchive(getState(), path, context), offset }))
         }}>Undo</a>
       </div>
-    ))
+    )
     setTimeout(() => alert(null), 10000)
 
     // execute existingThoughtMove after newThought has updated the state

src/shortcuts/newSubthought.js
@@ -15,6 +15,8 @@ const Icon = ({ fill = 'black', size = 20, style }) => <svg version="1.1" classN
   </g>
 </svg>
 
+const exec = () => store.dispatch(newThought({ insertNewSubthought: true }))
+
 export default {
   id: 'newSubthought',
   name: 'New Subhought',
@@ -23,7 +25,7 @@ export default {
   keyboard: { key: 'Enter', meta: true },
   svg: Icon,
   canExecute: () => isDocumentEditable(),
-  exec: () => store.dispatch(newThought({ insertNewSubthought: true }))
+  exec
 }
 
 // add aliases to help with mis-swipes since MultiGesture does not support diagonal swipes
@@ -34,5 +36,5 @@ export const newSubthoughtAliases = {
     'rdlr', 'rdldr', 'rdldlr', 'rdldldr', 'rldr', 'rldlr', 'rldldr', 'rldldlr', 'rdru', 'rdrdru', 'rdrdrru', 'rdrdrdru', 'rlru', 'rdrlru', 'rdrdlru', 'rdrdrlru', 'rdllru', 'rdrd', 'rdrdrd', 'rdrdrrd', 'rdrdrdrd', 'rdlrd', 'rdldrd', 'rdldlrd', 'rdlru', 'rdldru', 'rdldlru', 'rdldldru', 'rldru', 'rldlru', 'rldldru', 'rldldlru'
   ],
   canExecute: () => isDocumentEditable(),
-  exec: () => store.dispatch(newThought({ insertNewSubthought: true }))
+  exec
 }

commit 9fc914cda0d46a1139dfac2e3b7f43ba2f07cd1e

Fix newThought when there is no cursor. Regression from #655.

src/action-creators/newThought.js
@@ -67,13 +67,13 @@ export default ({ at, insertNewSubthought, insertBefore, value = '', offset, pre
   const path = at || state.cursor || RANKED_ROOT
 
   // prevent adding Subthought to readonly or unextendable Thought
-  const sourceContext = insertNewSubthought ? state.cursor : contextOf(state.cursor)
-  if (meta(state, pathToContext(sourceContext)).readonly) {
-    dispatch(error(`"${ellipsize(headValue(sourceContext))}" is read-only. No subthoughts may be added.`))
+  const sourcePath = insertNewSubthought ? path : contextOf(path)
+  if (meta(state, pathToContext(sourcePath)).readonly) {
+    dispatch(error(`"${ellipsize(headValue(sourcePath))}" is read-only. No subthoughts may be added.`))
     return
   }
-  else if (meta(state, pathToContext(sourceContext)).unextendable) {
-    dispatch(error(`"${ellipsize(headValue(sourceContext))}" is unextendable. No subthoughts may be added.`))
+  else if (meta(state, pathToContext(sourcePath)).unextendable) {
+    dispatch(error(`"${ellipsize(headValue(sourcePath))}" is unextendable. No subthoughts may be added.`))
     return
   }
 