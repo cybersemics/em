ffaadbd8df170f060753eebabf9e92b6daeaf6f5
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-15 11:17:20 +0200

    converts ancestors to ts

--- src/util/ancestors.ts
+++ src/util/ancestors.ts
@@ -1,6 +1,6 @@
-//@ts-nocheck
-
 import { equalThoughtRanked } from './equalThoughtRanked'
+import { Child } from '../types'
+
 
 /** Returns a subset of thoughts from the start to the given thought (inclusive). */
-export const ancestors = (thoughtsRanked, thoughtRanked) => thoughtsRanked.slice(0, thoughtsRanked.findIndex(cur => equalThoughtRanked(cur, thoughtRanked)) + 1)
+export const ancestors = (thoughtsRanked: Child[], thoughtRanked: Child): Child[] => thoughtsRanked.slice(0, thoughtsRanked.findIndex(cur => equalThoughtRanked(cur, thoughtRanked)) + 1)
