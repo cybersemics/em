221b726a1e8a9330431f515d429695d1c67627dc
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-15 10:57:24 +0200

    updates imports

--- src/util/hashContext.ts
+++ src/util/hashContext.ts
@@ -9,7 +9,7 @@ import { pathToContext } from './pathToContext'
 const SEPARATOR_TOKEN = '__SEP__'
 
 /** Encode the thoughts (and optionally rank) as a string. */
-export const hashContext = (thoughts, rank) => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
+export const hashContext = (thoughts, rank='') => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
   .map(thought => thought ? escapeSelector(thought) : '')
   .join('__SEP__')
   + (rank != null ? SEPARATOR_TOKEN + rank : ''))
