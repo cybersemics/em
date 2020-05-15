d1c6318cbfa57a644955484e67ed896ef4c4b507
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-15 10:39:39 +0200

    renames files to TS

--- src/util/formatNumber.ts
+++ src/util/formatNumber.ts
@@ -1,3 +1,15 @@
+//@ts-nocheck
+
 /** Adds commas to a number. */
 // TODO: Localize
-export const formatNumber = (n: number, locale='en-US') => n.toLocaleString(locale)
+export const formatNumber = n => {
+  let s = '' // eslint-disable-line fp/no-let
+  const digits = n.toString().split('')
+  digits.forEach((value, i) => {
+    s = digits[digits.length - 1 - i] + s
+    if (i % 3 === 2 && i < digits.length - 1) {
+      s = ',' + s
+    }
+  })
+  return s
+}
