282921f40b257c5c5aaf0c2fa00f73ee8033bf3b
Raine Revere <raine@cybersemics.org>
2020-05-16 13:21:23 -0600

    Add concatOne, concatMany (#604)

---
+++ src/util/__tests__/concatOne.js
@@ -0,0 +1,13 @@
+import {
+  concatOne,
+} from '../../util'
+
+it('concatenate', () => {
+  expect(concatOne(['a', 'b', 'c'], 'd')).toEqual(['a', 'b', 'c', 'd'])
+})
+
+it('non-destrucitve', () => {
+  const arr = ['a', 'b', 'c']
+  concatOne(arr, 'd')
+  expect(arr).toEqual(['a', 'b', 'c'])
+})
