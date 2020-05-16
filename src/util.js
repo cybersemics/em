7371fc1718987cc207e94e0b8e6955548db30fce
Raine Revere <raine@cybersemics.org>
2020-05-16 13:29:32 -0600

    util: Combine import/export in index file.

--- src/util.js
+++ src/util.js
@@ -1,206 +1,103 @@
 // all util functions were moved into their own modules
 // this file was created to allow an iterative migration to importing them directly
 
-import { addContext } from './util/addContext'
-import { addThought } from './util/addThought'
-import { ancestors } from './util/ancestors'
-import { asyncFocus } from './util/asyncFocus'
-import { checkIfPathShareSubcontext } from './util/checkIfPathShareSubcontext'
-import { compareByRank } from './util/compareByRank'
-import { compareThought } from './util/compareThought'
-import { componentToThought } from './util/componentToThought'
-import { concatOne } from './util/concatOne'
-import { concatMany } from './util/concatMany'
-import { conjunction } from './util/conjunction'
-import { contextChainToPath } from './util/contextChainToPath'
-import { contextOf } from './util/contextOf'
-import { dataIntegrityCheck } from './util/dataIntegrityCheck'
-import { decodeCharacterEntities } from './util/decodeCharacterEntities'
-import { deleteThought } from './util/deleteThought'
-import { download } from './util/download'
-import { editableNode } from './util/editableNode'
-import { ellipsize } from './util/ellipsize'
-import { ellipsizeUrl } from './util/ellipsizeUrl'
-import { equalArrays } from './util/equalArrays'
-import { equalPath } from './util/equalPath'
-import { equalThoughtRanked } from './util/equalThoughtRanked'
-import { equalThoughtSorted } from './util/equalThoughtSorted'
-import { equalThoughtValue } from './util/equalThoughtValue'
-import { escapeRegExp } from './util/escapeRegExp'
-import { escapeSelector } from './util/escapeSelector'
-import { excludeMetaThoughts } from './util/excludeMetaThoughts'
-import { flatMap } from './util/flatMap'
-import { flatten } from './util/flatten'
-import { formatNumber } from './util/formatNumber'
-import { getPublishUrl } from './util/getPublishUrl'
-import { hasAttribute } from './util/hasAttribute'
-import { hashContext } from './util/hashContext'
-import { hashThought } from './util/hashThought'
-import { head } from './util/head'
-import { headRank } from './util/headRank'
-import { headValue } from './util/headValue'
-import { importText } from './util/importText'
-import { initEvents } from './util/initEvents'
-import { initFirebase } from './util/initFirebase'
-import { initialState } from './util/initialState'
-import { isDescendant } from './util/isDescendant'
-import { isDivider } from './util/isDivider'
-import isDocumentEditable from './util/isDocumentEditable'
-import { isElementHiddenByAutoFocus } from './util/isElementHiddenByAutoFocus'
-import { isElementInViewport } from './util/isElementInViewport'
-import { isFunction } from './util/isFunction'
-import { isHTML } from './util/isHTML'
-import { isRoot } from './util/isRoot'
-import { isEM } from './util/isEM'
-import { isURL } from './util/isURL'
-import { isArchived } from './util/isArchived'
-import { isThoughtArchived } from './util/isThoughtArchived'
-import { joinConjunction } from './util/joinConjunction'
-import { loadLocalState } from './util/loadLocalState'
-import { login } from './util/login'
-import { logout } from './util/logout'
-import { makeCompareByProp } from './util/makeCompareByProp'
-import { mergeUpdates } from './util/mergeUpdates'
-import { modalCleanup } from './util/modalCleanup'
-import { moveThought } from './util/moveThought'
-import { nextThought } from './util/nextThought'
-import { notFalse } from './util/notFalse'
-import { notNull } from './util/notNull'
-import { oppositeDirection } from './util/oppositeDirection'
-import { parseJsonSafe } from './util/parseJsonSafe'
-import { pathToContext } from './util/pathToContext'
-import { pathToIndex } from './util/pathToIndex'
-import { pathToArchive } from './util/pathToArchive'
-import { perma } from './util/perma'
-import { prevThoughtElement } from './util/prevThoughtElement'
-import publishMode from './util/publishMode'
-import { rankThoughtsSequential } from './util/rankThoughtsSequential'
-import { reduceObj } from './util/reduceObj'
-import { regExpEscapeSelector } from './util/regExpEscapeSelector'
-import { removeContext } from './util/removeContext'
-import { removeDuplicatedContext } from './util/removeDuplicatedContext'
-import { restoreCursorBeforeSearch } from './util/restoreCursorBeforeSearch'
-import { rootedContextOf } from './util/rootedContextOf'
-import { rotateClockwise } from './util/rotateClockwise'
-import { scrollIntoViewIfNeeded } from './util/scrollIntoViewIfNeeded'
-import { selectNextEditable } from './util/selectNextEditable'
-import { selectPrevEditable } from './util/selectPrevEditable'
-import { setSelection } from './util/setSelection'
-import { sort } from './util/sort'
-import { spellNumber } from './util/spellNumber'
-import { splice } from './util/splice'
-import { strip } from './util/strip'
-import { stripPunctuation } from './util/stripPunctuation'
-import { subsetThoughts } from './util/subsetThoughts'
-import { sumSubthoughtsLength } from './util/sumSubthoughtsLength'
-import { sync } from './util/sync'
-import { thoughtsEditingFromChain } from './util/thoughtsEditingFromChain'
-import { timeDifference } from './util/timeDifference'
-import { timestamp } from './util/timestamp'
-import { unroot } from './util/unroot'
-import { updateUrlHistory } from './util/updateUrlHistory'
-import urlDataSource from './util/urlDataSource'
-import { userAuthenticated } from './util/userAuthenticated'
-
-export {
-  addContext,
-  addThought,
-  ancestors,
-  asyncFocus,
-  checkIfPathShareSubcontext,
-  compareByRank,
-  compareThought,
-  componentToThought,
-  concatOne,
-  concatMany,
-  conjunction,
-  contextChainToPath,
-  contextOf,
-  dataIntegrityCheck,
-  decodeCharacterEntities,
-  deleteThought,
-  download,
-  editableNode,
-  ellipsize,
-  ellipsizeUrl,
-  equalArrays,
-  equalPath,
-  equalThoughtRanked,
-  equalThoughtSorted,
-  equalThoughtValue,
-  escapeRegExp,
-  escapeSelector,
-  excludeMetaThoughts,
-  flatMap,
-  flatten,
-  formatNumber,
-  getPublishUrl,
-  hasAttribute,
-  hashContext,
-  hashThought,
-  head,
-  headRank,
-  headValue,
-  importText,
-  initEvents,
-  initFirebase,
-  initialState,
-  isDescendant,
-  isDivider,
-  isDocumentEditable,
-  isElementHiddenByAutoFocus,
-  isElementInViewport,
-  isFunction,
-  isHTML,
-  isRoot,
-  isEM,
-  isURL,
-  isArchived,
-  isThoughtArchived,
-  joinConjunction,
-  loadLocalState,
-  login,
-  logout,
-  makeCompareByProp,
-  mergeUpdates,
-  modalCleanup,
-  moveThought,
-  nextThought,
-  notFalse,
-  notNull,
-  oppositeDirection,
-  parseJsonSafe,
-  pathToContext,
-  pathToIndex,
-  pathToArchive,
-  perma,
-  prevThoughtElement,
-  publishMode,
-  rankThoughtsSequential,
-  reduceObj,
-  regExpEscapeSelector,
-  removeContext,
-  removeDuplicatedContext,
-  restoreCursorBeforeSearch,
-  rootedContextOf,
-  rotateClockwise,
-  scrollIntoViewIfNeeded,
-  selectNextEditable,
-  selectPrevEditable,
-  setSelection,
-  sort,
-  spellNumber,
-  splice,
-  strip,
-  stripPunctuation,
-  subsetThoughts,
-  sumSubthoughtsLength,
-  sync,
-  thoughtsEditingFromChain,
-  timeDifference,
-  timestamp,
-  unroot,
-  updateUrlHistory,
-  userAuthenticated,
-  urlDataSource,
-}
+export { addContext } from './util/addContext'
+export { addThought } from './util/addThought'
+export { ancestors } from './util/ancestors'
+export { asyncFocus } from './util/asyncFocus'
+export { checkIfPathShareSubcontext } from './util/checkIfPathShareSubcontext'
+export { compareByRank } from './util/compareByRank'
+export { compareThought } from './util/compareThought'
+export { componentToThought } from './util/componentToThought'
+export { concatOne } from './util/concatOne'
+export { concatMany } from './util/concatMany'
+export { conjunction } from './util/conjunction'
+export { contextChainToPath } from './util/contextChainToPath'
+export { contextOf } from './util/contextOf'
+export { dataIntegrityCheck } from './util/dataIntegrityCheck'
+export { decodeCharacterEntities } from './util/decodeCharacterEntities'
+export { deleteThought } from './util/deleteThought'
+export { download } from './util/download'
+export { editableNode } from './util/editableNode'
+export { ellipsize } from './util/ellipsize'
+export { ellipsizeUrl } from './util/ellipsizeUrl'
+export { equalArrays } from './util/equalArrays'
+export { equalPath } from './util/equalPath'
+export { equalThoughtRanked } from './util/equalThoughtRanked'
+export { equalThoughtSorted } from './util/equalThoughtSorted'
+export { equalThoughtValue } from './util/equalThoughtValue'
+export { escapeRegExp } from './util/escapeRegExp'
+export { escapeSelector } from './util/escapeSelector'
+export { excludeMetaThoughts } from './util/excludeMetaThoughts'
+export { flatMap } from './util/flatMap'
+export { flatten } from './util/flatten'
+export { formatNumber } from './util/formatNumber'
+export { getPublishUrl } from './util/getPublishUrl'
+export { hasAttribute } from './util/hasAttribute'
+export { hashContext } from './util/hashContext'
+export { hashThought } from './util/hashThought'
+export { head } from './util/head'
+export { headRank } from './util/headRank'
+export { headValue } from './util/headValue'
+export { importText } from './util/importText'
+export { initEvents } from './util/initEvents'
+export { initFirebase } from './util/initFirebase'
+export { initialState } from './util/initialState'
+export { isDescendant } from './util/isDescendant'
+export { isDivider } from './util/isDivider'
+export { isDocumentEditable } from './util/isDocumentEditable'
+export { isElementHiddenByAutoFocus } from './util/isElementHiddenByAutoFocus'
+export { isElementInViewport } from './util/isElementInViewport'
+export { isFunction } from './util/isFunction'
+export { isHTML } from './util/isHTML'
+export { isRoot } from './util/isRoot'
+export { isEM } from './util/isEM'
+export { isURL } from './util/isURL'
+export { isArchived } from './util/isArchived'
+export { isThoughtArchived } from './util/isThoughtArchived'
+export { joinConjunction } from './util/joinConjunction'
+export { loadLocalState } from './util/loadLocalState'
+export { login } from './util/login'
+export { logout } from './util/logout'
+export { makeCompareByProp } from './util/makeCompareByProp'
+export { mergeUpdates } from './util/mergeUpdates'
+export { modalCleanup } from './util/modalCleanup'
+export { moveThought } from './util/moveThought'
+export { nextThought } from './util/nextThought'
+export { notFalse } from './util/notFalse'
+export { notNull } from './util/notNull'
+export { oppositeDirection } from './util/oppositeDirection'
+export { parseJsonSafe } from './util/parseJsonSafe'
+export { pathToContext } from './util/pathToContext'
+export { pathToIndex } from './util/pathToIndex'
+export { pathToArchive } from './util/pathToArchive'
+export { perma } from './util/perma'
+export { prevThoughtElement } from './util/prevThoughtElement'
+export { publishMode } from './util/publishMode'
+export { rankThoughtsSequential } from './util/rankThoughtsSequential'
+export { reduceObj } from './util/reduceObj'
+export { regExpEscapeSelector } from './util/regExpEscapeSelector'
+export { removeContext } from './util/removeContext'
+export { removeDuplicatedContext } from './util/removeDuplicatedContext'
+export { restoreCursorBeforeSearch } from './util/restoreCursorBeforeSearch'
+export { rootedContextOf } from './util/rootedContextOf'
+export { rotateClockwise } from './util/rotateClockwise'
+export { scrollIntoViewIfNeeded } from './util/scrollIntoViewIfNeeded'
+export { selectNextEditable } from './util/selectNextEditable'
+export { selectPrevEditable } from './util/selectPrevEditable'
+export { setSelection } from './util/setSelection'
+export { sort } from './util/sort'
+export { spellNumber } from './util/spellNumber'
+export { splice } from './util/splice'
+export { strip } from './util/strip'
+export { stripPunctuation } from './util/stripPunctuation'
+export { subsetThoughts } from './util/subsetThoughts'
+export { sumSubthoughtsLength } from './util/sumSubthoughtsLength'
+export { sync } from './util/sync'
+export { thoughtsEditingFromChain } from './util/thoughtsEditingFromChain'
+export { timeDifference } from './util/timeDifference'
+export { timestamp } from './util/timestamp'
+export { unroot } from './util/unroot'
+export { updateUrlHistory } from './util/updateUrlHistory'
+export { userAuthenticated } from './util/userAuthenticated'
+export { urlDataSource } from './util/urlDataSource'
