b7bc7296389aa9f561cb9ba313fcf96ad53e9ece
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-17 13:56:17 +0200

    converts contextOf to ts

--- src/util/contextOf.ts
+++ src/util/contextOf.ts
@@ -1,4 +1,4 @@
-//@ts-nocheck
+import { Context } from "../types";
 
 /** Gets the context of a context. */
-export const contextOf = context => context.slice(0, -1)
+export const contextOf = (context: Context): Context => context.slice(0, -1)
