ae59002e920aad58c2ba02e05720be9890e1a6e3
Raine Revere <raine@cybersemics.org>
2020-05-20 12:29:02 -0600

    Replace util/flatten with Array.prototype.flat.

--- src/util/contextChainToPath.js
+++ src/util/contextChainToPath.js
@@ -1,6 +1,4 @@
-import { flatten } from './flatten'
-
 /** join the segments of a context chain, eliminating the overlap, and return the resulting thoughtsRanked */
 // how is this different than chain()? Hmmmm... good question...
 export const contextChainToPath = contextChain =>
-  flatten([contextChain[0]].concat(contextChain.slice(1).map(thoughtsRanked => thoughtsRanked.slice(1))))
+  [contextChain[0]].concat(contextChain.slice(1).map(thoughtsRanked => thoughtsRanked.slice(1))).flat()
