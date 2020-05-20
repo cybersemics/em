4197df7bdc60029c022b2b51aa4825985a00da26
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-20 13:51:17 +0200

    converts download to ts

--- src/util/download.ts
+++ src/util/download.ts
@@ -1,9 +1,7 @@
-//@ts-nocheck
-
 /** Download data to a file.
  * See https://stackoverflow.com/a/30832210/480608.
  */
-export const download = (data, filename, type = 'text/plain') => {
+export const download = (data: string, filename: string, type = 'text/plain') => {
   const file = new Blob([data], { type })
 
   // IE10+
