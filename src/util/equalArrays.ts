cac07ee30dd5abbcb71d283a6337da375c783326
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:07:24 +0200

    converts equalArrays to ts | adds correct context type in ThoughtContext

--- src/util/equalArrays.ts
+++ src/util/equalArrays.ts
@@ -1,7 +1,5 @@
-//@ts-nocheck
-
 /** Equality for lists of lists. */
-export const equalArrays = (a, b) =>
+export const equalArrays = (a: unknown[], b: unknown[]): boolean =>
   a === b ||
   (a && b &&
   a.length === b.length &&
