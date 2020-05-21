620fad934d28291cbf3d8020b6ff5ce6951c9c2b
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:18:48 +0200

    removes ts-nocheck

--- src/util/equalThoughtValue.ts
+++ src/util/equalThoughtValue.ts
@@ -1,5 +1,3 @@
-//@ts-nocheck
-
 import { Child } from '../types'
 
 /** Makes a comparator with the given value to check a Child's value.
@@ -7,5 +5,6 @@ import { Child } from '../types'
       // finds the thought with value 'test'
       thoughtsRanked.map(equalThoughtValue('test'))
 */
+
 export const equalThoughtValue = (value: string) => (thought: Child) =>
   thought && thought.value === value
