ff21dc0748059a5758372c8047aad9262a5b20ea
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:28:53 +0200

    converts excludeMetaThoughts to ts

--- src/util/excludeMetaThoughts.ts
+++ src/util/excludeMetaThoughts.ts
@@ -1,7 +1,6 @@
-//@ts-nocheck
-
 import { isFunction } from './isFunction'
+import { Thought } from '../types'
 
 /** Exclude meta thoughts from the set of thoughts passed as ann argument. */
-export const excludeMetaThoughts = thoughts =>
+export const excludeMetaThoughts = (thoughts: Thought[]): Thought[] =>
   thoughts.filter(thought => !isFunction(thought.value))
