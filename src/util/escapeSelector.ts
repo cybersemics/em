b8e7550cbfc307a40e20d85c003336f35f5eda69
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:27:48 +0200

    converts escapeRegExp to ts

--- src/util/escapeSelector.ts
+++ src/util/escapeSelector.ts
@@ -1,8 +1,6 @@
-//@ts-nocheck
-
 import { escapeRegExp } from './escapeRegExp'
 
 export const regExpEscapeSelector = new RegExp('[' + escapeRegExp(' !"#$%&\'()*+,./:;<=>?@[]^`{|}~') + ']', 'g')
 
 /** Replace characters that are invalid in document.querySelector with their respective character codes. Prepend _ to escape leading digits. */
-export const escapeSelector = s => '_' + s.replace(regExpEscapeSelector, s => '_' + s.charCodeAt())
+export const escapeSelector = (s:string): string => '_' + s.replace(regExpEscapeSelector, s => `_${s.charCodeAt(0)}`)
