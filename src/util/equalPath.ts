8c805047b6ce403a08a6e57a51a192c77a6cd07f
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:11:28 +0200

    conversts equalPaths to ts

--- src/util/equalPath.ts
+++ src/util/equalPath.ts
@@ -1,7 +1,6 @@
-//@ts-nocheck
-
 import { equalThoughtRanked } from './equalThoughtRanked'
+import { Thought, Child } from '../types'
 
 /** Compares two thoughtsRanked arrays using { value, rank } as identity and ignoring other properties. */
-export const equalPath = (a, b) =>
+export const equalPath = (a: (Thought | Child)[], b: (Thought | Child)[]): boolean =>
   a === b || (a && b && a.length === b.length && a.every && a.every((_, i) => equalThoughtRanked(a[i], b[i])))
