7033adfde3c51a7608f05017531e157935e7c0ae
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 08:58:09 +0200

    converts ellipsizeUrl to ts

--- src/util/ellipsizeUrl.ts
+++ src/util/ellipsizeUrl.ts
@@ -1,10 +1,8 @@
-//@ts-nocheck
-
 import { URL_MAX_CHARS } from '../constants'
 import { isURL } from '../util/isURL'
 
 /** Ellipsize a value if it is a url. */
-export const ellipsizeUrl = value =>
+export const ellipsizeUrl = (value:string): string =>
   value && isURL(value) && value.length > URL_MAX_CHARS
     ? value.substring(0, URL_MAX_CHARS) + '...'
     : value
