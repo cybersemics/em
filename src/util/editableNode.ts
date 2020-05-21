d733528d5cd69665518fb7f0909974d04aad6607
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 08:54:34 +0200

    converts editableNode to ts

--- src/util/editableNode.ts
+++ src/util/editableNode.ts
@@ -1,10 +1,9 @@
-//@ts-nocheck
-
 import { hashContext } from './hashContext'
 import { headRank } from './headRank'
+import { Thought } from '../types'
 
 /** Returns the editable DOM node of the given thoughts. */
-export const editableNode = thoughtsRanked => {
+export const editableNode = (thoughtsRanked: Thought[]): Element => {
   const rank = headRank(thoughtsRanked)
   // also selects dividers
   return document.getElementsByClassName('editable-' + hashContext(thoughtsRanked, rank))[0]
