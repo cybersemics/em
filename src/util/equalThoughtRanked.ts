137b511d894284051e0f757193fd3721f7657278
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 09:13:17 +0200

    converts equalThoughtRanked to ts

--- src/util/equalThoughtRanked.ts
+++ src/util/equalThoughtRanked.ts
@@ -1,5 +1,5 @@
-//@ts-nocheck
+import { Thought, Child } from "../types";
 
 /** Compares two thought objects using { value, rank } as identity and ignoring other properties. */
-export const equalThoughtRanked = (a, b) =>
+export const equalThoughtRanked = (a: (Thought | Child), b: (Thought | Child)): boolean =>
   a === b || (a && b && a.value === b.value && a.rank === b.rank)
