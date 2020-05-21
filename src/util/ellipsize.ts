032d4a7b0124033a7ebf41e3cfb15f20f7b9f0f6
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-21 08:55:24 +0200

    converts ellipsize to ts

--- src/util/ellipsize.ts
+++ src/util/ellipsize.ts
@@ -1,8 +1,6 @@
-//@ts-nocheck
-
 import { THOUGHT_ELLIPSIZED_CHARS } from '../constants'
 
 /** Returns a string truncated with an ellipsis at a given limit n. */
-export const ellipsize = (s, n = THOUGHT_ELLIPSIZED_CHARS) =>
+export const ellipsize = (s: string, n:number = THOUGHT_ELLIPSIZED_CHARS): string =>
   // subtract 2 so that additional '...' is still within the char limit
   s.length > n - 2 ? s.slice(0, n - 2) + '...' : s
