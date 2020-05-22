03b9b0f770c1e2fc83889daacaa77f1baa97b6ae
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-22 20:31:36 +0200

    converts formatNumber to ts

--- src/util/formatNumber.ts
+++ src/util/formatNumber.ts
@@ -1,11 +1,9 @@
-//@ts-nocheck
-
-/** Adds commas to a number. */
+/** Adds commas to a number */
 // TODO: Localize
-export const formatNumber = n => {
+export const formatNumber = (n: number) => {
   let s = '' // eslint-disable-line fp/no-let
   const digits = n.toString().split('')
-  digits.forEach((value, i) => {
+  digits.forEach((_, i) => {
     s = digits[digits.length - 1 - i] + s
     if (i % 3 === 2 && i < digits.length - 1) {
       s = ',' + s
