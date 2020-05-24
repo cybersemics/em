3d1bfc8c4515789b369de901e5f8fa3c224018cf
Raine Revere <raine@cybersemics.org>
2020-05-24 13:16:51 -0600

    typo

--- src/util/__tests__/concatMany.js
+++ src/util/__tests__/concatMany.js
@@ -6,7 +6,7 @@ it('concatenate', () => {
   expect(concatMany(['a', 'b', 'c'], ['d'])).toEqual(['a', 'b', 'c', 'd'])
 })
 
-it('non-destrucitve', () => {
+it('non-destructive', () => {
   const arr = ['a', 'b', 'c']
   concatMany(arr, ['d'])
   expect(arr).toEqual(['a', 'b', 'c'])
