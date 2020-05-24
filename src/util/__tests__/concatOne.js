0943fc3dff809dd0bfbab8b5215382370defb20b
Raine Revere <raine@cybersemics.org>
2020-05-24 13:16:51 -0600

    typo

--- src/util/__tests__/concatOne.js
+++ src/util/__tests__/concatOne.js
@@ -6,7 +6,7 @@ it('concatenate', () => {
   expect(concatOne(['a', 'b', 'c'], 'd')).toEqual(['a', 'b', 'c', 'd'])
 })
 
-it('non-destrucitve', () => {
+it('non-destructive', () => {
   const arr = ['a', 'b', 'c']
   concatOne(arr, 'd')
   expect(arr).toEqual(['a', 'b', 'c'])
