0f91fbbae5abe6f10176d0b7542bc54273f9b438
Raine Revere <raine@cybersemics.org>
2020-05-25 10:15:04 -0600

    lint

--- src/redux-middleware/syncQueue.js
+++ src/redux-middleware/syncQueue.js
@@ -1,12 +1,12 @@
 import flushQueue from '../action-creators/flushQueue'
 
-/* Returns true if the state has updates in the syncQueue */
+/** Returns true if the state has updates in the syncQueue. */
 const hasUpdates = state => state && state.syncQueue && (
   Object.keys(state.syncQueue.thoughtIndexUpdates).length > 0 ||
   Object.keys(state.syncQueue.contextIndexUpdates).length > 0
 )
 
-/* Flushes the sync queue when updates are detected */
+/** Flushes the sync queue when updates are detected. */
 const syncQueueMiddleware = ({ getState, dispatch }) => {
   return next => action => {
     next(action)
