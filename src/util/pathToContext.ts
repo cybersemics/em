35341791801974cc1e0f48d2d93cba42a7c7014a
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-27 10:06:17 +0200

    converts hashContext & pathToContext to ts

--- src/util/pathToContext.ts
+++ src/util/pathToContext.ts
@@ -1,13 +1,8 @@
-//@ts-nocheck
+import { Context, Path } from "../types"
 
 /** Converts paths [{ value, rank }, ...] to contexts [key, ...]. */
 // if already converted, return a shallow copy
 // if falsey, return as-is
-export const pathToContext = thoughts => {
-  return thoughts
-    ? thoughts.length > 0 && typeof thoughts[0] === 'object' && 'value' in thoughts[0]
-      ? thoughts.map(child => child.value)
-      : thoughts.slice()
-    // return falsey value as-is
-    : thoughts
-}
+
+const isPath = (param: any): param is Path =>  !!(param as Path).length && (param as Path)[0].hasOwnProperty('value')
+export const pathToContext = (thoughts: Path | Context): Context  => isPath(thoughts) ? thoughts.map(child => child.value) : thoughts.slice()
