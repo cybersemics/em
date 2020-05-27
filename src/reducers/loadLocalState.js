7ef39ecb7c356aff60bcb3e438b83dc3ac685e11
Raine Revere <raine@cybersemics.org>
2020-05-27 12:46:16 -0600

    Separate loadLocalThoughts from loadLocalState.

--- src/reducers/loadLocalState.js
+++ src/reducers/loadLocalState.js
@@ -4,30 +4,9 @@ import render from './render'
 export default (state, { newState }) =>
   render({
     ...state,
-    isLoading: false,
-    cursor: newState.cursor,
-    cursorBeforeEdit: newState.cursorBeforeEdit,
-    schemaVersion: newState.schemaVersion,
-    thoughts: {
-      contextIndex: {
-        ...state.thoughts.contextIndex,
-        ...newState.thoughts.contextIndex,
-      },
-      thoughtIndex: {
-        ...state.thoughts.thoughtIndex,
-        ...newState.thoughts.thoughtIndex,
-      },
-    },
-    contextViews: {
-      ...state.contextViews,
-      ...newState.contextViews
-    },
-    expanded: {
-      ...state.expanded,
-      ...newState.expanded
-    },
     recentlyEdited: {
       ...state.recentlyEdited,
       ...newState.recentlyEdited
     },
+    schemaVersion: newState.schemaVersion,
   })
