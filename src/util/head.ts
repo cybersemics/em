bb34b3fef839b00927e28473db1c85c04c821499
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-27 10:08:31 +0200

    converts head to ts

--- src/util/head.ts
+++ src/util/head.ts
@@ -1,4 +1,4 @@
-//@ts-nocheck
+import { Path } from "../types";
 
 /** Gets the signifying label of the given context. */
-export const head = thoughts => thoughts[thoughts.length - 1]
+export const head = (thoughts: Path) => thoughts[thoughts.length - 1]
