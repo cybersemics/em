a839feb8a24dbed8904c57c7062c0a54c7a93c13
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:14:59 +0200

    converts removeContext to ts

--- src/util/removeContext.ts
+++ src/util/removeContext.ts
@@ -1,12 +1,10 @@
-//@ts-nocheck
-
 import { timestamp } from './timestamp'
 import { equalArrays } from './equalArrays'
 import { notNull } from './notNull'
+import { Context, Thought } from '../types'
 
 /** Returns a new thought less the given context. */
-export const removeContext = (thought, context, rank) => {
-  if (typeof thought === 'string') throw new Error('removeContext expects an [object] thought, not a [string] value.')
+export const removeContext = (thought: Thought, context: Context, rank: number): Thought => {
   return Object.assign({}, thought, notNull({
     contexts: thought.contexts ? thought.contexts.filter(parent =>
       !(equalArrays(parent.context, context) && (rank == null || parent.rank === rank))
