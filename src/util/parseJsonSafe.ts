9bdda0e54b253a44e8ad2cb1785ff9a1474c5eac
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 10:15:21 +0200

    converts parseJsonSafe to ts

--- src/util/parseJsonSafe.ts
+++ src/util/parseJsonSafe.ts
@@ -1,10 +1,10 @@
-//@ts-nocheck
+import { Nullable } from "../utilTypes"
 
 /** Safely JSON parse an unknown value, and default to a given fallback value. */
-export const parseJsonSafe = (value, defaultValue) => {
+export const parseJsonSafe = (value: Nullable<string>, defaultValue: number) => {
   let parsedValue = defaultValue // eslint-disable-line fp/no-let
   try {
-    parsedValue = JSON.parse(value)
+    parsedValue = value && JSON.parse(value)
   }
   catch (e) {
   }
