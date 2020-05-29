5df05b2f9dea0f374569a8e6d38bd6a320eb9e53
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 14:28:40 +0200

    converts selectNextEditable to ts

--- src/util/selectNextEditable.ts
+++ src/util/selectNextEditable.ts
@@ -1,10 +1,8 @@
-//@ts-nocheck
-
 /** Focus on the next .editable element in the DOM after the given .editable. May be a sibling or the nearest ancestor's next sibling. */
-export const selectNextEditable = currentNode => {
+export const selectNextEditable = (currentNode: Node) => {
   const allElements = document.querySelectorAll('.editable')
   const currentIndex = Array.prototype.findIndex.call(allElements, el => currentNode.isEqualNode(el))
   if (currentIndex < allElements.length - 1) {
-    allElements[currentIndex + 1].focus()
+    (allElements[currentIndex + 1] as HTMLElement).focus()
   }
 }
