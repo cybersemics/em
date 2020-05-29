d3d42d5216d8780e7703c1b15a3c75a65984adae
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 14:59:58 +0200

    converts spellNumber to ts

--- src/util/spellNumber.ts
+++ src/util/spellNumber.ts
@@ -1,6 +1,4 @@
-//@ts-nocheck
-
 const NUMBERS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
 
 /** Returns the name of a given number, 0-20. */
-export const spellNumber = n => NUMBERS[n] || n
+export const spellNumber = (n: number) => NUMBERS[n] || n
