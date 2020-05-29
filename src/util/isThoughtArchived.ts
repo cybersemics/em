ca733dfba2b82ce60b9381b8d0d0ae49c28a8bfa
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:49:40 +0200

    converts isThoughtArchived to ts

--- src/util/isThoughtArchived.ts
+++ src/util/isThoughtArchived.ts
@@ -1,10 +1,9 @@
-//@ts-nocheck
-
 // util
 import {
   equalThoughtValue,
 } from '../util'
+import { Path } from '../types'
 
 /** Determines whether a thought is archived or not. */
-export const isThoughtArchived = path =>
-  path.find(equalThoughtValue('=archive')) || false
+export const isThoughtArchived = (path: Path) =>
+  path.some(equalThoughtValue('=archive')) || false
