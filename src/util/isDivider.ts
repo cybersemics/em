cd9c05e33ea3c3f4ff049302d59c73ec7aeded4f
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 07:37:42 +0200

    converts isDivider to ts

--- src/util/isDivider.ts
+++ src/util/isDivider.ts
@@ -1,5 +1,3 @@
-//@ts-nocheck
-
 /** Returns true if the value starts with multiple dashes and should be interpreted as a divider. */
-export const isDivider = s =>
+export const isDivider = (s: string) =>
   s && (s.startsWith('---') || s.startsWith('—'))
