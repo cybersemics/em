74982ee2ed5cb022955efd9b7bbbd49a2ae1218c
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:37:36 +0200

    converts rootedContextOf to ts

--- src/util/rootedContextOf.ts
+++ src/util/rootedContextOf.ts
@@ -1,11 +1,10 @@
-//@ts-nocheck
-
 import {
   ROOT_TOKEN,
 } from '../constants'
 
 // util
 import { contextOf } from './contextOf'
+import { Path, Context } from '../types'
 
 /** Get the contextOf of thoughts or [ROOT_TOKEN] if there are none. */
-export const rootedContextOf = thoughts => thoughts.length > 1 ? contextOf(thoughts) : [ROOT_TOKEN]
+export const rootedContextOf = (thoughts: Context | Path) => thoughts.length > 1 ? contextOf(thoughts) : [ROOT_TOKEN]
