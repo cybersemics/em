e28805649abe6f794f2fd93be5932a737bae960a
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:50:08 +0200

    converts joinConjunction to ts

--- src/util/joinConjunction.ts
+++ src/util/joinConjunction.ts
@@ -1,7 +1,5 @@
-//@ts-nocheck
-
 /** Join a list of strings with "," and insert the given conjunction (default: 'and') before the last string. */
-export const joinConjunction = (arr, conjunction = 'and') =>
+export const joinConjunction = (arr: string[], conjunction = 'and') =>
   arr.length === 0 ? ''
   : arr.length === 1 ? arr[0]
   : arr.slice(0, arr.length - 1).join(', ') + (arr.length === 2 ? '' : ',') + ` ${conjunction} ` + arr[arr.length - 1]
