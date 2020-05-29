d20764f0c5070981a425a594a16a717a06b582b3
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:53:20 +0200

    converts isHTML to ts

--- src/util/isHTML.ts
+++ src/util/isHTML.ts
@@ -1,6 +1,4 @@
-//@ts-nocheck
-
 import { REGEXP_HTML } from '../constants'
 
 /** Checks if string contains html elements. */
-export const isHTML = s => REGEXP_HTML.test(s)
+export const isHTML = (s: string) => REGEXP_HTML.test(s)
