9df1ca0aa1d5371ff197b9156c17267800db2bb1
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:48:37 +0200

    converts isArchived to ts

--- src/util/isArchived.ts
+++ src/util/isArchived.ts
@@ -1,6 +1,6 @@
-//@ts-nocheck
+import { Thought } from "../types";
 
 /** Determines whether an indexed thought is archived or not. */
-export const isArchived = indexedThought =>
+export const isArchived = (indexedThought: Thought) =>
   // root thought does not have a contexts property
   !(indexedThought.contexts || []).some(context => (context.context || []).indexOf('=archive') === -1) || false
