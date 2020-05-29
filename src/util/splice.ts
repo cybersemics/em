90134872dfa37a0ace0e9fb64b0b89ffb9332beb
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 15:00:12 +0200

    converts splice to ts

--- src/util/splice.ts
+++ src/util/splice.ts
@@ -1,8 +1,6 @@
-//@ts-nocheck
-
 /** Pure splice. */
-export const splice = (arr, start, deleteCount, ...thoughts) =>
-  [].concat(
+export const splice = <T>(arr: T[], start: number, deleteCount: number, ...thoughts: T[]) =>
+  ([] as T[]).concat(
     arr.slice(0, start),
     thoughts,
     arr.slice(start + deleteCount)
