2908bd44fe0c83ebab31c4717ca0591596cba55f
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:10:03 +0200

    converts rankThoughtsSequential to ts

--- src/util/rankThoughtsSequential.ts
+++ src/util/rankThoughtsSequential.ts
@@ -1,5 +1,5 @@
-//@ts-nocheck
+import { Context, Child } from "../types";
 
 /** Ranks the thoughts from 0 to n in the given array order. */
-export const rankThoughtsSequential = thoughts =>
+export const rankThoughtsSequential = (thoughts: Context) =>
   thoughts.map((thought, i) => ({ value: thought, rank: i }))
