e52c79f409a2baddbe731ca10e5c09290dfa28d6
Raine Revere <raine@cybersemics.org>
2020-05-28 20:21:30 -0600

    Test: reducers/deleteAttribute

---
+++ src/reducers/__tests__/deleteAttribute.js
@@ -0,0 +1,39 @@
+import { ROOT_TOKEN } from '../../constants'
+import { initialState, reducerFlow } from '../../util'
+import { exportContext } from '../../selectors'
+
+// reducers
+import deleteAttribute from '../deleteAttribute'
+import newThought from '../newThought'
+import setAttribute from '../setAttribute'
+import setCursor from '../setCursor'
+
+it('delete attribute', () => {
+
+  const steps = [
+
+    // new thought
+    state => newThought(state, { value: 'a' }),
+
+    // set attribute
+    state => setAttribute(state, {
+      context: ['a'],
+      key: '=test',
+      value: 'hello'
+    }),
+
+    // delete attribute
+    state => deleteAttribute(state, {
+      context: ['a'],
+      key: '=test'
+    })
+  ]
+
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+
+  expect(exported).toBe(`- ${ROOT_TOKEN}
+  - a`)
+
+})
