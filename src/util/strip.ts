c0e026d4875106f1921c054eb8ceb085a5bb1b24
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 15:00:21 +0200

    converts strip to ts

--- src/util/strip.ts
+++ src/util/strip.ts
@@ -1,12 +1,11 @@
-//@ts-nocheck
-
 const regexAllTags = /<(?:.|\n)*?>/gmi
 const regexPreserveFormattingTags = /<(?!\/?[biu](?: (?:.|\n)*)?>)(?:.|\n)*?>/gmi
 const regexTagAndAttributes = /<(?![/])(?:(\w*)((?:.|\n)*?))\/?>/gmi
 const regexNbsp = /&nbsp;/gmi
 
+type StripOptions = {preserveFormatting: boolean, preventTrim: boolean}
 /** Strip HTML tags, convert nbsp to normal spaces, and trim. */
-export const strip = (html, { preserveFormatting, preventTrim } = {}) => {
+export const strip = (html: string, { preserveFormatting, preventTrim }: StripOptions = { preserveFormatting: false, preventTrim: false}) => {
   const replacedHtml = html
     .replace(preserveFormatting ? regexPreserveFormattingTags : regexAllTags, '')
     // second pass to replace formatting tag attributes e.g. <b style="...">
