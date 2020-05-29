6ce428603099e0d23f884a6600ba1e4429b58cef
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:14:19 +0200

    converts reducerFlow to ts

--- src/util/reducerFlow.ts
+++ src/util/reducerFlow.ts
@@ -1,10 +1,12 @@
+import { InitialStateInterface } from "./initialState";
+
 /**
  * Composes a list of reducers in order and merges the results.
  *
  * @param reducers      A list of unary reducers of type `oldState => newState`.
  * @param initialState
  */
-export const reducerFlow = (reducers: Function[]) => (initialState: any) =>
+export const reducerFlow = (reducers: Function[]) => (initialState: InitialStateInterface) =>
   reducers.reduce((state, reducer) => ({
     ...state,
     ...reducer ? reducer(state) : null,
