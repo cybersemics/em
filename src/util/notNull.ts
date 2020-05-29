d20e1af98332e3532eba94e95b850c5c663f158a
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 09:28:30 +0200

    refactors and converts notNull to ts

--- src/util/notNull.ts
+++ src/util/notNull.ts
@@ -1,12 +1,4 @@
 //@ts-nocheck
 
 /** Returns a shallow copy of an object with all keys that do not have a value of null or undefined. */
-export const notNull = o => {
-  const output = {}
-  Object.keys(o).forEach(key => {
-    if (o[key] != null) {
-      output[key] = o[key]
-    }
-  })
-  return output
-}
+export const notNull = (o: {[key: string]: any}) => Object.keys(o).reduce((acc, key) => (o[key]!==null ? {...acc, [key]: o[key]} : acc), {})
