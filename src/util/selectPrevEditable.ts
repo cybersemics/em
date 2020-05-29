5807b0c80116c45ab66aff6fb2f5e42d946845c0
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 14:28:54 +0200

    converts selectPrevEditable to ts

--- src/util/selectPrevEditable.ts
+++ src/util/selectPrevEditable.ts
@@ -1,10 +1,8 @@
-//@ts-nocheck
-
 /** Focus on the previous .editable element in the DOM before the given .editable. May be a sibling or the nearest ancestor's prev sibling. */
-export const selectPrevEditable = currentNode => {
+export const selectPrevEditable = (currentNode: Node) => {
   const allElements = document.querySelectorAll('.editable')
   const currentIndex = Array.prototype.findIndex.call(allElements, el => currentNode.isEqualNode(el))
   if (currentIndex > 0) {
-    allElements[currentIndex - 1].focus()
+    (allElements[currentIndex - 1] as HTMLElement).focus()
   }
 }
