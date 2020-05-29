4c5dfa171aa659023d89938b38219fb77dbff96b
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 10:26:42 +0200

    converts prevThoughtElement to ts

--- src/util/prevThoughtElement.ts
+++ src/util/prevThoughtElement.ts
@@ -1,9 +1,9 @@
-//@ts-nocheck
-
 import { editableNode } from './editableNode'
+import { Path } from '../types'
+import { Nullable } from '../utilTypes'
 
 /** Gets the editable node immediately before the node of the given path. */
-export const prevThoughtElement = path => {
+export const prevThoughtElement = (path: Path): Nullable<HTMLElement> => {
   const editable = path && editableNode(path)
   const child = editable && editable.closest('.child')
   const prevSubthought = child && child.previousElementSibling
