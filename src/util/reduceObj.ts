b181f3a249dee70d0506787a85a0eecff2e1f1b8
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:10:25 +0200

    converts redeuceObj to ts

--- src/util/reduceObj.ts
+++ src/util/reduceObj.ts
@@ -1,11 +1,12 @@
-//@ts-nocheck
+import { GenericObject } from "../utilTypes"
 
+type ReducerFunctionType = (a: string, b: any, accum: GenericObject) => GenericObject
 /** Reduces an object to another object constructed from all the key-value pairs that the reducer f returns.
  *
  * @param obj    An object.
  * @param f      (key, value) => { ... }.
  */
-export const reduceObj = (obj, f) =>
+export const reduceObj = (obj: GenericObject, f: ReducerFunctionType) =>
   Object.keys(obj).reduce((accum, key) => {
     const o = f(key, obj[key], accum)
     const insideObj = Object.keys(o).reduce((oaccum, okey) => ({
