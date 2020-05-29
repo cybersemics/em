689276270c572ef4bc630965d597aad8200ee4cd
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:53:03 +0200

    converts isFunction to ts

--- src/util/isFunction.ts
+++ src/util/isFunction.ts
@@ -1,4 +1,2 @@
-//@ts-nocheck
-
 /** Returns true if the given value starts `=`, indicating a metaprogramming attribute. */
-export const isFunction = s => s.startsWith('=')
+export const isFunction = (s: string) => s.startsWith('=')
