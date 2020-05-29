ecaf175ae3ea998dece1db120bc006ff899caf1b
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 07:54:44 +0200

    converts isElementHiddenByAutoFocus to ts

--- src/util/isElementHiddenByAutoFocus.ts
+++ src/util/isElementHiddenByAutoFocus.ts
@@ -1,8 +1,6 @@
-//@ts-nocheck
-
 /** Returns true if the element has been hidden by the distance-from-cursor autofocus. */
-export const isElementHiddenByAutoFocus = el => {
+export const isElementHiddenByAutoFocus = (el: HTMLElement) => {
   const children = el.closest('.children')
-  return (children.classList.contains('distance-from-cursor-2') && !el.closest('.cursor-parent')) ||
-    children.classList.contains('distance-from-cursor-3')
+  return children ? (children.classList.contains('distance-from-cursor-2') && !el.closest('.cursor-parent')) ||
+    children.classList.contains('distance-from-cursor-3') : false
 }
