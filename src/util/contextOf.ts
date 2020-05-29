821e0a71f415d51950ec8d872ae12659f78e3eb2
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 13:37:36 +0200

    converts rootedContextOf to ts

--- src/util/contextOf.ts
+++ src/util/contextOf.ts
@@ -3,6 +3,7 @@ import { Context, Path } from "../types";
 type ReturnType = {
   (param:Context): Context;
   (param:Path): Path;
+  (param: Path | Context) : Path | Context
 }
 
 /** Gets the context of a context. */
