448b17353b4f3db8896db4747f577f36b9f1f90c
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-29 08:53:30 +0200

    converts isURL to ts

--- src/util/isURL.ts
+++ src/util/isURL.ts
@@ -1,6 +1,4 @@
-//@ts-nocheck
-
 import { REGEXP_URL } from '../constants'
 
 /** Checks if string contains URL. */
-export const isURL = s => REGEXP_URL.test(s)
+export const isURL = (s: string) => REGEXP_URL.test(s)
