5396d4fdf8cc64e84800a3f98653c96df747251a
Raine Revere <raine@cybersemics.org>
2020-05-30 15:16:24 -0600

    Separate reducers index from app reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,146 +1,66 @@
-// reducers
-import alert from './alert'
-import archiveThought from './archiveThought'
-import authenticate from './authenticate'
-import clear from './clear'
-import clearQueue from './clearQueue'
-import cursorBack from './cursorBack'
-import cursorBeforeSearch from './cursorBeforeSearch'
-import cursorDown from './cursorDown'
-import cursorForward from './cursorForward'
-import cursorHistory from './cursorHistory'
-import cursorUp from './cursorUp'
-import deleteAttribute from './deleteAttribute'
-import deleteData from './deleteData'
-import deleteEmptyThought from './deleteEmptyThought'
-import deleteSubthoughts from './deleteSubthoughts'
-import deleteThought from './deleteThought'
-import dragInProgress from './dragInProgress'
-import editing from './editing'
-import editingValue from './editingValue'
-import error from './error'
-import existingThoughtChange from './existingThoughtChange'
-import existingThoughtDelete from './existingThoughtDelete'
-import existingThoughtMove from './existingThoughtMove'
-import expandContextThought from './expandContextThought'
-import indent from './indent'
-import invalidState from './invalidState'
-import loadLocalState from './loadLocalState'
-import loadLocalThoughts from './loadLocalThoughts'
-import modalComplete from './modalComplete'
-import modalRemindMeLater from './modalRemindMeLater'
-import moveThoughtDown from './moveThoughtDown'
-import moveThoughtUp from './moveThoughtUp'
-import newThought from './newThought'
-import newThoughtSubmit from './newThoughtSubmit'
-import outdent from './outdent'
-import prependRevision from './prependRevision'
-import render from './render'
-import search from './search'
-import searchLimit from './searchLimit'
-import selectionChange from './selectionChange'
-import setAttribute from './setAttribute'
-import setCursor from './setCursor'
-import setFirstSubthought from './setFirstSubthought'
-import setResourceCache from './setResourceCache'
-import settings from './settings'
-import showModal from './showModal'
-import splitThought from './splitThought'
-import status from './status'
-import subCategorizeAll from './subCategorizeAll'
-import subCategorizeOne from './subCategorizeOne'
-import toggleAttribute from './toggleAttribute'
-import toggleCodeView from './toggleCodeView'
-import toggleContextView from './toggleContextView'
-import toggleHiddenThoughts from './toggleHiddenThoughts'
-import toggleSidebar from './toggleSidebar'
-import toggleSplitView from './toggleSplitView'
-import tutorial from './tutorial'
-import tutorialChoice from './tutorialChoice'
-import tutorialNext from './tutorialNext'
-import tutorialPrev from './tutorialPrev'
-import tutorialStep from './tutorialStep'
-import undoArchive from './undoArchive'
-import unknownAction from './unknownAction'
-import updateSplitPosition from './updateSplitPosition'
-import updateThoughts from './updateThoughts'
-import { prioritizeScroll, setToolbarOverlay } from './toolbarOverlay'
-
-import { initialState } from '../util'
-
-const reducerMap = {
-  alert,
-  archiveThought,
-  authenticate,
-  clear,
-  clearQueue,
-  cursorBack,
-  cursorBeforeSearch,
-  cursorDown,
-  cursorForward,
-  cursorHistory,
-  cursorUp,
-  deleteAttribute,
-  deleteData,
-  deleteEmptyThought,
-  deleteSubthoughts,
-  deleteThought,
-  dragInProgress,
-  editing,
-  editingValue,
-  error,
-  existingThoughtChange,
-  existingThoughtDelete,
-  existingThoughtMove,
-  expandContextThought,
-  indent,
-  invalidState,
-  loadLocalState,
-  loadLocalThoughts,
-  modalComplete,
-  modalRemindMeLater,
-  moveThoughtDown,
-  moveThoughtUp,
-  newThought,
-  newThoughtSubmit,
-  outdent,
-  prependRevision,
-  prioritizeScroll,
-  render,
-  search,
-  searchLimit,
-  selectionChange,
-  setAttribute,
-  setCursor,
-  setFirstSubthought,
-  setResourceCache,
-  settings,
-  setToolbarOverlay,
-  subCategorizeAll,
-  subCategorizeOne,
-  showModal,
-  splitThought,
-  status,
-  toggleAttribute,
-  toggleCodeView,
-  toggleContextView,
-  toggleHiddenThoughts,
-  toggleSidebar,
-  toggleSplitView,
-  tutorial,
-  tutorialChoice,
-  tutorialNext,
-  tutorialPrev,
-  tutorialStep,
-  undoArchive,
-  updateSplitPosition,
-  updateThoughts,
-}
-
-/**
- * The main reducer.
- * Use action.type to select the reducer with the same name.
- * Otherwise throw an error with unknownAction.
- */
-export default (state = initialState(), action) =>
-  (reducerMap[action.type] || unknownAction)(state, action)
+export { default as alert } from './alert'
+export { default as archiveThought } from './archiveThought'
+export { default as authenticate } from './authenticate'
+export { default as clear } from './clear'
+export { default as clearQueue } from './clearQueue'
+export { default as cursorBack } from './cursorBack'
+export { default as cursorBeforeSearch } from './cursorBeforeSearch'
+export { default as cursorDown } from './cursorDown'
+export { default as cursorForward } from './cursorForward'
+export { default as cursorHistory } from './cursorHistory'
+export { default as cursorUp } from './cursorUp'
+export { default as deleteAttribute } from './deleteAttribute'
+export { default as deleteData } from './deleteData'
+export { default as deleteEmptyThought } from './deleteEmptyThought'
+export { default as deleteSubthoughts } from './deleteSubthoughts'
+export { default as deleteThought } from './deleteThought'
+export { default as dragInProgress } from './dragInProgress'
+export { default as editing } from './editing'
+export { default as editingValue } from './editingValue'
+export { default as error } from './error'
+export { default as existingThoughtChange } from './existingThoughtChange'
+export { default as existingThoughtDelete } from './existingThoughtDelete'
+export { default as existingThoughtMove } from './existingThoughtMove'
+export { default as expandContextThought } from './expandContextThought'
+export { default as indent } from './indent'
+export { default as invalidState } from './invalidState'
+export { default as loadLocalState } from './loadLocalState'
+export { default as loadLocalThoughts } from './loadLocalThoughts'
+export { default as modalComplete } from './modalComplete'
+export { default as modalRemindMeLater } from './modalRemindMeLater'
+export { default as moveThoughtDown } from './moveThoughtDown'
+export { default as moveThoughtUp } from './moveThoughtUp'
+export { default as newThought } from './newThought'
+export { default as newThoughtSubmit } from './newThoughtSubmit'
+export { default as outdent } from './outdent'
+export { default as prependRevision } from './prependRevision'
+export { default as render } from './render'
+export { default as search } from './search'
+export { default as searchLimit } from './searchLimit'
+export { default as selectionChange } from './selectionChange'
+export { default as setAttribute } from './setAttribute'
+export { default as setCursor } from './setCursor'
+export { default as setFirstSubthought } from './setFirstSubthought'
+export { default as setResourceCache } from './setResourceCache'
+export { default as settings } from './settings'
+export { default as showModal } from './showModal'
+export { default as splitThought } from './splitThought'
+export { default as status } from './status'
+export { default as subCategorizeAll } from './subCategorizeAll'
+export { default as subCategorizeOne } from './subCategorizeOne'
+export { default as toggleAttribute } from './toggleAttribute'
+export { default as toggleCodeView } from './toggleCodeView'
+export { default as toggleContextView } from './toggleContextView'
+export { default as toggleHiddenThoughts } from './toggleHiddenThoughts'
+export { default as toggleSidebar } from './toggleSidebar'
+export { default as toggleSplitView } from './toggleSplitView'
+export { default as tutorial } from './tutorial'
+export { default as tutorialChoice } from './tutorialChoice'
+export { default as tutorialNext } from './tutorialNext'
+export { default as tutorialPrev } from './tutorialPrev'
+export { default as tutorialStep } from './tutorialStep'
+export { default as undoArchive } from './undoArchive'
+export { default as unknownAction } from './unknownAction'
+export { default as updateSplitPosition } from './updateSplitPosition'
+export { default as updateThoughts } from './updateThoughts'
+export { prioritizeScroll, setToolbarOverlay } from './toolbarOverlay'
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 /** Defines the Redux app reducer and exports a global store.
-  NOTE: Exporting the store is not compatible with server-side rendering.
+ * NOTE: Exporting the store is not compatible with server-side rendering.
  */
 
 import { applyMiddleware, createStore } from 'redux'
@@ -7,9 +7,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import syncQueue from './redux-middleware/syncQueue'
 import updateUrlHistory from './redux-middleware/updateUrlHistory'
-
-// app reducer
-import appReducer from './reducers'
+import appReducer from './reducers/app'
 
 const composeEnhancers = composeWithDevTools({ trace: true })
 
