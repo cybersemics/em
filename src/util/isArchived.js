5ea6c694eb5e4bc5e280ca0342a607f4df3fdd49
Raine Revere <raine@cybersemics.org>
2020-05-30 08:54:45 -0600

    isArchived: Guard against null context.

--- src/util/isArchived.js
+++ src/util/isArchived.js
@@ -1,4 +1,4 @@
 /** Determines whether an indexed thought is archived or not. */
 export const isArchived = indexedThought =>
   // root thought does not have a contexts property
-  !(indexedThought.contexts || []).some(context => context.context.indexOf('=archive') === -1) || false
+  !(indexedThought.contexts || []).some(context => (context.context || []).indexOf('=archive') === -1) || false
