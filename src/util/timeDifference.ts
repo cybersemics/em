9b2bce937b7633735bc66827044738b9327ab435
Anmol Arora <anmolaroraxz@gmail.com>
2020-05-30 13:59:01 +0200

    convert timeDifference to ts

--- src/util/timeDifference.ts
+++ src/util/timeDifference.ts
@@ -1,5 +1,3 @@
-//@ts-nocheck
-
 /** Returns the difference in seconds between two timestamps (milliseconds). */
-export const timeDifference = (timestamp1, timestamp2) =>
+export const timeDifference = (timestamp1: string, timestamp2: string) =>
   Math.floor((new Date(timestamp1).getTime() - new Date(timestamp2).getTime()) / 1000)
