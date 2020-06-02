9365f94e24f745709a0d8e96f52a67e26697fac0
aqkhan <aqk.neo@gmail.com>
2020-06-02 20:19:23 +0500

    Replaced regex to remove spaces with trin()

--- src/util/compareThought.js
+++ src/util/compareThought.js
@@ -2,10 +2,9 @@ import { lower } from './lower'
 
 const regexPunctuation = /^[!@#$%^&*()\-_=+[\]{};:'"<>.,?\\/].*/
 const regexEmojis = /([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?|[\u20E3]|[\u26A0-\u3000]|\uD83E[\udd00-\uddff]|[\u00A0-\u269F]/g
-const regexSpaces = /^\s+|\s+$/g
 
 /** Remove emojis and trailing/leading spaces from camparator inputs using regex. */
-const removeEmojisAndSpaces = str => str.replace(regexEmojis, '').replace(regexSpaces, '')
+const removeEmojisAndSpaces = str => str.replace(regexEmojis, '').trim()
 
 /** The default comparator that can be used in sort. */
 export const compare = (a, b) => a > b ? 1 : a < b ? -1 : 0
