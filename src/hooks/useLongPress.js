d8974ef76fd75e0b36b4925f652782a93e75357c
Raine Revere <raine@cybersemics.org>
2020-06-02 13:25:35 -0600

    useLongPress: cleanup

--- src/hooks/useLongPress.js
+++ src/hooks/useLongPress.js
@@ -1,11 +1,14 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { NOOP } from '../constants'
 
 /** Custom hook useLongPress.js to manage long press. */
-export default function useLongPress(onLongPressStart = () => {}, onLongPressEnd = () => {}, ms = 250) {
+export default function useLongPress(onLongPressStart = NOOP, onLongPressEnd = NOOP, ms = 250) {
   const [startLongPress, setStartLongPress] = useState(false)
   const [startCallbackDispatched, setStartCallbackDispatched] = useState(false)
   const timerIdRef = useRef()
 
+  // when a long press is started, set a timer
+  // after the timer completes invoke the callback
   useEffect(() => {
     if (startLongPress) {
       timerIdRef.current = setTimeout(() => {
@@ -18,17 +21,21 @@ export default function useLongPress(onLongPressStart = () => {}, onLongPressEnd
     return () => clearTimeout(timerIdRef.current)
   }, [startLongPress])
 
+  // track that long press has started on mouseDown or touchStart
   const start = useCallback(e => {
     setStartLongPress(true)
   }, [])
+
+  // track that long press has stopped on mouseUp, touchEnd, or touchCancel
   const stop = useCallback(() => {
     setStartLongPress(false)
     onLongPressEnd()
     setStartCallbackDispatched(false)
   }, [])
+
+  // set long press state on scroll depending on completion of timer
   const scroll = useCallback(() => {
-    if (!startCallbackDispatched) setStartLongPress(false)
-    else setStartLongPress(true)
+    setStartLongPress(startCallbackDispatched)
   }, [startCallbackDispatched])
 
   return {
