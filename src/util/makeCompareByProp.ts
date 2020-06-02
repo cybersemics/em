0d5117581a451a8b7ff7ea0b1b5cec3719fc4bb2
Anmol Arora <anmolaroraxz@gmail.com>
2020-06-02 13:44:36 +0200

    adds type check to isGreater and isSmaller functions to validate keys

--- src/util/makeCompareByProp.ts
+++ src/util/makeCompareByProp.ts
@@ -1,18 +1,18 @@
 import { lower } from './lower'
 
-import { ComparatorFunction } from '../utilTypes'
+import { ComparatorFunction, GenericObject } from '../utilTypes'
 
 /** Returns true if the first object's key is greater than the second object's key. */
-export const isGreater = (a:Object, b:Object, key: keyof typeof a & keyof typeof b) => lower(a[key]) > lower(b[key])
+export const isGreater = <T1 extends GenericObject, T2 extends GenericObject, K extends keyof T1 & keyof T2>(a:T1, b:T2, key: K) => lower(a[key]) > lower(b[key])
 
 /** Returns true if the first object's key is smaller than the second object's key. */
-export const isSmaller = (a:Object, b:Object, key: keyof typeof a & keyof typeof b) => lower(a[key]) < lower(b[key])
+export const isSmaller = <T1 extends GenericObject, T2 extends GenericObject, K extends keyof T1 & keyof T2>(a:T1, b:T2, key: K) => lower(a[key]) < lower(b[key])
 
 /**
  * Creates a function that takes two values and compares the given key.
  * Does case insensitive comparison with strings.
  */
-export const makeCompareByProp = (key:any):ComparatorFunction<Object> => (a: Object, b: Object) =>
+export const makeCompareByProp = (key:any):ComparatorFunction<GenericObject> => (a: GenericObject, b: GenericObject) =>
   isGreater(a, b, key) ? 1
   : isSmaller(a, b, key) ? -1
   : 0
