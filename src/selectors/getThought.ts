32404fb87e2f86ce7ecd00ce8fff8d8d77a5a958
Raine Revere <raine@cybersemics.org>
2020-06-03 14:37:01 -0600

    Lint and fix type errors.

--- src/selectors/getThought.ts
+++ src/selectors/getThought.ts
@@ -1,9 +1,8 @@
 // util
 import { hashThought } from '../util'
-import { InitialStateInterface } from '../util/initialState'
+import { PartialStateWithThoughts } from '../util/initialState'
 import { Thought } from '../types'
 
-type PartialStateWithThoughts = Partial<InitialStateInterface> & Pick<InitialStateInterface, 'thoughts'>
 /** Gets a single thought with a list of its contexts from the thoughtIndex. */
 export const getThought = ({ thoughts: { thoughtIndex } }: PartialStateWithThoughts, value: string): Thought =>
   thoughtIndex[hashThought(value)]
--- src/util/flatten.ts
+++ src/util/flatten.ts
@@ -1,3 +1,6 @@
 // @ts-nocheck
 
+/**
+ *
+ */
 export const flatten = list => Array.prototype.concat.apply([], list)
