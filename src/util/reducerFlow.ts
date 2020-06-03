75a66c6eedc4494df4860448b1fb7040fb9c6677
Raine Revere <raine@cybersemics.org>
2020-06-03 15:00:09 -0600

    reducerFlow: Add UnaryReducer type.

--- src/util/reducerFlow.ts
+++ src/util/reducerFlow.ts
@@ -1,12 +1,14 @@
 import { State } from './initialState'
 
+type UnaryReducer = (state: State) => State
+
 /**
  * Composes a list of reducers in order and merges the results.
  *
  * @param reducers      A list of unary reducers of type `oldState => newState`.
  * @param initialState
  */
-export const reducerFlow = (reducers: Function[]) => (initialState: State) =>
+export const reducerFlow = (reducers: UnaryReducer[]) => (initialState: State) =>
   reducers.reduce((state, reducer) => ({
     ...state,
     ...reducer ? reducer(state) : null,
