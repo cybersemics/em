6f7eb436bbcd1721b6f490c464f8db5c53491f60
Raine Revere <raine@cybersemics.org>
2020-06-03 14:55:16 -0600

    parseJsonSafe defaultValue: any

--- src/util/parseJsonSafe.ts
+++ src/util/parseJsonSafe.ts
@@ -1,7 +1,7 @@
 import { Nullable } from '../utilTypes'
 
 /** Safely JSON parse an unknown value, and default to a given fallback value. */
-export const parseJsonSafe = (value: Nullable<string>, defaultValue: number) => {
+export const parseJsonSafe = (value: Nullable<string>, defaultValue: any) => {
   let parsedValue = defaultValue // eslint-disable-line fp/no-let
   try {
     parsedValue = value && JSON.parse(value)
