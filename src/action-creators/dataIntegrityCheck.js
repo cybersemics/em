9c5bbcec2fe701829e0003449d938e81801f344d
Raine Revere <raine@cybersemics.org>
2020-06-03 18:17:21 -0600

    dataIntegrityCheck: Only enable recreateMissingContextIndex.

--- src/action-creators/dataIntegrityCheck.js
+++ src/action-creators/dataIntegrityCheck.js
@@ -26,6 +26,12 @@ import {
   getThoughtsRanked,
 } from '../selectors'
 
+const deleteDuplicateContextIndex = false
+const recreateMissingContextIndex = true
+const recreateMissingThoughtIndex = false
+const recreateMissingThoughtContexts = false
+const syncDivergentRanks = false
+
 /** Performs a data integrity check and is able to fix minor problems with thoughtIndex and contextIndex being out of sync. */
 const dataIntegrityCheck = path => (dispatch, getState) => {
 
@@ -43,48 +49,54 @@ const dataIntegrityCheck = path => (dispatch, getState) => {
   const pathContext = contextOf(pathToContext(path))
 
   // delete duplicate thoughts in contextIndex
-  const uniqueThoughts = _.uniqBy(contextIndex[encoded], child => child.value + '__SEP' + child.rank)
-  if (contextIndex[encoded] && uniqueThoughts.length < contextIndex[encoded].length) {
-    console.warn('Deleting duplicate thoughts in contextIndex:', value)
-    dispatch({
-      type: 'updateThoughts',
-      contextIndexUpdates: {
-        [encoded]: uniqueThoughts
-      },
-      forceRender: true
-    })
+  if (deleteDuplicateContextIndex) {
+    const uniqueThoughts = _.uniqBy(contextIndex[encoded], child => child.value + '__SEP' + child.rank)
+    if (contextIndex[encoded] && uniqueThoughts.length < contextIndex[encoded].length) {
+      console.warn('Deleting duplicate thoughts in contextIndex:', value)
+      dispatch({
+        type: 'updateThoughts',
+        contextIndexUpdates: {
+          [encoded]: uniqueThoughts
+        },
+        forceRender: true
+      })
 
-    return
+      return
+    }
   }
 
   // recreate thoughts missing in thoughtIndex
-  for (const child of contextIndex[encoded] || []) { // eslint-disable-line fp/no-loops,fp/no-let
-    const childExists = exists(state, child.value)
-    if (!childExists) {
-      console.warn('Recreating missing thought in thoughtIndex:', child.value)
-      dispatch({
-        type: 'newThoughtSubmit',
-        context: pathToContext(path),
-        // guard against undefined
-        rank: child.rank || 0,
-        value: child.value || ''
-      })
-      return
+  if (recreateMissingThoughtIndex) {
+    for (const child of contextIndex[encoded] || []) { // eslint-disable-line fp/no-loops,fp/no-let
+      const childExists = exists(state, child.value)
+      if (!childExists) {
+        console.warn('Recreating missing thought in thoughtIndex:', child.value)
+        dispatch({
+          type: 'newThoughtSubmit',
+          context: pathToContext(path),
+          // guard against undefined
+          rank: child.rank || 0,
+          value: child.value || ''
+        })
+        return
+      }
     }
   }
 
   if (thought && thought.contexts) {
 
     // recreate thoughts missing in thought.contexts
-    const matchingThoughtInContexts = thought.contexts.find(cx => cx.context && equalArrays(unroot(cx.context), pathContext))
-    if (!matchingThoughtInContexts) {
-      console.warn('Recreating missing thought in thought.contexts:', path)
-      dispatch({
-        type: 'newThoughtSubmit',
-        context: pathContext,
-        rank,
-        value
-      })
+    if (recreateMissingThoughtContexts) {
+      const matchingThoughtInContexts = thought.contexts.find(cx => cx.context && equalArrays(unroot(cx.context), pathContext))
+      if (!matchingThoughtInContexts) {
+        console.warn('Recreating missing thought in thought.contexts:', path)
+        dispatch({
+          type: 'newThoughtSubmit',
+          context: pathContext,
+          rank,
+          value
+        })
+      }
     }
 
     // recreate thoughts missing in contextIndex
@@ -92,45 +104,49 @@ const dataIntegrityCheck = path => (dispatch, getState) => {
     // console.log('contextSubthoughts', contextSubthoughts)
     // console.log('contextIndex[encoded]', contextIndex[encoded])
     // console.log('thought.contexts', thought.contexts)
-    const contextIndexUpdates = thought.contexts.reduce((accum, cx) => {
-      // console.log('')
-      // console.log('cx.context', cx.context)
-      const otherContextChildren = getThoughts(state, cx.context)
-      // console.log('otherContextChildren', otherContextChildren)
-      const otherContextHasThought = otherContextChildren
-        .some(child => hashThought(child.value) === hashThought(thought.value) && child.rank === cx.rank)
-      // console.log('otherContextHasThought', otherContextHasThought)
-      const encoded = hashContext(cx.context)
-      const contextIndexUpdatesNew = !otherContextHasThought ? {
-        [encoded]: [
-          ...accum[encoded] || contextIndex[encoded] || [],
-          {
-            // guard against undefined
-            lastUpdated: cx.lastUpdated || timestamp(),
-            rank: cx.rank || 0,
-            value: thought.value || '',
-          }
-        ]
-      } : {}
-      // console.log('contextIndexUpdatesNew', contextIndexUpdatesNew)
-      return {
-        ...accum,
-        ...contextIndexUpdatesNew,
+    if (recreateMissingContextIndex) {
+      const contextIndexUpdates = thought.contexts.reduce((accum, cx) => {
+        // console.log('')
+        // console.log('cx.context', cx.context)
+        const otherContextChildren = getThoughts(state, cx.context)
+        // console.log('otherContextChildren', otherContextChildren)
+        const otherContextHasThought = otherContextChildren
+          .some(child => hashThought(child.value) === hashThought(thought.value) && child.rank === cx.rank)
+        // console.log('otherContextHasThought', otherContextHasThought)
+        const encoded = hashContext(cx.context)
+        const contextIndexUpdatesNew = !otherContextHasThought ? {
+          [encoded]: [
+            ...accum[encoded] || contextIndex[encoded] || [],
+            {
+              // guard against undefined
+              lastUpdated: cx.lastUpdated || timestamp(),
+              rank: cx.rank || 0,
+              value: thought.value || '',
+            }
+          ]
+        } : {}
+        // console.log('contextIndexUpdatesNew', contextIndexUpdatesNew)
+        return {
+          ...accum,
+          ...contextIndexUpdatesNew,
+        }
+      }, {})
+
+      // console.log('contextIndexUpdates', contextIndexUpdates)
+
+      if (Object.keys(contextIndexUpdates).length > 0) {
+        console.warn('Recreating missing thoughts in contextIndex:', contextIndexUpdates)
+        dispatch({
+          type: 'updateThoughts',
+          contextIndexUpdates,
+          forceRender: true
+        })
       }
-    }, {})
-
-    // console.log('contextIndexUpdates', contextIndexUpdates)
-
-    if (Object.keys(contextIndexUpdates).length > 0) {
-      console.warn('Recreating missing thoughts in contextIndex:', contextIndexUpdates)
-      dispatch({
-        type: 'updateThoughts',
-        contextIndexUpdates,
-        forceRender: true
-      })
+      return
     }
+
     // sync divergent ranks
-    else {
+    if (syncDivergentRanks) {
       const contextIndexThoughtsMatchingValue = getThoughtsRanked(state, rootedContextOf(path))
         .filter(equalThoughtValue(value))
 
