b2117765223e780e9e2f4647f7ff23e97c3f7be6
aqkhan <aqk.neo@gmail.com>
2020-06-07 16:30:33 +0500

    Added comparator executor

--- src/util/compareThought.ts
+++ src/util/compareThought.ts
@@ -6,7 +6,7 @@ const regexPunctuation = /^[!@#$%^&*()\-_=+[\]{};:'"<>.,?\\/].*/
 const regexEmojis = /([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?|[\u20E3]|[\u26A0-\u3000]|\uD83E[\udd00-\uddff]|[\u00A0-\u269F]/g
 
 /** Remove emojis and trailing/leading spaces from camparator inputs using regex. */
-const removeEmojisAndSpaces = (str: string) => str.replace(regexEmojis, '').trim()
+const removeEmojisAndSpaces = (str: string) => typeof str === 'string' ? str.replace(regexEmojis, '').trim() : str
 
 /** The default comparator that can be used in sort. */
 export const compare = (a:any, b: any): ComparatorValue => a > b ? 1 : a < b ? -1 : 0
@@ -25,7 +25,7 @@ export const compare = (a:any, b: any): ComparatorValue => a > b ? 1 : a < b ? -
 //       )
 // }
 
-/** A comparator that sorts string starting with emojis ahead of other. */
+/** A comparator that sorts string starting with emojis ahead of others. */
 export const compareStringsWithEmojis = (a: string, b: string) => {
   const aStartsWithEmoji = regexEmojis.test(a)
   const bStartsWithEmoji = regexEmojis.test(b)
@@ -62,7 +62,7 @@ export const compareNumbers = (a: number, b:number): ComparatorValue => {
 }
 
 /** A case-insensitive lexicographic comparator. */
-export const compareLowercase = (a: string, b: string): ComparatorValue => compare(lower(removeEmojisAndSpaces(a)), lower(removeEmojisAndSpaces(b)))
+export const compareLowercase = (a: string, b: string): ComparatorValue => compare(lower(a), lower(b))
 
 /** A comparator function that sorts strings that start with punctuation above others. */
 export const comparePunctuationAndOther = (a: string, b: string): ComparatorValue => {
@@ -75,13 +75,13 @@ export const comparePunctuationAndOther = (a: string, b: string): ComparatorValu
 
 /** A comparison function that sorts date strings. */
 export const compareDateStrings = (a: string, b: string): ComparatorValue => {
-  return compare(Date.parse(removeEmojisAndSpaces(a)), Date.parse(removeEmojisAndSpaces(b)))
+  return compare(Date.parse(a), Date.parse(b))
 }
 
 /** A comparator function that sorts date strings above others. */
 const compareDateAndOther = (a: string, b: string): ComparatorValue => {
-  const aIsDate = !isNaN(Date.parse(removeEmojisAndSpaces(a)))
-  const bIsDate = !isNaN(Date.parse(removeEmojisAndSpaces(b)))
+  const aIsDate = !isNaN(Date.parse(a))
+  const bIsDate = !isNaN(Date.parse(b))
   return aIsDate && !bIsDate ? -1
     : bIsDate && !aIsDate ? 1
     : 0
@@ -94,9 +94,13 @@ export const makeOrderedComparator = (comparators: ComparatorFunction<any>[]): C
       // base case
       ? 0
       // if the values are non equal by the initial comparator, return the result of the comparator
-      : comparators[0](a, b) ||
+      : execComparator(comparators[0], a, b) ||
         // if they are equal, move on to the next comparator
         makeOrderedComparator(comparators.slice(1))(a, b) // RECURSION
+
+/** Executes and comparator with respective string modifications. */
+const execComparator = (comparator: ComparatorFunction<any>, a:any, b: any) =>
+  comparator.name === 'compareStringsWithEmojis' ? comparator(a, b) : comparator(removeEmojisAndSpaces(a), removeEmojisAndSpaces(b))
 
 // eslint-disable-next-line jsdoc/require-description-complete-sentence
 /** A comparator that compares by reasonable, human-readable value:
