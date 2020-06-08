8684378af2bbcba91dd71fbfb34dd4bcf0234cdf
Raine Revere <raine@cybersemics.org>
2020-06-07 20:27:00 -0600

    thoughtCache: Refactor into smaller functions.

--- src/redux-middleware/thoughtCacheMiddleware.ts
+++ src/redux-middleware/thoughtCacheMiddleware.ts
@@ -16,6 +16,81 @@ const throttleFlushPending = 500
 // levels of descendants of each pending contexts to fetch
 const bufferDepth = 2
 
+/** Generates a map of all visible contexts, including the cursor, all its ancestors, and the expanded contexts. */
+const getVisibleContexts = (state: State): GenericObject<Path> => {
+  const { cursor, expanded } = state
+  return {
+    ...expanded,
+    // generate the cursor and all its ancestors
+    // i.e. ['a', b', 'c'], ['a', 'b'], ['a']
+    ...cursor ? cursor.reduce((accum, child, i) => {
+      const path = cursor.slice(0, cursor.length - i)
+      return {
+        ...accum,
+        ...path.length > 0 ? { [hashContext(pathToContext(path))]: path } : null
+      }
+    }, {}) : null,
+  }
+}
+
+/** Gets a map of all visible contexts and their children. */
+const nextPending = (state: State, pending: GenericObject<Path>, visibleContexts: GenericObject<Path>) => {
+
+  const { thoughts: { contextIndex } } = state
+
+  // get the encoded context keys that are not in the contextIndex
+  const expandedKeys = Object.keys(visibleContexts)
+
+  return _.reduce(expandedKeys, (accum, key) => {
+    const context = pathToContext(visibleContexts[key])
+    const children = getThoughtsOfEncodedContext(state, key)
+    return {
+      ...accum,
+
+      // current thought
+      // Typescript cannot see the truthy check for some reason (?)
+      // @ts-ignore
+      ...contextIndex[key] && contextIndex[key].pending ? { [key]: context } : null,
+
+      // because only parents are specified by visibleContexts, we need to queue the children as well
+      ...children.reduce((accumChildren, child) => {
+        const contextChild = unroot([...context, child.value])
+        const keyChild = hashContext(contextChild)
+        return {
+          ...accumChildren,
+          // Typescript cannot see the truthy check for some reason (?)
+          // @ts-ignore
+          ...contextIndex[keyChild] && contextIndex[keyChild].pending ? { [keyChild]: contextChild } : null,
+        }
+      }, {})
+    }
+  }, pending)
+}
+
+/** Fetches pending contexts from the data providers. */
+const fetchPending = async (pending: GenericObject<Path>) => {
+
+  // fetch descendant thoughts for each pending context
+  const thoughtsPending = await Promise.all(Object.keys(pending).map(key =>
+    db.getDescendantThoughts(pathToContext(pending[key]), { maxDepth: bufferDepth })
+  ))
+
+  // aggregate thoughts from all pending descendants
+  const thoughts = thoughtsPending.reduce((accum, result) => ({
+    ...accum,
+    contextIndex: {
+      ...accum.contextIndex,
+      ...result.contextIndex,
+    },
+    thoughtIndex: {
+      ...accum.thoughtIndex,
+      ...result.thoughtIndex,
+    },
+  }), { contextIndex: {}, thoughtIndex: {} })
+
+  return thoughts
+}
+
 /** Middleware that manages the in-memory thought cache (state.thoughts). Marks contexts to be loaded based on cursor and expanded contexts. Queues missing contexts every (debounced) action so that they may be fetched from the data providers and flushes the queue at a throttled interval.
  *
  * There are two main functions that are called after every action, albeit debounced and throttled, respectively:
@@ -37,54 +112,15 @@ const thoughtCacheMiddleware: Middleware = ({ getState, dispatch }) => {
   const updatePendingDebounced = _.debounce(() => {
 
     const state: State = getState()
-    const { cursor, expanded, thoughts: { contextIndex } } = state
 
-    // include expanded, cursor, and all cursor ancestors
-    const visibleContexts: GenericObject<Path> = {
-      ...expanded,
-      // generate the cursor and all its ancestors
-      // i.e. ['a', b', 'c'], ['a', 'b'], ['a']
-      ...cursor ? cursor.reduce((accum, child, i) => {
-        const path = cursor.slice(0, cursor.length - i)
-        return {
-          ...accum,
-          ...path.length > 0 ? { [hashContext(pathToContext(path))]: path } : null
-        }
-      }, {}) : null,
-    }
+    const visibleContexts = getVisibleContexts(state)
 
     // TODO: Currently this will always true since expandThoughts generates a new object each time. updateThoughts should instead only update the reference if the expanded have changed.
     // But not if local db is staged
     if (visibleContexts === lastVisibleContexts) return
 
-    // get the encoded context keys that are not in the contextIndex
-    const expandedKeys = Object.keys(visibleContexts)
-
     // update pending contexts and their children
-    pending = _.reduce(expandedKeys, (accum, key) => {
-      const context = pathToContext(visibleContexts[key])
-      const children = getThoughtsOfEncodedContext(state, key)
-      return {
-        ...accum,
-
-        // current thought
-        // Typescript cannot see the truthy check for some reason (?)
-        // @ts-ignore
-        ...contextIndex[key] && contextIndex[key].pending ? { [key]: context } : null,
-
-        // because only parents are specified by visibleContexts, we need to queue the children as well
-        ...children.reduce((accumChildren, child) => {
-          const contextChild = unroot([...context, child.value])
-          const keyChild = hashContext(contextChild)
-          return {
-            ...accumChildren,
-            // Typescript cannot see the truthy check for some reason (?)
-            // @ts-ignore
-            ...contextIndex[keyChild] && contextIndex[keyChild].pending ? { [keyChild]: contextChild } : null,
-          }
-        }, {})
-      }
-    }, pending)
+    pending = nextPending(state, pending, visibleContexts)
 
     // update last visibleContexts
     lastVisibleContexts = visibleContexts
@@ -99,23 +135,7 @@ const thoughtCacheMiddleware: Middleware = ({ getState, dispatch }) => {
 
     if (Object.keys(pending).length === 0) return
 
-    // fetch descendant thoughts for each pending context
-    const thoughtsPending = await Promise.all(Object.keys(pending).map(key =>
-      db.getDescendantThoughts(pathToContext(pending[key]), { maxDepth: bufferDepth })
-    ))
-
-    // aggregate thoughts from all pending descendants
-    const thoughts = thoughtsPending.reduce((accum, result) => ({
-      ...accum,
-      contextIndex: {
-        ...accum.contextIndex,
-        ...result.contextIndex,
-      },
-      thoughtIndex: {
-        ...accum.thoughtIndex,
-        ...result.thoughtIndex,
-      },
-    }), { contextIndex: {}, thoughtIndex: {} })
+    const thoughts = await fetchPending(pending)
 
     // update thoughts
     dispatch({
