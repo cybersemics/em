3b1f3bcf58a5a66c7591f71299ea6e8e6347f0ef
Raine Revere <raine@cybersemics.org>
2020-06-07 19:55:04 -0600

    thoughtCache: Include cursor ancestors.

--- src/redux-middleware/thoughtCacheMiddleware.ts
+++ src/redux-middleware/thoughtCacheMiddleware.ts
@@ -1,10 +1,11 @@
 import _ from 'lodash'
 import { Middleware } from 'redux'
-import { GenericObject, Nullable } from '../utilTypes'
+import { GenericObject } from '../utilTypes'
 import { Path } from '../types'
 import * as db from '../db'
 import { hashContext, pathToContext, unroot } from '../util'
 import { getThoughtsOfEncodedContext } from '../selectors'
+import { State } from '../util/initialState'
 
 // only check expanded and update pending thoughts every 10 ms rather than every action
 const throttleUpdatePending = 10
@@ -15,56 +16,69 @@ const throttleFlushPending = 500
 /** Middleware that queues missing contexts from state.expanded to be fetched from the local db. */
 const thoughtCacheMiddleware: Middleware = ({ getState, dispatch }) => {
 
-  // track when state.expanded changes
-  let lastExpanded: Nullable<GenericObject<boolean>> = null // eslint-disable-line fp/no-let
+  // track when visible contexts change
+  let lastVisibleContexts: GenericObject<Path> = {} // eslint-disable-line fp/no-let
 
   // store pending cache entries to update
   let pending: GenericObject<Path> = {} // eslint-disable-line fp/no-let
 
   /**
-   * Adds unloaded contexts from state.expanded to the pending queue.
+   * Adds unloaded contexts based on cursor and state.expanded to the pending queue.
    */
   const updatePendingThrottled = _.throttle(() => {
 
-    const state = getState()
+    const state: State = getState()
     const { cursor, expanded, thoughts: { contextIndex } } = state
 
-    const expandedAndCursor = {
+    // include expanded, cursor, and all cursor ancestors
+    const visibleContexts: GenericObject<Path> = {
       ...expanded,
-      ...cursor ? { [hashContext(pathToContext(cursor))]: cursor } : null,
+      // generate the cursor and all its ancestors
+      // i.e. ['a', b', 'c'], ['a', 'b'], ['a']
+      ...cursor ? cursor.reduce((accum, child, i) => {
+        const path = cursor.slice(0, cursor.length - i)
+        return {
+          ...accum,
+          ...path.length > 0 ? { [hashContext(pathToContext(path))]: path } : null
+        }
+      }, {}) : null,
     }
 
     // TODO: Currently this will always true since expandThoughts generates a new object each time. updateThoughts should instead only update the reference if the expanded have changed.
     // But not if local db is staged
-    if (expandedAndCursor === lastExpanded) return
+    if (visibleContexts === lastVisibleContexts) return
 
     // get the encoded context keys that are not in the contextIndex
-    const expandedKeys = Object.keys(expandedAndCursor)
+    const expandedKeys = Object.keys(visibleContexts)
 
-    // update pending contexts and their children from expandedAndCursor
+    // update pending contexts and their children
     pending = _.reduce(expandedKeys, (accum, key) => {
-      const context = pathToContext(expandedAndCursor[key])
+      const context = pathToContext(visibleContexts[key])
       const children = getThoughtsOfEncodedContext(state, key)
       return {
         ...accum,
 
         // current thought
+        // Typescript cannot see the truthy check for some reason (?)
+        // @ts-ignore
         ...contextIndex[key] && contextIndex[key].pending ? { [key]: context } : null,
 
-        // because only parents are specified by expandedAndCursor, we need to queue the children as well
+        // because only parents are specified by visibleContexts, we need to queue the children as well
         ...children.reduce((accumChildren, child) => {
           const contextChild = unroot([...context, child.value])
           const keyChild = hashContext(contextChild)
           return {
             ...accumChildren,
+            // Typescript cannot see the truthy check for some reason (?)
+            // @ts-ignore
             ...contextIndex[keyChild] && contextIndex[keyChild].pending ? { [keyChild]: contextChild } : null,
           }
         }, {})
       }
     }, pending)
 
-    // update last expandedAndCursor
-    lastExpanded = expandedAndCursor
+    // update last visibleContexts
+    lastVisibleContexts = visibleContexts
 
   }, throttleUpdatePending)
 
