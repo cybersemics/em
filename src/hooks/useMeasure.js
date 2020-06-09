6fb313438ff576b86a9649f40d1f13d0dac7d85a
Bijay Shrestha <sbijay777@gmail.com>
2020-06-09 19:09:58 +0545

    fixed lint errors

--- src/hooks/useMeasure.js
+++ src/hooks/useMeasure.js
@@ -1,13 +1,17 @@
 import { useEffect, useRef, useState } from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
 
-/** hook to observe elements size and position */
+/** Hook to observe elements size and position. */
 const useMeasure = () => {
-  const ref = useRef()
+  const ref = useRef(null)
   const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
-  const [ro] = useState(() => new ResizeObserver(([entry]) => set(entry.contentRect)))
+  const [ro] = useState(() => new ResizeObserver(([entry]) => {
+    return set(entry.contentRect)
+  }))
   useEffect(() => {
-    if (ref.current) ro.observe(ref.current)
+    if (ref.current) {
+      ro.observe(ref.current)
+    }
     return () => ro.disconnect()
   }, [])
   return [{ ref }, bounds]
