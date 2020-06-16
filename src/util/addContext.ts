caea783df4649f51a30a314e01f4900f1e2fba03
Raine Revere <raine@cybersemics.org>
2020-06-16 14:06:35 -0600

    addContext: Guard against null created.

--- src/util/addContext.ts
+++ src/util/addContext.ts
@@ -12,7 +12,7 @@ export const addContext = (thought: Lexeme, context: Context, rank: number, id: 
         !(equalArrays(parent.context, context) && parent.rank === rank)
       )
       .concat({ context, rank, id }),
-    created: thought.created,
+    created: thought.created || timestamp(),
     lastUpdated: timestamp()
   })
 })
