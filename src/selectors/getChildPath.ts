7b2ff1f6363b057758345a5101f31635363dab0a
Raine Revere <raine@cybersemics.org>
2020-06-19 16:00:23 -0600

    Add types to selectors: chain, decodeThoughtsUrl, exists, expandThoughts, getChildPath.

--- src/selectors/getChildPath.ts
+++ src/selectors/getChildPath.ts
@@ -1,26 +1,19 @@
-// util
-import {
-  hashThought,
-  head,
-  headValue,
-  unroot,
-} from '../util'
-
-// selectors
-import {
-  getThoughts,
-  rankThoughtsFirstMatch,
-} from '../selectors'
+import { State } from '../util/initialState'
+import { Child, Path, ThoughtContext } from '../types'
+import { getThoughts, rankThoughtsFirstMatch } from '../selectors'
+import { hashThought, head, headValue, unroot } from '../util'
 
 /** Because the current thought only needs to hash match another thought we need to use the exact value of the child from the other context child.context SHOULD always be defined when showContexts is true. */
-export default (state, child, thoughtsRanked, showContexts) => {
+export default (state: State, child: Child | ThoughtContext, thoughtsRanked: Path, showContexts?: boolean) => {
 
-  const otherSubthought = (showContexts && child.context ? getThoughts(state, child.context) : [])
+  // eslint-disable-next-line no-extra-parens
+  const otherSubthought = (showContexts && (child as ThoughtContext).context ? getThoughts(state, (child as ThoughtContext).context) : [])
     .find(child => hashThought(child.value) === hashThought(headValue(thoughtsRanked)))
     || head(thoughtsRanked)
 
   const childPath = showContexts
-    ? rankThoughtsFirstMatch(state, child.context).concat(otherSubthought)
+    ? rankThoughtsFirstMatch(state, (child as ThoughtContext).context).concat(otherSubthought) // eslint-disable-line no-extra-parens
+    // @ts-ignore
     : unroot(thoughtsRanked).concat(child)
 
   return childPath
