840365d8792a0f0cfd2d2368e2a26aa09fdbfa20
Raine Revere <raine@cybersemics.org>
2020-06-20 10:01:36 -0600

    Convert src/selectors/theme to typescript.

--- src/selectors/theme.ts
+++ src/selectors/theme.ts
@@ -1,17 +1,13 @@
-// util
-import {
-  publishMode,
-} from '../util'
-
-// selectors
-import {
-  getSetting,
-} from '../selectors'
+import { publishMode } from '../util'
+import { getSetting } from '../selectors'
+import { State } from '../util/initialState'
 
 const themeLocal = localStorage['Settings/Theme'] || 'Dark'
 
 /** Gets the theme, defaulting to localStorage while loading to avoid re-render. */
-export default state =>
+const theme = (state: State) =>
   publishMode() ? 'Light'
   : state.isLoading ? themeLocal
   : getSetting(state, 'Theme') || 'Dark'
+
+export default theme
