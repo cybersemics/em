09d4380a555f65b78e752237fa82e1fb6d9af69c
Raine Revere <raine@cybersemics.org>
2020-06-20 09:46:18 -0600

    Convert src/selectors/prevSibling to typescript.

--- src/selectors/prevSibling.ts
+++ src/selectors/prevSibling.ts
@@ -1,21 +1,23 @@
 import { getSortPreference, getThoughtsRanked, getThoughtsSorted, hasChild } from '../selectors'
 import { isFunction } from '../util'
+import { State } from '../util/initialState'
+import { Child, Context } from '../types'
 
 /**
  * Gets a context's previous sibling with its rank.
  *
  * @param context   Can be a context or path.
  */
-export default (state, value, context, rank) => {
+const prevSibling = (state: State, value: string, context: Context, rank: number) => {
   const { showHiddenThoughts } = state
   const sortPreference = getSortPreference(state, context)
   const siblings = (sortPreference === 'Alphabetical' ? getThoughtsSorted : getThoughtsRanked)(state, context)
   let prev// eslint-disable-line fp/no-let
 
   /** Returns true when thought is not hidden due to being a function or having a =hidden attribute. */
-  const isVisible = thoughtRanked => showHiddenThoughts || (
-    !isFunction(thoughtRanked.value) &&
-    !hasChild(state, [...context, thoughtRanked.value], '=hidden')
+  const isVisible = (child: Child) => showHiddenThoughts || (
+    !isFunction(child.value) &&
+    !hasChild(state, [...context, child.value], '=hidden')
   )
 
   siblings.find(child => {
@@ -32,3 +34,5 @@ export default (state, value, context, rank) => {
   })
   return prev
 }
+
+export default prevSibling
