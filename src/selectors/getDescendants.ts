eeb1ec667900ff27642e3fafdc056e481c155a36
Raine Revere <raine@cybersemics.org>
2020-06-20 09:13:21 -0600

    Convert src/selectors/getDescendants to typescript.

--- src/selectors/getDescendants.ts
+++ src/selectors/getDescendants.ts
@@ -1,22 +1,17 @@
 import { store } from '../store'
 import _ from 'lodash'
-
-// util
-import {
-  head,
-} from '../util'
-
-// selectors
-import {
-  getDescendants,
-  getThoughtsRanked,
-} from '../selectors'
+import { head } from '../util'
+import { getThoughtsRanked } from '../selectors'
+import { State } from '../util/initialState'
+import { Child, Path } from '../types'
 
 /** Generates a flat list of all descendants. */
-export default (state, thoughtsRanked, recur/* INTERNAL */) => {
+const getDescendants = (state: State, thoughtsRanked: Path, recur?: boolean/* INTERNAL */): Child[] => {
   const children = getThoughtsRanked(store.getState(), thoughtsRanked)
   // only append current thought in recursive calls
   return (recur ? [head(thoughtsRanked)] : []).concat(
     _.flatMap(children, child => getDescendants(state, thoughtsRanked.concat(child), true))
   )
 }
+
+export default getDescendants
