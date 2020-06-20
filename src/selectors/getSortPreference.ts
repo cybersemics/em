65cd4e2c33f4ba80ec8f76fe5c06dab070751d86
Raine Revere <raine@cybersemics.org>
2020-06-20 09:26:20 -0600

    Convert src/selectors/getSortPreference to typescript.

--- src/selectors/getSortPreference.ts
+++ src/selectors/getSortPreference.ts
@@ -1,7 +1,9 @@
 import { getSetting, getThoughts } from '../selectors'
+import { State } from '../util/initialState'
+import { Context } from '../types'
 
 /** Get the sort setting from the given context meta or, if not provided, the global sort. */
-const getSortPreference = (state, context) => {
+const getSortPreference = (state: State, context: Context) => {
   const childrenSort = getThoughts(state, [...context, '=sort'])
   return childrenSort.length > 0
     ? childrenSort[0].value
