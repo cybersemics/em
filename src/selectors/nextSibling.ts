6435e2c178f7438138e0aee7a006f4d6cc5053dd
Raine Revere <raine@cybersemics.org>
2020-06-20 09:43:56 -0600

    Convert src/selectors/nextSibling to typescript.

--- src/selectors/nextSibling.ts
+++ src/selectors/nextSibling.ts
@@ -1,18 +1,22 @@
 import { getSortPreference, getThoughtsRanked, getThoughtsSorted, hasChild } from '../selectors'
 import { equalThoughtRanked, equalThoughtSorted, isFunction, pathToContext } from '../util'
+import { State } from '../util/initialState'
+import { Child, Context } from '../types'
 
 /** Gets thoughts's next sibling with its rank. */
-export default (state, value, context, rank) => {
+const nextSibling = (state: State, value: string, context: Context, rank: number) => {
   const { showHiddenThoughts } = state
   const sortPreference = getSortPreference(state, pathToContext(context))
   const siblings = (sortPreference === 'Alphabetical' ? getThoughtsSorted : getThoughtsRanked)(state, context)
 
   /** Returns true if the child is not hidden due to being a function or having the =hidden attribute. */
-  const notHidden = child => !isFunction(child.value) && !hasChild(state, context.concat(child.value), '=hidden')
+  const notHidden = (child: Child) => !isFunction(child.value) && !hasChild(state, context.concat(child.value), '=hidden')
 
   const siblingsFiltered = showHiddenThoughts ? siblings : siblings.filter(notHidden)
-  const i = siblingsFiltered.findIndex(child => sortPreference === 'Alphabetical' ? equalThoughtSorted(child, { value }) :
+  const i = siblingsFiltered.findIndex(child => sortPreference === 'Alphabetical' ? equalThoughtSorted(child, { value, rank }) :
     equalThoughtRanked(child, { value, rank }))
 
   return siblingsFiltered[i + 1]
 }
+
+export default nextSibling
