040bbe98f90df652f1d3ec3e056801ee7ca69e7c
Raine Revere <raine@cybersemics.org>
2020-06-20 09:17:17 -0600

    Convert src/selectors/getNextRank to typescript.

--- src/selectors/getNextRank.ts
+++ src/selectors/getNextRank.ts
@@ -1,10 +1,9 @@
-// selectors
-import {
-  getThoughtsRanked,
-} from '../selectors'
+import { getThoughtsRanked } from '../selectors'
+import { State } from '../util/initialState'
+import { Context } from '../types'
 
 /** Gets the next rank at the end of a list. */
-export default (state, context) => {
+export default (state: State, context: Context) => {
   const children = getThoughtsRanked(state, context)
   return children.length > 0
     ? children[children.length - 1].rank + 1
