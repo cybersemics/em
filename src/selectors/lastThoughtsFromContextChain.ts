97e681e07f85761a884bb6ee6086ac6f6a106bf1
Raine Revere <raine@cybersemics.org>
2020-06-20 09:41:25 -0600

    Convert src/selectors/lastThoughtsFromContextChain to typescript.

--- src/selectors/lastThoughtsFromContextChain.ts
+++ src/selectors/lastThoughtsFromContextChain.ts
@@ -1,16 +1,18 @@
-// util
-import { contextOf, head, headValue, splice } from '../util'
-
-// selectors
 import { getThought, rankThoughtsFirstMatch } from '../selectors'
+import { contextOf, head, headValue, splice } from '../util'
+import { State } from '../util/initialState'
+import { Child } from '../types'
 
 /** Generates thoughtsRanked from the last segment of a context chain. */
-export default (state, contextChain) => {
+const lastThoughtsFromContextChain = (state: State, contextChain: Child[][]) => {
   if (contextChain.length === 1) return contextChain[0]
   const penult = contextChain[contextChain.length - 2]
   const thought = getThought(state, headValue(penult))
   const ult = contextChain[contextChain.length - 1]
   const parent = thought.contexts.find(parent => head(parent.context) === ult[0].value)
-  const thoughtsRankedPrepend = contextOf(rankThoughtsFirstMatch(state, parent.context))
+  const thoughtsRankedPrepend = contextOf(rankThoughtsFirstMatch(state, parent?.context))
+  // @ts-ignore
   return thoughtsRankedPrepend.concat(splice(ult, 1, 0, head(penult)))
 }
+
+export default lastThoughtsFromContextChain
