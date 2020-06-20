966c19b3315f9d7a3c7299d17f1ad2f344749923
Raine Revere <raine@cybersemics.org>
2020-06-20 15:40:56 -0600

    parseJsonSafe: cleanup

--- src/util/parseJsonSafe.ts
+++ src/util/parseJsonSafe.ts
@@ -2,12 +2,10 @@ import { Nullable } from '../utilTypes'
 
 /** Safely JSON parse an unknown value, and default to a given fallback value. */
 export const parseJsonSafe = (value: Nullable<string>, defaultValue: any) => {
-  let parsedValue = defaultValue // eslint-disable-line fp/no-let
   try {
-    parsedValue = value && JSON.parse(value)
+    return value && JSON.parse(value)
   }
   catch (e) {
-    // console.log(e)
+    return defaultValue
   }
-  return parsedValue
 }
