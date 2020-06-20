f7aad58002ae9143c298ca6678ee71e05c9b4587
Raine Revere <raine@cybersemics.org>
2020-06-20 09:45:33 -0600

    Convert src/selectors/pathToThoughtsRanked to typescript.

--- src/selectors/pathToThoughtsRanked.ts
+++ src/selectors/pathToThoughtsRanked.ts
@@ -1,8 +1,9 @@
-// selectors
 import { lastThoughtsFromContextChain, splitChain } from '../selectors'
+import { State } from '../util/initialState'
+import { Path } from '../types'
 
 /** Infers the thoughtsRanked from a path that may cross one or more context views. */
-const pathToThoughtsRanked = (state, path) => {
+const pathToThoughtsRanked = (state: State, path: Path) => {
   const contextChain = splitChain(state, path)
   return lastThoughtsFromContextChain(state, contextChain)
 }
