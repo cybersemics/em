07b30989534b6914ac7b60ce33ec772285b54ea0
Raine Revere <raine@cybersemics.org>
2020-06-20 09:20:58 -0600

    Convert src/selectors/getPrevRank to typescript.

--- src/selectors/getPrevRank.ts
+++ src/selectors/getPrevRank.ts
@@ -1,11 +1,10 @@
-// selectors
-import {
-  getThoughtsRanked,
-} from '../selectors'
+import { getThoughtsRanked } from '../selectors'
+import { State } from '../util/initialState'
+import { Context } from '../types'
 
 /** Gets a rank that comes before all thoughts in a context. */
 // TODO: Take context not path
-export default (state, context) => {
+export default (state: State, context: Context) => {
   const children = getThoughtsRanked(state, context)
   return children.length > 0
     ? children[0].rank - 1
