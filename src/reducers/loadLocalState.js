e50638e06fe78cfe7153c175f8ca286a1edb2e26
Raine Revere <raine@cybersemics.org>
2020-06-21 13:18:20 -0600

    loadLocalState: Restore cursor from local db.

--- src/reducers/loadLocalState.js
+++ src/reducers/loadLocalState.js
@@ -1,12 +1,27 @@
-import render from './render'
+import { render } from '../reducers'
+import { reducerFlow } from '../util'
 
-/** Merges loaded state. */
-export default (state, { newState }) =>
-  render({
-    ...state,
-    recentlyEdited: {
-      ...state.recentlyEdited,
-      ...newState.recentlyEdited
-    },
-    schemaVersion: newState.schemaVersion,
-  })
+/** Merges recentlyEdited and schemaVersion into state. */
+const loadLocalState = (state, { contextViews, cursor, lastUpdated, recentlyEdited, schemaVersion }) =>
+  reducerFlow([
+
+    // update recentlyEdited and schemaVersion
+    state => ({
+      ...state,
+      contextViews: contextViews || state.contextViews,
+      cursor: cursor || state.cursor,
+      cursorBeforeEdit: cursor || state.cursor,
+      lastUpdated: lastUpdated || state.lastUpdated,
+      recentlyEdited: {
+        ...state.recentlyEdited,
+        ...recentlyEdited
+      },
+      schemaVersion: schemaVersion || state.schemaVersion,
+    }),
+
+    // re-render
+    render,
+
+  ])(state)
+
+export default loadLocalState
