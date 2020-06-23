b204b977b558c7bfdaf856771baf5097a89295ce
Raine Revere <raine@cybersemics.org>
2020-06-23 10:03:01 -0600

    Convert src/reducers/cursorHistory to typescript.

--- src/reducers/cursorHistory.ts
+++ src/reducers/cursorHistory.ts
@@ -1,13 +1,14 @@
-// constants
-import {
-  MAX_CURSOR_HISTORY,
-} from '../constants'
+import { MAX_CURSOR_HISTORY } from '../constants'
+import { State } from '../util/initialState'
+import { Path } from '../types'
 
 /** Updates the cursor history for navigating forward/backward. */
-export default (state, { cursor }) => ({
+const cursorHistory = (state: State, { cursor }: { cursor: Path }) => ({
   ...state,
   cursorHistory: state.cursorHistory
     // shift first entry if history has exceeded its maximum size
     .slice(state.cursorHistory.length >= MAX_CURSOR_HISTORY ? 1 : 0)
     .concat([cursor])
 })
+
+export default cursorHistory
