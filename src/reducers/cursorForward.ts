6f3f76849fa44631fe72132f61e7ec47228e24a7
Raine Revere <raine@cybersemics.org>
2020-06-23 10:01:58 -0600

    Convert src/reducers/cursorForward to typescript.

--- src/reducers/cursorForward.ts
+++ src/reducers/cursorForward.ts
@@ -1,21 +1,24 @@
 import { setCursor } from '../reducers'
 import { getThoughtsRanked, hasChild } from '../selectors'
 import { isFunction, pathToContext } from '../util'
+import { State } from '../util/initialState'
 
 /** Moves the cursor forward in the cursorHistory. */
-const cursorForward = state => {
+const cursorForward = (state: State) => {
 
   // pop from cursor history
   if (state.cursorHistory.length > 0) {
     const cursorNew = state.cursorHistory[state.cursorHistory.length - 1]
+    // @ts-ignore
     return setCursor(state, { thoughtsRanked: cursorNew, cursorHistoryPop: true })
   }
   // otherwise move cursor to first child
   else {
     const cursorOld = state.cursor
     const firstSubthought = cursorOld && getThoughtsRanked(state, cursorOld).find(child => state.showHiddenThoughts || (!isFunction(child.value) && !hasChild(state, [...pathToContext(cursorOld), child.value], '=hidden')))
-    if (firstSubthought) {
+    if (firstSubthought && cursorOld) {
       const cursorNew = cursorOld.concat(firstSubthought)
+      // @ts-ignore
       return setCursor(state, { thoughtsRanked: cursorNew })
     }
     else {
