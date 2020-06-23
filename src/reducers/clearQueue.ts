9f0f98a5fbf0651a33f0173196391e9b1248034d
Raine Revere <raine@cybersemics.org>
2020-06-23 09:51:55 -0600

    Convert src/reducers/clearQueue to typescript.

--- src/reducers/clearQueue.ts
+++ src/reducers/clearQueue.ts
@@ -1,5 +1,7 @@
+import { State } from '../util/initialState'
+
 /** Clears the sync queue. This should only be done after the queued updates are persisted (See redux-middleware/syncQueue and action-creators/flushQueue). */
-export default state => ({
+const clearQueue = (state: State) => ({
   ...state,
   syncQueue: {
     thoughtIndexUpdates: {},
@@ -8,3 +10,5 @@ export default state => ({
     updates: null,
   }
 })
+
+export default clearQueue
