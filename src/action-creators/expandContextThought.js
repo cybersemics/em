62f6bd0de4817c5e2ec2ae4862641f517fcf209c
Raine Revere <raine@cybersemics.org>
2020-06-25 13:24:30 -0600

    Convert src/action-creators/expandContextThought to typescript.

--- src/action-creators/expandContextThought.js
+++
@@ -1,9 +0,0 @@
-/** Expands the inline breadcrumbs of a context in the context view. */
-export default thoughtsRanked => (dispatch, getState) => {
-  if (thoughtsRanked || getState().expandContextThought) {
-    dispatch({
-      type: 'expandContextThought',
-      thoughtsRanked
-    })
-  }
-}
