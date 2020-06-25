ccf37919664911db7e1dae214a6c0cc75fe37921
Raine Revere <raine@cybersemics.org>
2020-06-25 08:25:00 -0600

    Convert src/reducers/tutorialStep to typescript.

--- src/reducers/tutorialStep.js
+++
@@ -1,9 +0,0 @@
-// reducers
-import settings from './settings'
-
-/** Sets the Tutorial Step settings value. */
-export default (state, { value }) =>
-  settings(state, {
-    key: 'Tutorial Step',
-    value
-  })
