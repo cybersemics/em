17d4e531ea42685b42babbb7601b9ce6b86bfff5
Raine Revere <raine@cybersemics.org>
2020-06-25 08:27:41 -0600

    Convert src/reducers/unknownAction to typescript.

--- src/reducers/unknownAction.ts
+++ src/reducers/unknownAction.ts
@@ -1,5 +1,8 @@
+import { Action } from 'redux'
+import { State } from '../util/initialState'
+
 /** Handles an unknown action by printing an error if it is not a @@ Redux action. Returns state as-is. */
-const unknownAction = (state, action) => {
+const unknownAction = (state: State, action: Action) => {
   if (!action.type.startsWith('@@')) {
     console.error('Unrecognized action:', action.type, action)
   }
