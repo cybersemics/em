9362965981022308cdfc6e5dda895bf1030281b2
Raine Revere <raine@cybersemics.org>
2020-06-25 13:09:41 -0600

    Convert src/action-creators/cursorNext to typescript.

--- src/action-creators/cursorNext.ts
+++ src/action-creators/cursorNext.ts
@@ -1,9 +1,10 @@
 import { suppressExpansion } from '../action-creators'
 import { getThoughtAfter } from '../selectors'
 import { contextOf, headValue, isDivider, selectNextEditable } from '../util'
+import { ActionCreator } from '../types'
 
 /** Moves the cursor to the next sibling, ignoring children. */
-export default ({ target }) => (dispatch, getState) => {
+const cursorNext = ({ target }: { target: HTMLElement }): ActionCreator => (dispatch, getState) => {
   const state = getState()
   const { cursor } = state
 
@@ -21,7 +22,10 @@ export default ({ target }) => (dispatch, getState) => {
       }
       else if (isDivider(headValue(nextThoughtsRanked))) {
         dispatch({ type: 'setCursor', thoughtsRanked: nextThoughtsRanked })
-        document.getSelection().removeAllRanges()
+        const sel = document.getSelection()
+        if (sel) {
+          sel.removeAllRanges()
+        }
       }
       else {
         dispatch({ type: 'setCursor', thoughtsRanked: nextThoughtsRanked })
@@ -31,9 +35,11 @@ export default ({ target }) => (dispatch, getState) => {
   }
   // if no cursor, select first editable
   else {
-    const firstEditable = document.querySelector('.editable')
+    const firstEditable = document.querySelector('.editable') as HTMLInputElement | null
     if (firstEditable) {
       firstEditable.focus()
     }
   }
 }
+
+export default cursorNext
