7e2f20f6195417ad6f3fbd853b4dfc0b89687d25
Raine Revere <raine@cybersemics.org>
2020-07-03 15:37:53 -0600

    eslint-plugin-export-default-identifier
    Ensure export default does not export an arrow function or object literal so that proper TypeDocs documentation is generated.

--- src/selectors/getNextRank.ts
+++ src/selectors/getNextRank.ts
@@ -3,9 +3,11 @@ import { State } from '../util/initialState'
 import { Context } from '../types'
 
 /** Gets the next rank at the end of a list. */
-export default (state: State, context: Context) => {
+const getNextRank = (state: State, context: Context) => {
   const children = getThoughtsRanked(state, context)
   return children.length > 0
     ? children[children.length - 1].rank + 1
     : 0
 }
+
+export default getNextRank
