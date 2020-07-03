98560d316bdd35e83879b30d6cb2d3634dad355f
Bijay Shrestha <sbijay777@gmail.com>
2020-07-03 19:22:55 +0545

    Indent thought on adding space at the beginning (#775)
    * indent thought on adding space at the beginning
    
    add tests for the indent feature
    
    * added shortcut for indent on space
    
    added isLastVisibleChild selector
    
    added tests for the shortcut
    
    * changed shortcut space to indentOnSpace
    
    removed last visible child logic
    
    updated test
    
    * fixed lint errors

---
+++ src/components/__tests__/Editable.js
@@ -0,0 +1,73 @@
+import { store } from '../../store'
+import { RANKED_ROOT, ROOT_TOKEN } from '../../constants'
+import { importText } from '../../action-creators'
+import { exportContext } from '../../selectors'
+import { keyDown } from '../../shortcuts'
+import { noop } from 'lodash'
+
+afterEach(async () => {
+  await store.dispatch({ type: 'clear' })
+})
+
+it('indent on adding space at the beginning of the thought', async () => {
+
+  // skip tutorial and close welcome modal
+  await store.dispatch({ type: 'modalComplete', id: 'welcome' })
+  await store.dispatch({ type: 'tutorial', value: false })
+
+  await store.dispatch(importText(RANKED_ROOT, `
+    - a
+      - b
+        - c
+        - d`))
+
+  await store.dispatch({
+    type: 'setCursor',
+    thoughtsRanked: [{ value: 'a', rank: 0 }, { value: 'b', rank: 1 }, { value: 'd', rank: 3 }],
+  })
+
+  keyDown({ preventDefault: noop, key: ' ' })
+
+  const exported = exportContext(store.getState(), [ROOT_TOKEN], 'text/plaintext')
+
+  const expectedOutput = `- ${ROOT_TOKEN}
+  - a
+    - b
+      - c
+        - d`
+
+  expect(exported).toEqual(expectedOutput)
+})
+
+it('prevent indent on adding space at the beginning of the immovable thought', async () => {
+
+  // skip tutorial and close welcome modal
+  await store.dispatch({ type: 'modalComplete', id: 'welcome' })
+  await store.dispatch({ type: 'tutorial', value: false })
+
+  await store.dispatch(importText(RANKED_ROOT, `
+    - a
+      - b
+        - c
+        - d
+          - =immovable`))
+
+  await store.dispatch({
+    type: 'setCursor',
+    thoughtsRanked: [{ value: 'a', rank: 0 }, { value: 'b', rank: 1 }, { value: 'd', rank: 3 }]
+  })
+
+  keyDown({ preventDefault: noop, key: ' ' })
+
+  const exported = exportContext(store.getState(), [ROOT_TOKEN], 'text/plaintext')
+
+  // indent shouldn't happen and output should remain the same
+  const expectedOutput = `- ${ROOT_TOKEN}
+  - a
+    - b
+      - c
+      - d
+        - =immovable`
+
+  expect(exported).toEqual(expectedOutput)
+})
