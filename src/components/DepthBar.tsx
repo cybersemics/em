4d8a2210da9b67edd14c6afec882ff82d6f5bd3a
Raine Revere <raine@cybersemics.org>
2020-07-04 12:46:00 -0600

    Convert src/components/DepthBar to typescript.

--- src/components/DepthBar.tsx
+++ src/components/DepthBar.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import classNames from 'classnames'
 import { store } from '../store'
+import { getContexts } from '../selectors'
+import { contextOf, head } from '../util'
+import { Context } from '../types'
 
 // components
 import Modal from './Modal'
 
-// util
-import {
-  contextOf,
-  head,
-} from '../util'
-
-// selectors
-import { getContexts } from '../selectors'
+interface DepthBarProps {
+  numDescendantCharacters: number,
+  showContexts?: boolean,
+  thoughtsLive: Context,
+}
 
 /** A small horizontal bar that indicates how many descendants a thought has. */
-const DepthBar = ({ numDescendantCharacters, showContexts, thoughtsLive }) => <span>
+const DepthBar = ({ numDescendantCharacters, showContexts, thoughtsLive }: DepthBarProps) => <span>
   {numDescendantCharacters >= 16 ? <Modal id='depthBar' title="The length of this bar indicates the number of thoughts in this context." style={{ top: 30, marginLeft: -16 }} arrow='arrow arrow-up arrow-upleft' opaque>
     <p>This helps you quickly recognize contexts with greater depth as you navigate.</p>
   </Modal> : null}
