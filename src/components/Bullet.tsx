c542f717dbf9780a6e9a49e4478567302a54a17a
Raine Revere <raine@cybersemics.org>
2020-07-04 10:24:42 -0600

    Convert src/components/Bullet to typescript.

--- src/components/Bullet.tsx
+++ src/components/Bullet.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
-
-// util
-import {
-  isContextViewActive,
-} from '../selectors'
+import { isContextViewActive } from '../selectors'
+import { State } from '../util/initialState'
+import { Path } from '../types'
+import { pathToContext } from '../util'
 
 // other bullets
 // •◦◂◄◀︎ ➤▹▸►◥
 
+interface BulletProps {
+  glyph?: string,
+  invalidOption?: boolean,
+  isEditing?: boolean,
+  leaf?: boolean,
+  onClick: any,
+  showContexts?: boolean,
+  thoughtsResolved: Path,
+}
+
 // eslint-disable-next-line jsdoc/require-jsdoc
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (state: State, props: BulletProps) => {
   const { invalidState } = state
   return {
-    showContexts: isContextViewActive(state, props.thoughtsResolved),
+    showContexts: isContextViewActive(state, pathToContext(props.thoughtsResolved)),
     invalidOption: props.isEditing && invalidState // if being edited and meta validation error has occured
   }
 }
 
 /** Connect bullet to contextViews so it can re-render independent from <Subthought>. */
-const Bullet = ({ showContexts, glyph, leaf, onClick, invalidOption }) =>
+const Bullet = ({ showContexts, glyph, leaf, onClick, invalidOption }: BulletProps) =>
   <span className={classNames({
     bullet: true,
     'show-contexts': showContexts,
