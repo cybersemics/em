19764b0d95a4db6dbe4c1919403a301f38006706
Raine Revere <raine@cybersemics.org>
2020-07-04 17:00:29 -0600

    Convert src/components/StaticSuperscript to typescript.

--- src/components/StaticSuperscript.tsx
+++ src/components/StaticSuperscript.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 /** Renders a given number as a superscript. */
-const StaticSuperscript = ({ n }) => {
+const StaticSuperscript = ({ n }: { n: number }) => {
   return <span className='superscript-container'>
     <span className='num-contexts'>
       <sup>{n}</sup>
