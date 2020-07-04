bd5eb2bd8c9a8d0446b163dcd8b6acde547fb807
Raine Revere <raine@cybersemics.org>
2020-07-04 16:09:53 -0600

    Convert src/components/NavBar to typescript.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,33 +2,24 @@ import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
 import { store } from '../store'
-// constants
-import {
-  isDocumentEditable,
-  publishMode,
-} from '../util'
-
-// selectors
-import {
-  getSetting,
-  isTutorial,
-} from '../selectors'
-
-// components
+import { isDocumentEditable, publishMode } from '../util'
+import { getSetting, isTutorial } from '../selectors'
 import { Breadcrumbs } from './Breadcrumbs'
 import HomeLink from './HomeLink'
+import { State } from '../util/initialState'
+import { Path } from '../types'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const mapStateToProps = state => {
+const mapStateToProps = (state: State) => {
   const { cursor } = state
   return {
     cursor,
-    tutorialStep: +getSetting(state, 'Tutorial Step')
+    tutorialStep: +(getSetting(state, 'Tutorial Step') || 1)
   }
 }
 
 /** A navigation bar that contains a link to home and breadcrumbs. */
-const NavBar = ({ cursor, position, tutorialStep }) =>
+const NavBar = ({ cursor, position, tutorialStep }: { cursor: Path | null, position: string, tutorialStep: number }) => // eslint-disable-line @typescript-eslint/no-unused-vars
   <div className={classNames({
     nav: true,
     ['nav-' + position]: true
@@ -39,7 +30,7 @@ const NavBar = ({ cursor, position, tutorialStep }) =>
     })}>
       {!isTutorial(store.getState()) ? <React.Fragment>
         {isDocumentEditable() || (cursor && cursor.length > 2) ? <HomeLink /> : null}
-        <Breadcrumbs path={cursor ? cursor.slice(publishMode() ? 1 : 0, cursor.length - 1) : []} className={{ 'nav-breadcrumbs': true }} />
+        <Breadcrumbs path={cursor ? cursor.slice(publishMode() ? 1 : 0, cursor.length - 1) : []} classNamesObject={{ 'nav-breadcrumbs': true }} />
       </React.Fragment> : null}
     </div>
   </div>
