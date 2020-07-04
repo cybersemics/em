4b72873938b892375c925fbdc7f7349a7b732f54
Raine Revere <raine@cybersemics.org>
2020-07-04 10:15:15 -0600

    Convert src/components/Breadcrumbs to typescript.

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import classNames from 'classnames'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { isMobile } from '../browser'
+import { ancestors, strip } from '../util'
+import { Child, Path } from '../types'
+import { GenericObject } from '../utilTypes'
 
 // components
 import Link from './Link'
 import Superscript from './Superscript'
 
-// util
-import {
-  ancestors,
-  strip,
-} from '../util'
+interface BreadcrumbProps {
+  path: Path,
+  thoughtsLimit?: number,
+  charLimit?: number,
+  classNamesObject?: GenericObject<boolean>,
+}
+
+type OverflowChild = Child & {
+  isOverflow?: boolean,
+  label?: string,
+}
 
 /** Main navigation breadcrumbs.
  *
@@ -19,13 +28,14 @@ import {
  * @param charLimit Max number of character of a thought value that can be shown , if maxed out will be replaced by ellipsis.
  */
 // NOTE: Exporting as default breaks /build (???)
-export const Breadcrumbs = ({ path, thoughtsLimit, charLimit, className }) => {
+export const Breadcrumbs = ({ path, thoughtsLimit, charLimit, classNamesObject }: BreadcrumbProps) => {
   // if thoughtsLimit or charLimit is not passed , the default value of ellipsize will be false and component will have default behaviour
   const [ellipsize, setEllipsize] = React.useState(thoughtsLimit !== undefined && charLimit !== undefined)
 
   // calculate if the overflow occurs during ellipsized view
-  const overflow = path.length > thoughtsLimit ?
-    path.length - thoughtsLimit + 1
+  // 0 if thoughtsLimit is not defined
+  const overflow = path.length > thoughtsLimit! ?
+    path.length - thoughtsLimit! + 1
     : 0
 
   // if charLimit is exceeded then replace the remaining characters by ellipsis
@@ -33,14 +43,15 @@ export const Breadcrumbs = ({ path, thoughtsLimit, charLimit, className }) => {
     ({
       ...thought,
       // subtract 2 so that additional '...' is still within the char limit
-      label: strip(thought.value.length > charLimit - 2 ?
-        thought.value.substr(0, charLimit - 2) + '...'
+      label: strip(thought.value.length > charLimit! - 2 ?
+        thought.value.substr(0, charLimit! - 2) + '...'
         : thought.value)
     }))
     : path
 
   // after character limit is applied we need to remove the overflow thoughts if any and add isOverflow flag to render ellipsis at that position
   const overflowArray = ellipsize && overflow ?
+    // @ts-ignore
     charLimitedArray.slice(0, charLimitedArray.length - 1 - overflow).concat({ isOverflow: true }, charLimitedArray.slice(charLimitedArray.length - 1))
     : charLimitedArray
 
@@ -48,10 +59,10 @@ export const Breadcrumbs = ({ path, thoughtsLimit, charLimit, className }) => {
     <div className={classNames({
       breadcrumbs: true,
       nonempty: overflowArray.length > 0,
-      ...className
+      className: classNamesObject,
     })}>
       <TransitionGroup>
-        {overflowArray.map((thoughtRanked, i) => {
+        {overflowArray.map((thoughtRanked: OverflowChild, i) => {
           const subthoughts = !thoughtRanked.isOverflow ? ancestors(path, thoughtRanked) : null
           return <CSSTransition key={i} timeout={200} classNames='fade'>
             {/* Cannot use React.Fragment with CSSTransition, as it applies the class to the first child */}
