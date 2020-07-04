3581997a36bce98457a2b0ad974771f9c10998f7
Raine Revere <raine@cybersemics.org>
2020-07-04 17:22:12 -0600

    Convert src/components/Superscript to typescript.

--- src/components/Superscript.tsx
+++ src/components/Superscript.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { connect } from 'react-redux'
+import { exists, getContexts } from '../selectors'
+import { contextOf, equalArrays, head, headValue, pathToContext, rootedContextOf } from '../util'
+import { State } from '../util/initialState'
+import { Child, Context, Path } from '../types'
+import { GenericObject } from '../utilTypes'
 
-// util
-import {
-  contextOf,
-  equalArrays,
-  head,
-  headValue,
-  pathToContext,
-  rootedContextOf,
-} from '../util'
-
-// selectors
-import {
-  exists,
-  getContexts,
-} from '../selectors'
+interface SuperscriptProps {
+  contextViews?: GenericObject<boolean>,
+  contextChain?: Child[][],
+  empty?: boolean,
+  numContexts?: number,
+  showContexts?: boolean,
+  showModal?: string | null,
+  showSingle?: boolean,
+  superscript?: boolean,
+  thoughts?: Context,
+  thoughtsRanked: Path,
+  thoughtsRankedLive?: Path,
+  thoughtRaw?: Child,
+}
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (state: State, props: SuperscriptProps) => {
 
   const { contextViews, cursor, cursorBeforeEdit, showHiddenThoughts, showModal } = state
   // track the transcendental identifier if editing
@@ -55,19 +59,19 @@ const mapStateToProps = (state, props) => {
     // thoughtRaw is the head that is removed when showContexts is true
     thoughtRaw: props.showContexts ? head(props.thoughtsRanked) : head(thoughtsRankedLive),
     empty: thoughtsLive.length > 0 ? head(thoughtsLive).length === 0 : true, // ensure re-render when thought becomes empty
-    numContexts: exists(state, head(thoughtsLive)) && numContexts(),
+    numContexts: exists(state, head(thoughtsLive)) ? numContexts() : 0,
     showModal,
   }
 }
 
 /** Renders superscript if there are other contexts. Optionally pass thoughts (used by ContextBreadcrumbs) or thoughtsRanked (used by Subthought). */
-const Superscript = ({ contextViews, contextChain = [], empty, numContexts, showModal, showSingle, superscript = true, thoughts, thoughtsRanked, thoughtsRankedLive, thoughtRaw, dispatch }) => {
+const Superscript: FC<SuperscriptProps> = ({ empty, numContexts, showSingle, superscript = true }) => {
 
   // showContexts = showContexts || isContextViewActive(store.getState(), thoughtsRanked)
   // const numDescendantCharacters = getDescendants(showContexts ? thoughtsRankedLive.concat(thoughtRaw) : thoughtsRankedLive )
   //   .reduce((charCount, child) => charCount + child.length, 0)
 
-  return <span className='superscript-container'>{!empty && superscript && numContexts > (showSingle ? 0 : 1)
+  return <span className='superscript-container'>{!empty && superscript && numContexts! > (showSingle ? 0 : 1)
     ? <span className='num-contexts'> {/* Make the container position:relative so that the modal is positioned correctly */}
       {numContexts ? <sup>{numContexts}</sup> : null}
 
