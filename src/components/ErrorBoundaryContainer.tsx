b2a854e8c63144f2b757c8e7059d9943cc5447a1
Raine Revere <raine@cybersemics.org>
2020-07-04 13:47:56 -0600

    Convert src/components/ErrorBoundaryContainer to typescript.

--- src/components/ErrorBoundaryContainer.tsx
+++ src/components/ErrorBoundaryContainer.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import * as db from '../db'
 
 /** A fallback component for a global error boundary that the main App component is wrapped in. */
-const ErrorFallback = ({ error, componentStack }) => <div style={{ margin: 50 }}>
+const ErrorFallback = ({ error, componentStack }: { error?: Error, componentStack?: any }) => <div style={{ margin: 50 }}>
   <p>Oops, there was an error.</p>
   <div style={{ fontSize: 14 }}>
-    <pre className='error'>{error.message}</pre>
+    {error && <pre className='error'>{error.message}</pre>}
     <pre>{componentStack}</pre>
   </div>
 </div>
 
 /** Logs an error in the local database when it occurs. */
-const onError = (error, componentStack) =>
-  db.log({ message: error.message, componentStack })
+const onError = (error: Error, componentStack?: any) =>
+  db.log({ message: error.message, stack: componentStack })
 
 /** A higher-order component that catches errors of all descendant components. When an error is caught, a fallback component will be rendered. */
-const ErrorBoundaryContainer = ({ children }) =>
+const ErrorBoundaryContainer: FC = ({ children }) =>
   <ErrorBoundary
     FallbackComponent={ErrorFallback}
     onError={onError}
