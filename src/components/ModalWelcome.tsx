88595e323668a0252840776a9b5303f9b7aaeb62
Raine Revere <raine@cybersemics.org>
2020-07-04 15:36:32 -0600

    Convert src/components/ModalWelcome to typescript.

--- src/components/ModalWelcome.tsx
+++ src/components/ModalWelcome.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-
-// components
 import Modal from './Modal'
 
 /** Shrink modal text and logos to fit container vertically. */
-const onRef = el => {
+const onRef = (el: HTMLDivElement) => {
   if (el) {
     const BOTTOM_MARGIN = 20
     const MIN_FONT_SIZE = 10
     const LOGO_SCALE_PX_PER_PERCENTAGE = 0.3
 
-    const contentEl = el.querySelector('.modal-content')
+    const contentEl = el.querySelector('.modal-content') as HTMLElement
 
     if (!contentEl) return
 
-    const logoEls = el.querySelectorAll('.logo')
+    const logoEls = el.querySelectorAll('.logo') as NodeListOf<SVGGraphicsElement & HTMLElement & { width: number }>
     let fontSize = 100 // eslint-disable-line fp/no-let
     let width = logoEls[0] && logoEls[0].width // eslint-disable-line fp/no-let
 
@@ -25,13 +23,13 @@ const onRef = el => {
     }
 
     /** Decreases the font size of the element. */
-    const shrinkFontSize = el => el.style.fontSize = --fontSize + '%' // eslint-disable-line no-return-assign
+    const shrinkFontSize = (el: HTMLElement) => el.style.fontSize = --fontSize + '%' // eslint-disable-line no-return-assign
 
     /** Decreases the width of the element. */
-    const shrinkWidth = el => el.style.width = (width -= LOGO_SCALE_PX_PER_PERCENTAGE) + 'px' // eslint-disable-line no-return-assign
+    const shrinkWidth = (el: HTMLElement) => el.style.width = (width -= LOGO_SCALE_PX_PER_PERCENTAGE) + 'px' // eslint-disable-line no-return-assign
 
     if (fontSize) {
-      while (overflow() >= MIN_FONT_SIZE) { // eslint-disable-line fp/no-loops
+      while (overflow() && fontSize >= MIN_FONT_SIZE) { // eslint-disable-line fp/no-loops, no-unmodified-loop-condition
         shrinkFontSize(contentEl)
         logoEls.forEach(shrinkWidth)
       }
@@ -42,6 +40,7 @@ const onRef = el => {
 /** A modal that welcomes the user to em. */
 const ModalWelcome = () =>
   <div ref={onRef}>
+    {/* @ts-ignore */}
     <Modal id='welcome' title='Welcome to em' className='popup' center>
       <p><b>em</b> is a writing tool that reflects the structure of your thoughts.</p>
     </Modal>
