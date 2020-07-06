70429f008bd60379a72c00c9ff81ae77a13009b9
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-06 18:20:23 +0700

    Add JSDoc for .typedoc-plugin-external-module-name

--- .typedoc-plugin-external-module-name.js
+++ .typedoc-plugin-external-module-name.js
@@ -1,5 +1,7 @@
 /**
- * Custom mapping function for documentation modules.
+ * Custom module name mapping function for typedoc-plugin-external-module-name (https://github.com/christopherthielen/typedoc-plugin-external-module-name)
+ * This function defines all module names and creates module hierarchy.
+ * Created hierarchy are displayed in the right navbar of generated TypeDoc documentation.
  *
  * @param explicit - Value, if the module has an explicit annotation, i.e., @module explicit
  * @param implicit - The plugin's default mapping
