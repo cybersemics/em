36aaddc296c0da81aee98062ca86aee7f4a0f1f0
Bijay Shrestha <sbijay777@gmail.com>
2020-07-09 16:12:33 +0545

    rebased to dev and added noop fn to window.scrollTo for test
    converted Editable test to indentOnSpace shortcut test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,12 @@
 import Adapter from 'enzyme-adapter-react-16'
 import { configure } from 'enzyme'
+import { noop } from 'lodash'
 import 'jest-localstorage-mock'
 import './store'
 
 // TO-D0: Removing store from here causes circular dependency issue in utils. Fix it.
 
 configure({ adapter: new Adapter() })
+
+// add noop function to window.scrollTo to prevent implementation error during test
+window.scrollTo = noop
