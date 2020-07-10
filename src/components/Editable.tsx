60dad5f23bbddfc952295023a345a4eb0c03a6ae
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-10 21:32:35 +0700

    Remove alert dispatch on re-render

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -114,7 +114,6 @@ const Editable = ({ disabled, isEditing, thoughtsRanked, contextChain, cursorOff
   const opacityStyleRef = useRef('1')
 
   const duplicateThoughtsAlertTimeout = useRef<number | undefined>()
-  dispatch({ type: 'alert', value: null })
 
   /** Toggle invalid-option class using contentRef. */
   const setContentInvalidState = (value: boolean) =>
