94cca895ce63f653a8ad411eb7eae70f2896b981
Raine Revere <raine@cybersemics.org>
2020-07-11 08:44:32 -0600

    lint

--- src/shortcuts/newGrandChild.js
+++ src/shortcuts/newGrandChild.js
@@ -1,16 +1,12 @@
-// util
-import {
-  isDocumentEditable,
-} from '../util'
+import { isDocumentEditable } from '../util'
 
-// eslint-disable-next-line jsdoc/require-jsdoc
-const exec = dispatch => dispatch({ type: 'newGrandChild' })
-
-export default {
+const newGrandChild = {
   id: 'newGrandChild',
   name: 'New Grand Child',
   description: 'Create a new grand child in the current thought. Add it to the first visible subthought.',
   gesture: 'rdrd',
   canExecute: () => isDocumentEditable(),
-  exec
+  exec: dispatch => dispatch({ type: 'newGrandChild' })
 }
+
+export default newGrandChild
