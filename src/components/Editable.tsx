62ff3862fe653e0ba16a1cc58407de02d0668244
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-11 14:04:05 +0700

    Reorganize style manipulation

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -107,11 +107,12 @@ const Editable = ({ disabled, isEditing, thoughtsRanked, contextChain, cursorOff
 
   // store ContentEditable ref to update DOM without re-rendering the Editable during editing
   const contentRef = React.useRef<HTMLInputElement>(null)
-  contentRef.current && contentRef.current.setAttribute('style', 'opacity: 1;')
+  if (contentRef.current) {
+    contentRef.current.style.opacity = '1.0'
+  }
 
   // =style attribute on the thought itself
   const styleAttr = getStyle(state, thoughtsRanked)
-  const opacityStyleRef = useRef('1')
 
   const duplicateThoughtsAlertTimeout = useRef<number | undefined>()
 
@@ -278,7 +279,9 @@ const Editable = ({ disabled, isEditing, thoughtsRanked, contextChain, cursorOff
     dispatch(setEditingValue(newValue))
 
     if (newValue === oldValue) {
-      contentRef.current && contentRef.current.setAttribute('style', 'opacity: 1;')
+      if (contentRef.current) {
+        contentRef.current.style.opacity = '1.0'
+      }
       duplicateThoughtsAlertTimeout.current && window.clearTimeout(duplicateThoughtsAlertTimeout.current)
       dispatch({ type: 'alert', value: null })
 
@@ -298,11 +301,15 @@ const Editable = ({ disabled, isEditing, thoughtsRanked, contextChain, cursorOff
     if (duplicates.length > 0) {
       duplicateThoughtsAlertTimeout.current = window.setTimeout(() => dispatch({ type: 'alert', value: 'Duplicate thoughts are not allowed within the same context.' }), 2000)
       throttledChangeRef.current.cancel() // see above
-      contentRef.current && contentRef.current.setAttribute('style', 'opacity: 0.5;')
+      if (contentRef.current) {
+        contentRef.current.style.opacity = '0.5'
+      }
       return
     }
     else {
-      contentRef.current && contentRef.current.setAttribute('style', 'opacity: 1;')
+      if (contentRef.current) {
+        contentRef.current.style.opacity = '1.0'
+      }
       duplicateThoughtsAlertTimeout.current && window.clearTimeout(duplicateThoughtsAlertTimeout.current)
       dispatch({ type: 'alert', value: null })
     }
@@ -511,7 +518,6 @@ const Editable = ({ disabled, isEditing, thoughtsRanked, contextChain, cursorOff
     style={{
       ...style, // style prop
       ...styleAttr, // style attribute
-      opacity: opacityStyleRef.current
     }}
   />
 }
