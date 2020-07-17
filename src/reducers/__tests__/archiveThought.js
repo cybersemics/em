1816f2872aee34a401640f8f301202d8bec6784b
Raine Revere <raine@cybersemics.org>
2020-07-17 15:11:50 -0600

    archiveThought: Fix permanently delete archive.

--- src/reducers/__tests__/archiveThought.js
+++ src/reducers/__tests__/archiveThought.js
@@ -1,12 +1,10 @@
 import { ROOT_TOKEN } from '../../constants'
-import { initialState, reducerFlow } from '../../util'
-import { exportContext } from '../../selectors'
 
-// reducers
-import newThought from '../newThought'
-import archiveThought from '../archiveThought'
-import setCursor from '../setCursor'
-import cursorUp from '../cursorUp'
+// TODO: Why does util have to be imported before selectors and reducers?
+import { initialState, reducerFlow } from '../../util'
+
+import { exportContext, getContexts } from '../../selectors'
+import { archiveThought, cursorUp, newThought, setCursor } from '../../reducers'
 
 it('archive a thought', () => {
 
@@ -134,11 +132,7 @@ it('permanently delete archive', () => {
     state => newThought(state, { value: 'a' }),
     state => newThought(state, { value: 'b' }),
     archiveThought,
-    state => setCursor(state, {
-      thoughtsRanked: [{ value: '=archive', rank: -1 }]
-    }),
-
-    // delete the archive
+    state => setCursor(state, { thoughtsRanked: [{ value: '=archive', rank: -1 }] }),
     archiveThought,
   ]
 
@@ -148,6 +142,37 @@ it('permanently delete archive', () => {
 
   expect(exported).toBe(`- ${ROOT_TOKEN}
   - a`)
+
+  // ensure =archive is removed from thoughtIndex
+  expect(getContexts(stateNew, '=archive'))
+    .toHaveLength(0)
+
+})
+
+it('permanently delete archive with descendants', () => {
+
+  const steps = [
+    state => newThought(state, { value: 'a' }),
+    state => newThought(state, { value: 'b', insertNewSubthought: true }),
+    state => setCursor(state, { thoughtsRanked: [{ value: 'a', rank: 0 }] }),
+    archiveThought,
+    state => setCursor(state, { thoughtsRanked: [{ value: '=archive', rank: -1 }] }),
+    archiveThought,
+  ]
+
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+
+  expect(exported).toBe(`- ${ROOT_TOKEN}`)
+
+  // ensure =archive is removed from thoughtIndex
+  expect(getContexts(stateNew, '=archive'))
+    .toHaveLength(0)
+
+  // ensure descendants are remvoed from thoughtIndex
+  expect(getContexts(stateNew, 'a'))
+    .toHaveLength(0)
 
 })
 
