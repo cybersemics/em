076fe904a0aa36e6a334f7d9a1695f184a4b0db5
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-23 14:38:27 +0700

    add parseHTML

--- src/action-creators/importText.ts
+++ src/action-creators/importText.ts
@@ -3,6 +3,7 @@ import { parse } from 'jex-block-parser'
 import he from 'he'
 import { contextOf, head, importHtml, pathToContext, rootedContextOf, strip } from '../util'
 import { ActionCreator, Path } from '../types'
+import { parseHTML } from '../util/importHTML'
 
 // declare types until jex-block-parser merges PR
 // https://github.com/reergymerej/block-parser/pull/1
@@ -166,6 +167,8 @@ const importText = (thoughtsRanked: Path, inputText: string, { preventSetCursor,
   else {
 
     const { lastThoughtFirstLevel, thoughtIndexUpdates, contextIndexUpdates } = importHtml(state, thoughtsRanked, text, { skipRoot })
+    const json = parseHTML(state, thoughtsRanked, text, { skipRoot })
+    console.log('json: ', json)
     if (!preventSync) {
       dispatch({
         type: 'updateThoughts',
