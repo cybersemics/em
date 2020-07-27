5e26e7239f03225837cd949a075fa3a4932ee98b
Raine Revere <raine@cybersemics.org>
2020-07-27 13:19:25 -0600

    newThought action-creator: Add value argument.

--- src/action-creators/newThought.ts
+++ src/action-creators/newThought.ts
@@ -18,7 +18,7 @@ import {
  * @param offset - Cursor offset.
  * @param preventSplit - Set to true to prevent splitting thought.
  */
-const newThought = ({ offset, preventSplit }: { offset: number, preventSplit?: boolean }): ActionCreator => (dispatch, getState) => {
+const newThought = ({ offset, preventSplit, value = '' }: { offset: number, preventSplit?: boolean, value: string }): ActionCreator => (dispatch, getState) => {
   const state = getState()
   const { cursor } = state
 
@@ -40,7 +40,7 @@ const newThought = ({ offset, preventSplit }: { offset: number, preventSplit?: b
   // split the thought at the selection
   // do not split at the beginning of a line as the common case is to want to create a new thought after, and shift + Enter is so near
   // do not split with gesture, as Enter is avialable and separate in the context of mobile
-  const split = !preventSplit && cursor && isFocusOnEditable && !showContexts && offset > 0 && offset < headValue(cursor).length
+  const split = !preventSplit && cursor && isFocusOnEditable && !showContexts && !value && offset > 0 && offset < headValue(cursor).length
 
   if ((!split || !uneditable) && isMobile && isSafari) {
     asyncFocus()
@@ -50,7 +50,7 @@ const newThought = ({ offset, preventSplit }: { offset: number, preventSplit?: b
     ? uneditable && cursor
       ? { type: 'error', value: `"${ellipsize(headValue(cursor))}" is uneditable and cannot be split.` }
       : { type: 'splitThought', offset }
-    : { type: 'newThought', value: '' }
+    : { type: 'newThought', value }
   )
 }
 
