0b1da9babe300acc599d01c9fa0e17f92510cbfa
Raine Revere <raine@cybersemics.org>
2020-07-27 13:31:49 -0600

    deleteThought: Test caret

--- src/reducers/__tests__/deleteThought.js
+++ src/reducers/__tests__/deleteThought.js
@@ -1,6 +1,10 @@
+import { act } from 'react-dom/test-utils'
 import { ROOT_TOKEN } from '../../constants'
 import { initialState, reducerFlow } from '../../util'
 import { exportContext } from '../../selectors'
+import { store } from '../../store'
+import createTestApp from '../../test-helpers/createTestApp'
+import * as db from '../../db'
 
 // reducers
 import cursorBack from '../cursorBack'
@@ -147,5 +151,37 @@ it('cursor should be removed if the last thought is deleted', () => {
   const stateNew = reducerFlow(steps)(initialState())
 
   expect(stateNew.cursor).toBe(null)
+
+})
+
+/** Mount tests required for caret */
+
+describe('mount', () => {
+
+  beforeEach(async () => {
+    await createTestApp()
+  })
+
+  afterEach(async () => {
+    store.dispatch({ type: 'clear' })
+    await db.clearAll()
+  })
+
+  it('after deleting first child, caret should move to beginning of next sibling', async () => {
+    store.dispatch({ type: 'newThought', value: 'apple' })
+    store.dispatch({ type: 'newThought', value: 'banana' })
+    store.dispatch({ type: 'cursorUp' })
+    store.dispatch({ type: 'deleteThought' })
+    act(jest.runOnlyPendingTimers)
+    expect(window.getSelection()?.focusOffset).toBe(0)
+  })
+
+  it('after deleting last child, caret should move to end of previous sibling', async () => {
+    store.dispatch({ type: 'newThought', value: 'apple' })
+    store.dispatch({ type: 'newThought', value: 'banana' })
+    store.dispatch({ type: 'deleteThought' })
+    act(jest.runOnlyPendingTimers)
+    expect(window.getSelection()?.focusOffset).toBe('apple'.length)
+  })
 
 })
