53a1fc728c7004f89fc7db39b6813e5392164915
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-31 16:34:29 +0700

    cursorDown

--- src/shortcuts/cursorDown.tsx
+++ src/shortcuts/cursorDown.tsx
@@ -1,22 +1,14 @@
-import React from 'react'
-
-// util
-import {
-  contextOf,
-  getElementPaddings,
-  headValue,
-  pathToContext,
-} from '../util'
-
-// selectors
-import {
-  attributeEquals,
-} from '../selectors'
+import React, { Dispatch } from 'react'
+import { Icon as IconType } from '../types'
+import { contextOf, getElementPaddings, headValue, pathToContext } from '../util'
+import { attributeEquals } from '../selectors'
+import { State } from '../util/initialState'
+import { Action } from 'redux'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const Icon = ({ fill = 'black', size = 20, style }) => <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={fill} style={style} viewBox="0 0 19.481 19.481" enableBackground="new 0 0 19.481 19.481">
+const Icon = ({ fill = 'black', size = 20, style }: IconType) => <svg version='1.1' className='icon' xmlns='http://www.w3.org/2000/svg' width={size} height={size} fill={fill} style={style} viewBox='0 0 19.481 19.481' enableBackground='new 0 0 19.481 19.481'>
   <g>
-    <path d="m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z" />
+    <path d='m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z' />
   </g>
 </svg>
 
@@ -26,18 +18,22 @@ const cursorDownShortcut = {
   keyboard: { key: 'ArrowDown' },
   hideFromInstructions: true,
   svg: Icon,
-  canExecute: getState => {
+  canExecute: (getState: () => State) => {
     const state = getState()
     const { cursor } = state
 
     if (cursor) {
+      const selection = window.getSelection()
+      if (!selection) return false
       // default browser behavior in multiline field
-      const { baseNode, focusOffset, rangeCount } = window.getSelection()
+      const { anchorNode: baseNode, focusOffset, rangeCount } = selection
 
       if (rangeCount > 0) {
-        const [{ y: rangeY, height: rangeHeight } = {}] = window.getSelection().getRangeAt(0).getClientRects()
-        const [{ y: baseNodeY, height: baseNodeHeight } = {}] = baseNode.parentElement.getClientRects()
-        const [paddingTop, , paddingBottom] = getElementPaddings(baseNode.parentElement)
+        const [{ y: rangeY, height: rangeHeight } = { y: NaN, height: NaN }] = Array.from(selection.getRangeAt(0).getClientRects())
+        const baseNodeParentEl = baseNode?.parentElement as HTMLElement
+        if (!baseNodeParentEl) return false
+        const [{ y: baseNodeY, height: baseNodeHeight }] = Array.from(baseNodeParentEl.getClientRects())
+        const [paddingTop,, paddingBottom] = getElementPaddings(baseNodeParentEl)
 
         const isNotOnTheLastLine = rangeY + rangeHeight < baseNodeY + baseNodeHeight - paddingTop - paddingBottom
         if (isNotOnTheLastLine) {
@@ -54,7 +50,7 @@ const cursorDownShortcut = {
 
     return true
   },
-  exec: dispatch => dispatch({ type: 'cursorDown' })
+  exec: (dispatch: Dispatch<Action>) => dispatch({ type: 'cursorDown' })
 }
 
 export default cursorDownShortcut
