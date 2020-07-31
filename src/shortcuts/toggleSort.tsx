405b0be779b8b8ab5ea91eeb69e4079332fe1d28
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-07-31 16:46:51 +0700

    toggleSort

--- src/shortcuts/toggleSort.tsx
+++ src/shortcuts/toggleSort.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Dispatch } from 'react'
+import { Context, Icon as IconType, Path } from '../types'
 import { getSetting } from '../selectors'
 import { pathToContext } from '../util'
+import { State } from '../util/initialState'
+import { Nullable } from '../utilTypes'
+
+interface ToggleAttribute {
+  type: 'toggleAttribute',
+  context: Context,
+  key: string,
+  value: string,
+}
+
+interface SetCursor {
+  type: 'setCursor',
+  thoughtsRanked: Nullable<Path>,
+}
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const Icon = ({ size = 20, style }) => <svg version="1.1" className="icon" xmlns="http://www.w3.org/2000/svg" width={size} height={size} style={style} viewBox="0 0 24 24" enableBackground="new 0 0 24 24">
+const Icon = ({ size = 20, style }: IconType) => <svg version='1.1' className='icon' xmlns='http://www.w3.org/2000/svg' width={size} height={size} style={style} viewBox='0 0 24 24' enableBackground='new 0 0 24 24'>
   <g style={{ transform: 'translateY(4px)' }}>
-    <polygon points="5,14.2 5,0 3,0 3,14.2 1.4,12.6 0,14 4,18 8,14 6.6,12.6"/>
-    <rect x="10" y="16" width="11" height="2"/>
-    <rect x="10" y="12" width="9" height="2"/>
-    <rect x="10" y="8" width="7" height="2"/>
-    <rect x="10" y="4" width="5" height="2"/>
-    <rect x="10" y="0" width="3" height="2"/>
+    <polygon points='5,14.2 5,0 3,0 3,14.2 1.4,12.6 0,14 4,18 8,14 6.6,12.6'/>
+    <rect x='10' y='16' width='11' height='2'/>
+    <rect x='10' y='12' width='9' height='2'/>
+    <rect x='10' y='8' width='7' height='2'/>
+    <rect x='10' y='4' width='5' height='2'/>
+    <rect x='10' y='0' width='3' height='2'/>
   </g>
 </svg>
 
@@ -20,7 +35,7 @@ const toggleSortShortcut = {
   description: 'Sort the current context alphabetically.',
   keyboard: { key: 's', alt: true },
   svg: Icon,
-  exec: (dispatch, getState) => {
+  exec: (dispatch: Dispatch<ToggleAttribute | SetCursor>, getState: () => State) => {
     const state = getState()
     const { cursor } = state
     const globalSort = getSetting(state, ['Global Sort'])
