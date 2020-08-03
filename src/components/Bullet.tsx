e33021eb6197f72be81eddc26428ddcd19fcf6f5
Raine Revere <raine@cybersemics.org>
2020-08-03 15:16:01 -0600

    Bullet: only show pending if expanded.

--- src/components/Bullet.tsx
+++ src/components/Bullet.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames'
-import { isContextViewActive, isPending } from '../selectors'
-import { pathToContext } from '../util'
+import { isContextViewActive, isPending, pathToThoughtsRanked } from '../selectors'
+import { hashContext, pathToContext } from '../util'
 import { State } from '../util/initialState'
 import { Path } from '../types'
 
@@ -23,10 +23,12 @@ interface BulletProps {
 // eslint-disable-next-line jsdoc/require-jsdoc
 const mapStateToProps = (state: State, props: BulletProps) => {
   const { invalidState } = state
+  const context = pathToContext(pathToThoughtsRanked(state, props.thoughtsResolved))
   return {
-    pending: isPending(state, pathToContext(props.thoughtsResolved)),
+    // only show as pending if expanded
+    pending: isPending(state, context) && !!state.expanded[hashContext(context)],
     invalidOption: props.isEditing && invalidState, // if being edited and meta validation error has occured
-    showContexts: isContextViewActive(state, pathToContext(props.thoughtsResolved)),
+    showContexts: isContextViewActive(state, context),
   }
 }
 
