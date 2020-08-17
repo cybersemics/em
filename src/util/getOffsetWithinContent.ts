baf128666da9032454f80086383887c337dd8731
Raine Revere <raine@cybersemics.org>
2020-08-17 14:30:50 -0600

    newThought: Fix faulty offset when non-editable is selected.

--- src/util/getOffsetWithinContent.ts
+++ src/util/getOffsetWithinContent.ts
@@ -1,23 +1,18 @@
 /**
- * Find cursor offset relative to the innerHTML of content editable.
+ * Finds the selection offset relative to the innerHTML of an element.
  */
-export const getOffsetWithinContent = (contentEditableDiv: HTMLElement) => {
-  if (!document.getSelection()) return 0
+export const getOffsetWithinContent = (el: HTMLElement) => {
+  const sel = document.getSelection()
+  if (!sel || sel.rangeCount === 0) return 0
 
   // creating a dummy text node with unprintable character
   const target = document.createTextNode('\u0001')
 
-  // at some cases selection doesn't not have any range so getRangeAt(0) fails
-  try {
-    // inserting node at the end of the range
-    document.getSelection()!.getRangeAt(0).insertNode(target)
+  // insert dummy text at the end of the range
+  sel!.getRangeAt(0).insertNode(target)
 
-    // find the index of the dummy text within the inner html
-    const offset = contentEditableDiv.innerHTML.indexOf('\u0001')
-    target.parentNode!.removeChild(target)
-    return offset !== -1 ? offset : 0
-  }
-  catch (err) {
-    return 0
-  }
+  // find the index of the dummy text within the inner html of the contenteditable
+  const offset = el.innerHTML.indexOf('\u0001')
+  target.parentNode!.removeChild(target)
+  return offset !== -1 ? offset : 0
 }
