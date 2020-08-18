1e4644e291bcfb2742527527102387bfd7eb6b68
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-08-18 15:47:33 +0700

    wrap duplicate checking logic in function

--- src/action-creators/newThought.ts
+++ src/action-creators/newThought.ts
@@ -1,4 +1,4 @@
-import { ActionCreator } from '../types'
+import { ActionCreator, Context } from '../types'
 import { isMobile, isSafari } from '../browser'
 import { ROOT_TOKEN, TUTORIAL_STEP_START } from '../constants'
 import { getSetting, getThoughts, hasChild, isContextViewActive } from '../selectors'
@@ -11,6 +11,14 @@ import {
   headValue,
   pathToContext,
 } from '../util'
+import { State } from '../util/initialState'
+
+/** Split editingValue by offset and check if splitted parts are duplicate with siblings. */
+const isDuplicateOnSplit = (offset: number, context: Context | null, state: State) => {
+  const { editingValue } = state
+  const siblings = context && getThoughts(state, context)
+  return siblings && editingValue && siblings.some(sibling => sibling.value === editingValue.substring(0, offset) || sibling.value === editingValue.substring(offset))
+}
 
 /**
  * Creates a new thought.
@@ -20,7 +28,7 @@ import {
  */
 const newThought = ({ offset, preventSplit, value = '' }: { offset: number, preventSplit?: boolean, value: string }): ActionCreator => (dispatch, getState) => {
   const state = getState()
-  const { cursor, editingValue } = state
+  const { cursor } = state
   const tutorial = getSetting(state, 'Tutorial') !== 'Off'
   const tutorialStep = +!getSetting(state, 'Tutorial Step')
 
@@ -39,12 +47,10 @@ const newThought = ({ offset, preventSplit, value = '' }: { offset: number, prev
   const context = cursor && (showContexts && cursor.length > 2 ? pathToContext(contextOf(contextOf(cursor)))
     : !showContexts && cursor.length > 1 ? pathToContext(contextOf(cursor))
     : [ROOT_TOKEN])
-  const siblings = context && getThoughts(state, context)
-  const isDuplicateOnSplit = siblings && editingValue && siblings.some(sibling => sibling.value === editingValue.substring(0, offset) || sibling.value === editingValue.substring(offset))
   // split the thought at the selection
   // do not split at the beginning of a line as the common case is to want to create a new thought after, and shift + Enter is so near
   // do not split with gesture, as Enter is avialable and separate in the context of mobile
-  const split = !preventSplit && cursor && isFocusOnEditable && !showContexts && !value && offset > 0 && !isDuplicateOnSplit
+  const split = !preventSplit && cursor && isFocusOnEditable && !showContexts && !value && offset > 0 && !isDuplicateOnSplit(offset, context, state)
   if ((!split || !uneditable) && isMobile && isSafari) {
     asyncFocus()
   }
