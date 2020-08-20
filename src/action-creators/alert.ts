2cb361ecd4a7ed1e66981d1e7ac8d6cddcdd49c6
Raine Revere <raine@cybersemics.org>
2020-08-20 16:25:13 -0600

    Fix alert action-creator.

--- src/action-creators/alert.ts
+++ src/action-creators/alert.ts
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'react'
-import { store } from '../store'
+import { ActionCreator } from '../types'
 
 interface Options {
   alertType?: string,
@@ -13,11 +13,12 @@ interface Options {
  * @param showCloseLink Show a small 'x' in the upper right corner that allows the user to close the alert. Default: true.
  * @param type An arbitrary alert type that can be added to the alert. This is useful if specific alerts needs to be detected later on, for example, to determine if the alert should be closed, or if it has been superceded by a different alert type.
  */
-const alert = (value: string | FunctionComponent | null, { alertType, showCloseLink }: Options = {}) => {
+const alert = (value: string | FunctionComponent | null, { alertType, showCloseLink }: Options = {}): ActionCreator => (dispatch, getState) => {
 
-  if (store.getState().alert === value) return
+  const { alert } = getState()
+  if (alert && alert.value === value) return
 
-  store.dispatch({
+  dispatch({
     type: 'alert',
     alertType,
     showCloseLink,
