f27b132e9d0db1cb6dddd184e804d77fe4822a64
Raine Revere <raine@cybersemics.org>
2020-08-20 15:51:55 -0600

    Fix rename of cursorNext test. Remove old reducer test.

--- src/action-creators/__tests__/cursorNext.js
+++ src/action-creators/__tests__/cursorNext.js
@@ -10,64 +10,55 @@ import { createTestStore } from '../../test-helpers/createTestStore'
 
 describe('normal view', () => {
 
-  it('move cursor to next sibling', async() => {
+  it('move cursor to next sibling', () => {
 
     const store = createTestStore()
 
-    await store.dispatch(importText(RANKED_ROOT, `
-    - a
-      - a1
-    - b`))
-
-    await store.dispatch({
-      type: 'setCursor',
-      thoughtsRanked: [{ value: 'a', rank: 0 }],
-    })
-
-    store.dispatch(cursorNext())
+    store.dispatch([
+      importText(RANKED_ROOT, `
+      - a
+        - a1
+      - b`),
+      { type: 'setCursor', thoughtsRanked: [{ value: 'a', rank: 0 }] },
+      cursorNext()
+    ])
 
     expect(store.getState().cursor)
-      .toMatchObject([{ value: 'b', rank: 3 }])
+      .toMatchObject([{ value: 'b' }])
 
   })
 
-  it('move to first root child when there is no cursor', async() => {
+  it('move to first root child when there is no cursor', () => {
 
     const store = createTestStore()
 
-    await store.dispatch(importText(RANKED_ROOT, `
-    - a
-    - b`))
-
-    await store.dispatch({
-      type: 'setCursor',
-      thoughtsRanked: null,
-    })
-
-    store.dispatch(cursorNext())
+    store.dispatch([
+      importText(RANKED_ROOT, `
+      - a
+      - b`),
+      { type: 'setCursor', thoughtsRanked: null },
+      cursorNext()
+    ])
 
     expect(store.getState().cursor)
-      .toMatchObject([{ value: 'a', rank: 0 }])
+      .toMatchObject([{ value: 'a' }])
 
   })
 
-  it('do nothing when the cursor on the last sibling', async() => {
+  it('do nothing when the cursor on the last sibling', () => {
 
     const store = createTestStore()
 
-    await store.dispatch(importText(RANKED_ROOT, `
-    - a
-    - b`))
-
-    await store.dispatch({
-      type: 'setCursor',
-      thoughtsRanked: [{ value: 'b', rank: 1 }],
-    })
-
-    store.dispatch(cursorNext())
+    store.dispatch([
+      importText(RANKED_ROOT, `
+      - a
+      - b`),
+      { type: 'setCursor', thoughtsRanked: [{ value: 'b', rank: 1 }] },
+      cursorNext()
+    ])
 
     expect(store.getState().cursor)
-      .toMatchObject([{ value: 'b', rank: 1 }])
+      .toMatchObject([{ value: 'b' }])
 
   })
 
@@ -81,25 +72,25 @@ describe('normal view', () => {
 
   })
 
-  // it('work for sorted thoughts', async() => {
+  // it('work for sorted thoughts', () => {
 
   //   const store = createTestStore()
 
-  //   await store.dispatch(importText(RANKED_ROOT, `
+  //   store.dispatch(importText(RANKED_ROOT, `
   //   - SORT
   //     - a
   //       - a1
   //     - c
   //     - b`))
 
-  //   await store.dispatch({
+  //   store.dispatch({
   //     type: 'toggleAttribute',
   //     context: ['SORT'],
   //     key: '=sort',
   //     value: 'Alphabetical'
   //   })
 
-  //   await store.dispatch({
+  //   store.dispatch({
   //     type: 'setCursor',
   //     thoughtsRanked: [{ value: 'SORT', rank: 0 }, { value: 'a', rank: 1 }],
   //   })
@@ -111,24 +102,21 @@ describe('normal view', () => {
 
   // })
 
-  it('skip descendants', async() => {
+  it('skip descendants', () => {
 
     const store = createTestStore()
 
-    await store.dispatch(importText(RANKED_ROOT, `
-    - a
-      - a1
-    - b`))
-
-    await store.dispatch({
-      type: 'setCursor',
-      thoughtsRanked: [{ value: 'a', rank: 0 }],
-    })
-
-    store.dispatch(cursorNext())
+    store.dispatch([
+      importText(RANKED_ROOT, `
+      - a
+        - a1
+      - b`),
+      { type: 'setCursor', thoughtsRanked: [{ value: 'a', rank: 0 }] },
+      cursorNext()
+    ])
 
     expect(store.getState().cursor)
-      .toMatchObject([{ value: 'b', rank: 3 }])
+      .toMatchObject([{ value: 'b' }])
 
   })
 
