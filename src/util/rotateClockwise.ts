a08b187a512b9b476ced1144f861d754c28ad772
Raine Revere <raine@cybersemics.org>
2020-08-21 11:04:15 -0600

    Remove unused oppositeDirection and rotate Clockwise from util.
    Defined in GestureDiagram.

--- src/util/rotateClockwise.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-
-/** Returns the direction resulting from a 90 degree clockwise rotation. */
-export const rotateClockwise = dir => ({
-  l: 'u',
-  r: 'd',
-  u: 'r',
-  d: 'l'
-}[dir])
