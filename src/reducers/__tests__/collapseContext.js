43bbf580a479fdac0e0fcb80ca56197a22a14e55
aqkhan <aqk.neo@gmail.com>
2020-08-25 21:15:54 +0500

    Updated test suite and reducer for collapseContext

--- src/reducers/__tests__/collapseContext.js
+++ src/reducers/__tests__/collapseContext.js
@@ -6,6 +6,8 @@ import { initialState, reducerFlow } from '../../util'
 // reducers
 import {
   collapseContext,
+  cursorBack,
+  cursorUp,
   newSubthought,
   newThought,
 } from '../../reducers'
@@ -28,123 +30,119 @@ it('do nothing on leaf', () => {
   const stateNew = reducerFlow(steps)(state)
   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
 
-  console.log('exported', exported)
-
   expect(exported).toBe(`- ${ROOT_TOKEN}
   - a
     - b`)
 
-  expect(stateNew.cursor).toMatchObject(state.cursor)
+})
+
+it('collapse context with single child', () => {
+
+  const steps = [
+    newThought('a'),
+    newSubthought('b'),
+    newSubthought('c'),
+    cursorBack,
+    collapseContext,
+  ]
+
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+
+  expect(exported).toBe(`- ${ROOT_TOKEN}
+  - a
+    - =archive
+      - b
+    - c`)
+
+  expect(stateNew.cursor)
+    .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
 
 })
 
-// it('collapse context with single child', () => {
+it('collapse context with multiple children', () => {
 
-//   const steps = [
-//     newThought('a'),
-//     newSubthought('b'),
-//     newSubthought('c'),
-//     cursorBack,
-//     collapseContext,
-//   ]
+  const steps = [
+    newThought('a'),
+    newSubthought('b'),
+    newSubthought('c'),
+    newThought('d'),
+    cursorBack,
+    collapseContext,
+  ]
 
-//   // run steps through reducer flow and export as plaintext for readable test
-//   const stateNew = reducerFlow(steps)(initialState())
-//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
 
-//   expect(exported).toBe(`- ${ROOT_TOKEN}
-//   - a
-//     - =archive
-//       - b
-//     - c`)
+  expect(exported).toBe(`- ${ROOT_TOKEN}
+  - a
+    - =archive
+      - b
+    - c
+    - d`)
 
-//   expect(stateNew.cursor)
-//     .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
+  expect(stateNew.cursor)
+    .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
 
-// })
+})
 
-// it('collapse context with multiple children', () => {
+it('merge children', () => {
 
-//   const steps = [
-//     newThought('a'),
-//     newSubthought('b'),
-//     newSubthought('c'),
-//     newThought('d'),
-//     cursorBack,
-//     collapseContext,
-//   ]
+  const steps = [
+    newThought('a'),
+    newSubthought('b'),
+    newThought('x'),
+    cursorUp,
+    newSubthought('c'),
+    newThought('d'),
+    cursorBack,
+    collapseContext,
+  ]
 
-//   // run steps through reducer flow and export as plaintext for readable test
-//   const stateNew = reducerFlow(steps)(initialState())
-//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
 
-//   expect(exported).toBe(`- ${ROOT_TOKEN}
-//   - a
-//     - =archive
-//       - b
-//     - c
-//     - d`)
+  expect(exported).toBe(`- ${ROOT_TOKEN}
+  - a
+    - =archive
+      - b
+    - c
+    - x
+    - d`)
 
-//   expect(stateNew.cursor)
-//     .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
+  expect(stateNew.cursor)
+    .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
 
-// })
+})
 
-// it('merge children', () => {
+it('merge duplicate children', () => {
 
-//   const steps = [
-//     newThought('a'),
-//     newSubthought('b'),
-//     newThought('x'),
-//     cursorUp,
-//     newSubthought('c'),
-//     newThought('d'),
-//     cursorBack,
-//     collapseContext,
-//   ]
+  const steps = [
+    newThought('a'),
+    newSubthought('b'),
+    newThought('d'),
+    cursorUp,
+    newSubthought('c'),
+    newThought('d'),
+    cursorBack,
+    collapseContext,
+  ]
 
-//   // run steps through reducer flow and export as plaintext for readable test
-//   const stateNew = reducerFlow(steps)(initialState())
-//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
+  // run steps through reducer flow and export as plaintext for readable test
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
 
-//   expect(exported).toBe(`- ${ROOT_TOKEN}
-//   - a
-//     - =archive
-//       - b
-//     - c
-//     - d
-//     - x`)
+  expect(exported).toBe(`- ${ROOT_TOKEN}
+  - a
+    - =archive
+      - b
+    - c
+    - d`)
 
-//   expect(stateNew.cursor)
-//     .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
+  expect(stateNew.cursor)
+    .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
 
-// })
-
-// it('merge duplicate children', () => {
-
-//   const steps = [
-//     newThought('a'),
-//     newSubthought('b'),
-//     newThought('d'),
-//     cursorUp,
-//     newSubthought('c'),
-//     newThought('d'),
-//     cursorBack,
-//     collapseContext,
-//   ]
-
-//   // run steps through reducer flow and export as plaintext for readable test
-//   const stateNew = reducerFlow(steps)(initialState())
-//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plaintext')
-
-//   expect(exported).toBe(`- ${ROOT_TOKEN}
-//   - a
-//     - =archive
-//       - b
-//     - c
-//     - d`)
-
-//   expect(stateNew.cursor)
-//     .toMatchObject([{ value: 'a', rank: 0 }, { value: 'c', rank: 0 }])
-
-// })
+})
