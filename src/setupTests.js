7d969825843afb9aaac5e396d57ed6b40432fe5a
Raine Revere <raine@cybersemics.org>
2020-09-13 11:33:40 -0600

    Move mock IndexedDB to setupTests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,8 +2,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import { configure } from 'enzyme'
 import { noop } from 'lodash'
 import 'jest-localstorage-mock'
-
-// TO-D0: Removing store from here causes circular dependency issue in utils. Fix it.
+import 'fake-indexeddb/auto'
 
 configure({ adapter: new Adapter() })
 
