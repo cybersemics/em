a6ebfe8d000d762385e65917762cc869559b89a2
anmolarora1 <anmolaroraxyz@gmail.com>
2020-09-18 20:05:10 +0530

    updates undo/redo tests to dispatch respective actions instead of shortcuts | minor refactoring

--- src/shortcuts/__tests__/undo.ts
+++ src/shortcuts/__tests__/undo.ts
@@ -1,11 +1,7 @@
 import { importText } from '../../action-creators'
-import { NOOP, RANKED_ROOT, ROOT_TOKEN } from '../../constants'
+import { RANKED_ROOT, ROOT_TOKEN } from '../../constants'
 import { exportContext } from '../../selectors'
 import { createTestStore } from '../../test-helpers/createTestStore'
-import executeShortcut from '../../test-helpers/executeShortcut'
-import undoShortcut from '../undo'
-
-const event = { preventDefault: NOOP } as Event
 
 it('Undo thought change', async () => {
 
@@ -16,20 +12,19 @@ it('Undo thought change', async () => {
   - a
   - b`))
 
-  store.dispatch({ type: 'setCursor', thoughtsRanked: [
-    { value: 'a', rank: '0' },
-    { value: 'b', rank: '1' }
-  ] })
-
-  store.dispatch({ type: 'existingThoughtChange',
+  store.dispatch([{
+    type: 'setCursor', thoughtsRanked: [
+      { value: 'a', rank: '0' }
+    ]
+  }, {
+    type: 'existingThoughtChange',
     newValue: 'aa',
     oldValue: 'a',
     context: [ROOT_TOKEN],
     thoughtsRanked: [{ value: 'a', rank: 0 }]
-  })
-
-  // undo thought change
-  executeShortcut(undoShortcut, { store, type: 'keyboard', event })
+  }, {
+    type: 'undoAction'
+  }])
 
   const exported = exportContext(store.getState(), [ROOT_TOKEN], 'text/plain')
 
