691b9fdf1eaa928b96e42f723bcdb981ed27e21d
anmolarora1 <anmolaroraxyz@gmail.com>
2020-09-18 20:18:00 +0530

    adds tests for grouped actions, correct cursor restoration after undo, and dead action handling

--- src/shortcuts/__tests__/undo.ts
+++ src/shortcuts/__tests__/undo.ts
@@ -34,3 +34,93 @@ it('Undo thought change', async () => {
 
   expect(exported).toEqual(expectedOutput)
 })
+
+it('Group all navigation actions preceding a thought change and undo them together', async () => {
+
+  const store = createTestStore()
+
+  store.dispatch([importText(RANKED_ROOT, `
+  - a
+  - b
+  - c
+  - d`), {
+    type: 'cursorDown'
+  }, {
+    type: 'existingThoughtChange',
+    newValue: 'aa',
+    oldValue: 'a',
+    context: [ROOT_TOKEN],
+    thoughtsRanked: [{ value: 'a', rank: 0 }]
+  }, {
+    type: 'cursorDown'
+  }, {
+    type: 'cursorDown'
+  }, {
+    type: 'setCursor'
+  }, {
+    type: 'cursorBack'
+  }, {
+    type: 'existingThoughtChange',
+    context: [ROOT_TOKEN],
+    oldValue: 'c',
+    newValue: 'cc',
+    thoughtsRanked: [
+      {
+        value: 'c',
+        rank: 2,
+      }
+    ]
+  }])
+
+  // undo thought change and preceding navigation actions
+  store.dispatch({ type: 'undoAction' })
+
+  // restore the cursor to it's state before navigation actions
+  const { cursor } = store.getState()
+
+  expect(cursor).toMatchObject([{ value: 'aa' }])
+
+})
+
+it('Ignore dead actions/Combine dispensible actions with the preceding patch', () => {
+  const store = createTestStore()
+
+  store.dispatch([importText(RANKED_ROOT, `
+  - a
+   - b
+   - c
+   - d`), {
+    type: 'setCursor'
+  }, {
+    type: 'existingThoughtChange',
+    context: [
+      'a'
+    ],
+    oldValue: 'b',
+    newValue: 'bd',
+    rankInContext: 1,
+    thoughtsRanked: [
+      {
+        value: 'b',
+        rank: 1,
+      }
+    ]
+  },
+  // dispensible set cursor (which only updates datanonce)
+  {
+    type: 'setCursor'
+  }])
+
+  // undo setCursor and thoughtChange in a sinle action
+  store.dispatch({ type: 'undoAction' })
+
+  const exported = exportContext(store.getState(), [ROOT_TOKEN], 'text/plain')
+
+  const expectedOutput = `- ${ROOT_TOKEN}
+  - a
+    - b
+    - c
+    - d`
+
+  expect(exported).toEqual(expectedOutput)
+})
