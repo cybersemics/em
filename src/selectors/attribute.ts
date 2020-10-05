d2063a63a167874cdfadf6a8acdebb7fa154b66c
aqkhan <aqk.neo@gmail.com>
2020-10-05 14:54:30 +0500

    Requested updates in review

--- src/selectors/attribute.ts
+++ src/selectors/attribute.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import { Context, Path } from '../types'
-import { isRoot, pathToContext } from '../util'
+import { pathToContext, unroot } from '../util'
 import { getThoughts, isChildVisible } from '../selectors'
 import { State } from '../util/initialState'
 
 /** Returns the value of an attribute of the given context. O(children). Use attributeEquals for O(1) attribute equality check. Use a combination of hasChild and attribute to check if an attribute exists but has no children. */
 const attribute = (state: State, pathOrContext: Path|Context, attributeName: string) => {
   const context = pathToContext(pathOrContext)
-  const children = getThoughts(state, isRoot(context) ? [attributeName] : [...context, attributeName])
+  const children = getThoughts(state, [...unroot(context), attributeName])
   const firstVisibleChild = children.find(isChildVisible(state, context))
   return firstVisibleChild ? firstVisibleChild.value : null
 }
