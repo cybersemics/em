c67cbe885aaeab7eb1b6d5b8f627597ad0d8685a
Raine Revere <raine@cybersemics.org>
2020-10-15 15:40:21 -0600

    Type subtree.

--- src/selectors/subtree.ts
+++ src/selectors/subtree.ts
@@ -1,9 +1,9 @@
 import { getThoughtsRanked } from '../selectors'
 import { State } from '../util/initialState'
-import { Context } from '../types'
+import { Child, Context } from '../types'
 
 /** Gets a nested subtree of all of the given context's descendants. */
-const subtree = (state: State, context: Context): any =>
+const subtree = (state: State, context: Context): Child[] =>
   getThoughtsRanked(state, context).map(child => ({
     ...child,
     subthoughts: subtree(state, context.concat(child.value))
