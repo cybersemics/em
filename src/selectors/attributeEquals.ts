db68136d28b7881d831b669debe42b3c9f9967fe
Raine Revere <raine@cybersemics.org>
2020-10-15 15:00:42 -0600

    Type attributeEquals.

--- src/selectors/attributeEquals.ts
+++ src/selectors/attributeEquals.ts
@@ -1,18 +1,10 @@
 import { Context } from '../types'
-
-// util
-import {
-  equalThoughtValue,
-  unroot,
-} from '../util'
-
-// selectors
-import {
-  getThoughts,
-} from '../selectors'
+import { State } from '../util/initialState'
+import { equalThoughtValue, unroot } from '../util'
+import { getThoughts } from '../selectors'
 
 /** Returns true if the given attribute equals the given value. O(1). Use over attribute when possible for performance. */
-const attributeEquals = (state: any, context: Context, attr: string, value: string) => {
+const attributeEquals = (state: State, context: Context, attr: string, value: string) => {
   const children = getThoughts(state, [...unroot(context), attr])
   return children.find(equalThoughtValue(value))
 }
