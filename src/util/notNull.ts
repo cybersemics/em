e0a5f6316ffa4fd376752c13f4adb6d8cae20872
Raine Revere <raine@cybersemics.org>
2020-10-15 16:58:11 -0600

    Type notNull.

--- src/util/notNull.ts
+++ src/util/notNull.ts
@@ -1,6 +1,9 @@
 import { Index } from '../types'
 
-// @ts-nocheck
-
 /** Returns a shallow copy of an object with all keys that do not have a value of null or undefined. */
-export const notNull = (o: Index) => Object.keys(o).reduce((acc, key) => o[key] !== null ? { ...acc, [key]: o[key] } : acc, {})
+export const notNull = <T>(o: Index<T>) =>
+  Object.keys(o).reduce((acc, key) =>
+    o[key] !== null
+      ? { ...acc, [key]: o[key] }
+      : acc
+  , {} as Index<T>)
