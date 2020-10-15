8557bd545268c4b3c7b9612efa24ff6d4021edd0
Raine Revere <raine@cybersemics.org>
2020-10-15 16:12:49 -0600

    Type head.

--- src/util/head.ts
+++ src/util/head.ts
@@ -1,10 +1,2 @@
-import { Child, Context, Path } from '../types'
-
-type ReturnType = {
-  (param: Context): string,
-  (param: Path): Child,
-  (param: Context | Path): string | Child,
-}
-
 /** Gets the signifying label of the given context. */
-export const head: ReturnType = (thoughts: any) => thoughts[thoughts.length - 1]
+export const head = <T>(list: T[]) => list[list.length - 1]
