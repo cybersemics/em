631eff9b4fe620a11d0174a78a56bb46a183ea61
Raine Revere <raine@cybersemics.org>
2020-10-17 13:57:15 -0600

    attribute: Use SimplePath.

--- src/selectors/attribute.ts
+++ src/selectors/attribute.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import { Context, Path } from '../types'
 import { pathToContext, unroot } from '../util'
 import { getThoughts, isChildVisible } from '../selectors'
 import { State } from '../util/initialState'
+import { Context, SimplePath } from '../types'
 
 /** Returns the value of an attribute of the given context. O(children). Use attributeEquals for O(1) attribute equality check. Use a combination of hasChild and attribute to check if an attribute exists but has no children. */
-const attribute = (state: State, pathOrContext: Path|Context, attributeName: string) => {
+const attribute = (state: State, pathOrContext: SimplePath | Context, attributeName: string) => {
   const context = pathToContext(pathOrContext)
   const children = getThoughts(state, [...unroot(context), attributeName])
   const firstVisibleChild = children.find(isChildVisible(state, context))
