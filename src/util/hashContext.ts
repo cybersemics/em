a1e3ba0ad140903496148387cb703e2f2be8b8f8
Raine Revere <raine@cybersemics.org>
2020-10-17 18:11:26 -0600

    Add ContextHash type.

--- src/util/hashContext.ts
+++ src/util/hashContext.ts
@@ -5,12 +5,12 @@ import { ID } from '../constants'
 // util
 import { escapeSelector } from './escapeSelector'
 import { pathToContext } from './pathToContext'
-import { Context, Path } from '../types'
+import { Context, ContextHash, Path } from '../types'
 
 const SEPARATOR_TOKEN = '__SEP__'
 
 /** Encode the thoughts (and optionally rank) as a string. */
-export const hashContext = (thoughts: Context | Path, rank?: number): string => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
+export const hashContext = (thoughts: Context | Path, rank?: number): ContextHash => (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(pathToContext(thoughts)
   .map(thought => thought ? escapeSelector(thought) : '')
   .join(SEPARATOR_TOKEN)
-  + (typeof rank === 'number' ? SEPARATOR_TOKEN + rank : ''))
+  + (typeof rank === 'number' ? SEPARATOR_TOKEN + rank : '')) as ContextHash
