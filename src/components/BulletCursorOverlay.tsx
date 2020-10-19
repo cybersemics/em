044232cd97b81f48741aedc9399af389b8157978
Raine Revere <raine@cybersemics.org>
2020-10-19 14:20:39 -0600

    draggedThoughtsRanked: Use SimplePath.

--- src/components/BulletCursorOverlay.tsx
+++ src/components/BulletCursorOverlay.tsx
@@ -3,19 +3,19 @@ import { connect } from 'react-redux'
 import classNames from 'classnames'
 import { equalPath } from '../util'
 import { State } from '../util/initialState'
-import { Path } from '../types'
+import { SimplePath } from '../types'
 
 interface BulletCursorOverlayProps {
   isDragging?: boolean,
-  path: Path,
+  simplePath: SimplePath,
 }
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const mapStateToProps = (state: State, props: BulletCursorOverlayProps) => {
-  const { draggedThoughtsRanked, dragHold } = state
-  const { path, isDragging } = props
+  const { draggedSimplePath, dragHold } = state
+  const { simplePath, isDragging } = props
   return {
-    isDragging: isDragging || (dragHold && equalPath(draggedThoughtsRanked!, path))
+    isDragging: isDragging || (dragHold && equalPath(draggedSimplePath!, simplePath))
   }
 }
 
