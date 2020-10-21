7a58e50460510389e962dec2fcbbfaa78afbd1d1
Raine Revere <raine@cybersemics.org>
2020-10-21 14:32:54 -0600

    deleteThought: Type and minor refactor.

--- src/reducers/deleteThought.ts
+++ src/reducers/deleteThought.ts
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import { RANKED_ROOT } from '../constants'
 import { cursorBack, existingThoughtDelete, setCursor } from '../reducers'
 import { State } from '../util/initialState'
 import { Child, Path, SimplePath, ThoughtContext } from '../types'
@@ -22,19 +21,18 @@ import {
   getContexts,
   getContextsSortedAndRanked,
   isContextViewActive,
-  lastThoughtsFromContextChain,
   prevSibling,
   rankThoughtsFirstMatch,
-  splitChain,
+  simplifyPath,
   thoughtsEditingFromChain,
 } from '../selectors'
 
 /** Deletes a thought. */
-const deleteThought = (state: State, { path }: { path?: Path }) => {
+const deleteThought = (state: State, payload: { path?: Path }) => {
 
-  path = path || state.cursor || undefined
+  if (!state.cursor && !payload.path) return state
 
-  if (!path) return state
+  let path = (payload.path || state.cursor)! // eslint-disable-line fp/no-let
 
   // same as in newThought
   const showContexts = isContextViewActive(state, pathToContext(contextOf(path)))
@@ -42,36 +40,31 @@ const deleteThought = (state: State, { path }: { path?: Path }) => {
     // Get thought in ContextView
     const thoughtInContextView = head(contextOf(path))
     // Get context from which we are going to delete thought
-    const context = getContexts(state, thoughtInContextView.value).map(({ context }) => context).find(context => head(context) === headValue(path!))
+    const context = getContexts(state, thoughtInContextView.value).map(({ context }) => context)
+      .find(context => head(context) === headValue(path))
     if (context) {
       // Convert to path
       path = rankThoughtsFirstMatch(state, [...context, thoughtInContextView.value])
     }
   }
-  const contextChain = splitChain(state, path)
-  const thoughtsRanked = contextChain.length > 1
-    ? lastThoughtsFromContextChain(state, contextChain)
-    : path as SimplePath
-  const context = pathToContext(showContexts && contextChain.length > 1 ? contextChain[contextChain.length - 2]
-    : !showContexts && thoughtsRanked.length > 1 ? contextOf(thoughtsRanked) :
-    RANKED_ROOT)
-
-  const { value, rank } = head(thoughtsRanked)
-  const thoughts = pathToContext(thoughtsRanked)
+  const simplePath = simplifyPath(state, path)
+  const thoughts = pathToContext(simplePath)
+  const context = rootedContextOf(thoughts)
+  const { value, rank } = head(simplePath)
 
   /** Calculates the previous context within a context view. */
-  const prevContext = () => {
-    const thoughtsContextView = thoughtsEditingFromChain(state, thoughtsRanked)
-    const prevContext = perma(() => {
+  const prevContext = (): Child | null => {
+    const thoughtsContextView = thoughtsEditingFromChain(state, simplePath)
+    const prevContext: () => ThoughtContext = perma(() => {
       const contexts = getContextsSortedAndRanked(state, headValue(thoughtsContextView))
       const removedContextIndex = contexts.findIndex(context => head(context.context) === value)
       return contexts[removedContextIndex - 1]
     })
     const context = prevContext()
-    return context && {
+    return context ? {
       value: head(context.context),
       rank: prevContext().rank
-    }
+    } : null
   }
 
   // prev must be calculated before dispatching existingThoughtDelete
@@ -80,9 +73,9 @@ const deleteThought = (state: State, { path }: { path?: Path }) => {
     : prevSibling(state, value, context, rank)
 
   /** Sets the cursor or moves it back if it doesn't exist. */
-  const setCursorOrBack = (thoughtsRanked: Path, { offset }: { offset?: number } = {}) => thoughtsRanked
+  const setCursorOrBack = (path: Path | null, { offset }: { offset?: number } = {}) => path
     ? (state: State) => setCursor(state, {
-      thoughtsRanked,
+      thoughtsRanked: path,
       editing: state.editing,
       offset
     })
@@ -92,28 +85,29 @@ const deleteThought = (state: State, { path }: { path?: Path }) => {
 
     // delete thought
     existingThoughtDelete({
-      context: contextOf(pathToContext(thoughtsRanked)),
+      context: contextOf(pathToContext(simplePath)),
       showContexts,
-      thoughtRanked: head(thoughtsRanked)
+      thoughtRanked: head(simplePath)
     }),
 
     // move cursor
     state => {
 
       const next: () => Child | ThoughtContext = perma(() => showContexts
-        ? getContextsSortedAndRanked(state, headValue(contextOf(path as Path)))[0]
+        ? getContextsSortedAndRanked(state, headValue(contextOf(simplePath)))[0]
         : firstVisibleChild(state, context)
       )
 
-      // @ts-ignore
-      return setCursorOrBack(...prev ? [unroot(contextOf(path).concat(prev)), { offset: prev.value.length }] :
+      // Typescript validates with apply but not spread operator here
+      // eslint-disable-next-line prefer-spread
+      return setCursorOrBack.apply(null, prev ? [unroot(contextOf(path).concat(prev) as SimplePath), { offset: prev.value.length }] :
         // Case II: set cursor on next thought
         next() ? [unroot(showContexts
-          ? contextOf(path as Path).concat({ value: head((next() as ThoughtContext).context), rank: next().rank })
-          : contextOf(path as Path).concat(next() as Child)
+          ? contextOf(path).concat({ value: head((next() as ThoughtContext).context), rank: next().rank })
+          : contextOf(path).concat(next() as Child)
         ), { offset: 0 }] :
         // Case III: delete last thought in context; set cursor on context
-        thoughts.length > 1 ? [rootedContextOf(path as Path), { offset: head(context).length }]
+        thoughts.length > 1 ? [rootedContextOf(path), { offset: head(context).length }]
         // Case IV: delete very last thought; remove cursor
         : [null]
       )(state)
