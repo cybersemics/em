3e56cc1ffd22bf26db3314a863fc02eaf5c3dc2e
Raine Revere <raine@cybersemics.org>
2020-10-21 14:42:44 -0600

    Type perma

--- src/util/perma.ts
+++ src/util/perma.ts
@@ -1,5 +1,5 @@
 /** Returns a function that calls the given function once then returns the same result forever. */
-export const perma = <A extends any[], R>(f: (...args: A) => R) => {
-  let result: any = null // eslint-disable-line fp/no-let
-  return (...args: Parameters<typeof f>) => result || (result = f(...args))
+export const perma = <A extends any[], R>(f: (...args: A) => R): typeof f => {
+  let result: R // eslint-disable-line fp/no-let
+  return (...args: A) => result || f(...args)
 }
