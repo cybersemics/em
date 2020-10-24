5c845a757cab4f0a664ed66f77933d4e202f4661
Raine Revere <raine@cybersemics.org>
2020-10-23 18:11:37 -0600

    parseJsonSafe: Improve typing for when no default is specified.

--- src/util/parseJsonSafe.ts
+++ src/util/parseJsonSafe.ts
@@ -1,6 +1,10 @@
+// Most specific overload must come first
+// See: https://www.typescriptlang.org/docs/handbook/functions.html
+export function parseJsonSafe<T>(value: string | null, defaultValue: T): T
+export function parseJsonSafe(value: string | null): unknown
 
 /** Safely JSON parse an unknown value, and default to a given fallback value. */
-export const parseJsonSafe = <T>(value: string | null, defaultValue: T): T => {
+export function parseJsonSafe<T>(value: string | null, defaultValue?: T): any {
   try {
     return value && JSON.parse(value)
   }
