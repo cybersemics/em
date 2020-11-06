aba599150347de122d42d1c70d77a7cfad7d318f
anmola <anmolaroraxyz@gmail.com>
2020-11-06 17:21:42 +0530

    adds validateRoam

---
+++ src/util/validateRoam.ts
@@ -0,0 +1,32 @@
+import { RoamBlock, RoamPage } from 'roam'
+
+/**
+ * Validates the strucutre of RoamBlocks.
+ */
+const isRoamBlock = (children: RoamBlock[] = []): boolean => children.every(({
+  uid,
+  string,
+  children = [],
+  'create-time': createTime,
+  'create-email': createEmail
+}: RoamBlock) =>
+  Array.isArray(children)
+  && isRoamBlock(children)
+  && typeof uid === 'string'
+  && typeof string === 'string'
+  && typeof createTime === 'number'
+  && typeof createEmail === 'string'
+)
+
+/**
+ * Validates if a given string can be parsed as a Roam JSON.
+ */
+export const validateRoam = (input: string) => {
+  try {
+    const json: RoamPage[] = JSON.parse(input)
+    return json.every(({ title, children }) => typeof title === 'string' && Array.isArray(children) && isRoamBlock(children))
+  }
+  catch {
+    return false
+  }
+}
