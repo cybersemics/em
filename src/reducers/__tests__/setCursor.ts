1d6378d82a97d07590568a7d26b483efd2fdff3c
Raine Revere <raine@cybersemics.org>
2020-11-05 17:04:33 -0700

    Create initial setCursor test.

---
+++ src/reducers/__tests__/setCursor.ts
@@ -0,0 +1,57 @@
+import { RANKED_ROOT, ROOT_TOKEN } from '../../constants'
+
+// TODO: Why does util have to be imported before selectors and reducers?
+import { initialState, reducerFlow } from '../../util'
+
+import { importText, setCursor, toggleContextView } from '../../reducers'
+
+it('set the cursor to a SimplePath', () => {
+
+  const text = `
+    - a
+      - b
+        - c`
+
+  const cursor = [{ value: 'a', rank: 0 }, { value: 'b', rank: 0 }, { value: 'c', rank: 0 }]
+
+  const steps = [
+    importText({ path: RANKED_ROOT, text }),
+    setCursor({ path: cursor }),
+    toggleContextView,
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+
+  expect(stateNew.cursor).toMatchObject(cursor)
+
+})
+
+it('set the cursor to a Path across a context view', () => {
+
+  const text = `
+    - a
+      - m
+        - x
+    - b
+      - m
+        - y
+  `
+
+  const cursor = [
+    { value: 'a', rank: 0 },
+    { value: 'm', rank: 0 },
+    { value: 'b', rank: 1 },
+    { value: 'y', rank: 0 },
+  ]
+
+  const steps = [
+    importText({ path: RANKED_ROOT, text }),
+    setCursor({ path: cursor }),
+    toggleContextView,
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+
+  expect(stateNew.cursor).toMatchObject(cursor)
+
+})
