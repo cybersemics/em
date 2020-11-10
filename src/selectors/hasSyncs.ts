213a864271c01910c096553563c0ade801b00690
Raine Revere <raine@cybersemics.org>
2020-11-09 18:32:39 -0700

    Rename isSyncing to isPushing.

--- src/selectors/hasSyncs.ts
+++ src/selectors/hasSyncs.ts
@@ -2,7 +2,7 @@ import { State } from '../util/initialState'
 
 /** A selector that returns true if there are any local or remote pushes queued or in-progress. Does not include state-only updates, i.e. from pull. */
 const hasSyncs = (state: State) =>
-  state.isSyncing ||
+  state.isPushing ||
   state.pushQueue.some(batch => batch.local || batch.remote)
 
 export default hasSyncs
