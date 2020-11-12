c1b32581f28c7bcce615e2137d8c3277b4f5a632
Raine Revere <raine@cybersemics.org>
2020-11-12 09:21:11 -0700

    isContextViewActive: Change input type from Context to string[].

--- src/selectors/isContextViewActive.ts
+++ src/selectors/isContextViewActive.ts
@@ -1,19 +1,18 @@
 import { hashContext } from '../util'
 import { State } from '../util/initialState'
-import { Context } from '../types'
 
 /** Return true if the context view is active for the given key, including selected subthoughts. */
-const isContextViewActive = (state: State, context: Context/* may have cycles */) => {
+const isContextViewActive = (state: State, unrankedPath: string[]) => {
 
-  if (context.length === 0) return false
+  if (unrankedPath.length === 0) return false
 
-  return !!state.contextViews[hashContext(context)]
+  return !!state.contextViews[hashContext(unrankedPath)]
 
   // disable intrathought linking until add, edit, delete, and expansion can be implemented
   // TODO: Figure out why this causes unwanted re-rendering during editing
   // const { contextViews } = state
-  // const subthought = perma(() => getSubthoughtUnderSelection(head(context), 3, { state }))
-  // return contextViews[hashContext(context)] || (subthought() && contextViews[hashContext(parentOf(context).concat(subthought()))])
+  // const subthought = perma(() => getSubthoughtUnderSelection(head(unrankedPath), 3, { state }))
+  // return contextViews[hashContext(unrankedPath)] || (subthought() && contextViews[hashContext(parentOf(unrankedPath).concat(subthought()))])
 }
 
 export default isContextViewActive
