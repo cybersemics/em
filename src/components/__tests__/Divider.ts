46ff4aff29906f235b81e9e7a7b577dc2c52018d
Raine Revere <raine@cybersemics.org>
2020-11-15 10:32:31 -0700

    Do not convert "—" to divider. Fixes #835.

---
+++ src/components/__tests__/Divider.ts
@@ -0,0 +1,105 @@
+import { store } from '../../store'
+import { ROOT_TOKEN } from '../../constants'
+import { getChildrenRanked } from '../../selectors'
+import windowEvent from '../../test-helpers/windowEvent'
+import createTestApp, { cleanupTestApp } from '../../test-helpers/createTestApp'
+import { Await } from '../../types'
+
+let wrapper: Await<ReturnType<typeof createTestApp>> // eslint-disable-line fp/no-let
+
+beforeEach(async () => {
+  wrapper = await createTestApp()
+})
+
+afterEach(async () => {
+  await cleanupTestApp()
+  wrapper = null as any
+})
+
+it('convert "---" to divider', async () => {
+
+  // create thought
+  windowEvent('keydown', { key: 'Enter' })
+  wrapper.update()
+  const editable = wrapper.find('div.editable')
+  await editable.simulate('change', { target: { value: '---' } })
+
+  // cursor back to trigger existingThoughtChange
+  windowEvent('keydown', { key: 'Escape' })
+
+  jest.runOnlyPendingTimers()
+  wrapper.update()
+
+  const divider = wrapper.find('Divider')
+  expect(divider).toHaveLength(1)
+
+})
+
+it('convert "–-" (emdash + dash) to divider', async () => {
+
+  // create thought
+  windowEvent('keydown', { key: 'Enter' })
+  wrapper.update()
+  const editable = wrapper.find('div.editable')
+  await editable.simulate('change', { target: { value: '—-' } })
+
+  // cursor back to trigger existingThoughtChange
+  windowEvent('keydown', { key: 'Escape' })
+
+  jest.runOnlyPendingTimers()
+  wrapper.update()
+
+  const divider = wrapper.find('Divider')
+  expect(divider).toHaveLength(1)
+
+})
+
+it('do not convert "-" to divider', async () => {
+
+  // create thought
+  windowEvent('keydown', { key: 'Enter' })
+  wrapper.update()
+  const editable = wrapper.find('div.editable')
+  await editable.simulate('change', { target: { value: '-' } })
+
+  // cursor back to trigger existingThoughtChange
+  windowEvent('keydown', { key: 'Escape' })
+
+  jest.runOnlyPendingTimers()
+
+  // state
+  const rootSubthoughts = getChildrenRanked(store.getState(), [ROOT_TOKEN])
+  expect(rootSubthoughts).toHaveLength(1)
+  expect(rootSubthoughts[0]).toMatchObject({ value: '-', rank: 0 })
+
+  // DOM
+  wrapper.update()
+  const aEditable = wrapper.find('div.editable')
+  expect(aEditable.at(0).text()).toBe('-')
+
+})
+
+it('do not convert "—" (emdash) to divider', async () => {
+
+  // create thought
+  windowEvent('keydown', { key: 'Enter' })
+  wrapper.update()
+  const editable = wrapper.find('div.editable')
+  await editable.simulate('change', { target: { value: '—' } })
+
+  // cursor back to trigger existingThoughtChange
+  windowEvent('keydown', { key: 'Escape' })
+
+  jest.runOnlyPendingTimers()
+
+  // state
+  const rootSubthoughts = getChildrenRanked(store.getState(), [ROOT_TOKEN])
+  expect(rootSubthoughts).toHaveLength(1)
+  expect(rootSubthoughts[0]).toMatchObject({ value: '—', rank: 0 })
+
+  // DOM
+  wrapper.update()
+  const aEditable = wrapper.find('div.editable')
+  expect(aEditable.at(0).text()).toBe('—')
+
+})
