1d8407da33b7a019e1c6ff4cb6499dcbdce9914b
Raine Revere <raine@cybersemics.org>
2020-11-17 07:28:24 -0700

    getUserRef: Return null if not logged in.

--- src/util/getUserRef.ts
+++ src/util/getUserRef.ts
@@ -4,4 +4,5 @@ import { Ref } from '../types'
 /**
  * Get the user ref from an authenticated user's details stored in the state.
  */
-export const getUserRef = (state: State): Ref | null => window.firebase?.database().ref('users/' + state.user!.uid)
+export const getUserRef = (state: State): Ref | null =>
+ state.user?.uid ? window.firebase?.database().ref('users/' + state.user.uid) : null
