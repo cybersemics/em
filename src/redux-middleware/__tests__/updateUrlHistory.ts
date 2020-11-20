444fd0b577586a9df3dc8852912f837ac500a098
Raine Revere <raine@cybersemics.org>
2020-11-20 16:46:46 -0700

    Add basic updateUrlHistory tests.

---
+++ src/redux-middleware/__tests__/updateUrlHistory.ts
@@ -0,0 +1,46 @@
+import { store } from '../../store'
+import createTestApp, { cleanupTestApp } from '../../test-helpers/createTestApp'
+import { Index } from '../../types'
+
+// mock debounce and throttle so debounced updateUrlHistory will trigger
+// fake timers cause an infinite loop on _.debounce
+// Jest v26 contains a 'modern' option for useFakeTimers (https://github.com/facebook/jest/pull/7776), but I am getting a "TypeError: Cannot read property 'useFakeTimers' of undefined" error when I call jest.useFakeTimers('modern'). The same error does not occor when I use 'legacy' or omit the argument (react-scripts v4.0.0-next.64).
+// https://github.com/facebook/jest/issues/3465#issuecomment-504908570
+jest.mock('lodash', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { debounce, throttle } = require('../../test-helpers/mock-debounce-throttle')
+  return {
+    ...jest.requireActual('lodash') as Index<any>,
+    debounce,
+    throttle,
+  }
+})
+
+beforeEach(createTestApp)
+afterEach(cleanupTestApp)
+
+it('set url to cursor', () => {
+
+  store.dispatch({ type: 'newThought', value: 'a' })
+  expect(window.location.pathname).toBe('/~/a')
+
+  store.dispatch({ type: 'newThought', value: 'b', insertNewSubthought: true })
+  expect(window.location.pathname).toBe('/~/a/b')
+
+  store.dispatch({ type: 'cursorBack' })
+  expect(window.location.pathname).toBe('/~/a')
+
+  store.dispatch({ type: 'cursorBack' })
+  expect(window.location.pathname).toBe('/')
+
+})
+
+it('set url to home after deleting last empty thought', () => {
+
+  store.dispatch({ type: 'newThought' })
+  expect(window.location.pathname).toBe('/~/')
+
+  store.dispatch({ type: 'deleteThought' })
+  expect(window.location.pathname).toBe('/')
+
+})
