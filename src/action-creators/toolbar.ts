85cdf52bf821584e8e1bbdffe828e909b83206ec
Raine Revere <raine@cybersemics.org>
2020-11-23 13:41:03 -0700

    Convert toolbar to Typescript.

--- src/action-creators/toolbar.ts
+++ src/action-creators/toolbar.ts
@@ -1,19 +1,21 @@
+import { ActionCreator } from '../types'
+
 /** Reveals the toolbar overlay. */
-export const overlayReveal = id => (dispatch, getState) => {
+export const overlayReveal = (id: string): ActionCreator => (dispatch, getState) => {
   if (getState().toolbarOverlay !== id) {
     dispatch({ type: 'setToolbarOverlay', id })
   }
 }
 
 /** Enables or disables scrolling for the toolbar overlay. */
-export const scrollPrioritize = val => (dispatch, getState) => {
+export const scrollPrioritize = (val: boolean): ActionCreator => (dispatch, getState) => {
   if (getState().scrollPrioritized !== val) {
     dispatch({ type: 'prioritizeScroll', val })
   }
 }
 
 /** Hides the toolbar overlay. */
-export const overlayHide = () => (dispatch, getState) => {
+export const overlayHide = (): ActionCreator => (dispatch, getState) => {
   if (getState().toolbarOverlay) {
     dispatch({ type: 'setToolbarOverlay', id: null })
   }
