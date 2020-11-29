1d574564af5f4eb40b95eaa89f0c3a9b92ef4d9b
Raine Revere <raine@cybersemics.org>
2020-11-29 15:31:00 -0700

    perma: Use symbol to ensure falsey results are not re-calculated

--- src/util/perma.ts
+++ src/util/perma.ts
@@ -1,5 +1,10 @@
 /** Returns a function that calls the given function once then returns the same result forever. */
-export const perma = <A extends any[], R>(f: (...args: A) => R): typeof f => {
-  let result: R // eslint-disable-line fp/no-let
-  return (...args: A) => result || f(...args)
+export const perma = <A, R>(f: (...args: A[]) => R): typeof f => {
+  const symbol = Symbol('perma-result')
+  let result: R | symbol = symbol // eslint-disable-line fp/no-let
+  return (...args: A[]) => {
+    if (result !== symbol) return result as R
+    result = f(...args)
+    return result
+  }
 }
