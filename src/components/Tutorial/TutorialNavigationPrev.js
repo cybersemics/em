c7d8a39a7a0f274efe318ac063c08a7660511798
Raine Revere <raine@cybersemics.org>
2020-11-29 19:17:05 -0700

    TutorialNavigatioPrev: Replace plaintext actions with action creators for type safety.

--- src/components/Tutorial/TutorialNavigationPrev.js
+++ src/components/Tutorial/TutorialNavigationPrev.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import TutorialNavigationButton from './TutorialNavigationButton'
 import { store } from '../../store'
-
-import {
-  TUTORIAL_STEP_START,
-} from '../../constants'
+import { TUTORIAL_STEP_START } from '../../constants'
+import { tutorialPrev } from '../../action-creators'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const TutorialNavigationPrev = ({ tutorialStep }) =>
   <TutorialNavigationButton
     classes='tutorial-prev'
     disabled={tutorialStep === TUTORIAL_STEP_START}
-    clickHandler={() => store.dispatch({ type: 'tutorialPrev' })}
+    clickHandler={() => store.dispatch(tutorialPrev())}
     value='Prev' />
 
 export default TutorialNavigationPrev
