2a33628308ab62541246e72ede77e07391345de1
anmola <anmolaroraxyz@gmail.com>
2020-12-12 15:49:03 +0530

    minor refactoring - removes superfluous code

--- src/redux-enhancers/cursorChanged.ts
+++ src/redux-enhancers/cursorChanged.ts
@@ -1,44 +1,21 @@
 import { Action, Store, StoreEnhancer, StoreEnhancerStoreCreator } from 'redux'
-import { RANKED_ROOT } from '../constants'
-import { chain, simplifyPath } from '../selectors'
-import { Index, Path, SimplePath } from '../types'
 import { clearSelection, equalPath, headValue, isDivider } from '../util'
 import { State } from '../util/initialState'
 
-interface SetCursorAction extends Action<any> {
-    contextChain?: SimplePath[],
-    cursorHistoryClear?: boolean,
-    cursorHistoryPop?: boolean,
-    editing?: boolean | null,
-    noteFocus?: boolean,
-    offset?: number | null,
-    replaceContextViews?: Index<boolean>,
-    path: Path | null,
-}
-
 /**
- * Store enhancer to append the ability to undo/redo for all undoable actions.
+ * Store enhancer to detect cursor change and trigger appropriate actions (clear selection for now).
  */
 const cursorChangedEnhancer: StoreEnhancer<any> = (createStore: StoreEnhancerStoreCreator) => <A extends Action<any>>(reducer: (state: any, action: A) => any, initialState: any): Store<State, A> => {
 
   /**
    * Clear cursor selection if on divider.
    */
-  const cursorChangedReducer = (state: State | undefined = initialState, action: A & SetCursorAction): State => {
+  const cursorChangedReducer = (state: State | undefined = initialState, action: A): State => {
     if (!state) return reducer(initialState, action)
 
     const updatedState = reducer(state, action)
-    if (!equalPath(state.cursor, updatedState.cursor)) {
-      const { path, contextChain } = action
-      const simplePath = path ? simplifyPath(state, path) : RANKED_ROOT
-      const thoughtsResolved = path && contextChain && contextChain.length > 0
-        ? chain(state, contextChain, simplePath!)
-        : path || updatedState.cursor
-
-      // clear browser selection if the cursor is on a divider
-      if (thoughtsResolved && isDivider(headValue(thoughtsResolved))) {
-        clearSelection()
-      }
+    if (updatedState.cursor && !equalPath(state.cursor, updatedState.cursor) && isDivider(headValue(updatedState.cursor))) {
+      clearSelection()
     }
     return updatedState
 
