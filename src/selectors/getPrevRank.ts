041492f7118662c07eae629b9fec42a7fa2d8d4e
Raine Revere <raine@cybersemics.org>
2020-12-27 19:42:30 -0700

    getPrevRank: Default to rank above visible contexts. Closes #954.

--- src/selectors/getPrevRank.ts
+++ src/selectors/getPrevRank.ts
@@ -1,14 +1,28 @@
 import { getChildrenRanked } from '../selectors'
+import { isFunction } from '../util'
 import { State } from '../util/initialState'
 import { Context } from '../types'
 
-/** Gets a rank that comes before all thoughts in a context. */
+/** Gets a rank that comes before all visible thoughts in a context. */
 // TODO: Take context not path
-const getPrevRank = (state: State, context: Context) => {
+const getPrevRank = (state: State, context: Context, { aboveMeta }: { aboveMeta?: boolean } = {}) => {
   const children = getChildrenRanked(state, context)
-  return children.length > 0
-    ? children[0].rank - 1
-    : 0
+
+  // no children
+  if (children.length === 0) return 0
+
+  // first child
+  const i = aboveMeta || state.showHiddenThoughts
+    ? 0
+    : children.findIndex(child => !isFunction(child.value))
+
+  // between last metaprogramming attribute and first visible child
+  if (i === 0) return children[0].rank - 1
+
+  const nextChild = children[i]
+  const prevChild = children[i - 1]
+
+  return (prevChild.rank + nextChild.rank) / 2
 }
 
 export default getPrevRank
