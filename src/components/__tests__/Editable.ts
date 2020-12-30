f26f75bfd86c352ea70b31cf026f8eb6dd0cd100
Bijay Shrestha <sbijay777@gmail.com>
2020-12-30 11:46:02 +0545

    add test for comtent editable reset html

---
+++ src/components/__tests__/Editable.ts
@@ -0,0 +1,46 @@
+import { ReactWrapper } from 'enzyme'
+import { store } from '../../store'
+import { RANKED_ROOT } from '../../constants'
+import createTestApp, { cleanupTestApp } from '../../test-helpers/createTestApp'
+import { importText } from '../../action-creators'
+import { setCursorFirstMatchActionCreator } from '../../test-helpers/setCursorFirstMatch'
+import executeShortcut from '../../test-helpers/executeShortcut'
+import bumpThoughtDown from '../../shortcuts/bumpThoughtDown'
+import Editable from '../Editable'
+
+let wrapper: ReactWrapper<unknown, unknown> // eslint-disable-line fp/no-let
+
+beforeEach(async () => {
+  wrapper = await createTestApp()
+})
+
+afterEach(cleanupTestApp)
+
+it('reset content editable inner html on thought bump', async () => {
+
+  // import thoughts
+  store.dispatch([
+    importText({
+      path: RANKED_ROOT,
+      text: `
+        - a
+          - b`
+    }),
+    setCursorFirstMatchActionCreator(['a']),
+  ])
+
+  // update DOM
+  wrapper.update()
+
+  /* eslint-disable jsdoc/require-jsdoc */
+  const getContentEditableText = () => wrapper.find(Editable).first().find('div.editable').first().text()
+
+  expect(getContentEditableText()).toBe('a')
+
+  executeShortcut(bumpThoughtDown, { store })
+
+  jest.runOnlyPendingTimers()
+  wrapper.update()
+
+  expect(getContentEditableText()).toBe('')
+})
