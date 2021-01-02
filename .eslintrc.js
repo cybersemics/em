6e18d73a4a291fb23761830350261c501aee2207
Raine Revere <raine@cybersemics.org>
2021-01-02 09:02:32 -0700

    Reformat .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,151 +1,158 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "mocha": true,
-    "jest": true
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+    jest: true,
   },
-  "extends": [
-    "standard",
-    "react-app",
-    "plugin:react/recommended",
-    "plugin:import/typescript",
-    "raine"
+  extends: [
+    'standard',
+    'react-app',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'raine',
   ],
-  "ignorePatterns": "scripts",
-  "overrides": [
+  ignorePatterns: 'scripts',
+  overrides: [
     {
-      "files": ["**/*.ts", "**/*.tsx"],
-      "env": { "browser": true, "es6": true, "node": true },
-      "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-      ],
-      "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
-      "parser": "@typescript-eslint/parser",
-      "parserOptions": {
-        "ecmaFeatures": { "jsx": true },
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "project": "./tsconfig.json"
+      files: ['**/*.ts', '**/*.tsx'],
+      env: {
+        browser: true,
+        es6: true,
+        node: true
       },
-      "plugins": ["@typescript-eslint"],
-      "rules": {
-        "import/prefer-default-export": 0,
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended'
+      ],
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'import/prefer-default-export': 0,
         // temporary fix from 'typescript-eslint' docs
         // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
-        "no-extra-parens": 0,
-        "no-unused-vars": 0,
-        "no-use-before-define": 0,
-        "@typescript-eslint/member-delimiter-style": [2, {
-          "multiline": {
-            "delimiter": "comma"
-          },
-          "singleline": {
-            "delimiter": "comma"
-          }
-        }],
-        "@typescript-eslint/no-explicit-any": 0,
-        "@typescript-eslint/no-extra-parens": [
-          2,
-          "all",
+        'no-extra-parens': 0,
+        'no-unused-vars': 0,
+        'no-use-before-define': 0,
+        '@typescript-eslint/member-delimiter-style': [2,
           {
-            "nestedBinaryExpressions": false
+            multiline: {
+              delimiter: 'comma'
+            },
+            singleline: {
+              delimiter: 'comma'
+            }
+          }
+         ],
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-extra-parens': [2,
+          'all',
+          {
+            'nestedBinaryExpressions': false
           }
         ],
-        "@typescript-eslint/no-non-null-assertion": 0,
-        "@typescript-eslint/no-use-before-define": 2,
-        "@typescript-eslint/no-unused-vars": 2,
-        "@typescript-eslint/explicit-function-return-type": 0,
-        "@typescript-eslint/explicit-module-boundary-types": 0,
-        "@typescript-eslint/array-type": [2, {
-          "array-type": "array"
-        }],
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/no-use-before-define': 2,
+        '@typescript-eslint/no-unused-vars': 2,
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/array-type': [2,
+          {
+          'array-type': 'array'
+          }
+        ],
         // jsx
-        "jsx-quotes": [
-          "error",
-          "prefer-single"
+        'jsx-quotes': [2,
+          'prefer-single'
         ]
       },
-      "settings": { "react": { "version": "detect" } }
+      settings: { react: { version: 'detect' } }
     }
   ],
   // to be removed later
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
-  "plugins": [
-    "export-default-identifier",
-    "fp",
-    "jsdoc",
-    "react"
+  plugins: [
+    'export-default-identifier',
+    'fp',
+    'jsdoc',
+    'react'
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
+  settings: {
+    react: {
+      version: 'detect'
     }
   },
-  "rules": {
+  rules: {
 
     // export-default-identifier
-    "export-default-identifier/export-default-identifier": [2, {
-      "types": ["Identifier", "CallExpression", "FunctionDeclaration"]
-    }],
+    'export-default-identifier/export-default-identifier': [2,
+      {
+        types: ['Identifier', 'CallExpression', 'FunctionDeclaration']
+      }
+    ],
 
     // jsdoc
-    "jsdoc/check-tag-names": [
-      2,
+    'jsdoc/check-tag-names': [2,
       {
-        "definedTags": ["packageDocumentation"]
+        definedTags: ['packageDocumentation']
       }
     ],
-    "jsdoc/require-description-complete-sentence": [
-      2,
+    'jsdoc/require-description-complete-sentence': [2,
       {
-        "abbreviations": ["e.g.","i.e."]
+        abbreviations: ['e.g.','i.e.']
       }
     ],
-    "jsdoc/require-jsdoc": [
-      2,
+    'jsdoc/require-jsdoc': [2,
       {
-        "contexts": [
-          "VariableDeclarator > ArrowFunctionExpression"
+        contexts: [
+          'VariableDeclarator > ArrowFunctionExpression'
         ],
-        "require": {
-          "ClassDeclaration": true,
-          "ClassExpression": true
+        require: {
+          'ClassDeclaration': true,
+          'ClassExpression': true
         }
       }
     ],
 
     // jrx-a11y
-    "jsx-a11y/anchor-is-valid": 0,
+    'jsx-a11y/anchor-is-valid': 0,
 
     // react
-    "react/jsx-curly-spacing": 2,
-    "react/jsx-equals-spacing": 2,
-    "react/jsx-tag-spacing": [
-      2,
+    'react/jsx-curly-spacing': 2,
+    'react/jsx-equals-spacing': 2,
+    'react/jsx-tag-spacing': [2,
       {
-        "beforeSelfClosing": "allow"
+        beforeSelfClosing: 'allow'
       }
     ],
-    "react/no-children-prop": 0,
-    "react/no-unescaped-entities": 0,
-    "react/prop-types": 0,
+    'react/no-children-prop': 0,
+    'react/no-unescaped-entities': 0,
+    'react/prop-types': 0,
 
     // react-hooks
-    "react-hooks/exhaustive-deps": 0
+    'react-hooks/exhaustive-deps': 0
 
   }
 }
