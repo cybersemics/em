5b5ee18b9e86f56cd9dc2295d6d087853b396b2f
Raine Revere <raine@cybersemics.org>
2021-01-03 14:22:03 -0700

    typedoc-plugin-external-module-name: Rough fix for Custom Module Name Generation.

--- .typedoc-plugin-external-module-name.js
+++ .typedoc-plugin-external-module-name.js
@@ -1,5 +1,6 @@
 /**
- * Custom module name mapping function for typedoc-plugin-external-module-name (https://github.com/christopherthielen/typedoc-plugin-external-module-name)
+ * Custom module name mapping function for typedoc-plugin-external-module-name
+ * https://github.com/christopherthielen/typedoc-plugin-external-module-name#custom-module-name-generation
  * This function defines all module names and creates module hierarchy.
  * Created hierarchy are displayed in the right navbar of generated TypeDoc documentation.
  *
@@ -13,14 +14,15 @@ module.exports = (explicit, implicit, path) => {
 
     // Check for files which are placed in subdirectories of src
     if (implicit !== "."){
-        const match = path.match(RegExp(`${implicit}/.*`))[0]
-        const endIndex = match.lastIndexOf(".")
+        const matches = path.match(RegExp(`${implicit}/.*`))
+        if (!matches) return
+        const endIndex = matches[0].lastIndexOf(".")
         // Result: directoryName.fileName (e.g. "action-creators.alert")
-        return match.slice(0,endIndex).replace(/\//g, ".")
+        return matches[0].slice(0,endIndex).replace(/\//g, ".")
     } else {
-
-        const match = path.match(/src\/.*\./)[0]
+        const matches = path.match(/src\/.*\./)[0]
+        if (!matches) return
         // Result: app.fileName (e.g. "app.browser")
-        if (match) return match.replace(/src/,"app").replace(".","").replace(/\//g, ".")
+        return matches[0].replace(/src/,"app").replace(".","").replace(/\//g, ".")
     }
 }
