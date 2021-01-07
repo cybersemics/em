1742c6ecb3b91bf3a3e21c7863d663c553ea418a
Bijay Shrestha <sbijay777@gmail.com>
2021-01-07 11:15:27 +0545

    fix removeRoot to only remove first thought is root

---
+++ src/util/__tests__/removeRoot.ts
@@ -0,0 +1,24 @@
+import { ROOT_TOKEN } from '../../constants'
+import { removeRoot } from '../removeRoot'
+
+it('remove root thought', () => {
+  const exported = `- ${ROOT_TOKEN}
+  - a
+    - b
+  - c`
+
+  const expectedResult = `- a
+  - b
+- c`
+
+  expect(removeRoot(exported)).toBe(expectedResult)
+})
+
+it('do not remove first thought if it is not a root', () => {
+  const exported = `- k
+  - a
+    - b
+  - c`
+
+  expect(removeRoot(exported)).toBe(exported)
+})
