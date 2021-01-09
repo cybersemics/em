7cd740f569117dcc7d3ef89137e6c892720afa41
Raine Revere <raine@cybersemics.org>
2021-01-09 09:58:37 -0700

    getPrevRank: Add more tests.

--- src/selectors/__tests__/getPrevRank.ts
+++ src/selectors/__tests__/getPrevRank.ts
@@ -1,20 +1,70 @@
-import { initialState, reducerFlow } from '../../util'
-import { getAllChildren } from '../../selectors'
-import { newSubthought, newThought, cursorBack } from '../../reducers'
+import { initialState, isFunction, reducerFlow } from '../../util'
+import { RANKED_ROOT } from '../../constants'
+import { getAllChildrenSorted } from '../../selectors'
+import { importText, newSubthought, newThought } from '../../reducers'
 import getPrevRank from '../getPrevRank'
 
-it('getPrevRank when all the thoughts in the context are hidden', () => {
+it('get rank above all children', () => {
 
   const steps = [
     newThought('a'),
-    newSubthought('=archive'),
-    cursorBack
+    newSubthought('b'),
+    newThought('c'),
   ]
 
   const stateNew = reducerFlow(steps)(initialState())
 
-  const children = getAllChildren(stateNew, ['a'])
+  const children = getAllChildrenSorted(stateNew, ['a'])
 
   expect(getPrevRank(stateNew, ['a'])).toBeLessThan(children[0].rank)
+
+})
+
+it('get rank less than visible children but greater than hidden children', () => {
+
+  const text = `
+    - a
+      - =archive
+      - b
+      - c
+  `
+  const stateNew = importText({ path: RANKED_ROOT, text })(initialState())
+  const children = getAllChildrenSorted(stateNew, ['a'])
+  const firstVisibleIndex = children.findIndex(child => !isFunction(child.value))
+  const firstVisible = children[firstVisibleIndex]
+  const lastHidden = children[firstVisibleIndex - 1]
+
+  expect(getPrevRank(stateNew, ['a'])).toBeLessThan(firstVisible.rank)
+  expect(getPrevRank(stateNew, ['a'])).toBeGreaterThan(lastHidden.rank)
+
+})
+
+it('get rank greater than all hidden children', () => {
+
+  const steps = [
+    newThought('a'),
+    newSubthought('=b'),
+    newThought('=c'),
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const children = getAllChildrenSorted(stateNew, ['a'])
+
+  expect(getPrevRank(stateNew, ['a'])).toBeGreaterThan(children[children.length - 1].rank)
+
+})
+
+it('get rank less than all children hidden with aboveMeta: true', () => {
+
+  const steps = [
+    newThought('a'),
+    newSubthought('=b'),
+    newThought('=c'),
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const children = getAllChildrenSorted(stateNew, ['a'])
+
+  expect(getPrevRank(stateNew, ['a'], { aboveMeta: true })).toBeLessThan(children[0].rank)
 
 })
