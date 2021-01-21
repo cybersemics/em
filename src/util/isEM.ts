1662196d762e4674a6714e16a975055a883aa038
Bijay Shrestha <sbijay777@gmail.com>
2021-01-21 22:22:39 +0545

    check if path contains em token

--- src/util/isEM.ts
+++ src/util/isEM.ts
@@ -1,8 +1,11 @@
 import { EM_TOKEN } from '../constants'
-import { Path } from '../types'
+import { Child } from '../types'
 
 /** Returns true if the Path is the EM_TOKEN. */
-export const isEM = (path: Path): boolean =>
-  path.length === 1 &&
-  !!path[0] &&
-  path[0].value === EM_TOKEN
+export const isEM = (thoughts: (string | Child)[]): boolean =>
+  thoughts.length === 1 &&
+  !!thoughts[0] &&
+  (
+    (thoughts[0] as Child).value === EM_TOKEN ||
+    (thoughts[0] as string) === EM_TOKEN
+  )
