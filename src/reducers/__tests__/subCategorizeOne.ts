f69d008cc4a13dba45b98502bf44628fadf99038
Raine Revere <raine@cybersemics.org>
2021-01-30 16:35:32 -0700

    subCategorizeOne: Add some disabled tests related to #1013 and #862.
    They are disabled since I believe they duplicate existingThoughtMove tests, but can be re-enabled if necessary.

--- src/reducers/__tests__/subCategorizeOne.ts
+++ src/reducers/__tests__/subCategorizeOne.ts
@@ -78,3 +78,55 @@ it('set cursor on new empty thought', () => {
     .toMatchObject([{ value: 'a', rank: 0 }, { value: '', rank: -1 }])
 
 })
+
+// Duplicate test: existingThoughtMove - "move with duplicate descendant"
+// it('subcategorize a thought with duplicate descendants', () => {
+
+//   const text = `
+//   - a
+//     - b
+//       - b`
+
+//   const steps = [
+//     importText({ path: RANKED_ROOT, text }),
+//     setCursorFirstMatch(['a']),
+//     subCategorizeOne,
+//   ]
+
+//   // run steps through reducer flow and export as plaintext for readable test
+//   const stateNew = reducerFlow(steps)(initialState())
+//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plain')
+
+//   expect(exported).toBe(`- ${ROOT_TOKEN}
+//   - ${''/* prevent trim_trailing_whitespace */}
+//     - a
+//       - b
+//         - b`)
+
+// })
+
+// Duplicate test: existingThoughtMove - "move with duplicate descendant"
+// it('subcategorize a thought with hash matched descendants', () => {
+
+//   const text = `
+//   - a
+//     - =note
+//       - note`
+
+//   const steps = [
+//     importText({ path: RANKED_ROOT, text }),
+//     setCursorFirstMatch(['a']),
+//     subCategorizeOne,
+//   ]
+
+//   // run steps through reducer flow and export as plaintext for readable test
+//   const stateNew = reducerFlow(steps)(initialState())
+//   const exported = exportContext(stateNew, [ROOT_TOKEN], 'text/plain')
+
+//   expect(exported).toBe(`- ${ROOT_TOKEN}
+//   - ${''/* prevent trim_trailing_whitespace */}
+//     - a
+//       - =note
+//         - note`)
+
+// })
