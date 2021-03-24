919bc1726482351e16a1c34c8ebde09698fa9aa2
Bijay Shrestha <sbijay777@gmail.com>
2021-03-24 15:20:44 +0545

    split on semicolon
    spliton comma if value has only one period at the end

--- src/reducers/__tests__/splitSentences.ts
+++ src/reducers/__tests__/splitSentences.ts
@@ -49,6 +49,36 @@ it('split single thought on period when there is a combination of periods and co
   - Two, Three.`)
 })
 
+it('split single thought on comma if a thought with a mix of commas and periods has only one period and it\'s the last character', () => {
+  const steps = [
+    newThought('One, Two, Three.'),
+    splitSentences()
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+
+  expect(exported).toBe(`- ${HOME_TOKEN}
+  - One
+  - Two
+  - Three.`)
+})
+
+it('split single thought on main split characters if thought has only one period at the end but has other split characters too. ', () => {
+  const steps = [
+    newThought('One,Seven?Two!Three.'),
+    splitSentences()
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+
+  expect(exported).toBe(`- ${HOME_TOKEN}
+  - One,Seven.
+  - Two.
+  - Three.`)
+})
+
 it('split thought by sentences surrounded by siblings', () => {
   const steps = [
     newThought('a'),
