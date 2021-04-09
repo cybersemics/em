407a00bb5886241a60ef1fc0973f83e403bfa2ea
Onur Polattimur <onur.polattimur@gmail.com>
2021-04-09 22:37:59 +0300

    unroot context before getting value of sort attribute

--- src/selectors/getSortPreference.ts
+++ src/selectors/getSortPreference.ts
@@ -1,10 +1,11 @@
 import { getSetting, getAllChildren } from '../selectors'
 import { State } from '../util/initialState'
 import { Context } from '../types'
+import { unroot } from '../util'
 
 /** Get the sort setting from the given context meta or, if not provided, the global sort. */
 const getSortPreference = (state: State, context: Context) => {
-  const childrenSort = getAllChildren(state, [...context, '=sort'])
+  const childrenSort = getAllChildren(state, [...unroot(context), '=sort'])
   return childrenSort.length > 0
     ? childrenSort[0].value
     : getSetting(state, ['Global Sort']) || 'None'
