bdeb58e893d9e458966bf46ddb8c0eb0c52f3baa
Onur Polattimur <onur.polattimur@gmail.com>
2021-04-10 18:41:22 +0300

    add tests for sorting home context

--- src/shortcuts/__tests__/toggleSort.ts
+++ src/shortcuts/__tests__/toggleSort.ts
@@ -1,11 +1,11 @@
 import { EM_TOKEN, HOME_PATH } from '../../constants'
 import { createTestStore } from '../../test-helpers/createTestStore'
 import { attribute, rankThoughtsFirstMatch } from '../../selectors'
-import { existingThoughtChange, importText } from '../../action-creators'
+import { existingThoughtChange, importText, setCursor } from '../../action-creators'
 import toggleSortShortcut from '../toggleSort'
 import executeShortcut from '../../test-helpers/executeShortcut'
 import { setCursorFirstMatchActionCreator } from '../../test-helpers/setCursorFirstMatch'
-import { Thunk, SimplePath } from '../../types'
+import { SimplePath, Thunk } from '../../types'
 
 it('toggle on sort preference of cursor (initial state without =sort attribute)', () => {
 
@@ -53,6 +53,54 @@ it('toggle off sort preference of cursor (initial state with =sort/Alphabetical)
   executeShortcut(toggleSortShortcut, { store })
 
   expect(attribute(store.getState(), ['a'], '=sort')).toBe(null)
+})
+
+it('toggle on sort preference of home context when cursor is null (initial state without =sort attribute)', () => {
+
+  const store = createTestStore()
+
+  // import thoughts
+  store.dispatch([
+    importText({
+      path: HOME_PATH,
+      text: `
+        - b
+          - 1
+          - 2
+        - a
+          - 3
+          - 4`
+    }),
+
+    setCursor({ path: null }),
+  ])
+
+  executeShortcut(toggleSortShortcut, { store })
+
+  expect(attribute(store.getState(), [], '=sort')).toBe('Alphabetical')
+})
+
+it('toggle off sort preference of home context when cursor is null (initial state with =sort/Alphabetical)', () => {
+
+  const store = createTestStore()
+
+  // import thoughts
+  store.dispatch([
+    importText({
+      path: HOME_PATH,
+      text: `
+        - =sort
+          - Alphabetical
+        -a
+        -b`
+    }),
+
+    setCursor({ path: null }),
+  ])
+
+  executeShortcut(toggleSortShortcut, { store })
+
+  expect(attribute(store.getState(), [], '=sort')).toBe(null)
 })
 
 it('override global Alphabetical with local None', () => {
