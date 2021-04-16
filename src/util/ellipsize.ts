1e5f7762d14cb1d01a3f52554c69d8920e679004
Onur Polattimur <onur.polattimur@gmail.com>
2021-04-17 02:00:50 +0300

    add truncate library to ellipsize html thoughts properly

--- src/util/ellipsize.ts
+++ src/util/ellipsize.ts
@@ -1,6 +1,7 @@
 import { THOUGHT_ELLIPSIZED_CHARS } from '../constants'
+import truncate from 'truncate-html'
 
 /** Returns a string truncated with an ellipsis at a given limit n. */
 export const ellipsize = (s: string, n: number = THOUGHT_ELLIPSIZED_CHARS): string =>
   // subtract 2 so that additional '...' is still within the char limit
-  s.length > n - 2 ? s.slice(0, n - 2) + '...' : s
+  truncate(s, n - 2)
