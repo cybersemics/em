38eb7411722890859c641f8c24e8320eb8916f0f
Bijay Shrestha <sbijay777@gmail.com>
2021-05-02 09:33:00 +0545

    add optional filter function to getDescendants

--- src/selectors/getDescendants.ts
+++ src/selectors/getDescendants.ts
@@ -4,12 +4,22 @@ import { getChildrenRanked } from '../selectors'
 import { State } from '../util/initialState'
 import { Child, SimplePath } from '../types'
 
+interface Options {
+  recur?: boolean,
+  filterFunction?: (child: Child) => boolean,
+}
+
 /** Generates a flat list of all descendants. */
-const getDescendants = (state: State, path: SimplePath, recur?: boolean/* INTERNAL */): Child[] => {
+const getDescendants = (state: State, path: SimplePath, { recur, filterFunction }: Options): Child[] => {
   const children = getChildrenRanked(state, pathToContext(path))
   // only append current thought in recursive calls
   return (recur ? [head(path)] : []).concat(
-    _.flatMap(children, child => getDescendants(state, unroot(path.concat(child) as SimplePath), true))
+    _.flatMap(
+      filterFunction ? children.filter(filterFunction) : children,
+      child => getDescendants(state, unroot(path.concat(child) as SimplePath), {
+        recur: true,
+        filterFunction
+      }))
   )
 }
 
