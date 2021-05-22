3c7a229ef85f463c8d2055f262ffa6d1f5f13810
Ruchi Jain <ruchijn@outlook.com>
2021-05-22 12:19:53 -0400

    [1152]: Font size update to display alert and scroll user to bottom of page when font changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import * as pkg from '../../package.json'
 import { TUTORIAL2_STEP_SUCCESS } from '../constants'
-import { logout, showModal } from '../action-creators'
+import { alert, logout, showModal } from '../action-creators'
 import { getSetting, isTutorial } from '../selectors'
 import { scaleFontDown, scaleFontUp } from '../action-creators/scaleSize'
 import { State } from '../util/initialState'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const mapStateToProps = (state: State) => {
-  const { authenticated, status, user } = state
+  const { authenticated, status, user, fontSize } = state
   return {
     authenticated,
     status,
     isTutorialOn: isTutorial(state),
     tutorialStep: +(getSetting(state, 'Tutorial Step') || 1),
     user,
+    fontSize,
   }
 }
 
 /** A footer component with some useful links. */
-const Footer = ({ authenticated, tutorialStep, user, isTutorialOn, status }: ReturnType<typeof mapStateToProps>) => {
+const Footer = ({ authenticated, tutorialStep, user, isTutorialOn, status, fontSize }: ReturnType<typeof mapStateToProps>) => {
 
   const dispatch = useDispatch()
+
+  const firstUpdate = useRef(true)
+
+  useEffect(() => {
+    /** Below check is needed to stop alert dispatch when rendered for first time. */
+    if (!firstUpdate.current) {
+      dispatch(alert(`Font size: ${fontSize}`))
+
+      setTimeout(() => dispatch(alert(null)), 2000)
+
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+    else {
+      firstUpdate.current = false
+    }
+  }, [fontSize])
 
   // hide footer during tutorial
   // except for the last step that directs them to the Help link in the footer
