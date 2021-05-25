fb2e6a306a64409688480f2bd6bfef1ea42f2b6f
linghong <linghong_chen@hotmail.com>
2021-05-25 14:18:54 -0400

    added special cases for email and url address.

--- src/util/splitSentence.ts
+++ src/util/splitSentence.ts
@@ -43,7 +43,7 @@ export const splitSentence = (value: string) : string[] => {
      * Case3, ending with an email address.
      * Case4, ending with a website address.
      */
-    if (isAbbrev(prevSentence, s, spliters[i]) || isDecimalNum(prevSentence, s)) {
+    if (isAbbrev(prevSentence, s, spliters[i]) || isDecimalNum(prevSentence, s) || isEmail(prevSentence, s, spliters[i]) || isUrl(prevSentence, s)) {
 
       filterOut = [...filterOut, i - 1]
       newSentences = [...newSentences, prevCurrSentence]
@@ -80,4 +80,46 @@ export const splitSentence = (value: string) : string[] => {
  */
 function isDecimalNum (str1 : string, str2: string) {
   return !!str1.match(/[0-9]\.$/) && !!str2.match(/^[0-9]/)
+}
+
+/**
+ * Function: isEmail.
+ *
+ * @param str1 The charactor before the dot.
+ * @param s The current sentence which is right behind the spliter.
+ * @param spliter2 The second spliter.
+ * @returns A bolean that says whether the dot comes from an email address.
+ */
+function isEmail (str1 : string, s : string, spliter2 : string) {
+  if (str1[str1.length - 1] === ' ') return false
+  const isPartialEmail = (str1.match(/@/) || s.match(/@/)) && spliter2 === '.'
+
+  const firstPart = str1.split(' ')
+  const len = firstPart.length
+  const isFullEmail = (firstPart[len - 1] + s.trimEnd()).match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+  return isPartialEmail || isFullEmail
+}
+
+/**
+ * Function: isUrl.
+ *
+ * @param str1 The charactor before the dot.
+ * @param s The current sentence which is right behind the spliter.
+ * @param spliter2 The second spliter.
+ * @returns A bolean that says whether the dot comes from a url.
+ */
+function isUrl(str1 :string, s : string) {
+  if (str1[str1.length - 1] === ' ') return false
+  const finder = 'https://' || 'http://' || 'www'
+  const index = str1.indexOf(finder)
+  const isPartialUrl = index ? str1.slice(index).split(' ').length < 1 && str1.match(/.$/) : false
+
+  // Regex Reference for Url from https://stackoverflow.com/questions/42618872/regex-for-website-or-url-validation .
+  const urlPattern = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+
+  const firstPart = str1.split(' ')
+  const len = firstPart.length
+  const isFullUrl = (firstPart[len - 1] + s.split(' ')[0]).match(urlPattern)
+
+  return isPartialUrl || isFullUrl
 }
