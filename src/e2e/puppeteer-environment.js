0c1e2df8baaaec37cc18c7b324635e9e24b21ca0
Bijay Shrestha <sbijay777@gmail.com>
2021-05-31 18:43:35 +0545

    Remove static server spin up inside puppeteer env configuration (#1193)

--- src/e2e/puppeteer-environment.js
+++ src/e2e/puppeteer-environment.js
@@ -4,11 +4,7 @@
 
 const chalk = require('chalk')
 const JsDomEnvironment = require('jest-environment-jsdom')
-const { setup: setupDevServer, teardown: teardownDevServer } = require('jest-dev-server')
-const fs = require('fs')
-const path = require('path')
 const puppeteer = require('puppeteer')
-const portUsed = require('tcp-port-used')
 
 /** Puppeteer Environment for jest. */
 class PuppeteerEnvironment extends JsDomEnvironment {
@@ -22,28 +18,6 @@ class PuppeteerEnvironment extends JsDomEnvironment {
 
     await super.setup()
 
-    // use existing app if already running
-    if (await portUsed.check(3000, 'localhost')) {
-      console.info(chalk.yellow('Using the currently running app on http://localhost:3000'))
-    }
-    // otherwise serve up the build folder
-    else {
-      const buildPath = path.join(__dirname, '..', '..', 'build')
-      const doesBuildExist = fs.existsSync(buildPath)
-
-      if (!doesBuildExist) {
-        console.error(chalk.red('App build not found.'))
-        throw new Error('App build not found.')
-      }
-
-      await setupDevServer({
-        command: 'npm run servebuild',
-        launchTimeout: 300000,
-        debug: true,
-        port: 3000
-      })
-    }
-
     this.global.browser = await puppeteer.launch({
       headless: true,
     })
@@ -53,7 +27,6 @@ class PuppeteerEnvironment extends JsDomEnvironment {
   async teardown() {
     console.info(chalk.yellow('Teardown Test Environment.'))
     await this.global.browser.close()
-    await teardownDevServer()
     await super.teardown()
   }
 }
