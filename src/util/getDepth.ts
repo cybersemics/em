c893151246034f3f3beb32ecfdf32e743bc9823b
anmola <anmolaroraxyz@gmail.com>
2021-06-03 15:26:55 +0530

    replaces getChildrenRanked with getAllChildren

--- src/util/getDepth.ts
+++ src/util/getDepth.ts
@@ -1,11 +1,11 @@
-import { getChildrenRanked } from '../selectors'
+import { getAllChildren } from '../selectors'
 import { Context } from '../types'
 import { State } from './initialState'
 import { unroot } from './unroot'
 
 /** Get max depth of a visible context. */
 export const getDepth = (state: State, context: Context): number => {
-  const children = getChildrenRanked(state, context) ?? []
+  const children = getAllChildren(state, context) ?? []
   return children.length === 0
     ? 0
     : Math.max(...children.map(child => {
