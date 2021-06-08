715fdaf6486a8eb0b3ef89b0a69ec74702a53404
Raine Revere <raine@cybersemics.org>
2021-06-08 14:37:20 -0600

    gesture helper: Fix type (minor).

--- src/e2e/helpers/mobile/gestures/gesture.ts
+++ src/e2e/helpers/mobile/gestures/gesture.ts
@@ -7,18 +7,23 @@ export interface GestureOptions {
   segmentLength?: number,
 }
 
+interface Action<T = any> {
+  action: string,
+  options?: T,
+}
+
 const WAIT_ACTION = { action: 'wait', options: { ms: 100 } }
 
 /** Apply gesture action for the given path. */
 const gesture = async (browser: Browser<'async'>, path: Direction[], { xStart = 70, yStart = 300, segmentLength = 70 }: GestureOptions = {}) => {
 
-  const moveActions = path.reduce((acc: any[], cur, index) => {
+  const moveActions = path.reduce<Action[]>((acc, cur) => {
     const { options: { x: previousX, y: previousY } } = acc.length > 0
       ? acc[acc.length - 1]
       : { options: { x: xStart, y: yStart } }
     const x = previousX + (cur === 'r' ? +segmentLength : cur === 'l' ? -segmentLength : 0)
     const y = previousY + (cur === 'd' ? +segmentLength : cur === 'u' ? -segmentLength : 0)
-    return [...acc, WAIT_ACTION, { action: 'moveTo', options: { x: x, y: y } }]
+    return [...acc, WAIT_ACTION, { action: 'moveTo', options: { x, y } }]
   }, [])
 
   // add first and last action
