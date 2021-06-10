9f7140ad87522ab03b2aa7892bc829e2a0953426
Raine Revere <raine@cybersemics.org>
2021-06-10 14:33:44 -0600

    TutorialStepSuccess: Revert punctuation.

--- src/components/Tutorial/TutorialStepSuccess.jsx
+++ src/components/Tutorial/TutorialStepSuccess.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const TutorialStepSuccess = () => <Fragment>
-  <p>Huzzah... You have completed the tutorial. Creating thoughts and subthought is all you need to create a personalized thoughtspace. There's a lot more to <b>em</b>; however, additional commands have been designed to facilitate a truly fluid sensemaking experience.</p>
+  <p>Huzzah... You have completed the tutorial. Creating thoughts and subthought is all you need to create a personalized thoughtspace. There's a lot more to <b>em</b> however. Additional commands have been designed to facilitate a truly fluid sensemaking experience.</p>
   <p>How are you feeling? Would you like to learn more or play on your own?</p>
 </Fragment>
 
