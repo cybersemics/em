3f5148ce66a632ac78bb33a5010bd3abd88505dc
Raine Revere <raine@cybersemics.org>
2021-06-12 15:19:08 -0600

    Parse =let into environment and pass to descendants. Load styles from defined child expressions.

--- src/selectors/getStyle.ts
+++ src/selectors/getStyle.ts
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'react'
 import _ from 'lodash'
 import { getAllChildren } from '../selectors'
 import { State } from '../util/initialState'
@@ -5,7 +6,7 @@ import { Context } from '../types'
 import { keyValueBy } from '../util/keyValueBy'
 
 /** Parses the =style attribute of a given context into an object that can be passed to React styles. Returns null if there are no styles. */
-const getStyle = (state: State, context: Context, { container }: { container?: boolean } = {}) => {
+const getStyle = (state: State, context: Context, { container }: { container?: boolean } = {}): CSSProperties | null => {
 
   const styleContext = [...context, container ? '=styleContainer' : '=style']
   const children = getAllChildren(state, styleContext)
