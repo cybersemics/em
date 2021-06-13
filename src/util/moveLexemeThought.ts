19a06f0bd285385c09ca73287d0435f5750177ca
Raine Revere <raine@cybersemics.org>
2021-06-13 10:33:20 -0600

    Rename moveThought -> moveLexemeThought

--- src/util/moveLexemeThought.ts
+++ src/util/moveLexemeThought.ts
@@ -2,7 +2,7 @@ import { Context, Lexeme, Timestamp } from '../types'
 import { concatOne, equalArrays, timestamp } from '../util'
 
 /** Returns a new thought that has been moved either between contexts or within a context (i.e. Changed rank). Removes duplicates with the same { value, rank }. */
-export const moveThought = (thought: Lexeme, oldContext: Context, newContext: Context, oldRank: number, newRank: number, id: string | null, archived: Timestamp) => ({
+export const moveLexemeThought = (thought: Lexeme, oldContext: Context, newContext: Context, oldRank: number, newRank: number, id: string | null, archived: Timestamp) => ({
   ...thought,
   contexts: concatOne((thought.contexts || [])
     .filter(parent =>
