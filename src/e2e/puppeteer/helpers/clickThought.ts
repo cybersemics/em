d4e92a2b2b30e0bafc854bc0762335cafe617045
Raine Revere <raine@cybersemics.org>
2021-06-16 11:24:44 -0600

    Increase puppeteer clickThought timeout to avoid test failures.

--- src/e2e/puppeteer/helpers/clickThought.ts
+++ src/e2e/puppeteer/helpers/clickThought.ts
@@ -5,9 +5,9 @@ import waitForEditable from './waitForEditable'
  * Click the thought for the given thought value. Waits for the thought at the beginning in case it hasn't been rendered yet.
  */
 const clickThought = async (page: Page, value: string) => {
-  // use a very short timeout, assuming that if the user expects to be able to click on a thought then they shouldn't have any noticeable wait
-  // clickThought should be preceeded by a longer waitForEditable for steps that are known to take time, such as refreshing the page
-  const editableNode = await waitForEditable(page, value, { timeout: 500 })
+  // use a short timeout to make time for a render and async page communication
+  // precede clickThought by a longer waitForEditable for steps that are known to take time, such as refreshing the page
+  const editableNode = await waitForEditable(page, value, { timeout: 1000 })
   await editableNode.asElement()?.click()
 }
 
