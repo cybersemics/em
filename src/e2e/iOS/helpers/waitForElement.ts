b97a09bb80277e1a3743537f672a94ed9bea2bd6
Raine Revere <raine@cybersemics.org>
2021-06-16 13:02:09 -0400

    Add missing types to e2e iOS helpers (#1283)

--- src/e2e/iOS/helpers/waitForElement.ts
+++ src/e2e/iOS/helpers/waitForElement.ts
@@ -1,7 +1,7 @@
-import { Browser } from 'webdriverio'
+import { Browser, Element } from 'webdriverio'
 
 /** Wait for element. */
-const waitForElement = async(browser: Browser<any>, selector: string) => {
+const waitForElement = async (browser: Browser<'async'>, selector: string): Promise<Element<'async'>> => {
   await browser.waitUntil(async () => !!(await browser.$(selector)).elementId)
   return browser.$(selector)
 }
--- src/e2e/iOS/helpers/waitForElementNotExist.ts
+++ src/e2e/iOS/helpers/waitForElementNotExist.ts
@@ -1,8 +1,8 @@
 import { Browser } from 'webdriverio'
 
 /** Wait for the element until not exist. */
-const waitForElementNotExist = async (browser: Browser<any>, selector: string) => {
-  return browser.waitUntil(async () => !(await browser.$(selector)).elementId)
+const waitForElementNotExist = async (browser: Browser<'async'>, selector: string): Promise<boolean> => {
+  return !!browser.waitUntil(async () => !(await browser.$(selector)).elementId)
 }
 
 export default waitForElementNotExist
