d3d78d7c60fe8ff5c9fb2dc329a69a3b9e4c55a6
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-06-21 23:29:55 +0200

    linter clean up

--- src/action-creators/showModal.ts
+++ src/action-creators/showModal.ts
@@ -2,7 +2,7 @@ import { Thunk } from '../types'
 
 /** Display a given modal dialog box and scroll to the top. */
 const showModal = (payload: { id: string }): Thunk => (dispatch, getState) => {
-  console.log({ payload })
+
   dispatch({ type: 'showModal', ...payload })
   // window.scrollTo(0, 0)
 }
