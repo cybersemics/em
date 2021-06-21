77bb4a1e1f03820f34f3e7209071953ba3ddff2d
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-06-21 22:49:57 +0200

    temp comment middlewares

--- src/store.ts
+++ src/store.ts
@@ -27,9 +27,9 @@ export const store = createStore(
   appReducer,
   composeEnhancers(applyMiddleware(
     multi,
-    thunk,
-    pushQueue,
-    pullQueue,
-    updateUrlHistory
+    thunk
+    /*   pushQueue,
+      pullQueue, */
+    // updateUrlHistory
   ), /* undoRedoReducerEnhancer, */ cursorChangedEnhancer)
 )
