f23343267e25271d900b8e741fb4ced14acaaac5
Bijay Shrestha <sbijay777@gmail.com>
2021-06-25 20:48:17 +0545

    Add updated emoji regex (#1302)

--- src/util/addEmojiSpace.ts
+++ src/util/addEmojiSpace.ts
@@ -1,5 +1,12 @@
-// regex pattern that checks if string starts with number of emoji followed by non-whitespace, non emoji character
-const emojiWithoutSpaceRegex = /^((\p{Emoji_Presentation}|\p{Extended_Pictographic})\ufe0f?){1,}(?!(\s|\ufe0f|\p{Emoji_Presentation}|\p{Extended_Pictographic})|$)/gu
+import { EMOJI_REGEX } from '../constants'
+
+/*
+Regex pattern that matches a group of emojis starting from the beginning of the string.
+
+Note: Some emojis end with zero width joiner or zero width space which don't actually give any visible whitespace.
+        So optionally including them at the end of the regex.
+*/
+const emojiGroupRegex = new RegExp(`^(${EMOJI_REGEX.source}){1,}\u200D?\u200B?`)
 
 /* Note: The above regex uses unicode property escape to match emojis https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Unicode_Property_Escapes.
   However these emoji property escapes don't account for emoji variant selector (\ufe0f) which can be found in many emojis (example: 🖼️, 🖥️).
@@ -7,10 +14,18 @@ const emojiWithoutSpaceRegex = /^((\p{Emoji_Presentation}|\p{Extended_Pictograph
 */
 
 /**
- * If a string starts with an emoji followed by a non-whitespace, non-emoji character,
+ * If a string starts with an emoji or group of emojis followed by a non-whitespace, non-emoji character,
  * then insert a space between the emoji and the character.
  */
 export const addEmojiSpace = (text: string): string => {
-  const match = text.match(emojiWithoutSpaceRegex)
-  return match ? match[0] + ' ' + text.substring(match[0].length) : text
+  const match = text.match(emojiGroupRegex)
+
+  if (match && match[0] && match[0].length < text.length) {
+    const replaceRegex = new RegExp(`^${match[0]}`)
+    const afterEmojis = text.substring(match[0].length, text.length)
+    const startsWithNonWhiteSpaceCharacter = /^[^\s]/.test(afterEmojis)
+    return startsWithNonWhiteSpaceCharacter ? text.replace(replaceRegex, `${match[0]} `) : text
+  }
+
+  return text
 }
