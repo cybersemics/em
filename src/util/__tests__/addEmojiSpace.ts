fba0008f9bdccbb3599ab5d8b843653a4907ad55
Bijay Shrestha <sbijay777@gmail.com>
2021-06-25 20:21:57 +0545

    add more robust emoji regex
    add more tests for emoji related utils
    
    fixes #1289

--- src/util/__tests__/addEmojiSpace.ts
+++ src/util/__tests__/addEmojiSpace.ts
@@ -14,3 +14,14 @@ it('add space for emoji with selectors(\ufe0f) at the end.', () => {
   expect(addEmojiSpace('🖥️')).toEqual('🖥️')
   expect(addEmojiSpace('🖥️🖼️Amigo')).toEqual('🖥️🖼️ Amigo')
 })
+
+it('do not break zwj sequenced emojis.', () => {
+  expect(addEmojiSpace('👩🏻‍🏫Reading')).toEqual('👩🏻‍🏫 Reading')
+  expect(addEmojiSpace('👩🏻‍🏫')).toEqual('👩🏻‍🏫')
+  expect(addEmojiSpace('👩‍👩‍👦‍👦💏Family')).toEqual('👩‍👩‍👦‍👦💏 Family')
+})
+
+it('only add extra spaces if emojis are followed by non whitespace character.', () => {
+  expect(addEmojiSpace('🐶🐰 Animals')).toEqual('🐶🐰 Animals')
+  expect(addEmojiSpace('The Great Gatsby')).toEqual('The Great Gatsby')
+})
