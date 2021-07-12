b3583433d048e596dcd0420d6b771822ffb9d526
Raine Revere <raine@cybersemics.org>
2021-07-12 09:08:15 -0600

    Move subscribeRemote to initFirebase.

--- src/util/initFirebase.ts
+++ src/util/initFirebase.ts
@@ -1,5 +1,6 @@
 import { Store } from 'redux'
 import globals from '../globals'
+import { ALGOLIA_CONFIG, FIREBASE_CONFIG, OFFLINE_TIMEOUT } from '../constants'
 import {
   authenticate,
   loadPublicThoughts,
@@ -7,13 +8,65 @@ import {
   status as statusActionCreator,
   userAuthenticated,
 } from '../action-creators'
-import { ALGOLIA_CONFIG, FIREBASE_CONFIG, OFFLINE_TIMEOUT } from '../constants'
-import { owner, initFirebaseSubscriptions } from '../util'
+import { firebaseChangeTypes, subscribe } from '../data-providers/firebase'
+import { hashContext, hashThought, owner } from '../util'
 import { State } from '../util/initialState'
-import { Snapshot, User } from '../types'
+import { SessionType } from '../util/sessionManager'
+import { Lexeme, Parent, Snapshot, User } from '../types'
 import initAlgoliaSearch from '../search/algoliaSearch'
-import { getSubscriptionUtils } from './subscriptionUtils'
+import { getSubscriptionUtils, Updates } from './subscriptionUtils'
 
+/** Get Firebase change Handlers. */
+const getFirebaseChangeHandlers = ({ shouldIncludeUpdate }: ReturnType<typeof getSubscriptionUtils>) => ({
+  contextIndex: {
+    [firebaseChangeTypes.create]: (change: Parent) => {
+      return {
+        contextIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashContext(change.context)]: change } : {},
+      }
+    },
+    [firebaseChangeTypes.update]: (change: Parent) => {
+      return {
+        contextIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashContext(change.context)]: change } : {},
+      }
+    },
+    [firebaseChangeTypes.delete]: (change: Parent) => {
+      return {
+        contextIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashContext(change.context)]: null } : {},
+      }
+    },
+  },
+  thoughtIndex: {
+    [firebaseChangeTypes.create]: (change: Lexeme) => {
+      return {
+        thoughtIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashThought(change.value)]: change } : {},
+      }
+    },
+    [firebaseChangeTypes.update]: (change: Lexeme) => {
+      return {
+        thoughtIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashThought(change.value)]: change } : {},
+      }
+    },
+    [firebaseChangeTypes.delete]: (change: Lexeme) => {
+      return {
+        thoughtIndexUpdates:
+          change && shouldIncludeUpdate(change, SessionType.REMOTE) ? { [hashThought(change.value)]: null } : {},
+      }
+    },
+  },
+})
+
+/** Setup firebase subscriptions to handle local sync. */
+export const initFirebaseSubscriptions = (
+  userId: string,
+  subscritpionUtils: ReturnType<typeof getSubscriptionUtils>,
+) => {
+  subscribe<Updates>(userId, subscritpionUtils.getMergeAndApplyUpdates(), getFirebaseChangeHandlers(subscritpionUtils))
+}
 /** Initialize firebase and event handlers. */
 export const initFirebase = async ({ store }: { store: Store<State, any> }) => {
   if (window.firebase) {
