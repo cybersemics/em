772d64545c8bcee907d5b2b11c69bae25493cc12
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-07-12 19:27:20 +0200

    ♻️  fix useFooterUseSelectors

--- src/hooks/Footer.useSelectors.ts
+++ src/hooks/Footer.useSelectors.ts
@@ -1,37 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useSelector } from 'react-redux'
-import {
-  isTutorial,
-  authenticatedSelector,
-  userSelector,
-  statusSelector,
-  tutorialStepSelector,
-  fontSizeSelector,
-  isPushingSelector,
-  pushQueueLengthSelector,
-} from '../selectors'
-
-import { useShallowEqualSelector } from './useShallowEqualSelector'
+import { shallowEqual, useSelector } from 'react-redux'
+import { isTutorial, getSetting } from '../selectors'
+import { State } from '../util/initialState'
 
 /** Helper hook that allows web and native to share selectors for the footer component. */
 export const useFooterUseSelectors = () => {
-  const authenticated = useSelector(authenticatedSelector)
-  const user = useShallowEqualSelector(userSelector)
-  const status = useSelector(statusSelector)
-  const tutorialStep = useSelector(tutorialStepSelector)
-  const isPushing = useSelector(isPushingSelector)
-  const isTutorialOn = useSelector(isTutorial)
-  const fontSize = useSelector(fontSizeSelector)
-  const pushQueueLength = useSelector(pushQueueLengthSelector)
-
-  return {
-    authenticated,
-    user,
-    status,
-    tutorialStep,
-    isPushing,
-    isTutorialOn,
-    pushQueueLength,
-    fontSize,
-  }
+  return useSelector(
+    (state: State) => ({
+      authenticated: state.authenticated,
+      user: state.user,
+      status: state.status,
+      tutorialStep: +(getSetting(state, 'Tutorial Step') || 1),
+      isPushing: state.isPushing,
+      isTutorialOn: isTutorial(state),
+      fontSize: state.fontSize,
+      pushQueueLength: state.pushQueue.length,
+    }),
+    shallowEqual,
+  )
 }
