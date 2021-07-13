d7d759d229a1bbc0e16202b81a92299d1aeaf7be
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-07-13 15:45:38 +0200

    🎨  commonStyles

--- src/components/Footer.native.tsx
+++ src/components/Footer.native.tsx
@@ -1,44 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import * as pkg from '../../package.json'
-import { logout, showModal } from '../action-creators'
+import { logout, showModal, alert } from '../action-creators'
 import { scaleFontDown, scaleFontUp } from '../action-creators/scaleSize'
 
 import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { Text } from './Text.native'
 import { useFooterUseSelectors } from '../hooks/Footer.useSelectors'
+import { commonStyles } from '../style/commonStyles'
+
+const { flexEnd, textOpacityWhite, hyperlink, lightblueText, row, justifyContentEnd, flexItemsEndRow } = commonStyles
 
 /** A footer component with some useful links. */
 const Footer = () => {
   const dispatch = useDispatch()
-  const { authenticated, user, status, pushQueueLength } = useFooterUseSelectors()
+  const { authenticated, user, status, pushQueueLength, fontSize } = useFooterUseSelectors()
+
+  // alert when font size changes
+  const firstUpdate = useRef(true)
+
+  // alert when font size changes
+  useEffect(() => {
+    // prevent alert dispatch when rendered for first time
+    if (!firstUpdate.current) {
+      dispatch(alert(`Font size: ${fontSize}`, { clearTimeout: 2000 }))
+    } else {
+      firstUpdate.current = false
+    }
+  }, [fontSize])
 
   // if (isTutorialOn && tutorialStep !== TUTORIAL2_STEP_SUCCESS) return null
-
   return (
     <View style={styles.container}>
-      <View style={styles.topContent}>
+      <View style={[flexItemsEndRow]}>
         <TouchableOpacity style={styles.topContentMargin} onPress={() => dispatch(scaleFontUp())}>
-          <Text style={styles.scaleFontUp}>A</Text>
+          <Text style={[lightblueText, hyperlink, styles.scaleFontUp]}>A</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => dispatch(scaleFontDown())}>
-          <Text style={styles.lightblueText}>A</Text>
+          <Text style={[lightblueText, hyperlink]}>A</Text>
         </TouchableOpacity>
 
-        <View style={styles.modalOptions}>
+        <View style={[row, justifyContentEnd]}>
           <TouchableOpacity style={styles.optionButton} onPress={() => dispatch(showModal({ id: 'feedback' }))}>
-            <Text style={styles.lightblueText}>Feedback</Text>
+            <Text style={[lightblueText, hyperlink]}>Feedback</Text>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.optionButton} onPress={() => dispatch(showModal({ id: 'help' }))}>
-            <Text style={styles.lightblueText}>Help</Text>
+            <Text style={[lightblueText, hyperlink]}>Help</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             style={styles.authButton}
             onPress={() => (authenticated ? dispatch(logout()) : dispatch(showModal({ id: 'auth' })))}
           >
-            <Text style={styles.lightblueText}>{authenticated ? 'Log Out' : 'Log In'}</Text>
+            <Text style={[lightblueText, hyperlink]}>{authenticated ? 'Log Out' : 'Log In'}</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -46,8 +61,8 @@ const Footer = () => {
       {user && (
         <>
           <View>
-            <Text style={styles.textOpacityWhite}>Status: </Text>
-            <Text style={styles.textOpacityWhite}>
+            <Text style={textOpacityWhite}>Status: </Text>
+            <Text style={textOpacityWhite}>
               {pushQueueLength > 0
                 ? 'Saving'
                 : status === 'loaded'
@@ -59,14 +74,14 @@ const Footer = () => {
             <Text style={styles.textOpacityWhite}>Logged in as: {user?.email} </Text>
           </View>
           <View>
-            <Text style={styles.textOpacityWhite}>User ID: </Text>
-            <Text style={styles.textOpacityWhite}>{user?.uid?.slice(0, 6)}</Text>
+            <Text style={textOpacityWhite}>User ID: </Text>
+            <Text style={textOpacityWhite}>{user?.uid?.slice(0, 6)}</Text>
           </View>
         </>
       )}
 
-      <View style={styles.flexEnd}>
-        <Text style={styles.textOpacityWhite}>{`Version: ${pkg.version}`}</Text>
+      <View style={flexEnd}>
+        <Text style={textOpacityWhite}>{`Version: ${pkg.version}`}</Text>
       </View>
     </View>
   )
@@ -77,16 +92,14 @@ const styles = StyleSheet.create({
   flexEnd: { alignItems: 'flex-end' },
   textOpacityWhite: { color: 'white', opacity: 0.5 },
   container: { backgroundColor: '#1a1a1a', padding: 15 },
-  topContent: { flexDirection: 'row', alignItems: 'flex-end', marginBottom: 15 },
-  scaleFontUp: { color: 'lightblue', fontSize: 14, textDecorationLine: 'underline' },
-  lightblueText: { color: 'lightblue', textDecorationLine: 'underline' },
+  topContent: { marginBottom: 15 },
+  scaleFontUp: { fontSize: 14 },
   optionButton: {
     paddingHorizontal: 10,
     marginHorizontal: 5,
     borderRightWidth: 1,
     borderRightColor: '#fff',
   },
-  modalOptions: { flex: 1, justifyContent: 'flex-end', flexDirection: 'row' },
   authButton: {
     paddingLeft: 10,
     marginLeft: 5,
