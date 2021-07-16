1fe3ce9becd84f094490153c40e81b8c8267ee2e
Onur Polattimur <onur.polattimur@gmail.com>
2021-07-17 00:47:25 +0300

    use built in waitForExist method instead of custom util function to wait until element not exist

--- src/e2e/iOS/helpers/hideKeyboardByTappingDone.ts
+++ src/e2e/iOS/helpers/hideKeyboardByTappingDone.ts
@@ -1,13 +1,12 @@
-import waitForElementNotExist from './waitForElementNotExist'
-import { Browser } from 'webdriverio'
+import { Browser, Element } from 'webdriverio'
 
 /** Hide ios keyboard by tapping done button above the keyboard. */
 const hideKeyboardByTappingDone = async (browser: Browser<'async'>) => {
   const oldContext = (await browser.getContext()) || 'NATIVE_APP'
   await browser.switchContext('NATIVE_APP')
-  const done = await browser.$('//XCUIElementTypeButton[@name="Done"]')
+  const done = (await browser.$('//XCUIElementTypeButton[@name="Done"]')) as Element<'async'>
   await done.click()
-  await waitForElementNotExist(browser, '//XCUIElementTypeButton[@name="Done"]')
+  await done.waitForExist({ reverse: true })
   await browser.switchContext(oldContext)
 }
 
