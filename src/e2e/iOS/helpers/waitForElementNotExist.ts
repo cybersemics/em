cee974c3dbcbd038a66d6b8996bcafd3d6672ca4
Onur Polattimur <onur.polattimur@gmail.com>
2021-07-17 01:48:16 +0300

    trigger ci

--- src/e2e/iOS/helpers/waitForElementNotExist.ts
+++ src/e2e/iOS/helpers/waitForElementNotExist.ts
@@ -1,6 +1,6 @@
 import { Browser } from 'webdriverio'
 
-/** Wait for the element until not exist.    */
+/** Wait for the element until not exist. */
 const waitForElementNotExist = async (browser: Browser<'async'>, selector: string): Promise<boolean> => {
   return !!browser.waitUntil(async () => !(await browser.$(selector)).elementId)
 }
