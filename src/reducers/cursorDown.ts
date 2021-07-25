8eaf760cc5f69e17d8cf75313da53c985bee845c
Onur Polattimur <onur.polattimur@gmail.com>
2021-07-25 22:30:51 +0300

    make nextThoughts property of nextThoughtResult nullable

--- src/reducers/cursorDown.ts
+++ src/reducers/cursorDown.ts
@@ -1,7 +1,7 @@
 import { HOME_TOKEN } from '../constants'
 import { setCursor } from '../reducers'
 import { getChildrenSorted } from '../selectors'
-import { isRoot, nextThought } from '../util'
+import { nextThought } from '../util'
 import { State } from '../@types'
 
 /** Moves the cursor to the next child, sibling, or nearest uncle. */
@@ -11,7 +11,7 @@ const cursorDown = (state: State) => {
   // if there is a cursor, get the next logical child, sibling, or uncle
   if (cursor) {
     const { nextThoughts, contextChain } = nextThought(state, cursor)
-    return nextThoughts.length > 0 && !isRoot(nextThoughts)
+    return nextThoughts && nextThoughts.length > 0
       ? setCursor(state, {
           path: nextThoughts,
           contextChain: contextChain || [],
