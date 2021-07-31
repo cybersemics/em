1abb79f438f82b37779835d055949fbaee90a6f6
Onur Polattimur <onur.polattimur@gmail.com>
2021-07-31 07:20:47 +0300

    BrowserStack: remove resetSafariFormatSettings (#1374)

--- src/e2e/iOS/helpers/initSession.ts
+++ src/e2e/iOS/helpers/initSession.ts
@@ -1,7 +1,6 @@
 import { Browser } from 'webdriverio'
 import waitForElement from '../helpers/waitForElement'
 import tap from './tap'
-import resetSafariFormatSettings from './resetSafariFormatSettings'
 
 /** Returns a function that starts a new browserstack session and skips the tutorial. The function will reload the session after the first test. */
 const initSession = (): (() => Promise<Browser<'async'>>) => {
@@ -17,11 +16,9 @@ const initSession = (): (() => Promise<Browser<'async'>>) => {
     }
 
     await mobileBrowser.url('http://bs-local.com:3000')
-    await resetSafariFormatSettings(mobileBrowser)
     const skipElement = await waitForElement(mobileBrowser, '#skip-tutorial', { timeout: 90000 })
     await mobileBrowser.waitUntil(async () => await skipElement.isClickable())
     await tap(mobileBrowser, skipElement)
-    // await skipElement.click()
     await waitForElement(mobileBrowser, '.new-thought-instructions', { timeout: 90000 })
     return mobileBrowser
   }
