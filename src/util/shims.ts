4d2752eef2b122c22c9bb0db533d93788f533228
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-06 13:54:51 +0200

    ⚡️ shim document and window objects

---
+++ src/util/shims.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable no-global-assign */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-native-reassign */
+import { isMobile } from './isMobile'
+
+// @ts-ignore
+window = !isMobile
+  ? window
+  : {
+      getSelection: () => ({
+        focusOffset: 0,
+      }),
+      setTimeout: setTimeout,
+    }
+
+// @ts-ignore
+document = !isMobile
+  ? document
+  : {
+      activeElement: {
+        classList: {
+          contains: (str: string) => true,
+        },
+      },
+      getElementsByClassName: (str: string) => {},
+    }
