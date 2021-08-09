d093efc9f56a96d357fa36680969e5a6537f370e
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-09 17:56:38 +0200

    💩  toolbar icons isActive

--- src/components/Toolbar.native.tsx
+++ src/components/Toolbar.native.tsx
@@ -14,39 +14,48 @@ import { shortcutById } from '../shortcuts'
 import { FlatList, ListRenderItem, TouchableOpacity, View, StyleSheet, GestureResponderEvent } from 'react-native'
 // import { store } from '../store'
 import { TOOLBAR_DEFAULT_SHORTCUTS } from '../constants'
-import { Icon } from '../@types'
+import { Icon, State } from '../@types'
 import HamburgerMenu from './HamburgerMenu'
 import { store } from '../store'
+import { useSelector } from 'react-redux'
 
 /** Toolbar component mobile. */
-const Toolbar = (
-  {
-    dark = true,
-    fontSize = 12,
-    toolbarOverlay = null,
-    scrollPrioritized = true,
-    showTopControls = true,
-  } /* : ReturnType<typeof mapStateToProps> */,
-) => {
+const Toolbar = ({
+  dark = true,
+  fontSize = 12,
+  toolbarOverlay = null,
+  scrollPrioritized = true,
+  showTopControls = true,
+}) => {
+  const wholeStore = useSelector((state: State) => state)
+
   const fillColor = dark ? 'white' : 'black'
 
   const shortcutIds = TOOLBAR_DEFAULT_SHORTCUTS
 
   const keyExtractor = useCallback((item: string) => item, [])
-  const renderItem: ListRenderItem<string> = useCallback(({ item: id, index }) => {
-    const { svg, exec } = shortcutById(id)! ?? {}
 
-    const SVG = svg as React.FC<Icon>
+  // Todo: Fix re-render by adding a isActive methods within the main Icon component
+  const renderItem: ListRenderItem<string> = useCallback(
+    ({ item: id }) => {
+      const { svg, exec, isActive } = shortcutById(id)! ?? {}
 
-    return (
-      <TouchableOpacity
-        onPress={(e: GestureResponderEvent) => exec(store.dispatch, store.getState, e, { type: 'toolbar' })}
-        style={styles.button}
-      >
-        <SVG key={id} fill={fillColor} size={35} />
-      </TouchableOpacity>
-    )
-  }, [])
+      const isButtonActive = !isActive || isActive(store.getState)
+
+      const SVG = svg as React.FC<Icon>
+
+      return (
+        <TouchableOpacity
+          key={id}
+          onPress={(e: GestureResponderEvent) => exec(store.dispatch, store.getState, e, { type: 'toolbar' })}
+          style={styles.button}
+        >
+          <SVG fill={isButtonActive ? fillColor : 'grey'} size={35} />
+        </TouchableOpacity>
+      )
+    },
+    [wholeStore],
+  )
 
   /**********************************************************************
    * Render
