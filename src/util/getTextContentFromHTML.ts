194407b92c1529c1ae9ee62731459f7793f7ec17
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-13 14:11:51 +0200

    💩  temporary hack as shim createElement is causing to exceeded maximum calls

--- src/util/getTextContentFromHTML.ts
+++ src/util/getTextContentFromHTML.ts
@@ -1,7 +1,12 @@
+import { isMobile } from './isMobile'
+
 /**
  * Returns text without any html tags for the given HTML value. It accurately represents how user sees the text in the browser.
  */
 export const getTextContentFromHTML = (htmlValue: string) => {
+  // temporary hack as shim createElement is causing to exceeded maximum calls
+  if (isMobile()) return ''
+
   const dummyDiv = document.createElement('div')
   dummyDiv.innerHTML = htmlValue
 
