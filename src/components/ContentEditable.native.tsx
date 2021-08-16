6711aae2da9ed6abfae9fd1821a37fb715f1c9d5
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-16 17:10:02 +0200

    ⚡️  improve webview implementation

--- src/components/ContentEditable.native.tsx
+++ src/components/ContentEditable.native.tsx
@@ -38,18 +38,144 @@ enum WEBVIEW_POST_EVENTS {
   onFocus,
   onCopy,
 }
+
+interface IWebHTMLProps {
+  placeholder: string
+  innerHTML: string
+  isEditing?: boolean
+}
+
+/**
+ * Get html string.
+ */
+const getWebHTML = ({ placeholder, innerHTML, isEditing }: IWebHTMLProps) => `
+<html>
+<head>
+  <style>
+    * {
+      margin: 0;
+    }
+
+    #content:empty:before {
+      content: attr(placeholder);
+    }
+
+    #content {
+      font-size: 55px;
+      font-family: Arial;
+      background-color: #000;
+      color: #fff;
+      width: 100%;
+      height: 100%;
+      outline: none;
+    }
+  </style>
+</head>
+<script>
+  function getSelectionValues() {
+    const { anchorNode, focusNode, anchorOffset, focusOffset } = getSelection();
+
+    window.ReactNativeWebView.postMessage(JSON.stringify({ anchorNode, focusNode, anchorOffset, focusOffset }))
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const ele = document.getElementById("content");
+
+    // Todo: fix focus on new thought.
+  ${isEditing} && ele.focus();
+
+    // Get the placeholder attribute
+    const placeholder = ele.getAttribute("placeholder");
+
+    // Set the placeholder as initial content if it's empty
+    ele.innerHTML.trim() === "" && (ele.innerHTML = placeholder);
+
+    ele.addEventListener("focus", function (e) {
+      const value = e.target.innerHTML;
+      value === placeholder && (e.target.innerHTML = "");
+
+      window.ReactNativeWebView.postMessage(JSON.stringify({ event: e, eventType: ${WEBVIEW_POST_EVENTS.onFocus} }))
+    });
+
+    ele.addEventListener("blur", function (e) {
+      const value = e.target.innerHTML.trim();
+      value === "" && (e.target.innerHTML = placeholder);
+
+      window.ReactNativeWebView.postMessage(JSON.stringify({ event: e, text: value, eventType: ${
+        WEBVIEW_POST_EVENTS.onBlur
+      } }))
+    });
+
+    ele.addEventListener("keydown", function (e) {
+      const { keyCode, key } = e
+
+      window.ReactNativeWebView.postMessage(JSON.stringify({ event: { keyCode, key }, eventType: ${
+        WEBVIEW_POST_EVENTS.onKeyDown
+      } }))
+    });
+
+    ele.addEventListener("keyup", function (e) {
+      const text = e.target.innerHTML.trim()
+
+      window.ReactNativeWebView.postMessage(JSON.stringify({ event: text, eventType: ${WEBVIEW_POST_EVENTS.onChange} }))
+    });
+
+    ele.addEventListener("copy", function (e) {
+      const selectedText = getSelection().getRangeAt(0).toString();
+      e.clipboardData.setData("text/plain", selectedText);
+      const content = e.clipboardData.getData('text/plain');
+    });
+
+    ele.addEventListener("paste", function (e) {
+      const plainText = e.clipboardData.getData('text/plain')
+      const htmlText = e.clipboardData.getData('text/html')
+
+      e.preventDefault()
+      window.ReactNativeWebView.postMessage(JSON.stringify({
+        event: {
+          plainText,
+          htmlText,
+          innerHTML: ele?.innerHTML
+        },
+        eventType: ${WEBVIEW_POST_EVENTS.onPaste}
+      }))
+    });
+  });
+</script>
+
+<body>
+  <div
+    id="content"
+    contenteditable
+    placeholder="${placeholder}"
+  >
+    ${innerHTML || ''}
+  </div>
+</body>
+</html>
+  `
+
 /**
  * Content Editable Component.
  */
-const ContentEditable = ({ style, html, disabled, forceUpdate, ...props }: ContentEditableProps) => {
+const ContentEditable = ({
+  style,
+  html,
+  disabled,
+  forceUpdate,
+  placeholder,
+  isEditing,
+  ...props
+}: ContentEditableProps) => {
   const allowInnerHTMLChange = useRef<boolean>(true)
   const { width } = useDimensions().window
   const contentRef = useRef<WebView>(null)
 
-  const [value, setValue] = useState<string>()
+  const [webviewHTML, setWebviewHTML] = useState<string>(getWebHTML({ innerHTML: html, placeholder, isEditing }))
 
+  console.log({ html })
   useEffect(() => {
-    setValue(html)
+    // setValue(html)
   }, [])
 
   // eslint-disable-next-line jsdoc/require-jsdoc
@@ -60,116 +186,9 @@ const ContentEditable = ({ style, html, disabled, forceUpdate, ...props }: Conte
     props.onChange(e)
   }
 
-  if (value === undefined) return null
+  // if (value === undefined) return null
 
-  const _html = `
-  <html>
-  <head>
-    <style>
-      * {
-        margin: 0;
-      }
-
-      #content:empty:before {
-        content: attr(placeholder);
-      }
-
-      #content {
-        font-size: 55px;
-        font-family: Arial;
-        background-color: #000;
-        color: #fff;
-        width: 100%;
-        height: 100%;
-        outline: none;
-      }
-    </style>
-  </head>
-  <script>
-    function getSelectionValues() {
-      const { anchorNode, focusNode, anchorOffset, focusOffset } = getSelection();
-
-      window.ReactNativeWebView.postMessage(JSON.stringify({ anchorNode, focusNode, anchorOffset, focusOffset }))
-    }
-
-    document.addEventListener("DOMContentLoaded", function () {
-      const ele = document.getElementById("content");
-
-      // Todo: fix focus on new thought.
-      // ${props.isEditing} && ele.focus()
-
-      // Get the placeholder attribute
-      const placeholder = ele.getAttribute("placeholder");
-
-      // Set the placeholder as initial content if it's empty
-      ele.innerHTML.trim() === "" && (ele.innerHTML = placeholder);
-
-      ele.addEventListener("focus", function (e) {
-        const value = e.target.innerHTML;
-        value === placeholder && (e.target.innerHTML = "");
-
-        window.ReactNativeWebView.postMessage(JSON.stringify({ event: e, eventType: ${WEBVIEW_POST_EVENTS.onFocus} }))
-      });
-
-      ele.addEventListener("blur", function (e) {
-        const value = e.target.innerHTML.trim();
-        value === "" && (e.target.innerHTML = placeholder);
-
-        window.ReactNativeWebView.postMessage(JSON.stringify({ event: e, text: value, eventType: ${
-          WEBVIEW_POST_EVENTS.onBlur
-        } }))
-      });
-
-      ele.addEventListener("keydown", function (e) {
-        const { keyCode, key } = e
-
-        window.ReactNativeWebView.postMessage(JSON.stringify({ event: { keyCode, key }, eventType: ${
-          WEBVIEW_POST_EVENTS.onKeyDown
-        } }))
-      });
-
-      ele.addEventListener("keyup", function (e) {
-        const text = e.target.innerHTML.trim()
-
-        window.ReactNativeWebView.postMessage(JSON.stringify({ event: text, eventType: ${
-          WEBVIEW_POST_EVENTS.onChange
-        } }))
-      });
-
-      ele.addEventListener("copy", function (e) {
-        const selectedText = getSelection().getRangeAt(0).toString();
-        e.clipboardData.setData("text/plain", selectedText);
-        const content = e.clipboardData.getData('text/plain');
-      });
-
-      ele.addEventListener("paste", function (e) {
-        const plainText = e.clipboardData.getData('text/plain')
-        const htmlText = e.clipboardData.getData('text/html')
-
-        e.preventDefault()
-        window.ReactNativeWebView.postMessage(JSON.stringify({
-          event: {
-            plainText,
-            htmlText,
-            innerHTML: ele?.innerHTML
-          },
-          eventType: ${WEBVIEW_POST_EVENTS.onPaste}
-        }))
-      });
-    });
-  </script>
-
-  <body>
-    <div
-      id="content"
-      contenteditable
-      placeholder="${props.placeholder}"
-    >
-      ${value || ''}
-    </div>
-  </body>
-</html>
-    `
+  // const _html] =
 
   return (
     <View style={commonStyles.flexOne}>
@@ -177,7 +196,7 @@ const ContentEditable = ({ style, html, disabled, forceUpdate, ...props }: Conte
         ref={contentRef}
         originWhitelist={['*']}
         source={{
-          html: _html,
+          html: webviewHTML,
         }}
         hideKeyboardAccessoryView={true}
         domStorageEnabled={false}
@@ -195,7 +214,9 @@ const ContentEditable = ({ style, html, disabled, forceUpdate, ...props }: Conte
             }
 
             case WEBVIEW_POST_EVENTS.onBlur: {
-              setValue(html)
+              //  setValue(html)
+              //   console.log({ _html, value })
+              setWebviewHTML(getWebHTML({ innerHTML: html, placeholder }))
 
               if (props.onBlur) props.onBlur()
               break
