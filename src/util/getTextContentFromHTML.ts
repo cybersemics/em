24977db4d7e56b672306f66648670470e1978346
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-17 13:43:24 +0200

    ♻️  simplified getTextContentFromHTML method using Regex to retreive the text content from html tags.

--- src/util/getTextContentFromHTML.ts
+++ src/util/getTextContentFromHTML.ts
@@ -1,14 +1,4 @@
-import { isMobile } from './isMobile'
-
 /**
  * Returns text without any html tags for the given HTML value. It accurately represents how user sees the text in the browser.
  */
-export const getTextContentFromHTML = (htmlValue: string) => {
-  // temporary hack as shim createElement is causing to exceeded maximum calls
-  if (isMobile()) return ''
-
-  const dummyDiv = document.createElement('div')
-  dummyDiv.innerHTML = htmlValue
-
-  return dummyDiv.textContent!
-}
+export const getTextContentFromHTML = (htmlValue: string) => htmlValue.replace(/(<([^>]+)>)/gi, '')
