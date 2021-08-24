011b1fda2092d9c6a727a4e9d3d03379aa7b7082
Bijay Shrestha <sbijay777@gmail.com>
2021-08-24 16:45:05 +0545

    use previous method to get text from html context for web

--- src/util/getTextContentFromHTML.ts
+++ src/util/getTextContentFromHTML.ts
@@ -1,4 +1,12 @@
+import { isMobile } from './isMobile'
+
 /**
  * Returns text without any html tags for the given HTML value. It accurately represents how user sees the text in the browser.
  */
-export const getTextContentFromHTML = (htmlValue: string) => htmlValue.replace(/(<([^>]+)>)/gi, '')
+export const getTextContentFromHTML = (htmlValue: string) => {
+  if (isMobile()) return htmlValue.replace(/(<([^>]+)>)/gi, '')
+  const dummyDiv = document.createElement('div')
+  dummyDiv.innerHTML = htmlValue
+
+  return dummyDiv.textContent!
+}
