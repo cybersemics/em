848bb5d1951ee1192433cd2f912e4da5ec76ca82
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-24 15:51:38 +0200

    [React-Native]: content editable in webview (#1398)

--- src/util/getTextContentFromHTML.ts
+++ src/util/getTextContentFromHTML.ts
@@ -1,7 +1,10 @@
+import { isMobile } from './isMobile'
+
 /**
  * Returns text without any html tags for the given HTML value. It accurately represents how user sees the text in the browser.
  */
 export const getTextContentFromHTML = (htmlValue: string) => {
+  if (isMobile()) return htmlValue.replace(/(<([^>]+)>)/gi, '')
   const dummyDiv = document.createElement('div')
   dummyDiv.innerHTML = htmlValue
 
