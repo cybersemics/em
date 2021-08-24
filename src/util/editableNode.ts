ec40ffb4a9b33dd421ec372ebfce044441e6431e
Bruno Kiafuka <brunokiafuka@live.com.pt>
2021-08-24 15:56:21 +0200

    fix editable node to support RN.

--- src/util/editableNode.ts
+++ src/util/editableNode.ts
@@ -1,8 +1,11 @@
 import { hashContext, headRank, pathToContext } from '../util'
 import { Path } from '../@types'
+import { isMobile } from './isMobile'
 
 /** Returns the editable DOM node of the given thoughts. */
 export const editableNode = (path: Path): HTMLElement | null => {
+  if (isMobile()) return null
+
   const rank = headRank(path)
   // also selects dividers
   return (
