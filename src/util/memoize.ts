1ad2c450ab19a0d88eae32f9623a66c499eeab28
Raine Revere <raine@cybersemics.org>
2021-08-28 06:18:12 -0600

    Use import statement with proxy-memoize.

--- src/util/memoize.ts
+++ src/util/memoize.ts
@@ -1,6 +1,4 @@
-// cannot use import statement because proxy-memoize exports do not play nice with webpack
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const proxyMemoize = require('proxy-memoize').default
+import proxyMemoize from 'proxy-memoize'
 
 /** Memoizes a function with any number of arguments. Wraps proxy-memoize. Disables WeakMap so size is required (default: 1000). */
 export const memoize = <T extends any[], R>(f: (...args: T) => R, { size }: { size: number }): ((...args: T) => R) => {
