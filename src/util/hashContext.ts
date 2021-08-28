97a56e61b1600d9f10395c3d8775e425824e5bda
Raine Revere <raine@cybersemics.org>
2021-08-28 09:15:47 -0600

    Memoize hashContext

--- src/util/hashContext.ts
+++ src/util/hashContext.ts
@@ -1,6 +1,6 @@
 import * as murmurHash3 from 'murmurhash3js'
+import moize from 'moize'
 import globals from '../globals'
-import { ID } from '../constants'
 
 // util
 import { escapeSelector } from './escapeSelector'
@@ -9,9 +9,20 @@ import { normalizeThought } from './normalizeThought'
 
 const SEPARATOR_TOKEN = '__SEP__'
 
-/** Encode the thoughts (and optionally rank) as a string. */
-export const hashContext = (thoughts: Context, rank?: number): ContextHash =>
-  (globals.disableThoughtHashing ? ID : murmurHash3.x64.hash128)(
-    thoughts.map(thought => (thought ? escapeSelector(normalizeThought(thought)) : '')).join(SEPARATOR_TOKEN) +
+/** Encodes the context as a string with no hashing. Useful for debugging purposes. */
+const encodePlainContext = (context: Context, rank?: number): ContextHash =>
+  (context.map(thought => (thought ? escapeSelector(normalizeThought(thought)) : '')).join(SEPARATOR_TOKEN) +
+    (typeof rank === 'number' ? SEPARATOR_TOKEN + rank : '')) as ContextHash
+
+/** Encodes the context (and optionally rank) as a string using MurmurHash. */
+const encodeHashedContext = (context: Context, rank?: number): ContextHash => {
+  return murmurHash3.x64.hash128(
+    context.map(thought => (thought ? escapeSelector(normalizeThought(thought)) : '')).join(SEPARATOR_TOKEN) +
       (typeof rank === 'number' ? SEPARATOR_TOKEN + rank : ''),
   ) as ContextHash
+}
+
+/** Returns a hashContext function with or without hashing at compile-time for performance. */
+const hashContextFunction = globals.disableThoughtHashing ? encodePlainContext : encodeHashedContext
+
+export const hashContext = moize(hashContextFunction, { isDeepEqual: true, maxSize: 10000 })
