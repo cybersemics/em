fefda5560662b2f9f5eb00439ff6d92ef3c805a8
Raine Revere <raine@cybersemics.org>
2021-09-10 11:13:25 -0600

    Move selectNextEditable from util to device.

--- src/components/Note.native.tsx
+++ src/components/Note.native.tsx
@@ -4,9 +4,10 @@ import { isTouch } from '../browser'
 import { store } from '../store'
 import { attribute, getParent, isContextViewActive } from '../selectors'
 import { deleteAttribute, editing, setAttribute, setNoteFocus } from '../action-creators'
-import { selectNextEditable, setSelection, strip } from '../util'
+import { setSelection, strip } from '../util'
 import ContentEditable, { ContentEditableEvent, IKeyDown } from './ContentEditable.native'
 import asyncFocus from '../device/asyncFocus'
+import selectNextEditable from '../device/selectNextEditable'
 import { Context, State } from '../@types'
 
 interface NoteProps {
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -4,9 +4,10 @@ import { isTouch } from '../browser'
 import { store } from '../store'
 import { attribute, getParent, isContextViewActive } from '../selectors'
 import { deleteAttribute, editing, setAttribute, setNoteFocus } from '../action-creators'
-import { selectNextEditable, setSelection, strip } from '../util'
+import { setSelection, strip } from '../util'
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable'
 import asyncFocus from '../device/asyncFocus'
+import selectNextEditable from '../device/selectNextEditable'
 import { Context, State } from '../@types'
 
 interface NoteProps {
