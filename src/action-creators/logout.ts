59306ea3eb8bfe6327cdbcd9fbf723b6b721f1c7
Ruchi Jain <ruchijn@outlook.com>
2021-09-10 06:22:46 -0400

    Syncing up the tabs when user logs out from one of them.

--- src/action-creators/logout.ts
+++ src/action-creators/logout.ts
@@ -1,38 +1,12 @@
-import { clearAll } from '../data-providers/dexie'
-import { never } from '../util'
-import { clear, importText } from '../action-creators'
-import { EM_TOKEN, INITIAL_SETTINGS } from '../constants'
+import { resetSettings } from '../action-creators'
 import { Thunk } from '../@types'
-import { storage } from '../util/storage'
 
 /** Logs the user out of Firebase and clears the state. */
 const logout = (): Thunk => dispatch => {
   // sign out first to prevent updates to remote
   window.firebase.auth().signOut()
 
-  // clear local db
-  clearAll().catch(err => {
-    throw new Error(err)
-  })
-
-  // clear autologin
-  storage.clear()
-
-  // clear state variables
-  dispatch(clear())
-
-  // reset initial settings
-  dispatch(
-    importText({
-      path: [{ value: EM_TOKEN, rank: 0 }],
-      text: INITIAL_SETTINGS,
-      lastUpdated: never(),
-      preventSetCursor: true,
-    }),
-  )
-
-  // scroll to top
-  window.scrollTo(0, 0)
+  resetSettings()
 }
 
 export default logout
