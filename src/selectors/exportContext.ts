cybersemics/em

commit 712e5e0ac60f5a733b08380d96e494072032328d

ModalExport: Move markdown formatting from mime type to advanced option. Fixes #1460.

src/selectors/exportContext.ts
@@ -20,24 +20,21 @@ const replaceTitle = (text: string, title: string, format: MimeType) => {
 
 interface Options {
   indent?: number
+  // replaces the value of the root thought with a new title.
   title?: string
+  excludeMarkdownFormatting?: boolean
   excludeSrc?: boolean
   excludeMeta?: boolean
   depth?: number
   excludeArchived?: boolean
 }
 
-/** Exports the navigable subtree of the given context.
- *
- * @param context
- * @param format
- * @param title     Replace the value of the root thought with a new title.
- */
+/** Exports the navigable subtree of the given context. */
 export const exportContext = (
   state: State,
   context: Context,
   format: MimeType = 'text/html',
-  { indent = 0, title, excludeSrc, excludeMeta, depth = 0, excludeArchived }: Options = {},
+  { indent = 0, title, excludeMarkdownFormatting, excludeMeta, excludeSrc, depth = 0, excludeArchived }: Options = {},
 ): string => {
   const linePostfix = format === 'text/html' ? (indent === 0 ? '  ' : '') + '</li>' : ''
   const tab0 = Array(indent).fill('').join('  ')
@@ -66,6 +63,7 @@ export const exportContext = (
       excludeSrc,
       excludeMeta,
       excludeArchived,
+      excludeMarkdownFormatting,
       indent: indent + (isNoteAndMetaExcluded ? 0 : format === 'text/html' ? (indent === 0 ? 3 : 2) : 1),
       depth: depth + 1,
     })
@@ -88,8 +86,10 @@ export const exportContext = (
   const value = format === 'text/plain' ? currentThought.replace(REGEXP_TAGS, '') : currentThought
 
   const markdownText: string =
-    format === 'text/markdown' && !isHome([currentThought]) && !currentThought.includes('=')
-      ? turndownService.turndown(currentThought)
+    format === 'text/plain' && !excludeMarkdownFormatting && !isHome([currentThought]) && !currentThought.includes('=')
+      ? // turndown will remove newlines as described here: https://github.com/mixmark-io/turndown/issues/264
+        // replace newlines with a dummy token before converting, then replace them back to newlines
+        turndownService.turndown(currentThought.replace(/\n/g, '--EM-NEWLINE--')).replace(/--EM-NEWLINE--/g, '\n')
       : value
 
   // Handle newlines in thoughts.

src/selectors/__tests__/exportContext.ts
@@ -90,7 +90,7 @@ it('meta is excluded but archived is included', () => {
   - b`)
 })
 
-it('exported as plain text', () => {
+it('exported as plain text with no formatting', () => {
   const text = `- a
   - Hello <b>world</b>`
 
@@ -99,7 +99,7 @@ it('exported as plain text', () => {
   // run steps through reducer flow and export as plaintext for readable test
   const stateNew = reducerFlow(steps)(initialState())
 
-  const exported = exportContext(stateNew, ['a'], 'text/plain', { excludeMeta: true })
+  const exported = exportContext(stateNew, ['a'], 'text/plain', { excludeMeta: true, excludeMarkdownFormatting: true })
 
   expect(exported).toBe(`- a
   - Hello world`)
@@ -156,7 +156,7 @@ it('export as markdown', () => {
   const steps = [importText({ text }), setCursorFirstMatch([text])]
 
   const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [text], 'text/markdown')
+  const exported = exportContext(stateNew, [text], 'text/plain')
 
   expect(exported).toBe(`Hello **wor*ld***`)
 })
@@ -168,7 +168,7 @@ it('export as markdown without escaping metaprogramming attributes', () => {
   const steps = [importText({ text }), setCursorFirstMatch([text])]
 
   const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, ['Hello <b>wor<i>ld</i></b>'], 'text/markdown')
+  const exported = exportContext(stateNew, ['Hello <b>wor<i>ld</i></b>'], 'text/plain')
 
   expect(exported).toBe(`- Hello **wor*ld***
   - =readonly`)
@@ -195,7 +195,7 @@ it('export as plain and markdown text replacing html tags only from thoughts and
   - />
   - c`)
 
-  const exportedMarkdown = exportContext(stateNew, [HOME_TOKEN], 'text/markdown')
+  const exportedMarkdown = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
   expect(exportedMarkdown).toBe(`- ${HOME_TOKEN}
   - a

src/@types/MimeType.ts
@@ -1,2 +1,2 @@
 /** Set of file types supported for exporting thoughts. */
-export type MimeType = 'text/plain' | 'text/html' | 'text/markdown'
+export type MimeType = 'text/plain' | 'text/html'

src/components/ModalExport.tsx
@@ -125,7 +125,6 @@ interface AdvancedSetting {
 const exportOptions: ExportOption[] = [
   { type: 'text/plain', label: 'Plain Text', extension: 'txt' },
   { type: 'text/html', label: 'HTML', extension: 'html' },
-  { type: 'text/markdown', label: 'Markdown', extension: 'md' },
 ]
 
 /******************************************************************************
@@ -233,6 +232,7 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
   const [exportContent, setExportContent] = useState<string | null>(null)
   const [shouldIncludeMetaAttributes, setShouldIncludeMetaAttributes] = useState(true)
   const [shouldIncludeArchived, setShouldIncludeArchived] = useState(true)
+  const [shouldIncludeMarkdownFormatting, setShouldIncludeMarkdownFormatting] = useState(true)
   const [selected, setSelected] = useState(exportOptions[0])
   const [numDescendantsInState, setNumDescendantsInState] = useState<number | null>(null)
 
@@ -264,6 +264,7 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
       title: titleChild ? titleChild.value : undefined,
       excludeMeta: !shouldIncludeMetaAttributes,
       excludeArchived: !shouldIncludeArchived,
+      excludeMarkdownFormatting: !shouldIncludeMarkdownFormatting,
     })
 
     setExportContent(titleChild ? exported : removeHome(exported).trimStart())
@@ -278,7 +279,7 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
     if (!shouldIncludeMetaAttributes) setShouldIncludeArchived(false)
 
     // when exporting HTML, we have to do a full traversal since the numDescendants heuristic of counting the number of lines in the exported content does not work
-    if (selected.type === 'text/html' || selected.type === 'text/markdown') {
+    if (selected.type === 'text/html') {
       setNumDescendantsInState(
         getDescendantThoughtIds(state, head(simplePath), {
           filterFunction: and(
@@ -292,7 +293,7 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
     if (!isPulling) {
       setExportContentFromCursor()
     }
-  }, [selected, shouldIncludeMetaAttributes, shouldIncludeArchived])
+  }, [selected, shouldIncludeMetaAttributes, shouldIncludeArchived, shouldIncludeMarkdownFormatting])
 
   useEffect(() => {
     const clipboard = new ClipboardJS('.copy-clipboard-btn')
@@ -361,6 +362,7 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
       excludeSrc: true,
       excludeMeta: !shouldIncludeMetaAttributes,
       excludeArchived: !shouldIncludeArchived,
+      excludeMarkdownFormatting: !shouldIncludeMarkdownFormatting,
       title: titleChild ? titleChild.value : undefined,
     })
 
@@ -394,24 +396,36 @@ const ModalExport: FC<{ context: Context; simplePath: SimplePath; cursor: Path }
   /** Updates archived checkbox value when clicked and set the appropriate value in the selected option. */
   const onChangeArchivedCheckbox = () => setShouldIncludeArchived(!shouldIncludeArchived)
 
+  /** Updates archived checkbox value when clicked and set the appropriate value in the selected option. */
+  const onChangeFormattingCheckbox = () => setShouldIncludeMarkdownFormatting(!shouldIncludeMarkdownFormatting)
+
   /** Created an array of objects so that we can just add object here to get multiple checkbox options created. */
   const advancedSettingsArray: AdvancedSetting[] = [
     {
-      id: 'lossless-checkbox',
+      id: 'lossless',
       onChangeFunc: onChangeLosslessCheckbox,
       defaultChecked: true,
       checked: shouldIncludeMetaAttributes,
       title: 'Lossless',
       description:
-        'When checked, include all metaprogramming attributes such as archived thoughts, pins, table view, etc. Check this option for a backup-quality export that can be re-imported with no data loss. Uncheck this option for social sharing or exporting to platforms that do not support em metaprogramming attributes. Which is, uh, all of them.',
+        'When checked, include all metaprogramming attributes such as archived thoughts, pins, table view, etc. Check this option for a backup-quality export that can be re-imported with no data loss. Uncheck this option for social sharing or public display. ',
     },
     {
-      id: 'archived-checkbox',
+      id: 'archived',
       onChangeFunc: onChangeArchivedCheckbox,
       defaultChecked: true,
       checked: shouldIncludeArchived,
       title: 'Archived',
       description: 'When checked, the exported thoughts include archived thoughts.',
+    },
+    {
+      id: 'formatting',
+      onChangeFunc: onChangeFormattingCheckbox,
+      defaultChecked: true,
+      checked: shouldIncludeMarkdownFormatting,
+      title: 'Formatting Characters',
+      description:
+        'Include **double asteriskss** for bold and *single asterisks* for italics. If unchecked, formatting will be lost.',
     },
   ]
 

commit 7e9b69998839efb450043569efa9ffdc542b61d2

Fix markdown export regression from 712e5e0ac6.

src/selectors/exportContext.ts
@@ -1,12 +1,8 @@
 import { attribute, getChildrenRanked } from '../selectors'
-import { head, isFunction, unroot, isHome } from '../util'
+import { head, isFunction, unroot } from '../util'
 import { Context, MimeType, Parent, State } from '../@types'
 import { REGEXP_TAGS } from '../constants'
 import { and } from 'fp-and-or'
-import TurndownService from 'turndown'
-
-// Default emDelimiter is _ hence setting this option is needed to overwrite it to *
-const turndownService = new TurndownService({ emDelimiter: '*' })
 
 /** Replaces the root value with a given title. */
 const replaceTitle = (text: string, title: string, format: MimeType) => {
@@ -17,6 +13,9 @@ const replaceTitle = (text: string, title: string, format: MimeType) => {
     ? `- ${title}${text.slice(text.indexOf('\n'))}`
     : text
 }
+
+/** Convert formatting HTML tags to markdown asterisks. */
+const formattingTagsToMarkdown = (s: string) => s.replace(/(<\/?(b|strong)>)/gi, '**').replace(/(<\/?(i|em)>)/gi, '*')
 
 interface Options {
   indent?: number
@@ -83,19 +82,15 @@ export const exportContext = (
   // Get the thought under process
   // If it is root, then do not convert it to markdown
   const currentThought = head(context)
-  const value = format === 'text/plain' ? currentThought.replace(REGEXP_TAGS, '') : currentThought
-
-  const markdownText: string =
-    format === 'text/plain' && !excludeMarkdownFormatting && !isHome([currentThought]) && !currentThought.includes('=')
-      ? // turndown will remove newlines as described here: https://github.com/mixmark-io/turndown/issues/264
-        // replace newlines with a dummy token before converting, then replace them back to newlines
-        turndownService.turndown(currentThought.replace(/\n/g, '--EM-NEWLINE--')).replace(/--EM-NEWLINE--/g, '\n')
-      : value
+  const value =
+    format === 'text/html'
+      ? currentThought
+      : (excludeMarkdownFormatting ? currentThought : formattingTagsToMarkdown(currentThought)).replace(REGEXP_TAGS, '')
 
   // Handle newlines in thoughts.
   // This should never happen (newlines are converted to separate thoughts on import) but guard against newlines just in case.
   // Otherwise re-importing is disastrous (additional lines of text in a thought are moved to the root).
-  const lines = markdownText.split('\n')
+  const lines = value.split('\n')
   const firstLine = `${tab0}${linePrefix}${lines[0]}`
   const otherLines = lines
     .slice(1)

src/util/__tests__/importHtml.ts
@@ -9,7 +9,9 @@ import { Path } from '../../@types'
 const importExport = (html: string, isHTML = true) => {
   const state = initialState()
   const stateNew = importText(state, { path: HOME_PATH, text: html })
-  const exported = exportContext(stateNew, [HOME_TOKEN], isHTML ? 'text/html' : 'text/plain')
+  const exported = exportContext(stateNew, [HOME_TOKEN], isHTML ? 'text/html' : 'text/plain', {
+    excludeMarkdownFormatting: true,
+  })
 
   // remove root, de-indent (trim), and append newline to make tests cleaner
   return removeHome(exported)