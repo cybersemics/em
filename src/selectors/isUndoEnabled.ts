db3b59be04947c2c43b5e7c44f523dfbfa2e8ba7
Pravat Pandey <pandeypravat@gmail.com>
2022-01-31 08:46:05 +0545

    updated isUndoEnabled to check for falsy value

--- src/selectors/isUndoEnabled.ts
+++ src/selectors/isUndoEnabled.ts
@@ -2,4 +2,4 @@ import { State } from '../@types'
 import { getLatestActionType } from '../util/getLastActionType'
 
 /** Determines if undo is enabled. */
-export const isUndoEnabled = (state: State) => getLatestActionType(state.inversePatches) !== undefined
+export const isUndoEnabled = (state: State) => !!getLatestActionType(state.inversePatches)
