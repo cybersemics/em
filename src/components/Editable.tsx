a96d59462a9c95def2e20503df77af414a91977a
Ankit Karna <ankitkarna99@gmail.com>
2022-02-04 11:33:21 -0500

    Editing dispatch condition

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -250,7 +250,7 @@ const Editable = ({
   // side effect to set old value ref to head value from updated simplePath. Also update editing value, if it is different from current value.
   useEffect(() => {
     oldValueRef.current = value
-    if (state.editingValue !== value) {
+    if (isEditing && selection.isThought() && state.editingValue !== value) {
       dispatch(setEditingValue(value))
     }
   }, [value])
