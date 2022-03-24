6c4b90cf579e60c97bb41bf9d86b02d2df8b6e76
Karun Shrestha <karunkop@gmail.com>
2022-03-24 14:22:16 +1100

    Commented out set logic instead of removing it

--- src/device/selection.ts
+++ src/device/selection.ts
@@ -231,6 +231,14 @@ export const set = (
   const range = document.createRange()
   const sel = window.getSelection() || new Selection()
 
+  // Commenting this as of now because the addition of this logic is unknown.
+
+  // bail if already selected
+  // compare closest element node, since there is no need to update the selection if it is on either the text node or element node
+  // const focusElementNew = focusNode.nodeType === Node.TEXT_NODE ? focusNode.parentElement : focusNode
+  // const focusElementOld = sel.focusNode?.nodeType === Node.TEXT_NODE ? sel.focusNode.parentElement : sel.focusNode
+  // if (focusElementNew === focusElementOld) return
+
   // automatically constrain offset to text length
   // this may still throw an error if the text node does no exist any longer
   if (focusNode !== null) {
