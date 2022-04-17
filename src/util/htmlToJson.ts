697319aee8ee05d4b3e54a2503cadc82742647b9
Karun Shrestha <karunkop@gmail.com>
2022-04-17 20:44:27 +0545

    removed logs

--- src/util/htmlToJson.ts
+++ src/util/htmlToJson.ts
@@ -195,10 +195,7 @@ const himalayaToBlock = (nodes: HimalayaNode[]): Block | Block[] => {
 
   // retrieve first chunk, if the first element is Block and the second is Block[], join children (Block[]) with parent (Block), else return blocks as is.
   const [first, rest] = blocks
-  console.log('rest :', rest)
-  console.log('first :', first)
   const result = !Array.isArray(first) && Array.isArray(rest) ? joinChildren(blocks as Block[]) : (blocks as Block[])
-  console.log('result :', result)
 
   return result
 }
