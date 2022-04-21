044ab859726e7d3329dfd341b8fa5fbd15682577
Karun Shrestha <40414929+karunkop@users.noreply.github.com>
2022-04-21 22:55:01 +1000

    importJSON: Error importing nested list (#1549)

--- src/util/htmlToJson.ts
+++ src/util/htmlToJson.ts
@@ -120,11 +120,11 @@ const handleBr = (nodes: HimalayaNode[], brIndex: number): HimalayaNode[] => {
 }
 
 /** Append children to parent as children property if it's necessary. */
-const joinChildren = (nodes: Block[]) => {
+const joinChildren = (nodes: (Block | Block[])[]) => {
   // split by chunk with size of 2, first element in chunk is Block - parent, the second is Block[] - children
   const chunks = _.chunk(nodes, 2)
   const parentsWithChildren = chunks.map(chunk =>
-    chunk.reduce((accum, node, index) => {
+    chunk.flat().reduce((accum, node, index) => {
       if (index === 0) return node
       return {
         ...accum,
@@ -195,7 +195,7 @@ const himalayaToBlock = (nodes: HimalayaNode[]): Block | Block[] => {
 
   // retrieve first chunk, if the first element is Block and the second is Block[], join children (Block[]) with parent (Block), else return blocks as is.
   const [first, rest] = blocks
-  const result = !Array.isArray(first) && Array.isArray(rest) ? joinChildren(blocks as Block[]) : (blocks as Block[])
+  const result = !Array.isArray(first) && Array.isArray(rest) ? joinChildren(blocks) : (blocks as Block[])
 
   return result
 }
