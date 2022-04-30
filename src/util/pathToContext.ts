3f9fe0389d2d9a3a2b40d83f35ec8a08482476c8
Raine Revere <raine@cybersemics.org>
2022-04-30 08:53:01 -0600

    pathToContext: Improve error message.

--- src/util/pathToContext.ts
+++ src/util/pathToContext.ts
@@ -3,8 +3,8 @@ import { getThoughtById } from '../selectors'
 
 /** Converts a Path to a Context. */
 export const pathToContext = (state: State, path: Path): Context =>
-  path.map(child => {
-    const thought = getThoughtById(state, child)
-    if (!thought) throw Error('pathToContext: ')
+  path.map(id => {
+    const thought = getThoughtById(state, id)
+    if (!thought) throw Error('pathToContext: Missing thought with id ' + id)
     return thought.value
   })
