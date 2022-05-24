fb490466e9d55f400adb2f8f6cbcc206a424d3c3
Raine Revere <raine@cybersemics.org>
2022-05-24 10:24:17 -0600

    childIdsToThoughts: Avoid extra filter loop.

--- src/selectors/childIdsToThoughts.ts
+++ src/selectors/childIdsToThoughts.ts
@@ -2,7 +2,17 @@ import { getThoughtById } from './index'
 import { Thought, ThoughtId, State } from '../@types'
 
 /** Converts a list of ThoughtIds to a list of Thoughts. May return a smaller list if any thoughts are missing. */
-const childIdsToThoughts = (state: State, childIds: ThoughtId[]): Thought[] =>
-  childIds.map(id => getThoughtById(state, id)).filter(Boolean)
+const childIdsToThoughts = (state: State, childIds: ThoughtId[]): Thought[] => {
+  const thoughts = []
+  // eslint-disable-next-line fp/no-loops
+  for (let i = 0; i < childIds.length; i++) {
+    const thought = getThoughtById(state, childIds[i])
+    if (thought) {
+      // eslint-disable-next-line fp/no-mutating-methods
+      thoughts.push(thought)
+    }
+  }
+  return thoughts
+}
 
 export default childIdsToThoughts
