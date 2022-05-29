0d92a9434b482f50fafba54ca66d628af3d67c92
Raine Revere <raine@cybersemics.org>
2022-05-29 06:57:45 -0600

    findDescendant: Optimize attribute lookups with childrenMap.

--- src/selectors/attribute.ts
+++ src/selectors/attribute.ts
@@ -2,13 +2,12 @@ import { findDescendant, isChildVisible } from '../selectors'
 import { getAllChildrenAsThoughts } from './getChildren'
 import { State, ThoughtId } from '../@types'
 
-/** Returns the value of an attribute of the given context. O(children). Use attributeEquals for O(1) attribute equality check. Use a combination of hasChild and attribute to check if an attribute exists but has no children. */
+/** Returns the value of a child attribute of a thought. Use a combination of hasChild and attribute to check if an attribute exists but has no children. O(1). */
 const attribute = (state: State, thoughtId: ThoughtId | null, attributeName: string) => {
   const attributeId = findDescendant(state, thoughtId, attributeName)
-  if (!attributeId) return null
   const attributeChildren = getAllChildrenAsThoughts(state, attributeId)
   const firstVisibleChild = attributeChildren.find(isChildVisible(state))
-  return firstVisibleChild ? firstVisibleChild.value : null
+  return firstVisibleChild?.value || null
 }
 
 export default attribute
