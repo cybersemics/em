f85a02bc7be3cf0fea133078a9a6f486563b47c2
Raine Revere <raine@cybersemics.org>
2022-05-29 19:54:08 -0600

    Add script to convert schema v5 to v6 (childrenMap).

---
+++ scripts/schema5to6.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+
+/** Splits the thoughtIndex and lexemeIndex of a large db into two files. */
+const file = process.argv[2]
+if (!file) {
+  console.error('Usage: node split-db.js db.json')
+  process.exit(1)
+}
+
+const isFunction = value => value.startsWith('=')
+
+const thoughts = JSON.parse(fs.readFileSync(file, 'utf8'))
+Object.values(thoughts.thoughtIndex).forEach(thought => {
+  thought.childrenMap = thought.children.reduce((accum, childId) => {
+    const child = thoughts.thoughtIndex[childId]
+    if (!child) {
+      throw new Error('Missing child with id ' + childId)
+    }
+    // Firebase keys cannot contain [.$#[\]] or ASCII control characters 0-31 or 127
+    // https://firebase.google.com/docs/database/web/structure-data?authuser=1&hl=en#section-limitations
+    const key = isFunction(child.value) && !accum[child.value] ? child.value.replace(/[.$#[\]]/g, '-') : child.id
+    if (!key || key.match(/[.$#[\]]/)) {
+      console.error('child', child)
+      throw new Error(`Invalid key "${key}" for ${child.id}.`)
+    }
+    return {
+      ...accum,
+      // if there is a duplicate child attribute, key it by its id so that it is not overwritten
+      [key]: child.id,
+    }
+  }, {})
+  delete thought.children
+})
+
+const base = path.basename(file).slice(0, -5)
+fs.writeFileSync(`${path.dirname(file)}/${base}-v6.json`, JSON.stringify(thoughts, null, 2))
