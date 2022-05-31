a62528eed32e7565ef9bde89603881acd4a9f168
Raine Revere <raine@cybersemics.org>
2022-05-31 05:41:01 -0600

    Add split-db script.

---
+++ scripts/split-db.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+/** Splits the thoughtIndex and lexemeIndex of a large db into two files. */
+const file = process.argv[2]
+if (!file) {
+  console.error('Usage: node split-db.js db.json')
+  process.exit(1)
+}
+
+const thoughts = JSON.parse(fs.readFileSync(file, 'utf8'))
+const base = path.basename(file).slice(0, -5)
+fs.writeFileSync(`${path.dirname(file)}/${base}-thoughtIndex.json`, JSON.stringify(thoughts.thoughtIndex))
+fs.writeFileSync(`${path.dirname(file)}/${base}-lexemeIndex.json`, JSON.stringify(thoughts.lexemeIndex))
