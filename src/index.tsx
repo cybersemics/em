8d3cd22dbea5693771667892af9ed4d6455b1ab4
Raine Revere <raine@cybersemics.org>
2022-07-27 14:39:07 -0600

    Remove service worker since it is broken anyway (#1646).

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom'
 import { App } from './components/App'
 import './index.css'
 import { initialize } from './initialize'
-import registerServiceWorker from './registerServiceWorker'
 
 initialize()
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
