5791671426794be8bc37f9ace9e470b5f6362510
Raine Revere <raine@cybersemics.org>
2022-07-28 06:53:52 -0600

    Add service-worker from CRA v5 (#1646).
    Reverts and upgrades 8d3cd22dbe.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 import { App } from './components/App'
 import './index.css'
 import { initialize } from './initialize'
+import { register } from './serviceWorkerRegistration'
 
 initialize()
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+register()
