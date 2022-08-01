9b9b428727e73dc74f98b285251cfc41abf608cf
Raine Revere <raine@cybersemics.org>
2022-08-01 16:13:42 -0600

    Alert: Add support for children elements.

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Alert from '../@types/Alert'
@@ -6,11 +6,16 @@ import State from '../@types/State'
 import alertActionCreator from '../action-creators/alert'
 import useSwipeToDismiss from '../hooks/useSwipeToDismiss'
 
+interface AlertProps {
+  alert: NonNullable<Alert>
+  onClose: () => void
+}
+
 // eslint-disable-next-line jsdoc/require-jsdoc
 const mapStateToProps = ({ alert }: State) => ({ alert })
 
 /** An alert component with an optional closeLink that fades in and out. */
-const AlertWithTransition = ({ alert }: ReturnType<typeof mapStateToProps>) => {
+const AlertWithTransition: FC<{ alert?: Alert }> = ({ alert, children }) => {
   const [isDismissed, setDismiss] = useState(false)
   const dispatch = useDispatch()
 
@@ -26,7 +31,9 @@ const AlertWithTransition = ({ alert }: ReturnType<typeof mapStateToProps>) => {
       {alert ? (
         <CSSTransition key={0} timeout={800} classNames='fade' onEntering={() => setDismiss(false)}>
           {/* Specify a key to force the component to re-render and thus recalculate useSwipeToDismissProps when the alert changes. Otherwise the alert gets stuck off screen in the dismiss state. */}
-          <AlertComponent alert={alert} onClose={onClose} key={alert.value} />
+          <AlertComponent alert={alert} onClose={onClose} key={alert.value}>
+            {children}
+          </AlertComponent>
         </CSSTransition>
       ) : null}
     </TransitionGroup>
@@ -34,7 +41,7 @@ const AlertWithTransition = ({ alert }: ReturnType<typeof mapStateToProps>) => {
 }
 
 /** The alert component itself. Separate so that a key property can be used to force a reset of useSwipeToDismissProps. */
-const AlertComponent = ({ alert, onClose }: { alert: NonNullable<Alert>; onClose: () => void }) => {
+const AlertComponent: FC<AlertProps> = ({ alert, onClose, children }) => {
   const dispatch = useDispatch()
   const useSwipeToDismissProps = useSwipeToDismiss({
     // dismiss after animation is complete to avoid touch events going to the Toolbar
@@ -45,7 +52,9 @@ const AlertComponent = ({ alert, onClose }: { alert: NonNullable<Alert>; onClose
 
   return (
     <div className={alert.isInline ? 'alert alert-inline' : 'alert'} {...useSwipeToDismissProps}>
-      <span className='alert-text' dangerouslySetInnerHTML={{ __html: alert.value || '' }} />
+      <span className='alert-text' dangerouslySetInnerHTML={!children ? { __html: alert.value || '' } : undefined}>
+        {children}
+      </span>
       {alert.showCloseLink ? (
         <a className='upper-right status-close-x text-small' onClick={onClose}>
           ✕
