04e79aae2be67d38ec182898fa420ea72e9e0df3
Raine Revere <raine@cybersemics.org>
2022-08-06 05:35:21 -0600

    visibleDistanceAboveCursor: minor

--- src/selectors/visibleDistanceAboveCursor.ts
+++ src/selectors/visibleDistanceAboveCursor.ts
@@ -1,12 +1,12 @@
 import State from '../@types/State'
 import { HOME_TOKEN, MAX_DISTANCE_FROM_CURSOR } from '../constants'
-import { getChildren } from '../selectors/getChildren'
+import { hasChildren } from '../selectors/getChildren'
 import head from '../util/head'
 
 /**
  * Distance between the first visible thought nearest to the root and the cursor.
  */
 const visibleDistanceAboveCursor = (state: State) =>
-  MAX_DISTANCE_FROM_CURSOR - (getChildren(state, state.cursor ? head(state.cursor) : HOME_TOKEN).length === 0 ? 1 : 2)
+  MAX_DISTANCE_FROM_CURSOR - (hasChildren(state, state.cursor ? head(state.cursor) : HOME_TOKEN) ? 2 : 1)
 
 export default visibleDistanceAboveCursor
