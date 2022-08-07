e458cc9b8f1741515d148b259fdc095c1a45bb53
Raine Revere <raine@cybersemics.org>
2022-08-07 08:08:19 -0600

    setAttribute: refactor

--- src/reducers/setAttribute.ts
+++ src/reducers/setAttribute.ts
@@ -3,7 +3,7 @@ import Path from '../@types/Path'
 import State from '../@types/State'
 import createThought from '../reducers/createThought'
 import setFirstSubthought from '../reducers/setFirstSubthought'
-import { getAllChildrenAsThoughts } from '../selectors/getChildren'
+import findDescendant from '../selectors/findDescendant'
 import getPrevRank from '../selectors/getPrevRank'
 import createId from '../util/createId'
 import head from '../util/head'
@@ -13,23 +13,26 @@ import unroot from '../util/unroot'
 /** Sets an attribute on the given context. */
 const setAttribute = (state: State, { path, value, values }: { path: Path; value?: string; values?: string[] }) => {
   const _values = values || [value!]
-  const parentId = head(path)
-  const attributeId =
-    getAllChildrenAsThoughts(state, parentId).find(child => child.value === _values[0])?.id || createId()
+  if (!value && (!values || values.length === 0)) return state
+
+  const thoughtId = head(path)
+  const attributeId = findDescendant(state, thoughtId, _values[0])
+  const idNew = createId()
+
   return reducerFlow([
-    !getAllChildrenAsThoughts(state, parentId).some(child => child.value === _values[0])
+    !attributeId
       ? state =>
           createThought(state, {
-            id: attributeId,
+            id: idNew,
             path,
             value: _values[0],
-            rank: getPrevRank(state, parentId),
+            rank: getPrevRank(state, thoughtId),
           })
       : null,
 
     _values[1] != null
       ? setFirstSubthought({
-          path: unroot([...path, attributeId]),
+          path: unroot([...path, attributeId || idNew]),
           value: _values[1],
         })
       : null,
