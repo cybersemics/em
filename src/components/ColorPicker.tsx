13c213a24d182a41f7350e82cfb8431dc95fea56
Raine Revere <raine@cybersemics.org>
2022-08-08 10:15:24 -0600

    ColorPicker: Outline selected color.

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,7 +1,10 @@
 import React, { FC, useEffect, useRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import State from '../@types/State'
 import textColor from '../action-creators/textColor'
+import getStyle from '../selectors/getStyle'
 import themeColors from '../selectors/themeColors'
+import head from '../util/head'
 import TriangleDown from './TriangleDown'
 import TextColorIcon from './icons/TextColor'
 
@@ -26,10 +29,15 @@ const ColorSwatch: FC<{
   color?: string
   // aria-label; defaults to color or background color
   label?: string
+  cursorStyle?: React.CSSProperties
   shape?: 'text' | 'bullet'
   size?: number
-}> = ({ backgroundColor, color, label, shape, size }) => {
+}> = ({ backgroundColor, color, cursorStyle, label, shape, size }) => {
   const dispatch = useDispatch()
+  const colors = useSelector(themeColors)
+  const selected =
+    (color && (cursorStyle?.color || 'white') === color) ||
+    (backgroundColor && (cursorStyle?.backgroundColor || 'black') === backgroundColor)
   size = size || 20
   return (
     <span
@@ -65,8 +73,10 @@ const ColorSwatch: FC<{
           style={{
             backgroundColor,
             border: 'none',
+            fontWeight: selected ? 'bold' : 'normal',
             color: backgroundColor ? 'black' : color,
             margin: '3px 5px 5px',
+            ...(selected ? { outline: `solid 1px ${colors.gray66}` } : null),
           }}
         />
       )}
@@ -78,6 +88,13 @@ const ColorSwatch: FC<{
 const ColorPicker: FC<{ fontSize: number }> = ({ fontSize }) => {
   const colors = useSelector(themeColors)
   const ref = useRef<HTMLDivElement>(null)
+  const cursorStyle = useSelector(
+    (state: State) =>
+      state.showColorPicker && state.cursor
+        ? getStyle(state, head(state.cursor)) || ({} as React.CSSProperties)
+        : ({} as React.CSSProperties),
+    shallowEqual,
+  )
 
   useKeepInWindow(ref)
 
@@ -101,47 +118,47 @@ const ColorPicker: FC<{ fontSize: number }> = ({ fontSize }) => {
 
       {/* Bullet Color */}
       <div aria-label='bullet color swatches' style={{ marginBottom: -2, whiteSpace: 'nowrap' }}>
-        <ColorSwatch shape='bullet' color='white' />
-        <ColorSwatch shape='bullet' color='gray' />
-        <ColorSwatch shape='bullet' color='orange' />
-        <ColorSwatch shape='bullet' color='#ffee14' label='yellow' />
-        <ColorSwatch shape='bullet' color='mediumspringgreen' label='spring green' />
-        <ColorSwatch shape='bullet' color='mediumseagreen' label='sea green' />
-        <ColorSwatch shape='bullet' color='dodgerblue' label='blue' />
-        <ColorSwatch shape='bullet' color='mediumpurple' label='purple' />
-        <ColorSwatch shape='bullet' color='violet' />
-        <ColorSwatch shape='bullet' color='pink' />
-        <ColorSwatch shape='bullet' color='tomato' label='red' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='white' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='gray' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='orange' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='#ffee14' label='yellow' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='mediumspringgreen' label='spring green' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='mediumseagreen' label='sea green' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='dodgerblue' label='blue' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='mediumpurple' label='purple' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='violet' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='pink' />
+        <ColorSwatch cursorStyle={cursorStyle} shape='bullet' color='tomato' label='red' />
       </div>
 
       {/* Text Color */}
       <div aria-label='text color swatches' style={{ whiteSpace: 'nowrap' }}>
-        <ColorSwatch color='white' />
-        <ColorSwatch color='gray' />
-        <ColorSwatch color='orange' />
-        <ColorSwatch color='#ffee14' label='yellow' />
-        <ColorSwatch color='mediumspringgreen' label='spring green' />
-        <ColorSwatch color='mediumseagreen' label='sea green' />
-        <ColorSwatch color='dodgerblue' label='blue' />
-        <ColorSwatch color='mediumpurple' label='purple' />
-        <ColorSwatch color='violet' />
-        <ColorSwatch color='pink' />
-        <ColorSwatch color='tomato' label='red' />
+        <ColorSwatch cursorStyle={cursorStyle} color='white' />
+        <ColorSwatch cursorStyle={cursorStyle} color='gray' />
+        <ColorSwatch cursorStyle={cursorStyle} color='orange' />
+        <ColorSwatch cursorStyle={cursorStyle} color='#ffee14' label='yellow' />
+        <ColorSwatch cursorStyle={cursorStyle} color='mediumspringgreen' label='spring green' />
+        <ColorSwatch cursorStyle={cursorStyle} color='mediumseagreen' label='sea green' />
+        <ColorSwatch cursorStyle={cursorStyle} color='dodgerblue' label='blue' />
+        <ColorSwatch cursorStyle={cursorStyle} color='mediumpurple' label='purple' />
+        <ColorSwatch cursorStyle={cursorStyle} color='violet' />
+        <ColorSwatch cursorStyle={cursorStyle} color='pink' />
+        <ColorSwatch cursorStyle={cursorStyle} color='tomato' label='red' />
       </div>
 
       {/* Background Color */}
       <div aria-label='background color swatches' style={{ whiteSpace: 'nowrap' }}>
-        <ColorSwatch backgroundColor='white' />
-        <ColorSwatch backgroundColor='gray' />
-        <ColorSwatch backgroundColor='orange' />
-        <ColorSwatch backgroundColor='#ffee14' label='yellow' />
-        <ColorSwatch backgroundColor='mediumspringgreen' label='spring green' />
-        <ColorSwatch backgroundColor='mediumseagreen' label='sea green' />
-        <ColorSwatch backgroundColor='dodgerblue' label='blue' />
-        <ColorSwatch backgroundColor='mediumpurple' label='purple' />
-        <ColorSwatch backgroundColor='violet' />
-        <ColorSwatch backgroundColor='pink' />
-        <ColorSwatch backgroundColor='tomato' label='red' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='white' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='gray' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='orange' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='#ffee14' label='yellow' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='mediumspringgreen' label='spring green' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='mediumseagreen' label='sea green' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='dodgerblue' label='blue' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='mediumpurple' label='purple' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='violet' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='pink' />
+        <ColorSwatch cursorStyle={cursorStyle} backgroundColor='tomato' label='red' />
       </div>
     </div>
   )
