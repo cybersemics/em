0979e35ee06f3f6ae69da7fb86f427e09c5b1ea1
Raine Revere <raine@cybersemics.org>
2022-08-10 10:23:49 -0600

    appendToPath: Memoize to avoid child re-renders.

--- src/util/appendToPath.ts
+++ src/util/appendToPath.ts
@@ -1,7 +1,9 @@
+import moize from 'moize'
 import Path from '../@types/Path'
 import SimplePath from '../@types/SimplePath'
 import ThoughtId from '../@types/ThoughtId'
 import isRoot from '../util/isRoot'
+import { resolveArray } from './memoizeResolvers'
 
 /** Appends one or more Child nodes to a Path or SimplePath. Ensures ROOT is removed. */
 const appendToPath = <T extends Path | SimplePath>(path: T | null, ...children: ThoughtId[]): T =>
@@ -9,4 +11,11 @@ const appendToPath = <T extends Path | SimplePath>(path: T | null, ...children: 
   // also needed for Branding to SimplePath
   !path || isRoot(path) ? (children as T) : ([...path, ...children] as unknown as T)
 
-export default appendToPath
+// memoize to avoid child Thought re-renders
+const appendToPathMemoized = moize(appendToPath, {
+  maxSize: 1000,
+  profileName: 'appendToPath',
+  transformArgs: ([path, ...children]) => [path && resolveArray(path), ...resolveArray(children)],
+})
+
+export default appendToPathMemoized
