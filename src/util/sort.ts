d2a6a4860bc5039c5bad2f65604c8eeef481d415
Raine Revere <raine@cybersemics.org>
2022-08-13 09:03:34 -0600

    sort: Return stable object reference for empty arrays.

--- src/util/sort.ts
+++ src/util/sort.ts
@@ -1,6 +1,9 @@
 import ComparatorFunction from '../@types/ComparatorFunction'
 
-/** Pure sort. */
-const sort = <T>(arr: T[], ...args: ComparatorFunction<T>[]) => [...arr].sort(...args) // eslint-disable-line fp/no-mutating-methods
+const EMPTY_ARRAY: any = []
+
+/** Pure sort. Returns a stable object reference for empty arrays. */
+const sort = <T>(arr: T[], ...args: ComparatorFunction<T>[]): T[] =>
+  arr.length === 0 ? EMPTY_ARRAY : [...arr].sort(...args) // eslint-disable-line fp/no-mutating-methods
 
 export default sort
