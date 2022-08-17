c0aab0030a4603cd6da89aee50143ed3cea3ce1a
Raine Revere <raine@cybersemics.org>
2022-08-17 07:16:15 -0600

    Replace GestureHintIcon with GestureDiagram.

--- src/components/GestureHintExtendedButton.tsx
+++ src/components/GestureHintExtendedButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import alert from '../action-creators/alert'
 import { AlertType } from '../constants'
-import GestureHintIcon from './icons/GestureHintIcon'
+import GestureDiagram from './GestureDiagram'
 
 /** A button that activates the extended gesture hint. */
 const GestureHintExtendedButton: React.FC = () => {
@@ -18,8 +18,9 @@ const GestureHintExtendedButton: React.FC = () => {
           }),
         )
       }
+      style={{ overflow: 'hidden', width: 30, height: 20, marginTop: 2, marginRight: -16 }}
     >
-      <GestureHintIcon size={20} />
+      <GestureDiagram path={'rdr'} size={55} strokeWidth={4} style={{ marginTop: -19, marginLeft: -8 }} />
     </div>
   )
 }
