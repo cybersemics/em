e2b88a558c3ecf061c220f959fe6354e7f32f0cc
Raine Revere <raine@cybersemics.org>
2022-09-06 10:58:27 -0600

    thoughtToPath: Fix for Paths that begin with EM_TOKEN.

--- src/selectors/thoughtToPath.ts
+++ src/selectors/thoughtToPath.ts
@@ -1,13 +1,13 @@
 import SimplePath from '../@types/SimplePath'
 import State from '../@types/State'
 import ThoughtId from '../@types/ThoughtId'
-import { HOME_PATH } from '../constants'
+import { EM_TOKEN, HOME_PATH } from '../constants'
 import getThoughtById from '../selectors/getThoughtById'
 import isRoot from '../util/isRoot'
 
 /** Generates the SimplePath for a Thought by traversing upwards to the root thought. Return null if any ancestors are missing, e.g. pending context. */
 const thoughtToPath = (state: State, thoughtId: ThoughtId): SimplePath => {
-  if (isRoot([thoughtId])) return HOME_PATH
+  if (isRoot([thoughtId]) || thoughtId === EM_TOKEN) return [thoughtId] as SimplePath
   const thought = getThoughtById(state, thoughtId)
   if (!thought) return HOME_PATH
   if (isRoot([thought.parentId])) return [thoughtId] as SimplePath
