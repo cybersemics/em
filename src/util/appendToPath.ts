979ca0acd50ab298b3effab0fde39c4ecad8bdac
Raine Revere <raine@cybersemics.org>
2022-09-19 16:35:57 +0000

    appendToPath: Temporarily disable memoization due to context view bug.

--- src/util/appendToPath.ts
+++ src/util/appendToPath.ts
@@ -1,9 +1,9 @@
-import moize from 'moize'
+// import moize from 'moize'
+// import { resolveArray } from './memoizeResolvers'
 import Path from '../@types/Path'
 import SimplePath from '../@types/SimplePath'
 import ThoughtId from '../@types/ThoughtId'
 import isRoot from '../util/isRoot'
-import { resolveArray } from './memoizeResolvers'
 
 /** Appends one or more Child nodes to a Path or SimplePath. Ensures ROOT is removed. */
 const appendToPath = <T extends Path | SimplePath>(path: T | null, ...children: ThoughtId[]): T =>
@@ -11,11 +11,13 @@ const appendToPath = <T extends Path | SimplePath>(path: T | null, ...children: 
   // also needed for Branding to SimplePath
   !path || isRoot(path) ? (children as T) : ([...path, ...children] as unknown as T)
 
-// memoize to avoid child Thought re-renders
-const appendToPathMemoized = moize(appendToPath, {
-  maxSize: 1000,
-  profileName: 'appendToPath',
-  transformArgs: ([path, ...children]) => [path && resolveArray(path), ...resolveArray(children)],
-})
+// TODO: How to memoize? Using appendToPathMemoized causes the context view to disapppear.
+// e.g. Activate the context view on /Virtual Assistance/Onboarding/Start
+// We need to memoize to to avoid unnecessary child Thought re-renders.
+// const appendToPathMemoized = moize(appendToPath, {
+//   maxSize: 1000,
+//   profileName: 'appendToPath',
+//   transformArgs: ([path, ...children]) => [path && resolveArray(path), resolveArray(children)],
+// })
 
-export default appendToPathMemoized
+export default appendToPath
