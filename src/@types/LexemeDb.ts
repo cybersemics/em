1fb43d59aa659a4ad1f3e675decd35fd194d6467
Raine Revere <raine@cybersemics.org>
2022-09-25 15:59:18 +0000

    LexemeDb: Encode and decode array order.

--- src/@types/LexemeDb.ts
+++ src/@types/LexemeDb.ts
@@ -5,22 +5,29 @@ import Lexeme from './Lexeme'
 import ThoughtId from './ThoughtId'
 
 /** A Lexeme database type that defines contexts as an object to allow individual updates. */
-type LexemeDb = Omit<Lexeme, 'contexts'> & { contexts?: Index<true> }
+type LexemeDb = Omit<Lexeme, 'contexts'> & {
+  // number is from the new schema that encodes the order of the contexts
+  // boolean is from the old schema. It should be ignored unless the Lexeme contexts are rewritten.
+  contexts?: Index<number | boolean>
+}
 
 /** Converts a Lexeme to a LexemeDb. */
 export const toLexemeDb = (lexeme: Lexeme): LexemeDb =>
   ({
     ..._.omit(lexeme, 'contexts'),
-    contexts: keyValueBy(lexeme.contexts, id => ({ [id]: true })),
+    // store array order in object as 1-based index
+    contexts: keyValueBy(lexeme.contexts, (id, i) => ({ [id]: i + 1 })),
   } as LexemeDb)
 
 /** Converts a LexemeDb to a Lexeme. */
-export const fromLexemeDb = (lexemeDb?: LexemeDb): Lexeme | undefined =>
-  lexemeDb
-    ? {
-        ...lexemeDb,
-        contexts: Object.keys(lexemeDb.contexts || {}) as ThoughtId[],
-      }
-    : undefined
+export const fromLexemeDb = (lexemeDb?: LexemeDb): Lexeme | undefined => {
+  if (!lexemeDb) return undefined
+  // reconstruct the order of the Lexeme contexts array
+  const contexts = _.sortBy(Object.entries(lexemeDb.contexts || {}), 1).map(([id, index]) => id as ThoughtId)
+  return {
+    ...lexemeDb,
+    contexts,
+  }
+}
 
 export default LexemeDb
