1c8fb13f8e52c646bf1cad337ff72e834fedc403
Raine Revere <raine@cybersemics.org>
2022-10-07 16:48:41 +0000

    Superscript: Replace mapStateToProps with useSelector.

--- src/components/Superscript.tsx
+++ src/components/Superscript.tsx
@@ -1,5 +1,6 @@
+import _ from 'lodash'
 import React, { FC, useEffect, useState } from 'react'
-import { connect, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import Context from '../@types/Context'
 import Index from '../@types/IndexType'
 import SimplePath from '../@types/SimplePath'
@@ -23,47 +24,36 @@ interface SuperscriptProps {
   thoughts?: Context
 }
 
-// eslint-disable-next-line jsdoc/require-jsdoc
-const mapStateToProps = (state: State, props: SuperscriptProps) => {
-  const { contextViews, cursor, showHiddenThoughts, showModal } = state
-  const cursorContext = cursor ? pathToContext(state, cursor) : [HOME_TOKEN]
-
-  const editing =
-    cursor &&
-    equalArrays(cursorContext, pathToContext(state, props.simplePath || [])) &&
-    hasLexeme(state, headValue(state, cursor))
-
-  const thoughts = props.thoughts || pathToContext(state, props.simplePath)
-
-  const thoughtsLive = editing ? cursorContext : thoughts
-
-  const simplePathLive = editing ? (parentOf(props.simplePath).concat(head(cursor!)) as SimplePath) : props.simplePath
-
-  return {
-    contextViews,
-    empty: thoughtsLive.length > 0 ? head(thoughtsLive).length === 0 : true, // ensure re-render when thought becomes empty
-    showHiddenThoughts,
-    simplePathLive,
-    thoughts,
-    thoughtRaw: head(simplePathLive),
-    showModal,
-  }
-}
-
 /** Renders superscript if there are other contexts. Optionally pass thoughts (used by ContextBreadcrumbs) or simplePath (used by Subthought). */
-const Superscript: FC<ReturnType<typeof mapStateToProps> & SuperscriptProps> = ({
-  empty,
-  showSingle,
-  showHiddenThoughts,
-  simplePath,
-  simplePathLive,
-  superscript = true,
-}) => {
+const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, superscript = true, thoughts }) => {
   const [numContexts, setNumContexts] = useState(0)
 
-  const contexts = useSelector((state: State) => getContexts(state, head(simplePathLive || simplePath)))
+  const showHiddenThoughts = useSelector((state: State) => state.showHiddenThoughts)
 
-  // delay rendering of superscript for performance
+  const show = useSelector((state: State) => {
+    const cursorContext = state.cursor ? pathToContext(state, state.cursor) : [HOME_TOKEN]
+    const editing =
+      state.cursor &&
+      equalArrays(cursorContext, pathToContext(state, simplePath || [])) &&
+      hasLexeme(state, headValue(state, state.cursor))
+
+    const thoughtsLive = editing ? cursorContext : thoughts || pathToContext(state, simplePath)
+    const empty = thoughtsLive.length > 0 ? head(thoughtsLive).length === 0 : true
+    return !empty && superscript && numContexts! > (showSingle ? 0 : 1)
+  })
+
+  const contexts = useSelector((state: State) => {
+    const cursorContext = state.cursor ? pathToContext(state, state.cursor) : [HOME_TOKEN]
+    const editing =
+      state.cursor &&
+      equalArrays(cursorContext, pathToContext(state, simplePath || [])) &&
+      hasLexeme(state, headValue(state, state.cursor))
+
+    const simplePathLive = editing ? (parentOf(simplePath).concat(head(state.cursor!)) as SimplePath) : simplePath
+    return getContexts(state, head(simplePathLive || simplePath))
+  }, _.isEqual)
+
+  // delay filtering for performance
   // recalculate when Lexeme contexts are loaded
   // Note: This results in a total running time for all superscripts of O(totalNumberOfContexts * depth)
   useEffect(() => {
@@ -75,7 +65,7 @@ const Superscript: FC<ReturnType<typeof mapStateToProps> & SuperscriptProps> = (
   return (
     <span className='superscript-container'>
       {
-        !empty && superscript && numContexts! > (showSingle ? 0 : 1) ? (
+        show ? (
           <span className='num-contexts'> {numContexts ? <sup>{numContexts}</sup> : null}</span>
         ) : null /* <DepthBar/> */
       }
@@ -83,4 +73,4 @@ const Superscript: FC<ReturnType<typeof mapStateToProps> & SuperscriptProps> = (
   )
 }
 
-export default connect(mapStateToProps)(Superscript)
+export default Superscript
