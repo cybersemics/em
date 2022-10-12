a21b8f508960662851c89067e0c056e36bbfcee2
Raine Revere <raine@cybersemics.org>
2022-10-12 16:51:59 +0000

    SubthoughtsDropEnd: Fix in sorted lists.

--- src/components/Subthoughts/SubthoughtsDropEnd.tsx
+++ src/components/Subthoughts/SubthoughtsDropEnd.tsx
@@ -2,14 +2,20 @@ import classNames from 'classnames'
 import React from 'react'
 import { ConnectDropTarget } from 'react-dnd'
 import { useSelector } from 'react-redux'
+import DropThoughtZone from '../../@types/DropThoughtZone'
 import SimplePath from '../../@types/SimplePath'
 import State from '../../@types/State'
 import { isTouch } from '../../browser'
 import { ID } from '../../constants'
 import globals from '../../globals'
+import { getChildrenSorted } from '../../selectors/getChildren'
 import getSortPreference from '../../selectors/getSortPreference'
 import getThoughtById from '../../selectors/getThoughtById'
+import rootedParentOf from '../../selectors/rootedParentOf'
+import { compareReasonable } from '../../util/compareThought'
+import equalPath from '../../util/equalPath'
 import head from '../../util/head'
+import headId from '../../util/headId'
 import strip from '../../util/strip'
 
 /** The drop target at the end of the Subthoughts. The drop-hover components are ThoughtDropHover, SubthoughtsDropEnd, and SubthoughtsDropEmpty. */
@@ -32,14 +38,48 @@ const SubthoughtsDropEnd = ({
 }) => {
   const thoughtId = head(simplePath)
   const value = useSelector((state: State) => getThoughtById(state, thoughtId)?.value)
+  // const parentId = useSelector((state: State) => getThoughtById(state, thoughtId)?.parentId)
   const dragInProgress = useSelector((state: State) => state.dragInProgress)
 
   // a boolean indicating if the drop-hover component is shown
   // true if hovering and the context is not sorted
-  const showDropHover = useSelector(
-    (state: State) =>
-      (globals.simulateDrag || isHovering) && getSortPreference(state, thoughtId).type !== 'Alphabetical',
-  )
+  const showDropHover = useSelector((state: State) => {
+    if (globals.simulateDrag) return true
+
+    // if hovering, and the parent is not sorted, show the drop-hover
+    const isParentSorted = getSortPreference(state, thoughtId).type === 'Alphabetical'
+    if (isHovering && !isParentSorted) return true
+
+    // If sorted and hovering, only show the drop-hover if the thought would be dropped to the end of the list.
+    // Otherwise, asssume the ThoughtDropHover component will be rendered at the place of insertion.
+    if (!isParentSorted) return false
+
+    // only render drop-hover during drag-and-drop
+    const draggingThoughtValue = state.draggingThought
+      ? getThoughtById(state, headId(state.draggingThought))?.value
+      : null
+    if (!draggingThoughtValue) return false
+
+    // render the drop-hover if hovering over any thought in a sorted list
+    const isThoughtHovering =
+      state.hoveringPath &&
+      equalPath(rootedParentOf(state, state.hoveringPath), simplePath) &&
+      state.hoverZone === DropThoughtZone.ThoughtDrop
+
+    // render the drop-hover if hovering over sorted Subthoughts
+    const isSubthoughtsHovering =
+      state.hoveringPath &&
+      equalPath(state.hoveringPath, simplePath) &&
+      state.hoverZone === DropThoughtZone.SubthoughtsDrop
+
+    // only rendner drop-hover if hovering over the appropriate ThoughtDrop or SubthoughtsDrop
+    if (!isThoughtHovering && !isSubthoughtsHovering) return false
+
+    // check if the dragged thought would get dropped after the last thought in the list
+    const children = getChildrenSorted(state, thoughtId)
+    const lastChildValue = children[children.length - 1]?.value
+    return (isThoughtHovering || isSubthoughtsHovering) && compareReasonable(draggingThoughtValue, lastChildValue) > 0
+  })
 
   return (dropTarget || ID)(
     <li
