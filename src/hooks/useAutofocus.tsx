b87a606c5f0017824efe9a81d1d879befaae7bae
Raine Revere <raine@cybersemics.org>
2022-10-16 16:05:18 +0000

    Only remove hidden thoughts below the cursor.

--- src/hooks/useAutofocus.tsx
+++ src/hooks/useAutofocus.tsx
@@ -1,21 +1,38 @@
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Autofocus from '../@types/Autofocus'
+import Path from '../@types/Path'
 import SimplePath from '../@types/SimplePath'
 import State from '../@types/State'
+import { getChildrenSorted } from '../selectors/getChildren'
+import rootedParentOf from '../selectors/rootedParentOf'
 import themeColors from '../selectors/themeColors'
 import alpha from '../util/alpha'
+import head from '../util/head'
 import { isDescendantPath } from '../util/isDescendantPath'
 
 /** Applies alpha transparency to colors (or default foreground color) for autofocus. */
 const useAutofocus = (simplePath: SimplePath, autofocus?: Autofocus, style?: React.CSSProperties) => {
+  const timerRef = useRef<number | undefined>(undefined)
+  const [noHeight, setNoHeight] = useState<boolean>(false)
+
   const colors = useSelector(themeColors)
+
   const isHidden = useSelector(
     (state: State) =>
       (autofocus === 'hide' || autofocus === 'hide-parent') && !isDescendantPath(state.cursor, simplePath),
   )
-  const [noHeight, setNoHeight] = useState<boolean>(false)
-  const timerRef = useRef<number | undefined>(undefined)
+
+  // isHidden and isBelowCursor cannot be combined, since a hidden thought may no longer be below the cursor but still needs to have its height restored
+  const isBelowCursor = useSelector((state: State) => {
+    if (!state.cursor || simplePath.length > state.cursor.length) return false
+    const cursorAncestor = state.cursor.slice(0, simplePath.length) as Path
+    const thoughtId = head(simplePath)
+    const siblings = getChildrenSorted(state, head(rootedParentOf(state, simplePath)))
+    const index = siblings.findIndex(thought => thought.id === thoughtId)
+    const cursorIndex = siblings.findIndex(thought => thought.id === head(cursorAncestor))
+    return cursorIndex !== -1 && index > cursorIndex
+  })
 
   // track when the color or background color changes
   // used to determine transition
@@ -81,13 +98,14 @@ const useAutofocus = (simplePath: SimplePath, autofocus?: Autofocus, style?: Rea
   }, [autofocus, colors, noHeight, style?.color, style?.backgroundColor, disableTransition])
 
   // set display:none after the autofocus fade out transition completes (750ms)
-  if (isHidden && !noHeight) {
+  if (isHidden && isBelowCursor && !noHeight) {
     clearTimeout(timerRef.current)
     timerRef.current = setTimeout(() => {
       setNoHeight(true)
     }, 800) as unknown as number
   }
   // remove display:none when visible
+  // isHidden and isBelowCursor cannot be combined, since a hidden thought may no longer be below the cursor but still needs to have its height restored
   else if (noHeight && !isHidden) {
     clearTimeout(timerRef.current)
     setNoHeight(false)
