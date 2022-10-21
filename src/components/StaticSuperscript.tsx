9e3e8db6f15d394e3b57d4f46e0e54661e3d90e5
Raine Revere <raine@cybersemics.org>
2022-10-21 13:14:17 +0000

    Wrap StaticSuperscript in React.memo

--- src/components/StaticSuperscript.tsx
+++ src/components/StaticSuperscript.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 /** Renders a given number as a superscript. */
-const StaticSuperscript = ({ n, style }: { n: number; style?: React.CSSProperties }) => (
+const StaticSuperscript = React.memo(({ n, style }: { n: number; style?: React.CSSProperties }) => (
   <span className='superscript-container' style={style}>
     <span className='num-contexts'>
       <sup role='superscript'>{n}</sup>
     </span>
   </span>
-)
+))
+
+StaticSuperscript.displayName = 'StaticSuperscript'
 
 export default StaticSuperscript
