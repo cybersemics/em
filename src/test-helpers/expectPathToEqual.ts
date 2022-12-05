fea951556c16b5fad66360b62ab8fd56b472efda
Raine Revere <raine@cybersemics.org>
2022-12-05 14:28:19 +0000

    expectPathToEqual: Fail test when path is null.

--- src/test-helpers/expectPathToEqual.ts
+++ src/test-helpers/expectPathToEqual.ts
@@ -1,16 +1,14 @@
+import Path from '../@types/Path'
 import State from '../@types/State'
 import Thought from '../@types/Thought'
-import ThoughtId from '../@types/ThoughtId'
 import childIdsToThoughts from '../selectors/childIdsToThoughts'
 
 /**
  * Tests if the given childIds match the expected thoughts. Accepts a { value, rank }[] or string[].
  */
-const expectPathToEqual = (state: State, path: ThoughtId[] | null, expectedThoughts: (Partial<Thought> | string)[]) => {
-  if (!path) return false
-  // const expected =
-  //   typeof expectedThoughts[0] === 'string' ? expectedThoughts.map(value => ({ value })) : expectedThoughts
-  const thoughts = childIdsToThoughts(state, path)
+const expectPathToEqual = (state: State, path: Path | null, expectedThoughts: (Partial<Thought> | string)[]) => {
+  expect(path).not.toEqual(null)
+  const thoughts = childIdsToThoughts(state, path!)
   const thoughtsNormalized = thoughts.map(({ value, rank }) =>
     typeof expectedThoughts[0] === 'string' ? value : { value, rank },
   )
