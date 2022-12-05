ae8db064d2b63c02ebcaba6da287ab8a799febed
Raine Revere <raine@cybersemics.org>
2022-12-05 14:12:04 +0000

    Fix cursorForward in context view.

--- src/reducers/__tests__/cursorForward.ts
+++ src/reducers/__tests__/cursorForward.ts
@@ -1,33 +1,117 @@
+import importText from '../../reducers/importText'
+import toggleContextView from '../../reducers/toggleContextView'
 import expectPathToEqual from '../../test-helpers/expectPathToEqual'
-import setCursorFirstMatch from '../../test-helpers/setCursorFirstMatch'
+import setCursor from '../../test-helpers/setCursorFirstMatch'
 import initialState from '../../util/initialState'
 import reducerFlow from '../../util/reducerFlow'
 import cursorBack from '../cursorBack'
 import cursorForward from '../cursorForward'
 import newSubthought from '../newSubthought'
 import newThought from '../newThought'
-import setCursor from '../setCursor'
 
-it('reverse cursorBack', () => {
-  const steps = [newThought('a'), newSubthought('b'), cursorBack, cursorForward]
+describe('normal view', () => {
+  it('reverse cursorBack', () => {
+    const steps = [newThought('a'), newSubthought('b'), cursorBack, cursorForward]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, ['a', 'b'])
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'b'])
+  })
+
+  it('move to first child if there is no history', () => {
+    const steps = [newThought('a'), newSubthought('b'), newThought('c'), setCursor(['a']), cursorForward]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'b'])
+  })
+
+  it('move to first child if there is no cursor', () => {
+    const steps = [newThought('a'), setCursor(null), cursorForward]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expectPathToEqual(stateNew, stateNew.cursor, ['a'])
+  })
 })
 
-it('move to first child if there is no history', () => {
-  const steps = [newThought('a'), newSubthought('b'), newThought('c'), setCursorFirstMatch(['a']), cursorForward]
+describe('context view', () => {
+  it('reverse cursorBack', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+    `
+    const steps = [
+      importText({ text }),
+      setCursor(['a', 'm']),
+      toggleContextView,
+      setCursor(['a', 'm', 'b']),
+      cursorBack,
+      cursorForward,
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, ['a', 'b'])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'm', 'b'])
+  })
 
-it('move to first child if there is no cursor', () => {
-  const steps = [newThought('a'), setCursor({ path: null }), cursorForward]
+  it('move to first context if there is no history', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+    `
+    const steps = [importText({ text }), setCursor(['a', 'm']), toggleContextView, cursorForward]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, ['a'])
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'm', 'a'])
+  })
+
+  it('move to first context if history cursor is not in the context view', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+    `
+    const steps = [importText({ text }), setCursor(['a', 'm', 'x']), cursorBack, toggleContextView, cursorForward]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'm', 'a'])
+  })
+
+  it('move to the first thought if history cursor is in the context view and the context view has been closed', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+    `
+    const steps = [
+      importText({ text }),
+      setCursor(['a', 'm']),
+      toggleContextView,
+      setCursor(['a', 'm', 'b']),
+      cursorBack,
+      toggleContextView,
+      cursorForward,
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expectPathToEqual(stateNew, stateNew.cursor, ['a', 'm', 'x'])
+  })
 })
--- src/reducers/cursorForward.ts
+++ src/reducers/cursorForward.ts
@@ -1,27 +1,38 @@
+import { last } from 'lodash'
 import State from '../@types/State'
 import { HOME_PATH } from '../constants'
 import setCursor from '../reducers/setCursor'
 import { firstVisibleChild } from '../selectors/getChildren'
+import getContextsSortedAndRanked from '../selectors/getContextsSortedAndRanked'
+import getThoughtById from '../selectors/getThoughtById'
+import isContextViewActive from '../selectors/isContextViewActive'
+import appendToPath from '../util/appendToPath'
 import head from '../util/head'
+import headValue from '../util/headValue'
 import unroot from '../util/unroot'
 
 /** Moves the cursor forward in the cursorHistory. */
 const cursorForward = (state: State) => {
-  // pop from cursor history
-  if (state.cursorHistory.length > 0) {
-    const cursorNew = state.cursorHistory[state.cursorHistory.length - 1]
-    return setCursor(state, { path: cursorNew, cursorHistoryPop: true })
+  const cursorFromHistory = last(state.cursorHistory)
+  const cursor = state.cursor || HOME_PATH
+  const showContexts = isContextViewActive(state, cursor)
+
+  // context view
+  let cursorNew, isValidChild
+  if (showContexts) {
+    const contexts = getContextsSortedAndRanked(state, headValue(state, cursor))
+    const firstContext = contexts[0]
+    isValidChild = cursorFromHistory && contexts.some(cx => cx.parentId === head(cursorFromHistory))
+    cursorNew = isValidChild ? cursorFromHistory : appendToPath(cursor, firstContext.parentId)
   }
-  // otherwise move cursor to first child
+  // normal view
   else {
-    const cursor = state.cursor || HOME_PATH
-
     const firstChild = firstVisibleChild(state, head(cursor))
-    if (!firstChild) return state
-
-    const cursorNew = unroot([...cursor, firstChild.id])
-    return setCursor(state, { path: cursorNew })
+    isValidChild = cursorFromHistory && !!getThoughtById(state, head(cursor)).childrenMap[head(cursorFromHistory)]
+    cursorNew = isValidChild ? cursorFromHistory : unroot([...cursor, firstChild!.id])
   }
+
+  return cursorNew ? setCursor(state, { path: cursorNew, cursorHistoryPop: isValidChild }) : state
 }
 
 export default cursorForward
