c452dbb007d6aa75ad6dd2315842f35bce37d406
Raine Revere <raine@cybersemics.org>
2022-12-10 16:06:01 +0000

    Fix collapseContext in context view.

--- src/reducers/__tests__/collapseContext.ts
+++ src/reducers/__tests__/collapseContext.ts
@@ -6,150 +6,215 @@ import cursorUp from '../../reducers/cursorUp'
 import importText from '../../reducers/importText'
 import newSubthought from '../../reducers/newSubthought'
 import newThought from '../../reducers/newThought'
+import toggleContextView from '../../reducers/toggleContextView'
 import exportContext from '../../selectors/exportContext'
 import expectPathToEqual from '../../test-helpers/expectPathToEqual'
+import setCursor from '../../test-helpers/setCursorFirstMatch'
 import initialState from '../../util/initialState'
 import reducerFlow from '../../util/reducerFlow'
 
-it('do nothing on leaf', () => {
-  const steps = [newThought('a'), newSubthought('b'), collapseContext({})]
+describe('normal view', () => {
+  it('do nothing on leaf', () => {
+    const steps = [newThought('a'), newSubthought('b'), collapseContext({})]
 
-  const state = initialState()
-  const stateNew = reducerFlow(steps)(state)
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const state = initialState()
+    const stateNew = reducerFlow(steps)(state)
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - b`)
-})
+  })
 
-it('collapse context with single child', () => {
-  const steps = [newThought('a'), newSubthought('b'), newSubthought('c'), cursorBack, collapseContext({})]
+  it('collapse context with single child', () => {
+    const steps = [newThought('a'), newSubthought('b'), newSubthought('c'), cursorBack, collapseContext({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - c`)
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
+  })
 
-it('collapse context with multiple children', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('k'),
-    newThought('b'),
-    newSubthought('c'),
-    newThought('d'),
-    cursorBack,
-    collapseContext({}),
-  ]
+  it('collapse context with multiple children', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('k'),
+      newThought('b'),
+      newSubthought('c'),
+      newThought('d'),
+      cursorBack,
+      collapseContext({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - k
     - c
     - d`)
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
+  })
 
-it('merge children', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('b'),
-    newThought('x'),
-    cursorUp,
-    newSubthought('c'),
-    newThought('d'),
-    cursorBack,
-    collapseContext({}),
-  ]
+  it('merge children', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('b'),
+      newThought('x'),
+      cursorUp,
+      newSubthought('c'),
+      newThought('d'),
+      cursorBack,
+      collapseContext({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - c
     - d
     - x`)
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
+  })
 
-it('merge duplicate children', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('b'),
-    newThought('d'),
-    cursorUp,
-    newSubthought('c'),
-    newThought('d'),
-    cursorBack,
-    collapseContext({}),
-  ]
+  it('merge duplicate children', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('b'),
+      newThought('d'),
+      cursorUp,
+      newSubthought('c'),
+      newThought('d'),
+      cursorBack,
+      collapseContext({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - c
     - d`)
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
+  })
 
-it('after collapse context set cursor to the first visible children.', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('b'),
-    newSubthought('=x'),
-    newThought('c'),
-    cursorBack,
-    collapseContext({}),
-  ]
+  it('after collapse context set cursor to the first visible children.', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('b'),
+      newSubthought('=x'),
+      newThought('c'),
+      cursorBack,
+      collapseContext({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: 'c' }])
+  })
 
-it('after collapse context set cursor to the parent if there are no visible children.', () => {
-  const steps = [newThought('a'), newSubthought('b'), newSubthought('=x'), cursorBack, collapseContext({})]
+  it('after collapse context set cursor to the parent if there are no visible children.', () => {
+    const steps = [newThought('a'), newSubthought('b'), newSubthought('=x'), cursorBack, collapseContext({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }])
+  })
 
-it('collapse empty thought with empty child', () => {
-  const steps = [
-    importText({
-      text: `
+  it('collapse empty thought with empty child', () => {
+    const steps = [
+      importText({
+        text: `
       - a
         - ${''}
           - ${''}
             - b
     `,
-    }),
-    cursorDown,
-    collapseContext({}),
-  ]
+      }),
+      cursorDown,
+      collapseContext({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a
     - ${''}
       - b`)
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: '' }])
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a' }, { value: '' }])
+  })
+})
+
+describe('context view', () => {
+  it('disallow collapse context in context view', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+    `
+    const steps = [
+      importText({ text }),
+      setCursor(['a', 'm']),
+      toggleContextView,
+      setCursor(['a', 'm', 'b']),
+      collapseContext({}),
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+
+    expect(exported).toBe(`- ${HOME_TOKEN}
+  - a
+    - m
+      - x
+  - b
+    - m
+      - y`)
+  })
+
+  it('collapse context subthought in context view', () => {
+    const text = `
+      - a
+        - m
+          - x
+      - b
+        - m
+          - y
+            - z
+    `
+    const steps = [
+      importText({ text }),
+      setCursor(['a', 'm']),
+      toggleContextView,
+      setCursor(['a', 'm', 'b', 'y']),
+      collapseContext({}),
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+
+    expect(exported).toBe(`- ${HOME_TOKEN}
+  - a
+    - m
+      - x
+  - b
+    - m
+      - z`)
+  })
 })
--- src/reducers/collapseContext.ts
+++ src/reducers/collapseContext.ts
@@ -1,19 +1,20 @@
 import _ from 'lodash'
 import Path from '../@types/Path'
 import State from '../@types/State'
+import alert from '../reducers/alert'
 import moveThought from '../reducers/moveThought'
 import setCursor from '../reducers/setCursor'
 import { getAllChildrenAsThoughts, getChildren, isChildVisible } from '../selectors/getChildren'
 import getRankBefore from '../selectors/getRankBefore'
 import getThoughtById from '../selectors/getThoughtById'
+import isContextViewActive from '../selectors/isContextViewActive'
 import rootedParentOf from '../selectors/rootedParentOf'
 import simplifyPath from '../selectors/simplifyPath'
+import appendToPath from '../util/appendToPath'
 import createId from '../util/createId'
 import head from '../util/head'
-import normalizeThought from '../util/normalizeThought'
 import parentOf from '../util/parentOf'
 import reducerFlow from '../util/reducerFlow'
-import unroot from '../util/unroot'
 import deleteThought from './deleteThought'
 import editThought from './editThought'
 
@@ -29,66 +30,65 @@ const collapseContext = (state: State, { at }: Options) => {
 
   if (!path) return state
 
-  const simpleCursor = simplifyPath(state, path)
+  if (isContextViewActive(state, parentOf(path))) {
+    return alert(state, {
+      value: `Contexts may not be collapsed in the context view.`,
+    })
+  }
 
-  const children = getAllChildrenAsThoughts(state, head(simpleCursor))
+  const simplePath = simplifyPath(state, path)
+  const children = getAllChildrenAsThoughts(state, head(simplePath))
+
+  if (children.length === 0) return state
 
   /** Returns first moved child path as new cursor after collapse. */
-  const getNewCursor = (updatedState: State) => {
+  const getNewCursor = (state: State) => {
     const firstVisibleChildOfPrevCursor = (
       state.showHiddenThoughts ? children : children.filter(isChildVisible(state))
     )[0]
 
-    const parentId = head(rootedParentOf(updatedState, simpleCursor))
-    const childrenOfMovedContext = getChildren(updatedState, parentId)
+    const parentId = head(rootedParentOf(state, simplePath))
+    const childrenOfMovedContext = getChildren(state, parentId)
 
-    if (!firstVisibleChildOfPrevCursor) return unroot([...parentOf(path)])
-
-    const normalizedFirstChildValue = normalizeThought(firstVisibleChildOfPrevCursor.value)
+    if (!firstVisibleChildOfPrevCursor) return parentOf(path)
 
     const newChild =
-      childrenOfMovedContext.find(child => normalizeThought(child.value) === normalizedFirstChildValue) ||
-      childrenOfMovedContext[0]
+      childrenOfMovedContext.find(child => child.id === firstVisibleChildOfPrevCursor.id) || childrenOfMovedContext[0]
 
-    return unroot([...parentOf(path), newChild.id])
+    return newChild && appendToPath(parentOf(path), newChild.id)
   }
 
-  const thought = getThoughtById(state, head(simpleCursor))
+  const thought = getThoughtById(state, head(simplePath))
 
-  return reducerFlow(
-    children.length > 0
-      ? [
-          // first edit the collapsing thought to a unique value
-          // otherwise, it could get merged when children are outdented in the next step
-          editThought({
-            oldValue: thought.value,
-            newValue: createId(), // unique value
-            path: simpleCursor,
-          }),
-          // outdent each child
-          ...children.map(
-            child => (updatedState: State) =>
-              moveThought(updatedState, {
-                oldPath: unroot([...path, child.id]),
-                newPath: unroot([...parentOf(path), child.id]),
-                newRank: getRankBefore(updatedState, simpleCursor),
-              }),
-          ),
-          // delete the original cursor
-          deleteThought({
-            pathParent: parentOf(simpleCursor),
-            thoughtId: head(simpleCursor),
-          }),
-          // set the new cursor
-          state =>
-            setCursor(state, {
-              path: getNewCursor(state),
-              editing: state.editing,
-              offset: 0,
-            }),
-        ]
-      : [],
-  )(state)
+  return reducerFlow([
+    // first edit the collapsing thought to a unique value
+    // otherwise, it could get merged when children are outdented in the next step
+    editThought({
+      oldValue: thought.value,
+      newValue: createId(), // unique value
+      path: simplePath,
+    }),
+    // outdent each child
+    ...children.map(child =>
+      moveThought({
+        oldPath: appendToPath(simplePath, child.id),
+        newPath: appendToPath(parentOf(simplePath), child.id),
+        newRank: getRankBefore(state, simplePath),
+      }),
+    ),
+    // delete the original cursor
+    deleteThought({
+      pathParent: parentOf(simplePath),
+      thoughtId: head(simplePath),
+    }),
+    // set the new cursor
+    state =>
+      setCursor(state, {
+        path: getNewCursor(state),
+        editing: state.editing,
+        offset: 0,
+      }),
+  ])(state)
 }
 
 export default _.curryRight(collapseContext)
