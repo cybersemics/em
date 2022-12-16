e7441be7ef06782fa32625aa7e3423a551e9b851
Raine Revere <raine@cybersemics.org>
2022-12-16 03:04:21 +0000

    Implement websocket share route. Create client-side and server-side permissions ydoc.

--- scripts/server/index.ts
+++ scripts/server/index.ts
@@ -1,15 +1,16 @@
-import { nanoid } from 'nanoid'
 import { WebsocketProvider } from 'y-websocket-auth'
 import * as Y from 'yjs'
 
-const server = require('y-websocket-auth/server')
+const { getYDoc, createServer } = require('y-websocket-auth/server')
+
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 1234
-const permissionsDir = process.env.YPERSISTENCE || './.permissions.level'
-const serverId = nanoid()
+const PERMISSIONS_DOCID = 'permissions'
 
 /**
- * Per-document permissions. Persisted to PERMISSIONS_DIR (default: .permissions.level)
+ * All thoughtspace permissions. Mirrors Websocket documents at DOCID/permissions. Must be loaded into memory so that permissions are available for authentication.
+ *
+ * Persisted to YPERMISSIONS (default: .permissions.level).
  *
  * @example
  *   {
@@ -18,48 +19,60 @@ const serverId = nanoid()
  *     }
  *   }
  */
-
-// const PERMISSIONS_DOCID = 'permissions'
-// const LeveldbPersistence = require('y-leveldb').LeveldbPersistence
-// const ldb = new LeveldbPersistence(permissionsDir)
-// ;(async () => {
-//   const persistedYdoc = await ldb.getYDoc(PERMISSIONS_DOCID)
-//   const newUpdates = Y.encodeStateAsUpdate(ydoc)
-//   ldb.storeUpdate(PERMISSIONS_DOCID, newUpdates)
-//   Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc))
-//   ydoc.on('update', update => {
-//     ldb.storeUpdate(PERMISSIONS_DOCID, update)
-//   })
-// })()
+const ydoc = new Y.Doc()
+if (process.env.YPERMISSIONS) {
+  const LeveldbPersistence = require('y-leveldb').LeveldbPersistence
+  // do not use process.env.YPERSISTENCE or it will overwrite the thoughtspace leveldb
+  const ldb = new LeveldbPersistence(process.env.YPERMISSIONS)
+  ;(async () => {
+    const persistedYdoc = await ldb.getYDoc(PERMISSIONS_DOCID)
+    const newUpdates = Y.encodeStateAsUpdate(ydoc)
+    ldb.storeUpdate(PERMISSIONS_DOCID, newUpdates)
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc))
+    ydoc.on('update', update => {
+      ldb.storeUpdate(PERMISSIONS_DOCID, update)
+    })
+  })()
+}
 
 /** Authenticates the access token. */
-export const authenticate = (accessToken: string, docid: string, json: any) => {
-  if (accessToken === serverId) return true
-  const permissionsDocId = docid.endsWith('/permissions') ? docid.split('/permissions')[0] : null
-  const ydoc = new Y.Doc()
-  const permissionsProvider = new WebsocketProvider(
-    'ws://localhost:1234',
-    `${permissionsDocId || docid}/permissions`,
-    ydoc,
-    {
-      auth: serverId,
-      WebSocketPolyfill: require('ws'),
-    },
-  )
-  const yPermissions = ydoc.getMap<string>('permissions')
-  console.log('authenticate', { accessToken, docid, json, yPermissions: yPermissions.toJSON() })
-  const role = yPermissions.get(accessToken)
+export const authenticate = (accessToken: string, { name, params }: { name: string; params: any }) => {
+  const docName = name.endsWith('/permissions') ? name.split('/permissions')[0] : name
+  const permissionsDocName = `${docName}/permissions`
+  const permissionsDoc: Y.Doc = getYDoc(permissionsDocName)
+  const yPermissionsServer = ydoc.getMap<string>(docName)
+  let role = yPermissionsServer.get(accessToken)
 
   // if the document has no owner, automatically assign the current user as owner
-  if (yPermissions.size === 0) {
+  if (yPermissionsServer.size === 0) {
     console.info('assigning owner')
-    yPermissions.set(accessToken, 'owner')
-    return true
+    yPermissionsServer.set(accessToken, 'owner')
+    role = 'owner'
+  }
+
+  // Copy permissions from the server-side permissions doc to the client-side permission doc.
+  // The server-side permissions doc keeps all permissions for all documents into memory.
+  // The client-side permissions doc uses authentication and can be exposed to the client via websocket.
+  if (role === 'owner' && params.type === 'auth') {
+    const yPermissionsClient = permissionsDoc.getMap<string>(PERMISSIONS_DOCID)
+    yPermissionsServer.forEach((value, key) => {
+      yPermissionsClient.set(key, value)
+    })
   }
 
   return role === 'owner'
 }
 
-server({ authenticate }).listen(port, host, () => {
+const routes: { [key: string]: (...props: any) => any } = {
+  share: ({ docid, accessToken }: { docid: string; accessToken: string }) => {
+    const permissionsDoc: Y.Doc = getYDoc(docid)
+    const yPermissionsServer = ydoc.getMap<string>(docid)
+    const yPermissionsClient = permissionsDoc.getMap<string>(PERMISSIONS_DOCID)
+    yPermissionsServer.set(accessToken, 'owner')
+    yPermissionsClient.set(accessToken, 'owner')
+  },
+}
+
+createServer({ authenticate, routes }).listen(port, host, () => {
   console.info(`server running at '${host}' on port ${port}`)
 })
