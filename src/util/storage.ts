7b09218425fdd19a5f6a699f0603dba60987128a
Raine Revere <raine@cybersemics.org>
2022-12-22 21:40:20 +0000

    storage: Overload getItem to fix return type.

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,17 +1,22 @@
+function getItem(key: string): string | null
+function getItem(key: string, defaultValue: string | (() => string)): string
+
+/** Gets the item from local storage. If it does not exist and defaultValue is provided, sets the value in local storage to defaultValue and returns it. */
+function getItem(key: string, defaultValue?: string | (() => string)) {
+  let value = localStorage.getItem(key)
+  if (value === null && defaultValue !== undefined) {
+    value = typeof defaultValue === 'function' ? defaultValue() : defaultValue
+    localStorage.setItem(key, value)
+  }
+  return value
+}
+
 const storage = {
   clear(): void {
     localStorage.clear()
   },
 
-  /** Gets the item from local storage. If it does not exist and defaultValue is provided, sets the value in local storage to defaultValue and returns it. */
-  getItem(key: string, defaultValue?: string | (() => string)): string | null {
-    let value = localStorage.getItem(key)
-    if (value === null && defaultValue !== undefined) {
-      value = typeof defaultValue === 'function' ? defaultValue() : defaultValue
-      localStorage.setItem(key, value)
-    }
-    return value
-  },
+  getItem,
 
   removeItem(key: string): void {
     localStorage.removeItem(key)
