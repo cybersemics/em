8e686706dde80fe3f611873b7f33a826fba9b00f
Raine Revere <raine@cybersemics.org>
2023-01-08 03:46:35 +0000

    usePromise

---
+++ src/hooks/usePromise.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import once from '../util/once'
+
+/** A hook that safely subscribes to the result of a promise. Avoids calling setState after unmount. */
+const usePromise = <T>(promise: Promise<T>, defaultValue?: T) => {
+  const [state, setState] = useState<T | undefined>(defaultValue)
+  const unmounted = useRef(false)
+
+  // subscribe to the promise only once
+  const subscribe = useCallback(
+    once(() => {
+      promise.then(value => {
+        // prevent setState being called after unmount
+        if (unmounted.current) return
+        setState(value)
+      })
+    }),
+    [],
+  )
+
+  // Subscribe immediately in case the promise is already resolved.
+  // The next tick will be faster than useEffect if the promise is already resolved.
+  subscribe()
+
+  // prevent setState being called after unmount
+  useEffect(
+    () => () => {
+      unmounted.current = true
+    },
+    [],
+  )
+
+  return state
+}
+
+export default usePromise
