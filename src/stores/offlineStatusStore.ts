5dc325f2cbf4f77b940b8b8a75a99ed1cb4a7ce1
Raine Revere <raine@cybersemics.org>
2023-01-14 03:28:09 +0000

    offlineStatusStore: Fix connected status and idb sync handling and add descriptive comment.

--- src/stores/offlineStatusStore.ts
+++ src/stores/offlineStatusStore.ts
@@ -2,12 +2,7 @@ import OfflineStatus from '../@types/OfflineStatus'
 import ThoughtDb from '../@types/ThoughtDb'
 import WebsocketStatus from '../@types/WebsocketStatus'
 import { HOME_TOKEN, WEBSOCKET_CONNECTION_TIME } from '../constants'
-import {
-  indexeddbProviderThoughtspace,
-  websocketProviderPermissions,
-  websocketProviderThoughtspace,
-  ydoc,
-} from '../data-providers/yjs'
+import { indexeddbProviderThoughtspace, websocketProviderPermissions, ydoc } from '../data-providers/yjs'
 import ministore from './ministore'
 
 /** A store that tracks a derived websocket connection status that includes special statuses for initialization (preconnecting), the first connection attempt (connecting), and offline mode (offline). See: OfflineStatus. */
@@ -15,52 +10,58 @@ export const offlineStatusStore = ministore<OfflineStatus>(
   websocketProviderPermissions.wsconnected ? 'connected' : 'preconnecting',
 )
 
+/* Offline status state machine driven by websocket connection status changes.
+
+  connecting + preconnecting/connecting/offline -> no change
+  connecting + connected/synced -> reconnecting
+  connected + preconnecting -> preconnecting
+    Stay preconnecting when the provider is connected but not yet synced, otherwise the loading indicator will flash.
+  connected + connecting/connected/offline -> connected
+  disconnected -> reconnecting
+
+*/
 websocketProviderPermissions.on('status', (e: { status: WebsocketStatus }) => {
   offlineStatusStore.update(statusOld =>
     e.status === 'connecting'
-      ? // preconnecting/connecting/offline (no change)
-        statusOld === 'preconnecting' || statusOld === 'connecting' || statusOld === 'offline'
+      ? statusOld === 'preconnecting' || statusOld === 'connecting' || statusOld === 'offline'
         ? statusOld
         : 'reconnecting'
-      : // connected (stop reconnecting)
-      e.status === 'connected'
-      ? // Stay preconnecting when the provider becomes connected and wait for synced.
-        // Otherwise the loading indicator will flash.
-        (stopConnecting(), statusOld === 'preconnecting' ? 'preconnecting' : 'connected')
-      : // disconnecting (start reconnecting)
-      e.status === 'disconnected'
+      : e.status === 'connected'
+      ? (stopConnecting(), 'connected')
+      : e.status === 'disconnected'
       ? (startConnecting(), 'reconnecting')
       : (new Error('Unknown connection status: ' + e.status), statusOld),
   )
 })
-websocketProviderThoughtspace.on('synced', () => {
-  stopConnecting()
-  offlineStatusStore.update('synced')
+
+// If the websocket is still connecting for the first time when IDB is synced and non-empty, change the status to reconnecting to dismiss "Connecting..." and render the available thoughts. See: NoThoughts.tsx.
+indexeddbProviderThoughtspace.whenSynced.then(() => {
+  const yThoughtIndex = ydoc.getMap<ThoughtDb>('thoughtIndex')
+  if (Object.keys(yThoughtIndex.get(HOME_TOKEN)?.childrenMap || {}).length > 0) {
+    offlineStatusStore.update(statusOld =>
+      statusOld === 'preconnecting' || statusOld === 'connecting' ? 'reconnecting' : statusOld,
+    )
+  }
 })
 
 /** Enter a connecting state and then switch to offline after a delay. */
 const startConnecting = () => {
   stopConnecting()
-  offlineStatusStore.update('connecting')
+  // Unless this is a new user, IndexedDB probably already loaded the root thoughts and set the status to synced
+  offlineStatusStore.update(statusOld =>
+    statusOld !== 'synced' && statusOld !== 'reconnecting' ? 'connecting' : statusOld,
+  )
   offlineTimer = setTimeout(() => {
     offlineTimer = null
     offlineStatusStore.update('offline')
   }, WEBSOCKET_CONNECTION_TIME)
 }
 
-/** Clears the preconnecting and offline timers, indicating either that we have connected to the websocket server, or have entered offline mode as the client continues connecting in the background. */
+/** Clears the timer, indicating either that we have connected to the websocket server, or have entered offline mode as the client continues connecting in the background. */
 const stopConnecting = () => {
   clearTimeout(offlineTimer!)
   offlineTimer = null
 }
-
-// set status to synced when the thoughtspace loads if the root is not empty
-indexeddbProviderThoughtspace.whenSynced.then(() => {
-  const yThoughtIndex = ydoc.getMap<ThoughtDb>('thoughtIndex')
-  if (Object.keys(yThoughtIndex.get(HOME_TOKEN)?.childrenMap || {}).length > 0) {
-    offlineStatusStore.update('synced')
-  }
-})
 
 let offlineTimer: ReturnType<typeof setTimeout> | null = null
 
@@ -69,7 +70,7 @@ export const init = () => {
   // Start connecting to populate offlineStatusStore.
   // This must done in an init function that is called in app initalize, otherwise @sinonjs/fake-timers are not yet set and createTestApp tests break.
   // TODO: Why does deferring websocketProviderPermissions.connect() to init break tests?
-  offlineTimer = setTimeout(() => startConnecting(), 500)
+  offlineTimer = setTimeout(startConnecting, 500)
 }
 
 export default offlineStatusStore
