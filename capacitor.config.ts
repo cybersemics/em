0a877b50b023ad5fcfa73b32a266a70f5fab7053
Ankit Kumar Karna <ankitkarna.dev@gmail.com>
2023-01-19 15:24:20 +0545

    Environment specific builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,22 +8,22 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') })
 dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}`) })
 dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}.local`) })
 
+const serverConfig =
+  nodeEnv === 'development'
+    ? {
+        server: {
+          url: process.env.CAPACITOR_SERVER_URL,
+          cleartext: true,
+        },
+      }
+    : {}
+
 const config: CapacitorConfig = {
   appId: 'com.emtheapp.em',
   appName: 'em',
   webDir: 'build',
   bundledWebRuntime: false,
-  server: {
-    url: process.env.CAPACITOR_SERVER_URL,
-    cleartext: true,
-  },
-  plugins: {
-    Keyboard: {
-      resize: 'none',
-      style: 'DARK',
-      resizeOnFullScreen: true,
-    },
-  },
+  ...serverConfig,
 }
 
 export default config
