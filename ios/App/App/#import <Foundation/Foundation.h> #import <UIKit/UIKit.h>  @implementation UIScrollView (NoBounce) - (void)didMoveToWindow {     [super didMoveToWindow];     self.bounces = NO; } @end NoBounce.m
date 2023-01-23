1bc45300eb3bcebc505ce50a360152af0384d0c3
Ankit Kumar Karna <ankitkarna.dev@gmail.com>
2023-01-23 22:30:24 +0545

    Removed circular dependency of NoBounce.m file

--- ios/App/App/#import <Foundation/Foundation.h> #import <UIKit/UIKit.h>  @implementation UIScrollView (NoBounce) - (void)didMoveToWindow {     [super didMoveToWindow];     self.bounces = NO; } @end NoBounce.m
+++
@@ -1,8 +0,0 @@
-//
-//  UIKit.h>  @implementation UIScrollView (NoBounce) - (void)didMoveToWindow {     [super didMoveToWindow];     self.bounces = NO; } @end NoBounce.m
-//  App
-//
-//  Created by Ankit Kumar Karna on 17/01/2023.
-//
-
-#import <Foundation/Foundation.h>
