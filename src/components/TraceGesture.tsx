cbb32caadf8a06097e083557ebaef56916b7ddd8
Raine Revere <raine@cybersemics.org>
2023-02-01 13:22:41 +0000

    TraceGesture: Make preventDefault noop on pointer events to allow tap-to-edit.

--- src/components/TraceGesture.tsx
+++ src/components/TraceGesture.tsx
@@ -4,7 +4,7 @@ import SignaturePad from 'react-signature-pad-wrapper'
 import { CSSTransition } from 'react-transition-group'
 import GesturePath from '../@types/GesturePath'
 import State from '../@types/State'
-import { AlertType, GESTURE_CANCEL_ALERT_TEXT, Settings } from '../constants'
+import { AlertType, GESTURE_CANCEL_ALERT_TEXT, NOOP, Settings } from '../constants'
 import getUserSetting from '../selectors/getUserSetting'
 import themeColors from '../selectors/themeColors'
 import { gestureString, globalShortcuts } from '../shortcuts'
@@ -71,11 +71,21 @@ const TraceGesture = ({ eventNodeRef, gestureStore }: TraceGestureProps) => {
     if (!signaturePadRef.current) return
     const signaturePad = signaturePadRef.current.signaturePad
 
+    // Attach pointer handlers to a provided node rather than the signature pad canvas.
+    // See: eventNodeRef
     const handlePointerStart = signaturePad._handlePointerStart.bind(signaturePad)
     const handlePointerMove = signaturePad._handlePointerMove.bind(signaturePad)
     if (eventNodeRef?.current) {
-      eventNodeRef.current.addEventListener('pointerdown', handlePointerStart)
-      eventNodeRef.current.addEventListener('pointermove', handlePointerMove)
+      eventNodeRef.current.addEventListener('pointerdown', e => {
+        // Make preventDefault a noop otherwise tap-to-edit is broken.
+        // e.cancelable is readonly and monkeypatching preventDefault is easier than copying e.
+        e.preventDefault = NOOP
+        handlePointerStart(e)
+      })
+      eventNodeRef.current.addEventListener('pointermove', e => {
+        e.preventDefault = NOOP
+        handlePointerMove(e)
+      })
     }
 
     signaturePad.addEventListener('beginStroke', onBeginStroke)
