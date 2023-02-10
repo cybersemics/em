594261aa6fcb3915dce6ca782623c69b698c5d04
Raine Revere <raine@cybersemics.org>
2023-02-10 16:38:52 +0000

    compareThought: Minor refactor

--- src/util/compareThought.ts
+++ src/util/compareThought.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import ComparatorFunction from '../@types/ComparatorFunction'
 import ComparatorValue from '../@types/ComparatorValue'
 import Thought from '../@types/Thought'
@@ -34,18 +35,16 @@ const removeDiacritics = (s: string): string => {
 }
 
 /** Remove emojis and trailing/leading spaces from camparator inputs using regex. */
-const removeEmojisAndSpaces = (str: string) => str.replace(EMOJI_REGEX_GLOBAL, '').trim()
+const removeEmojisAndSpaces = (s: string) => s.replace(EMOJI_REGEX_GLOBAL, '').trim()
 
 /** Remove ignored prefixes from comparator inputs. */
-const removeIgnoredPrefixes = (str: string) => str.replace(regexIgnoredPrefixes, '$2')
+const removeIgnoredPrefixes = (s: string) => s.replace(regexIgnoredPrefixes, '$2')
 
 /** Removes emojis, spaces, and prefix 'the' to make a string comparable.  */
-const removeUncomparableCharacters = (str: string) => {
-  return removeDiacritics(removeIgnoredPrefixes(removeEmojisAndSpaces(str)))
-}
+const normalizeCharacters = _.flow(removeEmojisAndSpaces, removeIgnoredPrefixes, removeDiacritics)
 
 /** Parse a date string and handle M/d (e.g. "2/1") for Safari. */
-const parseDate = (s: string) =>
+const parseDate = (s: string): number =>
   Date.parse(
     // eslint-disable-next-line @typescript-eslint/no-extra-parens
     regexShortDateWithDash.test(s)
@@ -57,51 +56,54 @@ const parseDate = (s: string) =>
   )
 
 /** Returns trure if the given string is an integer or decimal number. Recognizes prefixed number strings like "#1" and "$1" as numbers. */
-const isNumber = (x: number | string) => !isNaN(toNumber(x))
+const isNumber = (x: number | string): boolean => !isNaN(toNumber(x))
 
-/** Converts a string to a number. If given a number, returns it as-is. If given a string with a prefixe such as "#" or "$", strips it and returns the actual number. If the input cannot be converted to a number, returns NaN. */
-const toNumber = (x: number | string) => (typeof x === 'number' ? x : +x.replace(/^[$₹₤₱₠₪₨€#] ?/, ''))
+/** Converts a string to a number. If given a number, returns it as-is. If given a string with a prefixe such as "#" or "$", strips it and returns the actual number. If given a number range, returns the start of the range. If the input cannot be converted to a number, returns NaN. */
+const toNumber = (x: number | string): number => (typeof x === 'number' ? x : +x.replace(/^[$₹₤₱₠₪₨€#] ?/, ''))
 
 /** The default comparator that uses the ">" operator. Can be passed to Array.prototype.sort. */
-export const compare = <T>(a: T, b: T): ComparatorValue => (a > b ? 1 : a < b ? -1 : 0)
+export const compare = <T>(a: T, b: T) => (a > b ? 1 : a < b ? -1 : 0)
 
 /** A comparator that sorts emojis above non-emojis. */
-export const compareStringsWithEmoji = (a: string, b: string) => {
+export const compareStringsWithEmoji: ComparatorFunction<string> = (a: string, b: string) => {
   const aStartsWithEmoji = !!a.match(STARTS_WITH_EMOJI_REGEX)
   const bStartsWithEmoji = !!b.match(STARTS_WITH_EMOJI_REGEX)
   return aStartsWithEmoji && !bStartsWithEmoji ? -1 : bStartsWithEmoji && !aStartsWithEmoji ? 1 : 0
 }
 
 /** A comparator that sorts meta-attributes above everything. */
-export const compareStringsWithMetaAttributes = (a: string, b: string) => {
+export const compareStringsWithMetaAttributes: ComparatorFunction<string> = (a: string, b: string) => {
   const aIsMetaAttribute = isAttribute(a)
   const bIsMetaAttribute = isAttribute(b)
   return aIsMetaAttribute && !bIsMetaAttribute ? -1 : bIsMetaAttribute && !aIsMetaAttribute ? 1 : 0
 }
 
 /** A comparator that sorts empty thoughts ahead of non-empty thoughts. */
-export const compareEmpty = (a: string, b: string): ComparatorValue => {
+export const compareEmpty: ComparatorFunction<string> = (a: string, b: string) => {
   const aIsEmpty = a === ''
   const bIsEmpty = b === ''
   return aIsEmpty && !bIsEmpty ? -1 : bIsEmpty && !aIsEmpty ? 1 : 0
 }
 
 /** A comparator that sorts numbers ahead of non-numbers. */
-export const compareNumberAndOther = (a: number | string, b: number | string): ComparatorValue => {
+export const compareNumberAndOther: ComparatorFunction<number | string> = (a: number | string, b: number | string) => {
   const aIsNum = isNumber(a)
   const bIsNum = isNumber(b)
   return aIsNum && !bIsNum ? -1 : bIsNum && !aIsNum ? 1 : 0
 }
 
 /** A comparator that sorts numbers in numeric order. */
-export const compareNumbers = (a: number | string, b: number | string): ComparatorValue => {
+export const compareNumbers: ComparatorFunction<number | string> = (
+  a: number | string,
+  b: number | string,
+): ComparatorValue => {
   const aIsNum = isNumber(a)
   const bIsNum = isNumber(b)
   return aIsNum && bIsNum ? compare(toNumber(a), toNumber(b)) : 0
 }
 
 /** A case-insensitive lexicographic comparator. */
-export const compareLowercase = (a: string, b: string): ComparatorValue => compare(lower(a), lower(b))
+export const compareLowercase: ComparatorFunction<string> = (a: string, b: string) => compare(lower(a), lower(b))
 
 /** A comparator function that sorts strings that start with punctuation above others. */
 export const comparePunctuationAndOther = <T, U>(a: T, b: U): ComparatorValue => {
@@ -111,12 +113,11 @@ export const comparePunctuationAndOther = <T, U>(a: T, b: U): ComparatorValue =>
 }
 
 /** A comparison function that sorts date strings. */
-export const compareDateStrings = (a: string, b: string): ComparatorValue => {
-  return compare(parseDate(a), parseDate(b))
-}
+export const compareDateStrings: ComparatorFunction<string> = (a: string, b: string) =>
+  compare(parseDate(a), parseDate(b))
 
 /** A comparator function that sorts date strings above others. */
-const compareDateAndOther = (a: string, b: string): ComparatorValue => {
+const compareDateAndOther: ComparatorFunction<string> = (a: string, b: string) => {
   const aIsDate = !isNaN(parseDate(a))
   const bIsDate = !isNaN(parseDate(b))
   return aIsDate && !bIsDate ? -1 : bIsDate && !aIsDate ? 1 : 0
@@ -140,7 +141,7 @@ export const makeOrderedComparator =
  * 2. dates (9/1, 10/1, 11/1)
  * 3. lexicographic (default)
  */
-const compareReadableText = makeOrderedComparator<string>([
+const compareReadableText: ComparatorFunction<string> = makeOrderedComparator<string>([
   compareNumberAndOther,
   compareNumbers,
   compareDateAndOther,
@@ -155,22 +156,22 @@ const compareReadableText = makeOrderedComparator<string>([
  * 3. emoji
  * 4. compareReadableText on text without emoji
  */
-export const compareReasonable = makeOrderedComparator<string>([
+export const compareReasonable: ComparatorFunction<string> = makeOrderedComparator<string>([
   compareEmpty,
   comparePunctuationAndOther,
   compareStringsWithMetaAttributes,
   compareStringsWithEmoji,
-  (a, b) => compareReadableText(removeUncomparableCharacters(a), removeUncomparableCharacters(b)),
+  (a, b) => compareReadableText(normalizeCharacters(a), normalizeCharacters(b)),
 ])
 
 /** Get reverse of the given comparator. */
 const reverse =
-  <T>(comparator: ComparatorFunction<T>) =>
+  <T>(comparator: ComparatorFunction<T>): ComparatorFunction<T> =>
   (a: T, b: T) =>
     comparator(b, a)
 
 /** Compare the value of two thoughts. If the thought has a sortValue, it takes precedence over value. This preserves the sort order of a thought edited to empty instead of moving it to the top of thi list. */
-export const compareThought = (a: Thought, b: Thought) =>
+export const compareThought: ComparatorFunction<Thought> = (a: Thought, b: Thought) =>
   compareReasonable(a.sortValue || a.value, b.sortValue || b.value)
 
 /** A comparator that sorts in descending order. */
