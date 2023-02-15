3a8a9593bb19f12df78e057d61684b0aa4fd2287
Raine Revere <raine@cybersemics.org>
2023-02-15 21:01:29 +0000

    Remove old bold, italic, underline reducers.

--- src/action-creators/bold.ts
+++
@@ -1,6 +0,0 @@
-import Thunk from '../@types/Thunk'
-
-/** Action-creator for bold. */
-const boldActionCreator = (): Thunk => dispatch => dispatch({ type: 'bold' })
-
-export default boldActionCreator
