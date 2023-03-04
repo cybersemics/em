553476ab7689a47ff55ec4e5da047070cb7aba18
Raine Revere <raine@cybersemics.org>
2023-03-04 04:11:12 +0000

    Convert TutorialNavigationPrev to typescript.

--- src/components/Tutorial/TutorialNavigationPrev.tsx
+++ src/components/Tutorial/TutorialNavigationPrev.tsx
@@ -5,7 +5,7 @@ import store from '../../stores/app'
 import TutorialNavigationButton from './TutorialNavigationButton'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const TutorialNavigationPrev = ({ tutorialStep }) => (
+const TutorialNavigationPrev = ({ tutorialStep }: { tutorialStep: number }) => (
   <TutorialNavigationButton
     classes='tutorial-prev'
     disabled={tutorialStep === TUTORIAL_STEP_START}
