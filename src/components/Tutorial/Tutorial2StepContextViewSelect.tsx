99003b5f410936cf835234477a9c9931847ba718
Raine Revere <raine@cybersemics.org>
2023-03-04 04:53:52 +0000

    Convert Tutorial2StepContextViewSelect to typescript.

--- src/components/Tutorial/Tutorial2StepContextViewSelect.tsx
+++ src/components/Tutorial/Tutorial2StepContextViewSelect.tsx
@@ -5,7 +5,7 @@ import getContexts from '../../selectors/getContexts'
 import store from '../../stores/app'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const Tutorial2StepContextViewSelect = ({ tutorialChoice }) => {
+const Tutorial2StepContextViewSelect = ({ tutorialChoice }: { tutorialChoice: keyof typeof TUTORIAL_CONTEXT }) => {
   const caseSensitiveValue =
     getContexts(store.getState(), TUTORIAL_CONTEXT[tutorialChoice]).length > 0
       ? TUTORIAL_CONTEXT[tutorialChoice]
