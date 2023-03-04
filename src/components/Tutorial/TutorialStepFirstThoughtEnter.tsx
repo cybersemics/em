23e54e5bf5c54b92fe73e1642bfef4fde0bb12e5
Raine Revere <raine@cybersemics.org>
2023-03-04 05:03:18 +0000

    Convert TutorialStepFirstThoughtEnter to typescript.

--- src/components/Tutorial/TutorialStepFirstThoughtEnter.tsx
+++ src/components/Tutorial/TutorialStepFirstThoughtEnter.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { useStore } from 'react-redux'
+import { useSelector } from 'react-redux'
+import State from '../../@types/State'
 import { isTouch } from '../../browser'
 import headValue from '../../util/headValue'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const TutorialStepFirstThoughtEnter = ({ cursor }) => {
-  const store = useStore()
-
+const TutorialStepFirstThoughtEnter = () => {
+  const ready = useSelector((state: State) => !state.cursor || headValue(state, state.cursor).length > 0)
   return (
     <>
       <p>You did it!</p>
-      {!cursor || headValue(store.getState(), cursor).length > 0 ? (
+      {ready ? (
         <p>{isTouch ? 'Tap' : 'Click'} the Next button when you are done entering your thought.</p>
       ) : (
         <p>Now type something. Anything will do.</p>
