baa1377658a1c8ba075aed99c300864846e49128
Raine Revere <raine@cybersemics.org>
2023-03-04 04:24:53 +0000

    Convert Tutorial2StepContext2 to typescript.

--- src/components/Tutorial/Tutorial2StepContext2.tsx
+++ src/components/Tutorial/Tutorial2StepContext2.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { useStore } from 'react-redux'
+import { useSelector } from 'react-redux'
+import Path from '../../@types/Path'
+import State from '../../@types/State'
+import Thought from '../../@types/Thought'
 import { isMac, isTouch } from '../../browser'
 import { TUTORIAL_CONTEXT, TUTORIAL_CONTEXT2_PARENT } from '../../constants'
 import childIdsToThoughts from '../../selectors/childIdsToThoughts'
@@ -7,9 +10,23 @@ import headValue from '../../util/headValue'
 import TutorialHint from './TutorialHint'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const Tutorial2StepContext2 = ({ tutorialChoice, rootChildren, cursor }) => {
-  const store = useStore()
-  const cursorThought = childIdsToThoughts(store.getState(), cursor)
+const Tutorial2StepContext2 = ({
+  tutorialChoice,
+  rootChildren,
+  cursor,
+}: {
+  cursor: Path | null
+  tutorialChoice: keyof typeof TUTORIAL_CONTEXT
+  rootChildren: Thought[]
+}) => {
+  const readyToType = useSelector((state: State) => {
+    if (!state.cursor) return false
+    const cursorThought = childIdsToThoughts(state, state.cursor)
+    return cursorThought.length === 2 && cursorThought[0].value === TUTORIAL_CONTEXT2_PARENT[tutorialChoice]
+  })
+  const select = useSelector(
+    (state: State) => !cursor || headValue(state, cursor) !== TUTORIAL_CONTEXT2_PARENT[tutorialChoice],
+  )
 
   return (
     <>
@@ -27,15 +44,11 @@ const Tutorial2StepContext2 = ({ tutorialChoice, rootChildren, cursor }) => {
             <TutorialHint>
               <br />
               <br />
-              {cursorThought &&
-              cursorThought.length === 2 &&
-              cursorThought[0].value === TUTORIAL_CONTEXT2_PARENT[tutorialChoice] ? (
+              {readyToType ? (
                 `Type "${TUTORIAL_CONTEXT[tutorialChoice]}."`
               ) : (
                 <>
-                  {!cursor || headValue(store.getState(), cursor) !== TUTORIAL_CONTEXT2_PARENT[tutorialChoice]
-                    ? `Select "${TUTORIAL_CONTEXT2_PARENT[tutorialChoice]}". `
-                    : null}
+                  {select ? `Select "${TUTORIAL_CONTEXT2_PARENT[tutorialChoice]}". ` : null}
                   {isTouch
                     ? 'Trace the line below with your finger'
                     : `Hold ${isMac ? 'Command' : 'Ctrl'} and hit Enter`}{' '}
