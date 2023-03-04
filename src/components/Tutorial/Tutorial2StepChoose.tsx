c02857556b5ef8d32a193e1287a593632f614d86
Raine Revere <raine@cybersemics.org>
2023-03-04 04:16:56 +0000

    Convert Tutorial2StepChoose to typescript.

---
+++ src/components/Tutorial/Tutorial2StepChoose.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+const Tutorial2StepChoose = () => (
+  <p>
+    For this tutorial, choose what kind of content you want to create. You will learn the same command regardless of
+    which one you choose.
+  </p>
+)
+
+export default Tutorial2StepChoose
