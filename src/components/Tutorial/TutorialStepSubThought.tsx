5bdda3fc2cb729332ba28f6e44d1e1c3f3c6f9ee
Raine Revere <raine@cybersemics.org>
2023-03-04 04:44:19 +0000

    Convert TutorialStepSubThought to typescript.

--- src/components/Tutorial/TutorialStepSubThought.tsx
+++ src/components/Tutorial/TutorialStepSubThought.tsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import { useStore } from 'react-redux'
+import { useSelector } from 'react-redux'
+import State from '../../@types/State'
 import { isMac, isTouch } from '../../browser'
 import headValue from '../../util/headValue'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-const TutorialStepSubThought = ({ cursor }) => {
-  const state = useStore().getState()
-
-  const headCursorValue = cursor && headValue(state, cursor)
+const TutorialStepSubThought = () => {
+  const headCursorValue = useSelector((state: State) => state.cursor && headValue(state, state.cursor))
+  const deleteBlank = useSelector((state: State) => state.cursor && headCursorValue === '')
+  const noCursor = useSelector((state: State) => !state.cursor)
   return (
     <>
       <p>
         Now I am going to show you how to add a thought <i>within</i> another thought.
       </p>
-      {cursor && headCursorValue === '' ? (
-        <p>Hit the Delete key to delete the current blank thought. It's not needed right now.</p>
-      ) : null}
-      {!cursor ? (
+      {deleteBlank ? <p>Hit the Delete key to delete the current blank thought. It's not needed right now.</p> : null}
+      {noCursor ? (
         <p>{isTouch ? 'Tap' : 'Click'} a thought to select it.</p>
       ) : (
         <p>
           {isTouch
             ? 'Trace the line below'
-            : `${cursor && headCursorValue === '' ? 'Then h' : 'H'}old the ${
+            : `${!noCursor && headCursorValue === '' ? 'Then h' : 'H'}old the ${
                 isMac ? 'Command' : 'Ctrl'
               } key and hit the Enter key`}
           .
