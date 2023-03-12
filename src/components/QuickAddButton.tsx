ef990f53f821229ae5c681db103de60780af1751
Raine Revere <raine@cybersemics.org>
2023-03-12 03:31:20 +0000

    QuickAddButton: fast click

--- src/components/QuickAddButton.tsx
+++ src/components/QuickAddButton.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import State from '../@types/State'
 import toggleAbsoluteContext from '../action-creators/toggleAbsoluteContext'
+import fastClick from '../util/fastClick'
 import isAbsolute from '../util/isAbsolute'
 import AddIcon from './AddIcon'
 
@@ -19,7 +20,7 @@ const QuickAddButton: React.FC = () => {
         'quick-add-button': true,
         rotate: isAbsolute(rootContext),
       })}
-      onClick={() => dispatch(toggleAbsoluteContext())}
+      {...fastClick(() => dispatch(toggleAbsoluteContext()))}
       title='Quick Add'
     >
       <AddIcon size={20} />
