86628d726fadc5ed3267338c953ae127d6c706c3
Raine Revere <raine@cybersemics.org>
2023-05-13 14:51:27 +0000

    createThoughts: Reuse parent.childrenMap instead of calculating anew.
    Otherwise if siblings are pending the childrenMap may be incomplete.

--- src/reducers/createThought.ts
+++ src/reducers/createThought.ts
@@ -7,10 +7,9 @@ import Thought from '../@types/Thought'
 import ThoughtId from '../@types/ThoughtId'
 import { clientId } from '../data-providers/yjs'
 import updateThoughts from '../reducers/updateThoughts'
-import { getAllChildren } from '../selectors/getChildren'
 import getLexeme from '../selectors/getLexeme'
 import getThoughtById from '../selectors/getThoughtById'
-import createChildrenMap from '../util/createChildrenMap'
+import { childrenMapKey } from '../util/createChildrenMap'
 import createId from '../util/createId'
 import hashThought from '../util/hashThought'
 import head from '../util/head'
@@ -31,8 +30,6 @@ interface Payload {
 }
 /**
  * Creates a new thought with a known context and rank. Does not update the cursor. Use the newThought reducer for a higher level function.
- *
- * @param addAsContext Adds the given context to the new thought.
  */
 const createThought = (state: State, { path, value, rank, id, idbSynced, children, splitSource }: Payload) => {
   id = id || createId()
@@ -56,9 +53,11 @@ const createThought = (state: State, { path, value, rank, id, idbSynced, childre
   if (path.length > 0) {
     const newValue = value
 
-    const siblings = getAllChildren(state, parentId)
-      .filter(child => child !== id)
-      .concat(id)
+    // TODO: Why is a duplicate id encountered sometimes?
+    // const duplicateId = getAllChildren(state, parentId).find(childId => childId === id)
+    // if (duplicateId) {
+    //   throw new Error(`Parent ${parent.value} (${parentId}) already contains thought ${duplicateId}`)
+    // }
 
     const thoughtNew: Thought = {
       id,
@@ -71,19 +70,15 @@ const createThought = (state: State, { path, value, rank, id, idbSynced, childre
       updatedBy: clientId,
       ...(splitSource ? { splitSource } : null),
     }
+
     thoughtIndexUpdates[id] = thoughtNew
-
-    // insert the new thought into the state just for createChildrenMap
-    // otherwise createChildrenMap will not be able to find the new child and thus not properly detect meta attributes which are stored differently
-    const stateWithNewThought = {
-      ...state,
-      thoughts: { ...state.thoughts, thoughtIndex: { ...state.thoughts.thoughtIndex, [id]: thoughtNew } },
-    }
-
     thoughtIndexUpdates[parentId] = {
       ...parent,
       id: parentId,
-      childrenMap: createChildrenMap(stateWithNewThought, siblings),
+      childrenMap: {
+        ...parent.childrenMap,
+        [childrenMapKey(parent.childrenMap, thoughtNew)]: id,
+      },
       lastUpdated: timestamp(),
       updatedBy: clientId,
     }
@@ -91,23 +86,12 @@ const createThought = (state: State, { path, value, rank, id, idbSynced, childre
 
   // if adding as the context of an existing thought
   let lexemeNew: Lexeme | undefined // eslint-disable-line fp/no-let
-  // if (addAsContext) {
-  //   const lexemeOld = getLexeme(state, parent.value)
-  //   lexemeNew = {
-  //     ...lexemeOld!,
-  //     contexts: (lexemeOld?.contexts || []).concat(id),
-  //     created: lexemeOld?.created || timestamp(),
-  //     lastUpdated: timestamp(),
-  //     updatedBy: clientId,
-  //   }
-  // } else {
   lexeme.contexts = !lexeme.contexts
     ? []
     : // floating thought (no context)
     path.length > 0
     ? [...lexeme.contexts, id]
     : lexeme.contexts
-  // }
 
   const lexemeIndexUpdates = {
     [hashThought(lexeme.lemma)]: lexeme,
--- src/util/createChildrenMap.ts
+++ src/util/createChildrenMap.ts
@@ -1,17 +1,21 @@
 import Index from '../@types/IndexType'
 import State from '../@types/State'
+import Thought from '../@types/Thought'
 import ThoughtId from '../@types/ThoughtId'
 import childIdsToThoughts from '../selectors/childIdsToThoughts'
 import isAttribute from '../util/isAttribute'
 import keyValueBy from '../util/keyValueBy'
 
-/** Generates an object for O(1) lookup of a thought's children. Meta attributes are keyed by value and normal are keyed by id. Missing thoughts are excluded. */
-const createChildrenMap = (state: State, childrenIds: ThoughtId[]): Index<ThoughtId> =>
-  keyValueBy(childIdsToThoughts(state, childrenIds), (child, i, accum) => {
-    // use id as key for duplicate child attributes
-    const key = child && isAttribute(child.value) && !accum[child.value] ? child.value : child.id
+/** Creates a childrenMapKey based on the value of meta thoughts and the id for non-meta thoughts. Always use the id as key if there is a duplicate meta value. */
+export const childrenMapKey = (thoughtIndex: Index<ThoughtId>, child: Thought) =>
+  child && isAttribute(child.value) && !thoughtIndex[child.value] ? child.value : child.id
 
-    return { [key]: child.id }
-  })
+/** Generates an object for O(1) lookup of a thought's children. Meta attributes are keyed by value and normal are keyed by id. Missing thoughts are excluded. */
+const createChildrenMap = (state: State, childrenIds: ThoughtId[]): Index<ThoughtId> => {
+  const children = childIdsToThoughts(state, childrenIds)
+  return keyValueBy(children, (child, i, accum) => ({
+    [childrenMapKey(accum, child)]: child.id,
+  }))
+}
 
 export default createChildrenMap
