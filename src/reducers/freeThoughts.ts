bf017072ec9a71c228d06eff55aab67157d2d133
Raine Revere <raine@cybersemics.org>
2023-05-25 16:20:53 +0000

    freeThoughts: Randomize start index.

--- src/reducers/freeThoughts.ts
+++ src/reducers/freeThoughts.ts
@@ -1,4 +1,5 @@
 import State from '../@types/State'
+import Thought from '../@types/Thought'
 import { ABSOLUTE_TOKEN, EM_TOKEN, HOME_TOKEN, MAX_THOUGHTS, MAX_THOUGHTS_MARGIN } from '../constants'
 import globals from '../globals'
 import { getAllChildren } from '../selectors/getChildren'
@@ -8,7 +9,7 @@ import head from '../util/head'
 import isAttribute from '../util/isAttribute'
 import deleteThought from './deleteThought'
 
-/** Frees invisible thoughts from memory when the memory limit is exceeded. Note: May not free any thoughts if all thoughts are expanded. */
+/** Frees a random block of invisible thoughts from memory when the memory limit is exceeded. Note: May not free any thoughts if all thoughts are expanded. */
 const freeThoughts = (state: State) => {
   const expandedIds = Object.values(state.expanded).map(head)
   const preserveSet = new Set([
@@ -21,20 +22,37 @@ const freeThoughts = (state: State) => {
     ...getDescendantThoughtIds(state, EM_TOKEN),
   ])
 
+  // Split the list at a random index to avoid the same thoughts getting deallocated every time.
+  // Better would be to sort by lastUpdated or lastVisited, but this is much faster.
+  const randomStartIndex = Math.floor(Math.random() * Object.values(state.thoughts.thoughtIndex).length)
+
   /** Find the next thought that can be safely deallocated. */
-  const findDeletableThought = (state: State) => {
+  const findDeletableThought = (state: State): Thought | null => {
     const allThoughts = Object.values(state.thoughts.thoughtIndex)
-    return allThoughts.find(
-      thought =>
-        // do not delete any thought or child of a thought in the preserve set
-        !preserveSet.has(thought.id) &&
-        !preserveSet.has(thought.parentId) &&
-        // do not delete a thought with a missing parent
-        state.thoughts.thoughtIndex[thought.parentId] &&
-        // do not delete meta attributes, or their descendants
-        !isAttribute(thought.value) &&
-        !thoughtToPath(state, thought.parentId).some(id => isAttribute(state.thoughts.thoughtIndex[id]?.value)),
-    )
+
+    /** Returns true if a thought is safe to deallocate. */
+    const isDeletable = (thought: Thought) =>
+      // do not delete any thought or child of a thought in the preserve set
+      !preserveSet.has(thought.id) &&
+      !preserveSet.has(thought.parentId) &&
+      // do not delete a thought with a missing parent
+      state.thoughts.thoughtIndex[thought.parentId] &&
+      // do not delete meta attributes, or their descendants
+      !isAttribute(thought.value) &&
+      !thoughtToPath(state, thought.parentId).some(id => isAttribute(state.thoughts.thoughtIndex[id]?.value))
+
+    // start searching at the randomStartIndex and wrap around the end of the array of thoughts
+    let deletableThought: Thought | null = null
+    // eslint-disable-next-line fp/no-loops
+    for (let i = 0; i++; i < allThoughts.length) {
+      const thought = allThoughts[(i + randomStartIndex) % allThoughts.length]
+      if (isDeletable(thought)) {
+        deletableThought = thought
+        break
+      }
+    }
+
+    return deletableThought
   }
 
   // iterate over the entire thoughtIndex, deleting thoughts that are no longer visible
