1e970fc9583f50fcb36809d19d9ff1827a8322a9
Raine Revere <raine@cybersemics.org>
2023-06-23 23:43:21 +0000

    comment

--- src/util/isRoot.ts
+++ src/util/isRoot.ts
@@ -1,7 +1,7 @@
 import ThoughtId from '../@types/ThoughtId'
 import { ROOT_CONTEXTS } from '../constants'
 
-/** Returns true if the Thoughts or Path is the one of the root contexts. */
+/** Returns true if the Thoughts or Path is the one of the root contexts (HOME or ABSOLUTE). */
 const isRoot = (thoughts: (string | ThoughtId)[]): boolean => {
   return thoughts.length === 1 && !!thoughts[0] && (ROOT_CONTEXTS as string[]).includes(thoughts[0])
 }
