596dae9e6e806aa335aa62a511ebe6b6b50051dc
Raine Revere <raine@cybersemics.org>
2023-08-15 19:17:22 +0000

    Jest: Transform yjs libraries with babel and define crypto.

--- react-app-rewired.config.js
+++ react-app-rewired.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  jest: config => {
+    return {
+      ...config,
+      transformIgnorePatterns: [
+        // default patterns
+        // '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+        '^.+\\.module\\.(css|sass|scss)$',
+
+        // yjs, y-indexddb, and lib0 need to be transformed.
+        // The original regex ignored everything in node_modules.
+        '[/\\\\]node_modules[/\\\\](?!(yjs|y-indexeddb|lib0)).+\\.(js|cjs|jsx|mjs|ts|tsx)$',
+      ],
+    }
+  },
   // Update webpack config to use custom loader for worker files
   webpack: config => {
     // Note: It's important that the "worker-loader" gets defined BEFORE the TypeScript loader!
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,21 @@
 import '@testing-library/jest-dom'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import crypto from 'crypto'
 import { configure } from 'enzyme'
 import 'fake-indexeddb/auto'
 import 'jest-localstorage-mock'
 import { noop } from 'lodash'
 
 configure({ adapter: new Adapter() })
+
+// define global crypto so that yjs can be imported in jest tests
+// https://stackoverflow.com/questions/52612122/how-to-use-jest-to-test-functions-using-crypto-or-window-mscrypto
+// eslint-disable-next-line fp/no-mutating-methods
+Object.defineProperty(global, 'crypto', {
+  value: {
+    getRandomValues: arr => crypto.randomBytes(arr.length),
+  },
+})
 
 // add noop functions to prevent implementation error during test
 window.blur = noop
