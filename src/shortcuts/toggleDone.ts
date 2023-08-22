6ef9d6ff426d6509f9bc849feb7d9292000a7261
Raine Revere <raine@cybersemics.org>
2023-08-22 01:55:07 +0000

    Fix toggleDone and add toolbar icon.

--- src/shortcuts/toggleDone.ts
+++ src/shortcuts/toggleDone.ts
@@ -1,6 +1,9 @@
 import Shortcut from '../@types/Shortcut'
+import toggleThought from '../action-creators/toggleThought'
+import Icon from '../components/icons/Check'
+import findDescendant from '../selectors/findDescendant'
+import head from '../util/head'
 import isDocumentEditable from '../util/isDocumentEditable'
-import pathToContext from '../util/pathToContext'
 
 /** Toggle the built-in =done style to cross out an item. */
 const toggleDone: Shortcut = {
@@ -8,16 +11,22 @@ const toggleDone: Shortcut = {
   label: 'Mark as done',
   keyboard: { alt: true, shift: true, key: 'Enter' },
   canExecute: getState => isDocumentEditable() && !!getState().cursor,
+  isActive: getState => {
+    const state = getState()
+    const cursor = state.cursor
+    return !!cursor && !!findDescendant(state, head(cursor), ['=done'])
+  },
   exec: (dispatch, getState) => {
     const state = getState()
     const cursor = state.cursor!
-    const context = pathToContext(state, cursor)
-    dispatch({
-      type: 'toggleAttribute',
-      context,
-      key: '=done',
-    })
+    dispatch(
+      toggleThought({
+        path: cursor,
+        values: ['=done'],
+      }),
+    )
   },
+  svg: Icon,
 }
 
 export default toggleDone
