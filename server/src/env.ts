54c12815543a9c897c0d7bb04fc69409965bec34
Raine Revere <raine@cybersemics.org>
2023-09-24 19:48:03 +0000

    server/env: Rename currentEnv -> nodeEnv.

--- server/src/env.ts
+++ server/src/env.ts
@@ -4,12 +4,12 @@ import path from 'path'
 // import is not working in commonjs build
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv')
-const currentEnv: string | undefined = process.env.NODE_ENV?.toLowerCase()
+const nodeEnv: string | undefined = process.env.NODE_ENV?.toLowerCase() || 'development'
 
 dotenv.config({ path: path.resolve(process.cwd(), '.env') })
 dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
 
-if (currentEnv) {
-  dotenv.config({ path: path.resolve(process.cwd(), `.env.${currentEnv}`) })
-  dotenv.config({ path: path.resolve(process.cwd(), `.env.${currentEnv}.local`) })
+if (nodeEnv) {
+  dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}`) })
+  dotenv.config({ path: path.resolve(process.cwd(), `.env.${nodeEnv}.local`) })
 }
