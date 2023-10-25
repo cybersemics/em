dea93ffcbf342da6febe94e715615a4db1e4edea
Raine Revere <raine@cybersemics.org>
2023-10-25 15:28:16 +0000

    nextSibling: Return null for root context.

--- src/selectors/nextSibling.ts
+++ src/selectors/nextSibling.ts
@@ -4,11 +4,13 @@ import Thought from '../@types/Thought'
 import ThoughtId from '../@types/ThoughtId'
 import { getChildrenSorted } from '../selectors/getChildren'
 import head from '../util/head'
+import isRoot from '../util/isRoot'
 import getThoughtById from './getThoughtById'
 
 /** Gets the next sibling after a thought according to its parent's sort preference. Normal view only. TODO: Add support for context view. */
 const nextSibling = (state: State, idOrPath: ThoughtId | Path): Thought | null => {
   const id = typeof idOrPath === 'string' ? idOrPath : head(idOrPath)
+  if (isRoot([id])) return null
   const thought = getThoughtById(state, id)
   if (!thought) return null
   const siblings = getChildrenSorted(state, thought.parentId)
