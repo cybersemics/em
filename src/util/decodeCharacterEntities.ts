b870e38f10de763f3e091ff482710e16ad0ff191
Raine Revere <raine@cybersemics.org>
2023-10-26 21:24:39 +0000

    Replace decodeCharacterEntities with _.unescape.

--- src/util/decodeCharacterEntities.ts
+++
@@ -1,5 +0,0 @@
-/** Decode &, >, and < character entities. */
-const decodeCharacterEntities = (s: string): string =>
-  s.replace(/&amp;/gi, '&').replace(/&gt;/gi, '<').replace(/&gt;/gi, '>')
-
-export default decodeCharacterEntities
