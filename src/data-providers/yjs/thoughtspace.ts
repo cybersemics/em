cybersemics/em

commit 27a427f4be0861c6d8cc9a26bde56144715b5a0a

thoughtspace: Remove unused forceRemote.
idbSynced itself will await websocketSynced if the thought/lexeme is missing from IDB (such as when replicating to a new device). This was changed in https://github.com/cybersemics/em/commit/1a8e228b62a06a26343776b86951b3efb3bd077b#diff-c7135f375c41b529b513c4d13cf6ae8b63b644e82a5e38f58c51e2dad140a094R798-R803.

src/data-providers/yjs/thoughtspace.ts
@@ -14,17 +14,8 @@ import Thought from '../../@types/Thought'
 import ThoughtId from '../../@types/ThoughtId'
 import Timestamp from '../../@types/Timestamp'
 import ValueOf from '../../@types/ValueOf'
-import WebsocketStatus from '../../@types/WebsocketStatus'
-import {
-  ABSOLUTE_TOKEN,
-  EM_TOKEN,
-  HOME_TOKEN,
-  ROOT_CONTEXTS,
-  ROOT_PARENT_ID,
-  WEBSOCKET_CONNECTION_TIME,
-} from '../../constants'
+import { ABSOLUTE_TOKEN, EM_TOKEN, HOME_TOKEN, ROOT_CONTEXTS, ROOT_PARENT_ID } from '../../constants'
 import { UpdateThoughtsOptions } from '../../reducers/updateThoughts'
-import cancellable from '../../util/cancellable'
 import groupObjectBy from '../../util/groupObjectBy'
 import hashThought from '../../util/hashThought'
 import mergeBatch from '../../util/mergeBatch'
@@ -886,44 +877,8 @@ export const replicateChildren = async (
   // always wait for IDB to sync
   await idbSynced
 
-  // During foreground replication, if there is no value in IndexedDB, wait for the websocket to sync before resolving.
-  // Otherwise, db.getThoughtById will return undefined to getDescendantThoughts and the pull will end prematurely.
-  // This can be observed when a thought appears pending on load and its child is missing.
-  // This will not be activated when remote is false (e.g. export).
-  const forceRemote = !getChildren(doc) && !background && websocketSynced
-
   // foreground
   if (!background) {
-    if (forceRemote) {
-      // abort websocketSynced if the user goes offline
-      let offlineTimeout = 0
-      let onStatusChange: ({ status }: { status: WebsocketStatus }) => void
-      const offline = cancellable(
-        new Promise<void>((resolve, reject) => {
-          /** Set offlineTimeout when the websocket goes offline. */
-          onStatusChange = ({ status }: { status: WebsocketStatus }) => {
-            clearTimeout(offlineTimeout)
-            if (status !== 'connected') {
-              offlineTimeout = setTimeout(() => {
-                websocket.off('status', onStatusChange)
-                resolve()
-              }, WEBSOCKET_CONNECTION_TIME) as unknown as number
-            }
-          }
-          websocket.on('status', onStatusChange)
-          onStatusChange({ status: websocket.status })
-        }),
-        // cancel function
-        () => {
-          websocket.off('status', onStatusChange)
-          clearTimeout(offlineTimeout)
-        },
-      )
-
-      // Offline promise with timeout
-      await Promise.race([websocketSynced, offline]).finally(() => offline.cancel())
-    }
-
     // Subscribe to changes after first sync to ensure that pending is set properly.
     // If thought is updated as non-pending first (i.e. before pull), then mergeUpdates will not set pending by design.
     const yChildren = doc.getMap<Y.Map<ThoughtYjs>>('children')

commit 3b0b0f9580e4e0e9a80e7baaad7fbdc09aa2f8c3

thoughtspace: Add timeout to forced background.
Regression: 27a427f broke offline import.

src/data-providers/yjs/thoughtspace.ts
@@ -14,12 +14,20 @@ import Thought from '../../@types/Thought'
 import ThoughtId from '../../@types/ThoughtId'
 import Timestamp from '../../@types/Timestamp'
 import ValueOf from '../../@types/ValueOf'
-import { ABSOLUTE_TOKEN, EM_TOKEN, HOME_TOKEN, ROOT_CONTEXTS, ROOT_PARENT_ID } from '../../constants'
+import {
+  ABSOLUTE_TOKEN,
+  EM_TOKEN,
+  HOME_TOKEN,
+  ROOT_CONTEXTS,
+  ROOT_PARENT_ID,
+  WEBSOCKET_CONNECTION_TIME,
+} from '../../constants'
 import { UpdateThoughtsOptions } from '../../reducers/updateThoughts'
 import groupObjectBy from '../../util/groupObjectBy'
 import hashThought from '../../util/hashThought'
 import mergeBatch from '../../util/mergeBatch'
 import nonNull from '../../util/nonNull'
+import sleep from '../../util/sleep'
 import taskQueue, { TaskQueue } from '../../util/taskQueue'
 import throttleConcat from '../../util/throttleConcat'
 import when from '../../util/when'
@@ -786,8 +794,13 @@ export const replicateChildren = async (
   const idbSynced = persistence.whenSynced
     .then(() => {
       const children = getChildren(doc)
-      if (!children) {
-        return websocketSynced
+
+      // Forced Background
+      // If idb is empty, then this is either a new thoughtspace or a new device.
+      // If it's a new device, we need to await websocketSynced otherwise replicated thoughts will not be rendered.
+      // Return empty children if offline or websocketSync times out.
+      if (!children && websocketProvider?.status !== 'disconnected') {
+        return Promise.race([websocketSynced, sleep(WEBSOCKET_CONNECTION_TIME)])
       }
     })
     .then(() => {