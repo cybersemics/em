1f4dda9ef26bfcbb7f101094b17713b4e350f796
Raine Revere <raine@cybersemics.org>
2023-11-25 18:55:56 +0000

    MultiGesture: Simplify onMoveShouldSetPanResponder condition.

--- src/components/MultiGesture.tsx
+++ src/components/MultiGesture.tsx
@@ -232,7 +232,7 @@ class MultiGesture extends React.Component<MultiGestureProps> {
       // Prevent gesture when any text is selected.
       // See https://github.com/cybersemics/em/issues/676.
       // NOTE: though it works simulating mobile on desktop, selectionchange is too late to prevent actual gesture on mobile, so we can't detect only when the text selection is being dragged
-      onMoveShouldSetPanResponder: () => !this.props.shouldCancelGesture?.() ?? true,
+      onMoveShouldSetPanResponder: () => !this.props.shouldCancelGesture?.(),
 
       onPanResponderMove: (e: GestureResponderEvent, gestureState: GestureState) => {
         if (this.abandon) {
