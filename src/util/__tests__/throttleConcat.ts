48c9b4dc3682fd6ef21ad66d4d88cbc4d599f747
Raine Revere <raine@cybersemics.org>
2023-11-26 15:32:33 +0000

    throttleConcat: Add option for custom throttle function.

--- src/util/__tests__/throttleConcat.ts
+++ src/util/__tests__/throttleConcat.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import sleep from '../sleep'
 import throttleConcat from '../throttleConcat'
 
@@ -11,6 +12,28 @@ it('synchronous: once on the leading edge and once on the trailing edge', async 
     output = [...output, ...values]
   }
   const g = throttleConcat(f, 10)
+
+  // eslint-disable-next-line fp/no-loops
+  for (let i = 0; i < 10; i++) {
+    g(i)
+  }
+
+  await sleep(20)
+
+  expect(calls).toBe(2)
+  expect(output).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+})
+
+it('custom throttle function', async () => {
+  let calls = 0
+  let output: number[] = []
+
+  /** Increment count and append output values. */
+  const f = (values: number[]) => {
+    calls++
+    output = [...output, ...values]
+  }
+  const g = throttleConcat(f, 10, { throttle: _.throttle })
 
   // eslint-disable-next-line fp/no-loops
   for (let i = 0; i < 10; i++) {
--- src/util/throttleConcat.ts
+++ src/util/throttleConcat.ts
@@ -1,11 +1,10 @@
-import { ThrottleSettings } from 'lodash'
 import throttleReduce, { ThrottledFunction } from './throttleReduce'
 
 /** Generates a throttled function that will be invoked with a list of values from the batch of calls queued during the cooldown. */
 const throttleConcat = <T, R>(
   f: (accum: T[]) => R,
   ms: number,
-  throttleSettings?: ThrottleSettings,
-): ThrottledFunction<T, R> => throttleReduce(f, (a, b) => [...b, a], [] as T[], ms, throttleSettings)
+  options?: Parameters<typeof throttleReduce>[4],
+): ThrottledFunction<T, R> => throttleReduce(f, (a, b) => [...b, a], [] as T[], ms, options)
 
 export default throttleConcat
