577e9975cfb8c61bf8adceaa7b733e26e23e09f6
Raine Revere <raine@cybersemics.org>
2023-11-29 16:36:17 +0000

    collapseContext: Pull all parent descendants for merging duplicates.
    TODO: If the number of parent's descendants exceeds the cache size, then freeThoughts may deallocate thoughts before they get merged. Take a similar approach as importFiles and move the thoughts incrementally while pulling only one level at a time.

--- src/action-creators/collapseContext.ts
+++ src/action-creators/collapseContext.ts
@@ -1,10 +1,21 @@
 import Thunk from '../@types/Thunk'
 import collapseContext from '../reducers/collapseContext'
+import rootedParentOf from '../selectors/rootedParentOf'
+import head from '../util/head'
+import pull from './pull'
 
-/** Action-creator for collapseContext. */
+/** Action-creator for collapseContext. Pulls all descendants of the parent to ensure duplicates are merged correctly. */
 const collapseContextActionCreator =
-  (payload: Parameters<typeof collapseContext>[1]): Thunk =>
-  dispatch =>
-    dispatch({ type: 'collapseContext', ...payload })
+  ({ at }: Parameters<typeof collapseContext>[1]): Thunk =>
+  async (dispatch, getState) => {
+    const state = getState()
+    const path = at || state.cursor
+
+    if (!path) return
+
+    // TODO: If the number of parent's descendants exceeds the cache size, then freeThoughts may deallocate thoughts before they get merged. Take a similar approach as importFiles and move the thoughts incrementally while pulling only one level at a time.
+    await dispatch(pull([head(rootedParentOf(state, path))], { maxDepth: Infinity }))
+    dispatch({ type: 'collapseContext', at })
+  }
 
 export default collapseContextActionCreator
