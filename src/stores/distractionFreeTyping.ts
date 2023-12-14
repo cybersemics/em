6e3a1c8555edab6dfbee5f51625e46fb7556345b
Raine Revere <raine@cybersemics.org>
2023-12-14 23:47:23 +0000

    distractionFreeTyping: Cancel updateThrottled on update.

--- src/stores/distractionFreeTyping.ts
+++ src/stores/distractionFreeTyping.ts
@@ -6,6 +6,11 @@ import reactMinistore from './react-ministore'
 const store = reactMinistore<boolean>(false)
 const distractionFreeTypingStore = {
   ...store,
+  /** Performs a normal ministore update and cancels updateThrottled. */
+  update: (value: boolean) => {
+    distractionFreeTypingStore.updateThrottled.cancel()
+    return store.update(value)
+  },
   updateThrottled: _.throttle(store.update, THROTTLE_DISTRACTION_FREE_TYPING, { leading: false }),
 }
 
