0ef5ba924afdcf84275185da4e7dccc2007f5ce8
Raine Revere <raine@cybersemics.org>
2023-12-28 23:59:28 +0000

    Superscript: Remove unneeded simplePathLive.

--- src/components/Superscript.tsx
+++ src/components/Superscript.tsx
@@ -1,55 +1,39 @@
 import React, { FC, useEffect, useRef, useState } from 'react'
 import { shallowEqual, useSelector } from 'react-redux'
-import Context from '../@types/Context'
 import Index from '../@types/IndexType'
 import SimplePath from '../@types/SimplePath'
 import State from '../@types/State'
 import ThoughtId from '../@types/ThoughtId'
-import { HOME_TOKEN } from '../constants'
 import getContexts from '../selectors/getContexts'
 import getThoughtById from '../selectors/getThoughtById'
-import hasLexeme from '../selectors/hasLexeme'
 import store from '../stores/app'
-import equalArrays from '../util/equalArrays'
 import head from '../util/head'
-import headValue from '../util/headValue'
 import isVisibleContext from '../util/isVisibleContext'
-import parentOf from '../util/parentOf'
-import pathToContext from '../util/pathToContext'
 
 interface SuperscriptProps {
   contextViews?: Index<boolean>
   showSingle?: boolean
   superscript?: boolean
   simplePath: SimplePath
-  thoughts?: Context
 }
 
 const NO_CONTEXTS: ThoughtId[] = []
 
 /** Renders superscript if there are other contexts. Optionally pass thoughts (used by ContextBreadcrumbs) or simplePath (used by Subthought). */
-const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, superscript = true, thoughts }) => {
+const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, superscript = true }) => {
   const [numContexts, setNumContexts] = useState(0)
   const ref = useRef<HTMLElement>(null)
 
   const showHiddenThoughts = useSelector(state => state.showHiddenThoughts)
-  const simplePathLive = useSelector(state => {
-    const cursorContext = state.cursor ? pathToContext(state, state.cursor) : [HOME_TOKEN]
-    const editing =
-      state.cursor &&
-      equalArrays(cursorContext, pathToContext(state, simplePath || [])) &&
-      hasLexeme(state, headValue(state, state.cursor))
-    return editing ? (parentOf(simplePath).concat(head(state.cursor!)) as SimplePath) : simplePath
-  }, shallowEqual)
 
   const show = useSelector(state => {
-    const value = getThoughtById(state, head(simplePathLive))?.value || ''
+    const value = getThoughtById(state, head(simplePath))?.value || ''
     const emptyThought = value.length === 0
     return !emptyThought && superscript && numContexts! > (showSingle ? 0 : 1)
   })
 
   const contexts = useSelector(
-    (state: State) => (show ? getContexts(state, head(simplePathLive || simplePath)) : NO_CONTEXTS),
+    (state: State) => (show ? getContexts(state, head(simplePath)) : NO_CONTEXTS),
     shallowEqual,
   )
 
