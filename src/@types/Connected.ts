3e61819104dcba705431bcb0f876e1627a2bb6a3
Raine Revere <raine@cybersemics.org>
2023-12-29 00:56:12 +0000

    Remove unused Connected type.

--- src/@types/Connected.ts
+++
@@ -1,8 +0,0 @@
-import Dispatch from './Dispatch'
-
-/** When a component is connected, the dispatch prop is added. */
-type Connected<T> = T & {
-  dispatch: Dispatch
-}
-
-export default Connected
