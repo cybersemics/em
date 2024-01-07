2d474124b08c67e145b8addfe5677792720d1dc4
Raine Revere <raine@cybersemics.org>
2024-01-07 02:01:22 +0000

    attribute: comment

--- src/selectors/attribute.ts
+++ src/selectors/attribute.ts
@@ -3,7 +3,7 @@ import ThoughtId from '../@types/ThoughtId'
 import findDescendant from '../selectors/findDescendant'
 import { findAnyChild, isVisible } from './getChildren'
 
-/** Returns the value of a visible child attribute of a thought. Use a combination of findDescendant and attribute to check if an attribute exists but has no children. O(1) as long as attributeName is a meta attribute and can take advantage of childrenMap. */
+/** Returns the value of a visible child attribute of a thought. Returns null if the attribute does not exist or has no child. Use a combination of findDescendant and attribute to distinguish these cases, i.e. .to check if an attribute exists but has no children. If attributeName is a meta attribute, performs in O(1) due to special childrenMap keys of attributes. */
 const attribute = (state: State, thoughtId: ThoughtId | null, attributeName: string): string | null => {
   const attributeId = findDescendant(state, thoughtId, attributeName)
   if (!attributeId) return null
