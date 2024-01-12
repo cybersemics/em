774fcfcfe035b1310011f19a0a4e09893ee1926e
Raine Revere <raine@cybersemics.org>
2024-01-12 00:33:28 +0000

    Co-locate alertWithMinistore with reducer.

--- src/reducers/alert.ts
+++ src/reducers/alert.ts
@@ -15,6 +15,9 @@ interface Options {
   importFileId?: string
 }
 
+/** A special alert value that is masked by alertStore. This just needs to be a non-empty stable value to avoid Redux state changes. */
+const ALERT_WITH_MINITORE = '__ALERT_WITH_MINITORE__'
+
 let clearAlertTimeoutId: ReturnType<typeof setTimeout> | null = null
 
 /** Set an alert with an optional close link. */
@@ -106,6 +109,19 @@ export const alertActionCreator =
       importFileId,
       isInline,
     })
+  }
+
+/** Dispatches an alert, but uses the alertMinistore to circumvent repeated renders. See: stores/alertMinistore. */
+export const alertWithMinistore =
+  (
+    value: string | null,
+    options?: Omit<Alert, 'value'> & {
+      clearDelay?: number
+    },
+  ): Thunk =>
+  dispatch => {
+    alertStore.update(value)
+    dispatch(alertActionCreator(value != null ? ALERT_WITH_MINITORE : null, options))
   }
 
 export default _.curryRight(alertReducer)
