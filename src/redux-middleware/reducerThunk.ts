71a025a77b9304d1e8b4074004673e0d64afcedf
Raine Revere <raine@cybersemics.org>
2024-01-28 02:00:25 +0000

    reducerThunk: refactor

--- src/redux-middleware/reducerThunk.ts
+++ src/redux-middleware/reducerThunk.ts
@@ -8,21 +8,18 @@ export type ReducerThunk = (state: State) => State
 // TODO: Fix type.
 // For now, type curryReducer as a drop-in replacement for _.curryRight.
 /** Curries a reducer and allows it to be dispatched with arguments. */
-export const curryReducer = (<A extends any[]>(reducer: (state: State, ...args: A) => State, arity?: number) => {
+export const curryReducer = (<A extends any[]>(reducer: (...args: A) => State, arity?: number) => {
   const curry = _.curryRight(reducer, arity)
 
   /** Wrap the curried function so that we can store the payload. */
   const wrapper = (...args: Parameters<typeof curry>) => {
     const curried = curry(...args)
-    curried.curriedAction = {
-      type: reducer.name,
-      ...args[0],
-    }
+    Object.defineProperty(curried, '__curriedAction', { value: { type: reducer.name, ...args[0] } })
     return curried
   }
 
-  // set curriedAction on the wrapper so that reducers with no arguments can be dispatched by passing the function reference
-  Object.defineProperty(wrapper, 'curriedAction', { value: { type: reducer.name } })
+  // set __curriedAction on the wrapper so that reducers with no arguments can be dispatched by passing the function reference
+  Object.defineProperty(wrapper, '__curriedAction', { value: { type: reducer.name } })
 
   return wrapper
 }) as _.CurryRight
@@ -30,8 +27,8 @@ export const curryReducer = (<A extends any[]>(reducer: (state: State, ...args: 
 /** Redux Middleware that allows curried reducers to be dispatched as actions. */
 const reducerThunk: Middleware<any, State, Dispatch> = () => next => action =>
   next(
-    // dispatch curried reducers using the payload stored in the curriedAction property
-    action.curriedAction ||
+    // dispatch curried reducers using the payload stored in the __curriedAction property
+    action.__curriedAction ||
       // dispatch plain reducers that take no arguments
       (typeof action === 'function' && action.toString().startsWith('state =>')
         ? { type: action.name }
