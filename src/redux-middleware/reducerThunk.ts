22e9c7fdb8da9e1fbaf61cbb696e1a86ecdc3dbe
Raine Revere <raine@cybersemics.org>
2024-01-28 01:42:54 +0000

    reduxThunk: Allow reducers with no arguments to be dispatched with or without ().

--- src/redux-middleware/reducerThunk.ts
+++ src/redux-middleware/reducerThunk.ts
@@ -10,7 +10,9 @@ export type ReducerThunk = (state: State) => State
 /** Curries a reducer and allows it to be dispatched with arguments. */
 export const curryReducer = (<A extends any[]>(reducer: (state: State, ...args: A) => State, arity?: number) => {
   const curry = _.curryRight(reducer, arity)
-  return (...args: Parameters<typeof curry>) => {
+
+  /** Wrap the curried function so that we can store the payload. */
+  const wrapper = (...args: Parameters<typeof curry>) => {
     const curried = curry(...args)
     curried.curriedAction = {
       type: reducer.name,
@@ -18,19 +20,23 @@ export const curryReducer = (<A extends any[]>(reducer: (state: State, ...args: 
     }
     return curried
   }
+
+  // set curriedAction on the wrapper so that reducers with no arguments can be dispatched by passing the function reference
+  Object.defineProperty(wrapper, 'curriedAction', { value: { type: reducer.name } })
+
+  return wrapper
 }) as _.CurryRight
 
 /** Redux Middleware that allows curried reducers to be dispatched as actions. */
 const reducerThunk: Middleware<any, State, Dispatch> = () => next => action =>
   next(
-    // dispatch plain reducers that take no arguments
-    typeof action === 'function' && action.toString().startsWith('state =>')
-      ? { type: action.name }
-      : // dispatch curried reducers using the payload stored in the curriedAction property
-        typeof action === 'function' && action.curriedAction
-        ? action.curriedAction
+    // dispatch curried reducers using the payload stored in the curriedAction property
+    action.curriedAction ||
+      // dispatch plain reducers that take no arguments
+      (typeof action === 'function' && action.toString().startsWith('state =>')
+        ? { type: action.name }
         : // otherwise pass the action on to the next middleware
-          action,
+          action),
   )
 
 export default reducerThunk
