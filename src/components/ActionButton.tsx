26dfa015c3ed2fe135ed6dd8adfb85d76bf2b37f
Raine Revere <raine@cybersemics.org>
2024-02-01 16:20:38 +0000

    ActionButton: Add inverse prop.

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import themeColors from '../selectors/themeColors'
 import fastClick from '../util/fastClick'
 import Loader from './Loader'
 
@@ -9,6 +11,8 @@ interface ActionButtonProps {
   onClick?: (e: React.MouseEvent | React.TouchEvent) => void
   inActive?: boolean
   small?: boolean
+  /** Render button with fg and bg colors inverted. */
+  inverse?: boolean
   isLoading?: boolean
   isDisabled?: boolean
 }
@@ -20,27 +24,39 @@ export const ActionButton = ({
   title,
   active,
   inActive,
+  inverse,
   small,
   isLoading,
   isDisabled,
   onClick,
-  style,
   ...restProps
-}: ActionButtonProps & React.HTMLAttributes<HTMLAnchorElement>) => (
-  <a
-    className={classNames({
-      button: true,
-      'button-active': active,
-      'button-inactive': inActive,
-      'button-small': small,
-      'action-button': true,
-      disabled: isDisabled,
-    })}
-    {...(onClick && !isDisabled ? fastClick(onClick) : null)}
-    {...restProps}
-  >
-    {/* TODO: Animate on loader toggle. */}
-    {isLoading && <Loader size={35} style={{ marginRight: '15px' }} />}
-    {title}
-  </a>
-)
+}: ActionButtonProps & React.HTMLAttributes<HTMLAnchorElement>) => {
+  const colors = useSelector(themeColors)
+  return (
+    <a
+      className={classNames({
+        button: true,
+        'button-active': active,
+        'button-inactive': inActive,
+        'button-small': small,
+        'action-button': true,
+        disabled: isDisabled,
+      })}
+      style={
+        inverse
+          ? {
+              backgroundColor: colors.gray15,
+              border: `solid 1px ${colors.gray50}`,
+              color: colors.fg,
+            }
+          : undefined
+      }
+      {...(onClick && !isDisabled ? fastClick(onClick) : null)}
+      {...restProps}
+    >
+      {/* TODO: Animate on loader toggle. */}
+      {isLoading && <Loader size={35} style={{ marginRight: '15px' }} />}
+      {title}
+    </a>
+  )
+}
