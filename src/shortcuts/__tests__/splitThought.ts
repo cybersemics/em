c13a6e79e7cd9d33c9b4cd40c20eec0634d26fa7
Raine Revere <raine@cybersemics.org>
2024-04-09 21:29:00 +0000

    splitThought: Fix act warnings in test and switch to createRtlTestApp.

--- src/shortcuts/__tests__/splitThought.ts
+++ src/shortcuts/__tests__/splitThought.ts
@@ -1,36 +1,27 @@
 import { screen } from '@testing-library/dom'
+import { act } from '@testing-library/react'
 import newThought from '../../action-creators/newThought'
 import store from '../../stores/app'
-import createTestApp, { cleanupTestApp } from '../../test-helpers/createTestApp'
-import testTimer from '../../test-helpers/testTimer'
-
-const fakeTimer = testTimer()
+import createTestApp, { cleanupTestApp } from '../../test-helpers/createRtlTestApp'
 
 beforeEach(createTestApp)
 afterEach(cleanupTestApp)
 
 it('split thought after non-word character', async () => {
-  fakeTimer.useFakeTimer()
-
-  store.dispatch([
-    newThought({
-      value: '*test',
-    }),
-  ])
-
-  await fakeTimer.runAllAsync()
-
-  store.dispatch({
-    type: 'splitThought',
-    splitResult: {
-      left: '*',
-      right: 'test',
-    },
+  await act(async () => {
+    store.dispatch([
+      newThought({
+        value: '*test',
+      }),
+      {
+        type: 'splitThought',
+        splitResult: {
+          left: '*',
+          right: 'test',
+        },
+      },
+    ])
   })
-
-  await fakeTimer.runAllAsync()
-
-  await fakeTimer.useRealTimer()
 
   // ensure that the Lexeme is not duplicated since it is the same as the source thought
   expect(() => screen.getByText('2')).toThrow('Unable to find an element')
