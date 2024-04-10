aaef1cbbdb19a680eb30ff600458628b30a0ebe2
Raine Revere <raine@cybersemics.org>
2024-04-10 21:53:47 +0000

    Editable: Convert bumpThoughtDown test from Enzyme to RTL.

--- src/components/__tests__/Editable.ts
+++ src/components/__tests__/Editable.ts
@@ -1,23 +1,16 @@
-import { ReactWrapper } from 'enzyme'
+import { act } from '@testing-library/react'
 import importText from '../../action-creators/importText'
 import bumpThoughtDown from '../../shortcuts/bumpThoughtDown'
-import store from '../../stores/app'
-import createTestApp, { cleanupTestApp } from '../../test-helpers/createTestApp'
+import createTestApp, { cleanupTestApp } from '../../test-helpers/createRtlTestApp'
+import dispatch from '../../test-helpers/dispatch'
 import executeShortcut from '../../test-helpers/executeShortcut'
 import { setCursorFirstMatchActionCreator as setCursor } from '../../test-helpers/setCursorFirstMatch'
-import Editable from '../Editable'
 
-let wrapper: ReactWrapper<unknown, unknown>
-
-beforeEach(async () => {
-  wrapper = await createTestApp()
-})
-
+beforeEach(createTestApp)
 afterEach(cleanupTestApp)
 
-it('reset content editable inner html on thought bump', async () => {
-  // import thoughts
-  store.dispatch([
+it('reset content editable inner html on bumpThoughtDown', async () => {
+  await dispatch([
     importText({
       text: `
         - a
@@ -26,18 +19,11 @@ it('reset content editable inner html on thought bump', async () => {
     setCursor(['a']),
   ])
 
-  // update DOM
-  wrapper.update()
+  expect(document.querySelector('div.editable')?.textContent).toBe('a')
 
-  /* eslint-disable jsdoc/require-jsdoc */
-  const getContentEditableText = () => wrapper.find(Editable).first().find('div.editable').first().text()
+  await act(async () => {
+    executeShortcut(bumpThoughtDown)
+  })
 
-  expect(getContentEditableText()).toBe('a')
-
-  executeShortcut(bumpThoughtDown, { store })
-
-  jest.runOnlyPendingTimers()
-  wrapper.update()
-
-  expect(getContentEditableText()).toBe('')
+  expect(document.querySelector('div.editable')?.textContent).toBe('')
 })
