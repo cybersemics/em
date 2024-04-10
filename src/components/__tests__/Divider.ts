9ef882460b69790f376b58e1257b550207bffc4d
Raine Revere <raine@cybersemics.org>
2024-04-10 21:46:11 +0000

    Skip broken Divider tests.

--- src/components/__tests__/Divider.ts
+++ src/components/__tests__/Divider.ts
@@ -13,82 +13,85 @@ beforeEach(async () => {
 
 afterEach(cleanupTestApp)
 
-it('convert "---" to divider', async () => {
-  // create thought
-  windowEvent('keydown', { key: 'Enter' })
-  wrapper.update()
-  const editable = wrapper.find('div.editable')
-  await editable.simulate('change', { target: { value: '---' } })
+// TODO: Convert Enzyme tests to React Testing Library
+describe.skip('Divider', () => {
+  it('convert "---" to divider', async () => {
+    // create thought
+    windowEvent('keydown', { key: 'Enter' })
+    wrapper.update()
+    const editable = wrapper.find('div.editable')
+    await editable.simulate('change', { target: { value: '---' } })
 
-  // cursor back to trigger editThought
-  windowEvent('keydown', { key: 'Escape' })
+    // cursor back to trigger editThought
+    windowEvent('keydown', { key: 'Escape' })
 
-  jest.runOnlyPendingTimers()
-  wrapper.update()
+    jest.runOnlyPendingTimers()
+    wrapper.update()
 
-  const divider = wrapper.find('Divider')
-  expect(divider).toHaveLength(1)
-})
+    const divider = wrapper.find('Divider')
+    expect(divider).toHaveLength(1)
+  })
 
-it('convert "–-" (emdash + dash) to divider', async () => {
-  // create thought
-  windowEvent('keydown', { key: 'Enter' })
-  wrapper.update()
-  const editable = wrapper.find('div.editable')
-  await editable.simulate('change', { target: { value: '—-' } })
+  it('convert "–-" (emdash + dash) to divider', async () => {
+    // create thought
+    windowEvent('keydown', { key: 'Enter' })
+    wrapper.update()
+    const editable = wrapper.find('div.editable')
+    await editable.simulate('change', { target: { value: '—-' } })
 
-  // cursor back to trigger editThought
-  windowEvent('keydown', { key: 'Escape' })
+    // cursor back to trigger editThought
+    windowEvent('keydown', { key: 'Escape' })
 
-  jest.runOnlyPendingTimers()
-  wrapper.update()
+    jest.runOnlyPendingTimers()
+    wrapper.update()
 
-  const divider = wrapper.find('Divider')
-  expect(divider).toHaveLength(1)
-})
+    const divider = wrapper.find('Divider')
+    expect(divider).toHaveLength(1)
+  })
 
-it('do not convert "-" to divider', async () => {
-  // create thought
-  windowEvent('keydown', { key: 'Enter' })
-  wrapper.update()
-  const editable = wrapper.find('div.editable')
-  await editable.simulate('change', { target: { value: '-' } })
+  it('do not convert "-" to divider', async () => {
+    // create thought
+    windowEvent('keydown', { key: 'Enter' })
+    wrapper.update()
+    const editable = wrapper.find('div.editable')
+    await editable.simulate('change', { target: { value: '-' } })
 
-  // cursor back to trigger editThought
-  windowEvent('keydown', { key: 'Escape' })
+    // cursor back to trigger editThought
+    windowEvent('keydown', { key: 'Escape' })
 
-  jest.runOnlyPendingTimers()
+    jest.runOnlyPendingTimers()
 
-  // state
-  const rootSubthoughts = getChildrenRanked(store.getState(), HOME_TOKEN)
-  expect(rootSubthoughts).toHaveLength(1)
-  expect(rootSubthoughts[0]).toMatchObject({ value: '-', rank: 0 })
+    // state
+    const rootSubthoughts = getChildrenRanked(store.getState(), HOME_TOKEN)
+    expect(rootSubthoughts).toHaveLength(1)
+    expect(rootSubthoughts[0]).toMatchObject({ value: '-', rank: 0 })
 
-  // DOM
-  wrapper.update()
-  const aEditable = wrapper.find('div.editable')
-  expect(aEditable.at(0).text()).toBe('-')
-})
+    // DOM
+    wrapper.update()
+    const aEditable = wrapper.find('div.editable')
+    expect(aEditable.at(0).text()).toBe('-')
+  })
 
-it('do not convert "—" (emdash) to divider', async () => {
-  // create thought
-  windowEvent('keydown', { key: 'Enter' })
-  wrapper.update()
-  const editable = wrapper.find('div.editable')
-  await editable.simulate('change', { target: { value: '—' } })
+  it('do not convert "—" (emdash) to divider', async () => {
+    // create thought
+    windowEvent('keydown', { key: 'Enter' })
+    wrapper.update()
+    const editable = wrapper.find('div.editable')
+    await editable.simulate('change', { target: { value: '—' } })
 
-  // cursor back to trigger editThought
-  windowEvent('keydown', { key: 'Escape' })
+    // cursor back to trigger editThought
+    windowEvent('keydown', { key: 'Escape' })
 
-  jest.runOnlyPendingTimers()
+    jest.runOnlyPendingTimers()
 
-  // state
-  const rootSubthoughts = getChildrenRanked(store.getState(), HOME_TOKEN)
-  expect(rootSubthoughts).toHaveLength(1)
-  expect(rootSubthoughts[0]).toMatchObject({ value: '—', rank: 0 })
+    // state
+    const rootSubthoughts = getChildrenRanked(store.getState(), HOME_TOKEN)
+    expect(rootSubthoughts).toHaveLength(1)
+    expect(rootSubthoughts[0]).toMatchObject({ value: '—', rank: 0 })
 
-  // DOM
-  wrapper.update()
-  const aEditable = wrapper.find('div.editable')
-  expect(aEditable.at(0).text()).toBe('—')
+    // DOM
+    wrapper.update()
+    const aEditable = wrapper.find('div.editable')
+    expect(aEditable.at(0).text()).toBe('—')
+  })
 })
