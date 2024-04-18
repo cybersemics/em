7e9984c73e97d9691aab5a67ca13d0450521ae52
Raine Revere <raine@cybersemics.org>
2024-04-18 13:58:14 +0000

    Upgrade typescript to v4.9.5.

--- src/device/download.ts
+++ src/device/download.ts
@@ -6,24 +6,17 @@ import MimeType from '../@types/MimeType'
 const download = (data: string, filename: string, type: MimeType = 'text/plain') => {
   const file = new Blob([data], { type })
 
-  // IE10+
-  if (window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveOrOpenBlob(file, filename)
-  }
-  // Others
-  else {
-    const a = document.createElement('a')
-    const url = URL.createObjectURL(file)
-    a.href = url
-    a.download = filename
-    document.body.appendChild(a)
-    a.click()
+  const a = document.createElement('a')
+  const url = URL.createObjectURL(file)
+  a.href = url
+  a.download = filename
+  document.body.appendChild(a)
+  a.click()
 
-    setTimeout(() => {
-      document.body.removeChild(a)
-      window.URL.revokeObjectURL(url)
-    }, 0)
-  }
+  setTimeout(() => {
+    document.body.removeChild(a)
+    window.URL.revokeObjectURL(url)
+  }, 0)
 }
 
 export default download
