1e1b93ac5938f652dbf013a262226a1381b696ad
Raine Revere <raine@cybersemics.org>
2024-04-30 16:27:25 +0000

    Puppeteer/screenshot: Clear browser selection before taking screenshot.

--- src/e2e/puppeteer/helpers/screenshot.ts
+++ src/e2e/puppeteer/helpers/screenshot.ts
@@ -1,6 +1,18 @@
 import { Page } from 'puppeteer'
 
-/** Refreshes the page. */
-const screenshot = (page: Page) => page.screenshot()
+/** Takes a screenshot. Note: Clears the browser selection first, as the timing of the blinking caret differs between runs. */
+const screenshot = async (page: Page) => {
+  await page.evaluate(() => {
+    // For some reason, in headless mode, removeAllRanges is not enough to remove the caret. It will show up in the screenshot at the beginning of the focusNode.
+    // Blurring the active element works as expected (parallels the implementation of selection.clear).
+    window.getSelection()?.removeAllRanges()
+
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur()
+    }
+  })
+
+  return page.screenshot()
+}
 
 export default screenshot
