fda68dbdc4b86705f42bcf3901e634274db583ed
Raine Revere <raine@cybersemics.org>
2024-05-06 17:52:50 +0000

    Do not override =headings style with .thought font-weight.

--- src/components/__tests__/headings.ts
+++ src/components/__tests__/headings.ts
@@ -1,3 +1,4 @@
+import { act } from '@testing-library/react'
 import importText from '../../action-creators/importText'
 import store from '../../stores/app'
 import createTestApp, { cleanupTestApp } from '../../test-helpers/createRtlTestApp'
@@ -7,10 +8,11 @@ import { setCursorFirstMatchActionCreator as setCursor } from '../../test-helper
 beforeEach(createTestApp)
 afterEach(cleanupTestApp)
 
-it.skip('headings should set font weight', async () => {
-  store.dispatch([
-    importText({
-      text: `
+it('headings should set font weight', async () => {
+  await act(async () => {
+    store.dispatch([
+      importText({
+        text: `
         - Normal Text
         - My Heading 1
           - =heading1
@@ -23,31 +25,35 @@ it.skip('headings should set font weight', async () => {
         - My Heading 5
           - =heading5
       `,
-    }),
-  ])
+      }),
+    ])
+  })
 
   // normal text should not be bold
   const thought0 = await findThoughtByText('Normal Text')
   expect(thought0).not.toHaveStyle({ fontWeight: 700 })
 
   // headings should be bold
-  const thought1 = await findThoughtByText('My Heading 1')
+  const thought1 = (await findThoughtByText('My Heading 1'))?.closest('[aria-label="thought-container"]')
   expect(thought1).toHaveStyle({ fontWeight: 700 })
 
-  const thought2 = await findThoughtByText('My Heading 2')
+  const thought2 = (await findThoughtByText('My Heading 2'))?.closest('[aria-label="thought-container"]')
   expect(thought2).toHaveStyle({ fontWeight: 700 })
 
-  const thought3 = await findThoughtByText('My Heading 3')
+  const thought3 = (await findThoughtByText('My Heading 3'))?.closest('[aria-label="thought-container"]')
   expect(thought3).toHaveStyle({ fontWeight: 700 })
 
-  const thought4 = await findThoughtByText('My Heading 4')
+  const thought4 = (await findThoughtByText('My Heading 4'))?.closest('[aria-label="thought-container"]')
   expect(thought4).toHaveStyle({ fontWeight: 600 })
 
-  const thought5 = await findThoughtByText('My Heading 5')
+  const thought5 = (await findThoughtByText('My Heading 5'))?.closest('[aria-label="thought-container"]')
   expect(thought5).toHaveStyle({ fontWeight: 600 })
 
   // child should not be bold
-  store.dispatch(setCursor(['My Heading 1', '=heading1']))
+  await act(async () => {
+    store.dispatch(setCursor(['My Heading 1', '=heading1']))
+  })
+
   const thought1Child = await findThoughtByText('=heading1')
   expect(thought1Child).not.toHaveStyle({ fontWeight: 700 })
 })
--- src/test-helpers/queries/findThoughtByText.ts
+++ src/test-helpers/queries/findThoughtByText.ts
@@ -5,7 +5,7 @@ import { findAllByText, screen } from '@testing-library/dom'
  */
 export const findThoughtByText = async (value: string, container?: HTMLElement | null) => {
   const thoughtNodes = await (container
-    ? findAllByText(container!, value, { exact: true })
+    ? findAllByText(container, value, { exact: true })
     : screen.findAllByText(value, { exact: true }))
   return thoughtNodes.find(t => t.hasAttribute('contenteditable')) || null
 }
