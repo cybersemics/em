7a590fa88315a49f96a67ffb5904b0831f8105a9
Hugo Martinez <hugomartinezdev@gmail.com>
2024-05-08 14:48:49 -0400

    Remove unnecessary jest dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -24,7 +24,6 @@ module.exports = {
     // https://github.com/jsdom/whatwg-encoding/pull/11
     'ipfs-http-client',
 
-    // jest-puppeteer requires puppeteer <v10
     'puppeteer',
 
     // Broken:  ^6.0.1 → ^7.0.5
