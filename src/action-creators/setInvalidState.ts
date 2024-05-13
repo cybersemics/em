147cad17f9d47b8e21883775eeaafac9ff100b11
Raine Revere <raine@cybersemics.org>
2024-05-13 18:23:39 +0000

    Co-locate setInvalidStateActionCreator with reducer (#1797).

--- src/action-creators/setInvalidState.ts
+++
@@ -1,9 +0,0 @@
-import Thunk from '../@types/Thunk'
-
-/** Real-time meta validation error. It is dispatched by Editable handlers and is used by Bullet and ThoughtsAnnotation to make visual changes. */
-const setInvalidState =
-  (value: boolean): Thunk =>
-  (dispatch, getState) =>
-    getState().invalidState !== value ? dispatch({ type: 'invalidState', value }) : null
-
-export default setInvalidState
--- src/reducers/invalidState.ts
+++ src/reducers/invalidState.ts
@@ -1,10 +1,17 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 
 /** Real-time meta validation error status. */
 const invalidState = (state: State, { value }: { value: string }) => ({
   ...state,
   invalidState: value,
 })
+
+/** Real-time meta validation error. It is dispatched by Editable handlers and is used by Bullet and ThoughtsAnnotation to make visual changes. */
+export const setInvalidStateActionCreator =
+  (value: boolean): Thunk =>
+  (dispatch, getState) =>
+    getState().invalidState !== value ? dispatch({ type: 'invalidState', value }) : null
 
 export default _.curryRight(invalidState)
