8bbc79c3dcbf5d2af93661459531bf443feba257
Raine Revere <raine@cybersemics.org>
2024-05-13 18:08:42 +0000

    Co-locate initThoughtsActionCreator with reducer (#1797).

--- src/reducers/initThoughts.ts
+++ src/reducers/initThoughts.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 import keyValueBy from '../util/keyValueBy'
 
 /** Set the initial thoughts' updatedBy field once the cliendId is ready. */
@@ -30,5 +31,15 @@ const initThoughts = (state: State, { clientId }: { clientId: string }) => ({
     },
   },
 })
+
+/** Update the initial thoughts with the clientId once it is ready. */
+export const initThoughtsActionCreator =
+  (clientId: string): Thunk =>
+  (dispatch, getState) => {
+    dispatch({
+      type: 'initThoughts',
+      clientId,
+    })
+  }
 
 export default _.curryRight(initThoughts)
