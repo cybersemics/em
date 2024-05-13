852b2607198cd6e1d8025d422ec586dcddb6f1d0
Raine Revere <raine@cybersemics.org>
2024-05-13 18:24:06 +0000

    Co-locate setNoteFocusActionCreator with reducer (#1797).

--- src/reducers/setNoteFocus.ts
+++ src/reducers/setNoteFocus.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 import headValue from '../util/headValue'
 
 /** Sets state.noteFocus to true or false, indicating if the caret is on a note. Sets state.cursorOffset to the end of the thought when disabling note focus so the selection gets placed back correctly on the thought. */
@@ -13,5 +14,11 @@ const setNoteFocus = (state: State, { value }: { value: boolean }): State => ({
   // it will be set in the Note's onFocus anyway, but set it here so that we are not as dependent on what happens there
   ...(value ? { editing: true } : null),
 })
+
+/** Action-creator for setNoteFocus. */
+export const setNoteFocusActionCreator =
+  (payload: Parameters<typeof setNoteFocus>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'setNoteFocus', ...payload })
 
 export default _.curryRight(setNoteFocus, 2)
