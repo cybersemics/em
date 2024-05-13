ae3e7a1952ae026617ea60fac3207b005394cac0
Raine Revere <raine@cybersemics.org>
2024-05-13 18:38:13 +0000

    Co-locate toggleSplitViewActionCreator with reducer (#1797).

--- src/shortcuts/toggleSplitView.tsx
+++ src/shortcuts/toggleSplitView.tsx
@@ -1,6 +1,6 @@
 import IconType from '../@types/Icon'
 import Shortcut from '../@types/Shortcut'
-import toggleSplitView from '../action-creators/toggleSplitView'
+import { toggleSplitViewActionCreator as toggleSplitView } from '../reducers/toggleSplitView'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const Icon = ({ fill, size = 20, style }: IconType) => (
