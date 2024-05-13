52e3bddd13c1076dfaba2fd1ca9e3ad0a21d1ba2
Raine Revere <raine@cybersemics.org>
2024-05-13 18:25:14 +0000

    Co-locate setResourceCacheActionCreator with reducer (#1797).

--- src/reducers/setResourceCache.ts
+++ src/reducers/setResourceCache.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 
 /** Sets a value in the resource cache. */
 const setResourceCache = (state: State, { key, value }: { key: string; value: boolean }) => ({
@@ -9,5 +10,11 @@ const setResourceCache = (state: State, { key, value }: { key: string; value: bo
     [key]: value,
   },
 })
+
+/** Action-creator for setResourceCache. */
+export const setResourceCacheActionCreator =
+  (payload: Parameters<typeof setResourceCache>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'setResourceCache', ...payload })
 
 export default _.curryRight(setResourceCache)
