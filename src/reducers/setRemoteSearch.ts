1855ecae712cda19d5136aaad6823c881e8bb832
Raine Revere <raine@cybersemics.org>
2024-05-13 18:24:42 +0000

    Co-locate setRemoteSearchActionCreator with reducer (#1797).

--- src/reducers/setRemoteSearch.ts
+++ src/reducers/setRemoteSearch.ts
@@ -1,6 +1,13 @@
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 
 /** Set availability of remote search. */
 const setRemoteSearch = (state: State, { value }: { value: boolean }): State => ({ ...state, remoteSearch: value })
+
+/** Action-creator for setRemoteSearch. */
+export const setRemoteSearchActionCreator =
+  (payload: Parameters<typeof setRemoteSearch>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'setRemoteSearch', ...payload })
 
 export default setRemoteSearch
