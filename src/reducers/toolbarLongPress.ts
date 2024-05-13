ec4d1db1a86dd7e7cc4c3e95bb2c01e102be3459
Raine Revere <raine@cybersemics.org>
2024-05-13 18:39:38 +0000

    Co-locate toolbarLongPressActionCreator with reducer (#1797).

--- src/reducers/toolbarLongPress.ts
+++ src/reducers/toolbarLongPress.ts
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import DragShortcutZone from '../@types/DragShortcutZone'
 import Shortcut from '../@types/Shortcut'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 import { AlertText, AlertType } from '../constants'
 import alert from './alert'
 
@@ -23,5 +24,11 @@ const toolbarLongPress = (state: State, { shortcut, sourceZone }: Payload) => ({
   // Prevent setting new draggedThoughtRanked before, if previous value wasn't reset to undefined
   // draggedSimplePath: state.draggedSimplePath ? (!simplePath ? undefined : state.draggedSimplePath) : simplePath,
 })
+
+/** Action-creator for long pressing a toolbar button in the customize modal. */
+export const toolbarLongPressActionCreator =
+  (payload: Parameters<typeof toolbarLongPress>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'toolbarLongPress', ...payload })
 
 export default _.curryRight(toolbarLongPress)
