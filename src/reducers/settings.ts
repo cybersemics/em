8e88de4e205ec61ef21dfd2ae07f83d6f6e4545e
Raine Revere <raine@cybersemics.org>
2024-05-13 18:26:14 +0000

    Co-locate settingsActionCreator with reducer (#1797).

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 import { EM_TOKEN } from '../constants'
 import findDescendant from '../selectors/findDescendant'
 import toggleAttribute from './toggleAttribute'
@@ -10,5 +11,11 @@ const settings = (state: State, { key, value }: { key: string; value: string }) 
   const exists = !!findDescendant(state, EM_TOKEN, emContext)
   return exists ? state : toggleAttribute(state, { path: [EM_TOKEN], values: emContext })
 }
+
+/** Action-creator for settings. */
+export const settingsActionCreator =
+  (payload: Parameters<typeof settings>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'settings', ...payload })
 
 export default _.curryRight(settings)
