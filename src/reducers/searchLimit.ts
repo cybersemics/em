1c14cb8427ede774626c79608fa42d199006dcb8
Raine Revere <raine@cybersemics.org>
2024-05-13 18:20:41 +0000

    Co-locate searchLimitActionCreator with reducer (#1797).

--- src/reducers/searchLimit.ts
+++ src/reducers/searchLimit.ts
@@ -1,10 +1,17 @@
 import _ from 'lodash'
 import State from '../@types/State'
+import Thunk from '../@types/Thunk'
 
 /** Sets the search limit. */
-const searchLimits = (state: State, { value }: { value: number }) => ({
+const searchLimit = (state: State, { value }: { value: number }) => ({
   ...state,
   searchLimit: value,
 })
 
-export default _.curryRight(searchLimits)
+/** Action-creator for searchLimit. */
+export const searchLimitActionCreator =
+  (payload: Parameters<typeof searchLimit>[1]): Thunk =>
+  dispatch =>
+    dispatch({ type: 'searchLimit', ...payload })
+
+export default _.curryRight(searchLimit)
