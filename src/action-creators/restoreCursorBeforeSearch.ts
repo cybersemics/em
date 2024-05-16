fe993c647e28490e1a4b77f96166dbfbc065d4df
Raine Revere <raine@cybersemics.org>
2024-05-16 20:19:41 +0000

    Convert restoreCursorBeforeSearch to named export.

--- src/action-creators/restoreCursorBeforeSearch.ts
+++ src/action-creators/restoreCursorBeforeSearch.ts
@@ -2,11 +2,9 @@ import Thunk from '../@types/Thunk'
 import { setCursorActionCreator as setCursor } from '../reducers/setCursor'
 
 /** Restores cursor to its position before search. */
-const restoreCursorBeforeSearch = (): Thunk => (dispatch, getState) => {
+export const restoreCursorBeforeSearch = (): Thunk => (dispatch, getState) => {
   const { cursorBeforeSearch, editing } = getState()
   if (cursorBeforeSearch) {
     dispatch(setCursor({ path: cursorBeforeSearch, editing }))
   }
 }
-
-export default restoreCursorBeforeSearch
