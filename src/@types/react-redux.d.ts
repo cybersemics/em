29f2afbbc0b53a231646e71a86ae791cffd98175
Raine Revere <raine@cybersemics.org>
2024-05-29 14:15:49 +0000

    Override useSelector and useDispatch types for react-redux >= 8.

--- src/@types/react-redux.d.ts
+++ src/@types/react-redux.d.ts
@@ -1,7 +1,14 @@
-import 'react-redux'
+// Override react-redux types to use our custom State.
+// See: paths comment in tsconfig.json
+import {
+  TypedUseSelectorHook,
+  useDispatch as useDefaultDispatch,
+  useSelector as useDefaultSelector,
+} from 'react-redux-default'
+import Dispatch from './Dispatch'
 import State from './State'
 
-declare module 'react-redux' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultRootState extends State {}
-}
+export * from 'react-redux-default'
+
+export const useDispatch: () => Dispatch = useDefaultDispatch
+export const useSelector: TypedUseSelectorHook<State> = useDefaultSelector
