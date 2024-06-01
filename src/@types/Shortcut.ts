e15c1b356bcde780a9908e258099599c7ddd80a2
Raine Revere <raine@cybersemics.org>
2024-06-01 16:05:10 +0000

    Shortcut: comment

--- src/@types/Shortcut.ts
+++ src/@types/Shortcut.ts
@@ -12,23 +12,23 @@ interface Shortcut {
   /** Allow the shortcut to be executed when a modal is open. */
   allowExecuteFromModal?: boolean
 
-  // a function that returns true if the shortcut can be executed with the current state
+  /** A function that returns true if the shortcut can be executed with the current state. */
   canExecute?: (getState: () => State) => boolean
 
-  // an ad hoc property to track conflicting shortcuts
-  // this should not be in the main Shortcut type
+  /** An ad hoc property to track conflicting shortcuts. */
+  // TODO: Refactor so this is not in the main Shortcut type.
   conflicts?: string[]
 
-  // a description of what the shortcut does that is shown in the Help modal
+  /** A description of what the shortcut does that is shown in the Help modal. */
   description?: string | ((getState: () => State) => string)
 
-  // a description of what the shortcut does whnn it is in an inverse state
+  /** A description of what the shortcut does whnn it is in an inverse state. */
   descriptionInverse?: string | ((getState: () => State) => string)
 
-  // a function that returns an error message if the shortcut should indicate an error
+  /** A function that returns an error message if the shortcut should indicate an error. */
   error?: (getState: () => State) => string | null
 
-  // executes the shortcut
+  /** Executes the shortcut. */
   exec: (
     dispatch: Dispatch,
     getState: () => State,
@@ -36,34 +36,34 @@ interface Shortcut {
     { type }: { type: ShortcutType },
   ) => void
 
-  // a MultiGesture sequence to activate the shortcut on touch screens
+  /** A MultiGesture sequence to activate the shortcut on touch screens. */
   gesture?: GesturePath | GesturePath[]
 
-  // hide the shortcut CommandPalette
+  /** Hide the shortcut in the CommandPalette. */
   hideFromCommandPalette?: boolean
 
-  // hide the shortcut from the Help modal
+  /** Hide the shortcut in the Help modal and CommandPalette. */
   hideFromInstructions?: boolean
 
-  // a readable, internal unique id
+  /** A readable, internal unique id. */
   id: ShortcutId
 
-  // a function that returns true if the shortcut should be highlighted in the Toolbar
+  /** A function that returns true if the shortcut should be highlighted in the Toolbar. */
   isActive?: (getState: () => State) => boolean
 
-  // when true, a small open dropdown indicator will be rendered beneath the icon
+  /** When true, a small open dropdown indicator will be rendered beneath the icon. */
   isDropdownOpen?: (getState: () => State) => boolean
 
-  // a keyboard sequence to activate the shortcut
+  /** A keyboard sequence to activate the shortcut. */
   keyboard?: Key | string
 
-  // short label
+  /** Short label. */
   label: string
 
-  // For toggling shortcuts, a short label that indicates the inverse action from the current state (e.g. "Add to Favorites" and "Remove from Favorites")
+  /** For toggling shortcuts, a short label that indicates the inverse action from the current state (e.g. "Add to Favorites" and "Remove from Favorites"). */
   labelInverse?: string
 
-  // specify backup gesture or keyboard that is shown in the Toolbar overlay
+  /** Specify backup gesture or keyboard that is shown in the Toolbar overlay. */
   overlay?: {
     gesture?: GesturePath
     keyboard?: Key | string
