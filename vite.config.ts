f93238d7e40a5f385df2e1147286a74e0aa7c1b5
Hugo Martinez <hugomartinezdev@gmail.com>
2024-06-04 09:31:59 -0400

    Fix ServiceWorker (#1996)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ injectRegister: null, strategies: 'injectManifest', srcDir: 'src', filename: 'service-worker.ts' }),
+  ],
 })
