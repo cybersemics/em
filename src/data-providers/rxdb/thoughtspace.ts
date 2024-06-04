fd1e9a7291f4f8d5adfc405395bd4483c8b6aa30
Hugo Martinez <hugomartinezdev@gmail.com>
2024-06-04 16:33:08 -0400

    Create RxDB Lexeme schema and collection

--- src/data-providers/rxdb/thoughtspace.ts
+++ src/data-providers/rxdb/thoughtspace.ts
@@ -1,6 +1,7 @@
 import { RxCollection, RxDatabase, addRxPlugin, createRxDatabase } from 'rxdb'
 import { getRxStorageIndexedDB } from 'rxdb-premium/plugins/storage-indexeddb'
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode'
+import { LexemeDocType, lexemeSchema } from './schemas/lexeme'
 import { ThoughtDocType, thoughtSchema } from './schemas/thought'
 
 if (import.meta.env.MODE !== 'production') {
@@ -11,6 +12,7 @@ const DATABASE_NAME = 'em'
 
 type EmRxDB = RxDatabase<{
   thoughts: RxCollection<ThoughtDocType>
+  lexemes: RxCollection<LexemeDocType>
 }>
 
 /* rxDB database */
@@ -26,6 +28,9 @@ export const init = async () => {
   await rxDB.addCollections({
     thoughts: {
       schema: thoughtSchema,
+    },
+    lexemes: {
+      schema: lexemeSchema,
     },
   })
 }
