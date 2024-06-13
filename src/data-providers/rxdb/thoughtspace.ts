e2b33027e017f60ef66971c19bd16021ba9eb751
Hugo Martinez <hugomartinezdev@gmail.com>
2024-06-13 07:22:36 -0400

    Remove TextEncoder from setupTests and ignore duplicate RxDB instances for test environment

--- src/data-providers/rxdb/thoughtspace.ts
+++ src/data-providers/rxdb/thoughtspace.ts
@@ -5,7 +5,7 @@ import { LexemeDocType, lexemeSchema } from './schemas/lexeme'
 import { PermissionDocType, permissionSchema } from './schemas/permission'
 import { ThoughtDocType, thoughtSchema } from './schemas/thought'
 
-if (import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE === 'development') {
   addRxPlugin(RxDBDevModePlugin)
 }
 
@@ -25,6 +25,7 @@ export const init = async () => {
   rxDB = await createRxDatabase({
     name: DATABASE_NAME,
     storage: getRxStorageIndexedDB(),
+    ignoreDuplicate: import.meta.env.MODE === 'test',
   })
 
   await rxDB.addCollections({
