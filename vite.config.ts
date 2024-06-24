c387a4f26adcb33f6f0bc24f327a366b26828f69
Hugo Martinez <hugomartinezdev@gmail.com>
2024-06-24 14:44:56 -0400

    Add rxdb-premium script to build the project using the IndexedDB storage

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { defineConfig, loadEnv } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      'react-native': 'react-native-web',
+/** Vite configuration - https://vitejs.dev/config/. */
+const config = ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    resolve: {
+      alias: {
+        'react-native': 'react-native-web',
+        /*
+          Use the premium RxDB IndexedDB plugin if the environment variable is set to true.
+          Otherwise, use the default Dexie plugin.
+         */
+        'rxdb-indexeddb':
+          env.VITE_USE_RXDB_PREMIUM === 'true'
+            ? resolve(__dirname, 'node_modules/rxdb-premium/plugins/storage-indexeddb')
+            : resolve(__dirname, 'node_modules/rxdb/plugins/storage-dexie'),
+      },
     },
-  },
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    react(),
-    VitePWA({ injectRegister: null, strategies: 'injectManifest', srcDir: 'src', filename: 'service-worker.ts' }),
-  ],
-})
+    build: {
+      outDir: 'build',
+    },
+    plugins: [
+      react(),
+      VitePWA({ injectRegister: null, strategies: 'injectManifest', srcDir: 'src', filename: 'service-worker.ts' }),
+    ],
+  })
+}
+
+export default config
