9fe47ac956025cb1c467b1c4fb3c79649970ec8c
Raine Revere <raine@cybersemics.org>
2024-06-25 18:26:26 +0000

    Thought: Animate hovering child with pulse.

--- src/components/Thought.tsx
+++ src/components/Thought.tsx
@@ -27,7 +27,7 @@ import getStyle from '../selectors/getStyle'
 import getThoughtById from '../selectors/getThoughtById'
 import isContextViewActive from '../selectors/isContextViewActive'
 import rootedParentOf from '../selectors/rootedParentOf'
-// import themeColors from '../selectors/themeColors'
+import themeColors from '../selectors/themeColors'
 import distractionFreeTypingStore from '../stores/distractionFreeTyping'
 import equalPath from '../util/equalPath'
 import equalThoughtRanked from '../util/equalThoughtRanked'
@@ -183,7 +183,7 @@ const ThoughtContainer = ({
 
   const hideBullet = useHideBullet({ children, env, hideBulletProp, isEditing, simplePath, thoughtId })
   const style = useThoughtStyle({ children, env, styleProp, thoughtId })
-  // const colors = useSelector(themeColors)
+  const colors = useSelector(themeColors)
   const styleAnnotation = useSelector(
     state =>
       safeRefMerge(
@@ -266,30 +266,21 @@ const ThoughtContainer = ({
   // Highlight the parent of the current drop target to make it easier to drop in the intended place.
   // Use -webkit-text-stroke-width instead of font-weight:bold, as bold changes the width of the text and can cause the thought to become multiline during a drag. This can even create an oscillation effect as the increased Thought height triggers a different hoveringPath ad infinitum (often resulting in a Shaker cancel false positive).
   // See: https://stackoverflow.com/a/46452396/480608
-  // const styleThought = useMemo(
-  //   (): React.CSSProperties => ({
-  //     ...(isChildHovering
-  //       ? { color: isChildHovering ? '#538392' : '#FFFFFF', transition: 'color 1s', WebkitTextStrokeWidth: '0.05em' }
-  //       : null),
-  //     // textDecoration does not inherit from inline-block elements, so we apply it here instead of .child
-  //     textDecoration: style?.textDecoration,
-  //   }),
-  //   [isChildHovering, style?.textDecoration],
-  // )
-
   const styleThought = useMemo(
     (): React.CSSProperties => ({
       ...(isChildHovering
         ? {
             WebkitTextStrokeWidth: '0.05em',
-            animation: 'animateHoverColor 2s linear infinite',
+            animation: 'pulse 0.5s linear infinite alternate',
+            color: colors.highlight,
           }
         : null),
       // textDecoration does not inherit from inline-block elements, so we apply it here instead of .child
       textDecoration: style?.textDecoration,
     }),
-    [isChildHovering, style?.textDecoration],
+    [colors.highlight, isChildHovering, style?.textDecoration],
   )
+
   // useWhyDidYouUpdate('<Thought> ' + prettyPath(store.getState(), simplePath), {
   //   allowSingleContext,
   //   childrenForced,
