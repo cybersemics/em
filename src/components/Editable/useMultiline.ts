0bbd038011dc8a45860179e19acbd77c5e491066
Fabian Finke <finkef@icloud.com>
2024-06-26 15:26:23 +0200

    Delay multiline calculations until changes have been applied to the DOM

--- src/components/Editable/useMultiline.ts
+++ src/components/Editable/useMultiline.ts
@@ -13,17 +13,21 @@ const useMultiline = (contentRef: React.RefObject<HTMLElement>, simplePath: Simp
   const splitPosition = useSelector(state => state.splitPosition)
 
   const updateMultiline = useCallback(() => {
-    if (!contentRef.current) return
+    // useSelectorEffect fires _before_ the DOM updates, so we need to
+    // schedule the measurement using requestAnimationFrame.
+    requestAnimationFrame(() => {
+      if (!contentRef.current) return
 
-    const height = contentRef.current.getBoundingClientRect().height
-    // 1.72 must match line-height as defined in .thought-container
-    const singleLineHeight = fontSize * 1.72
-    // .editable.multiline gets 5px of padding-top to offset the collapsed line-height
-    // we need to account for padding-top, otherwise it can cause a false positive
-    const paddingTop = parseInt(window.getComputedStyle(contentRef.current).paddingTop)
-    // The element is multiline if its height is twice the single line height.
-    // (Actually we just check if it is over 1.5x the single line height for a more forgiving condition.)
-    setMultiline(height - paddingTop > singleLineHeight * 1.5)
+      const height = contentRef.current.getBoundingClientRect().height
+      // 1.72 must match line-height as defined in .thought-container
+      const singleLineHeight = fontSize * 1.72
+      // .editable.multiline gets 5px of padding-top to offset the collapsed line-height
+      // we need to account for padding-top, otherwise it can cause a false positive
+      const paddingTop = parseInt(window.getComputedStyle(contentRef.current).paddingTop)
+      // The element is multiline if its height is twice the single line height.
+      // (Actually we just check if it is over 1.5x the single line height for a more forgiving condition.)
+      setMultiline(height - paddingTop > singleLineHeight * 1.5)
+    })
   }, [contentRef, fontSize])
 
   // Recalculate multiline when the cursor changes.
