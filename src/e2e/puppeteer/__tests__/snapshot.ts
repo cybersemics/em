f685da1625738f59576522157b35ac4a2073527c
Jay Mathis <code@mathis.network>
2024-06-27 12:55:05 -0500

    do not run table tests for every font size

--- src/e2e/puppeteer/__tests__/snapshot.ts
+++ src/e2e/puppeteer/__tests__/snapshot.ts
@@ -209,55 +209,55 @@ const testSuite = () => {
       expect(image).toMatchImageSnapshot()
     })
   })
-
-  describe('Table View', () => {
-    beforeEach(removeHUD)
-
-    /**
-     * "col1 narrow" means that all thoughts in the first column have short text, so the first table column can be narrower in order to give plenty of room for the second column.
-     * "no wrap" refers to the fact that all the thoughts in the second column can fit on a single line.
-     */
-    it('col1 narrow + no wrap width', async () => {
-      await paste(`
-        - X
-          - =view
-            - Table
-          - A
-            - Atlanta, Georgia
-          - B
-            - Boston, Massachusetts
-          - C
-            - Cincinnati, Ohio
-      `)
-
-      await sleep(500)
-      const image = await screenshot()
-      expect(image).toMatchImageSnapshot()
-    })
-
-    /**
-     * "col2 narrow" means that all thoughts in the second column have short text, so the second table column can be narrower in order to give plenty of room for the first column.
-     * "no wrap" refers to the fact that all the thoughts in the first column can fit on a single line.
-     */
-    it('col2 narrow + no wrap width', async () => {
-      await paste(`
-        - X
-          - =view
-            - Table
-          - All of Us Strangers
-            - 4/5
-          - Eileen
-            - 2/5
-          - May December
-            - 2/5
-      `)
-
-      await sleep(500)
-      const image = await screenshot()
-      expect(image).toMatchImageSnapshot()
-    })
-  })
 }
+
+describe('Table View', () => {
+  beforeEach(removeHUD)
+
+  /**
+   * "col1 narrow" means that all thoughts in the first column have short text, so the first table column can be narrower in order to give plenty of room for the second column.
+   * "no wrap" refers to the fact that all the thoughts in the second column can fit on a single line.
+   */
+  it('col1 narrow + no wrap width', async () => {
+    await paste(`
+      - X
+        - =view
+          - Table
+        - A
+          - Atlanta, Georgia
+        - B
+          - Boston, Massachusetts
+        - C
+          - Cincinnati, Ohio
+    `)
+
+    await sleep(500)
+    const image = await screenshot()
+    expect(image).toMatchImageSnapshot()
+  })
+
+  /**
+   * "col2 narrow" means that all thoughts in the second column have short text, so the second table column can be narrower in order to give plenty of room for the first column.
+   * "no wrap" refers to the fact that all the thoughts in the first column can fit on a single line.
+   */
+  it('col2 narrow + no wrap width', async () => {
+    await paste(`
+      - X
+        - =view
+          - Table
+        - All of Us Strangers
+          - 4/5
+        - Eileen
+          - 2/5
+        - May December
+          - 2/5
+    `)
+
+    await sleep(1000)
+    const image = await screenshot()
+    expect(image).toMatchImageSnapshot()
+  })
+})
 
 describe('Font Size: 18 (default)', () => {
   // run the snapshot tests at font size 18 (default)
