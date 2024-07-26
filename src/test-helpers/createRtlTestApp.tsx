9a6f080c559422868d423f982db9532dce0ce4a6
Raine Revere <raine@cybersemics.org>
2024-07-26 21:15:42 +0000

    createRtlTestApp: Use App.tsx.

--- src/test-helpers/createRtlTestApp.tsx
+++ src/test-helpers/createRtlTestApp.tsx
@@ -1,31 +1,15 @@
 import { render } from '@testing-library/react'
-import React, { createRef } from 'react'
+import { createRef } from 'react'
 import { wrapInTestContext } from 'react-dnd-test-utils'
 import { act } from 'react-dom/test-utils'
-import { Provider } from 'react-redux'
 import Await from '../@types/Await'
 import { clearActionCreator as clear } from '../actions/clear'
-import AppComponent from '../components/AppComponent'
-import ErrorBoundaryContainer from '../components/ErrorBoundaryContainer'
-import TouchMonitor from '../components/TouchMonitor'
+import { App } from '../components/App'
 import * as db from '../data-providers/yjs/thoughtspace'
 import { initialize } from '../initialize'
 import store from '../stores/app'
 import storage from '../util/storage'
 
-/**
- * Test App.
- */
-// eslint-disable-next-line react/display-name, react-refresh/only-export-components
-export const App = React.forwardRef(() => (
-  <Provider store={store}>
-    <ErrorBoundaryContainer>
-      <TouchMonitor>
-        <AppComponent />
-      </TouchMonitor>
-    </ErrorBoundaryContainer>
-  </Provider>
-))
 let cleanup: Await<ReturnType<typeof initialize>>['cleanup']
 
 /** Set up testing and mock document and window functions. */
@@ -61,7 +45,6 @@ const createTestApp = async () => {
 }
 
 /** Clear store, localStorage, local db, and window event handlers. */
-// eslint-disable-next-line react-refresh/only-export-components
 export const cleanupTestApp = async () => {
   await act(async () => {
     // clear localStorage before dispatching clear action, since initialState reads from localStorage
@@ -85,7 +68,6 @@ export const cleanupTestApp = async () => {
 }
 
 /** Refresh the test app. */
-// eslint-disable-next-line react-refresh/only-export-components
 export const refreshTestApp = async () => {
   await act(async () => {
     await store.dispatch(clear())
@@ -93,5 +75,4 @@ export const refreshTestApp = async () => {
   })
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 export default createTestApp
