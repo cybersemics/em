a63bd0c6fe4dd825be8f1da1dbaafca6b8b1d6ba
Raine Revere <raine@cybersemics.org>
2024-08-01 19:58:31 +0000

    DragAndDropType enum

--- src/@types/DragThoughtItem.ts
+++ src/@types/DragThoughtItem.ts
@@ -3,8 +3,6 @@ import DragThoughtZone from './DragThoughtZone'
 import Path from './Path'
 import SimplePath from './SimplePath'
 
-type DragAndDropType = 'thought' | 'toolbar-button'
-
 /** Represents the currently dragged thought from react-dnd. Returned by monitor.getItem() in the drop handler. */
 interface DragThoughtItem {
   path: Path
