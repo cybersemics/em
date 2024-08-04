2436a0ef40146a068dc9bf4be78a2c7ba0d56f68
Jay Mathis <jrmathis.home@gmail.com>
2024-08-04 08:27:42 -0500

    Fix #543; divider width should match largest sibling (#2108)
    Co-authored-by: Raine Revere <raine@cybersemics.org>
    resolve #543; divider alignment and width calculation
    resolve further issues with #543 discussed in PR 2108#pullrequestreview-2156802356

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,16 +1,24 @@
 import classNames from 'classnames'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { createRef, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Path from '../@types/Path'
+import State from '../@types/State'
 import { setCursorActionCreator as setCursor } from '../actions/setCursor'
-import { DIVIDER_MIN_WIDTH, DIVIDER_PLUS_PX } from '../constants'
+import editingValueStore from '../stores/editingValue'
 import fastClick from '../util/fastClick'
+import hashPath from '../util/hashPath'
 import head from '../util/head'
+import parentOf from '../util/parentOf'
+
+const DIVIDER_PLUS_PX = 20
+const DIVIDER_MIN_WIDTH = 85
 
 /** A custom horizontal rule. */
 const Divider = ({ path }: { path: Path }) => {
-  const dividerSetWidth = React.createRef<HTMLInputElement>()
   const dispatch = useDispatch()
+  const dividerRef = createRef<HTMLDivElement>()
+  const thoughtIndex = useSelector((state: State) => state.thoughts.thoughtIndex)
+  const [width, setWidth] = useState(DIVIDER_MIN_WIDTH)
 
   /** Sets the cursor to the divider. */
   const setCursorToDivider = (e: React.MouseEvent | React.TouchEvent) => {
@@ -18,33 +26,57 @@ const Divider = ({ path }: { path: Path }) => {
     dispatch(setCursor({ path }))
   }
 
-  /** Get the max width of nearby for divider list child elements, add 30 px and set this width for divider. */
+  /** Get the max width of nearby elements, add DIVIDER_PLUS_PX and set this width for divider. */
   const setStyle = () => {
-    if (dividerSetWidth.current) {
-      const parentUl = dividerSetWidth.current.closest('ul')
-      const children = parentUl ? (Array.from(parentUl.childNodes) as HTMLElement[]) : []
-      const widths = children.map((child: HTMLElement) => {
-        if (child.classList.contains('child-divider')) return DIVIDER_PLUS_PX
-        const subs = child.getElementsByClassName('subthought') as HTMLCollectionOf<HTMLElement>
-        return subs.length ? subs[0].offsetWidth + DIVIDER_PLUS_PX : DIVIDER_PLUS_PX
-      })
-      const maxWidth = Math.max(...widths)
-      dividerSetWidth.current.style.width = `${maxWidth > DIVIDER_MIN_WIDTH ? maxWidth : DIVIDER_MIN_WIDTH}px`
+    if (dividerRef.current) {
+      const parentPath = parentOf(path)
+
+      const treeNode = dividerRef.current.closest('div.tree-node') as HTMLElement
+      if (!treeNode) throw new Error('Divider tree node not found')
+
+      const thoughtPath = treeNode.dataset.path
+      if (!thoughtPath || thoughtPath === '') throw new Error('Divider path not found on tree node')
+
+      let elements = document.querySelectorAll(`.tree-node[data-path^="${hashPath(parentPath)}"]:not(.thought-divider)`)
+
+      if (elements.length === 1) {
+        /** If this divider is an only child, search further up the tree. */
+        const grandparentPath = parentOf(parentPath)
+        if (!grandparentPath) throw new Error('Divider grandparent not found')
+
+        elements = document.querySelectorAll(
+          `.tree-node[data-path^="${hashPath(grandparentPath)}"]:not(.thought-divider)`,
+        )
+      } else if (!elements.length) {
+        /** This is a top-level divider, use its siblings instead. */
+        elements = document.querySelectorAll('.tree-node.top-level:not(.thought-divider)')
+      }
+
+      /** Find and set the max width of our selected elements. */
+      const maxWidth = Math.max(
+        ...Array.from(elements).map(element => (element.querySelector('.thought') as HTMLElement).offsetWidth),
+      )
+
+      setWidth(maxWidth + DIVIDER_PLUS_PX)
     }
   }
 
-  useEffect(setStyle)
+  // Suppress the exhaustive-deps linter rule so that we can ignore dividerRef. Refs do not work in useEffect dependencies, as mutations do not trigger a re-render.
+  // We could use a callback ref to trigger setStyle every time the DOM node changes, but subscribing to the thoughtIndex (which is included for other thought updates) seems to cover all cases.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(setStyle, [path, thoughtIndex])
+  editingValueStore.useEffect(setStyle)
 
   return (
     <div
       aria-label='divider'
-      ref={dividerSetWidth}
+      ref={dividerRef}
       style={{
         margin: '-2px -4px -5px',
         maxWidth: '100%',
         padding: '10px 4px 16px',
         position: 'relative',
-        width: 85,
+        width: width > DIVIDER_MIN_WIDTH ? width : DIVIDER_MIN_WIDTH,
       }}
       className='divider-container z-index-stack'
       {...fastClick(setCursorToDivider)}
--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -30,6 +30,7 @@ import { appendToPathMemo } from '../util/appendToPath'
 import equalPath from '../util/equalPath'
 import hashPath from '../util/hashPath'
 import head from '../util/head'
+import isDivider from '../util/isDivider'
 import isRoot from '../util/isRoot'
 import parentOf from '../util/parentOf'
 import parseLet from '../util/parseLet'
@@ -670,7 +671,14 @@ const LayoutTree = () => {
                 // The key must be unique to the thought, both in normal view and context view, in case they are both on screen.
                 // It should not be based on editable values such as Path, value, rank, etc, otherwise moving the thought would make it appear to be a completely new thought to React.
                 key={key}
-                className={classNames({ 'table-col1': isTableCol1 })}
+                data-path={hashPath(path)}
+                className={classNames({
+                  'tree-node': true,
+                  'top-level': simplePath.length === 1,
+                  'table-col1': isTableCol1,
+                  'table-col2': isTableCol2,
+                  'thought-divider': isDivider(thought.value),
+                })}
                 style={{
                   position: 'absolute',
                   // Cannot use transform because it creates a new stacking context, which causes later siblings' DropChild to be covered by previous siblings'.
