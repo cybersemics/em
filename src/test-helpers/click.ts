6af1d8484c99c4e8baf45648846d46d8ad2cc2bf
Trevin Hofmann <trevinhofmann@gmail.com>
2024-08-05 14:59:24 -0500

    test: update the click test helper to simulate mouseDown and mouseUp separately

--- src/test-helpers/click.ts
+++ src/test-helpers/click.ts
@@ -1,11 +1,13 @@
-import { act } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { act, fireEvent } from '@testing-library/react'
 
-/** Clicks an element within an act block. */
+/** Clicks an element within an act block, simulating mousedown and mouseup separately. */
 const click = async (selector: string) => {
   const el = document.querySelector(selector)!
   await act(async () => {
-    userEvent.click(el)
+    fireEvent.mouseDown(el)
+  })
+  await act(async () => {
+    fireEvent.mouseUp(el)
   })
 }
 
