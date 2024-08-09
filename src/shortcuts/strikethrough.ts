ad655e9486928f3dad57052eeb2c596fa381a4b9
Trevin Hofmann <trevinhofmann@gmail.com>
2024-08-09 08:23:35 -0500

    fix: correctly activate bold/italic/strikethrough/underline buttons on selection (#2211)
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- src/shortcuts/strikethrough.ts
+++ src/shortcuts/strikethrough.ts
@@ -1,8 +1,6 @@
 import Shortcut from '../@types/Shortcut'
 import { formatSelectionActionCreator as formatSelection } from '../actions/formatSelection'
 import Icon from '../components/icons/StrikethroughIcon'
-import getThoughtById from '../selectors/getThoughtById'
-import head from '../util/head'
 import isDocumentEditable from '../util/isDocumentEditable'
 
 /** Toggles formatting of the current browser selection as strikethrough. If there is no selection, formats the entire thought. */
@@ -18,12 +16,7 @@ const strikethrough: Shortcut = {
     e.preventDefault()
     dispatch(formatSelection('strikethrough'))
   },
-  isActive: getState => {
-    const state = getState()
-    if (!state.cursor) return false
-    const thought = getThoughtById(state, head(state.cursor))
-    return thought.value.includes('<strike>')
-  },
+  // the activation logic for commands is located in ToolbarButton (isCommandActive)
 }
 
 export default strikethrough
