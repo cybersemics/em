cb4c9078c72547b07379ed2df463424ca34e8dfc
Raine Revere <raine@cybersemics.org>
2024-08-09 14:39:44 +0000

    cursorChanged: Remove cursor thought validation (should be stable by now).

--- src/redux-middleware/cursorChanged.ts
+++ src/redux-middleware/cursorChanged.ts
@@ -1,28 +1,8 @@
 import { ThunkMiddleware } from 'redux-thunk'
 import State from '../@types/State'
-import { HOME_TOKEN } from '../constants'
 import getThoughtById from '../selectors/getThoughtById'
 import editingValueStore from '../stores/editingValue'
 import head from '../util/head'
-import isRoot from '../util/isRoot'
-
-/** Asserts that the cursor thought is valid. */
-const validateCursor = (state: State) => {
-  const thought = state.cursor ? getThoughtById(state, head(state.cursor)) : null
-  if (state.cursor && !thought) {
-    const errorMessage = `Cursor thought does not exist: ${state.cursor}`
-    console.error(errorMessage, {
-      cursor: state.cursor,
-    })
-    throw new Error(errorMessage)
-  } else if (state.cursor && isRoot(state.cursor)) {
-    const errorMessage = `Cursor should be set to null, not [${HOME_TOKEN}]`
-    console.error(errorMessage, {
-      cursor: state.cursor,
-    })
-    throw new Error(errorMessage)
-  }
-}
 
 /** The live editing value is stored in a separate ministore to avoid Redux store churn. When the cursor changes, update the editingValue store. */
 const updateEditingValue = (state: State) => {
@@ -38,7 +18,6 @@ const cursorChangedMiddleware: ThunkMiddleware<State> = ({ getState }) => {
     next(action)
     const state = getState()
 
-    validateCursor(state)
     updateEditingValue(state)
   }
 }
