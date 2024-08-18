d20642c9b0f680461623e023dc365b7bd40bb65f
Raine Revere <raine@cybersemics.org>
2024-08-18 19:40:00 +0000

    RecentlyEdited: Show LoadingEllipsis while loading.

--- src/components/RecentlyEdited.tsx
+++ src/components/RecentlyEdited.tsx
@@ -1,25 +1,28 @@
 import _ from 'lodash'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Thunk from '../@types/Thunk'
 import { pullActionCreator as pull } from '../actions/pull'
 import recentlyEdited from '../selectors/recentlyEdited'
 import hashPath from '../util/hashPath'
 import nonNull from '../util/nonNull'
+import LoadingEllipsis from './LoadingEllipsis'
 import ThoughtLink from './ThoughtLink'
 
 /** Pulls all paths in the jump history. */
 const pullJumpHistory = (): Thunk => async (dispatch, getState) => {
   const state = getState()
   const paths = state.jumpHistory.filter(nonNull)
-  // Pull all thoughts at once, rather than incrementally.
-  // Otherwise thoughts can disappear as recentlyEdited is updated, which is visually disruptive.
   return dispatch(pull(paths.flat()))
 }
 
 /** Recently edited thoughts derived from the jump history. */
 const RecentlyEdited = () => {
   const dispatch = useDispatch()
+
+  // Only render when all the thoughts in the jump history have been pulled.
+  // Otherwise thoughts can disappear as recentlyEdited is updated, which is visually disruptive.
+  const [loaded, setLoaded] = useState(false)
 
   const jumpHistory = useSelector(recentlyEdited, _.isEqual)
 
@@ -28,12 +31,17 @@ const RecentlyEdited = () => {
 
   useEffect(() => {
     // TODO: Fix type
-    dispatch(pullJumpHistory()) as unknown as Promise<void>
+    const p = dispatch(pullJumpHistory()) as unknown as Promise<void>
+    p.then(() => {
+      setLoaded(true)
+    })
   }, [dispatch])
 
   return (
     <div style={{ marginBottom: '4em', marginTop: '1.5em' }}>
-      {paths.length > 0 ? (
+      {!loaded ? (
+        <LoadingEllipsis />
+      ) : paths.length > 0 ? (
         <div>
           {paths.map(path => (
             <ThoughtLink key={hashPath(path)} path={path} />
