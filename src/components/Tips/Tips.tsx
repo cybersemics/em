4f77d17701c72c441f33747f879fbf2c3136942e
Trevin Hofmann <trevinhofmann@gmail.com>
2024-08-20 16:56:15 -0500

    refactor: inline the getTip selector

--- src/components/Tips/Tips.tsx
+++ src/components/Tips/Tips.tsx
@@ -1,13 +1,12 @@
 import { FC, Fragment } from 'react'
 import { useSelector } from 'react-redux'
 import TipId from '../../@types/TipId'
-import getTip from '../../selectors/getTip'
 import NewSubthoughtTip from './NewSubthoughtTip'
 import NewThoughtTip from './NewThoughtTip'
 
 /** A container for rendering all of the tips. */
 const Tips: FC = () => {
-  const tip = useSelector(getTip)
+  const tip = useSelector(state => state.tips[0])
   return (
     <Fragment>
       <NewThoughtTip display={tip === TipId.NewThought} />
--- src/selectors/getTip.ts
+++
@@ -1,7 +0,0 @@
-import State from '../@types/State'
-import TipId from '../@types/TipId'
-
-/** Gets the tip to be displayed. */
-export const getTip = (state: State): TipId | undefined => state.tips[0]
-
-export default getTip
