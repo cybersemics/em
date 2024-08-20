a345ed330b37b51a5448dc8ca67d0c30d41256f4
Trevin Hofmann <trevinhofmann@gmail.com>
2024-08-19 23:49:17 -0500

    fix: change the useIsVisible hook to manually calculate visibility from getBoundingClientRect

--- src/hooks/useIsVisible.ts
+++ src/hooks/useIsVisible.ts
@@ -1,28 +1,33 @@
 import { useEffect, useRef, useState } from 'react'
 
-/**
- * Determines if an element is visible.
- */
+/** Determines if an element is partially visible in the viewport. */
 const useIsVisible = <T extends HTMLElement>(initialValue = false) => {
   const [isVisible, setIsVisible] = useState(initialValue)
   const elementRef = useRef<T | null>(null)
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      const visible = entry.isIntersecting
-      setIsVisible(visible)
-    })
+  /** Determines if the element is visible in the viewport. */
+  const checkVisibility = () => {
     const element = elementRef.current
 
     if (element) {
-      observer.observe(element)
+      const rect = element.getBoundingClientRect()
+      const top = rect.top < window.innerHeight
+      const bottom = rect.bottom > 0
+      const left = rect.left < window.innerWidth
+      const right = rect.right > 0
+      const visible = top && bottom && left && right
+      setIsVisible(visible)
     }
+  }
+
+  useEffect(() => {
+    checkVisibility()
+    window.addEventListener('scroll', checkVisibility)
+    window.addEventListener('resize', checkVisibility)
 
     return () => {
-      if (element) {
-        observer.unobserve(element)
-      }
-      observer.disconnect()
+      window.removeEventListener('scroll', checkVisibility)
+      window.removeEventListener('resize', checkVisibility)
     }
   }, [])
 
