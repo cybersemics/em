2cb1730d40d1d5e842219811e54d3e51a61668e0
Raine Revere <raine@cybersemics.org>
2024-08-25 23:37:31 +0000

    vite-plugin-html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
@@ -19,5 +20,7 @@ export default defineConfig({
     // The dev server is usually running anyway, and tsc is run in lint:tsc which is triggered prepush.
     ...[!process.env.VITEST ? checker({ typescript: true }) : undefined],
     VitePWA({ injectRegister: null, strategies: 'injectManifest', srcDir: 'src', filename: 'service-worker.ts' }),
+    // minify and add EJS capabilities to index.html
+    createHtmlPlugin({ minify: true }),
   ],
 })
