b66ed0b0f6c0e6d0863d9d6c745aec99b17bb3cb
Christina Yang <ychristina011@gmail.com>
2024-08-26 17:39:09 -0700

    add fontSize tokens to panda.config to replace text-small and text-medium classes

--- panda.config.ts
+++ panda.config.ts
@@ -16,7 +16,18 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        fontSizes: {
+          sm: {
+            value: '80%',
+          },
+          md: {
+            value: '90%',
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
