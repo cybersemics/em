003ca4c0c5584dba7b8f091e039fed8839e2a5fc
Raine Revere <raine@cybersemics.org>
2024-08-27 21:47:30 +0000

    Rename getSubthoughtsByContext -> getDescendantsOfContext.

--- src/test-helpers/queries/getDescendantsOfContext.ts
+++ src/test-helpers/queries/getDescendantsOfContext.ts
@@ -2,8 +2,8 @@ import { screen } from '@testing-library/dom'
 import contextToPath from '../../selectors/contextToPath'
 import store from '../../stores/app'
 
-/** Get a list of subthoughts of a thought with the context. */
-export function getSubthoughtsByContext(context: string[]) {
+/** Get a list of descendant thoughts of a context. */
+export function getDescendantsOfContext(context: string[]) {
   const pathOfThoughtA = contextToPath(store.getState(), context)
   const matcherThoughtA = new RegExp(`thought-${pathOfThoughtA}[^"]+`)
   const subthoughtsOfA = screen.getAllByTestId(matcherThoughtA)
