a6d6e8b4bf6a4f6412b023e6d23ed4c0c40c9567
Fabian Finke <finkef@icloud.com>
2024-08-28 19:11:40 +0200

    Run puppeteer with dockerized chromium

--- src/e2e/puppeteer-environment.ts
+++ src/e2e/puppeteer-environment.ts
@@ -1,6 +1,5 @@
-import puppeteer from 'puppeteer'
-import type { Environment } from 'vitest'
-import { builtinEnvironments, populateGlobal } from 'vitest/environments'
+import puppeteer from 'puppeteer-core'
+import { type Environment, builtinEnvironments, populateGlobal } from 'vitest/environments'
 
 /** Puppeteer Environment for vitest. */
 const PuppeteerEnvironment: Environment = {
@@ -9,28 +8,22 @@ const PuppeteerEnvironment: Environment = {
   async setup(global, options) {
     builtinEnvironments['happy-dom'].setup(global, options)
 
+    const args = [
+      '--deterministic-fetch',
+      '--disable-dev-shm-usage',
+      '--disable-features=IsolateOrigins',
+      '--disable-setuid-sandbox',
+      '--disable-site-isolation-trials',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+    ]
+
     const browser = await puppeteer
-      .launch({
-        headless: true,
-        // Disable Chrome features that crash GitHub Actions with "Protocol error (Target.createTarget): Target closed."
-        // See: https://stackoverflow.com/a/66994528/480608
-        // List of Chromium switches: https://peter.sh/experiments/chromium-command-line-switches/
-        args: [
-          '--deterministic-fetch',
-          '--disable-dev-shm-usage',
-          '--disable-features=IsolateOrigins',
-          '--disable-setuid-sandbox',
-          '--disable-site-isolation-trials',
-          '--no-first-run',
-          '--no-sandbox',
-          '--no-zygote',
-        ],
-        // Increase timeout to avoid: "TimeoutError: Timed out after 30000 ms while trying to connect to the browser! Only Chrome at revision r869685 is guaranteed to work."
-        timeout: 60000,
-      })
+      .connect({ browserWSEndpoint: `ws://localhost:7566?${args.join('&')}` })
       // catch and log a launch error, otherwise it will not appear in the CI logs
       .catch((err: Error) => {
-        console.error('Error launching puppeteer:', err)
+        console.log('Could not connect to browserless.\nMake sure to run `yarn browserless` before running the tests.')
         throw err
       })
 
@@ -44,10 +37,7 @@ const PuppeteerEnvironment: Environment = {
 
     return {
       async teardown() {
-        if (browser) {
-          await browser.close()
-        }
-
+        await browser.disconnect()
         keys.forEach(key => delete global[key])
         originals.forEach((v, k) => (global[k] = v))
       },
