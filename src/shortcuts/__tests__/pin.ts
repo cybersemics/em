49ece5787219272d1023a403aa99f03af0b1a26e
Fabian Finke <finkef@icloud.com>
2024-09-13 16:35:10 +0200

    Add pin multicursor tests

--- src/shortcuts/__tests__/pin.ts
+++ src/shortcuts/__tests__/pin.ts
@@ -1,9 +1,10 @@
 import { importTextActionCreator as importText } from '../../actions/importText'
 import { HOME_TOKEN } from '../../constants'
 import exportContext from '../../selectors/exportContext'
+import { addMulticursorAtFirstMatchActionCreator as addMulticursor } from '../../test-helpers/addMulticursorAtFirstMatch'
 import createTestStore from '../../test-helpers/createTestStore'
 import { setCursorFirstMatchActionCreator as setCursor } from '../../test-helpers/setCursorFirstMatch'
-import executeShortcut from '../../util/executeShortcut'
+import executeShortcut, { executeShortcutWithMulticursor } from '../../util/executeShortcut'
 import pinShortcut from '../pin'
 
 it('toggle on when there is no =pin attribute', () => {
@@ -139,4 +140,188 @@ it('remove =pin/true when toggling off', () => {
     - e
       - f
       - g`)
+})
+
+describe('multicursor', () => {
+  it('pins multiple thoughts', async () => {
+    const store = createTestStore()
+
+    store.dispatch([
+      importText({
+        text: `
+            - a
+              - b
+              - c
+            - d
+              - e
+              - f
+            - g
+              - h
+              - i
+          `,
+      }),
+      setCursor(['a', 'b']),
+      addMulticursor(['d', 'e']),
+      addMulticursor(['g']),
+    ])
+
+    await executeShortcutWithMulticursor(pinShortcut, { store })
+
+    const exported = exportContext(store.getState(), [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- __ROOT__
+  - a
+    - b
+      - =pin
+    - c
+  - d
+    - e
+      - =pin
+    - f
+  - g
+    - =pin
+    - h
+    - i`)
+  })
+
+  it('toggles pin state for multiple thoughts', async () => {
+    const store = createTestStore()
+
+    store.dispatch([
+      importText({
+        text: `
+            - a
+              - =pin
+              - b
+              - c
+            - d
+              - e
+              - f
+            - g
+              - =pin
+              - h
+              - i
+          `,
+      }),
+      setCursor(['a']),
+      addMulticursor(['d', 'e']),
+      addMulticursor(['g']),
+    ])
+
+    await executeShortcutWithMulticursor(pinShortcut, { store })
+
+    const exported = exportContext(store.getState(), [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- __ROOT__
+  - a
+    - b
+    - c
+  - d
+    - e
+      - =pin
+    - f
+  - g
+    - h
+    - i`)
+  })
+
+  it('pins thoughts at different levels', async () => {
+    const store = createTestStore()
+
+    store.dispatch([
+      importText({
+        text: `
+            - a
+              - b
+                - c
+            - d
+              - e
+            - f
+          `,
+      }),
+      setCursor(['a']),
+      addMulticursor(['a', 'b', 'c']),
+      addMulticursor(['d', 'e']),
+    ])
+
+    await executeShortcutWithMulticursor(pinShortcut, { store })
+
+    const exported = exportContext(store.getState(), [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- __ROOT__
+  - a
+    - =pin
+    - b
+      - c
+        - =pin
+  - d
+    - e
+      - =pin
+  - f`)
+  })
+
+  it('toggles pin state for thoughts with mixed initial states', async () => {
+    const store = createTestStore()
+
+    store.dispatch([
+      importText({
+        text: `
+            - a
+              - =pin
+              - b
+            - c
+              - =pin
+            - d
+              - e
+          `,
+      }),
+      setCursor(['a']),
+      addMulticursor(['c']),
+      addMulticursor(['d', 'e']),
+    ])
+
+    await executeShortcutWithMulticursor(pinShortcut, { store })
+
+    const exported = exportContext(store.getState(), [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- __ROOT__
+  - a
+    - b
+  - c
+  - d
+    - e
+      - =pin`)
+  })
+
+  it('handles nested thoughts correctly', async () => {
+    const store = createTestStore()
+
+    store.dispatch([
+      importText({
+        text: `
+            - a
+              - b
+                - c
+              - d
+            - e
+              - f
+                - g
+          `,
+      }),
+      setCursor(['a', 'b']),
+      addMulticursor(['a', 'd']),
+      addMulticursor(['e', 'f', 'g']),
+    ])
+
+    await executeShortcutWithMulticursor(pinShortcut, { store })
+
+    const exported = exportContext(store.getState(), [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- __ROOT__
+  - a
+    - b
+      - =pin
+      - c
+    - d
+      - =pin
+  - e
+    - f
+      - g
+        - =pin`)
+  })
 })
