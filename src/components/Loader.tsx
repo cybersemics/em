a44d54a744bdce014649985cfb27f76ac3ff3109
Christina Yang <ychristina011@gmail.com>
2024-09-14 11:49:33 -0700

    convert ripple_loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,19 @@
 import { CSSProperties } from 'react'
+import { css } from '../../styled-system/css'
 
 interface LoaderProps {
   size?: number
   style?: CSSProperties
+}
+
+const rippleLoaderChild = {
+  position: 'absolute',
+  borderWidth: '19.04%',
+  borderStyle: 'solid',
+  opacity: 1,
+  borderRadius: '50%',
+  animation: 'ripple_loader 1s cubic-bezier(0, 0.2, 0.8, 1) infinite',
+  boxSizing: 'content-box',
 }
 
 /**
@@ -10,10 +21,22 @@ interface LoaderProps {
  */
 const Loader = ({ size = 32, style }: LoaderProps) => {
   return (
-    <div className='ripple_loader_container' style={{ ...style, height: size, width: size }}>
-      <div className='ripple_loader'>
-        <div></div>
-        <div></div>
+    <div
+      className={css({ display: 'inline-block', overflow: 'hidden', background: 'rgba(255, 255, 255, 0)' })}
+      style={{ ...style, height: size, width: size }}
+    >
+      <div
+        className={css({
+          width: '100%',
+          height: '100%',
+          position: 'relative',
+          transform: 'translateZ(0) scale(0.49)',
+          backfaceVisibility: 'hidden',
+          transformOrigin: '0 0',
+        })}
+      >
+        <div className={css(rippleLoaderChild, { borderColor: '#e90c59', animationDelay: '0s' })}></div>
+        <div className={css(rippleLoaderChild, { borderColor: '#46dff0', animationDelay: '-0.5s' })}></div>
       </div>
     </div>
   )
