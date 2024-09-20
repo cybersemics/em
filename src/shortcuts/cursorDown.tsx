c56cc0160e3df833ebdb0352d29228bb2c4a8501
Fabian Finke <finkef@icloud.com>
2024-09-20 18:26:19 +0200

    Fix cursorUp, cursorDown multicursor shortcuts

--- src/shortcuts/cursorDown.tsx
+++ src/shortcuts/cursorDown.tsx
@@ -3,14 +3,23 @@ import Dispatch from '../@types/Dispatch'
 import IconType from '../@types/Icon'
 import Shortcut from '../@types/Shortcut'
 import State from '../@types/State'
-import { addMulticursorActionCreator } from '../actions/addMulticursor'
+import { addMulticursorActionCreator as addMulticursor } from '../actions/addMulticursor'
 import { cursorDownActionCreator as cursorDown } from '../actions/cursorDown'
+import { removeMulticursorActionCreator as removeMulticursor } from '../actions/removeMulticursor'
+import { setCursorActionCreator as setCursor } from '../actions/setCursor'
+import { HOME_PATH, HOME_TOKEN } from '../constants'
 import * as selection from '../device/selection'
 import attributeEquals from '../selectors/attributeEquals'
+import { getChildrenSorted } from '../selectors/getChildren'
+import hasMulticursor from '../selectors/hasMulticursor'
+import isMulticursorPath from '../selectors/isMulticursorPath'
+import nextSibling from '../selectors/nextSibling'
 import nextThought from '../selectors/nextThought'
 import rootedParentOf from '../selectors/rootedParentOf'
+import appendToPath from '../util/appendToPath'
 import head from '../util/head'
 import headValue from '../util/headValue'
+import parentOf from '../util/parentOf'
 // import directly since util/index is not loaded yet when shortcut is initialized
 import throttleByAnimationFrame from '../util/throttleByAnimationFrame'
 
@@ -57,11 +66,56 @@ const cursorDownShortcut: Shortcut = {
   },
   exec: throttleByAnimationFrame((dispatch: Dispatch, getState: () => State, e: KeyboardEvent) => {
     if (e.shiftKey) {
-      // Add the next thought to the multicursor
-      const next = nextThought(getState())
-      if (next) dispatch(addMulticursorActionCreator({ path: next }))
+      const state = getState()
+      const { cursor } = state
+      const path = cursor || HOME_PATH
+      const isMulticursorEmpty = !hasMulticursor(state)
+      const isCurrentCursorMulticursor = cursor && isMulticursorPath(state, cursor)
 
-      dispatch(cursorDown({ preserveMulticursor: true }))
+      const nextSiblingThought = cursor
+        ? // if cursor exists, get the next sibling
+          nextSibling(state, cursor)
+        : // otherwise, get the first thought in the home context
+          getChildrenSorted(state, HOME_TOKEN)[0]
+
+      const nextPath = nextSiblingThought
+        ? // non-first child path
+          appendToPath(parentOf(path), nextSiblingThought.id)
+        : nextThought(state)
+
+      const isNextPathMulticursor = nextPath && isMulticursorPath(state, nextPath)
+
+      dispatch([
+        setCursor({ path: nextPath, preserveMulticursor: true }),
+        dispatch => {
+          // New multicursor set
+          if (isMulticursorEmpty) {
+            // Add the current cursor to the multicursor, if it exists
+            if (cursor) {
+              dispatch(addMulticursor({ path: cursor }))
+            }
+
+            // Add the next cursor to the multicursor, if it exists
+            if (nextPath) {
+              dispatch(addMulticursor({ path: nextPath }))
+            }
+
+            return
+          }
+
+          // Extend the multicursor set to the next cursor
+          if (isCurrentCursorMulticursor && !isNextPathMulticursor && nextPath) {
+            dispatch(addMulticursor({ path: nextPath }))
+            return
+          }
+
+          // Remove the next cursor from the multicursor set
+          if (isCurrentCursorMulticursor && isNextPathMulticursor && cursor) {
+            dispatch(removeMulticursor({ path: cursor }))
+            return
+          }
+        },
+      ])
     } else dispatch(cursorDown())
   }),
 }
--- src/shortcuts/cursorUp.tsx
+++ src/shortcuts/cursorUp.tsx
@@ -3,12 +3,22 @@ import Dispatch from '../@types/Dispatch'
 import IconType from '../@types/Icon'
 import Shortcut from '../@types/Shortcut'
 import State from '../@types/State'
+import { addMulticursorActionCreator as addMulticursor } from '../actions/addMulticursor'
 import { cursorUpActionCreator as cursorUp } from '../actions/cursorUp'
 import { removeMulticursorActionCreator as removeMulticursor } from '../actions/removeMulticursor'
+import { setCursorActionCreator as setCursor } from '../actions/setCursor'
+import { HOME_PATH, HOME_TOKEN } from '../constants'
 import * as selection from '../device/selection'
 import attributeEquals from '../selectors/attributeEquals'
+import { getChildrenSorted } from '../selectors/getChildren'
+import hasMulticursor from '../selectors/hasMulticursor'
+import isMulticursorPath from '../selectors/isMulticursorPath'
+import prevSibling from '../selectors/prevSibling'
 import rootedParentOf from '../selectors/rootedParentOf'
+import appendToPath from '../util/appendToPath'
 import head from '../util/head'
+import isRoot from '../util/isRoot'
+import parentOf from '../util/parentOf'
 // import directly since util/index is not loaded yet when shortcut is initialized
 import throttleByAnimationFrame from '../util/throttleByAnimationFrame'
 
@@ -55,10 +65,61 @@ const cursorUpShortcut: Shortcut = {
   },
   exec: throttleByAnimationFrame((dispatch: Dispatch, getState: () => State, e: KeyboardEvent) => {
     if (e.shiftKey) {
-      const cursor = getState().cursor
-      if (cursor) dispatch(removeMulticursor({ path: cursor }))
+      const state = getState()
+      const isMulticursorEmpty = !hasMulticursor(state)
+      const isCurrentCursorMulticursor = state.cursor && isMulticursorPath(state, state.cursor)
 
-      dispatch(cursorUp({ preserveMulticursor: true }))
+      const { cursor } = state
+      const path = cursor || HOME_PATH
+      const pathParent = rootedParentOf(state, path)
+
+      const prevThought = cursor
+        ? // if cursor exists, get the previous sibling
+          prevSibling(state, cursor)
+        : // otherwise, get the last thought in the home context
+          getChildrenSorted(state, HOME_TOKEN).slice(-1)[0]
+
+      const prevPath = prevThought
+        ? // non-first child path
+          appendToPath(parentOf(path), prevThought.id)
+        : // when the cursor is on the first child in a context, move up a level
+          !isRoot(pathParent)
+          ? pathParent
+          : null
+
+      const isPrevPathMulticursor = prevPath && isMulticursorPath(state, prevPath)
+
+      dispatch([
+        setCursor({ path: prevPath, preserveMulticursor: true }),
+        dispatch => {
+          // New multicursor set
+          if (isMulticursorEmpty) {
+            // Add the current cursor to the multicursor, if it exists
+            if (state.cursor) {
+              dispatch(addMulticursor({ path: state.cursor }))
+            }
+
+            // Add the previous cursor to the multicursor, if it exists
+            if (prevPath) {
+              dispatch(addMulticursor({ path: prevPath }))
+            }
+
+            return
+          }
+
+          // Extend the multicursor set to the previous cursor
+          if (isCurrentCursorMulticursor && !isPrevPathMulticursor && prevPath) {
+            dispatch(addMulticursor({ path: prevPath }))
+            return
+          }
+
+          // Remove the previous cursor from the multicursor set
+          if (isCurrentCursorMulticursor && isPrevPathMulticursor && state.cursor) {
+            dispatch(removeMulticursor({ path: state.cursor }))
+            return
+          }
+        },
+      ])
     } else dispatch(cursorUp())
   }),
 }
