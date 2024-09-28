2c5bf521ef935d18576ba09b36ec11f6f5824ef4
Blake Stovall <blake@disposis.com>
2024-09-27 21:20:52 -0500

    preloadSources default export conversion

--- src/actions/preloadSources.ts
+++ src/actions/preloadSources.ts
@@ -7,7 +7,7 @@ import thoughtToContext from '../selectors/thoughtToContext'
 import unroot from '../util/unroot'
 
 /** Fetch and import all =src attributes with =preload. */
-export const preloadSourcesActionCreator = (): Thunk => (dispatch, getState) => {
+const preloadSourcesActionCreator = (): Thunk => (dispatch, getState) => {
   const state = getState()
 
   // get a list of all =src contexts with =preload converted to paths
@@ -29,3 +29,5 @@ export const preloadSourcesActionCreator = (): Thunk => (dispatch, getState) => 
     path && dispatch(loadResource(path))
   })
 }
+
+export default preloadSourcesActionCreator
