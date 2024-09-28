cybersemics/em

commit 23fe15a494144048108ba7c14ff5c1be37641981

make Superscript use StaticSuperscript, convert to PandaCSS

src/components/ContextBreadcrumbs.tsx
@@ -122,7 +122,9 @@ const BreadCrumb = React.memo(
           ) : (
             <Link className='extend-tap-small' simplePath={simplePath} label={label} />
           ))}
-        {!isDeleting && <Superscript simplePath={simplePath} />}
+        {!isDeleting && (
+          <Superscript simplePath={simplePath} css={{ position: 'relative', left: '-2px', top: '-3px' }} />
+        )}
       </span>
     ) : (
       <span ref={ref}>

src/components/StaticSuperscript.tsx
@@ -1,12 +1,56 @@
 import React from 'react'
+import { css } from '../../styled-system/css'
+import { SystemStyleObject } from '../../styled-system/types'
 
 /** Renders a given number as a superscript. */
-const StaticSuperscript = ({ n, style }: { n: number; style?: React.CSSProperties }) => (
-  <span className='superscript-container' style={style}>
-    <span className='num-contexts'>
-      <sup role='superscript'>{n}</sup>
-    </span>
+const StaticSuperscript = React.forwardRef<
+  HTMLSpanElement,
+  {
+    n: number
+    css?: SystemStyleObject
+    style?: React.CSSProperties
+    show?: boolean
+    hideZero?: boolean
+    absolute?: boolean
+  }
+>(({ n, style, show = true, hideZero, absolute }, forwardRef) => (
+  <span
+    ref={forwardRef}
+    className={css({
+      /* prevent expanded click area from wrapping */
+      whiteSpace: 'nowrap',
+      zIndex: 'stack',
+      pointerEvents: 'none',
+      userSelect: 'none',
+      ...css,
+    })}
+    style={style}
+  >
+    {show && (
+      <span
+        className={css({
+          fontSize: '60%',
+          whiteSpace: 'nowrap',
+          position: absolute ? 'absolute' : undefined,
+        })}
+      >
+        {(n || !hideZero) && (
+          <sup
+            role='superscript'
+            className={css({
+              position: 'relative',
+              zIndex: 'stack',
+              top: '-1px',
+              left: '1px',
+            })}
+          >
+            {n}
+          </sup>
+        )}
+      </span>
+    )}
   </span>
-)
+))
 
+StaticSuperscript.displayName = 'StaticSuperscript'
 export default StaticSuperscript

src/components/Superscript.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useRef, useState } from 'react'
 import { shallowEqual, useSelector } from 'react-redux'
+import { SystemStyleObject } from '../../styled-system/types'
 import Index from '../@types/IndexType'
 import SimplePath from '../@types/SimplePath'
 import ThoughtId from '../@types/ThoughtId'
@@ -8,18 +9,20 @@ import getThoughtById from '../selectors/getThoughtById'
 import store from '../stores/app'
 import head from '../util/head'
 import isVisibleContext from '../util/isVisibleContext'
+import StaticSuperscript from './StaticSuperscript'
 
 interface SuperscriptProps {
   contextViews?: Index<boolean>
   showSingle?: boolean
   superscript?: boolean
   simplePath: SimplePath
+  css?: SystemStyleObject
 }
 
 const NO_CONTEXTS: ThoughtId[] = []
 
 /** Renders superscript if there are other contexts. Optionally pass thoughts (used by ContextBreadcrumbs) or simplePath (used by Subthought). */
-const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath }) => {
+const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, css }) => {
   const [numContexts, setNumContexts] = useState(0)
   const ref = useRef<HTMLElement>(null)
 
@@ -43,11 +46,7 @@ const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath }) => {
     })
   }, [contexts, showHiddenThoughts])
 
-  return (
-    <span ref={ref} className='superscript-container'>
-      {show && numContexts && <span className='num-contexts'> {numContexts && <sup>{numContexts}</sup>}</span>}
-    </span>
-  )
+  return <StaticSuperscript ref={ref} show={!!(show && numContexts)} n={numContexts} css={css} hideZero />
 }
 
 const SuperscriptMemo = React.memo(Superscript)

src/App.css
@@ -207,12 +207,6 @@ a.text-link {
   padding-top: 0.5em;
 }
 
-.context-breadcrumbs .superscript-container {
-  position: relative;
-  left: -2px;
-  top: -3px;
-}
-
 .thought-annotation .context-breadcrumbs {
   margin-right: 2px;
 }
@@ -403,10 +397,6 @@ This prevents table cols being rendered as multiline content, which causes inaac
 
 .subthought-highlight {
   border-bottom: solid 1px;
-}
-
-.thought-annotation .num-contexts {
-  position: absolute;
 }
 
 /* Use line-height to vertically center the text and bullet. We cannot use padding since it messes up the selection. This needs to be overwritten on multiline elements. See ".child .editable" below. */
@@ -903,41 +893,6 @@ This prevents table cols being rendered as multiline content, which causes inaac
 
 .link:active {
   -webkit-text-stroke-width: 0.05em;
-}
-
-.superscript-container {
-  white-space: nowrap; /* prevent expanded click area from wrapping */
-  z-index: var(--z-index-stack);
-  pointer-events: none;
-  user-select: none;
-}
-
-.superscript-container sup {
-  position: relative;
-  z-index: var(--z-index-stack);
-}
-
-.num-contexts {
-  font-size: 60%;
-  white-space: nowrap;
-}
-
-.num-contexts > sup {
-  top: -1px;
-  left: 1px;
-}
-
-.num-contexts > sup > a {
-  padding-right: 15px; /* additional click area */
-  margin-right: -15px;
-  padding-bottom: 8px;
-  margin-bottom: -8px;
-}
-
-h1 .num-contexts {
-  font-size: 40%;
-  top: -4px;
-  left: 1px;
 }
 
 .add-new-thought-link {

src/components/ThoughtAnnotation.tsx
@@ -277,7 +277,7 @@ const ThoughtAnnotation = React.memo(
           {email && <EmailIconLink email={email} />}
           {
             // with real time context update we increase context length by 1 // with the default minContexts of 2, do not count the whole thought
-            showSuperscript ? <StaticSuperscript n={numContexts} style={style} /> : null
+            showSuperscript ? <StaticSuperscript absolute n={numContexts} style={style} /> : null
           }
         </div>
       </div>

commit d52bc4c2bd051b65d891ce388f9931fe848b4a29

fix: small visual regression from 23fe15a

src/components/ContextBreadcrumbs.tsx
@@ -32,8 +32,6 @@ type OverflowChild = {
 
 type OverflowPath = OverflowChild[]
 
-const superscriptCss = { position: 'relative', left: '-2px', top: '-3px' }
-
 /** Ellipsizes thoughts in a path by thoughtsLimit and charLimit. Complexity: O(n), but does not work if thoughtsLimit or charLimit are undefined. */
 const useEllipsizedThoughts = (
   path: Path,
@@ -131,7 +129,9 @@ const BreadCrumb = React.memo(
               label={label}
             />
           ))}
-        {!isDeleting && <Superscript simplePath={simplePath} css={superscriptCss} />}
+        {!isDeleting && (
+          <Superscript simplePath={simplePath} cssRaw={css.raw({ position: 'relative', left: '-2px', top: '-3px' })} />
+        )}
       </span>
     ) : (
       <span ref={ref}>

src/components/StaticSuperscript.tsx
@@ -7,23 +7,25 @@ const StaticSuperscript = React.forwardRef<
   HTMLSpanElement,
   {
     n: number
-    css?: SystemStyleObject
+    cssRaw?: SystemStyleObject
     style?: React.CSSProperties
     show?: boolean
     hideZero?: boolean
     absolute?: boolean
   }
->(({ n, style, show = true, hideZero, absolute }, forwardRef) => (
+>(({ n, style, show = true, hideZero, absolute, cssRaw }, forwardRef) => (
   <span
     ref={forwardRef}
-    className={css({
-      /* prevent expanded click area from wrapping */
-      whiteSpace: 'nowrap',
-      zIndex: 'stack',
-      pointerEvents: 'none',
-      userSelect: 'none',
-      ...css,
-    })}
+    className={css(
+      {
+        /* prevent expanded click area from wrapping */
+        whiteSpace: 'nowrap',
+        zIndex: 'stack',
+        pointerEvents: 'none',
+        userSelect: 'none',
+      },
+      cssRaw,
+    )}
     style={style}
   >
     {show && (

src/components/Superscript.tsx
@@ -16,13 +16,13 @@ interface SuperscriptProps {
   showSingle?: boolean
   superscript?: boolean
   simplePath: SimplePath
-  css?: SystemStyleObject
+  cssRaw?: SystemStyleObject
 }
 
 const NO_CONTEXTS: ThoughtId[] = []
 
 /** Renders superscript if there are other contexts. Optionally pass thoughts (used by ContextBreadcrumbs) or simplePath (used by Subthought). */
-const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, css }) => {
+const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, cssRaw }) => {
   const [numContexts, setNumContexts] = useState(0)
   const ref = useRef<HTMLElement>(null)
 
@@ -46,7 +46,7 @@ const Superscript: FC<SuperscriptProps> = ({ showSingle, simplePath, css }) => {
     })
   }, [contexts, showHiddenThoughts])
 
-  return <StaticSuperscript ref={ref} show={!!(show && numContexts)} n={numContexts} css={css} hideZero />
+  return <StaticSuperscript ref={ref} show={!!(show && numContexts)} n={numContexts} cssRaw={cssRaw} hideZero />
 }
 
 const SuperscriptMemo = React.memo(Superscript)