3784b6f439fee42eade1e8569004d09a31e7cb60
Raine Revere <raine@cybersemics.org>
2024-10-08 09:55:45 +0000

    ColorPicker: Move shouldRemoveStyle to formatSelectionActionCreator.

--- src/actions/formatSelection.ts
+++ src/actions/formatSelection.ts
@@ -1,15 +1,31 @@
 /* eslint-disable import/prefer-default-export */
+import { rgbToHex } from '@mui/material'
 import Thunk from '../@types/Thunk'
 import * as selection from '../device/selection'
+import getThoughtById from '../selectors/getThoughtById'
 import pathToThought from '../selectors/pathToThought'
+import simplifyPath from '../selectors/simplifyPath'
+import themeColors from '../selectors/themeColors'
 import { updateCommandState } from '../stores/commandStateStore'
 import suppressFocusStore from '../stores/suppressFocus'
+import head from '../util/head'
+import { editThoughtActionCreator as editThought } from './editThought'
 
 /** Format the browser selection or cursor thought as bold, italic, strikethrough, underline. */
 export const formatSelectionActionCreator =
-  (command: 'bold' | 'italic' | 'strikethrough' | 'underline' | 'foreColor' | 'backColor', color: string = ''): Thunk =>
+  (
+    command: 'bold' | 'italic' | 'strikethrough' | 'underline' | 'foreColor' | 'backColor',
+    color: string = '',
+    {
+      isDefault,
+    }: {
+      /** True if the clicked swatch has the default label. */
+      isDefault?: boolean
+    } = {},
+  ): Thunk =>
   (dispatch, getState) => {
     const state = getState()
+    const colors = themeColors(state)
     if (!state.cursor) return
     const thought = pathToThought(state, state.cursor)
     const sel = window.getSelection()
@@ -27,5 +43,39 @@ export const formatSelectionActionCreator =
       document.execCommand(command, false, color)
       updateCommandState()
     }
+
+    if (command === 'foreColor' || command === 'backColor') {
+      /** Function to check if a style(background) should be removed based on the color and background-color. */
+      const shouldRemoveStyle = (styleString: string) => {
+        const styleLower = styleString.toLowerCase()
+        const hasBackgroundColor = styleLower.includes('background-color')
+        const colorMatch = styleLower.match(/color\s*:\s*([^;]+);?/)
+        const elementColor = colorMatch ? colorMatch[1].trim() : null
+        const isDifferentColor = elementColor && elementColor !== rgbToHex(colors.bg)
+        if ((elementColor && isDifferentColor) || (isDefault && hasBackgroundColor)) return true
+        return false
+      }
+
+      dispatch((dispatch, getState) => {
+        const state = getState()
+        if (!state.cursor) return
+        const thought = getThoughtById(state, head(state.cursor))
+        const simplePath = simplifyPath(state, state.cursor)
+        const styleAttrPattern = /style\s*=\s*["'][^"']*["']/gi
+        //Replace style attributes based on the conditions
+        const newThoughtValue = thought.value.replace(styleAttrPattern, match => {
+          if (shouldRemoveStyle(match)) return ''
+          return match
+        })
+        dispatch(
+          editThought({
+            oldValue: thought.value,
+            newValue: newThoughtValue,
+            path: simplePath,
+          }),
+        )
+      })
+    }
+
     suppressFocusStore.update(false)
   }
