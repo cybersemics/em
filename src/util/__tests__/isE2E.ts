aa4052b970b7f23798e488f63ecbfed5ee7e4aa3
Raine Revere <raine@cybersemics.org>
2024-10-16 21:44:46 +0000

    Use navigator.webdriver instead of isE2E.

--- src/util/__tests__/isE2E.ts
+++
@@ -1,8 +0,0 @@
-import isE2E from '../isE2E'
-
-// Navigator.webdriver is readonly so we probably cannot test the true path.
-describe('isE2e', () => {
-  it('should return false if webdriver is falsy', () => {
-    expect(isE2E()).toBe(false)
-  })
-})
