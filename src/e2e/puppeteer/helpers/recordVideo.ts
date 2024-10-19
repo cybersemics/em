60293d726c985c3122d205802db354e0797e17dd
Blake Stovall <blake@disposis.com>
2024-10-18 23:41:17 -0500

    move recordVideo helper filename to start function call

--- src/e2e/puppeteer/helpers/recordVideo.ts
+++ src/e2e/puppeteer/helpers/recordVideo.ts
@@ -2,11 +2,11 @@ import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder'
 import { page } from '../setup'
 
 /** Initialize recorder. */
-async function init(filename: string) {
+async function init() {
   const recorder = new PuppeteerScreenRecorder(page)
 
   /** Start recording. */
-  const start = async () => recorder.start(filename)
+  const start = async (filename: string) => recorder.start(filename)
 
   /** Stop recording. */
   const stop = async () => recorder.stop()
