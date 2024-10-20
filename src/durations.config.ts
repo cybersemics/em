3f89671b35cd01ccb9cb7a5d015b0bc7936701f6
Blake Stovall <blake@disposis.com>
2024-10-20 15:50:33 -0500

    add top-level duration config

---
+++ src/durations.config.ts
@@ -0,0 +1,10 @@
+export type DurationConfig = Record<string, number>
+
+const durationsMillis: DurationConfig = {
+  highlightPulseDuration: 500,
+  hoverPulseDuration: 300,
+  layoutSlowShiftDuration: 750,
+  layoutNodeAnimationDuration: 150,
+}
+
+export default durationsMillis
