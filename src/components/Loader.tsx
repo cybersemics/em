df5639c18fa1b75cdb330e92d0dff007d4d48087
Christina Yang <ychristina011@gmail.com>
2024-10-23 12:17:40 -0700

    fix: loader margin

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -24,21 +24,18 @@ const rippleLoaderChild = {
 const Loader = ({ size = 32, style, cssRaw }: LoaderProps) => {
   return (
     <div
-      className={css({ display: 'inline-block', overflow: 'hidden', background: 'rgba(255, 255, 255, 0)' })}
+      className={css({ display: 'inline-block', overflow: 'hidden', background: 'rgba(255, 255, 255, 0)' }, cssRaw)}
       style={{ ...style, height: size, width: size }}
     >
       <div
-        className={css(
-          {
-            width: '100%',
-            height: '100%',
-            position: 'relative',
-            transform: 'translateZ(0) scale(0.49)',
-            backfaceVisibility: 'hidden',
-            transformOrigin: '0 0',
-          },
-          cssRaw,
-        )}
+        className={css({
+          width: '100%',
+          height: '100%',
+          position: 'relative',
+          transform: 'translateZ(0) scale(0.49)',
+          backfaceVisibility: 'hidden',
+          transformOrigin: '0 0',
+        })}
       >
         <div className={css(rippleLoaderChild, { borderColor: '#e90c59', animationDelay: '0s' })}></div>
         <div className={css(rippleLoaderChild, { borderColor: '#46dff0', animationDelay: '-0.5s' })}></div>
