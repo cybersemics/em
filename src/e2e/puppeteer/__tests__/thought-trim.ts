df54675a2b13f8f2c6ea1eb2bce25e959769e92d
Clever-Wolf <146128882+RED-ROSE515@users.noreply.github.com>
2024-10-22 21:39:47 -0500

    fix puppeteer test cases and paste behavior issue

--- src/e2e/puppeteer/__tests__/thought-trim.ts
+++ src/e2e/puppeteer/__tests__/thought-trim.ts
@@ -1,27 +1,42 @@
-import click from '../helpers/click'
 import getEditingText from '../helpers/getEditingText'
+import newThought from '../helpers/newThought'
 import paste from '../helpers/paste'
 import press from '../helpers/press'
 
 vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
 
-it('when cursor is null, thought will be trimmed.', async () => {
+it('thought will be trimmed when paste the text', async () => {
   const importText = `
   -    Hello World.      `
 
   await paste(importText)
-  await click('#content')
   const editingText = await getEditingText()
   expect(editingText).toBe(`Hello World.`)
 })
 
 it('when change the row, thought will be trimmed.', async () => {
-  const importText = `
-  -       Hello World.      `
+  await newThought('')
+  press('Space')
+  press('Space')
+  press('Space')
+  press('H')
+  press('e')
+  press('l')
+  press('l')
+  press('o')
+  press('Space')
+  press('W')
+  press('o')
+  press('r')
+  press('l')
+  press('d')
+  press('.')
+  press('Space')
+  press('Space')
+  press('Space')
+  press('Enter')
+  press('ArrowUp')
 
-  await paste(importText)
-  await press('Enter')
-  await press('ArrowUp')
   const editingText = await getEditingText()
   expect(editingText).toBe(`Hello World.`)
 })
