40f1a7cda7edddc44f44899e44922e87f1913d44
Raine Revere <raine@cybersemics.org>
2024-10-25 10:00:41 +0000

    Disable undo/redo until persistence is fixed.
    https://github.com/cybersemics/em/issues/1631

--- src/shortcuts/undo.ts
+++ src/shortcuts/undo.ts
@@ -1,6 +1,6 @@
 import { startCase } from 'lodash'
 import Shortcut from '../@types/Shortcut'
-import { undoActionCreator as undo } from '../actions/undo'
+import { alertActionCreator as alert } from '../actions/alert'
 import UndoIcon from '../components/UndoIcon'
 import isUndoEnabled from '../selectors/isUndoEnabled'
 import getLatestActionType from '../util/getLastActionType'
@@ -18,11 +18,14 @@ const undoShortcut: Shortcut = {
 
     return 'Undo.'
   },
-  keyboard: { key: 'z', meta: true },
+  // keyboard: { key: 'z', meta: true },
   svg: UndoIcon,
   exec: (dispatch, getState) => {
     if (!isUndoEnabled(getState())) return
-    dispatch(undo())
+
+    // TODO: https://github.com/cybersemics/em/issues/1631
+    // dispatch(undo())
+    dispatch(alert('Undo/Redo is currently under develeopment.'))
   },
   canExecute: state => isUndoEnabled(state),
 }
