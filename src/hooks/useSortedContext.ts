853a23d229963df8d04340f1b3308efdc167caa0
Raine Revere <raine@cybersemics.org>
2024-10-26 18:01:36 +0000

    useSortedContext: Combine into single useSelector.

--- src/hooks/useSortedContext.ts
+++ src/hooks/useSortedContext.ts
@@ -1,8 +1,7 @@
 import { useDragDropManager } from 'react-dnd'
-import { useSelector } from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 import DragThoughtItem from '../@types/DragThoughtItem'
 import DropThoughtZone from '../@types/DropThoughtZone'
-import Path from '../@types/Path'
 import attributeEquals from '../selectors/attributeEquals'
 import getSortedRank from '../selectors/getSortedRank'
 import getThoughtById from '../selectors/getThoughtById'
@@ -11,30 +10,25 @@ import parentOf from '../util/parentOf'
 
 /** A hook that checks if a dragging thought is hovering over a sorted context, and returns new rank where that thought will be dropped. */
 const useSortedContext = () => {
-  const hoveringPath = useSelector(state => state.hoveringPath) as Path
-  const contextParentPath = parentOf(hoveringPath || [])
+  const dragDropManager = useDragDropManager()
 
-  // check if the hovering path is on a sorted context
-  const isSortedContext = useSelector(state => {
+  return useSelector(state => {
+    if (!state.hoveringPath) {
+      return { isSortedContext: false, hoveringOnDropEnd: false, newRank: -1 }
+    }
+
+    const contextParentPath = parentOf(state.hoveringPath)
+
     // Check if the drop target is on sorted context children or on its parent.
     const isContextChildren = attributeEquals(state, head(contextParentPath), '=sort', 'Alphabetical')
     const thoughtDrop = state.hoverZone === DropThoughtZone.ThoughtDrop
+    const isSortedContext = isContextChildren && thoughtDrop
 
-    return isContextChildren && thoughtDrop
-  })
-
-  // check if the hovering path is on a drop end of parent sorted context
-  const hoveringOnDropEnd = useSelector(state => {
+    // check if the hovering path is on a drop end of parent sorted context
     const dropTargetId = head(state.hoveringPath || [])
     const isContextParent = attributeEquals(state, dropTargetId, '=sort', 'Alphabetical')
+    const hoveringOnDropEnd = state.hoverZone === 'SubthoughtsDrop' && isContextParent
 
-    return state.hoverZone === 'SubthoughtsDrop' && isContextParent
-  })
-
-  const dragDropManager = useDragDropManager()
-
-  // get the source thought and its new rank
-  const sourceThought = useSelector(state => {
     const monitor = dragDropManager.getMonitor()
     const item = monitor.getItem() as DragThoughtItem
 
@@ -42,18 +36,17 @@ const useSortedContext = () => {
     const isThought = item?.zone === 'Thoughts'
     const sourceThoughtId = head(item?.path || [])
 
+    // get the source thought and its new rank
     const sourceThought = isThought ? getThoughtById(state, sourceThoughtId) : null
-    return sourceThought
-  })
+    const contextpath = hoveringOnDropEnd ? state.hoveringPath : contextParentPath
+    const newRank = getSortedRank(state, head(contextpath), sourceThought?.value || '')
 
-  const contextpath = hoveringOnDropEnd ? hoveringPath : contextParentPath
-  const newRank = useSelector(state => getSortedRank(state, head(contextpath), sourceThought?.value || ''))
-
-  return {
-    isSortedContext,
-    newRank,
-    hoveringOnDropEnd,
-  }
+    return {
+      isSortedContext,
+      hoveringOnDropEnd,
+      newRank,
+    }
+  }, shallowEqual)
 }
 
 export default useSortedContext
