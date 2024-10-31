f4459c657ddc055bd52cf5d19936c08059222f43
Blake Stovall <blake@disposis.com>
2024-10-31 14:00:05 -0500

    rollback helpers

--- src/e2e/puppeteer/__tests__/thought-trim.ts
+++ src/e2e/puppeteer/__tests__/thought-trim.ts
@@ -1,8 +1,7 @@
-import { KeyInput } from 'puppeteer'
-import executeKeyPresses from '../helpers/executeKeyPresses'
 import getEditingText from '../helpers/getEditingText'
 import newThought from '../helpers/newThought'
 import paste from '../helpers/paste'
+import press from '../helpers/press'
 
 vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
 
@@ -18,30 +17,26 @@ it('thought will be trimmed when paste the text', async () => {
 it('when change the row, thought will be trimmed.', async () => {
   await newThought('')
 
-  const keyPresses = [
-    'Space',
-    'Space',
-    'Space',
-    'H',
-    'e',
-    'l',
-    'l',
-    'o',
-    'Space',
-    'W',
-    'o',
-    'r',
-    'l',
-    'd',
-    '.',
-    'Space',
-    'Space',
-    'Space',
-    'Enter',
-    'ArrowUp',
-  ] as KeyInput[]
-
-  await executeKeyPresses(keyPresses)
+  await press('Space')
+  await press('Space')
+  await press('Space')
+  await press('H')
+  await press('e')
+  await press('l')
+  await press('l')
+  await press('o')
+  await press('Space')
+  await press('W')
+  await press('o')
+  await press('r')
+  await press('l')
+  await press('d')
+  await press('.')
+  await press('Space')
+  await press('Space')
+  await press('Space')
+  await press('Enter')
+  await press('ArrowUp')
 
   const editingText = await getEditingText()
   expect(editingText).toBe(`Hello World.`)
