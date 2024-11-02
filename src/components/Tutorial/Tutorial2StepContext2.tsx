de9afecf94f18d9d815a1df85fd71382bf9373b1
Raine Revere <raine@cybersemics.org>
2024-11-02 10:23:04 +0000

    Tutorial2StepContext2: Convert rootChildren to selector.

--- src/components/Tutorial/Tutorial2StepContext2.tsx
+++ src/components/Tutorial/Tutorial2StepContext2.tsx
@@ -1,21 +1,19 @@
 import { useSelector } from 'react-redux'
 import Path from '../../@types/Path'
-import Thought from '../../@types/Thought'
 import { isMac, isTouch } from '../../browser'
-import { TUTORIAL_CONTEXT, TUTORIAL_CONTEXT2_PARENT } from '../../constants'
+import { HOME_TOKEN, TUTORIAL_CONTEXT, TUTORIAL_CONTEXT2_PARENT } from '../../constants'
 import childIdsToThoughts from '../../selectors/childIdsToThoughts'
+import { getAllChildrenAsThoughts } from '../../selectors/getChildren'
 import headValue from '../../util/headValue'
 import TutorialHint from './TutorialHint'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 const Tutorial2StepContext2 = ({
   tutorialChoice,
-  rootChildren,
   cursor,
 }: {
   cursor: Path | null
   tutorialChoice: keyof typeof TUTORIAL_CONTEXT
-  rootChildren: Thought[]
 }) => {
   const readyToType = useSelector(state => {
     if (!state.cursor) return false
@@ -23,6 +21,12 @@ const Tutorial2StepContext2 = ({
     return cursorThought.length === 2 && cursorThought[0].value === TUTORIAL_CONTEXT2_PARENT[tutorialChoice]
   })
   const select = useSelector(state => !cursor || headValue(state, cursor) !== TUTORIAL_CONTEXT2_PARENT[tutorialChoice])
+  const context2Exists = useSelector(state => {
+    const rootChildren = getAllChildrenAsThoughts(state, HOME_TOKEN)
+    return rootChildren.find(
+      child => child.value.toLowerCase() === TUTORIAL_CONTEXT2_PARENT[tutorialChoice].toLowerCase(),
+    )
+  })
 
   return (
     <>
@@ -32,9 +36,7 @@ const Tutorial2StepContext2 = ({
       </p>
       {
         // e.g. Work
-        rootChildren.find(
-          child => child.value.toLowerCase() === TUTORIAL_CONTEXT2_PARENT[tutorialChoice].toLowerCase(),
-        ) ? (
+        context2Exists ? (
           <p>
             Do you remember how to do it?
             <TutorialHint>
