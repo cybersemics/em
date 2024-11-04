cfea627ccea936592f2e8a640db70c75b8394c8f
Ethan James <ethan.e.james@gmail.com>
2024-11-04 10:45:48 -0800

    Multiline caret tests for moving cursor up

--- src/e2e/puppeteer/__tests__/caret-multiline.ts
+++ src/e2e/puppeteer/__tests__/caret-multiline.ts
@@ -7,7 +7,7 @@ import waitForEditable from '../helpers/waitForEditable'
 vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
 
 describe('all platforms', () => {
-  it('when positioned at the beginning of a cursor, on cursorDown, the caret should move to the beginning of the new cursor.', async () => {
+  it('on cursorDown, the caret should move from the beginning of a cursor to the beginning of the new cursor.', async () => {
     const importText = `
     - a
       - b
@@ -30,7 +30,7 @@ describe('all platforms', () => {
     expect(offset).toBe(0)
   })
 
-  it('when positioned at the end of a cursor, on cursorDown, the caret should move to the end of the new cursor.', async () => {
+  it('on cursorDown, the caret should move from the end of a cursor to the beginning of the new cursor.', async () => {
     const importText = `
   - a
     - b
@@ -48,6 +48,52 @@ describe('all platforms', () => {
     // the focus must be at the beginning of 'b' after cursor down
     const textContext = await getSelection().focusNode?.textContent
     expect(textContext).toBe('b')
+
+    const offset = await getSelection().focusOffset
+    expect(offset).toBe(0)
+  })
+
+  it('on cursorUp, the caret should move from the beginning of a cursor to the beginning of the new cursor.', async () => {
+    const importText = `
+    - a
+      - b
+        - c
+        - Beautiful antique furnishings fill this quiet, comfortable flat across from the Acropolis museum. AC works great. It is in an heavily touristic area, but the convenience can't be beat. Highly recommended.
+        - d`
+
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('b')
+    await click(editableNodeHandle, { edge: 'left' })
+
+    await press('ArrowUp')
+
+    // the focus must be at the beginning of 'b' after cursor down
+    const textContext = await getSelection().focusNode?.textContent
+    expect(textContext).toBe('a')
+
+    const offset = await getSelection().focusOffset
+    expect(offset).toBe(0)
+  })
+
+  it('on cursorUp, the caret should move from the end of a cursor to the end of the new cursor.', async () => {
+    const importText = `
+  - a
+    - b
+      - c
+      - Beautiful antique furnishings fill this quiet, comfortable flat across from the Acropolis museum. AC works great. It is in an heavily touristic area, but the convenience can't be beat. Highly recommended.
+      - d`
+
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('b')
+    await click(editableNodeHandle, { edge: 'right' })
+
+    await press('ArrowUp')
+
+    // the focus must be at the beginning of 'b' after cursor down
+    const textContext = await getSelection().focusNode?.textContent
+    expect(textContext).toBe('a')
 
     const offset = await getSelection().focusOffset
     expect(offset).toBe(0)
