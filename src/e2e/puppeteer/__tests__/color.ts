27f40d09b52a5a6cff53a8647760c9b99a3349f0
Red Rose <146128882+RED-ROSE515@users.noreply.github.com>
2024-11-06 21:52:19 +0200

    ColorPicker: remove empty <font> element (#2546)

--- src/e2e/puppeteer/__tests__/color.ts
+++ src/e2e/puppeteer/__tests__/color.ts
@@ -20,8 +20,8 @@ const extractStyleProperty = (html: string) => {
 
 it('Set the text color of the text', async () => {
   const importText = `
-    - Labrador
-    - Golden Retriever`
+  - Labrador
+  - Golden Retriever`
 
   await paste(importText)
 
@@ -34,7 +34,7 @@ it('Set the text color of the text', async () => {
   const cursorText = await getEditingText()
   const result = extractStyleProperty(cursorText!)
   expect(result?.color).toBe('#00c7e6')
-  expect(result?.backgroundColor).toBe('rgb(0, 0, 0)')
+  expect(result?.backgroundColor).toBe(null)
 })
 
 it('Set the background color of the text', async () => {
@@ -54,28 +54,6 @@ it('Set the background color of the text', async () => {
   expect(result?.backgroundColor).toBe('rgb(0, 214, 136)')
 })
 
-it('Clear the text color when selecting white', async () => {
-  const importText = `
-    - Labrador
-    - Golden Retriever`
-
-  await paste(importText)
-
-  await waitForEditable('Golden Retriever')
-  await clickThought('Golden Retriever')
-  let cursorText = await getEditingText()
-  expect(extractStyleProperty(cursorText!)?.color).toBe(null)
-
-  await click('[data-testid="toolbar-icon"][aria-label="Text Color"]')
-  await click('[aria-label="text color swatches"] [aria-label="green"]')
-  cursorText = await getEditingText()
-  expect(extractStyleProperty(cursorText!)?.color).toBe('#00d688')
-
-  await click('[aria-label="text color swatches"] [aria-label="default"]')
-  cursorText = await getEditingText()
-  expect(extractStyleProperty(cursorText!)?.color).toBe(null)
-})
-
 it('Clear the background color when selecting text color', async () => {
   const importText = `
     - Labrador
@@ -92,11 +70,12 @@ it('Clear the background color when selecting text color', async () => {
   await click('[aria-label="background color swatches"] [aria-label="green"]')
   cursorText = await getEditingText()
   expect(extractStyleProperty(cursorText!)?.backgroundColor).toBe('rgb(0, 214, 136)')
+  expect(extractStyleProperty(cursorText!)?.color).toBe('#000000')
 
   await click('[aria-label="text color swatches"] [aria-label="purple"]')
   cursorText = await getEditingText()
   expect(extractStyleProperty(cursorText!)?.color).toBe('#aa80ff')
-  expect(extractStyleProperty(cursorText!)?.backgroundColor).toBe('rgb(0, 0, 0)')
+  expect(extractStyleProperty(cursorText!)?.backgroundColor).toBe(null)
 })
 
 it('Clear the text color when setting background color', async () => {
@@ -120,4 +99,41 @@ it('Clear the text color when setting background color', async () => {
   cursorText = await getEditingText()
   expect(extractStyleProperty(cursorText!)?.backgroundColor).toBe('rgb(170, 128, 255)')
   expect(extractStyleProperty(cursorText!)?.color).toBe('#000000')
+})
+
+it('Empty <font> element will be removed after setting color to default.', async () => {
+  const importText = `
+  - Labrador
+  - Golden Retriever`
+
+  await paste(importText)
+
+  await waitForEditable('Golden Retriever')
+  await clickThought('Golden Retriever')
+
+  await click('[data-testid="toolbar-icon"][aria-label="Text Color"]')
+  await click('[aria-label="background color swatches"] [aria-label="blue"]')
+
+  await click('[aria-label="text color swatches"] [aria-label="default"]')
+  const result = await getEditingText()
+  expect(result).toBe('Golden Retriever')
+})
+
+it('Empty <span> element will be removed after setting color to default.', async () => {
+  const importText = `
+  - Labrador
+  - Golden Retriever`
+
+  await paste(importText)
+
+  await waitForEditable('Golden Retriever')
+  await clickThought('Golden Retriever')
+
+  await click('[data-testid="toolbar-icon"][aria-label="Text Color"]')
+  await click('[aria-label="text color swatches"] [aria-label="blue"]')
+
+  await click('[aria-label="text color swatches"] [aria-label="default"]')
+
+  const result = await getEditingText()
+  expect(result).toBe('Golden Retriever')
 })
