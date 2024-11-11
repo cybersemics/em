a3718eb011d6de6b8ad053d91f04455434b6238c
Ethan James <ethan.e.james@gmail.com>
2024-11-10 20:53:50 -0800

    Tests for closing Sidebar w/ Escape & empty click

--- src/e2e/puppeteer/__tests__/toggleSidebar.ts
+++ src/e2e/puppeteer/__tests__/toggleSidebar.ts
@@ -1,12 +1,14 @@
 import click from '../helpers/click'
 import getSelection from '../helpers/getSelection'
 import paste from '../helpers/paste'
+import press from '../helpers/press'
 import waitForEditable from '../helpers/waitForEditable'
+import { page } from '../setup'
 
 vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
 
 describe('toggle sidebar', () => {
-  it('on toggleSidebar, the caret offset should be preserved once it closes.', async () => {
+  it('when clicking the menu button twice to open & close the sidebar, the caret offset should be preserved once it closes.', async () => {
     const cursor = 'Hello'
     const importText = `
     - ${cursor}`
@@ -25,5 +27,48 @@ describe('toggle sidebar', () => {
 
     const offset = await getSelection().focusOffset
     expect(offset).toBe(1)
+  })
+
+  it('when clicking the menu button then pressing Escape to open & close the sidebar, the caret offset should be preserved once it closes.', async () => {
+    const cursor = 'Hello'
+    const importText = `
+    - ${cursor}`
+
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('Hello')
+    await click(editableNodeHandle, { offset: 2 })
+
+    await click('[aria-label=menu]')
+    await new Promise(r => setTimeout(r, 1000))
+    await press('Escape')
+
+    // the focus must be after 'e' after cursor down
+    const textContext = await getSelection().focusNode?.textContent
+    expect(textContext).toBe(cursor)
+
+    const offset = await getSelection().focusOffset
+    expect(offset).toBe(2)
+  })
+
+  it('when clicking the menu button then clicking in empty space to open & close the sidebar, the caret offset should be preserved once it closes.', async () => {
+    const cursor = 'Hello'
+    const importText = `
+    - ${cursor}`
+
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('Hello')
+    await click(editableNodeHandle, { offset: 3 })
+
+    await click('[aria-label=menu]')
+    await click('[role=presentation] [aria-hidden=true]')
+
+    // the focus must be after 'l' after cursor down
+    const textContext = await getSelection().focusNode?.textContent
+    expect(textContext).toBe(cursor)
+
+    const offset = await getSelection().focusOffset
+    expect(offset).toBe(3)
   })
 })
