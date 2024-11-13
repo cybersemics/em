ae442ad3f90063be7cab9efd0867adb76be1d160
ohrytskov <ohrytskov20230516@gmail.com>
2024-11-13 14:08:47 +0000

    Refactor: Move color manipulation functions outside LottieAnimation component for improved performance and clarity.

--- src/components/icons/LottieAnimation.tsx
+++ src/components/icons/LottieAnimation.tsx
@@ -1,5 +1,5 @@
 import Player, { LottieRefCurrentProps } from 'lottie-react'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 interface LottieAnimationProps {
   animationData: any
@@ -7,6 +7,78 @@ interface LottieAnimationProps {
   color?: string
   onComplete?: () => void
   style?: React.CSSProperties
+}
+
+/**
+ * Converts hex color to RGBA.
+ *
+ * @param hex - The hex color code.
+ * @returns The RGBA values.
+ */
+const hexToRGBA = (hex: string): number[] => {
+  let r = 0,
+    g = 0,
+    b = 0
+  const a = 1
+  if (hex.length === 7) {
+    r = parseInt(hex.slice(1, 3), 16) / 255
+    g = parseInt(hex.slice(3, 5), 16) / 255
+    b = parseInt(hex.slice(5, 7), 16) / 255
+  }
+  return [r, g, b, a]
+}
+
+/**
+ * Updates colors in a single shape item.
+ *
+ * This function checks if the item represents a stroke or
+ * fill and updates its color using the provided RGBA array.
+ * If the item contains a nested items array, it recursively
+ * calls itself to ensure that all relevant shapes within the
+ * structure are updated with the new color.
+ *
+ * @param item - The shape item object which may contain
+ * stroke or fill color properties.
+ * @param rgbaArray - The RGBA values to set as new color.
+ */
+const updateColorsInItem = (item: any, rgbaArray: number[]) => {
+  if ((item.ty === 'st' || item.ty === 'fl') && item.c) {
+    // Check for animated color property
+    if (item.c.a === 1) {
+      item.c.k.forEach((keyframe: any) => {
+        keyframe.s = rgbaArray
+      })
+    } else {
+      item.c.k = rgbaArray
+    }
+  }
+  if (item.it) {
+    item.it.forEach((subItem: any) => updateColorsInItem(subItem, rgbaArray))
+  }
+}
+
+/**
+ * Changes the color of stroke and fill attributes in the Lottie animation data.
+ *
+ * This function traverses the Lottie animation data structure to locate all items that
+ * represent shape colors (strokes and fills) and applies a new color based on the provided
+ * RGBA values derived from the 'newColor' hexadecimal string.
+ *
+ * @param data - The Lottie animation data object containing multiple layers and shapes.
+ * Each layer is iterated through to apply the color change to eligible elements.
+ * @param newColor - A string representing the new color to be applied, in hexadecimal format.
+ * This string is converted into an RGBA array to update the animation colors.
+ */
+const changeLineColor = (data: any, newColor: string): void => {
+  const rgbaArray = hexToRGBA(newColor)
+
+  data.layers.forEach((layer: any) => {
+    if (layer.shapes) {
+      layer.shapes.forEach((shape: any) => {
+        shape.it.forEach((item: any) => updateColorsInItem(item, rgbaArray))
+      })
+    }
+  })
 }
 
 /**
@@ -35,81 +107,6 @@ const LottieAnimation: React.FC<LottieAnimationProps> = ({
   const [key, setKey] = useState(0)
   const lottieRef = useRef<LottieRefCurrentProps | null>(null)
 
-  /**
-   * Converts hex color to RGBA.
-   *
-   * @param hex - The hex color code.
-   * @returns The RGBA values.
-   */
-  const hexToRGBA = useCallback((hex: string): number[] => {
-    let r = 0,
-      g = 0,
-      b = 0
-    const a = 1
-    if (hex.length === 7) {
-      r = parseInt(hex.slice(1, 3), 16) / 255
-      g = parseInt(hex.slice(3, 5), 16) / 255
-      b = parseInt(hex.slice(5, 7), 16) / 255
-    }
-    return [r, g, b, a]
-  }, [])
-
-  /**
-   * Updates colors in a single shape item.
-   *
-   * This function checks if the item represents a stroke or
-   * fill and updates its color using the provided RGBA array.
-   * If the item contains a nested items array, it recursively
-   * calls itself to ensure that all relevant shapes within the
-   * structure are updated with the new color.
-   *
-   * @param item - The shape item object which may contain
-   * stroke or fill color properties.
-   * @param rgbaArray - The RGBA values to set as new color.
-   */
-  const updateColorsInItem = useCallback((item: any, rgbaArray: number[]) => {
-    if ((item.ty === 'st' || item.ty === 'fl') && item.c) {
-      // Check for animated color property
-      if (item.c.a === 1) {
-        item.c.k.forEach((keyframe: any) => {
-          keyframe.s = rgbaArray
-        })
-      } else {
-        item.c.k = rgbaArray
-      }
-    }
-    if (item.it) {
-      item.it.forEach((subItem: any) => updateColorsInItem(subItem, rgbaArray))
-    }
-  }, [])
-
-  /**
-   * Changes the color of stroke and fill attributes in the Lottie animation data.
-   *
-   * This function traverses the Lottie animation data structure to locate all items that
-   * represent shape colors (strokes and fills) and applies a new color based on the provided
-   * RGBA values derived from the 'newColor' hexadecimal string.
-   *
-   * @param data - The Lottie animation data object containing multiple layers and shapes.
-   * Each layer is iterated through to apply the color change to eligible elements.
-   * @param newColor - A string representing the new color to be applied, in hexadecimal format.
-   * This string is converted into an RGBA array to update the animation colors.
-   */
-  const changeLineColor = useCallback(
-    (data: any, newColor: string): void => {
-      const rgbaArray = hexToRGBA(newColor)
-
-      data.layers.forEach((layer: any) => {
-        if (layer.shapes) {
-          layer.shapes.forEach((shape: any) => {
-            shape.it.forEach((item: any) => updateColorsInItem(item, rgbaArray))
-          })
-        }
-      })
-    },
-    [hexToRGBA, updateColorsInItem],
-  )
-
   useEffect(() => {
     if (lottieRef.current) {
       lottieRef.current.setSpeed(speed)
@@ -118,7 +115,7 @@ const LottieAnimation: React.FC<LottieAnimationProps> = ({
         setKey(prevKey => prevKey + 1)
       }
     }
-  }, [speed, color, animationData, changeLineColor])
+  }, [speed, color, animationData])
 
   return (
     <Player
