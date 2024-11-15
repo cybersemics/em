560ff541f3f5ddefe95320bf144f51b0c2b989cf
Raine Revere <raine@cybersemics.org>
2024-11-15 12:34:01 +0000

    Revert "LayoutTree: Exclude overscroll bottom from viewportBottom."
    This reverts commit 9f8f25a8aec508939583a5b491b57823b4637890.
    
    It resulted in an incorrect viewportBottom calculation that caused thoughts to get cut off.

--- src/stores/scrollTop.ts
+++ src/stores/scrollTop.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import reactMinistore from './react-ministore'
 
-/** A ministore that tracks dcoument.documentElement.scrollTop (throttled to 60 fps). Includes overscroll on Safari. */
+/** A ministore that tracks dcoument.documentElement.scrollTop (throttled to 60 fps). */
 const scrollTopStore = reactMinistore(document.documentElement.scrollTop)
 
 /** Throttled update of scrollTop. Invoked on window scroll. */
