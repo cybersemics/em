55a630b73bcd0fd95c42858b7ad9a7b5c45a4d0b
Raine Revere <raine@cybersemics.org>
2024-11-17 17:54:08 +0000

    comment

--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -414,6 +414,7 @@ const TreeNode = ({
       // When y changes React re-renders the component with the new value of y. It will result in a visual change in the DOM.
       // Because this is a state-driven change, React applies the updated value to the DOM, which causes the browser to recognize that
       // a CSS property has changed, thereby triggering the CSS transition.
+      // Without this additional render, updates get batched and subsequent CSS transitions may not work properly. For example, when moving a thought down, it would not animate.
       setY(_y)
     }
   }, [y, _y])
