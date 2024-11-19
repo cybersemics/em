10ded9ce84fb8170bf83e8a45fad3b60c4b045e9
Blake Stovall <blake@disposis.com>
2024-11-18 22:20:09 -0600

    update calls to use runAllTimersAsync and wrap in act

--- src/redux-middleware/__tests__/updateUrlHistory.ts
+++ src/redux-middleware/__tests__/updateUrlHistory.ts
@@ -1,3 +1,4 @@
+import { act } from 'react'
 import { cursorBackActionCreator as cursorBack } from '../../actions/cursorBack'
 import { deleteThoughtWithCursorActionCreator as deleteThoughtWithCursor } from '../../actions/deleteThoughtWithCursor'
 import { newThoughtActionCreator as newThought } from '../../actions/newThought'
@@ -11,13 +12,13 @@ afterEach(cleanupTestApp)
 
 it('set url to cursor', async () => {
   await dispatch(newThought({ value: 'a' }))
-  vi.runOnlyPendingTimers()
+  await act(() => vi.runAllTimersAsync())
 
   const thoughtA = contextToThought(store.getState(), ['a'])!
   expect(window.location.pathname).toBe(`/~/${thoughtA.id}`)
 
   await dispatch(newThought({ value: 'b', insertNewSubthought: true }))
-  vi.runOnlyPendingTimers()
+  await act(() => vi.runAllTimersAsync())
 
   const thoughtB = contextToThought(store.getState(), ['a', 'b'])!
   expect(window.location.pathname).toBe(`/~/${thoughtA.id}/${thoughtB.id}`)
@@ -26,14 +27,14 @@ it('set url to cursor', async () => {
   expect(window.location.pathname).toBe(`/~/${thoughtA.id}`)
 
   await dispatch(cursorBack())
-  vi.runOnlyPendingTimers()
+  await act(() => vi.runAllTimersAsync())
 
   expect(window.location.pathname).toBe('/')
 })
 
 it('set url to home after deleting last empty thought', async () => {
   await dispatch(newThought({}))
-  vi.runOnlyPendingTimers()
+  await act(() => vi.runAllTimersAsync())
 
   const thoughtA = contextToThought(store.getState(), [''])!
   expect(window.location.pathname).toBe(`/~/${thoughtA.id}`)
