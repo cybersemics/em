63a729624b7931f7f19df7edf866ea77e5efc5d0
Blake Stovall <blake@disposis.com>
2024-11-20 00:26:54 -0600

    add await act

--- src/components/__tests__/LetterCasePicker.ts
+++ src/components/__tests__/LetterCasePicker.ts
@@ -1,3 +1,4 @@
+import { act } from 'react'
 import { newThoughtActionCreator as newThought } from '../../actions/newThought'
 import { HOME_TOKEN } from '../../constants'
 import exportContext from '../../selectors/exportContext'
@@ -13,6 +14,8 @@ it('Set Lower Case to the current thought', async () => {
   await dispatch([newThought({ value: 'Hello everyone, this is Rose. Thanks for your help.' })])
   await click('[data-testid="toolbar-icon"][aria-label="LetterCase"]')
   await click('[aria-label="letter case swatches"] [aria-label="LowerCase"]')
+  
+  await act(async () => vi.runOnlyPendingTimersAsync())
 
   const state = store.getState()
 
@@ -26,6 +29,8 @@ it('Set Upper Case to the current thought', async () => {
   await click('[data-testid="toolbar-icon"][aria-label="LetterCase"]')
   await click('[aria-label="letter case swatches"] [aria-label="UpperCase"]')
 
+  await act(async () => vi.runOnlyPendingTimersAsync())
+
   const state = store.getState()
 
   const exported = exportContext(state, [HOME_TOKEN], 'text/plain')
@@ -38,6 +43,8 @@ it('Set Sentence Case to the current thought', async () => {
   await click('[data-testid="toolbar-icon"][aria-label="LetterCase"]')
   await click('[aria-label="letter case swatches"] [aria-label="SentenceCase"]')
 
+  await act(async () => vi.runOnlyPendingTimersAsync())
+
   const state = store.getState()
 
   const exported = exportContext(state, [HOME_TOKEN], 'text/plain')
@@ -49,6 +56,8 @@ it('Set Title Case to the current thought', async () => {
   await dispatch([newThought({ value: 'Hello everyone, this is Rose. Thanks for your help.' })])
   await click('[data-testid="toolbar-icon"][aria-label="LetterCase"]')
   await click('[aria-label="letter case swatches"] [aria-label="TitleCase"]')
+
+  await act(async () => vi.runOnlyPendingTimersAsync())
 
   const state = store.getState()
 
