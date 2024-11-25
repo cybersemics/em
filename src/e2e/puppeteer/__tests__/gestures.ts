a9631601c9073cc9c860ae0cf2a7cc55192a33d2
Ethan James <ethan.e.james@gmail.com>
2024-11-25 11:52:20 -0800

    Finish tests for gesture with selection

--- src/e2e/puppeteer/__tests__/gestures.ts
+++ src/e2e/puppeteer/__tests__/gestures.ts
@@ -1,13 +1,14 @@
 import { KnownDevices } from 'puppeteer'
 import { drawHorizontalLineGesture } from '../helpers/gesture'
 import paste from '../helpers/paste'
+import waitForEditable from '../helpers/waitForEditable'
 import { page } from '../setup'
 
 vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
 const iPhone = KnownDevices['iPhone 15 Pro']
 
 describe('gestures', () => {
-  it.skip('when starting a gesture, the command palette will open', async () => {
+  it('when starting a gesture, the command palette will open', async () => {
     const cursor = 'Hello'
     const importText = `
     - ${cursor}`
@@ -16,6 +17,52 @@ describe('gestures', () => {
     await paste(importText)
 
     await drawHorizontalLineGesture()
+
+    // the command palette should open
+    const popupValue = await page.$('[data-testid=popup-value]')
+    expect(popupValue).toBeTruthy()
+  })
+
+  it('when starting a gesture near the selection, the gesture will be abandoned', async () => {
+    const cursor = 'Hello'
+    const importText = `
+    - ${cursor}`
+
+    await page.emulate(iPhone)
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('Hello')
+    const rect = await page.evaluate(element => {
+      const rect = element!.getBoundingClientRect()
+      return { left: rect.left, top: rect.top, right: rect.right, bottom: rect.bottom }
+    }, editableNodeHandle)
+
+    await page.touchscreen.tap(rect.left + 10, rect.top + 10)
+    await page.touchscreen.tap(rect.left + 10, rect.top + 10)
+    await drawHorizontalLineGesture(rect.bottom + 10)
+
+    // the command palette should not open
+    const popupValue = await page.$('[data-testid=popup-value]')
+    expect(popupValue).toBeFalsy()
+  })
+
+  it('when starting a gesture far from the selection, the command palette will open', async () => {
+    const cursor = 'Hello'
+    const importText = `
+    - ${cursor}`
+
+    await page.emulate(iPhone)
+    await paste(importText)
+
+    const editableNodeHandle = await waitForEditable('Hello')
+    const rect = await page.evaluate(element => {
+      const rect = element!.getBoundingClientRect()
+      return { left: rect.left, top: rect.top, right: rect.right, bottom: rect.bottom }
+    }, editableNodeHandle)
+
+    await page.touchscreen.tap(rect.left + 10, rect.top + 10)
+    await page.touchscreen.tap(rect.left + 10, rect.top + 10)
+    await drawHorizontalLineGesture(rect.bottom + 110)
 
     // the command palette should open
     const popupValue = await page.$('[data-testid=popup-value]')
