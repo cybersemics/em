94d9731f3fe0fd191aa20e4f9e5c71f3f86e999d
Esen <s7b5en@gmail.com>
2024-11-26 19:39:31 +0600

    add transition to translateY

--- src/components/ScrollZone.tsx
+++ src/components/ScrollZone.tsx
@@ -7,6 +7,7 @@ import viewportStore from '../stores/viewport'
 
 /** An overlay for the scroll zone that blocks pointer events. */
 const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
+  
   const scrollTop = useScrollTop()
   const scrollZoneWidth = viewportStore.useSelector(state => state.scrollZoneWidth)
   const hideScrollZone = useSelector(state => state.showModal || getUserSetting(state, Settings.hideScrollZone))
@@ -19,7 +20,7 @@ const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
         backgroundImage: `url('/img/scroll-zone/stardust.png')`,
         backgroundRepeat: 'repeat',
         zIndex: 'scrollZone',
-        backgroundSize: "500px",
+        backgroundSize: "800px",
         position: 'fixed',
         left: leftHanded ? 0 : undefined,
         right: leftHanded ? undefined : 0,
@@ -30,6 +31,7 @@ const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
       style={{
         transform: `translateY(-${scrollTop / 4 + 400}px)`,
         width: scrollZoneWidth,
+        transition: 'transform 0.1s ease-in-out',
       }}
     ></div>
   )
