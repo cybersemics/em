8bb697f97144627e62bf0dd113e6338768071c72
Esen <s7b5en@gmail.com>
2024-11-26 19:53:47 +0600

    fix lint

--- src/components/ScrollZone.tsx
+++ src/components/ScrollZone.tsx
@@ -7,7 +7,6 @@ import viewportStore from '../stores/viewport'
 
 /** An overlay for the scroll zone that blocks pointer events. */
 const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
-  
   const scrollTop = useScrollTop()
   const scrollZoneWidth = viewportStore.useSelector(state => state.scrollZoneWidth)
   const hideScrollZone = useSelector(state => state.showModal || getUserSetting(state, Settings.hideScrollZone))
@@ -16,11 +15,11 @@ const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
   return (
     <div
       className={css({
-        backgroundColor: "black",
+        backgroundColor: 'black',
         backgroundImage: `url('/img/scroll-zone/stardust.png')`,
         backgroundRepeat: 'repeat',
         zIndex: 'scrollZone',
-        backgroundSize: "800px",
+        backgroundSize: '800px',
         position: 'fixed',
         left: leftHanded ? 0 : undefined,
         right: leftHanded ? undefined : 0,
