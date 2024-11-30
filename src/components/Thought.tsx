0390b0b3196158d2b60bb7e783a98eccc125ccc8
Raine Revere <raine@cybersemics.org>
2024-11-30 11:07:39 +0000

    Remove minThoughtHeight.
    It does not cover the gap left by clip-path as it claims, and it shifts the layout after url thoughts.

--- src/components/Thought.tsx
+++ src/components/Thought.tsx
@@ -420,7 +420,7 @@ const ThoughtContainer = ({
         className={css({
           /* Use line-height to vertically center the text and bullet. We cannot use padding since it messes up the selection. This needs to be overwritten on multiline elements. See ".child .editable" below. */
           /* must match value used in Editable useMultiline */
-          lineHeight: '1.72',
+          lineHeight: '1.87',
           // ensure that ThoughtAnnotation is positioned correctly
           position: 'relative',
           ...(hideBullet ? { marginLeft: -12 } : null),
