a0fd81529bb4c072e06b26b7bcef3e3cfda4c8f2
Ethan James <ethan.e.james@gmail.com>
2024-12-03 10:09:18 -0800

    Colocate hideCaret animation names with helpers

--- src/util/getHideCaretAnimationName.ts
+++ src/util/getHideCaretAnimationName.ts
@@ -1,7 +1,25 @@
 import { cva } from '../../styled-system/css'
-import { RecipeVariantRecord } from '../../styled-system/types/recipe'
+import { type RecipeVariantRecord } from '../../styled-system/types/recipe'
 import { isSafari, isTouch } from '../browser'
-import hideCaretAnimationNames from '../hideCaret.config'
+
+export const hideCaretAnimationNames = [
+  'hideCaret0',
+  'hideCaret1',
+  'hideCaret2',
+  'hideCaret3',
+  'hideCaret4',
+  'hideCaret5',
+  'hideCaret6',
+  'hideCaret7',
+  'hideCaret8',
+  'hideCaret9',
+  'hideCaretA',
+  'hideCaretB',
+  'hideCaretC',
+  'hideCaretD',
+  'hideCaretE',
+  'hideCaretF',
+] as const
 
 const hideCaret = cva({
   base: {
