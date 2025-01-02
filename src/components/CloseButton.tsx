5c96172f8f106f8cc3acc18d228991ca5ef90034
Christina Yang <ychristina011@gmail.com>
2025-01-01 20:53:19 -0800

    show x on hover

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
 import { css, cx } from '../../styled-system/css'
 import { upperRightRecipe } from '../../styled-system/recipes'
+import { SystemStyleObject } from '../../styled-system/types'
 import fastClick from '../util/fastClick'
 
 /** A close button with an ✕. */
@@ -8,10 +9,12 @@ const CloseButton = ({
   onClose,
   disableSwipeToDismiss,
   size = 'md',
+  cssRaw,
 }: {
   onClose: () => void
   disableSwipeToDismiss?: boolean
   size?: 'sm' | 'md'
+  cssRaw?: SystemStyleObject
 }) => {
   const fontSize = useSelector(state => state.fontSize)
   const padding = (fontSize / 2 + 2) / (size === 'sm' ? 2 : 1)
@@ -20,16 +23,20 @@ const CloseButton = ({
       {...fastClick(onClose)}
       className={cx(
         upperRightRecipe(),
-        css({
-          // inherit not yet supported by plugin
-          // eslint-disable-next-line @pandacss/no-hardcoded-color
-          color: 'inherit',
-          textDecoration: 'none',
-        }),
+        css(
+          {
+            // inherit not yet supported by plugin
+            // eslint-disable-next-line @pandacss/no-hardcoded-color
+            color: 'inherit',
+            textDecoration: 'none',
+          },
+          cssRaw,
+        ),
       )}
       style={{ fontSize: size === 'sm' ? fontSize / 2 : fontSize, padding: `${padding}px ${padding * 1.25}px` }}
       aria-label={disableSwipeToDismiss ? 'no-swipe-to-dismiss' : undefined}
       data-testid='close-button'
+      data-close-button
     >
       ✕
     </a>
