8262b300d0ddd9d1c34049bc5c2b6f426b585783
Red-Rose515 <Albertchess990919@gmail.com>
2025-01-10 04:16:01 +0200

    fix error importing empty parent in root

--- src/actions/importFiles.ts
+++ src/actions/importFiles.ts
@@ -271,8 +271,12 @@ export const importFilesActionCreator =
           )
           const parentContext =
             ancestors.length === 0 ? baseContext : [...unroot(baseContext), ...relativeAncestorContext]
-          // TODO: It would be better to get the id from importText rather than contextToPath
-          const parentPath = contextToPath(stateAfterPull, parentContext)
+
+          // Special handling for empty parent nodes
+          const parentPath =
+            parentContext.length === 1 && parentContext[0] === ''
+              ? path // Use the current path for empty parent nodes
+              : contextToPath(stateAfterPull, parentContext)
 
           // validate parentPath
           if (!parentPath) {
