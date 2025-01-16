7d0db5e5a27d64c89fa5ef53d4825c8b93145940
Ethan James <ethan.e.james@gmail.com>
2025-01-16 14:02:02 -0800

    LayoutTree: no hideCaret animation for 1st thought (#2771)

--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -937,7 +937,12 @@ const LayoutTree = () => {
   return (
     <div
       // the hideCaret animation must run every time the indent changes on iOS Safari, which necessitates replacing the animation with an identical substitute with a different name
-      className={cx(css({ marginTop: '0.501em' }), hideCaret({ animation: getHideCaretAnimationName(indentDepth) }))}
+      className={cx(
+        css({ marginTop: '0.501em' }),
+        hideCaret({
+          animation: getHideCaretAnimationName(indentDepth),
+        }),
+      )}
       style={{
         // add a full viewport height's space above to ensure that there is room to scroll by the same amount as spaceAbove
         transform: `translateY(${-spaceAboveExtended + viewportHeight}px)`,
