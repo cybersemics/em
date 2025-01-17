56c10ba79674f25884383f4efd2d5f0a79e6c9e3
Esen <s7b5en@gmail.com>
2025-01-17 18:51:32 +0600

    remove ensuring steps

--- src/components/Tutorial/__tests__/Tutorial.ts
+++ src/components/Tutorial/__tests__/Tutorial.ts
@@ -1,28 +1,7 @@
 import { getByText, screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
 import { act } from 'react'
-import {
-  HOME_TOKEN,
-  TUTORIAL2_STEP_CHOOSE,
-  TUTORIAL2_STEP_CONTEXT1,
-  TUTORIAL2_STEP_CONTEXT1_PARENT,
-  TUTORIAL2_STEP_CONTEXT1_SUBTHOUGHT,
-  TUTORIAL2_STEP_CONTEXT2_PARENT,
-  TUTORIAL2_STEP_CONTEXT_VIEW_EXAMPLES,
-  TUTORIAL2_STEP_CONTEXT_VIEW_OPEN,
-  TUTORIAL2_STEP_CONTEXT_VIEW_SELECT,
-  TUTORIAL2_STEP_START,
-  TUTORIAL_STEP_AUTOEXPAND,
-  TUTORIAL_STEP_AUTOEXPAND_EXPAND,
-  TUTORIAL_STEP_FIRSTTHOUGHT,
-  TUTORIAL_STEP_FIRSTTHOUGHT_ENTER,
-  TUTORIAL_STEP_SECONDTHOUGHT,
-  TUTORIAL_STEP_SECONDTHOUGHT_ENTER,
-  TUTORIAL_STEP_START,
-  TUTORIAL_STEP_SUBTHOUGHT,
-  TUTORIAL_STEP_SUBTHOUGHT_ENTER,
-  TUTORIAL_STEP_SUCCESS,
-} from '../../../constants'
+import { HOME_TOKEN } from '../../../constants'
 import exportContext from '../../../selectors/exportContext'
 import store from '../../../stores/app'
 import createTestApp, { cleanupTestApp, cleanupTestEventHandlers } from '../../../test-helpers/createTestApp'
@@ -34,18 +13,12 @@ const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime })
 /** Get last created thought in the document, used mostly after `user.keyboard('{Enter}')`. */
 const lastThought = () => Array.from(document.querySelectorAll('[contenteditable="true"]')).at(-1)!
 
-/** Ensure we are at the given step of the tutorial. */
-const ensureStep = (step: number) => {
-  expect(store.getState().storageCache?.tutorialStep).toBe(step)
-}
-
 describe('Tutorial 1', async () => {
   beforeEach(() => createTestApp({ tutorial: true }))
   afterEach(cleanupTestEventHandlers)
   afterAll(cleanupTestApp)
   describe('step start', () => {
     it('shows the welcome text', () => {
-      ensureStep(TUTORIAL_STEP_START)
       expect(screen.getByText('Welcome to your personal thoughtspace.')).toBeInTheDocument()
     })
 
@@ -56,12 +29,10 @@ describe('Tutorial 1', async () => {
   })
 
   it('step first thought - how to create a thought ', async () => {
-    ensureStep(TUTORIAL_STEP_FIRSTTHOUGHT)
     expect(screen.getByText('Hit the Enter key to create a new thought.')).toBeInTheDocument()
     await user.keyboard('{Enter}')
     expect(screen.getByText('You did it!')).toBeInTheDocument()
 
-    ensureStep(TUTORIAL_STEP_FIRSTTHOUGHT_ENTER)
     expect(screen.getByText('Now type something. Anything will do.')).toBeInTheDocument()
     await user.type(lastThought(), 'my first thought')
     await user.keyboard('{Enter}')
@@ -71,13 +42,9 @@ describe('Tutorial 1', async () => {
   it('step second thought - prompts to add another thought', async () => {
     expect(screen.getByText('Well done!')).toBeInTheDocument()
     expect(screen.getByText(/Try adding another thought/)).toBeInTheDocument()
-    ensureStep(TUTORIAL_STEP_SECONDTHOUGHT)
-
     await user.type(lastThought(), 'my second thought')
     await user.keyboard('{Enter}')
     await act(vi.runOnlyPendingTimersAsync)
-
-    ensureStep(TUTORIAL_STEP_SECONDTHOUGHT_ENTER)
 
     expect(screen.getByText('Good work!')).toBeInTheDocument()
     expect(screen.getByText('Now type some text for the new thought.')).toBeInTheDocument()
@@ -87,7 +54,6 @@ describe('Tutorial 1', async () => {
   })
 
   it('step subthought - how to create a thought inside thought', async () => {
-    ensureStep(TUTORIAL_STEP_SUBTHOUGHT)
     expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
       `- __ROOT__
   - my first thought
@@ -119,27 +85,23 @@ describe('Tutorial 1', async () => {
     expect(screen.getByText(/As you can see, the new thought "child" is nested/)).toBeInTheDocument()
     expect(getByText(screen.getByTestId('tutorial-step'), /"third thought"/)).toBeInTheDocument()
 
-    ensureStep(TUTORIAL_STEP_SUBTHOUGHT_ENTER)
     await user.click(screen.getByText('Next'))
   })
 
   describe('step autoexpand', async () => {
     it('thoughts are automatically hidden when you click away', async () => {
-      ensureStep(TUTORIAL_STEP_AUTOEXPAND)
       expect(screen.getByText(/thoughts are automatically hidden when you click away/)).toBeInTheDocument()
     })
 
     it('click on "uncle" thought to hide child', async () => {
       await user.click(screen.getByText('my second thought'))
       expect(screen.getByText('Notice that "child" is hidden now.', { exact: false })).toBeInTheDocument()
-      ensureStep(TUTORIAL_STEP_AUTOEXPAND_EXPAND)
     })
 
     it('click back on a "parent" thought to reveal child', async () => {
       expect(screen.getByText(/Click "third thought" to reveal its subthought "child"/)).toBeInTheDocument()
       await user.click(screen.getAllByText('third thought').at(-1)!)
       expect(screen.getByText('Lovely. You have completed the tutorial', { exact: false })).toBeInTheDocument()
-      ensureStep(TUTORIAL_STEP_SUCCESS)
     })
   })
 
@@ -162,7 +124,6 @@ describe('Tutorial 2', async () => {
   it('step start - tell about context menu', async () => {
     await user.click(screen.getByText('Learn more'))
 
-    ensureStep(TUTORIAL2_STEP_START)
     expect(screen.getByText(`If the same thought appears in more than one place`, { exact: false })).toBeInTheDocument()
     expect(
       screen.getByText(`shows a small number to the right of the thought, for example`, { exact: false }),
@@ -171,7 +132,6 @@ describe('Tutorial 2', async () => {
 
   it('step choose - gives 3 choices of what project to proceed with', async () => {
     await user.click(screen.getByText('Next'))
-    ensureStep(TUTORIAL2_STEP_CHOOSE)
     expect(screen.getByText(/For this tutorial, choose what kind of content you want to create/)).toBeInTheDocument()
     expect(screen.getByText('To-Do List')).toBeInTheDocument()
     expect(screen.getByText('Journal Theme')).toBeInTheDocument()
@@ -181,13 +141,11 @@ describe('Tutorial 2', async () => {
   it('step context 1 parent - we create "Home" thought with children', async () => {
     await user.click(screen.getAllByText('To-Do List').at(-1)!)
     expect(screen.getByText(/Excellent choice. Now create a new thought with the text “Home”/)).toBeInTheDocument()
-    ensureStep(TUTORIAL2_STEP_CONTEXT1_PARENT)
 
     // we create a `Home` thought
     await user.keyboard('{Enter}')
     await user.type(lastThought(), 'Home')
     await act(vi.runOnlyPendingTimersAsync)
-    ensureStep(TUTORIAL2_STEP_CONTEXT1)
 
     // Home -> To Do
     expect(screen.getByText(/Let's say that you want to make a list of things/)).toBeInTheDocument()
@@ -195,7 +153,6 @@ describe('Tutorial 2', async () => {
     await user.keyboard('{Control>}{Enter}{/Control}')
     await user.type(lastThought(), 'To Do')
     await act(vi.runOnlyPendingTimersAsync)
-    ensureStep(TUTORIAL2_STEP_CONTEXT1_SUBTHOUGHT)
 
     // Home -> To Do -> or to not
     expect(screen.getByText(/Now add a thought to “To Do”/)).toBeInTheDocument()
@@ -214,7 +171,6 @@ describe('Tutorial 2', async () => {
 
   it('step context 2 - we create "Work" thought with children', async () => {
     await user.click(screen.getByText('Next'))
-    ensureStep(TUTORIAL2_STEP_CONTEXT2_PARENT)
     expect(screen.getByText(/Now we are going to create a different "To Do" list./)).toBeInTheDocument()
 
     // we created a new thought on 3rd level, so we shift-tab our way back to root
@@ -259,7 +215,6 @@ describe('Tutorial 2', async () => {
     await user.click(screen.getByText('Next'))
     await act(vi.runOnlyPendingTimersAsync)
 
-    ensureStep(TUTORIAL2_STEP_CONTEXT_VIEW_SELECT)
     expect(screen.getByText(/First select "To Do"./)).toBeInTheDocument()
     await user.keyboard('{Escape}') // focus out
     await user.click(screen.getAllByText('To Do').at(-1)!) // and click on To Do
@@ -268,7 +223,6 @@ describe('Tutorial 2', async () => {
     expect(screen.getByText("Hit Alt + Shift + S to view the current thought's contexts.")).toBeInTheDocument()
     await user.keyboard('{Alt>}{Shift>}S{/Shift}{/Alt}')
     await act(vi.runOnlyPendingTimersAsync)
-    ensureStep(TUTORIAL2_STEP_CONTEXT_VIEW_OPEN)
     expect(
       screen.getByText(
         `Well, look at that. We now see all of the contexts in which "To Do" appears, namely "Home" and "Work".`,
@@ -278,7 +232,6 @@ describe('Tutorial 2', async () => {
 
   it('step context examples - we see real-world examples', async () => {
     await user.click(screen.getByText('Next'))
-    ensureStep(TUTORIAL2_STEP_CONTEXT_VIEW_EXAMPLES)
     expect(screen.getByText(/Here are some real-world examples of using contexts in/)).toBeInTheDocument()
     expect(screen.getByText('View all thoughts related to a particular person, place, or thing.')).toBeInTheDocument()
     expect(screen.getByText('Keep track of quotations from different sources.')).toBeInTheDocument()
