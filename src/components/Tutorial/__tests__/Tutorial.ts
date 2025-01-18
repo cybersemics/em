a42df99f2fc9eb2f81e0ac6befc9feaed46687bb
Esen <s7b5en@gmail.com>
2025-01-19 05:06:18 +0600

    another wave of simplification

--- src/components/Tutorial/__tests__/Tutorial.ts
+++ src/components/Tutorial/__tests__/Tutorial.ts
@@ -1,17 +1,14 @@
 import { screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
 import { act } from 'react'
-import { HOME_TOKEN } from '../../../constants'
-import exportContext from '../../../selectors/exportContext'
-import store from '../../../stores/app'
 import createTestApp, { cleanupTestApp, cleanupTestEventHandlers } from '../../../test-helpers/createTestApp'
 
 // as per https://testing-library.com/docs/user-event/options/#advancetimers
 // we should avoid using { delay: null }, and use jest.advanceTimersByTime instead
 const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime })
 
-/** Get last created empty thought in the document. Gets the last thought that is not empty. Mostly used after `user.keyboard('{Enter}')` to get the new thought. */
-const lastThought = () =>
+/** Gets the last empty thought in the document. Mostly used after `user.keyboard('{Enter}')` to get the new thought. */
+const lastEmptyThought = () =>
   Array.from(document.querySelectorAll('[data-editable="true"]'))
     .filter(it => !it.textContent)
     .at(-1)!
@@ -21,95 +18,100 @@ describe('Tutorial 1', async () => {
   afterEach(cleanupTestEventHandlers)
   afterAll(cleanupTestApp)
   describe('step start', () => {
-    it('shows the welcome text', () => {
+    it('we see the welcome text', () => {
       expect(screen.getByText('Welcome to your personal thoughtspace.')).toBeInTheDocument()
     })
 
-    it('shows the "Next" button, that leads to the first step', async () => {
+    it('we can proceed to first step by clicking Next', async () => {
       await user.click(screen.getByText('Next'))
-      expect(screen.getByText(/First, let me show you how to create a new thought/)).toBeInTheDocument()
+      expect(screen.getByText('Hit the Enter key to create a new thought.')).toBeInTheDocument()
     })
   })
 
-  it('step first thought - how to create a thought ', async () => {
-    expect(screen.getByText('Hit the Enter key to create a new thought.')).toBeInTheDocument()
-    await user.keyboard('{Enter}')
-    expect(screen.getByText('You did it!')).toBeInTheDocument()
+  describe('step first thought', () => {
+    it('we learn how to create a thought', async () => {
+      await user.keyboard('{Enter}')
+      expect(screen.getByText('Now type something. Anything will do.')).toBeInTheDocument()
+    })
 
-    expect(screen.getByText('Now type something. Anything will do.')).toBeInTheDocument()
-    await user.type(lastThought(), 'my first thought')
-    await user.keyboard('{Enter}')
-    await act(vi.runOnlyPendingTimersAsync)
-    expect(screen.getByText('Well done!')).toBeInTheDocument()
+    it('we create our first thought', async () => {
+      await user.type(lastEmptyThought(), 'first')
+      await user.keyboard('{Enter}')
+      await act(vi.runOnlyPendingTimersAsync)
+
+      expect(screen.getByText('Well done!')).toBeInTheDocument()
+    })
   })
 
-  it('step second thought - prompts to add another thought', async () => {
+  it('we create a second thought', async () => {
     expect(screen.getByText(/Try adding another thought/)).toBeInTheDocument()
-    await user.type(lastThought(), 'my second thought')
+    await user.type(lastEmptyThought(), 'second')
     await user.keyboard('{Enter}')
     await act(vi.runOnlyPendingTimersAsync)
 
-    expect(screen.getByText('Good work!')).toBeInTheDocument()
     expect(screen.getByText('Now type some text for the new thought.')).toBeInTheDocument()
-    await user.type(lastThought(), 'third thought')
-    await user.keyboard('{Enter}')
-    await act(vi.runOnlyPendingTimersAsync)
   })
 
-  it('step subthought - how to create a thought inside thought', async () => {
-    expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
-      `- __ROOT__
-  - my first thought
-  - my second thought
-  - third thought
-  - `,
-    )
+  it('we create a third thought', async () => {
+    await user.type(lastEmptyThought(), 'third')
+    await user.keyboard('{Enter}')
+    await act(vi.runOnlyPendingTimersAsync)
 
     expect(screen.getByText(/Hit the Delete key to delete the current blank thought/)).toBeInTheDocument()
-    expect(screen.getByText(/Then hold the Ctrl key and hit the Enter key/)).toBeInTheDocument()
+  })
+
+  it('we learn to create nested thoughts', async () => {
+    /* thoughts:
+  - first
+  - second
+  - third
+  -
+    */
     await user.keyboard('{Backspace}')
     await user.keyboard('{Control>}{Enter}{/Control}')
     await act(vi.runOnlyPendingTimersAsync)
 
-    await user.type(lastThought(), 'child')
+    await user.type(lastEmptyThought(), 'child of third')
     await act(vi.runOnlyPendingTimersAsync)
-    expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
-      `- __ROOT__
-  - my first thought
-  - my second thought
-  - third thought
-    - child`,
-    )
 
-    expect(screen.getByText(/As you can see, the new thought "child" is nested/)).toBeInTheDocument()
-
-    await user.click(screen.getByText('Next'))
+    expect(screen.getByText(/As you can see, the new thought "child of third" is nested/)).toBeInTheDocument()
   })
 
   describe('step autoexpand', async () => {
-    it('thoughts are automatically hidden when you click away', async () => {
+    it('we learn about automatic thought hiding', async () => {
+      await user.click(screen.getByText('Next'))
       expect(screen.getByText(/thoughts are automatically hidden when you click away/)).toBeInTheDocument()
     })
 
-    it('click on "uncle" thought to hide child', async () => {
-      await user.click(screen.getByText('my second thought'))
-      expect(screen.getByText(/Notice that "child" is hidden now/)).toBeInTheDocument()
+    it('we hide nested thoughts by clicking away', async () => {
+      /* thoughts:
+      - first
+      - second
+      - third
+        - child of third
+      */
+      await user.click(screen.getByText('second'))
+      expect(screen.getByText(/Notice that "child of third" is hidden now/)).toBeInTheDocument()
     })
 
-    it('click back on a "parent" thought to reveal child', async () => {
-      expect(screen.getByText(/Click "third thought" to reveal its subthought "child"/)).toBeInTheDocument()
-      await user.click(screen.getAllByText('third thought').at(-1)!)
-      expect(screen.getByText(/Lovely\. You have completed the tutorial/)).toBeInTheDocument()
+    it('we reveal hidden thoughts by clicking parent', async () => {
+      await user.click(screen.getAllByText('third').at(-1)!)
+      expect(screen.getByText('child of third')).toBeInTheDocument()
     })
   })
 
-  describe('step success - congratulates on completing first tutorial', async () => {
-    it('congratulate on completing first tutorial', async () => {
+  describe('step success', async () => {
+    it('we complete the first tutorial', async () => {
       expect(screen.getByText(/Lovely\. You have completed the tutorial/)).toBeInTheDocument()
     })
 
-    it('asks to continue with tutorial or play on own', async () => {
-      expect(screen.getByText('Play on my own')).toBeInTheDocument()
+    it('we can exit tutorial mode', async () => {
+      await user.click(screen.getByText('Play on my own'))
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(() => screen.getByTestId('tutorial-step')).toThrow('Unable to find an element')
+    })
+
+    it('we can continue to advanced tutorial', async () => {
       expect(screen.getByText('Learn more')).toBeInTheDocument()
     })
   })
@@ -119,14 +121,13 @@ describe('Tutorial 2', async () => {
   beforeEach(() => createTestApp({ tutorial: true }))
   afterEach(cleanupTestEventHandlers)
   afterAll(cleanupTestApp)
-  it('step start - tell about context menu', async () => {
+  it('we learn about context indicators', async () => {
     await user.click(screen.getByText('Learn more'))
 
-    expect(screen.getByText(/If the same thought appears in more than one place/)).toBeInTheDocument()
     expect(screen.getByText(/shows a small number to the right of the thought, for example/)).toBeInTheDocument()
   })
 
-  it('step choose - gives 3 choices of what project to proceed with', async () => {
+  it('we choose a project type, one of three options', async () => {
     await user.click(screen.getByText('Next'))
     expect(screen.getByText(/For this tutorial, choose what kind of content you want to create/)).toBeInTheDocument()
     expect(screen.getByText('To-Do List')).toBeInTheDocument()
@@ -134,109 +135,126 @@ describe('Tutorial 2', async () => {
     expect(screen.getByText('Book/Podcast Notes')).toBeInTheDocument()
   })
 
-  it('step context 1 parent - we create "Home" thought with children', async () => {
+  it('we start creating a to-do list', async () => {
     await user.click(screen.getAllByText('To-Do List').at(-1)!)
-    expect(screen.getByText(/Excellent choice. Now create a new thought with the text “Home”/)).toBeInTheDocument()
-
-    await user.keyboard('{Enter}')
-    await user.type(lastThought(), 'Home')
-    await act(vi.runOnlyPendingTimersAsync)
-
-    // Home -> To Do
-    expect(screen.getByText(/Let's say that you want to make a list of things/)).toBeInTheDocument()
-    expect(screen.getByText(/Add a thought with the text "To Do"/)).toBeInTheDocument()
-    await user.keyboard('{Control>}{Enter}{/Control}')
-    await user.type(lastThought(), 'To Do')
-    await act(vi.runOnlyPendingTimersAsync)
-
-    // Home -> To Do -> or to not
-    expect(screen.getByText(/Now add a thought to “To Do”/)).toBeInTheDocument()
-    await user.keyboard('{Control>}{Enter}{/Control}')
-    await user.type(lastThought(), 'or to not')
-    await act(vi.runOnlyPendingTimersAsync)
-
-    expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
-      `- __ROOT__
-  - Home
-    - To Do
-      - or to not`,
-    )
-    expect(screen.getByText(/Nice work!/)).toBeInTheDocument()
+    expect(screen.getByText(/Excellent choice\. Now create a new thought with the text “Home”/)).toBeInTheDocument()
   })
 
-  it('step context 2 - we create "Work" thought with children', async () => {
-    await user.click(screen.getByText('Next'))
-    expect(screen.getByText(/Now we are going to create a different "To Do" list./)).toBeInTheDocument()
+  describe('step context 1 - create a "Home" to-do list', () => {
+    it('we create a Home thought', async () => {
+      await user.keyboard('{Enter}')
+      await user.type(lastEmptyThought(), 'Home')
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(screen.getByText(/Add a thought with the text "To Do"/)).toBeInTheDocument()
+    })
 
-    // we created a new thought on 3rd level, clicking "Home" gets us to root
-    await user.click(screen.getByText('Home'))
-    await user.keyboard('{Enter}')
-    await user.type(lastThought(), 'Work')
-    await act(vi.runOnlyPendingTimersAsync)
+    it('we add a Home->To Do subthought', async () => {
+      await user.keyboard('{Control>}{Enter}{/Control}')
+      await user.type(lastEmptyThought(), 'To Do')
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(screen.getByText(/Now add a thought to “To Do”/)).toBeInTheDocument()
+    })
 
-    expect(screen.getByText('Now add a thought with the text "To Do"', { exact: false })).toBeInTheDocument()
-    await user.keyboard('{Control>}{Enter}{/Control}')
-    await user.type(lastThought(), 'To Do')
-    expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
-      `- __ROOT__
-  - Home
-    - To Do
-      - or to not
-  - Work
-    - To Do`,
-    )
-    expect(screen.getByText('Very good!')).toBeInTheDocument()
+    it('we add a Home->To Do->or to not subthought sub-subthought', async () => {
+      await user.keyboard('{Control>}{Enter}{/Control}')
+      await user.type(lastEmptyThought(), 'or to not')
+      await act(vi.runOnlyPendingTimersAsync)
 
-    expect(screen.getAllByRole('superscript')[0]).toHaveTextContent('2')
-    expect(screen.getByText('This means that “To Do” appears in two places', { exact: false })).toBeInTheDocument()
-
-    expect(screen.getByText('Imagine a new work task. Add it to this “To Do” list.'))
-    await user.keyboard('{Control>}{Enter}{/Control}')
-    await user.type(lastThought(), 'new work task')
+      /* thoughts:
+      - Home
+        - To Do
+          - or to not
+      */
+      expect(screen.getByText(/Nice work!/)).toBeInTheDocument()
+    })
   })
 
-  it('step context view open - we press Alt+Shift+S and see "To Do" in both "Home" and "Work"', async () => {
-    expect(exportContext(store.getState(), [HOME_TOKEN], 'text/plain')).toBe(
-      `- __ROOT__
-  - Home
-    - To Do
-      - or to not
-  - Work
-    - To Do
-      - new work task`,
-    )
-    await user.click(screen.getByText('Next'))
-    await act(vi.runOnlyPendingTimersAsync)
+  describe('step context 2 - create a "Work" to-do list', () => {
+    it('we prepare to create another list', async () => {
+      await user.click(screen.getByText('Next'))
+      expect(screen.getByText(/Now we are going to create a different "To Do" list./)).toBeInTheDocument()
+    })
 
-    expect(screen.getByText(/First select "To Do"./)).toBeInTheDocument()
-    await user.keyboard('{Escape}')
-    await user.click(screen.getAllByText('To Do').at(-1)!)
-    await act(vi.runOnlyPendingTimersAsync)
+    it('we create a Work thought', async () => {
+      // we created a new thought on 3rd level, clicking "Home" gets us to root
+      await user.click(screen.getByText('Home'))
+      await user.keyboard('{Enter}')
+      await user.type(lastEmptyThought(), 'Work')
+      await act(vi.runOnlyPendingTimersAsync)
 
-    expect(screen.getByText("Hit Alt + Shift + S to view the current thought's contexts.")).toBeInTheDocument()
-    await user.keyboard('{Alt>}{Shift>}S{/Shift}{/Alt}')
-    await act(vi.runOnlyPendingTimersAsync)
-    expect(
-      screen.getByText(
-        `Well, look at that. We now see all of the contexts in which "To Do" appears, namely "Home" and "Work".`,
-      ),
-    ).toBeInTheDocument()
+      /* thoughts:
+      - Home
+        - To Do
+          - or to not
+      - Work
+      */
+      expect(screen.getByText(/Now add a thought with the text "To Do"/)).toBeInTheDocument()
+    })
+
+    it('we add another Work->To Do thought', async () => {
+      await user.keyboard('{Control>}{Enter}{/Control}')
+      await user.type(lastEmptyThought(), 'To Do')
+      expect(screen.getByText('Imagine a new work task. Add it to this “To Do” list.'))
+    })
+
+    it('we see context indicators', async () => {
+      expect(screen.getAllByRole('superscript')[0]).toHaveTextContent('2')
+      expect(screen.getByText(/This means that “To Do” appears in two places/)).toBeInTheDocument()
+    })
+
+    it('we add a Work->To Do->Text boss', async () => {
+      await user.keyboard('{Control>}{Enter}{/Control}')
+      await user.type(lastEmptyThought(), 'Text boss')
+      await act(vi.runOnlyPendingTimersAsync)
+      /* thoughts:
+      - Home
+        - To Do
+          - or to not
+      - Work
+        - To Do
+          - Text boss
+      */
+      expect(screen.getByText('Next')).toBeInTheDocument()
+    })
   })
 
-  it('step context examples - we see real-world examples', async () => {
+  describe('step context view open', () => {
+    it('we learn about multiple contexts', async () => {
+      await user.click(screen.getByText('Next'))
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(screen.getByText(/First select "To Do"./)).toBeInTheDocument()
+    })
+
+    it('we select a thought with multiple contexts', async () => {
+      await user.click(screen.getAllByText('To Do').at(-1)!)
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(screen.getByText("Hit Alt + Shift + S to view the current thought's contexts.")).toBeInTheDocument()
+    })
+
+    it('we view contexts of a thought', async () => {
+      await user.keyboard('{Alt>}{Shift>}S{/Shift}{/Alt}')
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(
+        screen.getByText(/We now see all of the contexts in which "To Do" appears, namely "Home" and "Work"./),
+      ).toBeInTheDocument()
+    })
+  })
+
+  it('we see real-world context examples', async () => {
     await user.click(screen.getByText('Next'))
     expect(screen.getByText(/Here are some real-world examples of using contexts in/)).toBeInTheDocument()
-    expect(screen.getByText('View all thoughts related to a particular person, place, or thing.')).toBeInTheDocument()
-    expect(screen.getByText('Keep track of quotations from different sources.')).toBeInTheDocument()
   })
 
-  it('step success - congratulations, hide tutorial after clicking "Finish"', async () => {
-    await user.click(screen.getByText('Next'))
-    expect(screen.getByText(/Congratulations! You have completed Part II of the tutorial./)).toBeInTheDocument()
+  describe('step success', () => {
+    it('we complete the advanced tutorial', async () => {
+      await user.click(screen.getByText('Next'))
+      expect(screen.getByText(/Congratulations! You have completed Part II of the tutorial./)).toBeInTheDocument()
+    })
 
-    user.click(screen.getByText('Finish'))
-    await act(vi.runOnlyPendingTimersAsync)
-
-    expect(() => screen.getByTestId('tutorial-step')).toThrow('Unable to find an element')
+    it('we exit the tutorial', async () => {
+      user.click(screen.getByText('Finish'))
+      await act(vi.runOnlyPendingTimersAsync)
+      expect(() => screen.getByTestId('tutorial-step')).toThrow('Unable to find an element')
+    })
   })
 })
