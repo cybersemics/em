557316666cca84eca721719f3a8f42b909e57938
Esen <s7b5en@gmail.com>
2025-01-24 21:57:18 +0600

    dropcliff

---
+++ src/components/DropCliff.tsx
@@ -0,0 +1,62 @@
+import { css } from '../../styled-system/css'
+import Path from '../@types/Path'
+import { isTouch } from '../browser'
+import { HOME_PATH } from '../constants'
+import head from '../util/head'
+import unroot from '../util/unroot'
+import DropEnd from './DropEnd'
+
+/** Renders a drop target for dropping thoughts after a cliff. */
+const DropCliff = ({
+  cliff,
+  depth,
+  path,
+  isTableCol2,
+  prevWidth = 0,
+  isLastVisible,
+}: {
+  cliff: number
+  depth: number
+  path: Path
+  isTableCol2: boolean
+  prevWidth?: number
+  isLastVisible?: boolean
+}) => {
+  if (cliff < 0) {
+    return Array(-cliff)
+      .fill(0)
+      .map((_, i) => {
+        const pathEnd = -(cliff + i) < path.length ? (path.slice(0, cliff + i) as Path) : HOME_PATH
+        const cliffDepth = unroot(pathEnd).length
+        const depthDiff = cliffDepth - depth
+        const dropEndMarginLeft = isTableCol2 && depthDiff < 0 ? prevWidth : 0
+        return (
+          <div
+            key={'DropEnd-' + head(pathEnd)}
+            className={css({
+              position: 'relative',
+              top: '-0.2em',
+              transition: `left {durations.fast} ease-out`,
+              zIndex: 'subthoughtsDropEnd',
+            })}
+            style={{
+              left: `calc(${cliffDepth - depth}em - ${dropEndMarginLeft}px + ${isTouch ? -1 : 1}px)`,
+            }}
+          >
+            <DropEnd
+              depth={pathEnd.length}
+              path={pathEnd}
+              cliff={cliff}
+              isLastVisible={isLastVisible}
+              // Extend the click area of the drop target when there is nothing below.
+              // The last visible drop-end will always be a dimmed thought at distance 1 (an uncle).
+              // Dimmed thoughts at distance 0 should not be extended, as they are dimmed siblings and sibling descendants that have thoughts below
+              // last={!nextChildId}
+            />
+          </div>
+        )
+      })
+  }
+}
+
+export default DropCliff
