5f73adf7b9cea3a1f4cddf9a1b7c87e9a24e9da2
Heri Setiawan <heri.dehero@gmail.com>
2025-02-12 07:50:58 +0700

    Update test: swapParent action

--- src/actions/__tests__/swapParent.ts
+++ src/actions/__tests__/swapParent.ts
@@ -8,63 +8,64 @@ import importText from '../importText'
 import setCursor from '../setCursor'
 import swapParent from '../swapParent'
 
-describe('swapParent', () => {
-  it('swaps child thought with parent', () => {
-    const text = `
-    - ${HOME_TOKEN}
-      - x
-      - a
-        - b
-          - c`
+it('no-op if cursor is not set', () => {
+  const text = `
+  - x
+  - a
+    - b
+     - c`
 
-    const steps = [
-      importText({ text }),
-      (state: State) => setCursor({ path: contextToPath(state, ['a', 'b'])! })(state),
-      swapParent,
-    ]
+  const steps = [importText({ text }), swapParent]
 
-    const stateNew = reducerFlow(steps)(initialState())
-    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
-    expect(exported).toBe(`- ${HOME_TOKEN}
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+  expect(exported).toBe(`- ${HOME_TOKEN}
+  - x
+  - a
+    - b
+      - c`)
+})
+
+it('no-op if cursor is a root thought', () => {
+  const text = `
+  - x
+  - a
+    - b
+     - c`
+
+  const steps = [
+    importText({ text }),
+    (state: State) => setCursor({ path: contextToPath(state, ['a'])! })(state),
+    swapParent,
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+  expect(exported).toBe(`- ${HOME_TOKEN}
+  - x
+  - a
+    - b
+      - c`)
+})
+
+it('swaps child thought with parent', () => {
+  const text = `
+  - x
+  - a
+    - b
+     - c`
+
+  const steps = [
+    importText({ text }),
+    (state: State) => setCursor({ path: contextToPath(state, ['a', 'b'])! })(state),
+    swapParent,
+  ]
+
+  const stateNew = reducerFlow(steps)(initialState())
+  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+  expect(exported).toBe(`- ${HOME_TOKEN}
   - x
   - b
     - a
       - c`)
-  })
-
-  it('maintains cursor on swapped child', () => {
-    const text = `
-    - ${HOME_TOKEN}
-      - a
-        - b`
-
-    const steps = [
-      importText({ text }),
-      (state: State) => setCursor({ path: contextToPath(state, ['a', 'b'])! })(state),
-      swapParent,
-    ]
-
-    const stateNew = reducerFlow(steps)(initialState())
-    expect(stateNew.cursor).toEqual(contextToPath(stateNew, ['b']))
-  })
-
-  it('does nothing when cursor is at root', () => {
-    const text = `
-    - ${HOME_TOKEN}
-      - a
-        - b`
-
-    const steps = [
-      importText({ text }),
-      (state: State) => setCursor({ path: contextToPath(state, ['a'])! })(state),
-      swapParent,
-    ]
-
-    const stateNew = reducerFlow(steps)(initialState())
-    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
-
-    expect(exported).toBe(`- ${HOME_TOKEN}
-  - a
-    - b`)
-  })
 })
