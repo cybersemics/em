e6957bffb30eb87d563ee6ec2e4ef82bbf1ddd77
Christina Yang <ychristina011@gmail.com>
2025-03-01 22:48:07 -0800

    Add tabs to Help modal, comment out About section

--- src/components/modals/Help.tsx
+++ src/components/modals/Help.tsx
@@ -1,19 +1,16 @@
-import { FC, useCallback, useState } from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { css } from '../../../styled-system/css'
-import { extendTapRecipe, modalTextRecipe } from '../../../styled-system/recipes'
-import Icon from '../../@types/IconType'
+import { modalTextRecipe } from '../../../styled-system/recipes'
 import { closeModalActionCreator as closeModal } from '../../actions/closeModal'
 import { tutorialActionCreator as tutorial } from '../../actions/tutorial'
 import { tutorialStepActionCreator as setTutorialStep } from '../../actions/tutorialStep'
 import { TUTORIAL2_STEP_START, TUTORIAL_STEP_START, TUTORIAL_STEP_SUCCESS } from '../../constants'
 import getSetting from '../../selectors/getSetting'
 import fastClick from '../../util/fastClick'
+import { Tab, TabItem } from '../Tab'
 import ActionButton from './../ActionButton'
 import CommandTable from './../CommandTable'
-import CommandLibraryIcon from './../icons/CommandLibraryIcon'
-import MetaIcon from './../icons/MetaIcon'
-import TutorialsIcon from './../icons/TutorialsIcon'
 import ModalComponent from './ModalComponent'
 
 enum Section {
@@ -23,51 +20,6 @@ enum Section {
   Metaprogramming = 'Metaprogramming',
   Tutorials = 'Tutorials',
 }
-
-/** An item within the help menu. */
-const HelpMenuItem: FC<{ Icon: FC<Icon>; onTap: () => void; title: string; description: string }> = ({
-  Icon,
-  onTap,
-  title,
-  description,
-}) => {
-  const fontSize = useSelector(state => state.fontSize)
-  return (
-    <div {...fastClick(onTap)} className={css({ display: 'flex', marginBottom: '1em' })}>
-      <div className={css({ marginRight: '0.5em', paddingTop: '0.5em' })}>
-        <Icon size={fontSize * 3} />
-      </div>
-      <div>
-        <a className={css({ display: 'inline-block', paddingBottom: '0.25em' })}>{title}</a>
-        <p className={css({ color: 'dim', fontSize: 'md', marginBottom: '1.5em' })}>{description}</p>
-      </div>
-    </div>
-  )
-}
-
-/** Menu of help sections. */
-const HelpMenu = ({ onSelect }: { onSelect: (section: Section) => void }) => (
-  <div>
-    <HelpMenuItem
-      Icon={TutorialsIcon}
-      onTap={() => onSelect(Section.Tutorials)}
-      title='Tutorials'
-      description='Play the interactive tutorials to learn the basics.'
-    />
-    <HelpMenuItem
-      Icon={CommandLibraryIcon}
-      onTap={() => onSelect(Section.CommandLibrary)}
-      title='Command Library'
-      description='View a list of all commands that are available via the toolbar, command palette, keyboard, and mobile gestures.'
-    />
-    <HelpMenuItem
-      Icon={MetaIcon}
-      onTap={() => onSelect(Section.Metaprogramming)}
-      title='Metaprogramming'
-      description={`Peek under the hood at em's unique metaprogramming feature for customizing the appearance and behavior of individual thoughts.`}
-    />
-  </div>
-)
 
 /** Tutorials section. */
 const Tutorials = () => {
@@ -241,150 +193,150 @@ const Metaprogramming = () => {
 }
 
 /** List the credits for em and copyrighted assets. */
-const About = () => {
-  return (
-    <div>
-      {' '}
-      <div className={css({ fontSize: 'sm', marginTop: '2em', fontStyle: 'italic', opacity: 0.7 })}>
-        <div>
-          Context View icon by{' '}
-          <a href='https://thenounproject.com/travisavery/collection/connection-power/?i=2184164'>Travis Avery</a> from
-          the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Export icon by{' '}
-          <a href='https://www.flaticon.com/authors/those-icons' title='Those Icons'>
-            Those Icons
-          </a>{' '}
-          from{' '}
-          <a href='https://www.flaticon.com/' title='Flaticon'>
-            www.flaticon.com
-          </a>
-        </div>
-        <div>
-          Export icon by <a href='https://thenounproject.com/tgtdesign18'>Mahesh Keshvala</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Feedback icon by <a href='https://thenounproject.com/deanmtam'>Dean Mocha</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Hidden Thoughts icon by <a href='https://thenounproject.com/search/?q=show%20hidden&i=1791510'>Joyce Lau</a>{' '}
-          from the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Indent icons by{' '}
-          <a href='https://www.flaticon.com/authors/bqlqn' title='bqlqn'>
-            bqlqn
-          </a>{' '}
-          from{' '}
-          <a href='https://www.flaticon.com/' title='Flaticon'>
-            flaticon.com
-          </a>
-        </div>
-        <div>
-          Note icon by <a href='https://thenounproject.com/iconsphere/collection/populars/?i=2321491'>iconsphere</a>{' '}
-          from the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Pin icon by <a href='https://thenounproject.com/search/?q=%22pin%20many%22&i=496735'>Hea Poh Lin</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Prose View icon by <a href='https://thenounproject.com/coquet_adrien'>Adrien Coquet</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Search icon by <a href='https://icons8.com/icon/7695/search'>Icons8</a>
-        </div>
-        <div>
-          Subcategorize icons by <a href='https://thenounproject.com/term/circuit/1685927/'>Hare Krishna</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Table icon by{' '}
-          <a href='https://thenounproject.com/icon54app/collection/table-light-icon-set/?i=2762107'>icon 54</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Undo and Redo Icons by{' '}
-          <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>
-            Pixel perfect
-          </a>{' '}
-          from{' '}
-          <a href='https://www.flaticon.com/' title='Flaticon'>
-            {' '}
-            www.flaticon.com
-          </a>
-        </div>
-        <div>
-          Share icon by <a href='https://thenounproject.com/term/share/1058861/'>Тимур Минвалеев</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Settings icon by <a href='https://thenounproject.com/icon/settings-5241749/'>Parisa Qolbi</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Gift icon by <a href='https://thenounproject.com/search/?q=gift&i=2221484'> Sarote Impheng</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Copy to clipboard icon by{' '}
-          <a href='https://thenounproject.com/search/?q=copy+to+clipboard&i=1669410'>Hare Krishna</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Checkmark icon by <a href='https://thenounproject.com/search/?q=checkmark&i=870288'>arif fajar yulianto</a>{' '}
-          from the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Delete icon by <a href='https://thenounproject.com/icon/trash-1371974/'>Clea Doltz</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Gesture icon by <a href='https://thenounproject.com/icon/gesture-2211316/'>Adrien Coquet</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Download icon by <a href='https://thenounproject.com/icon/download-1000840/'>DinosoftLab</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Command Center icon by <a href='https://thenounproject.com/icon/one-finger-touch-68664/'>iconsmind.com</a>{' '}
-          from the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Teach icon by <a href='https://thenounproject.com/icon/teach-4200138/'>Alzam</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Metaprogramming icon by <a href='https://thenounproject.com/icon/inspect-element-4199164/'>Ary Prasetyo</a>{' '}
-          from the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Strikethrough icon by <a href='https://thenounproject.com/icon/strikethrough-1107500/'>Ary Prasetyo</a> from
-          the <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Check icon by <a href='https://thenounproject.com/icon/check-3669824/'>Media Creative</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-        <div>
-          Email icon by <a href='https://thenounproject.com/icon/email-1560074/'>il Capitano</a> from the{' '}
-          <a href='https://thenounproject.com'>Noun Project</a>
-        </div>
-      </div>
-    </div>
-  )
-}
+// const About = () => {
+//   return (
+//     <div>
+//       {' '}
+//       <div className={css({ fontSize: 'sm', marginTop: '2em', fontStyle: 'italic', opacity: 0.7 })}>
+//         <div>
+//           Context View icon by{' '}
+//           <a href='https://thenounproject.com/travisavery/collection/connection-power/?i=2184164'>Travis Avery</a> from
+//           the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Export icon by{' '}
+//           <a href='https://www.flaticon.com/authors/those-icons' title='Those Icons'>
+//             Those Icons
+//           </a>{' '}
+//           from{' '}
+//           <a href='https://www.flaticon.com/' title='Flaticon'>
+//             www.flaticon.com
+//           </a>
+//         </div>
+//         <div>
+//           Export icon by <a href='https://thenounproject.com/tgtdesign18'>Mahesh Keshvala</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Feedback icon by <a href='https://thenounproject.com/deanmtam'>Dean Mocha</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Hidden Thoughts icon by <a href='https://thenounproject.com/search/?q=show%20hidden&i=1791510'>Joyce Lau</a>{' '}
+//           from the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Indent icons by{' '}
+//           <a href='https://www.flaticon.com/authors/bqlqn' title='bqlqn'>
+//             bqlqn
+//           </a>{' '}
+//           from{' '}
+//           <a href='https://www.flaticon.com/' title='Flaticon'>
+//             flaticon.com
+//           </a>
+//         </div>
+//         <div>
+//           Note icon by <a href='https://thenounproject.com/iconsphere/collection/populars/?i=2321491'>iconsphere</a>{' '}
+//           from the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Pin icon by <a href='https://thenounproject.com/search/?q=%22pin%20many%22&i=496735'>Hea Poh Lin</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Prose View icon by <a href='https://thenounproject.com/coquet_adrien'>Adrien Coquet</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Search icon by <a href='https://icons8.com/icon/7695/search'>Icons8</a>
+//         </div>
+//         <div>
+//           Subcategorize icons by <a href='https://thenounproject.com/term/circuit/1685927/'>Hare Krishna</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Table icon by{' '}
+//           <a href='https://thenounproject.com/icon54app/collection/table-light-icon-set/?i=2762107'>icon 54</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Undo and Redo Icons by{' '}
+//           <a href='https://www.flaticon.com/authors/pixel-perfect' title='Pixel perfect'>
+//             Pixel perfect
+//           </a>{' '}
+//           from{' '}
+//           <a href='https://www.flaticon.com/' title='Flaticon'>
+//             {' '}
+//             www.flaticon.com
+//           </a>
+//         </div>
+//         <div>
+//           Share icon by <a href='https://thenounproject.com/term/share/1058861/'>Тимур Минвалеев</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Settings icon by <a href='https://thenounproject.com/icon/settings-5241749/'>Parisa Qolbi</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Gift icon by <a href='https://thenounproject.com/search/?q=gift&i=2221484'> Sarote Impheng</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Copy to clipboard icon by{' '}
+//           <a href='https://thenounproject.com/search/?q=copy+to+clipboard&i=1669410'>Hare Krishna</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Checkmark icon by <a href='https://thenounproject.com/search/?q=checkmark&i=870288'>arif fajar yulianto</a>{' '}
+//           from the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Delete icon by <a href='https://thenounproject.com/icon/trash-1371974/'>Clea Doltz</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Gesture icon by <a href='https://thenounproject.com/icon/gesture-2211316/'>Adrien Coquet</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Download icon by <a href='https://thenounproject.com/icon/download-1000840/'>DinosoftLab</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Command Center icon by <a href='https://thenounproject.com/icon/one-finger-touch-68664/'>iconsmind.com</a>{' '}
+//           from the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Teach icon by <a href='https://thenounproject.com/icon/teach-4200138/'>Alzam</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Metaprogramming icon by <a href='https://thenounproject.com/icon/inspect-element-4199164/'>Ary Prasetyo</a>{' '}
+//           from the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Strikethrough icon by <a href='https://thenounproject.com/icon/strikethrough-1107500/'>Ary Prasetyo</a> from
+//           the <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Check icon by <a href='https://thenounproject.com/icon/check-3669824/'>Media Creative</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//         <div>
+//           Email icon by <a href='https://thenounproject.com/icon/email-1560074/'>il Capitano</a> from the{' '}
+//           <a href='https://thenounproject.com'>Noun Project</a>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
 /** A modal that offers links to the tutorial, a list of commands, and other helpful things. */
 const ModalHelp = () => {
-  const [section, setSection] = useState(Section.Menu)
+  const [section, setSection] = useState(Section.Tutorials)
   const fontSize = useSelector(state => state.fontSize)
-  const back = useCallback(() => setSection(Section.Menu), [])
+  const showDot = useSelector(state => !state.storageCache?.tutorialComplete)
   return (
     <ModalComponent
       id='help'
@@ -392,38 +344,17 @@ const ModalHelp = () => {
       actions={({ close }) => <ActionButton key='close' title='Close' {...fastClick(() => close())} />}
       style={{ fontSize }}
     >
-      {section === Section.Menu ? (
-        <HelpMenu onSelect={setSection} />
-      ) : (
-        <span className={css({ fontSize: 'sm' })}>
-          &lt;{' '}
-          <a {...fastClick(back)} className={extendTapRecipe()}>
-            Back
-          </a>
-        </span>
-      )}
-
-      {section === Section.Tutorials ? (
-        <Tutorials />
-      ) : section === Section.CommandLibrary ? (
-        <CommandCenter />
-      ) : section === Section.Metaprogramming ? (
-        <Metaprogramming />
-      ) : section === Section.About ? (
-        <About />
-      ) : null}
-
-      {
-        // TODO: Remove Section.Tutorials condition once it has more content.
-        section !== Section.Menu && section !== Section.Tutorials && (
-          <div className={css({ fontSize: 'sm', marginTop: '2em' })}>
-            &lt;{' '}
-            <a {...fastClick(back)} className={extendTapRecipe()}>
-              Back
-            </a>
-          </div>
-        )
-      }
+      <Tab currentTab={section} onTabChange={setSection}>
+        <TabItem value={Section.Tutorials} label='Tutorials' showDot={showDot}>
+          <Tutorials />
+        </TabItem>
+        <TabItem value={Section.CommandLibrary} label='Command Library'>
+          <CommandCenter />
+        </TabItem>
+        <TabItem value={Section.Metaprogramming} label='Metaprogramming'>
+          <Metaprogramming />
+        </TabItem>
+      </Tab>
     </ModalComponent>
   )
 }
