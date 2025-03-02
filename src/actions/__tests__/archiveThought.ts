7c248b55946eaee8ff86870f53385d8bcd19dfe8
Raine Revere <raine@cybersemics.org>
2025-03-02 16:14:04 +0000

    archiveThought test: Group normal view tests in describe.

--- src/actions/__tests__/archiveThought.ts
+++ src/actions/__tests__/archiveThought.ts
@@ -14,164 +14,179 @@ import pathToContext from '../../util/pathToContext'
 import reducerFlow from '../../util/reducerFlow'
 import importText from '../importText'
 
-it('archive a thought', () => {
-  const steps = [newThought('a'), newThought('b'), archiveThought({})]
+describe('normal view', () => {
+  it('archive a thought', () => {
+    const steps = [newThought('a'), newThought('b'), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - =archive
     - b
   - a`)
-})
+  })
 
-it('deduplicate archived thoughts with the same value', () => {
-  const steps = [newThought('a'), newThought('b'), newThought('b'), archiveThought({}), archiveThought({})]
+  it('deduplicate archived thoughts with the same value', () => {
+    const steps = [newThought('a'), newThought('b'), newThought('b'), archiveThought({}), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - =archive
     - b
   - a`)
-})
+  })
 
-it('do nothing if there is no cursor', () => {
-  const steps = [newThought('a'), setCursor(null), archiveThought({})]
+  it('do nothing if there is no cursor', () => {
+    const steps = [newThought('a'), setCursor(null), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a`)
-})
+  })
 
-it('move to top of archive', () => {
-  const steps = [newThought('a'), newThought('b'), newThought('c'), archiveThought({}), archiveThought({})]
+  it('move to top of archive', () => {
+    const steps = [newThought('a'), newThought('b'), newThought('c'), archiveThought({}), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - =archive
     - b
     - c
   - a`)
-})
+  })
 
-it('permanently delete empty thought', () => {
-  const steps = [newThought('a'), newThought(''), archiveThought({})]
+  it('permanently delete empty thought', () => {
+    const steps = [newThought('a'), newThought(''), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a`)
-})
+  })
 
-it('permanently delete thought from archive', () => {
-  const steps = [newThought('a'), newThought('b'), archiveThought({}), setCursor(['=archive', 'b']), archiveThought({})]
+  it('permanently delete thought from archive', () => {
+    const steps = [
+      newThought('a'),
+      newThought('b'),
+      archiveThought({}),
+      setCursor(['=archive', 'b']),
+      archiveThought({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - =archive
   - a`)
-})
+  })
 
-it('permanently delete archive', () => {
-  const steps = [newThought('a'), newThought('b'), archiveThought({}), setCursor(['=archive']), archiveThought({})]
+  it('permanently delete archive', () => {
+    const steps = [newThought('a'), newThought('b'), archiveThought({}), setCursor(['=archive']), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - a`)
 
-  // ensure =archive is removed from lexemeIndex
-  expect(getContexts(stateNew, '=archive')).toHaveLength(0)
-})
+    // ensure =archive is removed from lexemeIndex
+    expect(getContexts(stateNew, '=archive')).toHaveLength(0)
+  })
 
-it('permanently delete archive with descendants', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('b'),
-    setCursor(['a']),
-    archiveThought({}),
-    setCursor(['=archive']),
-    archiveThought({}),
-  ]
+  it('permanently delete archive with descendants', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('b'),
+      setCursor(['a']),
+      archiveThought({}),
+      setCursor(['=archive']),
+      archiveThought({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}`)
+    expect(exported).toBe(`- ${HOME_TOKEN}`)
 
-  // ensure =archive is removed from lexemeIndex
-  expect(getContexts(stateNew, '=archive')).toHaveLength(0)
+    // ensure =archive is removed from lexemeIndex
+    expect(getContexts(stateNew, '=archive')).toHaveLength(0)
 
-  // ensure descendants are remvoed from lexemeIndex
-  expect(getContexts(stateNew, 'a')).toHaveLength(0)
-})
+    // ensure descendants are remvoed from lexemeIndex
+    expect(getContexts(stateNew, 'a')).toHaveLength(0)
+  })
 
-it('cursor should move to prev sibling', () => {
-  const steps = [newThought('a'), newSubthought('a1'), newThought('a2'), newThought('a3'), cursorUp, archiveThought({})]
+  it('cursor should move to prev sibling', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('a1'),
+      newThought('a2'),
+      newThought('a3'),
+      cursorUp,
+      archiveThought({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  expectPathToEqual(stateNew, stateNew.cursor, [
-    { value: 'a', rank: 0 },
-    { value: 'a1', rank: 0 },
-  ])
-})
+    const stateNew = reducerFlow(steps)(initialState())
+    expectPathToEqual(stateNew, stateNew.cursor, [
+      { value: 'a', rank: 0 },
+      { value: 'a1', rank: 0 },
+    ])
+  })
 
-it('cursor should move to next sibling if there is no prev sibling', () => {
-  const steps = [
-    newThought('a'),
-    newSubthought('a1'),
-    newThought('a2'),
-    newThought('a3'),
-    cursorUp,
-    cursorUp,
-    archiveThought({}),
-  ]
+  it('cursor should move to next sibling if there is no prev sibling', () => {
+    const steps = [
+      newThought('a'),
+      newSubthought('a1'),
+      newThought('a2'),
+      newThought('a3'),
+      cursorUp,
+      cursorUp,
+      archiveThought({}),
+    ]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, [
-    { value: 'a', rank: 0 },
-    { value: 'a2', rank: 1 },
-  ])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [
+      { value: 'a', rank: 0 },
+      { value: 'a2', rank: 1 },
+    ])
+  })
 
-it('cursor should move to parent if the deleted thought has no siblings', () => {
-  const steps = [newThought('a'), newSubthought('a1'), archiveThought({})]
+  it('cursor should move to parent if the deleted thought has no siblings', () => {
+    const steps = [newThought('a'), newSubthought('a1'), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a', rank: 0 }])
-})
+    expectPathToEqual(stateNew, stateNew.cursor, [{ value: 'a', rank: 0 }])
+  })
 
-it('cursor should be removed if the last thought is deleted', () => {
-  const steps = [newThought('a'), archiveThought({})]
+  it('cursor should be removed if the last thought is deleted', () => {
+    const steps = [newThought('a'), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
+    const stateNew = reducerFlow(steps)(initialState())
 
-  expect(stateNew.cursor).toBe(null)
-})
+    expect(stateNew.cursor).toBe(null)
+  })
 
-it('empty thought should be archived if it has descendants', () => {
-  const steps = [newThought('a'), newThought(''), newSubthought('b'), setCursor(['']), archiveThought({})]
+  it('empty thought should be archived if it has descendants', () => {
+    const steps = [newThought('a'), newThought(''), newSubthought('b'), setCursor(['']), archiveThought({})]
 
-  const stateNew = reducerFlow(steps)(initialState())
-  const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
+    const stateNew = reducerFlow(steps)(initialState())
+    const exported = exportContext(stateNew, [HOME_TOKEN], 'text/plain')
 
-  expect(exported).toBe(`- ${HOME_TOKEN}
+    expect(exported).toBe(`- ${HOME_TOKEN}
   - =archive
     - ${'' /* prevent trim_trailing_whitespace */}
       - b
   - a`)
+  })
 })
 
 describe('context view', () => {
