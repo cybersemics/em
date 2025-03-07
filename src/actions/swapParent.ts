770b54000a55e43a2bff22041efe709332e35530
Heri Setiawan <heri.dehero@gmail.com>
2025-03-07 17:56:27 +0700

    Fix unit test: swap parent action

--- src/actions/swapParent.ts
+++ src/actions/swapParent.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import State from '../@types/State'
 import ThoughtId from '../@types/ThoughtId'
 import Thunk from '../@types/Thunk'
@@ -7,6 +8,7 @@ import getThoughtById from '../selectors/getThoughtById'
 import appendToPath from '../util/appendToPath'
 import head from '../util/head'
 import parentOf from '../util/parentOf'
+import reducerFlow from '../util/reducerFlow'
 
 /** Swaps the current cursor's thought with its parent by moving nodes. */
 const swapParent = (state: State) => {
@@ -28,40 +30,47 @@ const swapParent = (state: State) => {
   if (!childThought || !parentThought) return state
 
   // Get children of the child thought that will be moved
-  const childChildren = getChildrenRanked(state, childId)
-
-  // First move the child to replace its parent's position
-  let stateNew = moveThought(state, {
-    oldPath: cursor,
-    newPath: parent,
-    newRank: parentThought.rank,
+  const childChildren = getChildrenRanked(state, childId).filter(child => {
+    return child.rank < parentThought.rank
   })
 
-  // Then move the parent under the child
-  stateNew = moveThought(stateNew, {
-    oldPath: parent,
-    newPath: appendToPath([childId], parentId),
-    newRank: 0,
-  })
+  // Get the grandparent path (parent of parent)
+  const grandparent = parentOf(parent)
 
-  // Move all child's children under the parent's new position
-  stateNew = childChildren.reduce((accState, childChild) => {
-    return moveThought(accState, {
-      oldPath: appendToPath(cursor, childChild.id),
-      newPath: appendToPath([childId, parentId], childChild.id),
-      newRank: childChild.rank,
-    })
-  }, stateNew)
+  return reducerFlow([
+    // First move the child to replace its parent's position
+    state => moveThought(state, {
+      oldPath: cursor,
+      newPath: parent,
+      newRank: parentThought.rank,
+    }),
 
-  // Keep cursor on the child at its new position
-  return {
-    ...stateNew,
-    cursor: [childId] as [ThoughtId, ...ThoughtId[]],
-    offset: childThought.value.length,
-  }
+    // Then move the parent under the child
+    state => moveThought(state, {
+      oldPath: parent,
+      newPath: appendToPath([childId], parentId),
+      newRank: 0,
+    }),
+
+    // Move all child's children under the parent's new position
+    state => childChildren.reduce((accState, childChild) => {
+      return moveThought(accState, {
+        oldPath: appendToPath(cursor, childChild.id),
+        newPath: appendToPath([...grandparent, childId, parentId], childChild.id),
+        newRank: childChild.rank,
+      })
+    }, state),
+
+    // Keep cursor on the child at its new position, preserving the full path
+    state => ({
+      ...state,
+      cursor: [...grandparent, childId, parentId] as [ThoughtId, ...ThoughtId[]],
+      offset: childThought.value.length,
+    }),
+  ])(state)
 }
 
 /** Action-creator for swapParent. */
 export const swapParentActionCreator = (): Thunk => dispatch => dispatch({ type: 'swapParent' })
 
-export default swapParent
+export default _.curryRight(swapParent)
--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -589,6 +589,7 @@ const Editable = ({
 
   return (
     <ContentEditable
+      data-rank={rank}
       disabled={disabled}
       innerRef={contentRef}
       aria-label={'editable-' + head(path)}
