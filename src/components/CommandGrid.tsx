fc0c702b41334effaf5e42f561c828189c4926fe
Martyna Runkowska <martyna@cosy.land>
2025-03-12 00:01:44 +0000

    prettier

--- src/components/CommandGrid.tsx
+++ src/components/CommandGrid.tsx
@@ -166,18 +166,18 @@ const CommandGrid = ({
   return (
     <div>
       <div
-      className={css({
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        gap: '5px',
-      })}
+        className={css({
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          gap: '5px',
+        })}
       >
         <SearchCommands onInput={setSearch} />
         <SortButton onSortChange={setSortOrder} />
       </div>
       <div className={css({ textAlign: 'left' })}>
-      {(() => {
+        {(() => {
           if (search) {
             return (
               <CommandsGroup
@@ -211,14 +211,14 @@ const CommandGrid = ({
             })
           } else if (sortOrder === 'alphabetical') {
             return (
-                <CommandsGroup
-                  title={'All Commands'}
-                  commands={commands}
-                  selectedCommand={selectedCommand}
-                  customize={customize}
-                  onSelect={onSelect}
-                  isGrid={true}
-                />
+              <CommandsGroup
+                title={'All Commands'}
+                commands={commands}
+                selectedCommand={selectedCommand}
+                customize={customize}
+                onSelect={onSelect}
+                isGrid={true}
+              />
             )
           }
         })()}
