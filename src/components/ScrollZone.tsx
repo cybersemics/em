d06901bdc5793b1fae9643defce9ef9a448f3ae1
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2025-03-26 17:17:54 +0000

    fix: show scroll zone while cheatsheet is active
    To fix a bug where the scroll zone would apply to the cheatsheet, we hid the scroll zone when the cheatsheet was active. But this is actually unnecessary as the bug was found and fixed elsewhere – so this commit reverses the change and brings back the scroll zone visually in the cheatsheet.

--- src/components/ScrollZone.tsx
+++ src/components/ScrollZone.tsx
@@ -9,9 +9,7 @@ import viewportStore from '../stores/viewport'
 const ScrollZone = ({ leftHanded }: { leftHanded?: boolean } = {}) => {
   const scrollTop = useScrollTop()
   const scrollZoneWidth = viewportStore.useSelector(state => state.scrollZoneWidth)
-  const hideScrollZone = useSelector(
-    state => state.showModal || state.showGestureCheatsheet || getUserSetting(state, Settings.hideScrollZone),
-  )
+  const hideScrollZone = useSelector(state => state.showModal || getUserSetting(state, Settings.hideScrollZone))
   if (hideScrollZone) return null
 
   return (
