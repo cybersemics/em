05b55863e9890212c65e6ee37297f0c2a5300ec3
Raine Revere <raine@cybersemics.org>
2025-03-29 18:45:14 +0000

    Remove unneeded react-dnd type definition override.

--- src/@types/react-dnd.d.ts
+++
@@ -1,6 +0,0 @@
-import { DndProviderProps } from 'react-dnd'
-
-declare module 'react-dnd' {
-  // eslint-disable-next-line import/prefer-default-export
-  export declare const DndProvider: React.FC<DndProviderProps<any, any>>
-}
