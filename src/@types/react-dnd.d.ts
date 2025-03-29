eee13d7012d4a62172b0aba8ebae3afc79ec11a5
Raine Revere <raine@cybersemics.org>
2025-03-29 18:44:39 +0000

    react-dnd: simplify type definition.

--- src/@types/react-dnd.d.ts
+++ src/@types/react-dnd.d.ts
@@ -1,6 +1,6 @@
 import { DndProviderProps } from 'react-dnd'
 
 declare module 'react-dnd' {
-  // eslint-disable-next-line import/prefer-default-export, @typescript-eslint/no-explicit-any
-  export declare const DndProvider: React.FC<DndProviderProps<any, any> & { children?: React.ReactNode }>
+  // eslint-disable-next-line import/prefer-default-export
+  export declare const DndProvider: React.FC<DndProviderProps<any, any>>
 }
