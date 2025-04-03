bf6a88082246b6bb5296f8120a51db7ab29c92c8
Raine Revere <raine@cybersemics.org>
2025-04-03 13:31:13 +0000

    Attempt to mitigate failing render-thoughts puppeteer test.
    Test intermittently fails with small differences in 'b'.
    Tested manually with navigator.webdriver = true and 'b' renders at the correct opacity in the next frame, without any animation, so I do not know why this fails.
    Example failed test run: https://github.com/cybersemics/em/actions/runs/14236307211

--- src/e2e/puppeteer/__tests__/render-thoughts.ts
+++ src/e2e/puppeteer/__tests__/render-thoughts.ts
@@ -11,6 +11,7 @@ import press from '../helpers/press'
 import screenshot from '../helpers/screenshot'
 import scroll from '../helpers/scroll'
 import setTheme from '../helpers/setTheme'
+import waitForRender from '../helpers/waitForRender'
 
 expect.extend({
   toMatchImageSnapshot: configureSnapshots({ fileName: path.basename(__filename).replace('.ts', '') }),
@@ -28,6 +29,11 @@ const superscriptSnapshot = async () => {
   `)
 
   await press('ArrowUp')
+
+  // TODO: Test intermittently fails with small differences in 'b'.
+  // Tested manually with navigator.webdriver = true and 'b' renders at the correct opacity in the next frame, without any animation, so I do not know why this fails.
+  // Example failed test run: https://github.com/cybersemics/em/actions/runs/14236307211
+  await waitForRender()
 
   return screenshot()
 }
@@ -189,6 +195,11 @@ describe('multiline', () => {
       `)
 
     await press('ArrowUp')
+
+    // TODO: Test intermittently fails with small differences in 'b'.
+    // Tested manually with navigator.webdriver = true and 'b' renders at the correct opacity in the next frame, without any animation, so I do not know why this fails.
+    // Example failed test run: https://github.com/cybersemics/em/actions/runs/14236307211
+    await waitForRender()
 
     const image = await screenshot()
     expect(image).toMatchImageSnapshot()
