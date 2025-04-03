3c46df70190a1c977e9da92c2b9c5d7aae0d0b91
Christina Yang <ychristina011@gmail.com>
2025-04-02 23:19:49 -0700

    Refactor pickers: Create shared Popover component (#2877)
    * refactor: create popover shared component, and make LetterCasePicker use it.
    
    * refactor: make ColorPicker use Popover component
    
    * refactor: SortPicker to use Popover component
    
    * style: adjust line spacing
    
    * fix comment
    
    * update comment for consistency in TextColorWithColorPicker

--- src/components/icons/SortWithPicker.tsx
+++ src/components/icons/SortWithPicker.tsx
@@ -1,40 +1,16 @@
-import { useRef } from 'react'
 import { useSelector } from 'react-redux'
-import { css } from '../../../styled-system/css'
 import IconType from '../../@types/IconType'
-import FadeTransition from '../FadeTransition'
 import SortPicker from '../SortPicker'
 import SortIcon from './Sort'
 
-/** Sort Icon Component with popup SortPicker. */
+/** Sort Icon Component with popup Picker. */
 const SortWithPicker = ({ size = 18, style, cssRaw }: IconType) => {
   const showSortPicker = useSelector(state => state.showSortPicker)
-  const toolbarPopupRef = useRef<HTMLDivElement>(null)
 
   return (
     <div>
       <SortIcon size={size} style={style} cssRaw={cssRaw} animated={showSortPicker} />
-      <FadeTransition duration='fast' nodeRef={toolbarPopupRef} in={showSortPicker} exit={false} unmountOnExit>
-        <div
-          ref={toolbarPopupRef}
-          className={css({
-            position: 'relative',
-            zIndex: 'stack',
-            // position fixed or absolute causes the SortPicker to get clipped by the toolbar's overflow-x: scroll
-            // ideally we want overflow-x:scroll and overflow-y:visible, but Safari does not differing allow overflow-x and overflow-y
-            // instead, keep position:static but set the width to 0
-            // this will increase the height of the toolbar so the SortPicker does not get clipped without taking up horizontal space
-            width: 0,
-          })}
-          style={{
-            // eyeballing it to get font sizes 14–24 to look right
-            left: (size * (size + 90)) / 200 + 600 / (size * size),
-            marginTop: size * 1.2 - 10,
-          }}
-        >
-          <SortPicker fontSize={size} cssRaw={css.raw({ transform: `translate(-50%)` })} />
-        </div>
-      </FadeTransition>
+      <SortPicker size={size} />
     </div>
   )
 }
--- src/components/icons/TextColorWithColorPicker.tsx
+++ src/components/icons/TextColorWithColorPicker.tsx
@@ -1,16 +1,12 @@
-import { useRef } from 'react'
 import { useSelector } from 'react-redux'
-import { css } from '../../../styled-system/css'
 import { token } from '../../../styled-system/tokens'
 import IconType from '../../@types/IconType'
 import ColorPicker from '../ColorPicker'
-import FadeTransition from '../FadeTransition'
 import TextColorIcon from './TextColor'
 
-/** Text Color Icon with popup ColorPicker. */
+/** Text Color Icon with popup Picker. */
 const TextColorWithColorPicker = ({ size = 18, style, cssRaw }: IconType) => {
   const showColorPicker = useSelector(state => state.showColorPicker)
-  const toolbarPopupRef = useRef<HTMLDivElement>(null)
 
   return (
     <div>
@@ -21,27 +17,7 @@ const TextColorWithColorPicker = ({ size = 18, style, cssRaw }: IconType) => {
         animated={showColorPicker}
         fill={style?.fill || token('colors.fg')}
       />
-      <FadeTransition duration='fast' nodeRef={toolbarPopupRef} in={showColorPicker} exit={false} unmountOnExit>
-        <div
-          ref={toolbarPopupRef}
-          className={css({
-            zIndex: 'stack',
-            position: 'relative',
-            // position fixed or absolute causes the ColorPicker to get clipped by the toolbar's overflow-x: scroll
-            // ideally we want overflow-x:scroll and overflow-y:visible, but Safari does not differing allow overflow-x and overflow-y
-            // instead, keep position:static but set the width to 0
-            // this will increase the height of the toolbar so the ColorPicker does not get clipped without taking up horizontal space
-            width: 0,
-          })}
-          style={{
-            // eyeballing it to get font sizes 14–24 to look right
-            left: (size * (size + 90)) / 200 + 600 / (size * size),
-            marginTop: size * 1.2 - 10,
-          }}
-        >
-          <ColorPicker fontSize={size} cssRaw={css.raw({ transform: `translate(-50%)` })} />
-        </div>
-      </FadeTransition>
+      <ColorPicker size={size} />
     </div>
   )
 }
