e1c9b7c31d35f1d526bf53900b398a8f70813b54
Cindy Michelle <cindy@kodefox.com>
2025-04-04 02:38:12 +0700

    fix table view cursor position

--- src/components/BulletCursorOverlay.tsx
+++ src/components/BulletCursorOverlay.tsx
@@ -1,11 +1,25 @@
 import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { css } from '../../styled-system/css'
+import SimplePath from '../@types/SimplePath'
 import { isSafari, isTouch, isiPhone } from '../browser'
+import attributeEquals from '../selectors/attributeEquals'
+import rootedParentOf from '../selectors/rootedParentOf'
+import head from '../util/head'
 
 const isIOSSafari: boolean = isTouch && isiPhone && isSafari()
 /** A larger circle that surrounds the bullet of the cursor thought. */
-const BulletCursorOverlay = ({ isCursorActive, x, y }: { isCursorActive?: boolean; x?: number; y?: number }) => {
+const BulletCursorOverlay = ({
+  isCursorActive,
+  x,
+  y,
+  simplePath,
+}: {
+  isCursorActive?: boolean
+  x?: number
+  y?: number
+  simplePath?: SimplePath
+}) => {
   const svgElement = useRef<SVGSVGElement>(null)
 
   const fontSize = useSelector(state => state.fontSize)
@@ -13,7 +27,15 @@ const BulletCursorOverlay = ({ isCursorActive, x, y }: { isCursorActive?: boolea
 
   const bulletOverlayRadius = isIOSSafari ? 300 : 245
 
-  const translateX = (x || 0) + lineHeight * 0.317 - lineHeight - (11 - (fontSize - 9) * 0.5)
+  const isTableCol1 = useSelector(state =>
+    simplePath ? attributeEquals(state, head(rootedParentOf(state, simplePath)), '=view', 'Table') : false,
+  )
+
+  // calculate position of bullet for different font sizes
+  // Table column 1 needs more space between the bullet and thought for some reason
+  const width = 11 - (fontSize - 9) * 0.5 + (isTableCol1 ? fontSize / 4 : 0)
+  const translateX = (x || 0) + lineHeight * 0.317 - lineHeight - (isTableCol1 ? 0 : width)
+  // const translateX = (x || 0) + lineHeight * 0.317 - lineHeight  + (-extendClickWidth + -width + extendClickWidth) + width
   const translateY = (y || 0) + fontSize * (isIOSSafari ? 0.2 : 0.3)
 
   return (
