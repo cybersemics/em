fbdd0c192f7ba02b4601ed78e1eb33b805a3b9b8
Cindy Michelle <cindy@kodefox.com>
2025-04-05 03:39:16 +0700

    fix overlay position when table view and context view active

--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -286,6 +286,7 @@ const LayoutTree = () => {
             x={activeThought?.x}
             y={activeThought?.y}
             simplePath={activeThought?.simplePath}
+            path={activeThought?.path}
           />
           {treeThoughtsPositioned.map((thought, index) => (
             <TreeNode
