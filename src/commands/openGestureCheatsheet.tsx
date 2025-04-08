514083b3e8933f9db86c0a35a7f98a622b8b5fe5
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2025-04-09 00:10:37 +0100

    fix: don't scroll to top when opening the cheatsheet

--- src/commands/openGestureCheatsheet.tsx
+++ src/commands/openGestureCheatsheet.tsx
@@ -1,19 +1,14 @@
 import Command from '../@types/Command'
 import { toggleGestureCheatsheetActionCreator } from '../actions/toggleGestureCheatsheet'
 import GestureCheatsheetIcon from '../components/icons/GestureCheatsheetIcon'
-import scrollTo from '../device/scrollTo'
 import isDocumentEditable from '../util/isDocumentEditable'
 
 /**
  * Execute the openGestureCheatsheet command by calling the
  * toggleGestureCheatsheetActionCreator.
- * We scroll to the top of the page to ensure the cheatsheet is visible.
- * Inside the cheatsheet, we lock the scroll of the page to ensure
- * the cheatsheet is the only element that can be scrolled.
  */
 const exec: Command['exec'] = dispatch => {
   dispatch(toggleGestureCheatsheetActionCreator({ value: true }))
-  scrollTo('top')
 }
 
 const openGestureCheatsheetCommand: Command = {
