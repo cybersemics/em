38a57861287900021acf98aaa66394d1d9dc48e8
Christina Yang <ychristina011@gmail.com>
2025-04-08 07:07:52 -0700

    don't allow tips and alerts to stack

--- src/actions/showTip.ts
+++ src/actions/showTip.ts
@@ -2,18 +2,22 @@ import State from '../@types/State'
 import Thunk from '../@types/Thunk'
 import TipId from '../@types/TipId'
 import { registerActionMetadata } from '../util/actionMetadata.registry'
+import { alertActionCreator } from './alert'
 
 /** Shows a popup with a tip. */
 const showTip = (state: State, { tip }: { tip: TipId }): State => ({
   ...state,
-  tips: state.tips.includes(tip) ? state.tips : [...state.tips, tip],
+  // for now, don't allow stacking tips
+  tips: [tip],
 })
 
 /** Action-creator for showTip. */
 export const showTipActionCreator =
   (payload: Parameters<typeof showTip>[1]): Thunk =>
-  dispatch =>
+  dispatch => {
     dispatch({ type: 'showTip', ...payload })
+    dispatch(alertActionCreator(null))
+  }
 
 export default showTip
 
