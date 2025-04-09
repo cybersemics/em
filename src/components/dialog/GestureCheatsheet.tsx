3c744a02315bd3028f1f00d9a4107418ac579b3c
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2025-04-09 04:40:35 +0100

    refactor: replace manual animation with FadeTransition component

--- src/components/dialog/GestureCheatsheet.tsx
+++ src/components/dialog/GestureCheatsheet.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { css } from '../../../styled-system/css'
 import { toggleGestureCheatsheetActionCreator } from '../../actions/toggleGestureCheatsheet'
 import allowScroll from '../../device/disableScroll'
-import durations from '../../util/durations'
 import CommandTable from '../CommandTable'
+import FadeTransition from '../FadeTransition'
 import Dialog from './Dialog'
 import DialogContent from './DialogContent'
 import DialogTitle from './DialogTitle'
@@ -15,25 +15,13 @@ import DialogTitle from './DialogTitle'
 const GestureCheatsheet: React.FC = () => {
   const dispatch = useDispatch()
   const isOpen = useSelector(state => state.showGestureCheatsheet)
-  const dialogRef = useRef<HTMLDivElement | null>(null)
 
   /**
    * Handles the closure of the gesture cheatsheet.
    */
   const handleClose = () => {
-    if (dialogRef.current) {
-      dialogRef.current.style.opacity = '0'
-      setTimeout(() => {
-        dispatch(toggleGestureCheatsheetActionCreator({ value: false }))
-      }, durations.get('medium'))
-    }
+    dispatch(toggleGestureCheatsheetActionCreator({ value: false }))
   }
-
-  useEffect(() => {
-    if (dialogRef.current) {
-      dialogRef.current.style.opacity = '1'
-    }
-  }, [isOpen])
 
   useEffect(() => {
     /** Disable scrolling when the dialog is open. */
@@ -45,41 +33,34 @@ const GestureCheatsheet: React.FC = () => {
   }, [isOpen])
 
   return (
-    <>
-      {isOpen && (
-        <>
-          <div
-            ref={dialogRef}
-            className={css({
-              opacity: 0,
-              transition: 'opacity {durations.medium} ease',
-              zIndex: 'dialogContainer',
-              position: 'fixed',
-            })}
-          >
-            <Dialog onClose={handleClose}>
-              <DialogTitle onClose={handleClose}>Gesture Cheatsheet</DialogTitle>
-              <DialogContent>
-                <CommandTable viewType='grid' />
+    <FadeTransition in={isOpen} unmountOnExit duration='medium'>
+      <div
+        className={css({
+          zIndex: 'dialogContainer',
+          position: 'fixed',
+        })}
+      >
+        <Dialog onClose={handleClose}>
+          <DialogTitle onClose={handleClose}>Gesture Cheatsheet</DialogTitle>
+          <DialogContent>
+            <CommandTable viewType='grid' />
 
-                {/* In Dialog's styles, we fix a gradient to the bottom of the dialog to subtly indicate that
-                    the dialog is scrollable.
-                    When the dialog reaches the bottom, we want to ensure the gradient is no
-                    longer visible.
-                    Adding a 64px margin to the bottom of the dialog content ensures that the gradient
-                    is not visible, and signals to the user that they have reached the bottom of the dialog.
-                */}
-                <div
-                  style={{
-                    height: '64px',
-                  }}
-                />
-              </DialogContent>
-            </Dialog>
-          </div>
-        </>
-      )}
-    </>
+            {/* In Dialog's styles, we fix a gradient to the bottom of the dialog to subtly indicate that
+                the dialog is scrollable.
+                When the dialog reaches the bottom, we want to ensure the gradient is no
+                longer visible.
+                Adding a 64px margin to the bottom of the dialog content ensures that the gradient
+                is not visible, and signals to the user that they have reached the bottom of the dialog.
+            */}
+            <div
+              style={{
+                height: '64px',
+              }}
+            />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </FadeTransition>
   )
 }
 
