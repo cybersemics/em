b9d1d8159e42f1452062f20bf93fd03bfff1012d
Christina Yang <ychristina011@gmail.com>
2025-04-13 16:24:57 -0700

    refactor: update showTip clear alert in reducer instead of with action creator

--- src/actions/showTip.ts
+++ src/actions/showTip.ts
@@ -2,13 +2,14 @@ import State from '../@types/State'
 import Thunk from '../@types/Thunk'
 import TipId from '../@types/TipId'
 import { registerActionMetadata } from '../util/actionMetadata.registry'
-import { alertActionCreator } from './alert'
 
-/** Shows a popup with a tip. */
+/** Shows a popup with a tip and clears any existing alert. */
 const showTip = (state: State, { tip }: { tip: TipId }): State => ({
   ...state,
   // for now, don't allow stacking tips
   tips: [tip],
+  // clear any existing alert when showing a tip
+  ...(tip && { alert: null }),
 })
 
 /** Action-creator for showTip. */
@@ -16,7 +17,6 @@ export const showTipActionCreator =
   (payload: Parameters<typeof showTip>[1]): Thunk =>
   dispatch => {
     dispatch({ type: 'showTip', ...payload })
-    dispatch(alertActionCreator(null))
   }
 
 export default showTip
