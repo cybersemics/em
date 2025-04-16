99e84fc2167547a9ed75868648ca556eebdc9e59
Raine Revere <raine@cybersemics.org>
2025-04-16 13:58:42 +0000

    indent: Remove canExecute special handling.
    I am not sure if this is still needed, but I cannot reproduce and there is no test coverage.

--- src/commands/indent.tsx
+++ src/commands/indent.tsx
@@ -1,11 +1,6 @@
 import Command from '../@types/Command'
 import { indentActionCreator as indent } from '../actions/indent'
 import IndentIcon from '../components/icons/IndentIcon'
-import attributeEquals from '../selectors/attributeEquals'
-import prevSibling from '../selectors/prevSibling'
-import rootedParentOf from '../selectors/rootedParentOf'
-import simplifyPath from '../selectors/simplifyPath'
-import head from '../util/head'
 import isDocumentEditable from '../util/isDocumentEditable'
 import moveCursorForward from './moveCursorForward'
 
@@ -18,20 +13,6 @@ const indentCommand: Command = {
   },
   multicursor: {
     filter: 'prefer-ancestor',
-    execMulticursor(cursors, dispatch, getState, e, {}, execAll) {
-      // Make sure we can execute for all cursors before proceeding.
-      // This is shifted here to allow `e.preventDefault()` to work.
-      const canExecute = cursors.every(cursor => {
-        const path = simplifyPath(getState(), cursor)
-        const parentId = head(rootedParentOf(getState(), path))
-        const isTable = attributeEquals(getState(), parentId, '=view', 'Table')
-        return isDocumentEditable() && (isTable || !!prevSibling(getState(), cursor))
-      })
-
-      if (!canExecute) return
-
-      return execAll()
-    },
   },
   gesture: 'rlr',
   svg: IndentIcon,
