c5a3660219f2b7d48bf1436b60dd2e4a6a331598
Raine Revere <raine@cybersemics.org>
2025-04-16 16:38:00 +0000

    Make all commands use .ts and factor out remaining icons into .tsx files.

--- src/commands/deleteEmptyThoughtOrOutdent.ts
+++ src/commands/deleteEmptyThoughtOrOutdent.ts
@@ -1,12 +1,9 @@
 import { Key } from 'ts-key-enum'
-import { css, cx } from '../../styled-system/css'
-import { iconRecipe } from '../../styled-system/recipes'
-import { token } from '../../styled-system/tokens'
 import Command from '../@types/Command'
-import IconType from '../@types/IconType'
 import State from '../@types/State'
 import { deleteEmptyThoughtActionCreator as deleteEmptyThought } from '../actions/deleteEmptyThought'
 import { outdentActionCreator as outdent } from '../actions/outdent'
+import DeleteEmptyThoughtIcon from '../components/icons/DeleteEmptyThoughtIcon'
 import * as selection from '../device/selection'
 import { getChildren, getChildrenRanked } from '../selectors/getChildren'
 import getThoughtBefore from '../selectors/getThoughtBefore'
@@ -74,25 +71,6 @@ const exec: Command['exec'] = (dispatch, getState) => {
   }
 }
 
-// eslint-disable-next-line jsdoc/require-jsdoc, react-refresh/only-export-components
-const Icon = ({ fill = token('colors.bg'), size = 20, style, cssRaw }: IconType) => (
-  <svg
-    version='1.1'
-    className={cx(iconRecipe(), css(cssRaw))}
-    xmlns='http://www.w3.org/2000/svg'
-    width={size}
-    height={size}
-    fill={fill}
-    style={style}
-    viewBox='0 0 19.481 19.481'
-    enableBackground='new 0 0 19.481 19.481'
-  >
-    <g>
-      <path d='m10.201,.758l2.478,5.865 6.344,.545c0.44,0.038 0.619,0.587 0.285,0.876l-4.812,4.169 1.442,6.202c0.1,0.431-0.367,0.77-0.745,0.541l-5.452-3.288-5.452,3.288c-0.379,0.228-0.845-0.111-0.745-0.541l1.442-6.202-4.813-4.17c-0.334-0.289-0.156-0.838 0.285-0.876l6.344-.545 2.478-5.864c0.172-0.408 0.749-0.408 0.921,0z' />
-    </g>
-  </svg>
-)
-
 const deleteEmptyThoughtOrOutdent: Command = {
   id: 'deleteEmptyThoughtOrOutdent',
   label: 'Delete Empty Thought Or Outdent',
@@ -102,7 +80,7 @@ const deleteEmptyThoughtOrOutdent: Command = {
     preventSetCursor: true,
     reverse: true,
   },
-  svg: Icon,
+  svg: DeleteEmptyThoughtIcon,
   canExecute,
   exec,
 }
