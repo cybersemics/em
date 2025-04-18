1ed4cbc47fc8a19ae0b75452b7f9e1e3af4f7a30
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2025-04-18 06:40:17 +0100

    upd: hide keyboard when showing Command Menu

--- src/actions/toggleCommandMenu.ts
+++ src/actions/toggleCommandMenu.ts
@@ -3,10 +3,18 @@ import State from '../@types/State'
 import Thunk from '../@types/Thunk'
 
 /** Toggles the Command Menu. */
-const toggleCommandMenu = (state: State, { value }: { value?: boolean } = {}) => ({
-  ...state,
-  showCommandMenu: value === undefined ? !state.showCommandMenu : value,
-})
+const toggleCommandMenu = (state: State, { value }: { value?: boolean } = {}) => {
+  // Blur the active element when opening
+  if (document && document.activeElement) {
+    const activeEl = document.activeElement as HTMLElement
+    activeEl?.blur?.()
+  }
+
+  return {
+    ...state,
+    showCommandMenu: value === undefined ? !state.showCommandMenu : value,
+  }
+}
 
 /** Action-creator for toggleCommandMenu. */
 export const toggleCommandMenuActionCreator =
