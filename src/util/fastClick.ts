e8c214bb0a1628ec021756b55847e138000f7709
Ethan James <ethan.e.james@gmail.com>
2025-04-28 14:41:06 -0700

    Use onClick and role=button in fastClick

--- src/util/fastClick.ts
+++ src/util/fastClick.ts
@@ -3,89 +3,55 @@ import React from 'react'
 import { isTouch } from '../browser'
 import haptics from './haptics'
 
-export type FastClickEvent = React.TouchEvent<Element> | React.MouseEvent<Element, MouseEvent>
-
-// the number of pixels of scrolling or dragging from touchStart that is allowed to still trigger fastClick
-const MOVE_THRESHOLD = 15
-
-// track the touch start coordinates
-let touchStart: { x: number; y: number } | null = null
-
 /** A faster alternative to onClick or checkbox onChange. Activates on mouseUp/touchEnd to avoid interfering with mobile scroll. Returns onTouchStart/Move/End or onMouseUp handler depending on if touch is supported. Cancelled on scroll (with 15px error tolerance). */
 const fastClick = isTouch
   ? (
       // triggered on mouseup or touchend
       // cancelled if the user scroll or drags
-      tapUp: (e: FastClickEvent) => void,
+      tapUp: (e: React.MouseEvent<Element, MouseEvent>) => void,
       {
         // whether to trigger haptics on tap
         enableHaptics = true,
         // triggered on mousedown or touchstart
         tapDown,
-        // triggered when tapUp is cancelled due to scrolling or dragging
-        // does not work with drag-and-drop on desktop (onMouseUp does not trigger)
-        tapCancel,
         // triggered with touchMove, which can never be a MouseEvent
         touchMove,
       }: {
         enableHaptics?: boolean
-        tapDown?: (e: FastClickEvent) => void
-        tapCancel?: (e: FastClickEvent) => void
+        tapDown?: (e: React.TouchEvent) => void
         touchMove?: (e: React.TouchEvent) => void
       } = {},
     ) => ({
-      onTouchStart: (e: React.TouchEvent) => {
-        if (e.touches.length > 0) {
-          const x = e.touches[0].clientX
-          const y = e.touches[0].clientY
-          touchStart = { x, y }
-        }
-        tapDown?.(e)
-      },
-      // cancel tap if touchmove exceeds threshold (e.g. with scrolling or dragging)
-      onTouchMove: _.throttle((e: React.TouchEvent) => {
-        touchMove?.(e)
-        if (touchStart && e.changedTouches.length > 0) {
-          const x = e.changedTouches[0].clientX
-          const y = e.changedTouches[0].clientY
-          if (Math.abs(touchStart.x - x) > MOVE_THRESHOLD || Math.abs(touchStart.y - y) > MOVE_THRESHOLD) {
-            touchStart = null
-          }
-        }
-      }, 16.666),
-      onTouchEnd: (e: React.TouchEvent) => {
+      onTouchStart: tapDown,
+      onTouchMove: touchMove,
+      onClick: (e: React.MouseEvent) => {
         if (enableHaptics) {
           haptics.light()
         }
-        let cancel = !touchStart
 
-        if (touchStart && e.changedTouches.length > 0) {
-          const x = e.changedTouches[0].clientX
-          const y = e.changedTouches[0].clientY
-          if (Math.abs(touchStart.x - x) > MOVE_THRESHOLD || Math.abs(touchStart.y - y) > MOVE_THRESHOLD) {
-            cancel = true
-          }
-        }
-
-        if (cancel) {
-          tapCancel?.(e)
-        } else {
-          tapUp(e)
-        }
-
-        touchStart = null
+        tapUp(e)
       },
+      role: 'button',
     })
   : (
       tapUp: (e: React.MouseEvent) => void,
       {
+        enableHaptics = true,
         tapDown,
       }: {
+        enableHaptics?: boolean
         tapDown?: (e: React.MouseEvent) => void
       } = {},
     ) => ({
-      onMouseUp: tapUp,
+      onClick: (e: React.MouseEvent) => {
+        if (enableHaptics) {
+          haptics.light()
+        }
+
+        tapUp(e)
+      },
       ...(tapDown ? { onMouseDown: tapDown } : null),
+      role: 'button',
     })
 
 export default fastClick
