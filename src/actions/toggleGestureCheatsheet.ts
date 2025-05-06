ddac9017bce9de73100dad397f07b885f0aad687
Raine Revere <raine@cybersemics.org>
2025-05-06 15:46:20 +0000

    toggleGestureCheatsheet: Register action metadata.

--- src/actions/toggleGestureCheatsheet.ts
+++ src/actions/toggleGestureCheatsheet.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import State from '../@types/State'
 import Thunk from '../@types/Thunk'
+import { registerActionMetadata } from '../util/actionMetadata.registry'
 
 /** Toggles (hide/show) the gesture cheatsheet. */
 const toggleGestureCheatsheet = (state: State, { value }: { value?: boolean }) => ({
@@ -15,3 +16,7 @@ export const toggleGestureCheatsheetActionCreator =
     dispatch({ type: 'toggleGestureCheatsheet', ...payload })
 
 export default _.curryRight(toggleGestureCheatsheet)
+
+registerActionMetadata('toggleGestureCheatsheet', {
+  undoable: false,
+})
