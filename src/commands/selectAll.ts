149b06da6d9dcfc82e393cc523ee5bb7aff31036
Raine Revere <raine@cybersemics.org>
2025-05-10 23:05:38 +0000

    selectAll: Toggle between Select All and Deselect All on mobile.

--- src/commands/selectAll.ts
+++ src/commands/selectAll.ts
@@ -1,12 +1,31 @@
 import Command from '../@types/Command'
+import State from '../@types/State'
 import { addAllMulticursorActionCreator as addAllMulticursor } from '../actions/addAllMulticursor'
+import { clearMulticursorsActionCreator as clearMulticursors } from '../actions/clearMulticursors'
+import { isTouch } from '../browser'
+import { HOME_TOKEN } from '../constants'
+import getChildren from '../selectors/getChildren'
 import hasMulticursor from '../selectors/hasMulticursor'
+import rootedParentOf from '../selectors/rootedParentOf'
+import head from '../util/head'
 import isDocumentEditable from '../util/isDocumentEditable'
+
+/** Returns true if the cursor and all its siblings are selected in the multicursor. */
+const isAllSelected = (state: State) => {
+  const { cursor } = state
+  const parentId = cursor ? head(rootedParentOf(state, cursor)) : HOME_TOKEN
+  const childrenIds = getChildren(state, parentId).map(child => child.id)
+  // ignore order
+  const multicursorIdSet = new Set(Object.values(state.multicursors).map(path => head(path)))
+  return childrenIds.length === multicursorIdSet.size && childrenIds.every(childId => multicursorIdSet.has(childId))
+}
 
 const selectAllCommand: Command = {
   id: 'selectAll',
   label: 'Select All',
+  labelInverse: 'Deselect All',
   description: 'Selects all thoughts at the current level. May reduce wrist strain.',
+  descriptionInverse: 'Deselects all thoughts at the current level.',
   gesture: 'ldr',
   // meta + alt + a is the default keyboard shortcut and always works.
   // meta + a is conditionally active when multicursor is active
@@ -15,8 +34,8 @@ const selectAllCommand: Command = {
     { key: 'a', meta: true },
   ],
   multicursor: false,
+  isActive: isAllSelected,
   canExecute: state => {
-    //document must be editable
     if (!isDocumentEditable()) {
       return false
     }
@@ -31,7 +50,12 @@ const selectAllCommand: Command = {
 
     return true
   },
-  exec: addAllMulticursor(),
+  exec: (dispatch, getState) => {
+    // Toggle between Select All and Deselect All
+    // i.e. If all thoughts at the current level are selected, clear the multicursor instead.
+    // Only Deselect All on mobile, since desktop has Escape to easily deselect all.
+    dispatch(isTouch && isAllSelected(getState()) ? clearMulticursors() : addAllMulticursor())
+  },
 }
 
 export default selectAllCommand
