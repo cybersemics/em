1a437137affb4046cc52e38d8eda2698931737ca
Raine Revere <raine@cybersemics.org>
2025-05-11 12:33:37 +0000

    Increase timeout in setTheme helper in attempt to fix flaky test.
    https://github.com/cybersemics/em/actions/runs/14950148342/job/41998427394

--- src/e2e/puppeteer/helpers/setTheme.ts
+++ src/e2e/puppeteer/helpers/setTheme.ts
@@ -7,7 +7,7 @@ const em = window.em as WindowEm
 /** Set color theme to light or dark by directly dispatching settings action. */
 const setTheme = async (theme: 'Light' | 'Dark'): Promise<void> => {
   // TODO
-  await sleep(100)
+  await sleep(200)
 
   await page.evaluate(theme => {
     em.store.dispatch({ type: 'settings', key: 'Theme', value: theme })
@@ -15,7 +15,7 @@ const setTheme = async (theme: 'Light' | 'Dark'): Promise<void> => {
 
   // TODO
   // Waiting for requestAnimationFrame does not fix the issue.
-  await sleep(100)
+  await sleep(200)
 }
 
 export default setTheme
