7bbeb9f9680f4225bde6e5478b0730ab5a8c702a
Raine Revere <raine@cybersemics.org>
2025-05-17 22:09:11 +0000

    openModal: Import sleep.

--- src/e2e/puppeteer/helpers/openModal.ts
+++ src/e2e/puppeteer/helpers/openModal.ts
@@ -1,12 +1,13 @@
 import ModalType from '../../../@types/Modal'
 import { WindowEm } from '../../../initialize'
+import sleep from '../../../util/sleep'
 import { page } from '../setup'
 
 const em = window.em as WindowEm
 
 /** Directly opens a Modal. */
 const openModal = async (id: ModalType): Promise<void> => {
-  await new Promise(resolve => setTimeout(resolve, 100))
+  await sleep(100)
 
   await page.evaluate(id => {
     em.store.dispatch({ type: 'showModal', id })
