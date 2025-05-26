ac40a877a62838f679c8e716c4a36af9b1689253
karunkop <karunkop@gmail.com>
2025-05-26 21:47:36 +0545

    fixed noteValue comment

--- src/util/noteValue.ts
+++ src/util/noteValue.ts
@@ -6,7 +6,7 @@ import head from './head'
 
 /** Gets the value of a thought's note. Returns null if the thought does not have a note. */
 const noteValue = (state: State, path: Path) => {
-  // Try to resolve path (checks =children first, then regular =note/=path)
+  // Try to resolve path (checks =note first, then =children/=note/=path)
   const targetPath = resolveNotePath(state, path)
   if (targetPath) {
     return firstVisibleChild(state, head(targetPath))?.value ?? null
