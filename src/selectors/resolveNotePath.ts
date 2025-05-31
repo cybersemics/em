d7fc89360445168954e073a75e14ca271bca2dd0
Raine Revere <raine@cybersemics.org>
2025-05-31 11:10:33 +0000

    Factor out resolveNoteKey into module with default export.

--- src/selectors/resolveNotePath.ts
+++ src/selectors/resolveNotePath.ts
@@ -1,37 +1,9 @@
 import Path from '../@types/Path'
 import State from '../@types/State'
-import ThoughtId from '../@types/ThoughtId'
 import appendToPath from '../util/appendToPath'
 import head from '../util/head'
 import findDescendant from './findDescendant'
-import { anyChild } from './getChildren'
-import getThoughtById from './getThoughtById'
-
-interface NoteKeyResult {
-  noteKey: string
-  noteId?: ThoughtId
-}
-
-/** Resolves note key and note id by checking for note thoughts. */
-export const resolveNoteKey = (state: State, thoughtId: ThoughtId): NoteKeyResult => {
-  const parentId = getThoughtById(state, thoughtId)?.parentId ?? null
-
-  // the id of the thought's =note or the parent's =children/=note
-  const noteId = findDescendant(state, thoughtId, '=note') || findDescendant(state, parentId, ['=children', '=note'])
-
-  if (!noteId) return { noteKey: '=note' }
-
-  const noteThought = getThoughtById(state, noteId)
-
-  if (noteThought?.pending) return { noteKey: '=note' }
-
-  const notePathId = findDescendant(state, noteId, '=path')
-  const noteKey = anyChild(state, notePathId)?.value ?? '=note'
-  return {
-    noteKey,
-    ...(notePathId ? {} : { noteId }),
-  }
-}
+import resolveNoteKey from './resolveNoteKey'
 
 /** Resolves note path by looking for a note thought, then checking the parent's =children/=note.*/
 const resolveNotePath = (state: State, path: Path): Path | null => {
