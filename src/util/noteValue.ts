7b1fa69c550722045cdc87646061eff0a5a8c080
Karun Shrestha <40414929+karunkop@users.noreply.github.com>
2025-06-03 17:09:55 +0545

    =note/=path  (#2951)
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- src/util/noteValue.ts
+++ src/util/noteValue.ts
@@ -1,16 +1,17 @@
+import Path from '../@types/Path'
 import State from '../@types/State'
-import ThoughtId from '../@types/ThoughtId'
-import findDescendant from '../selectors/findDescendant'
 import { firstVisibleChild } from '../selectors/getChildren'
-import getThoughtById from '../selectors/getThoughtById'
+import resolveNotePath from '../selectors/resolveNotePath'
+import head from './head'
 
 /** Gets the value of a thought's note. Returns null if the thought does not have a note. */
-const noteValue = (state: State, id: ThoughtId) => {
-  const noteId = findDescendant(state, id, '=note')
-  if (!noteId) return null
-  const noteThought = getThoughtById(state, noteId)
-  if (noteThought?.pending) return null
-  return firstVisibleChild(state, noteId!)?.value ?? null
+const noteValue = (state: State, path: Path) => {
+  // Try to resolve path (checks =note first, then =children/=note/=path)
+  const targetPath = resolveNotePath(state, path)
+  if (targetPath) {
+    return firstVisibleChild(state, head(targetPath))?.value ?? null
+  }
+  return null
 }
 
 export default noteValue
