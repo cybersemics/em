553e0d43bd6c854c9a4720213fd6bbf7ff01138a
Heri Setiawan <heri.dehero@gmail.com>
2025-06-09 17:44:26 +0700

    Implement exact reversed easing

--- panda.config.ts
+++ panda.config.ts
@@ -348,7 +348,7 @@ export default defineConfig({
             value: 'cubic-bezier(.84, 0, 1, 1)',
           },
           easeInSmooth: {
-            value: 'cubic-bezier(0.5, 0.1, 0.4, 1)',
+            value: 'cubic-bezier(0.75, 0.00, 0.75, 0.90)',
           },
           nodeCurveXLayer: {
             value: 'cubic-bezier(0.8,0,0.2,0.2)',
