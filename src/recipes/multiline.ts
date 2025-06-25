27ec8f1288a221a155865f6ee165f02111516ac4
Ethan James <ethan.e.james@gmail.com>
2025-06-25 16:47:14 -0700

    Remove !important from paddingTop in multilineRecipe (#3048)
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- src/recipes/multiline.ts
+++ src/recipes/multiline.ts
@@ -10,8 +10,12 @@ const multilineRecipe = defineRecipe({
     /* A small amount of margin-bottom may make multiline thoughts feel less crunched. However, this must be prevented or overridden when the thought has a note, otherwise there will be too much space in between the thought and the note. */
 
     paddingTop: {
-      base: '0.33em !important',
-      _safari: '0.3em !important',
+      base: '0.33em',
+      // The base rule used to have !important on it to allow it to take precedence over editableRecipe's top padding.
+      // That was causing problems on iPhone, but adding a redundant _chrome rule fixes the precedence issue.
+      // https://github.com/cybersemics/em/pull/2969
+      _chrome: '0.33em !important',
+      _safari: '0.3em',
       _mobile: {
         /* Hacky, nonlinear multiline padding-top that works across all font sizes on mobile Safari. Otherwise the thought goes out of alignment with the bullet when it becomes multiline. */
         /* 0.2em works at font size 13 */
