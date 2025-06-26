e7153201ab1e2fdd08a73e0e63de24946f0ca6de
karunkop <karunkop@gmail.com>
2025-06-26 11:33:21 +0545

    reverted to original to ensure test fails

--- src/e2e/puppeteer/__tests__/render-thoughts.ts
+++ src/e2e/puppeteer/__tests__/render-thoughts.ts
@@ -11,7 +11,8 @@ import screenshot from '../helpers/screenshot'
 import scroll from '../helpers/scroll'
 import setTheme from '../helpers/setTheme'
 import testIfNotCI from '../helpers/testIfNotCI'
-import { page } from '../setup'
+
+// import { page } from '../setup'
 
 expect.extend({
   toMatchImageSnapshot: configureSnapshots({ fileName: path.basename(__filename).replace('.ts', '') }),
@@ -87,12 +88,13 @@ const testSuite = () => {
       // - https://github.com/cybersemics/em/actions/runs/14236307211
       // - https://github.com/cybersemics/em/actions/runs/14783509675/job/41507408875?pr=2917
       // Waiting for requestAnimationFrame does not fix the issue.
-      await page.evaluate(() => {
-        return new Promise(resolve => {
-          // Wait for 2 frames to ensure RAF completion
-          requestAnimationFrame(() => requestAnimationFrame(resolve))
-        })
-      })
+      // await page.evaluate(() => {
+      //   return new Promise(resolve => {
+      //     // Wait for 2 frames to ensure RAF completion
+      //     requestAnimationFrame(() => requestAnimationFrame(resolve))
+      //   })
+      // })
+      await sleep(200)
 
       expect(await screenshot()).toMatchImageSnapshot()
     })
