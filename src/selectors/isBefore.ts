1913fc5409ee5669c234bc08c1726a4f4c4d921c
Raine Revere <raine@cybersemics.org>
2025-06-26 01:17:15 +0000

    isBefore: Compare by id instead of compare by value + rank.
    This function dates all the way back to aa11fda.

--- src/selectors/isBefore.ts
+++ src/selectors/isBefore.ts
@@ -2,28 +2,22 @@ import SimplePath from '../@types/SimplePath'
 import State from '../@types/State'
 import { getChildrenRanked } from '../selectors/getChildren'
 import head from '../util/head'
-import headRank from '../util/headRank'
-import headValue from '../util/headValue'
 import parentOf from '../util/parentOf'
 
-/** Returns true if thoughtsA comes immediately before thoughtsB.
- * Assumes they have the same context.
- */
+/** Returns true if A comes immediately before B. */
 const isBefore = (state: State, simplePathA: SimplePath, simplePathB: SimplePath) => {
-  const valueA = headValue(state, simplePathA)
-  const rankA = headRank(state, simplePathA)
-  const valueB = headValue(state, simplePathB)
-  const rankB = headRank(state, simplePathB)
-  const parentPathA = parentOf(simplePathA)
-  const children = getChildrenRanked(state, head(parentPathA))
+  const parentIdA = head(parentOf(simplePathA))
+  const parentIdB = head(parentOf(simplePathB))
+  if (parentIdA !== parentIdB) return false
 
-  if (children.length === 0 || valueA === undefined || valueB === undefined) {
-    return false
-  }
+  const children = getChildrenRanked(state, parentIdA)
 
-  const i = children.findIndex(child => child.value === valueB && child.rank === rankB)
+  // find the thought immediately before B
+  const i = children.findIndex(child => child.id === head(simplePathB))
   const prevSubthought = children[i - 1]
-  return prevSubthought && prevSubthought.value === valueA && prevSubthought.rank === rankA
+
+  // return true if A is the thought immediately before B
+  return prevSubthought && prevSubthought.id === head(simplePathA)
 }
 
 export default isBefore
