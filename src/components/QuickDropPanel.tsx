97c1173136ebd56fcffdadaea10a3690a84d52d3
Raine Revere <raine@cybersemics.org>
2025-06-30 02:23:01 +0000

    QuickDropPanel: Remove unnecessary SlideTransition.

--- src/components/QuickDropPanel.tsx
+++ src/components/QuickDropPanel.tsx
@@ -2,7 +2,6 @@ import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { css } from '../../styled-system/css'
 import DeleteDrop from './DeleteDrop'
-import SlideTransition from './SlideTransition'
 
 /** A panel of buttons that slides out from the right edge of the screen during drag-and-drop to quickly execute certain commands on a droppedthought. */
 const QuickDropPanel = () => {
@@ -10,15 +9,17 @@ const QuickDropPanel = () => {
   const quickDropPanelRef = useRef<HTMLDivElement>(null)
 
   return (
-    <SlideTransition duration='fast' nodeRef={quickDropPanelRef} in={isDragging} from='right' unmountOnExit>
-      <div
-        ref={quickDropPanelRef}
-        className={css({ position: 'fixed', right: 0, top: 0, bottom: 0, zIndex: 'popup' })}
-        data-testid='quick-drop-panel'
-      >
-        <DeleteDrop />
-      </div>
-    </SlideTransition>
+    <>
+      {isDragging && (
+        <div
+          ref={quickDropPanelRef}
+          className={css({ position: 'fixed', right: 0, top: 0, bottom: 0, zIndex: 'popup' })}
+          data-testid='quick-drop-panel'
+        >
+          <DeleteDrop />
+        </div>
+      )}
+    </>
   )
 }
 
