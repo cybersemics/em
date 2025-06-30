7b5188bf02ff591ecd40c7f4354194efa3c86521
Code Builder <107672586+codbuild-er@users.noreply.github.com>
2025-06-30 11:04:18 -0400

    Add puppeteer snapshot test for #2939 issue - Multiline thought height is wrong on categorize/uncategorize (#3078)

--- src/e2e/puppeteer/helpers/press.ts
+++ src/e2e/puppeteer/helpers/press.ts
@@ -6,17 +6,25 @@ type Options = Parameters<Keyboard['press']>[1]
 /** Presses a key on the keyboad. Extends page.keyboard.press options with meta, ctrl, and shift for easy modifier presses. */
 const press = async (
   key: KeyInput,
-  { ctrl, meta, shift, ...options }: Options & { ctrl?: boolean; meta?: boolean; shift?: boolean } = {},
+  {
+    alt,
+    ctrl,
+    meta,
+    shift,
+    ...options
+  }: Options & { alt?: boolean; ctrl?: boolean; meta?: boolean; shift?: boolean } = {},
 ) => {
   if (ctrl) await page.keyboard.down('Control')
   if (meta) await page.keyboard.down('Meta')
   if (shift) await page.keyboard.down('Shift')
+  if (alt) await page.keyboard.down('Alt')
 
   await page.keyboard.press(key, options)
 
   if (ctrl) await page.keyboard.up('Control')
   if (meta) await page.keyboard.up('Meta')
   if (shift) await page.keyboard.up('Shift')
+  if (alt) await page.keyboard.up('Alt')
 }
 
 export default press
