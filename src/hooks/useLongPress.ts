5047440543d9849714df9225da1d39e2f1ef8c2e
Ethan James <ethan.e.james@gmail.com>
2025-07-22 14:20:04 -0700

    Removed unused event parameters from handler callbacks

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -36,7 +36,7 @@ const useLongPress = (
 
   useEffect(() => {
     /** Begin a long press, after the timer elapses on desktop, or the dragStart event is fired by TouchBackned in react-dnd. */
-    const onStart = (e?: React.TouchEvent) => {
+    const onStart = () => {
       if (isLocked || !pressing) return
 
       globals.longpressing = true
@@ -74,29 +74,26 @@ const useLongPress = (
   // track that long press has stopped on mouseUp, touchEnd, or touchCancel
   // Note: This method is not guaranteed to be called, so make sure you perform any cleanup from onLongPressStart elsewhere (e.g. in useDragHold.)
   // TODO: Maybe an unmount handler would be better?
-  const stop = useCallback(
-    (e: React.MouseEvent | React.TouchEvent) => {
-      setPressing(false)
-      // Delay setPressed(false) to ensure that onLongPressEnd is not called until bubbled events complete.
-      // This gives other components a chance to short circuit.
-      // We can't stop propagation here without messing up other components like Bullet.
-      setTimeout(() => {
-        clearTimeout(timerIdRef.current)
-        timerIdRef.current = 0
-        longPressStore.unlock()
+  const stop = useCallback(() => {
+    setPressing(false)
+    // Delay setPressed(false) to ensure that onLongPressEnd is not called until bubbled events complete.
+    // This gives other components a chance to short circuit.
+    // We can't stop propagation here without messing up other components like Bullet.
+    setTimeout(() => {
+      clearTimeout(timerIdRef.current)
+      timerIdRef.current = 0
+      longPressStore.unlock()
 
-        // If not longpressing, it means that the long press was canceled by a move event.
-        // in this case, onLongPressEnd should not be called, since it was already called by the move event.
-        if (!globals.longpressing) return
+      // If not longpressing, it means that the long press was canceled by a move event.
+      // in this case, onLongPressEnd should not be called, since it was already called by the move event.
+      if (!globals.longpressing) return
 
-        globals.longpressing = false
+      globals.longpressing = false
 
-        // If a long press occurred, mark it as not canceled
-        onLongPressEnd?.()
-      }, 10)
-    },
-    [onLongPressEnd, setPressing],
-  )
+      // If a long press occurred, mark it as not canceled
+      onLongPressEnd?.()
+    }, 10)
+  }, [onLongPressEnd, setPressing])
 
   // Prevent context menu from appearing on long press, otherwise it interferes with drag-and-drop.
   // Allow double tap to open the context menu as usual.
