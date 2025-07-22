137f5ca7e70616056f88a5e8310e180ba01d27ce
Ethan James <ethan.e.james@gmail.com>
2025-07-22 11:08:02 -0700

    Prevent clickOnEmptyThought during long press (#3122)

---
+++ src/e2e/puppeteer/__tests__/multiselect.ts
@@ -0,0 +1,56 @@
+import path from 'path'
+import { KnownDevices } from 'puppeteer'
+import configureSnapshots from '../configureSnapshots'
+import emulate from '../helpers/emulate'
+import longPressThought from '../helpers/longPressThought'
+import multiselectThoughts from '../helpers/multiselectThoughts'
+import paste from '../helpers/paste'
+import waitForEditable from '../helpers/waitForEditable'
+import { page } from '../setup'
+
+expect.extend({
+  toMatchImageSnapshot: configureSnapshots({ fileName: path.basename(__filename).replace('.ts', '') }),
+})
+
+describe('multiselect', () => {
+  it('should multiselect two thoughts at once', async () => {
+    await paste(`
+        - a
+        - b
+        `)
+
+    await multiselectThoughts(['a', 'b'])
+
+    const highlightedBullets = await page.$$('.bullet[data-highlighted=true]')
+    const alertContent = await page.$eval('[data-testid=alert-content]', el => el.textContent)
+
+    expect(highlightedBullets.length).toBe(2)
+    expect(alertContent).toContain('2 thoughts selected')
+  })
+})
+
+describe('mobile only', () => {
+  beforeEach(async () => {
+    await emulate(KnownDevices['iPhone 11'])
+  }, 10000)
+
+  it('should multiselect two thoughts at once', async () => {
+    await paste(`
+        - a
+        - b
+        - c
+        `)
+
+    const a = await waitForEditable('a')
+    const b = await waitForEditable('b')
+
+    await longPressThought(a, { edge: 'right', x: 100 })
+    await longPressThought(b, { edge: 'right', x: 100 })
+
+    const highlightedBullets = await page.$$('.bullet[data-highlighted=true]')
+    const commandMenuPanelTextContent = await page.$eval('[data-testid=command-menu-panel]', el => el.textContent)
+
+    expect(highlightedBullets.length).toBe(2)
+    expect(commandMenuPanelTextContent).toContain('2 thoughts selected')
+  })
+})
