321ff05acefbca006766838968f28eb2a98fb822
karunkop <karunkop@gmail.com>
2025-07-23 11:47:26 +0545

    replaced some old snapshots, used waitForFrames for consistent snapshots in CI

--- src/e2e/puppeteer/helpers/openModal.ts
+++ src/e2e/puppeteer/helpers/openModal.ts
@@ -1,13 +1,13 @@
 import ModalType from '../../../@types/Modal'
 import { WindowEm } from '../../../initialize'
-import sleep from '../../../util/sleep'
 import { page } from '../setup'
+import waitForFrames from './waitForFrames'
 
 const em = window.em as WindowEm
 
 /** Directly opens a Modal. */
 const openModal = async (id: ModalType): Promise<void> => {
-  await sleep(100)
+  await waitForFrames(4)
 
   await page.evaluate(id => {
     em.store.dispatch({ type: 'showModal', id })
