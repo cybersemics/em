a7dc733b4f4266711abeb28209591725573e882e
karunkop <karunkop@gmail.com>
2025-07-23 15:32:04 +0545

    removed use of waitForFrames

--- src/e2e/puppeteer/helpers/openModal.ts
+++ src/e2e/puppeteer/helpers/openModal.ts
@@ -2,15 +2,10 @@ import ModalType from '../../../@types/Modal'
 import { WindowEm } from '../../../initialize'
 import { page } from '../setup'
 
-// import waitForFrames from './waitForFrames'
-
 const em = window.em as WindowEm
 
 /** Directly opens a Modal. */
 const openModal = async (id: ModalType): Promise<void> => {
-  // disable temporarily
-  // await waitForFrames()
-
   await page.evaluate(id => {
     em.store.dispatch({ type: 'showModal', id })
   }, id)
