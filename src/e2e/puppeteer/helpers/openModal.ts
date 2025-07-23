cc8aca8550ecdfcd3c0b1e14ea36e150b2d2d089
karunkop <karunkop@gmail.com>
2025-07-23 12:06:09 +0545

    temporarily disabled waitForFrames to check for flakiness

--- src/e2e/puppeteer/helpers/openModal.ts
+++ src/e2e/puppeteer/helpers/openModal.ts
@@ -1,13 +1,15 @@
 import ModalType from '../../../@types/Modal'
 import { WindowEm } from '../../../initialize'
 import { page } from '../setup'
-import waitForFrames from './waitForFrames'
+
+// import waitForFrames from './waitForFrames'
 
 const em = window.em as WindowEm
 
 /** Directly opens a Modal. */
 const openModal = async (id: ModalType): Promise<void> => {
-  await waitForFrames()
+  // disable temporarily
+  // await waitForFrames()
 
   await page.evaluate(id => {
     em.store.dispatch({ type: 'showModal', id })
