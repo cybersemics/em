c80aebf45d999cc9a1a0159d8e0ec81a2f10a67e
Cindy Michelle <cindy@kodefox.com>
2025-07-28 23:31:08 +0700

    bullet cursor overlay with placeholder approach

--- src/components/LayoutTree.tsx
+++ src/components/LayoutTree.tsx
@@ -16,7 +16,9 @@ import nextSibling from '../selectors/nextSibling'
 import reactMinistore from '../stores/react-ministore'
 import scrollTopStore from '../stores/scrollTop'
 import viewportStore from '../stores/viewport'
+import equalPath from '../util/equalPath'
 import head from '../util/head'
+import BulletCursorOverlay from './BulletCursorOverlay'
 import HoverArrow from './HoverArrow'
 import TreeNode from './TreeNode'
 
@@ -215,6 +217,11 @@ const LayoutTree = () => {
     },
   )
 
+  // compare between state.cursor and the position of the thought
+  const cursorThoughtPositioned = useSelector(state =>
+    treeThoughtsPositioned.find(thought => equalPath(state.cursor, thought.path)),
+  )
+
   // The indentDepth multipicand (0.9) causes the horizontal counter-indentation to fall short of the actual indentation, causing a progressive shifting right as the user navigates deeper. This provides an additional cue for the user's depth, which is helpful when autofocus obscures the actual depth, but it must stay small otherwise the thought width becomes too small.
   // The indentCursorAncestorTables multipicand (0.5) is smaller, since animating over by the entire width of column 1 is too abrupt.
   // (The same multiplicand is applied to the vertical translation that crops hidden thoughts above the cursor.)
@@ -258,6 +265,18 @@ const LayoutTree = () => {
           marginRight: `${-indent + (isTouch ? 2 : -1)}em`,
         }}
       >
+        {cursorThoughtPositioned && (
+          <BulletCursorOverlay
+            isTableCol1={cursorThoughtPositioned.isTableCol1}
+            path={cursorThoughtPositioned.path}
+            simplePath={cursorThoughtPositioned.simplePath}
+            x={cursorThoughtPositioned.x}
+            y={cursorThoughtPositioned.y}
+            showContexts={cursorThoughtPositioned.showContexts}
+            width={cursorThoughtPositioned.width}
+            parentId={cursorThoughtPositioned.path[cursorThoughtPositioned.path.length - 2] as ThoughtId}
+          />
+        )}
         <TransitionGroup>
           {treeThoughtsPositioned.map((thought, index) => (
             <TreeNode
