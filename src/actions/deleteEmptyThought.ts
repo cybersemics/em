d66f7fbfcbf2ef85eef1705f3cfbaaa22072df2c
karunkop <karunkop@gmail.com>
2025-08-20 10:17:28 +0545

    added debugging logs

--- src/actions/deleteEmptyThought.ts
+++ src/actions/deleteEmptyThought.ts
@@ -97,6 +97,7 @@ const deleteEmptyThought = (state: State): State => {
       // if splitSource equals prev sibling thought id, then add an intervening space to preserve the original space while splitting from the source thought
       const valueNew = splitSource === prev.id ? prev.value + ' ' + value : prev.value + value
       const pathPrevNew = appendToPath(parentOf(simplePath), prev.id)
+      console.info('getTextContentFromHTML(prev.value) :', getTextContentFromHTML(prev.value))
 
       return reducerFlow([
         // change first thought value to concatenated value
@@ -128,6 +129,11 @@ const deleteEmptyThought = (state: State): State => {
           offset: getTextContentFromHTML(prev.value).length,
           isKeyboardOpen,
         }),
+
+        state => {
+          console.info('state :', state.cursorOffset)
+          return state
+        },
       ])(state)
     }
   }
