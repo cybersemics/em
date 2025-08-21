2ae57b27884050bed168602939435fadf5890c7a
Karun Shrestha <40414929+karunkop@users.noreply.github.com>
2025-08-21 09:16:46 +0545

    Fix for drag and drop favorite throws an error (#3186)

--- src/hooks/useDragDropFavorites.ts
+++ src/hooks/useDragDropFavorites.ts
@@ -24,7 +24,7 @@ type DragAndDropFavoriteReturnType = DragThoughtItem & {
 }
 
 /** Handles drag start. */
-const beginDrag = ({ path, simplePath }: DragThoughtItem): DragThoughtItem => {
+const beginDrag = ({ path, simplePath }: DragThoughtItem): DragThoughtItem[] => {
   const offset = selection.offset()
 
   store.dispatch(
@@ -35,7 +35,7 @@ const beginDrag = ({ path, simplePath }: DragThoughtItem): DragThoughtItem => {
       ...(offset != null ? { offset } : null),
     }),
   )
-  return { path, simplePath, zone: DragThoughtZone.Favorites }
+  return [{ path, simplePath, zone: DragThoughtZone.Favorites }]
 }
 
 /** Handles drag end. */
@@ -69,7 +69,10 @@ const drop = (
   // no bubbling
   if (monitor.didDrop() || !monitor.isOver({ shallow: true })) return
 
-  const { simplePath: thoughtsFrom, zone } = monitor.getItem() as DragThoughtItem
+  const item = monitor.getItem() as DragThoughtItem[]
+  // For favorites, we expect only a single item in the array
+  const draggedItem = item[0]
+  const { simplePath: thoughtsFrom, zone } = draggedItem
   if (zone === DragThoughtZone.Thoughts) {
     console.error('TODO: Add support for other thought drag sources', monitor.getItem())
     return
