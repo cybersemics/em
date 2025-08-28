5d7bf8ebd63f97c7f62ccb3f6865ffd1f79a6cb8
codebuild-ro <218734912+codebuild-ro@users.noreply.github.com>
2025-08-29 00:34:23 +0200

    Update configuration of dependabot (#3200)

--- src/e2e/iOS/helpers/asBrowserEnvironment.ts
+++ src/e2e/iOS/helpers/asBrowserEnvironment.ts
@@ -4,8 +4,8 @@ import { BrowserEnvironment } from '../../browserEnvironment/types'
 /** Converts a Page to a BrowserEnvironment. */
 const asBrowserEnvironment = (browser: Browser): BrowserEnvironment => ({
   // 'function' is required here because webdriverio uses 'this'
-  execute: function (...args) {
-    return browser.execute(...args)
+  execute: function <R>(...args: [() => R]) {
+    return browser.execute(...args) as Promise<R>
   },
 })
 
