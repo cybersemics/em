32e34ea0e2747c1c0be4e35000bba6aa9d68ce4d
codebuild-ro <218734912+codebuild-ro@users.noreply.github.com>
2025-09-02 19:40:35 +0200

    Fix/dependency update (#3206)
    Co-authored-by: Raine Revere <raine@cybersemics.org>

--- src/e2e/iOS/helpers/asBrowserEnvironment.ts
+++ src/e2e/iOS/helpers/asBrowserEnvironment.ts
@@ -4,8 +4,8 @@ import { BrowserEnvironment } from '../../browserEnvironment/types'
 /** Converts a Page to a BrowserEnvironment. */
 const asBrowserEnvironment = (browser: Browser): BrowserEnvironment => ({
   // 'function' is required here because webdriverio uses 'this'
-  execute: function (...args) {
-    return browser.execute(...args)
+  execute: function <R>(f: () => R) {
+    return browser.execute(f) as Promise<R>
   },
 })
 
