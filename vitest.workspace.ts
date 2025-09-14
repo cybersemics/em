5f1a5b04057b0638ea97ff59f24b419c8baa55ec
karunkop <karunkop@gmail.com>
2025-09-14 17:29:29 +0545

    enabled singleThread for unit tests

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -19,6 +19,11 @@ export default defineWorkspace([
           },
         },
       },
+      poolOptions: {
+        threads: {
+          singleThread: true,
+        },
+      },
     },
   },
   {
