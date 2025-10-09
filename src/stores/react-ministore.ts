b4e07353b27713e17d0cdf73659c3c0d3306111c
Ethan James <ethan.e.james@gmail.com>
2025-10-09 11:39:30 -0700

    Disable lint rules on useChangeEffect

--- src/stores/react-ministore.ts
+++ src/stores/react-ministore.ts
@@ -9,12 +9,14 @@ const makeReactStore = <U extends Store<any>>(store: U) => {
   type T = U extends Store<infer V> ? V : never
 
   /** A hook that invokes a callback when the state changes. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const useChangeEffect = (cb: (state: T) => void, deps: any[] = []) =>
     useEffect(
       () =>
         store.subscribe(() => {
           cb(store.getState())
         }),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       [cb, ...deps],
     )
 
