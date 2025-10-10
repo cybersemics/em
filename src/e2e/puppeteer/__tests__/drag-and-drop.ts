8e377d56199cac8d548f5a473d094450d457140a
kevin <218734912+codebuild-ro@users.noreply.github.com>
2025-10-10 11:43:47 +0200

    Revert "Increase timeout values"
    This reverts commit b89243a2acc13f626e9ed18d1a175e39cac4bcad.

--- src/e2e/puppeteer/__tests__/drag-and-drop.ts
+++ src/e2e/puppeteer/__tests__/drag-and-drop.ts
@@ -27,7 +27,7 @@ expect.extend({
   toMatchImageSnapshot: configureSnapshots({ fileName: path.basename(__filename).replace('.ts', '') }),
 })
 
-vi.setConfig({ testTimeout: 60000, hookTimeout: 60000 })
+vi.setConfig({ testTimeout: 60000, hookTimeout: 20000 })
 
 /**
  * Checks if an element with the given text content is visible in the UI.
@@ -65,7 +65,7 @@ const isElementVisible = async (text: string, selector = '[data-editable]'): Pro
   )
 }
 /* From jest-image-snapshot README:
-
+    
   Jest supports automatic retries on test failures. This can be useful for browser screenshot tests which tend to have more frequent false positives. Note that when using jest.retryTimes you'll have to use a unique customSnapshotIdentifier as that's the only way to reliably identify snapshots.
 
 */
