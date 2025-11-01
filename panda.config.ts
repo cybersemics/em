2ac29f085506460ca9eafcea23c8e5f2d308c49a
Christina Yang <ychristina011@gmail.com>
2025-10-31 18:05:50 -0700

    add comment explaining fallback

--- panda.config.ts
+++ panda.config.ts
@@ -330,6 +330,7 @@ const globalCss = defineGlobalStyles({
     content: 'attr(placeholder)',
     cursor: 'text',
   },
+  // PandaCSS does not directly support fallbacks: https://github.com/chakra-ui/panda/discussions/846
   ':root': {
     '--active-glow-gradient':
       'linear-gradient(180deg, {colors.commandCenterBlue} 0%, {colors.commandCenterPurple} 100%)',
