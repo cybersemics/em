865b9ae880d4d15772174cbc1ed10562bdeca5a4
Ethan James <ethan.e.james@gmail.com>
2025-12-08 18:58:16 -0800

    Add comment for thought-annotation marginTop

--- src/components/ThoughtAnnotationWrapper.tsx
+++ src/components/ThoughtAnnotationWrapper.tsx
@@ -27,6 +27,11 @@ const ThoughtAnnotationWrapper: FC<
         userSelect: 'none',
         boxSizing: 'border-box',
         lineHeight: multiline ? 1.25 : undefined,
+        // For single-line thoughts, the caret is positioned about 7.5px (at 18px font size) down from the top of the line (vertical-align: top).
+        // That is the position that gets measured in usePositionedAnnotation, and so it needs to be bumped up here because, for some reason,
+        // offsetting the top position directly doesn't work. The multiline margin was already set on the children, and doesn't seem to require
+        // as much adjustment, possibly due to the difference between single-line line height (2em) and multiline line height (1.25em).
+        // Perhaps the aggregate top margins add up to 50% of the difference between 1em and line-height, and could be consolidated further.
         marginTop: multiline ? 'calc(-0.12em - 0.5px)' : value ? '-0.425em' : undefined,
         marginLeft: 'calc(0.666em - 18px)',
         display: 'inline-block',
