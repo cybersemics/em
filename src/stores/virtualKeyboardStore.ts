c82a33c938436013a36dada53ca868c23392f65f
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2026-02-01 03:56:09 +0100

    refactor(virtualKeyboardStore): directly export react-ministore instance

--- src/stores/virtualKeyboardStore.ts
+++ src/stores/virtualKeyboardStore.ts
@@ -1,18 +1,11 @@
 import VirtualKeyboardState from '../@types/VirtualKeyboardState'
 import reactMinistore from './react-ministore'
 
-/** Underlying store for virtual keyboard state. */
-const store = reactMinistore<VirtualKeyboardState>({
+/** A store that tracks the state of the virtual keyboard.
+ * Its value is updated by platform-specific handlers (see `src/device/virtual-keyboard/handlers/`). */
+const virtualKeyboardStore = reactMinistore<VirtualKeyboardState>({
   open: false,
   height: 0,
 })
-
-/** A store that tracks the state of the virtual keyboard. Its value is updated by platform-specific Handlers. */
-const virtualKeyboardStore = {
-  ...store,
-  update: (updates: Partial<VirtualKeyboardState>) => {
-    store.update(updates)
-  },
-}
 
 export default virtualKeyboardStore
