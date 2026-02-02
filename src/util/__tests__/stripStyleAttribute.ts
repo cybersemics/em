593e7ea6f097d8890b27775841b33ea16648c376
Raine Revere <raine@cybersemics.org>
2026-02-02 01:27:39 +0000

    stripStyleAttribute test: minor

--- src/util/__tests__/stripStyleAttribute.ts
+++ src/util/__tests__/stripStyleAttribute.ts
@@ -1,18 +1,19 @@
 import stripStyleAttribute from '../stripStyleAttribute'
 
-it('only allow font weight, font-style, color, background-color and text-decoration', () => {
-  expect(
-    stripStyleAttribute(
-      'color: red; background-color: white; font-weight: bold; font-size: 14px; font-style: italic; text-decoration: underline;',
-    ),
-  ).toBe('color: red;background-color: white;font-weight: bold;font-style: italic;text-decoration: underline;')
+it('Allow color, background-color, font-weight, font-style, and text-decoration', () => {
+  const value = 'color: red;background-color: white;font-weight: bold;font-style: italic;text-decoration: underline;'
+  expect(stripStyleAttribute(value)).toBe(value)
 })
 
-it('strip text-decoration: none', () => {
-  expect(stripStyleAttribute('font-weight: bold; text-decoration: none')).toBe('font-weight: bold;')
+it('Strip font-size', () => {
+  expect(stripStyleAttribute('color: red; font-size: 14px;')).toBe('color: red;')
 })
 
-it('strip font-weight: normal or 300', () => {
+it('Strip text-decoration: none', () => {
+  expect(stripStyleAttribute('color: red; text-decoration: none')).toBe('color: red;')
+})
+
+it('Strip font-weight: normal or 300', () => {
   expect(stripStyleAttribute('color: red; font-weight: normal')).toBe('color: red;')
   expect(stripStyleAttribute('color: red; font-weight: 300')).toBe('color: red;')
 })
