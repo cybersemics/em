3745ce3d0f3b5bba74a5ed889e0154e2f61bb8d0
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2026-02-11 03:54:47 +0100

    tweak easing and animation duration
    The easing and animation duration used for the sidebar felt fitting for its old design, but less so with the new blurry aesthetic.
    
    The sidebar should feel fluid and gradual, like you're eased into the sidebar. Having it snap quickly in and out feels inconsistent with that.
    
    This commit tweaks the duration and easing curves. These can be reverted if the new animations feel too slow or jarring.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -227,7 +227,7 @@ const Sidebar = () => {
   const transition = useMemo(
     () => ({
       duration: durations.get('medium') / 1000,
-      ease: showSidebar ? ([0.16, 0.6, 0.2, 1] as const) : ([0.25, 0.1, 0.25, 1] as const),
+      ease: [0.16, 0.6, 0.2, 1] as const,
     }),
     [showSidebar],
   )
