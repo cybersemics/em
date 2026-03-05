51b1adbec7c4323e167303483dce755fec75b743
Ethan James <ethan.e.james@gmail.com>
2026-03-05 13:38:31 -0800

    Select note editable if noteFocus is true

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -188,6 +188,7 @@ const Note = React.memo(
           html={note || ''}
           innerRef={noteRef as React.RefObject<HTMLElement>}
           aria-label='note-editable'
+          data-thought-id={head(path)}
           placeholder='Enter a note'
           className={css({
             display: 'inline-block',
