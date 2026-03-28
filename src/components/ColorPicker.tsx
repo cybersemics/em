e541124f9ebbbd4898dec6ca9f616cfacbb018b4
Ethan James <ethan.e.james@gmail.com>
2026-03-28 15:26:12 -0700

    Comment for fgColor

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -91,7 +91,7 @@ const ColorSwatch: FC<{
     )
   })
 
-  //
+  // Note is semi-transparent by default and its color must be reset to that rather than white, which is the fg color for thoughts. (#3902)
   const fgColor = useSelector(state => (state.noteFocus ? 'fgNote' : 'fg'))
 
   /** Toggles the text color to the clicked swatch. If the swatch is already selected, sets text color and background color back to default. */
