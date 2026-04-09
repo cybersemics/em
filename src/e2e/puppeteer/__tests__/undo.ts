9ea0dbeabe4dbd9b26eeff80cad2abbdceff5970
Ethan James <ethan.e.james@gmail.com>
2026-04-09 13:39:22 -0700

    Add Puppeteer test for undoing background color changes

--- src/e2e/puppeteer/__tests__/undo.ts
+++ src/e2e/puppeteer/__tests__/undo.ts
@@ -1,10 +1,12 @@
 import { KnownDevices } from 'puppeteer'
 import newThoughtCommand from '../../../commands/newThought'
+import click from '../helpers/click'
 import clickThought from '../helpers/clickThought'
 import exportThoughts from '../helpers/exportThoughts'
 import gesture from '../helpers/gesture'
 import getEditingText from '../helpers/getEditingText'
 import keyboard from '../helpers/keyboard'
+import paste from '../helpers/paste'
 import press from '../helpers/press'
 import { page } from '../setup'
 
@@ -67,4 +69,63 @@ it('Undo Select All + Categorize chained command in one step', async () => {
   const highlightedCount = await page.evaluate(() => document.querySelectorAll('[data-highlighted=true]').length)
 
   expect(highlightedCount).toBe(0)
+})
+
+it('Should revert background color changes back to previous values', async () => {
+  const importText = `
+    - Lorem Ipsum Dolor Sit Amet`
+
+  await paste(importText)
+
+  // open the ColorPicker
+  await click('[data-testid="toolbar-icon"][aria-label="Text Color"]')
+
+  const thought = await page.$('[aria-label=thought] [data-editable=true]')
+  const boundingBox = await thought?.boundingBox()
+
+  if (!boundingBox) throw new Error('boundingBox not found')
+
+  const y = boundingBox.y + boundingBox.height / 2
+
+  // get a position near the left edge of the thought
+  const leftX = boundingBox.x + 1
+
+  // double click to select the first word
+  await page.mouse.click(leftX, y, { clickCount: 2 })
+
+  // set the first word's background color to green
+  await click('[aria-label="background color swatches"] [aria-label="green"]')
+
+  // dismiss the existing selection range
+  await page.mouse.click(leftX, y)
+
+  // get a position near the right edge of the thought
+  const rightX = boundingBox.x + boundingBox.width - 36
+
+  // double click to select the last word
+  await page.mouse.click(rightX, y, { clickCount: 2 })
+
+  // set the last word's background color to green
+  await click('[aria-label="background color swatches"] [aria-label="green"]')
+
+  // dismiss the existing selection range
+  await page.mouse.click(rightX, y)
+
+  // get a position at the center of the thought
+  const centerX = boundingBox.x + boundingBox.width / 2
+
+  // click to place the caret in the center of the thought
+  await page.mouse.click(centerX, y)
+
+  // set the entire thought's background color to red
+  await click('[aria-label="background color swatches"] [aria-label="red"]')
+
+  // undo
+  await press('z', { meta: true })
+
+  // now the first and last words should have a green background again
+  const text = await getEditingText()
+  expect(text).toBe(
+    '<font color="#000000" style="background-color: rgb(0, 214, 136);">Lorem</font> Ipsum Dolor Sit <font color="#000000" style="background-color: rgb(0, 214, 136);">Amet</font>',
+  )
 })
