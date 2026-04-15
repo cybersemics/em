6e1495fb97235ca31b923a039f563f583cf7ebf8
Cindy Michelle <cindy.michelle@unloan.com.au>
2026-04-15 16:59:15 +0700

    fix: TraceGesture and GestureMenu should start fading-out at same time

--- src/components/TraceGesture.tsx
+++ src/components/TraceGesture.tsx
@@ -46,7 +46,9 @@ const useGestureCancelled = () => {
 const TraceGesture = ({ eventNodeRef }: TraceGestureProps) => {
   const colors = useSelector(themeColors)
   const leftHanded = useSelector(getUserSetting(Settings.leftHanded))
-  const show = gestureStore.useSelector(state => state.gesture.length > 0)
+  const show = gestureStore.useSelector(
+    state => state.gesture.length > 0 && state.gestureMenuAnimationState !== 'exiting',
+  )
   const cancelled = useGestureCancelled()
   const innerHeight = viewportStore.useSelector(state => state.innerHeight)
   const innerWidth = viewportStore.useSelector(state => state.innerWidth)
