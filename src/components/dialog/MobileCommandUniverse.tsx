9238547807e884df97d814197177576326e46ca2
Faiz Mustafa <faiz@tangerinestudio.co.uk>
2026-04-27 09:44:14 +0100

    Wire MobileCommandUniverse to CommandUniverseGrid with own search/sort chrome

--- src/components/dialog/MobileCommandUniverse.tsx
+++ src/components/dialog/MobileCommandUniverse.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { css } from '../../../styled-system/css'
+import { SwitchTransition } from 'react-transition-group'
+import { css, cx } from '../../../styled-system/css'
+import { modalTextRecipe } from '../../../styled-system/recipes'
 import { toggleMobileCommandUniverseActionCreator } from '../../actions/toggleMobileCommandUniverse'
-import CommandTable from '../CommandTable'
+import useCommandList from '../../hooks/useCommandList'
+import CommandUniverseGrid from '../CommandUniverseGrid'
 import FadeTransition from '../FadeTransition'
+import SearchCommands from '../SearchCommands'
+import SortButton from '../SortButton'
 import Dialog from './Dialog'
 import DialogContent from './DialogContent'
 import DialogTitle from './DialogTitle'
@@ -18,6 +23,61 @@ const HiddenDialogAssets = () => (
     <div className={css({ backgroundImage: 'url(/img/dialog/dialog-highlight-rainbow.avif)', visibility: 'hidden' })} />
   </>
 )
+
+/** Renders the search/sort chrome and the filtered command grid groups. */
+const MobileCommandUniverseContent = () => {
+  const { search, setSearch, sortOrder, setSortOrder, groups } = useCommandList()
+  const modalClasses = modalTextRecipe()
+
+  return (
+    <>
+      <div
+        className={css({
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          gap: '5px',
+          position: 'relative',
+          zIndex: 1,
+        })}
+      >
+        <SearchCommands onInput={setSearch} />
+        <SortButton onSortChange={setSortOrder} />
+      </div>
+
+      <SwitchTransition>
+        <FadeTransition key={`${sortOrder}-${search}`} in={true} type='medium' unmountOnExit>
+          <div>
+            {groups.map(group => (
+              <div
+                key={group.title}
+                className={css({
+                  position: 'relative',
+                  contain: 'layout paint',
+                })}
+              >
+                <h2
+                  className={cx(
+                    modalClasses.subtitle,
+                    css({
+                      fontSize: '1.15rem',
+                      borderBottom: 'none',
+                      padding: '0.5rem 0 1rem 0',
+                      margin: '0.444rem 0 0 0',
+                    }),
+                  )}
+                >
+                  {group.title}
+                </h2>
+                <CommandUniverseGrid commands={group.commands} search={search} />
+              </div>
+            ))}
+          </div>
+        </FadeTransition>
+      </SwitchTransition>
+    </>
+  )
+}
 
 /**
  * Mobile Command Universe component.
@@ -41,7 +101,7 @@ const MobileCommandUniverse: React.FC = () => {
         <Dialog onClose={handleClose} nodeRef={nodeRef}>
           <DialogTitle onClose={handleClose}>Command Universe</DialogTitle>
           <DialogContent>
-            <CommandTable viewType='grid' />
+            <MobileCommandUniverseContent />
           </DialogContent>
         </Dialog>
       </FadeTransition>
