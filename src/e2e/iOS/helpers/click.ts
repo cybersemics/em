bbae4dc83b4cc1f6b403e83d8384e58a5a8d8b18
Ethan James <ethan.e.james@gmail.com>
2026-05-11 11:46:32 -0700

    Use webdriver browser.action in iOS click handler

--- src/e2e/iOS/helpers/click.ts
+++ src/e2e/iOS/helpers/click.ts
@@ -1,13 +1,19 @@
-import tap from './tap'
 import waitForElement from './waitForElement'
 
-/** Click a node by selector. */
+/** Click a node by selector or element with WebDriver click or synthetic DOM clicks. */
 const click = async (selector: string) => {
-  const el = await waitForElement(selector, { timeout: 10000 })
+  const el = await waitForElement(selector)
+  const rect = await browser.getElementRect(el.elementId)
 
-  if (!el) throw new Error(`editable node for the given selector(${selector}) not found.`)
+  const centerX = rect.x + rect.width / 2
+  const centerY = rect.y + rect.height / 2
 
-  await tap(el)
+  await browser
+    .action('pointer', { parameters: { pointerType: 'touch' } })
+    .move({ x: Math.round(centerX), y: Math.round(centerY), origin: 'viewport' })
+    .down()
+    .up()
+    .perform()
 }
 
 export default click
