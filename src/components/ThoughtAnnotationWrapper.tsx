830bd047920729f7be7fcae1678c113aaca47f1a
Ethan James <ethan.e.james@gmail.com>
2026-05-14 15:04:42 -0700

    Fix margin/padding in ThoughtAnnotationWrapper to match editable recipe

--- src/components/ThoughtAnnotationWrapper.tsx
+++ src/components/ThoughtAnnotationWrapper.tsx
@@ -72,7 +72,6 @@ const ThoughtAnnotationWrapper: FC<
                 }),
               display: 'inline-block',
               maxWidth: '100%',
-              paddingLeft: '0.333em',
               boxSizing: 'border-box',
               whiteSpace: ellipsizedUrl ? 'nowrap' : undefined,
               /*
@@ -80,8 +79,8 @@ const ThoughtAnnotationWrapper: FC<
                   To make sure all lines are aligned need to apply the margin here, and remove margin from the .editable-annotation-text.
                   This margin should match the margin set in editableRecipe (#3353).
                 */
-              margin: '-0.5px calc(18px - 1em) 0 calc(1em - 18px)',
-              paddingRight: '0.333em',
+              margin: '0 calc(18px - 1em) 0 calc(1em - 18px)',
+              padding: '0.375em 1em 0.375em 0.333em',
               textAlign: isTableCol1 ? 'right' : 'left',
             }),
           )
