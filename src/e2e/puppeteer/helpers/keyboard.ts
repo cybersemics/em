e4d0516300ab2e8033b926aded1316066063cdd8
Antonov548 <antonov5551998@gmail.com>
2026-06-05 17:39:28 +0200

    fix eslint

--- src/e2e/puppeteer/helpers/keyboard.ts
+++ src/e2e/puppeteer/helpers/keyboard.ts
@@ -1,8 +1,5 @@
 import { page } from '../setup'
 
-// Match waitForEditable's default: parallel Puppeteer runs can make browser round trips exceed 1s.
-const editableReadyTimeout = 6000
-
 /** Type text on the keyboard. To press a key (optionally with modifiers), see the press helper. */
 const keyboard = {
   // export keyboard object because 'type' is a reserved word and cannot be used as a function name
