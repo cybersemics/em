e3d36d04c083fe3de67c79053840ea1ba246b091
Ethan James <ethan.e.james@gmail.com>
2026-06-16 13:27:35 -0700

    Move tap down way too far

--- src/e2e/iOS/__tests__/caret.ts
+++ src/e2e/iOS/__tests__/caret.ts
@@ -268,7 +268,7 @@ describe('Caret', () => {
     await clickThought('Hello')
 
     const editableNodeHandleWorld = await waitForEditable('World')
-    await tap(editableNodeHandleWorld, { horizontalTapLine: 'right', y: 44 })
+    await tap(editableNodeHandleWorld, { horizontalTapLine: 'right', y: 200 })
 
     await clickThought('Hello')
 
