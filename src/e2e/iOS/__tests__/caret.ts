0573ddce09dd8f3b227473b5a241164fc1cf7308
Ethan James <ethan.e.james@gmail.com>
2026-06-29 13:35:18 -0700

    Nudge x position of tap

--- src/e2e/iOS/__tests__/caret.ts
+++ src/e2e/iOS/__tests__/caret.ts
@@ -260,7 +260,7 @@ describe('Caret', () => {
     const homeNodeHandle = await waitForElement('[data-testid="home"]')
     await tap(homeNodeHandle)
 
-    await tap(await waitForEditable('Hello'), { horizontalTapLine: 'right', x: 5, pointerType: 'touch' })
+    await tap(await waitForEditable('Hello'), { horizontalTapLine: 'right', x: 6, pointerType: 'touch' })
 
     const activeElementIsBody = await browser.execute(() => document.activeElement === document.body)
     expect(activeElementIsBody).toBe(true)
