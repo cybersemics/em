6962fb68d8357a1a29099557a3f14b697856fcf6
Marcus Pousette <marcus.pousette@live.com>
2026-07-06 17:43:07 +0200

    Fix context view root sort stability

--- src/selectors/getContextsSortedAndRanked.ts
+++ src/selectors/getContextsSortedAndRanked.ts
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import State from '../@types/State'
 import Thought from '../@types/Thought'
 import ThoughtId from '../@types/ThoughtId'
+import { HOME_DISPLAY_VALUE } from '../constants'
 import getContexts from '../selectors/getContexts'
 import getThoughtById from '../selectors/getThoughtById'
 import isRoot from '../util/isRoot'
@@ -51,9 +52,11 @@ const getContextsSortedAndRanked = (state: State, value: string): Thought[] => {
     const parent = getThoughtById(state, thought.parentId)
     const breadcrumbThoughts = childIdsToThoughts(state, breadcrumbs)
     if (!breadcrumbThoughts.every(nonNull)) return MISSING_TOKEN
-    const encodedBreadcrumbs = breadcrumbThoughts
-      .map(thought => (thought ? thought.value : MISSING_TOKEN))
-      .join('\x00SEP2')
+    // Root token values are provider implementation details. Sort the root as its display value so
+    // context order does not change between providers that use different root ids.
+    const encodedBreadcrumbs = isRoot(breadcrumbs)
+      ? HOME_DISPLAY_VALUE
+      : breadcrumbThoughts.map(thought => (thought ? thought.value : MISSING_TOKEN)).join('\x00SEP2')
     // for contexts that have the same ancestors, we need to sort by the rendered value (parent.value)
     // use SEP1 to ensure the value is sorted ahead of the breadcrumbs separator SEP2
     return [encodedBreadcrumbs, parent?.value || ''].join('\x00SEP1')
