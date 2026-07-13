11661bba7d2f13dfa5aad1b9a3d84ee09e3808c8
Marcus Pousette <marcus.pousette@live.com>
2026-07-13 18:56:26 +0200

    Inline Puppeteer initialization waits

--- src/e2e/puppeteer/helpers/waitForAppReady.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from 'puppeteer'
-import type { WindowEm } from '../../../initialize'
-
-/** Waits for asynchronous thoughtspace and cursor initialization. */
-const waitForAppReady = async (page: Page): Promise<void> => {
-  await page.evaluate(() => (window.em as WindowEm).testHelpers.waitForInitialized())
-}
-
-export default waitForAppReady
