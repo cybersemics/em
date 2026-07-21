92c779dc12ba21a9ebd3c074ac56dddf4c2eb324
Marcus Pousette <marcus.pousette@live.com>
2026-07-21 10:24:54 +0200

    Remove unrelated Puppeteer URL override

--- src/e2e/puppeteer/setup.ts
+++ src/e2e/puppeteer/setup.ts
@@ -55,7 +55,7 @@ export const usePersistentTreecrdtStorage = ({ runtime = 'direct' }: { runtime?:
 const setup = async ({
   puppeteerBrowser = global.browser,
   // Use host.docker.internal to connect to the host machine from inside the container. On Github actions, host.docker.internal is not available, so use 172.17.0.1 instead.
-  url = process.env.PUPPETEER_URL || (process.env.CI ? 'https://172.17.0.1:3000' : 'https://host.docker.internal:2552'),
+  url = process.env.CI ? 'https://172.17.0.1:3000' : 'https://host.docker.internal:2552',
   // url = 'https://google.com',
   emulatedDevice,
   skipTutorial = true,
